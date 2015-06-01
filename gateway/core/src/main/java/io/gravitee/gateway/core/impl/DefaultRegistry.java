82648b41e0017d396b19ca2d2dbc23deef354b66
David BRASSELY <david@MacBook-Pro-de-David-2.local>
2015-06-01 09:07:25 +0200

    refactor: Add default registery implementation

---
+++ gateway/core/src/main/java/io/gravitee/gateway/core/impl/DefaultRegistry.java
@@ -0,0 +1,102 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.gateway.core.impl;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.gravitee.model.Api;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Default API registry.
+ * This registry is based on JSON processed files to provide Gateway configuration.
+ *
+ * @author David BRASSELY (brasseld at gmail.com)
+ */
+public class DefaultRegistry extends AbstractRegistry {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DefaultRegistry.class);
+
+    private final static String JSON_EXTENSION = ".json";
+
+    public DefaultRegistry() {
+        this(System.getProperty("gateway.conf", "/etc/gravitee.io/conf"));
+    }
+
+    public DefaultRegistry(String configurationPath) {
+        File configuration = new File(configurationPath);
+
+        if (configuration.exists()) {
+            readConfiguration(configuration);
+        } else {
+            LOGGER.error("No configuration can be read from {}",
+                    configuration.getAbsolutePath());
+        }
+    }
+
+    private void readConfiguration(File configuration) {
+        LOGGER.info("Loading Gravitee configuration from {}", configuration.getAbsolutePath());
+
+        Set<File> configurations = lookingForConfigurationFiles(configuration);
+
+        // Initialize Jackson mapper to read json files
+        ObjectMapper mapper = new ObjectMapper();
+
+        // Read all configuration files
+        for(File conf : configurations) {
+            try {
+                register(mapper.readValue(conf, Api.class));
+            } catch (IOException ioe) {
+                LOGGER.error("Unable to read file : {}", conf, ioe);
+            }
+        }
+
+        LOGGER.info("{} API(s) registered", listAll().size());
+    }
+
+    private Set<File> lookingForConfigurationFiles(File configuration) {
+        if (configuration.isFile()) {
+            LOGGER.debug("Provided configuration is a file...");
+
+            // Check if provided file is suffixed with .json
+            if (! configuration.getName().endsWith(JSON_EXTENSION)) {
+                LOGGER.error("Configuration file is not a JSON file (does not end with .json)");
+                throw new IllegalStateException("Configuration file is not a JSON file (does not end with .json)");
+            }
+
+            return Collections.singleton(configuration);
+        } else {
+            LOGGER.debug("Provided configuration is a directory, looking for json files.");
+            File [] confs = configuration.listFiles(new FileFilter() {
+                @Override
+                public boolean accept(File pathname) {
+                    return pathname.getName().endsWith(JSON_EXTENSION);
+                }
+            });
+
+
+            return new HashSet<File>(Arrays.asList(confs));
+        }
+    }
+}
---
+++ model/src/main/java/io/gravitee/model/Api.java
@@ -0,0 +1,93 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import java.util.Objects;
+
+/**
+ * @author David BRASSELY (brasseld at gmail.com)
+ */
+public class Api {
+
+    private String name;
+
+    @JsonIgnore
+    private String version = "unknown";
+
+    private String contextPath;
+
+    @JsonIgnore
+    private boolean enabled = true;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public String getContextPath() {
+        return contextPath;
+    }
+
+    public void setContextPath(String contextPath) {
+        this.contextPath = contextPath;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Api api = (Api) o;
+        return Objects.equals(name, api.name) &&
+                Objects.equals(version, api.version);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, version);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Api{");
+        sb.append("name='").append(name).append('\'');
+        sb.append(", version='").append(version).append('\'');
+        sb.append(", contextPath='").append(contextPath).append('\'');
+        sb.append(", enabled=").append(enabled);
+        sb.append('}');
+        return sb.toString();
+    }
+}
