4cb5d873cead2d467ca314fc46da59b7101a8a8f
Azize Elamrani <azize.elamrani@gmail.com>
2015-07-09 23:58:01 +0200

    refactor: Renamed GAV for coherency purpose

--- src/app/main/main.controller.spec.js
+++ src/app/main/main.controller.spec.js
@@ -22,9 +22,6 @@
 
     it('should define more than 5 awesome things', inject(function($controller) {
       var vm = $controller('MainController');
-
-      expect(angular.isArray(vm.awesomeThings)).toBeTruthy();
-      expect(vm.awesomeThings.length > 5).toBeTruthy();
     }));
   });
 })();
