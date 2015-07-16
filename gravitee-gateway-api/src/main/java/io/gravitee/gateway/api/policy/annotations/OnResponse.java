faacc8a59dedf51cfd896154a612d7ccdada9a24
brasseld <brasseld@gmail.com>
2015-07-16 20:36:10 +0200

    feat(policy): Add annotations to handle request flow / response flow in a policy

---
+++ gravitee-gateway-api/src/main/java/io/gravitee/gateway/api/policy/annotations/OnResponse.java
@@ -0,0 +1,14 @@
+package io.gravitee.gateway.api.policy.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author David BRASSELY (brasseld at gmail.com)
+ */
+@Target({ ElementType.METHOD })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface OnResponse {
+}
