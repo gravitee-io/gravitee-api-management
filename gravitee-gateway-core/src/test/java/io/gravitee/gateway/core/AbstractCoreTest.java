3800a80b9892f479164f2d56b091ed8631249f6c
brasseld <brasseld@gmail.com>
2015-07-23 12:11:51 +0200

    refactor: gravitee.yml must not be part of classpath

--- gravitee-gateway-core/src/test/java/io/gravitee/gateway/core/AbstractCoreTest.java
+++ gravitee-gateway-core/src/test/java/io/gravitee/gateway/core/AbstractCoreTest.java
@@ -38,8 +38,11 @@ public abstract class AbstractCoreTest implements ApplicationContextAware {
     protected ApplicationContext applicationContext;
 
     public AbstractCoreTest() {
-        URL home = AbstractCoreTest.class.getResource("/io/gravitee/gateway/core/policy");
-        System.setProperty("gravitee.home", home.getPath());
+        System.setProperty("gravitee.home",
+                AbstractCoreTest.class.getResource("/io/gravitee/gateway/core/policy").getPath());
+
+        System.setProperty("gravitee.conf",
+                AbstractCoreTest.class.getResource("/gravitee.yml").getPath());
     }
 
     @Configuration
