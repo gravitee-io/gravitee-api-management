031e49583bdcb6cceb9b6d3ae4409c304bcd0c14
brasseld <brasseld@gmail.com>
2015-08-03 13:29:15 +0200

    Initial API implementation

---
+++ src/main/java/io/gravitee/repository/model/Node.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.repository.model;
+
+import java.util.Date;
+
+/**
+ * @author David BRASSELY (brasseld at gmail.com)
+ */
+public class Node {
+
+    private String name;
+    private String host;
+    private Date startedOn;
+    private String cluster;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCluster() {
+        return cluster;
+    }
+
+    public void setCluster(String cluster) {
+        this.cluster = cluster;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public Date getStartedOn() {
+        return startedOn;
+    }
+
+    public void setStartedOn(Date startedOn) {
+        this.startedOn = startedOn;
+    }
+}
