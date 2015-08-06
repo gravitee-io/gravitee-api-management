7673b3c4e2300e832c8e88f2c5acb6299bef6418
Loic dassonville <loic.dassonville@gmail.com>
2015-08-06 22:46:28 +0200

    feat(api_repository): Added api repository implementation tests

--- src/test/java/io/gravitee/repositories/mongodb/ApiRepositoryTest.java
+++ src/test/java/io/gravitee/repositories/mongodb/ApiRepositoryTest.java
@@ -16,7 +16,9 @@
 package io.gravitee.repositories.mongodb;
 
 import java.util.List;
+import java.util.Set;
 
+import org.bson.types.ObjectId;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -26,49 +28,108 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import io.gravitee.repositories.mongodb.internal.api.ApiRepository;
 import io.gravitee.repositories.mongodb.internal.model.Api;
+import io.gravitee.repositories.mongodb.internal.model.Team;
+import io.gravitee.repositories.mongodb.internal.model.User;
+import io.gravitee.repositories.mongodb.internal.team.TeamRepository;
+import io.gravitee.repositories.mongodb.internal.user.UserRepository;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes={ RepositoryConfiguration.class})
+@ContextConfiguration(classes = { RepositoryConfiguration.class })
 public class ApiRepositoryTest {
 
 	@Autowired
-    private ApiRepository  repository;
-    
-	@Test 
-	public void createApiTest(){
-		
-    	Api api = new Api();
-    	api.setName("sample");
-    	api.setVersion("1");
-    	repository.save(api);
+	private ApiRepository apiRepository;
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private TeamRepository teamRepository;
+
+	@Test
+	public void createApiTest() {
+
+		try {
+			User creator = new User();
+			creator.setMail("sample@mail.com");
+			creator.setUsername("myusername");
+			userRepository.save(creator);
+
+			Team owner = new Team();
+			owner.setName("myteamname");
+			owner.setDescription("Team used for unit tests");
+			teamRepository.save(owner);
+			
+			Api api = new Api();
+			api.setName("sample");
+			api.setVersion("1");
+			api.setCreator(creator);
+			api.setOwner(owner);
+
+			apiRepository.save(api);
+		} catch (Exception e) {
+			e.printStackTrace();
+			Assert.fail();
+		}
+	}
+
+	@Test
+	public void findByCreatorIdTest() {
+		List<Api> apis = apiRepository.findByCreatorId(new ObjectId("55c346a8d4c60e0dd348183d"));
+		System.out.println(apis);
+	
+		Assert.assertNotNull(apis);
+	}
+
+	@Test
+	public void findByCreatorNameTest() {
+		Set<Api> apis = apiRepository.findByCreator("testcaseusername");
+		System.out.println(apis);
+	
+		Assert.assertNotNull(apis);
 	}
 	
-    @Test 
-    public void findByNameTest(){
-    	Api api = repository.findByName("sample");
-    	Assert.assertNotNull(api);
-    }
-    
-    @Test
-    public void start(){
-    	repository.start("sample");
-    }
-     
-    @Test
-    public void stop(){
-    	repository.stop("sample");
-    }
-    
-    @Test
-    public void findAll(){
-    	List<Api> apis = repository.findAll();
-    	Assert.assertNotNull(apis);
-    }
-    
 	@Test
-	public void deleteApiTest(){
-		repository.delete("sample");
+	public void findByTeamNameTest() {
+		Set<Api> apis = apiRepository.findByTeam("testcaseteamname");
+		System.out.println(apis);
+	
+		Assert.assertNotNull(apis);
 	}
-    
+	
+	@Test
+	public void findAllTest() {
+		List<Api> apis = apiRepository.findAll();
+		
+		Assert.assertNotNull(apis);
+		Assert.assertFalse("Fail to resolve api in findAll", apis.isEmpty());
+	}	
+	
+	@Test
+	public void findByNameTest() {
+		Api api = apiRepository.findByName("sample");
+		Assert.assertNotNull(api);
+	}
+
+	@Test
+	public void start() {
+		apiRepository.start("sample");
+	}
+
+	@Test
+	public void stop() {
+		apiRepository.stop("sample");
+	}
+
+	@Test
+	public void findAll() {
+		List<Api> apis = apiRepository.findAll();
+		Assert.assertNotNull(apis);
+	}
+
+	@Test
+	public void deleteApiTest() {
+		apiRepository.delete("sample");
+	}
 
 }
