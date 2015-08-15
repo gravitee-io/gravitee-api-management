e24340899108281b12c45395e8049b7f20707d07
brasseld <brasseld@gmail.com>
2015-08-16 00:45:43 +0200

    feat(team): better management of teams and private teams

---
+++ rest/src/main/java/io/gravitee/management/api/model/UpdateTeamEntity.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.management.api.model;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+import javax.validation.constraints.NotNull;
+
+/**
+ * @author David BRASSELY (brasseld at gmail.com)
+ */
+@JsonSerialize(include=JsonSerialize.Inclusion.NON_NULL)
+public class UpdateTeamEntity {
+
+    @NotNull
+    private String description;
+
+    @NotNull
+    private String email;
+
+    private boolean isPrivate;
+
+    public boolean isPrivate() {
+        return isPrivate;
+    }
+
+    public void setPrivate(boolean isPrivate) {
+        this.isPrivate = isPrivate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
