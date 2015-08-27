121650f2277787cc6e7b3213c12d4015c104670e
tcompiegne <titouan.compiegne@gmail.com>
2015-08-28 00:54:52 +0200

    fix(corsfilter) : temporary fix for cors filter with spring security filter : spring security filter seems to be invoked before ResponseCORSFilter (Jersey Filter)

---
+++ security/src/main/java/io/gravitee/management/security/filter/CORSFilter.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.management.security.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 
+ * @author Titouan COMPIEGNE
+ * 
+ * TODO : fix filter order between Jersey Filter (CORSResponseFilter) and Spring Security Filter
+ * TODO : remove this filter or CORSResponseFilter when the problem will be solved
+ *
+ */
+public class CORSFilter implements Filter {
+	
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+		HttpServletResponse response = (HttpServletResponse) res;
+		HttpServletRequest request = (HttpServletRequest) req;
+		response.setHeader("Access-Control-Allow-Origin", "*");
+		response.setHeader("Access-Control-Allow-Credentials", "true");
+		response.setHeader("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With");
+		response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE, X-XSRF-TOKEN");
+		response.setHeader("Access-Control-Max-Age", "1209600");
+		if (!"OPTIONS".equals(request.getMethod())) {
+			chain.doFilter(req, res);
+		} else {
+		}
+	}
+
+	public void init(FilterConfig filterConfig) {
+	}
+
+	public void destroy() {
+	}
+
+}
