0b3542f23cec8f0736ff03bca4c380152838de73
brasseld <brasseld@gmail.com>
2015-08-29 01:26:20 +0200

    feat(sync): Provide implementation algorithm to calculate sync and relative uni tests

---
+++ gravitee-gateway-core/src/main/java/io/gravitee/gateway/core/sync/impl/SyncManager.java
@@ -0,0 +1,118 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.gateway.core.sync.impl;
+
+import io.gravitee.gateway.core.model.Api;
+import io.gravitee.gateway.core.model.ApiLifecycleState;
+import io.gravitee.repository.api.ApiRepository;
+import io.gravitee.repository.exceptions.TechnicalException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * @author David BRASSELY (brasseld at gmail.com)
+ */
+public class SyncManager {
+
+    /**
+     * Logger.
+     */
+    private final Logger LOGGER = LoggerFactory.getLogger(SyncManager.class);
+
+    @Autowired
+    private ApiRepository apiRepository;
+
+    private final Map<String, Api> cache = new HashMap<>();
+
+    public void refresh() {
+        try {
+            Set<io.gravitee.repository.model.Api> apis = apiRepository.findAll();
+
+            Map<String, Api> apisMap = apis.stream()
+                    .map(api -> convert(api))
+                    .collect(Collectors.toMap(Api::getName, api -> api));
+
+            // Determine APIs to remove
+            Set<String> apiToRemove = cache.keySet().stream()
+                    .filter(apiName -> !apisMap.containsKey(apiName))
+                    .collect(Collectors.toSet());
+
+            apiToRemove.stream().forEach(apiName -> remove(cache.get(apiName)));
+
+            // Determine APIs to update
+            apisMap.keySet().stream()
+                    .filter(apiName -> cache.containsKey(apiName))
+                    .forEach(apiName -> {
+                        // Get local cached API
+                        Api cachedApi = cache.get(apiName);
+
+                        // Get API from store
+                        Api remoteApi = apisMap.get(apiName);
+
+                        if (cachedApi.getUpdatedAt().before(remoteApi.getUpdatedAt())) {
+                            update(cachedApi);
+                        }
+                    });
+
+            // Determine APIs to create
+            apisMap.keySet().stream()
+                    .filter(apiName -> !cache.containsKey(apiName))
+                    .forEach(apiName -> add(apisMap.get(apiName)));
+
+        } catch (TechnicalException te) {
+            te.printStackTrace();
+        }
+    }
+
+    public void remove(Api api) {
+        LOGGER.info("{} has been removed.", api);
+
+        cache.remove(api.getName());
+    }
+
+    public void add(Api api) {
+        LOGGER.info("{} has been added.", api);
+
+        cache.put(api.getName(), api);
+    }
+
+    public void update(Api api) {
+        LOGGER.info("{} has been updated.", api);
+    }
+
+    private Api convert(io.gravitee.repository.model.Api remoteApi) {
+        Api api = new Api();
+
+        api.setName(remoteApi.getName());
+        api.setPublicURI(remoteApi.getPublicURI());
+        api.setTargetURI(remoteApi.getTargetURI());
+        api.setCreatedAt(remoteApi.getCreatedAt());
+        api.setUpdatedAt(remoteApi.getUpdatedAt());
+        api.setState(ApiLifecycleState.START);
+
+        return api;
+    }
+
+    public void setApiRepository(ApiRepository apiRepository) {
+        this.apiRepository = apiRepository;
+    }
+}
---
+++ gravitee-gateway-core/src/test/java/io/gravitee/gateway/core/builder/RepositoryApiBuilder.java
@@ -0,0 +1,60 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.gateway.core.builder;
+
+
+import io.gravitee.repository.model.Api;
+import io.gravitee.repository.model.LifecycleState;
+
+import java.net.URI;
+import java.util.Date;
+
+/**
+ * @author David BRASSELY (brasseld at gmail.com)
+ */
+public class RepositoryApiBuilder {
+
+    private final Api api = new Api();
+
+    public RepositoryApiBuilder name(String name) {
+        this.api.setName(name);
+        return this;
+    }
+
+    public RepositoryApiBuilder target(String target) {
+        this.api.setTargetURI(URI.create(target));
+        return this;
+    }
+
+    public RepositoryApiBuilder origin(String origin) {
+        this.api.setPublicURI(URI.create(origin));
+        return this;
+    }
+
+    public RepositoryApiBuilder start() {
+        this.api.setLifecycleState(LifecycleState.STARTED);
+        return this;
+    }
+
+    public RepositoryApiBuilder updatedAt(Date updatedAt) {
+        this.api.setUpdatedAt(updatedAt);
+        return this;
+    }
+
+    public Api build() {
+        return this.api;
+    }
+}
