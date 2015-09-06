0fdf1b6728c065ee4b121628837eb0a2ad4cbb71
tcompiegne <titouan.compiegne@gmail.com>
2015-09-06 15:43:47 +0100

    feat(documentation) : add Page model, PageType model PageRepository

---
+++ src/main/java/io/gravitee/repository/model/Page.java
@@ -0,0 +1,114 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.repository.model;
+
+import java.util.Date;
+
+/**
+ * @author Titouan COMPIEGNE
+ */
+public class Page {
+
+	private String name;
+
+	private PageType type;
+
+	private String title;
+
+	private String content;
+
+	private String lastContributor;
+	
+	private int order;
+	
+	private String apiName;
+
+	private Date createdAt;
+
+	private Date updatedAt;
+
+	public PageType getType() {
+		return type;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setType(PageType type) {
+		this.type = type;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getLastContributor() {
+		return lastContributor;
+	}
+
+	public void setLastContributor(String lastContributor) {
+		this.lastContributor = lastContributor;
+	}
+	
+	public int getOrder() {
+		return order;
+	}
+
+	public void setOrder(int order) {
+		this.order = order;
+	}
+
+	public String getApiName() {
+		return apiName;
+	}
+
+	public void setApiName(String apiName) {
+		this.apiName = apiName;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+}
