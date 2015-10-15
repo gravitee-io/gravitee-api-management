943b27f4e8f40daa691a1491ce8c1c625a109549
brasseld <brasseld@gmail.com>
2015-10-15 03:22:46 +0200

    feat(ratelimit): Add Repository API to store RateLimit data

---
+++ src/main/java/io/gravitee/repository/ratelimit/api/RateLimitRepository.java
@@ -0,0 +1,29 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.repository.ratelimit.api;
+
+import io.gravitee.repository.ratelimit.model.RateLimitResult;
+
+import java.io.Serializable;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author David BRASSELY (brasseld at gmail.com)
+ */
+public interface RateLimitRepository<T extends Serializable> {
+
+    RateLimitResult acquire(T key, int pound, long limit, long periodTime, TimeUnit periodTimeUnit);
+}
---
+++ src/main/java/io/gravitee/repository/ratelimit/model/RateLimitResult.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.repository.ratelimit.model;
+
+/**
+ * @author David BRASSELY (brasseld at gmail.com)
+ */
+public class RateLimitResult {
+
+    private boolean exceeded;
+
+    private long remains;
+
+    private long resetTime;
+
+    public boolean isExceeded() {
+        return exceeded;
+    }
+
+    public void setExceeded(boolean exceeded) {
+        this.exceeded = exceeded;
+    }
+
+    public long getRemains() {
+        return remains;
+    }
+
+    public void setRemains(long remains) {
+        this.remains = remains;
+    }
+
+    public long getResetTime() {
+        return resetTime;
+    }
+
+    public void setResetTime(long resetTime) {
+        this.resetTime = resetTime;
+    }
+}
