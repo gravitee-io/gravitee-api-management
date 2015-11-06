4fdeccc8b1d97df474bf2f68dd78d33699db8e4b
tcompiegne <titouan.compiegne@gmail.com>
2015-11-06 19:31:18 +0100

    style: refactor api documentation page and set white background for the content app

--- src/app/api/api.controller.js
+++ src/app/api/api.controller.js
@@ -32,14 +32,15 @@ class ApiController {
 
     this.selectedPolicy = null;
 
-
-
+    // Chart Part (static values)
     $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
     $scope.series = ['Series A', 'Series B'];
     $scope.data = [
       [65, 59, 80, 81, 56, 55, 40],
       [28, 48, 40, 19, 86, 27, 90]
     ];
+    $scope.labelsPie = ["200", "404", "500"];
+    $scope.dataPie = [500, 50, 100];
   }
 
   get(apiName) {
--- src/app/index.route.js
+++ src/app/index.route.js
@@ -69,6 +69,12 @@ function routerConfig ($stateProvider, $urlRouterProvider) {
       templateUrl: 'app/documentation/documentation.html',
       controller: 'DocumentationController',
       controllerAs: 'documentationCtrl'
+    })
+    .state('profile', {
+      url: '/profile',
+      templateUrl: 'app/profile/profile_stub.html',
+      controller: 'ProfileController',
+      controllerAs: 'profileCtrl'
     });
 
   $urlRouterProvider.otherwise('/');
