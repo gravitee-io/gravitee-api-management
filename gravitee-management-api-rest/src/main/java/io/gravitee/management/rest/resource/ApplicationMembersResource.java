0ab6cb5ca4b3831976dcce68214c228978a6c99c
brasseld <brasseld@gmail.com>
2015-11-20 17:18:59 +0100

    feat(member): Add members list per application

---
+++ gravitee-management-api-rest/src/main/java/io/gravitee/management/rest/resource/ApplicationMembersResource.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.management.rest.resource;
+
+import io.gravitee.management.model.MemberEntity;
+import io.gravitee.management.service.ApiService;
+import io.gravitee.management.service.ApplicationService;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.Set;
+
+/**
+ * @author David BRASSELY (brasseld at gmail.com)
+ */
+public class ApplicationMembersResource {
+
+    @Inject
+    private ApplicationService applicationService;
+
+    @PathParam("applicationName")
+    private String applicationName;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Set<MemberEntity> members() {
+        // Check that the API exists
+        applicationService.findByName(applicationName);
+
+        return applicationService.getMembers(applicationName, null);
+    }
+}
