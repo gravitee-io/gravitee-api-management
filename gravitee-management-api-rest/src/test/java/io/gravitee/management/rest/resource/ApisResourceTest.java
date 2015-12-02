4a475d464a54c6c1dd2bad702dfd0230bfad35e3
brasseld <brasseld@gmail.com>
2015-12-02 23:37:59 +0100

    fix: Fix UTs

--- gravitee-management-api-rest/src/test/java/io/gravitee/management/rest/resource/ApisResourceTest.java
+++ gravitee-management-api-rest/src/test/java/io/gravitee/management/rest/resource/ApisResourceTest.java
@@ -16,15 +16,19 @@
 package io.gravitee.management.rest.resource;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.doReturn;
 
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.Response;
 
+import io.gravitee.definition.model.Proxy;
+import io.gravitee.management.model.ApiEntity;
 import io.gravitee.management.model.NewApiEntity;
 import org.junit.Ignore;
 import org.junit.Test;
 
 import io.gravitee.common.http.HttpStatusCode;
+import org.mockito.Mockito;
 
 /**
  * @author David BRASSELY (brasseld at gmail.com)
@@ -45,6 +49,14 @@ public class ApisResourceTest extends AbstractResourceTest {
     public void testPostApi_newApiWithoutPath() {
         final NewApiEntity apiEntity = new NewApiEntity();
         apiEntity.setName("My beautiful api");
+        apiEntity.setVersion("v1");
+        apiEntity.setDescription("my description");
+        apiEntity.setProxy(new Proxy());
+
+        ApiEntity returnedApi = new ApiEntity();
+        returnedApi.setId("my-beautiful-api");
+        doReturn(returnedApi).when(apiService).create(Mockito.any(NewApiEntity.class),
+                Mockito.eq(USER_NAME));
 
         final Response response = target().request().post(Entity.json(apiEntity));
         assertEquals(HttpStatusCode.CREATED_201, response.getStatus());
