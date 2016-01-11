1ce9926eb50acb807b86a24d9b1e76f79b8f6af9
brasseld <brasseld@gmail.com>
2016-01-11 08:57:48 +0100

    refactor: Remove the strip context path feature from UI because of the lack of comprehension'

--- src/app/api/admin/general/apiGeneral.controller.js
+++ src/app/api/admin/general/apiGeneral.controller.js
@@ -93,14 +93,6 @@ class ApiAdminController {
       this.NotificationService.show('API ' + this.initialApi.name + ' has been updated !');
     });
   }
-
-  getStripContextExample() {
-    var endpoint = this.$scope.$parent.apiCtrl.api.proxy.endpoint;
-    if (!this.$scope.$parent.apiCtrl.api.proxy.strip_context_path) {
-      endpoint += this.$scope.$parent.apiCtrl.api.proxy.context_path;
-    }
-    return endpoint + '/*';
-  }
 }
 
 export default ApiAdminController;
