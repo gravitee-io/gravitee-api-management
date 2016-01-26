72c3079c088b81f4bd0a033cbf9f65c537faf98d
Ludovic Dussart <ludovic.dussart.pro@gmail.com>
2016-01-26 16:48:34 +0100

    feat(page): add PageType to Page model
    this commit add a PageType enum containing supported type for writing
    documentations like MD, SWAGGER, etc
    
    Breaking changes:
    - type attribute in Page.java is now of type PageType

---
+++ src/main/java/io/gravitee/repository/management/model/PageType.java
@@ -0,0 +1,14 @@
+package io.gravitee.repository.management.model;
+
+/**
+ * @author Ludovic Dussart (ludovic.dussart at gmail.com)
+ * Managed types for page documentation
+ *
+ */
+public enum PageType {
+
+	MARKDOWN,
+	RAML,
+	SWAGGER
+	
+}
