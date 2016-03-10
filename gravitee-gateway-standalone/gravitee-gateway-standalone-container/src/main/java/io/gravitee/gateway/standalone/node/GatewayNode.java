2a210d4639f4f5963fcdb40a59c6b1d352a8bd23
brasseld <brasseld@gmail.com>
2016-03-10 22:58:39 +0100

    feat(node): Add more informations when node is starting

--- gravitee-gateway-standalone/gravitee-gateway-standalone-container/src/main/java/io/gravitee/gateway/standalone/node/GatewayNode.java
+++ gravitee-gateway-standalone/gravitee-gateway-standalone-container/src/main/java/io/gravitee/gateway/standalone/node/GatewayNode.java
@@ -17,19 +17,30 @@ package io.gravitee.gateway.standalone.node;
 
 import io.gravitee.common.component.LifecycleComponent;
 import io.gravitee.common.node.AbstractNode;
+import io.gravitee.common.util.Version;
 import io.gravitee.common.utils.UUIDGenerator;
 import io.gravitee.gateway.core.Reactor;
 import io.gravitee.gateway.standalone.vertx.VertxEmbeddedContainer;
 import io.gravitee.repository.management.api.EventRepository;
 import io.gravitee.repository.management.model.Event;
 import io.gravitee.repository.management.model.EventType;
+import org.springframework.beans.factory.annotation.Value;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * @author David BRASSELY (brasseld at gmail.com)
  */
 public class GatewayNode extends AbstractNode {
+
+    private static final String TAGS_PROP = "tags";
+    private static final String TAGS_DELIMITER = ",";
+
+    @Value("${tags:}")
+    private String propertyTags;
 
     @Override
     public String name() {
@@ -64,12 +75,28 @@ public class GatewayNode extends AbstractNode {
         event.setType(eventType);
         event.setCreatedAt(new Date());
         Map<String, String> properties = new HashMap<>();
-        properties.put("gateway_id", this.id());
+        properties.put("id", this.id());
+        if (eventType == EventType.START_GATEWAY) {
+            properties.put("version", Version.RUNTIME_VERSION.toString());
+            properties.put("tags", tags().stream().collect(Collectors.joining(TAGS_DELIMITER)));
+            try {
+                properties.put("hostname", InetAddress.getLocalHost().getHostName());
+                properties.put("ip", InetAddress.getLocalHost().getHostAddress());
+            } catch (UnknownHostException uhe) {}
+        }
         event.setProperties(properties);
         try {
             applicationContext.getBean(EventRepository.class).create(event);
         } catch (Exception ex) {
             LOGGER.error("Unexpected error while sending a {} event into the repository", eventType, ex);
         }
+    }
+
+    public List<String> tags() {
+        final String systemPropertyTags = System.getProperty(TAGS_PROP);
+        final String tags = systemPropertyTags == null ? propertyTags : systemPropertyTags;
+        return (tags == null || tags.isEmpty()) ?
+                Collections.EMPTY_LIST :
+                Arrays.asList(tags.trim().split(TAGS_DELIMITER));
     }
 }
