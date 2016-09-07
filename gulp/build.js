f82c6af4567b1c31051247f7d86bc1567a506534
Azize Elamrani <azize.elamrani@gmail.com>
2016-09-07 10:20:14 +0200

    chore: Remove comments on generated JS (license header...)

--- gulp/build.js
+++ gulp/build.js
@@ -59,7 +59,7 @@ gulp.task('html', ['inject', 'partials'], function () {
     .pipe($.rev())
     .pipe(jsFilter)
     .pipe($.ngAnnotate())
-    .pipe($.uglify({ preserveComments: $.uglifySaveLicense })).on('error', conf.errorHandler('Uglify'))
+    .pipe($.uglify({ preserveComments: false })).on('error', conf.errorHandler('Uglify'))
     .pipe(jsFilter.restore())
     .pipe(cssFilter)
     .pipe($.csso())
