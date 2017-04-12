a19b62ff6c308c9b085eceaf00bb9bab13428504
brasseld <brasseld@gmail.com>
2017-04-12 09:18:16 +0200

    fix(healthcheck): Service implementation using Vert.x to replace async-http-client
    Closes gravitee-io/issues#536

--- gravitee-gateway-services/gravitee-gateway-services-healthcheck/src/main/java/io/gravitee/gateway/services/healthcheck/EndpointHealthCheck.java
+++ gravitee-gateway-services/gravitee-gateway-services-healthcheck/src/main/java/io/gravitee/gateway/services/healthcheck/EndpointHealthCheck.java
@@ -18,13 +18,22 @@ package io.gravitee.gateway.services.healthcheck;
 import io.gravitee.common.http.HttpStatusCode;
 import io.gravitee.definition.model.Api;
 import io.gravitee.definition.model.Endpoint;
+import io.gravitee.definition.model.HttpClientSslOptions;
+import io.gravitee.definition.model.HttpProxy;
 import io.gravitee.definition.model.services.healthcheck.HealthCheck;
 import io.gravitee.gateway.el.function.JsonPathFunction;
 import io.gravitee.gateway.report.ReporterService;
 import io.gravitee.gateway.services.healthcheck.el.HealthCheckResponse;
 import io.gravitee.reporter.api.health.HealthStatus;
-import org.asynchttpclient.*;
-import org.asynchttpclient.uri.Uri;
+import io.netty.channel.ConnectTimeoutException;
+import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpClient;
+import io.vertx.core.http.HttpClientOptions;
+import io.vertx.core.http.HttpClientRequest;
+import io.vertx.core.http.HttpMethod;
+import io.vertx.core.net.PemTrustOptions;
+import io.vertx.core.net.ProxyOptions;
+import io.vertx.core.net.ProxyType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
@@ -34,7 +43,7 @@ import org.springframework.expression.spel.SpelEvaluationException;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
 import org.springframework.expression.spel.support.StandardEvaluationContext;
 
-import java.net.SocketTimeoutException;
+import java.net.URI;
 import java.util.Iterator;
 
 /**
@@ -43,90 +52,137 @@ import java.util.Iterator;
  */
 class EndpointHealthCheck implements Runnable {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(EndpointHealthCheck.class);
+    private final Logger logger = LoggerFactory.getLogger(EndpointHealthCheck.class);
 
-    private static final int GLOBAL_TIMEOUT = 2000;
+    private static final String HTTPS_SCHEME = "https";
 
     private ReporterService reporterService;
 
-    private final AsyncHttpClient asyncHttpClient;
     private final Api api;
     private final HealthCheck healthCheck;
 
     private final EndpointStatusManager statusManager = new EndpointStatusManager();
+    private final Vertx vertx;
 
-    EndpointHealthCheck(Api api) {
+    EndpointHealthCheck(Vertx vertx, Api api) {
+        this.vertx = vertx;
         this.api = api;
         this.healthCheck = api.getServices().get(HealthCheck.class);
-        asyncHttpClient = createAsyncHttpClient();
     }
 
     @Override
     public void run() {
-        LOGGER.debug("Runnning health-check for {}", api);
-
-        // Prepare request
-        RequestBuilder requestBuilder = new org.asynchttpclient.RequestBuilder(
-                healthCheck.getRequest().getMethod().name());
-
-        if (healthCheck.getRequest().getHeaders() != null) {
-            healthCheck.getRequest().getHeaders().forEach(
-                    httpHeader -> requestBuilder.setHeader(httpHeader.getName(), httpHeader.getValue()));
-        }
-
-        if (healthCheck.getRequest().getBody() != null && !healthCheck.getRequest().getBody().isEmpty()) {
-            requestBuilder.setBody(healthCheck.getRequest().getBody());
-        }
+        logger.debug("Running health-check for {}", api);
 
         // Run request for each endpoint
         if (api.getProxy().getEndpoints() != null) {
             for (Endpoint endpoint : api.getProxy().getEndpoints()) {
                 if (endpoint.isHealthcheck()) {
-                    String requestUri = endpoint.getTarget() + healthCheck.getRequest().getUri();
-                    requestBuilder.setUri(Uri.create(requestUri));
+                    // Prepare HTTP client
+                    HttpClientOptions httpClientOptions = new HttpClientOptions()
+                            .setMaxPoolSize(1)
+                            .setKeepAlive(false)
+                            .setTcpKeepAlive(false)
+                            .setIdleTimeout((int) (endpoint.getHttpClientOptions().getIdleTimeout() / 1000))
+                            .setConnectTimeout((int) endpoint.getHttpClientOptions().getConnectTimeout())
+                            .setTryUseCompression(endpoint.getHttpClientOptions().isUseCompression());
 
-                    Request request = requestBuilder.build();
-                    LOGGER.debug("Execute health-check request: {}", request);
+                    // Configure HTTP proxy
+                    HttpProxy proxy = endpoint.getHttpProxy();
+                    if (proxy != null && proxy.isEnabled()) {
+                        ProxyOptions proxyOptions = new ProxyOptions()
+                                .setHost(proxy.getHost())
+                                .setPort(proxy.getPort())
+                                .setUsername(proxy.getUsername())
+                                .setPassword(proxy.getPassword())
+                                .setType(ProxyType.valueOf(proxy.getType().name()));
+
+                        httpClientOptions.setProxyOptions(proxyOptions);
+                    }
+
+                    // Configure TLS if required
+                    HttpClientSslOptions sslOptions = endpoint.getHttpClientSslOptions();
+                    if (sslOptions != null && sslOptions.isEnabled()) {
+                        httpClientOptions
+                                .setSsl(sslOptions.isEnabled())
+                                .setVerifyHost(sslOptions.isHostnameVerifier())
+                                .setTrustAll(sslOptions.isTrustAll());
+
+                        if (sslOptions.getPem() != null && ! sslOptions.getPem().isEmpty()) {
+                            httpClientOptions.setPemTrustOptions(
+                                    new PemTrustOptions().addCertValue(
+                                            io.vertx.core.buffer.Buffer.buffer(sslOptions.getPem())));
+                        }
+                    }
+
+                    HttpClient httpClient = vertx.createHttpClient(httpClientOptions);
+
+                    // Run health-check
+                    URI requestUri = URI.create(endpoint.getTarget() + healthCheck.getRequest().getUri());
+
+                    final int port = requestUri.getPort() != -1 ? requestUri.getPort() :
+                            (HTTPS_SCHEME.equals(requestUri.getScheme()) ? 443 : 80);
+
+                    HttpClientRequest healthRequest = httpClient.request(
+                            HttpMethod.valueOf(healthCheck.getRequest().getMethod().name().toUpperCase()),
+                            port,
+                            requestUri.getHost(),
+                            requestUri.toString()
+                    );
+
+                    // Prepare request
+                    if (healthCheck.getRequest().getHeaders() != null) {
+                        healthCheck.getRequest().getHeaders().forEach(
+                                httpHeader -> healthRequest.headers().set(httpHeader.getName(), httpHeader.getValue()));
+                    }
 
                     final HealthStatus.Builder healthBuilder = HealthStatus
                             .forApi(api.getId())
                             .on(System.currentTimeMillis())
                             .method(healthCheck.getRequest().getMethod())
-                            .url(requestUri);
+                            .url(requestUri.toString());
 
-                    asyncHttpClient.prepareRequest(requestBuilder.build()).execute(new AsyncCompletionHandler<Void>() {
-                        @Override
-                        public Void onCompleted(Response response) throws Exception {
-                            validateAssertions(healthBuilder, new HealthCheckResponse(response));
-                            report(endpoint, healthBuilder);
-                            return null;
+                    healthRequest.handler(response -> response.bodyHandler(buffer -> {
+                        logger.debug("Health-check endpoint returns a response with a {} status code", response.statusCode());
+
+                        validateAssertions(healthBuilder, new HealthCheckResponse(response, buffer.toString()));
+                        report(endpoint, healthBuilder);
+
+                        // Close client
+                        httpClient.close();
+                    }));
+
+                    healthRequest.exceptionHandler(event -> {
+                        logger.info("An error occurs while running health-check request {}: {}", requestUri, event.getMessage());
+                        healthBuilder.fail().message(event.getMessage());
+
+                        if (event instanceof ConnectTimeoutException) {
+                            healthBuilder.message(event.getMessage());
+                            healthBuilder.status(HttpStatusCode.REQUEST_TIMEOUT_408);
+                        } else {
+                            healthBuilder.status(HttpStatusCode.SERVICE_UNAVAILABLE_503);
                         }
 
-                        @Override
-                        public void onThrowable(Throwable t) {
-                            LOGGER.debug("An error occurs while executing request {}: {}", request, t.getMessage());
-                            healthBuilder.fail().message(t.getMessage());
+                        report(endpoint, healthBuilder);
 
-                            if (t instanceof SpelEvaluationException) {
-                                healthBuilder.message(t.getMessage());
-                                healthBuilder.status(HttpStatusCode.INTERNAL_SERVER_ERROR_500);
-                            } else if (t instanceof SocketTimeoutException) {
-                                healthBuilder.message(t.getMessage());
-                                healthBuilder.status(HttpStatusCode.REQUEST_TIMEOUT_408);
-                            } else {
-                                healthBuilder.status(HttpStatusCode.SERVICE_UNAVAILABLE_503);
-                            }
-
-                            report(endpoint, healthBuilder);
-                        }
+                        // Close client
+                        httpClient.close();
                     });
+
+                    // Send request
+                    logger.debug("Execute health-check request: {}", healthRequest);
+                    if (healthCheck.getRequest().getBody() != null && !healthCheck.getRequest().getBody().isEmpty()) {
+                        healthRequest.end(healthCheck.getRequest().getBody());
+                    } else {
+                        healthRequest.end();
+                    }
                 }
             }
         }
     }
 
     private void report(final Endpoint endpoint, final HealthStatus.Builder healthBuilder) {
-        LOGGER.debug("Report health results for {}", api);
+        logger.debug("Report health results for {}", api);
         statusManager.update(endpoint, healthBuilder.isSuccess());
         reporterService.report(healthBuilder.build());
     }
@@ -139,39 +195,30 @@ class EndpointHealthCheck implements Runnable {
             Iterator<String> assertionIterator = healthCheck.getExpectation().getAssertions().iterator();
             boolean success = true;
             while (success && assertionIterator.hasNext()) {
-                String assertion = assertionIterator.next();
-                ExpressionParser parser = new SpelExpressionParser();
-                Expression expr = parser.parseExpression(assertion);
+                try {
+                    String assertion = assertionIterator.next();
+                    ExpressionParser parser = new SpelExpressionParser();
+                    Expression expr = parser.parseExpression(assertion);
 
-                StandardEvaluationContext context = new StandardEvaluationContext();
-                context.registerFunction("jsonPath",
-                        BeanUtils.resolveSignature("evaluate", JsonPathFunction.class));
+                    StandardEvaluationContext context = new StandardEvaluationContext();
+                    context.registerFunction("jsonPath",
+                            BeanUtils.resolveSignature("evaluate", JsonPathFunction.class));
 
-                context.setVariable("response", response);
+                    context.setVariable("response", response);
 
-                success = expr.getValue(context, boolean.class);
+                    success = expr.getValue(context, boolean.class);
 
-                if (!success) {
-                    healthBuilder.message("Assertion can not be verified : " + assertion);
+                    if (!success) {
+                        healthBuilder.message("Assertion can not be verified : " + assertion);
+                    }
+                } catch (SpelEvaluationException spelex) {
+                    success = false;
+                    healthBuilder.message("Assertion can not be verified : " + spelex.getMessage());
                 }
             }
 
             healthBuilder.success(success);
         }
-    }
-
-    private AsyncHttpClient createAsyncHttpClient() {
-        AsyncHttpClientConfig cf = new DefaultAsyncHttpClientConfig.Builder()
-                .setConnectTimeout(GLOBAL_TIMEOUT)
-                .setReadTimeout(GLOBAL_TIMEOUT)
-                .setRequestTimeout(GLOBAL_TIMEOUT)
-                .setMaxConnections(10)
-                .setMaxConnectionsPerHost(5)
-                .setAcceptAnyCertificate(true)
-                .setThreadPoolName("healthcheck-poller")
-                .build();
-
-        return new DefaultAsyncHttpClient(cf);
     }
 
     void setReporterService(ReporterService reporterService) {
