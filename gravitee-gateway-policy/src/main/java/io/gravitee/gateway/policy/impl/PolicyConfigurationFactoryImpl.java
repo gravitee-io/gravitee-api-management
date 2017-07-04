475c7289a90f6accecd463f3b0b647976e33d97b
brasseld <brasseld@gmail.com>
2017-07-04 16:18:24 +0200

    feat(gateway): Add support for HTTP/2
    Closes gravitee-io/issues#673

--- gravitee-gateway-policy/src/main/java/io/gravitee/gateway/policy/impl/PolicyConfigurationFactoryImpl.java
+++ gravitee-gateway-policy/src/main/java/io/gravitee/gateway/policy/impl/PolicyConfigurationFactoryImpl.java
@@ -15,7 +15,6 @@
  */
 package io.gravitee.gateway.policy.impl;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.gravitee.gateway.policy.PolicyConfigurationFactory;
 import io.gravitee.policy.api.PolicyConfiguration;
@@ -31,8 +30,7 @@ public class PolicyConfigurationFactoryImpl implements PolicyConfigurationFactor
 
     private final Logger LOGGER = LoggerFactory.getLogger(PolicyConfigurationFactoryImpl.class);
 
-    private final ObjectMapper mapper = new ObjectMapper()
-            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    private final ObjectMapper mapper = new ObjectMapper();
 
     @Override
     public <T extends PolicyConfiguration> T create(Class<T> policyConfigurationClass, String configuration) {
