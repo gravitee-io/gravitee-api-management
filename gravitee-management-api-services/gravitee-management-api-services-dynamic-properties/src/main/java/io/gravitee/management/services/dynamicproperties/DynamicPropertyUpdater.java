shoaibjdev/gravitee-management-rest-api

fix(dynamic properties): add log in case of duplicate keys
fix gravitee-io/issues#722

gravitee-management-api-services/gravitee-management-api-services-dynamic-properties/src/main/java/io/gravitee/management/services/dynamicproperties/DynamicPropertyUpdater.java
@@ -84,7 +84,11 @@ public class DynamicPropertyUpdater implements Runnable {
 
         // Create properties container
         Properties apiProperties = new Properties();
-        apiProperties.setProperties(updatedProperties);
+        try {
+            apiProperties.setProperties(updatedProperties);
+        } catch (RuntimeException e) {
+            logger.info(e.getMessage(), e);
+        }
         latestApi.setProperties(apiProperties);
 
         boolean isSync = apiService.isSynchronized(api.getId());

gravitee-io/gravitee-api-management

gravitee-management-api-services/gravitee-management-api-services-dynamic-properties/src/main/java/io/gravitee/management/services/dynamicproperties/DynamicPropertyUpdater.java
@@ -84,7 +84,11 @@ public class DynamicPropertyUpdater implements Runnable {
 
         // Create properties container
         Properties apiProperties = new Properties();
-        apiProperties.setProperties(updatedProperties);
+        try {
+            apiProperties.setProperties(updatedProperties);
+        } catch (RuntimeException e) {
+            logger.error(e.getMessage(), e);
+        }
         latestApi.setProperties(apiProperties);
 
         boolean isSync = apiService.isSynchronized(api.getId());