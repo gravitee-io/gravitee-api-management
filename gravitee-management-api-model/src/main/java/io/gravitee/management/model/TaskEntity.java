shengnian/gravitee-management-rest-api

feat(taks): add a task list for each user
fix gravitee-io/issues#971

gravitee-management-api-model/src/main/java/io/gravitee/management/model/TaskEntity.java
@@ -1,0 +1,59 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.management.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.gravitee.management.model.permissions.RoleScope;
+
+import java.util.Date;
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * @author Nicolas GERAUD (nicolas.geraud at graviteesource.com) 
+ * @author GraviteeSource Team
+ */
+public class TaskEntity {
+
+    private TaskType type;
+    private String data;
+    @JsonProperty(value = "created_at")
+    private Date createdAt;
+
+    public TaskType getType() {
+        return type;
+    }
+
+    public void setType(TaskType type) {
+        this.type = type;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+}

gravitee-io/gravitee-api-management

gravitee-management-api-model/src/main/java/io/gravitee/management/model/TaskEntity.java
@@ -1,0 +1,59 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.management.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.gravitee.management.model.permissions.RoleScope;
+
+import java.util.Date;
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * @author Nicolas GERAUD (nicolas.geraud at graviteesource.com) 
+ * @author GraviteeSource Team
+ */
+public class TaskEntity {
+
+    private TaskType type;
+    private Object data;
+    @JsonProperty(value = "created_at")
+    private Date createdAt;
+
+    public TaskType getType() {
+        return type;
+    }
+
+    public void setType(TaskType type) {
+        this.type = type;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+}