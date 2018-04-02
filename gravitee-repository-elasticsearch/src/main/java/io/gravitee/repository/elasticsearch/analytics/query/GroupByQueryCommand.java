gravitee-io/gravitee-common-elasticsearch

feat(elasticsearch): Add ES6 support
Closes gravitee-io/issues#1170

gravitee-repository-elasticsearch/src/main/java/io/gravitee/repository/elasticsearch/analytics/query/GroupByQueryCommand.java
@@ -1,0 +1,128 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.repository.elasticsearch.analytics.query;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import io.gravitee.elasticsearch.exception.ElasticsearchException;
+import io.gravitee.elasticsearch.model.Aggregation;
+import io.gravitee.elasticsearch.model.SearchResponse;
+import io.gravitee.elasticsearch.utils.Type;
+import io.gravitee.repository.analytics.AnalyticsException;
+import io.gravitee.repository.analytics.query.Query;
+import io.gravitee.repository.analytics.query.groupby.GroupByQuery;
+import io.gravitee.repository.analytics.query.groupby.GroupByResponse;
+import io.reactivex.Single;
+
+import java.util.Iterator;
+
+/**
+ * Command used to handle GroupByQuery.
+ * 
+ * @author Guillaume Waignier
+ * @author Sebastien Devaux
+ * @author Guillaume Gillon
+ *
+ */
+public class GroupByQueryCommand extends AstractElasticsearchQueryCommand<GroupByResponse> {
+
+	private final static String TEMPLATE = "groupBy.ftl";
+
+	@Override
+	public Class<? extends Query<GroupByResponse>> getSupportedQuery() {
+		return GroupByQuery.class;
+	}
+
+	@Override
+	public GroupByResponse executeQuery(Query<GroupByResponse> query) throws AnalyticsException {
+		final GroupByQuery groupByQuery = (GroupByQuery) query;
+
+		final String sQuery = this.createQuery(TEMPLATE, query);
+
+		try {
+			final Long from = groupByQuery.timeRange().range().from();
+			final Long to = groupByQuery.timeRange().range().to();
+
+			final Single<SearchResponse> result = this.client.search(
+					this.indexNameGenerator.getIndexName(Type.REQUEST, from, to),
+					Type.REQUEST.getType(),
+					sQuery);
+			return this.toGroupByResponse(result.blockingGet());
+		} catch (ElasticsearchException eex) {
+			logger.error("Impossible to perform GroupByQuery", eex);
+			throw new AnalyticsException("Impossible to perform GroupByQuery", eex);
+		}
+	}
+
+	private GroupByResponse toGroupByResponse(final SearchResponse response) {
+		final GroupByResponse groupByresponse = new GroupByResponse();
+
+		if (response.getAggregations() == null) {
+			return groupByresponse;
+		}
+
+		final String aggregationName = response.getAggregations().keySet().iterator().next();
+		final Aggregation aggregation = response.getAggregations().get(aggregationName);
+
+		final String fieldName = aggregationName.substring(aggregationName.indexOf('_') + 1);
+
+		groupByresponse.setField(fieldName);
+
+		if (aggregationName.endsWith("_range")) {
+
+			for (final JsonNode bucket : aggregation.getBuckets()) {
+
+				final String keyAsString = bucket.get("key").asText();
+				final long docCount = bucket.get("doc_count").asLong();
+				GroupByResponse.Bucket value = new GroupByResponse.Bucket(keyAsString, docCount);
+				groupByresponse.values().add(value);
+			}
+
+		} else if (aggregationName.startsWith("by_")) {
+
+			for (final JsonNode bucket : aggregation.getBuckets()) {
+				final JsonNode subAggragation = this.getFirstSubAggregation(bucket);
+
+				if (subAggragation != null) {
+					final JsonNode aggValue = subAggragation.get("value");
+					if (aggValue.isNumber()) {
+						final String keyAsString = bucket.get("key").asText();
+						GroupByResponse.Bucket value = new GroupByResponse.Bucket(keyAsString, aggValue.asLong());
+						groupByresponse.values().add(value);
+					}
+
+				} else {
+					final String keyAsString = bucket.get("key").asText();
+					final long docCount = bucket.get("doc_count").asLong();
+					final GroupByResponse.Bucket value = new GroupByResponse.Bucket(keyAsString, docCount);
+					groupByresponse.values().add(value);
+				}
+			}
+		}
+		return groupByresponse;
+
+	}
+
+	private JsonNode getFirstSubAggregation(JsonNode bucket) {
+
+		for (final Iterator<String> it = bucket.fieldNames(); it.hasNext();) {
+			final String fieldName = it.next();
+			if (fieldName.startsWith("by_") || fieldName.startsWith("avg_") || fieldName.startsWith("min_")
+					|| fieldName.startsWith("max_"))
+				return bucket.get(fieldName);
+		}
+		return null;
+	}
+}

gravitee-repository-elasticsearch/src/main/java/io/gravitee/repository/elasticsearch/log/ElasticLogRepository.java
@@ -1,0 +1,147 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.repository.elasticsearch.log;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import io.gravitee.elasticsearch.exception.ElasticsearchException;
+import io.gravitee.elasticsearch.model.SearchHit;
+import io.gravitee.elasticsearch.model.SearchHits;
+import io.gravitee.elasticsearch.model.SearchResponse;
+import io.gravitee.elasticsearch.utils.Type;
+import io.gravitee.repository.analytics.AnalyticsException;
+import io.gravitee.repository.analytics.query.tabular.TabularQuery;
+import io.gravitee.repository.analytics.query.tabular.TabularResponse;
+import io.gravitee.repository.elasticsearch.AbstractElasticsearchRepository;
+import io.gravitee.repository.exceptions.TechnicalException;
+import io.gravitee.repository.log.api.LogRepository;
+import io.gravitee.repository.log.model.ExtendedLog;
+import io.gravitee.repository.log.model.Log;
+import io.reactivex.Single;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author David BRASSELY (david.brassely at graviteesource.com)
+ * @author GraviteeSource Team
+ * @author Sebastien Devaux (Zenika)
+ * @author Guillaume Waignier (Zenika)
+ */
+public class ElasticLogRepository extends AbstractElasticsearchRepository implements LogRepository {
+
+	/**
+	 * Logger.
+	 */
+	private final Logger logger = LoggerFactory.getLogger(ElasticLogRepository.class);
+
+	/**
+	 * Freemarker template name.
+	 */
+	private static final String LOG_TEMPLATE = "log/log.ftl";
+
+	/**
+	 * Freemarker template name for finding log by id.
+	 */
+	private static final String LOG_BY_ID_TEMPLATE = "log/logById.ftl";
+
+	@Override
+	public TabularResponse query(final TabularQuery query) throws AnalyticsException {
+		final String sQuery = this.createElasticsearchJsonQuery(query);
+		
+		final Long from = query.timeRange().range().from();
+		final Long to = query.timeRange().range().to();
+
+		try {
+			final Single<SearchResponse> result = this.client.search(
+					this.indexNameGenerator.getIndexName(Type.REQUEST, from, to),
+					Type.REQUEST.getType(),
+					sQuery);
+
+			return this.toTabularResponse(result.blockingGet());
+		} catch (final ElasticsearchException eex) {
+			logger.error("Impossible to perform log request", eex);
+			throw new AnalyticsException("Impossible to perform log request", eex);
+		}
+	}
+
+	/**
+	 * Create JSON Elasticsearch query for the log
+	 * @param query user query
+	 * @return JSON Elasticsearch query
+	 */
+	private String createElasticsearchJsonQuery(final TabularQuery query) {
+		final Map<String, Object> data = new HashMap<>();
+		data.put("query", query);
+
+		return this.freeMarkerComponent.generateFromTemplate(LOG_TEMPLATE, data);
+	}
+
+	@Override
+	public ExtendedLog findById(final String requestId) throws AnalyticsException {
+		final Map<String, Object> data = new HashMap<>();
+		data.put("requestId", requestId);
+
+		String sQuery = this.freeMarkerComponent.generateFromTemplate(LOG_BY_ID_TEMPLATE, data);
+
+		try {
+			Single<SearchResponse> result = this.client.search(
+					this.indexNameGenerator.getWildcardIndexName(Type.REQUEST),
+					Type.REQUEST.getType(),
+					sQuery);
+
+			SearchResponse searchResponse = result.blockingGet();
+			if (searchResponse.getSearchHits().getTotal() == 0) {
+				throw new AnalyticsException("Request [" + requestId + "] does not exist");
+			}
+
+			SearchHit searchHit = searchResponse.getSearchHits().getHits().get(0);
+
+			String searchHitIndex = searchHit.getIndex();
+
+			sQuery = this.freeMarkerComponent.generateFromTemplate(LOG_BY_ID_TEMPLATE, data);
+
+			result = this.client.search(searchHitIndex, Type.LOG.getType(), sQuery);
+			searchResponse = result.blockingGet();
+
+			JsonNode log = null;
+			if (searchResponse.getSearchHits().getTotal() != 0) {
+				log = searchResponse.getSearchHits().getHits().get(0).getSource();
+			}
+
+			return LogBuilder.createExtendedLog(searchHit, log);
+		} catch (TechnicalException e) {
+			logger.error("Request [{}] does not exist", requestId, e);
+			throw new AnalyticsException("Request [" + requestId + "] does not exist");
+		}
+	}
+
+	private TabularResponse toTabularResponse(final SearchResponse response) {
+		final SearchHits hits = response.getSearchHits();
+		final TabularResponse tabularResponse = new TabularResponse(hits.getTotal());
+		final List<Log> logs = new ArrayList<>(hits.getHits().size());
+		for (int i = 0; i < hits.getHits().size(); i++) {
+			logs.add(LogBuilder.createLog(hits.getHits().get(i)));
+		}
+		tabularResponse.setLogs(logs);
+
+		return tabularResponse;
+
+	}
+}

gravitee-io/gravitee-api-management

gravitee-repository-elasticsearch/src/main/java/io/gravitee/repository/elasticsearch/analytics/query/GroupByQueryCommand.java
@@ -1,0 +1,127 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.repository.elasticsearch.analytics.query;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import io.gravitee.elasticsearch.model.Aggregation;
+import io.gravitee.elasticsearch.model.SearchResponse;
+import io.gravitee.elasticsearch.utils.Type;
+import io.gravitee.repository.analytics.AnalyticsException;
+import io.gravitee.repository.analytics.query.Query;
+import io.gravitee.repository.analytics.query.groupby.GroupByQuery;
+import io.gravitee.repository.analytics.query.groupby.GroupByResponse;
+import io.reactivex.Single;
+
+import java.util.Iterator;
+
+/**
+ * Command used to handle GroupByQuery.
+ * 
+ * @author Guillaume Waignier
+ * @author Sebastien Devaux
+ * @author Guillaume Gillon
+ *
+ */
+public class GroupByQueryCommand extends AstractElasticsearchQueryCommand<GroupByResponse> {
+
+	private final static String TEMPLATE = "groupBy.ftl";
+
+	@Override
+	public Class<? extends Query<GroupByResponse>> getSupportedQuery() {
+		return GroupByQuery.class;
+	}
+
+	@Override
+	public GroupByResponse executeQuery(Query<GroupByResponse> query) throws AnalyticsException {
+		final GroupByQuery groupByQuery = (GroupByQuery) query;
+
+		final String sQuery = this.createQuery(TEMPLATE, query);
+
+		try {
+			final Long from = groupByQuery.timeRange().range().from();
+			final Long to = groupByQuery.timeRange().range().to();
+
+			final Single<SearchResponse> result = this.client.search(
+					this.indexNameGenerator.getIndexName(Type.REQUEST, from, to),
+					Type.REQUEST.getType(),
+					sQuery);
+			return this.toGroupByResponse(result.blockingGet());
+		} catch (Exception eex) {
+			logger.error("Impossible to perform GroupByQuery", eex);
+			throw new AnalyticsException("Impossible to perform GroupByQuery", eex);
+		}
+	}
+
+	private GroupByResponse toGroupByResponse(final SearchResponse response) {
+		final GroupByResponse groupByresponse = new GroupByResponse();
+
+		if (response.getAggregations() == null) {
+			return groupByresponse;
+		}
+
+		final String aggregationName = response.getAggregations().keySet().iterator().next();
+		final Aggregation aggregation = response.getAggregations().get(aggregationName);
+
+		final String fieldName = aggregationName.substring(aggregationName.indexOf('_') + 1);
+
+		groupByresponse.setField(fieldName);
+
+		if (aggregationName.endsWith("_range")) {
+
+			for (final JsonNode bucket : aggregation.getBuckets()) {
+
+				final String keyAsString = bucket.get("key").asText();
+				final long docCount = bucket.get("doc_count").asLong();
+				GroupByResponse.Bucket value = new GroupByResponse.Bucket(keyAsString, docCount);
+				groupByresponse.values().add(value);
+			}
+
+		} else if (aggregationName.startsWith("by_")) {
+
+			for (final JsonNode bucket : aggregation.getBuckets()) {
+				final JsonNode subAggragation = this.getFirstSubAggregation(bucket);
+
+				if (subAggragation != null) {
+					final JsonNode aggValue = subAggragation.get("value");
+					if (aggValue.isNumber()) {
+						final String keyAsString = bucket.get("key").asText();
+						GroupByResponse.Bucket value = new GroupByResponse.Bucket(keyAsString, aggValue.asLong());
+						groupByresponse.values().add(value);
+					}
+
+				} else {
+					final String keyAsString = bucket.get("key").asText();
+					final long docCount = bucket.get("doc_count").asLong();
+					final GroupByResponse.Bucket value = new GroupByResponse.Bucket(keyAsString, docCount);
+					groupByresponse.values().add(value);
+				}
+			}
+		}
+		return groupByresponse;
+
+	}
+
+	private JsonNode getFirstSubAggregation(JsonNode bucket) {
+
+		for (final Iterator<String> it = bucket.fieldNames(); it.hasNext();) {
+			final String fieldName = it.next();
+			if (fieldName.startsWith("by_") || fieldName.startsWith("avg_") || fieldName.startsWith("min_")
+					|| fieldName.startsWith("max_"))
+				return bucket.get(fieldName);
+		}
+		return null;
+	}
+}

gravitee-repository-elasticsearch/src/main/java/io/gravitee/repository/elasticsearch/log/ElasticLogRepository.java
@@ -1,0 +1,147 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.repository.elasticsearch.log;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import io.gravitee.elasticsearch.model.SearchHit;
+import io.gravitee.elasticsearch.model.SearchHits;
+import io.gravitee.elasticsearch.model.SearchResponse;
+import io.gravitee.elasticsearch.utils.Type;
+import io.gravitee.repository.analytics.AnalyticsException;
+import io.gravitee.repository.analytics.query.tabular.TabularQuery;
+import io.gravitee.repository.analytics.query.tabular.TabularResponse;
+import io.gravitee.repository.elasticsearch.AbstractElasticsearchRepository;
+import io.gravitee.repository.log.api.LogRepository;
+import io.gravitee.repository.log.model.ExtendedLog;
+import io.gravitee.repository.log.model.Log;
+import io.reactivex.Single;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author David BRASSELY (david.brassely at graviteesource.com)
+ * @author GraviteeSource Team
+ * @author Sebastien Devaux (Zenika)
+ * @author Guillaume Waignier (Zenika)
+ */
+public class ElasticLogRepository extends AbstractElasticsearchRepository implements LogRepository {
+
+	/**
+	 * Logger.
+	 */
+	private final Logger logger = LoggerFactory.getLogger(ElasticLogRepository.class);
+
+	/**
+	 * Freemarker template name.
+	 */
+	private static final String LOG_TEMPLATE = "log/log.ftl";
+
+	/**
+	 * Freemarker template name for finding log by id.
+	 */
+	private static final String LOG_BY_ID_TEMPLATE = "log/logById.ftl";
+
+	@Override
+	public TabularResponse query(final TabularQuery query) throws AnalyticsException {
+		final String sQuery = this.createElasticsearchJsonQuery(query);
+		
+		final Long from = query.timeRange().range().from();
+		final Long to = query.timeRange().range().to();
+
+		try {
+			final Single<SearchResponse> result = this.client.search(
+					this.indexNameGenerator.getIndexName(Type.REQUEST, from, to),
+					Type.REQUEST.getType(),
+					sQuery);
+
+			return this.toTabularResponse(result.blockingGet());
+		} catch (final Exception eex) {
+			logger.error("Impossible to perform log request", eex);
+			throw new AnalyticsException("Impossible to perform log request", eex);
+		}
+	}
+
+	/**
+	 * Create JSON Elasticsearch query for the log
+	 * @param query user query
+	 * @return JSON Elasticsearch query
+	 */
+	private String createElasticsearchJsonQuery(final TabularQuery query) {
+		final Map<String, Object> data = new HashMap<>();
+		data.put("query", query);
+
+		return this.freeMarkerComponent.generateFromTemplate(LOG_TEMPLATE, data);
+	}
+
+	@Override
+	public ExtendedLog findById(final String requestId) throws AnalyticsException {
+		final Map<String, Object> data = new HashMap<>();
+		data.put("requestId", requestId);
+
+		String sQuery = this.freeMarkerComponent.generateFromTemplate(LOG_BY_ID_TEMPLATE, data);
+
+		try {
+			Single<SearchResponse> result = this.client.search(
+					this.indexNameGenerator.getWildcardIndexName(Type.REQUEST),
+					Type.REQUEST.getType(),
+					sQuery);
+
+			SearchResponse searchResponse = result.blockingGet();
+			if (searchResponse.getSearchHits().getTotal() == 0) {
+				throw new AnalyticsException("Request [" + requestId + "] does not exist");
+			}
+
+			SearchHit searchHit = searchResponse.getSearchHits().getHits().get(0);
+
+			String searchHitIndex = searchHit.getIndex();
+			sQuery = this.freeMarkerComponent.generateFromTemplate(LOG_BY_ID_TEMPLATE, data);
+
+			// Search index must be updated in case of per-type index
+			searchHitIndex = searchHitIndex.replaceAll(Type.REQUEST.getType(), Type.LOG.getType());
+
+			result = this.client.search(searchHitIndex, Type.LOG.getType(), sQuery);
+			searchResponse = result.blockingGet();
+
+			JsonNode log = null;
+			if (searchResponse.getSearchHits().getTotal() != 0) {
+				log = searchResponse.getSearchHits().getHits().get(0).getSource();
+			}
+
+			return LogBuilder.createExtendedLog(searchHit, log);
+		} catch (Exception e) {
+			logger.error("Request [{}] does not exist", requestId, e);
+			throw new AnalyticsException("Request [" + requestId + "] does not exist");
+		}
+	}
+
+	private TabularResponse toTabularResponse(final SearchResponse response) {
+		final SearchHits hits = response.getSearchHits();
+		final TabularResponse tabularResponse = new TabularResponse(hits.getTotal());
+		final List<Log> logs = new ArrayList<>(hits.getHits().size());
+		for (int i = 0; i < hits.getHits().size(); i++) {
+			logs.add(LogBuilder.createLog(hits.getHits().get(i)));
+		}
+		tabularResponse.setLogs(logs);
+
+		return tabularResponse;
+
+	}
+}