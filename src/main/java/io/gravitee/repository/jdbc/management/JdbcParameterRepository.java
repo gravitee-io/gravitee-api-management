fed91aff7e2109a43869be37e5f05ff3e8d79161
Nicolas Géraud <nicolas.geraud@gmail.com>
2018-05-07 23:13:14 +0200

    feat(settings): move constants.json config to database
    fix gravitee-io/issues#1197

--- src/main/java/io/gravitee/repository/jdbc/management/JdbcParameterRepository.java
+++ src/main/java/io/gravitee/repository/jdbc/management/JdbcParameterRepository.java
@@ -15,19 +15,33 @@
  */
 package io.gravitee.repository.jdbc.management;
 
+import io.gravitee.repository.exceptions.TechnicalException;
 import io.gravitee.repository.jdbc.orm.JdbcObjectMapper;
 import io.gravitee.repository.management.api.ParameterRepository;
 import io.gravitee.repository.management.model.Parameter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 
+import java.sql.PreparedStatement;
 import java.sql.Types;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static io.gravitee.repository.jdbc.common.AbstractJdbcRepositoryConfiguration.escapeReservedWord;
+import static org.springframework.util.StringUtils.isEmpty;
 
 /**
- *
  * @author njt
+ * @author Nicolas GERAUD (nicolas.geraud at graviteesource.com)
+ * @author GraviteeSource Team
+ *
  */
 @Repository
 public class JdbcParameterRepository extends JdbcAbstractCrudRepository<Parameter, String> implements ParameterRepository {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(JdbcParameterRepository.class);
 
     private static final JdbcObjectMapper ORM = JdbcObjectMapper.builder(Parameter.class, "parameters", "key")
             .addColumn("key", Types.NVARCHAR, String.class)
@@ -42,5 +56,24 @@ public class JdbcParameterRepository extends JdbcAbstractCrudRepository<Paramete
     @Override
     protected String getId(Parameter item) {
         return item.getKey();
+    }
+
+    @Override
+    public List<Parameter> findAll(List<String> keys) throws TechnicalException {
+        LOGGER.debug("JdbcParameterRepository.findAll({})", keys);
+        try {
+            if (isEmpty(keys)) {
+                return Collections.emptyList();
+            }
+            List<Parameter> parameters = jdbcTemplate.query("select * from parameters where " + escapeReservedWord("key") + " in ( "
+                            + ORM.buildInClause(keys) + " )"
+                    , (PreparedStatement ps) -> ORM.setArguments(ps, keys, 1)
+                    , ORM.getRowMapper()
+            );
+            return new ArrayList<>(parameters);
+        } catch (final Exception ex) {
+            LOGGER.error("Failed to find parameters by keys:", ex);
+            throw new TechnicalException("Failed to find parameters by keys", ex);
+        }
     }
 }
