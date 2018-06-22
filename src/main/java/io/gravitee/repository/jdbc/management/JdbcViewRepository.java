3904261c72715843d6f1cc9234ea000fa9c28928
Titouan COMPIEGNE <titouan.compiegne@gmail.com>
2018-06-22 10:55:46 +0200

    feat(view): add highlight API
    Closes gravitee-io/issues#1011

--- src/main/java/io/gravitee/repository/jdbc/management/JdbcViewRepository.java
+++ src/main/java/io/gravitee/repository/jdbc/management/JdbcViewRepository.java
@@ -37,6 +37,7 @@ public class JdbcViewRepository extends JdbcAbstractCrudRepository<View, String>
             .addColumn("default_view", Types.BIT, boolean.class)
             .addColumn("hidden", Types.BIT, boolean.class)
             .addColumn("order", Types.INTEGER, int.class)
+            .addColumn("highlight_api", Types.NVARCHAR, String.class)
             .addColumn("created_at", Types.TIMESTAMP, Date.class)
             .addColumn("updated_at", Types.TIMESTAMP, Date.class)
             .build();
