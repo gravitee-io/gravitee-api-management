b29424f3975f64d900a2e05835bc30fdcc7a2481
brasseld <brasseld@gmail.com>
2018-11-14 09:43:40 +0100

    feat(plan): API Publisher can ask for a mandatory comment while consumer is subscribing to the plan
    Closes gravitee-io/issues#1660

--- src/main/java/io/gravitee/repository/jdbc/management/JdbcPlanRepository.java
+++ src/main/java/io/gravitee/repository/jdbc/management/JdbcPlanRepository.java
@@ -61,6 +61,7 @@ public class JdbcPlanRepository implements PlanRepository {
             .addColumn("updated_at", Types.TIMESTAMP, Date.class)
             .addColumn("published_at", Types.TIMESTAMP, Date.class)
             .addColumn("closed_at", Types.TIMESTAMP, Date.class)
+            .addColumn("comment_required", Types.BOOLEAN, Boolean.class)
             .build(); 
     
     private static final JdbcHelper.ChildAdder<Plan> CHILD_ADDER = (Plan parent, ResultSet rs) -> {
