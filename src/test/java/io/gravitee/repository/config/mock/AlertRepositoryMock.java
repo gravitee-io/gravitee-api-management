b5cbd6b278d57a50557852a494246848c6940d44
aelamrani <azize.elamrani@gmail.com>
2019-01-09 12:19:20 +0100

    fix: Alert is not correctly mapped on some cases
    This closes gravitee-io/issues#1801

--- src/test/java/io/gravitee/repository/config/mock/AlertRepositoryMock.java
+++ src/test/java/io/gravitee/repository/config/mock/AlertRepositoryMock.java
@@ -16,27 +16,13 @@
 package io.gravitee.repository.config.mock;
 
 import io.gravitee.repository.management.api.AlertRepository;
-import io.gravitee.repository.management.api.ApiRepository;
-import io.gravitee.repository.management.api.search.ApiCriteria;
-import io.gravitee.repository.management.api.search.ApiFieldExclusionFilter;
-import io.gravitee.repository.management.api.search.builder.PageableBuilder;
 import io.gravitee.repository.management.model.Alert;
-import io.gravitee.repository.management.model.Api;
-import io.gravitee.repository.management.model.LifecycleState;
-import io.gravitee.repository.management.model.Visibility;
 import org.mockito.ArgumentMatcher;
-import org.mockito.internal.util.collections.Sets;
 
+import java.util.Date;
 import java.util.Set;
 
-import static io.gravitee.repository.management.model.LifecycleState.STARTED;
-import static io.gravitee.repository.management.model.LifecycleState.STOPPED;
-import static io.gravitee.repository.management.model.Visibility.PUBLIC;
-import static io.gravitee.repository.utils.DateUtils.parse;
-import static java.util.Arrays.asList;
-import static java.util.Collections.singleton;
 import static java.util.Collections.singletonList;
-import static java.util.Optional.empty;
 import static java.util.Optional.of;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.argThat;
@@ -56,30 +42,67 @@ public class AlertRepositoryMock extends AbstractRepositoryMock<AlertRepository>
 
     @Override
     void prepare(AlertRepository alertRepository) throws Exception {
+        final Date date = new Date(1439022010883L);
+
         final Alert alert = new Alert();
         alert.setId("new-alert");
         alert.setName("Alert name");
         alert.setDescription("Description for the new alert");
+        alert.setReferenceType("API");
+        alert.setReferenceId("api-id");
+        alert.setType("HEALTH_CHECK");
+        alert.setCreatedAt(date);
+        alert.setUpdatedAt(date);
 
         final Alert alert2 = new Alert();
         alert2.setId("alert");
         alert2.setName("Health-check");
 
+        final Alert alertBeforUpdate = new Alert();
+        alertBeforUpdate.setId("quota80");
+        alertBeforUpdate.setName("Quota80");
+
         final Alert alert2Updated = new Alert();
-        alert2Updated.setId("alert");
+        alert2Updated.setId("quota80");
         alert2Updated.setName("New name");
         alert2Updated.setDescription("New description");
+        alert2Updated.setReferenceType("New reference type");
+        alert2Updated.setReferenceId("New reference id");
+        alert2Updated.setType("New type");
+        alert2Updated.setMetricType("New metric type");
+        alert2Updated.setMetric("New metric");
+        alert2Updated.setThreshold(99D);
+        alert2Updated.setPlan("New plan");
+        alert2Updated.setEnabled(true);
+        alert2Updated.setCreatedAt(date);
+        alert2Updated.setUpdatedAt(date);
 
-        final Set<Alert> alerts = newSet(alert, alert2);
-        final Set<Alert> alertsAfterDelete = newSet(alert);
-        final Set<Alert> alertsAfterAdd = newSet(alert, alert2, mock(Alert.class));
+        final Alert alertQuota = new Alert();
+        alertQuota.setId("quota90");
+        alertQuota.setName("Quota90");
+        alertQuota.setDescription("Description for alert API quota 90%");
+        alertQuota.setReferenceType("APPLICATION");
+        alertQuota.setReferenceId("application-id");
+        alertQuota.setType("QUOTA");
+        alertQuota.setMetricType("QUOTA");
+        alertQuota.setThresholdType("PERCENT_RATE");
+        alertQuota.setThreshold(90D);
+        alertQuota.setPlan("gold");
+        alertQuota.setEnabled(true);
+        alertQuota.setCreatedAt(date);
+        alertQuota.setUpdatedAt(new Date(1439022010883L));
+
+        final Set<Alert> alerts = newSet(alert, alertQuota, alert2Updated);
+        final Set<Alert> alertsAfterDelete = newSet(alert, alertQuota);
+        final Set<Alert> alertsAfterAdd = newSet(alert, alert2, mock(Alert.class), mock(Alert.class));
 
         when(alertRepository.findAll()).thenReturn(alerts, alertsAfterAdd, alerts, alertsAfterDelete, alerts);
 
         when(alertRepository.create(any(Alert.class))).thenReturn(alert);
 
         when(alertRepository.findById("new-alert")).thenReturn(of(alert));
-        when(alertRepository.findById("health-check")).thenReturn(of(alert2), of(alert2Updated));
+//        when(alertRepository.findById("health-check")).thenReturn(of(alert2));
+        when(alertRepository.findById("quota80")).thenReturn(of(alertBeforUpdate), of(alert2Updated));
 
         when(alertRepository.update(argThat(new ArgumentMatcher<Alert>() {
             @Override
