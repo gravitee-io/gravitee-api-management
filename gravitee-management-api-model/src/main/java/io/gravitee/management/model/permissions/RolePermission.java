ivan801/gravitee-management-rest-api

feat: display entrypoints by API sharding tags
This closes gravitee-io/issues#1706

gravitee-management-api-model/src/main/java/io/gravitee/management/model/permissions/RolePermission.java
@@ -34,6 +34,7 @@ public enum RolePermission {
     MANAGEMENT_MESSAGE      (RoleScope.MANAGEMENT, ManagementPermission.MESSAGE),
     MANAGEMENT_DICTIONARY   (RoleScope.MANAGEMENT, ManagementPermission.DICTIONARY),
     MANAGEMENT_ALERT        (RoleScope.MANAGEMENT, ManagementPermission.ALERT),
+    MANAGEMENT_ENTRY_POINT  (RoleScope.MANAGEMENT, ManagementPermission.ENTRY_POINT),
 
     PORTAL_METADATA         (RoleScope.PORTAL, PortalPermission.METADATA),
     PORTAL_DOCUMENTATION    (RoleScope.PORTAL, PortalPermission.DOCUMENTATION),

gravitee-io/gravitee-api-management

gravitee-management-api-model/src/main/java/io/gravitee/management/model/permissions/RolePermission.java
@@ -34,6 +34,7 @@ public enum RolePermission {
     MANAGEMENT_MESSAGE      (RoleScope.MANAGEMENT, ManagementPermission.MESSAGE),
     MANAGEMENT_DICTIONARY   (RoleScope.MANAGEMENT, ManagementPermission.DICTIONARY),
     MANAGEMENT_ALERT        (RoleScope.MANAGEMENT, ManagementPermission.ALERT),
+    MANAGEMENT_ENTRYPOINT  (RoleScope.MANAGEMENT, ManagementPermission.ENTRYPOINT),
 
     PORTAL_METADATA         (RoleScope.PORTAL, PortalPermission.METADATA),
     PORTAL_DOCUMENTATION    (RoleScope.PORTAL, PortalPermission.DOCUMENTATION),