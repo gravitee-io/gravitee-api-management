213268350576e0188e1dea1a2db7ee4ea965f313
Nicolas Géraud <nicolas.geraud@gmail.com>
2019-01-16 18:54:08 +0100

    fix(SSL): update UT due to #1811

--- gravitee-gateway-standalone/gravitee-gateway-standalone-container/src/test/java/io/gravitee/gateway/standalone/ClientAuthenticationPEMInlineTest.java
+++ gravitee-gateway-standalone/gravitee-gateway-standalone-container/src/test/java/io/gravitee/gateway/standalone/ClientAuthenticationPEMInlineTest.java
@@ -84,28 +84,32 @@ public class ClientAuthenticationPEMInlineTest extends AbstractGatewayTest {
         Request request = Request.Get("http://localhost:8082/test/my_team");
         Response response = request.execute();
         HttpResponse returnResponse = response.returnResponse();
-        assertEquals(HttpStatus.SC_BAD_GATEWAY, returnResponse.getStatusLine().getStatusCode());
+        assertEquals("without ssl configuration => 502",
+                HttpStatus.SC_BAD_GATEWAY, returnResponse.getStatusLine().getStatusCode());
 
-        // Second call is calling an endpoint where trustAll = false, without keystore => 200
+        // Second call is calling an endpoint where trustAll = false, without keystore => 502
         request = Request.Get("http://localhost:8082/test/my_team");
         response = request.execute();
         returnResponse = response.returnResponse();
-        assertEquals(HttpStatus.SC_OK, returnResponse.getStatusLine().getStatusCode());
+        assertEquals("trustAll = false, without keystore => 200",
+                HttpStatus.SC_BAD_GATEWAY, returnResponse.getStatusLine().getStatusCode());
 
         // Third call is calling an endpoint where trustAll = true, with keystore => 200
         request = Request.Get("http://localhost:8082/test/my_team");
         response = request.execute();
         returnResponse = response.returnResponse();
-        assertEquals(HttpStatus.SC_OK, returnResponse.getStatusLine().getStatusCode());
+        assertEquals("trustAll = true, with keystore => 200",
+                HttpStatus.SC_OK, returnResponse.getStatusLine().getStatusCode());
 
-        // Third call is calling an endpoint where trustAll = false, with truststore and keystore => 200
+        // Fourth call is calling an endpoint where trustAll = false, with truststore and keystore => 200
         request = Request.Get("http://localhost:8082/test/my_team");
         response = request.execute();
         returnResponse = response.returnResponse();
-        assertEquals(HttpStatus.SC_OK, returnResponse.getStatusLine().getStatusCode());
+        assertEquals("trustAll = false, with truststore and keystore => 200",
+                HttpStatus.SC_OK, returnResponse.getStatusLine().getStatusCode());
 
         // Check that the stub has been successfully invoked by the gateway
-        verify(3, getRequestedFor(urlEqualTo("/team/my_team")));
+        verify(2, getRequestedFor(urlEqualTo("/team/my_team")));
     }
 
     @Override
