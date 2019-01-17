bbf2b3814fa6768ca18fc9764f76a74efd7ec367
Nicolas Géraud <nicolas.geraud@gmail.com>
2019-01-17 21:14:56 +0100

    fix(plan) do not redeploy api on every plan modification
    fix gravitee-io/issues#1827

--- src/main/java/io/gravitee/repository/management/model/Plan.java
+++ src/main/java/io/gravitee/repository/management/model/Plan.java
@@ -89,6 +89,11 @@ public class Plan {
      */
     private List<String> excludedGroups;
 
+    /**
+     * last time modification introduced an api redeployment
+     */
+    private Date needRedeployAt;
+
     public Plan() {
     }
 
@@ -109,6 +114,7 @@ public class Plan {
         this.definition = cloned.getDefinition();
         this.characteristics = cloned.getCharacteristics();
         this.excludedGroups = cloned.getExcludedGroups();
+        this.needRedeployAt = cloned.getNeedRedeployAt();
     }
 
 
@@ -246,6 +252,14 @@ public class Plan {
 
     public void setExcludedGroups(List<String> excludedGroups) {
         this.excludedGroups = excludedGroups;
+    }
+
+    public Date getNeedRedeployAt() {
+        return needRedeployAt;
+    }
+
+    public void setNeedRedeployAt(Date needRedeployAt) {
+        this.needRedeployAt = needRedeployAt;
     }
 
     @Override
