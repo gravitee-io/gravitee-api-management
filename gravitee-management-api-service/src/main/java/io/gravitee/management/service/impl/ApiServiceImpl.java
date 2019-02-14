netfreeperson/gravitee-management-rest-api

fix(plan) do not redeploy api on every plan modification
fix gravitee-io/issues#1827

gravitee-management-api-service/src/main/java/io/gravitee/management/service/impl/ApiServiceImpl.java
@@ -603,7 +603,7 @@ public class ApiServiceImpl extends TransactionalService implements ApiService {
                     Set<PlanEntity> plans = planService.findByApi(api.getId());
                     sync = plans.stream()
                             .filter(plan -> plan.getStatus() != PlanStatus.STAGING)
-                            .filter(plan -> plan.getUpdatedAt().after(api.getDeployedAt())).count() == 0;
+                            .filter(plan -> plan.getLastNeedRedeployDate().after(api.getDeployedAt())).count() == 0;
                 }
 
                 return sync;

gravitee-io/gravitee-api-management

gravitee-management-api-service/src/main/java/io/gravitee/management/service/impl/ApiServiceImpl.java
@@ -603,7 +603,7 @@ public class ApiServiceImpl extends TransactionalService implements ApiService {
                     Set<PlanEntity> plans = planService.findByApi(api.getId());
                     sync = plans.stream()
                             .filter(plan -> plan.getStatus() != PlanStatus.STAGING)
-                            .filter(plan -> plan.getUpdatedAt().after(api.getDeployedAt())).count() == 0;
+                            .filter(plan -> plan.getNeedRedeployAt().after(api.getDeployedAt())).count() == 0;
                 }
 
                 return sync;