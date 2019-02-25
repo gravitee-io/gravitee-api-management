micheldeblok/gravitee-management-rest-api

feat(tree): fetch pages recursively
fix gravitee-io/issues#1565

gravitee-management-api-service/src/main/java/io/gravitee/management/service/PageService.java
@@ -37,9 +37,9 @@ public interface PageService {
 
 	void transformSwagger(PageEntity pageEntity, String apiId);
 
-	PageEntity createApiPage(String apiId, NewPageEntity page);
+	PageEntity createPage(String apiId, NewPageEntity page);
 
-	PageEntity createPortalPage(NewPageEntity page);
+	PageEntity createPage(NewPageEntity page);
 	
 	PageEntity update(String pageId, UpdatePageEntity updatePageEntity);
 
@@ -54,4 +54,8 @@ public interface PageService {
 	boolean isDisplayable(ApiEntity api, boolean isPagePublished, String username);
 
 	PageEntity fetch(String pageId, String contributor);
+
+	List<PageEntity> importFiles(NewPageEntity pageEntity);
+
+	List<PageEntity> importFiles(String apiId, NewPageEntity pageEntity);
 }

gravitee-management-api-service/src/test/java/io/gravitee/management/service/PageService_ImportDescriptorTest.java
@@ -1,0 +1,116 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.management.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.gravitee.common.utils.UUID;
+import io.gravitee.management.fetcher.FetcherConfigurationFactory;
+import io.gravitee.management.model.NewPageEntity;
+import io.gravitee.management.model.PageEntity;
+import io.gravitee.management.model.PageSourceEntity;
+import io.gravitee.management.service.impl.GraviteeDescriptorServiceImpl;
+import io.gravitee.management.service.impl.PageServiceImpl;
+import io.gravitee.management.service.search.SearchEngineService;
+import io.gravitee.plugin.core.api.PluginManager;
+import io.gravitee.plugin.fetcher.FetcherPlugin;
+import io.gravitee.repository.management.api.PageRepository;
+import io.gravitee.repository.management.model.Page;
+import io.gravitee.repository.management.model.PageType;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatcher;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.context.ApplicationContext;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Matchers.*;
+import static org.mockito.Mockito.*;
+
+/**
+ * @author Nicolas GERAUD (nicolas.geraud at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class PageService_ImportDescriptorTest {
+
+
+    @InjectMocks
+    private PageServiceImpl pageService = new PageServiceImpl();
+
+    @Mock
+    private PageRepository pageRepository;
+
+    @Mock
+    private AuditService auditService;
+
+    @Mock
+    private SearchEngineService searchEngineService;
+
+    @Mock
+    private PluginManager<FetcherPlugin> fetcherPluginManager;
+
+    @Mock
+    private FetcherConfigurationFactory fetcherConfigurationFactory;
+
+    @Mock
+    private ApplicationContext applicationContext;
+
+    @Mock
+    private GraviteeDescriptorServiceImpl graviteeDescriptorService;
+
+    @Mock
+    private ObjectMapper mockMapper;
+
+    private ObjectMapper mapper = new ObjectMapper();
+
+    @Test
+    public void shouldImportDescriptor() throws Exception {
+        PageSourceEntity pageSource = new PageSourceEntity();
+        pageSource.setType("type");
+        pageSource.setConfiguration(mapper.readTree("{}"));
+        NewPageEntity pageEntity = new NewPageEntity();
+        pageEntity.setSource(pageSource);
+        FetcherPlugin fetcherPlugin = mock(FetcherPlugin.class);
+        when(fetcherPlugin.clazz()).thenReturn("io.gravitee.management.service.PageService_ImportDescriptorMockFetcher");
+        when(fetcherPlugin.configuration()).thenReturn(PageService_MockDescriptorFetcherConfiguration.class);
+        when(fetcherPluginManager.get(any())).thenReturn(fetcherPlugin);
+        Class<PageService_ImportDescriptorMockFetcher> mockFetcherClass = PageService_ImportDescriptorMockFetcher.class;
+        when(fetcherPlugin.fetcher()).thenReturn(mockFetcherClass);
+        PageService_MockDescriptorFetcherConfiguration fetcherConfiguration = new PageService_MockDescriptorFetcherConfiguration();
+        when(fetcherConfigurationFactory.create(eq(PageService_MockDescriptorFetcherConfiguration.class), anyString())).thenReturn(fetcherConfiguration);
+        AutowireCapableBeanFactory mockAutowireCapableBeanFactory = mock(AutowireCapableBeanFactory.class);
+        when(applicationContext.getAutowireCapableBeanFactory()).thenReturn(mockAutowireCapableBeanFactory);
+        Page newPage = mock(Page.class);
+        when(newPage.getId()).thenReturn(UUID.toString(UUID.random()));
+        when(pageRepository.create(any())).thenReturn(newPage);
+        when(graviteeDescriptorService.descriptorName()).thenReturn(".gravitee.json");
+        when(graviteeDescriptorService.read(anyString())).thenCallRealMethod();
+        when(mockMapper.valueToTree(anyString())).thenCallRealMethod();
+
+        List<PageEntity> pageEntities = pageService.importFiles(pageEntity);
+
+
+        assertNotNull(pageEntities);
+        assertEquals(8, pageEntities.size());
+
+    }
+}

gravitee-io/gravitee-api-management

gravitee-management-api-service/src/main/java/io/gravitee/management/service/PageService.java
@@ -37,9 +37,9 @@ public interface PageService {
 
 	void transformSwagger(PageEntity pageEntity, String apiId);
 
-	PageEntity createApiPage(String apiId, NewPageEntity page);
+	PageEntity createPage(String apiId, NewPageEntity page);
 
-	PageEntity createPortalPage(NewPageEntity page);
+	PageEntity createPage(NewPageEntity page);
 	
 	PageEntity update(String pageId, UpdatePageEntity updatePageEntity);
 
@@ -54,4 +54,8 @@ public interface PageService {
 	boolean isDisplayable(ApiEntity api, boolean isPagePublished, String username);
 
 	PageEntity fetch(String pageId, String contributor);
+
+	List<PageEntity> importFiles(ImportPageEntity pageEntity);
+
+	List<PageEntity> importFiles(String apiId, ImportPageEntity pageEntity);
 }

gravitee-management-api-service/src/test/java/io/gravitee/management/service/PageService_ImportDescriptorTest.java
@@ -1,0 +1,115 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.management.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.gravitee.common.utils.UUID;
+import io.gravitee.management.fetcher.FetcherConfigurationFactory;
+import io.gravitee.management.model.ImportPageEntity;
+import io.gravitee.management.model.PageEntity;
+import io.gravitee.management.model.PageSourceEntity;
+import io.gravitee.management.service.impl.GraviteeDescriptorServiceImpl;
+import io.gravitee.management.service.impl.PageServiceImpl;
+import io.gravitee.management.service.search.SearchEngineService;
+import io.gravitee.plugin.core.api.PluginManager;
+import io.gravitee.plugin.fetcher.FetcherPlugin;
+import io.gravitee.repository.management.api.PageRepository;
+import io.gravitee.repository.management.model.Page;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.context.ApplicationContext;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Matchers.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * @author Nicolas GERAUD (nicolas.geraud at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class PageService_ImportDescriptorTest {
+
+
+    @InjectMocks
+    private PageServiceImpl pageService = new PageServiceImpl();
+
+    @Mock
+    private PageRepository pageRepository;
+
+    @Mock
+    private AuditService auditService;
+
+    @Mock
+    private SearchEngineService searchEngineService;
+
+    @Mock
+    private PluginManager<FetcherPlugin> fetcherPluginManager;
+
+    @Mock
+    private FetcherConfigurationFactory fetcherConfigurationFactory;
+
+    @Mock
+    private ApplicationContext applicationContext;
+
+    @Mock
+    private GraviteeDescriptorServiceImpl graviteeDescriptorService;
+
+    @Mock
+    private ObjectMapper mockMapper;
+
+    private ObjectMapper mapper = new ObjectMapper();
+
+    @Test
+    public void shouldImportDescriptor() throws Exception {
+        PageSourceEntity pageSource = new PageSourceEntity();
+        pageSource.setType("type");
+        pageSource.setConfiguration(mapper.readTree("{}"));
+        ImportPageEntity pageEntity = new ImportPageEntity();
+        pageEntity.setSource(pageSource);
+        FetcherPlugin fetcherPlugin = mock(FetcherPlugin.class);
+        when(fetcherPlugin.clazz()).thenReturn("io.gravitee.management.service.PageService_ImportDescriptorMockFetcher");
+        when(fetcherPlugin.configuration()).thenReturn(PageService_MockDescriptorFetcherConfiguration.class);
+        when(fetcherPluginManager.get(any())).thenReturn(fetcherPlugin);
+        Class<PageService_ImportDescriptorMockFetcher> mockFetcherClass = PageService_ImportDescriptorMockFetcher.class;
+        when(fetcherPlugin.fetcher()).thenReturn(mockFetcherClass);
+        PageService_MockDescriptorFetcherConfiguration fetcherConfiguration = new PageService_MockDescriptorFetcherConfiguration();
+        when(fetcherConfigurationFactory.create(eq(PageService_MockDescriptorFetcherConfiguration.class), anyString())).thenReturn(fetcherConfiguration);
+        AutowireCapableBeanFactory mockAutowireCapableBeanFactory = mock(AutowireCapableBeanFactory.class);
+        when(applicationContext.getAutowireCapableBeanFactory()).thenReturn(mockAutowireCapableBeanFactory);
+        Page newPage = mock(Page.class);
+        when(newPage.getId()).thenReturn(UUID.toString(UUID.random()));
+        when(pageRepository.create(any())).thenReturn(newPage);
+        when(graviteeDescriptorService.descriptorName()).thenReturn(".gravitee.json");
+        when(graviteeDescriptorService.read(anyString())).thenCallRealMethod();
+        when(mockMapper.valueToTree(anyString())).thenCallRealMethod();
+
+        List<PageEntity> pageEntities = pageService.importFiles(pageEntity);
+
+
+        assertNotNull(pageEntities);
+        assertEquals(8, pageEntities.size());
+
+    }
+}