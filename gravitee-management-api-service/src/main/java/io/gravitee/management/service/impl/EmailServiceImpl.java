fbf41ea81066e44232d72bfebd3e204a15686389
Titouan COMPIEGNE <titouan.compiegne@gmail.com>
2019-02-25 09:28:00 +0100

    fix(email): do not load local image resources if src tag is an absolute http url
    fixes gravitee-io/issues#1972

--- gravitee-management-api-service/src/main/java/io/gravitee/management/service/impl/EmailServiceImpl.java
+++ gravitee-management-api-service/src/main/java/io/gravitee/management/service/impl/EmailServiceImpl.java
@@ -124,6 +124,7 @@ public class EmailServiceImpl extends TransactionalService implements EmailServi
         final Elements imageElements = document.getElementsByTag("img");
         resources.addAll(imageElements.stream()
                 .filter(imageElement -> imageElement.hasAttr("src"))
+                .filter(imageElement -> !imageElement.attr("src").startsWith("http"))
                 .map(imageElement -> {
                     final String src = imageElement.attr("src");
                     imageElement.attr("src", "cid:" + src);
