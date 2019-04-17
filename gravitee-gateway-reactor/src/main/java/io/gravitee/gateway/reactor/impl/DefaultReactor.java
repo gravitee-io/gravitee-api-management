mgerold/gravitee-gateway

feat(api): Define response templates to override policy response
Closes gravitee-io/issues#972

gravitee-gateway-reactor/src/main/java/io/gravitee/gateway/reactor/impl/DefaultReactor.java
@@ -93,18 +93,22 @@ public class DefaultReactor extends AbstractService implements
                                     // Ensure that response has been ended before going further
                                     context1.response().end();
 
-                                    responseProcessorChainFactory
-                                            .create()
-                                            .handler(handler)
-                                            .handle(context1);
+                                    processResponse(context, handler);
                                 })
                                 .handle(ctx);
                     } else {
                         sendNotFound(context);
                     }
                 })
-                .errorHandler(__ -> {})
-                .exitHandler(__ -> {})
+                .errorHandler(__ -> processResponse(context, handler))
+                .exitHandler(__ -> processResponse(context, handler))
+                .handle(context);
+    }
+
+    private void processResponse(ExecutionContext context, Handler<ExecutionContext> handler) {
+        responseProcessorChainFactory
+                .create()
+                .handler(handler)
                 .handle(context);
     }
 

gravitee-io/gravitee-api-management

gravitee-gateway-reactor/src/main/java/io/gravitee/gateway/reactor/impl/DefaultReactor.java
@@ -93,18 +93,22 @@ public class DefaultReactor extends AbstractService implements
                                     // Ensure that response has been ended before going further
                                     context1.response().end();
 
-                                    responseProcessorChainFactory
-                                            .create()
-                                            .handler(handler)
-                                            .handle(context1);
+                                    processResponse(context1, handler);
                                 })
                                 .handle(ctx);
                     } else {
-                        sendNotFound(context);
+                        sendNotFound(ctx);
                     }
                 })
-                .errorHandler(__ -> {})
-                .exitHandler(__ -> {})
+                .errorHandler(__ -> processResponse(context, handler))
+                .exitHandler(__ -> processResponse(context, handler))
+                .handle(context);
+    }
+
+    private void processResponse(ExecutionContext context, Handler<ExecutionContext> handler) {
+        responseProcessorChainFactory
+                .create()
+                .handler(handler)
                 .handle(context);
     }
 