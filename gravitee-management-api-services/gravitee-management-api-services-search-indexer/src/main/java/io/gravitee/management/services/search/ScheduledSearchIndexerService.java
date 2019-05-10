humblemh/gravitee-management-rest-api

feat(search): synchronize search indexes across management instances
fix gravitee-io/issues#2166

gravitee-management-api-services/gravitee-management-api-services-search-indexer/src/main/java/io/gravitee/management/services/search/ScheduledSearchIndexerService.java
@@ -1,0 +1,106 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.management.services.search;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.gravitee.common.service.AbstractService;
+import io.gravitee.management.model.message.MessageEntity;
+import io.gravitee.management.model.message.MessageQuery;
+import io.gravitee.management.model.message.MessageSearchIndexerEntity;
+import io.gravitee.management.model.message.MessageTags;
+import io.gravitee.management.model.search.Indexable;
+import io.gravitee.management.service.MessageService;
+import io.gravitee.management.service.search.SearchEngineService;
+import io.gravitee.repository.management.model.MessageRecipient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.support.CronTrigger;
+
+import java.io.IOException;
+import java.time.Instant;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * @author Nicolas GERAUD (nicolas.geraud at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+public class ScheduledSearchIndexerService extends AbstractService implements Runnable {
+
+    /**
+     * Logger.
+     */
+    private final Logger logger = LoggerFactory.getLogger(ScheduledSearchIndexerService.class);
+
+    @Autowired
+    private TaskScheduler scheduler;
+
+    @Value("${services.search_indexer.cron:*/2 * * * * *}")
+    private String cronTrigger;
+
+    @Value("${services.search_indexer.enabled:true}")
+    private boolean enabled;
+
+    private final AtomicLong counter = new AtomicLong(0);
+
+    @Autowired
+    private MessageService messageService;
+
+    @Autowired
+    private SearchEngineService searchEngineService;
+
+    private ObjectMapper mapper = new ObjectMapper();
+
+    @Override
+    protected String name() {
+        return "Search Indexer Service";
+    }
+
+    @Override
+    protected void doStart() throws Exception {
+        if (enabled) {
+            super.doStart();
+            logger.info("Search Indexer service has been initialized with cron [{}]", cronTrigger);
+            scheduler.schedule(this, new CronTrigger(cronTrigger));
+        } else {
+            logger.warn("Search Indexer service has been disabled");
+        }
+    }
+
+    @Override
+    public void run() {
+        logger.debug("Search Indexer #{} started at {}", counter.incrementAndGet(), Instant.now());
+        MessageQuery query = new MessageQuery();
+        query.setTo(MessageRecipient.MANAGEMENT_APIS.name());
+        query.setTags(Collections.singletonList(MessageTags.DATA_TO_INDEX));
+        List<MessageEntity> messageEntities = messageService.search(query);
+        messageEntities.forEach( messageEntity -> {
+            messageService.ack(messageEntity.getId());
+            try {
+                searchEngineService.readMessage(
+                        mapper.readValue(messageEntity.getContent(), MessageSearchIndexerEntity.class));
+            } catch (IOException e) {
+                logger.error("Search Indexer has received a bad message.", e);
+            }
+        });
+
+        logger.debug("Search Indexer #{} ended at {}", counter.get(), Instant.now());
+    }
+}

gravitee-management-api-services/gravitee-management-api-services-search-indexer/src/test/java/io/gravitee/management/services/search/ScheduledSearchIndexerServiceTest.java
@@ -1,0 +1,77 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.management.services.search;
+
+import io.gravitee.management.model.message.MessageEntity;
+import io.gravitee.management.model.message.MessageTags;
+import io.gravitee.management.service.MessageService;
+import io.gravitee.management.service.search.SearchEngineService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import static org.mockito.Mockito.*;
+
+/**
+ * @author Nicolas GERAUD (nicolas.geraud at graviteesource.com) 
+ * @author GraviteeSource Team
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class ScheduledSearchIndexerServiceTest {
+
+    @InjectMocks
+    ScheduledSearchIndexerService service = new ScheduledSearchIndexerService();
+
+    @Mock
+    MessageService messageService;
+
+    @Mock
+    SearchEngineService searchEngineService;
+
+    @Test
+    public void shouldDoNothing() {
+        when(messageService.search(any())).thenReturn(Collections.emptyList());
+
+        service.run();
+
+        verify(messageService, never()).ack(anyString());
+        verify(searchEngineService, never()).readMessage(any());
+    }
+
+
+    @Test
+    public void shouldInsertAndDelete() {
+        MessageEntity insert = new MessageEntity();
+        insert.setId("insertid");
+        insert.setTags(Collections.singletonList(MessageTags.DATA_TO_INDEX));
+        insert.setContent("{\"id\":\"1\"}");
+        MessageEntity delete = new MessageEntity();
+        delete.setId("deleteid");
+        delete.setTags(Collections.singletonList(MessageTags.DATA_TO_INDEX));
+        delete.setContent("{\"id\":\"2\"}");
+        when(messageService.search(any())).thenReturn(Arrays.asList(delete, insert));
+
+        service.run();
+
+        verify(messageService, times(2)).ack(anyString());
+        verify(searchEngineService, times(2)).readMessage(any());
+    }
+}

gravitee-io/gravitee-api-management

gravitee-management-api-services/gravitee-management-api-services-search-indexer/src/main/java/io/gravitee/management/services/search/ScheduledSearchIndexerService.java
@@ -1,0 +1,105 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.management.services.search;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.gravitee.common.service.AbstractService;
+import io.gravitee.management.model.command.CommandEntity;
+import io.gravitee.management.model.command.CommandQuery;
+import io.gravitee.management.model.command.CommandSearchIndexerEntity;
+import io.gravitee.management.model.command.CommandTags;
+import io.gravitee.management.service.CommandService;
+import io.gravitee.management.service.search.SearchEngineService;
+import io.gravitee.repository.management.model.MessageRecipient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.support.CronTrigger;
+
+import java.io.IOException;
+import java.time.Instant;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * @author Nicolas GERAUD (nicolas.geraud at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+public class ScheduledSearchIndexerService extends AbstractService implements Runnable {
+
+    /**
+     * Logger.
+     */
+    private final Logger logger = LoggerFactory.getLogger(ScheduledSearchIndexerService.class);
+
+    @Autowired
+    private TaskScheduler scheduler;
+
+    @Value("${services.search_indexer.cron:*/5 * * * * *}")
+    private String cronTrigger;
+
+    @Value("${services.search_indexer.enabled:true}")
+    private boolean enabled;
+
+    private final AtomicLong counter = new AtomicLong(0);
+
+    @Autowired
+    private CommandService commandService;
+
+    @Autowired
+    private SearchEngineService searchEngineService;
+
+    private ObjectMapper mapper = new ObjectMapper();
+
+    @Override
+    protected String name() {
+        return "Search Indexer Service";
+    }
+
+    @Override
+    protected void doStart() throws Exception {
+        if (enabled) {
+            super.doStart();
+            logger.info("Search Indexer service has been initialized with cron [{}]", cronTrigger);
+            scheduler.schedule(this, new CronTrigger(cronTrigger));
+        } else {
+            logger.warn("Search Indexer service has been disabled");
+        }
+    }
+
+    @Override
+    public void run() {
+        logger.debug("Search Indexer #{} started at {}", counter.incrementAndGet(), Instant.now());
+        CommandQuery query = new CommandQuery();
+        query.setTo(MessageRecipient.MANAGEMENT_APIS.name());
+        query.setTags(Collections.singletonList(CommandTags.DATA_TO_INDEX));
+        List<CommandEntity> messageEntities = commandService.search(query);
+        messageEntities.forEach(commandEntity -> {
+            commandService.ack(commandEntity.getId());
+            try {
+                searchEngineService.process(
+                        mapper.readValue(commandEntity.getContent(), CommandSearchIndexerEntity.class));
+            } catch (IOException e) {
+                logger.error("Search Indexer has received a bad message.", e);
+            }
+        });
+
+        logger.debug("Search Indexer #{} ended at {}", counter.get(), Instant.now());
+    }
+}

gravitee-management-api-services/gravitee-management-api-services-search-indexer/src/test/java/io/gravitee/management/services/search/ScheduledSearchIndexerServiceTest.java
@@ -1,0 +1,77 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.management.services.search;
+
+import io.gravitee.management.model.command.CommandEntity;
+import io.gravitee.management.model.command.CommandTags;
+import io.gravitee.management.service.CommandService;
+import io.gravitee.management.service.search.SearchEngineService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import static org.mockito.Mockito.*;
+
+/**
+ * @author Nicolas GERAUD (nicolas.geraud at graviteesource.com) 
+ * @author GraviteeSource Team
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class ScheduledSearchIndexerServiceTest {
+
+    @InjectMocks
+    ScheduledSearchIndexerService service = new ScheduledSearchIndexerService();
+
+    @Mock
+    CommandService commandService;
+
+    @Mock
+    SearchEngineService searchEngineService;
+
+    @Test
+    public void shouldDoNothing() {
+        when(commandService.search(any())).thenReturn(Collections.emptyList());
+
+        service.run();
+
+        verify(commandService, never()).ack(anyString());
+        verify(searchEngineService, never()).process(any());
+    }
+
+
+    @Test
+    public void shouldInsertAndDelete() {
+        CommandEntity insert = new CommandEntity();
+        insert.setId("insertid");
+        insert.setTags(Collections.singletonList(CommandTags.DATA_TO_INDEX));
+        insert.setContent("{\"id\":\"1\"}");
+        CommandEntity delete = new CommandEntity();
+        delete.setId("deleteid");
+        delete.setTags(Collections.singletonList(CommandTags.DATA_TO_INDEX));
+        delete.setContent("{\"id\":\"2\"}");
+        when(commandService.search(any())).thenReturn(Arrays.asList(delete, insert));
+
+        service.run();
+
+        verify(commandService, times(2)).ack(anyString());
+        verify(searchEngineService, times(2)).process(any());
+    }
+}