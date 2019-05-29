605ae1af8250a527d5a5a9e623bdca85cd66f52c
David BRASSELY <brasseld@gmail.com>
2019-05-29 15:02:54 +0200

    feat(plan): Add a selection rule to the plan to select it using contextual data
    closes gravitee-io/issues#2220

--- src/main/java/io/gravitee/repository/mongodb/management/internal/model/PlanMongo.java
+++ src/main/java/io/gravitee/repository/mongodb/management/internal/model/PlanMongo.java
@@ -83,6 +83,8 @@ public class PlanMongo extends Auditable {
 
     private Set<String> tags;
 
+    private String selectionRule;
+
     public String getId() {
         return id;
     }
@@ -233,6 +235,14 @@ public class PlanMongo extends Auditable {
 
     public void setTags(Set<String> tags) {
         this.tags = tags;
+    }
+
+    public String getSelectionRule() {
+        return selectionRule;
+    }
+
+    public void setSelectionRule(String selectionRule) {
+        this.selectionRule = selectionRule;
     }
 
     @Override
