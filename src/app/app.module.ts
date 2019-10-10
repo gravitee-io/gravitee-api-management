37262cfbe5e6430a7ad89453e6b4bcb36f397da3
Guillaume Cusnieux <guillaume.cusnieux@graviteesource.com>
2019-10-10 11:30:00 +0200

    chore: add support for webcomponents

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  schemas: [
+      CUSTOM_ELEMENTS_SCHEMA,
+  ],
+  bootstrap: [
+    AppComponent,
+  ]
 })
-export class AppModule { }
+export class AppModule {
+}
