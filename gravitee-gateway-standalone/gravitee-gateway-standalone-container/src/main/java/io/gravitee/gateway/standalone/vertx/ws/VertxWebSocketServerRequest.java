e5c9e57ecc7968f013df4365c9b93e1280c48107
Florent CHAMFROY <florent.chamfroy@graviteesource.com>
2019-10-14 16:28:12 +0200

    feat(gateway): remove legacy mode for url encoding Closes gravitee-io/issues#2634

--- gravitee-gateway-standalone/gravitee-gateway-standalone-container/src/main/java/io/gravitee/gateway/standalone/vertx/ws/VertxWebSocketServerRequest.java
+++ gravitee-gateway-standalone/gravitee-gateway-standalone-container/src/main/java/io/gravitee/gateway/standalone/vertx/ws/VertxWebSocketServerRequest.java
@@ -29,7 +29,7 @@ public class VertxWebSocketServerRequest extends VertxHttpServerRequest {
     private VertxWebSocket vertxWebSocket;
 
     public VertxWebSocketServerRequest(HttpServerRequest httpServerRequest) {
-        super(httpServerRequest, false);
+        super(httpServerRequest);
 
         this.vertxWebSocket = new VertxWebSocket(httpServerRequest);
     }
