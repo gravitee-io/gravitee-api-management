RTabary/gravitee-management-rest-api

feat(multi-env): add organization feature
Closes gravitee-io/issues#3182

gravitee-rest-api-service/src/main/java/io/gravitee/rest/api/service/impl/EnvironmentServiceImpl.java
@@ -24,18 +24,16 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import io.gravitee.common.utils.IdGenerator;
+import io.gravitee.common.utils.UUID;
 import io.gravitee.repository.exceptions.TechnicalException;
 import io.gravitee.repository.management.api.EnvironmentRepository;
 import io.gravitee.repository.management.model.Environment;
 import io.gravitee.rest.api.model.EnvironmentEntity;
 import io.gravitee.rest.api.model.NewEnvironmentEntity;
 import io.gravitee.rest.api.model.UpdateEnvironmentEntity;
-import io.gravitee.rest.api.service.EnvironmentService;
+import io.gravitee.rest.api.service.*;
 import io.gravitee.rest.api.service.common.GraviteeContext;
-import io.gravitee.rest.api.service.exceptions.DuplicateEnvironmentNameException;
-import io.gravitee.rest.api.service.exceptions.EnvironmentNotFoundException;
-import io.gravitee.rest.api.service.exceptions.TechnicalManagementException;
+import io.gravitee.rest.api.service.exceptions.*;
 
 /**
  * @author Florent CHAMFROY (florent.chamfroy at graviteesource.com)
@@ -48,6 +46,18 @@ public class EnvironmentServiceImpl extends TransactionalService implements Envi
 
     @Autowired
     private EnvironmentRepository environmentRepository;
+
+    @Autowired
+    private OrganizationService organizationService;
+
+    @Autowired
+    private ApiHeaderService apiHeaderService;
+
+    @Autowired
+    private ViewService viewService;
+    
+    @Autowired
+    private PageService pageService;
 
     @Override
     public EnvironmentEntity findById(String environmentId) {
@@ -81,19 +91,26 @@ public class EnvironmentServiceImpl extends TransactionalService implements Envi
 
     @Override
     public EnvironmentEntity create(final NewEnvironmentEntity environmentEntity) {
-        // First we prevent the duplicate environment name
-        final List<String> environmentNames = this.findAll().stream()
-                .map(EnvironmentEntity::getName)
-                .collect(Collectors.toList());
-
-        if (environmentNames.contains(environmentEntity.getName())) {
-            throw new DuplicateEnvironmentNameException(environmentEntity.getName());
-        }
-
+        String organizationId = GraviteeContext.getCurrentOrganization();
+        // First we check that organization exist
+        this.organizationService.findById(organizationId);
+        
         try {
-            Environment environment = convert(environmentEntity);
-            EnvironmentEntity savedEnvironmentEntity = convert(environmentRepository.create(environment));
-            return savedEnvironmentEntity;
+            String id = UUID.toString(UUID.random());
+            Optional<Environment> checkEnvironment = environmentRepository.findById(id);
+            if (checkEnvironment.isPresent()) {
+                throw new EnvironmentAlreadyExistsException(id);
+            }
+            
+            Environment environment = convert(id, organizationId, environmentEntity);
+            EnvironmentEntity createdEnvironment = convert(environmentRepository.create(environment));
+            
+            //create Default items for environment
+            apiHeaderService.createDefault(createdEnvironment.getId());
+            viewService.createDefaultView(createdEnvironment.getId());
+            pageService.createDefaultPages(createdEnvironment.getId());
+            
+            return createdEnvironment;
         } catch (TechnicalException ex) {
             LOGGER.error("An error occurs while trying to create environment {}", environmentEntity.getName(), ex);
             throw new TechnicalManagementException("An error occurs while trying to create environment " + environmentEntity.getName(), ex);
@@ -126,6 +143,8 @@ public class EnvironmentServiceImpl extends TransactionalService implements Envi
             Optional<Environment> environmentOptional = environmentRepository.findById(environmentId);
             if (environmentOptional.isPresent()) {
                 environmentRepository.delete(environmentId);
+            } else {
+                throw new EnvironmentNotFoundException(environmentId);
             }
         } catch (TechnicalException ex) {
             LOGGER.error("An error occurs while trying to delete environment {}", environmentId, ex);
@@ -133,10 +152,13 @@ public class EnvironmentServiceImpl extends TransactionalService implements Envi
         }
     }
 
-    private Environment convert(final NewEnvironmentEntity environmentEntity) {
+    private Environment convert(final String id, final String organizationId, final NewEnvironmentEntity environmentEntity) {
         final Environment environment = new Environment();
-        environment.setId(IdGenerator.generate(environmentEntity.getName()));
+        environment.setId(id);
         environment.setName(environmentEntity.getName());
+        environment.setDescription(environmentEntity.getDescription());
+        environment.setOrganization(organizationId);
+        environment.setDomainRestrictions(environmentEntity.getDomainRestrictions());
         return environment;
     }
 
@@ -144,6 +166,9 @@ public class EnvironmentServiceImpl extends TransactionalService implements Envi
         final Environment environment = new Environment();
         environment.setId(environmentEntity.getId());
         environment.setName(environmentEntity.getName());
+        environment.setDescription(environmentEntity.getDescription());
+        environment.setOrganization(environmentEntity.getOrganizationId());
+        environment.setDomainRestrictions(environmentEntity.getDomainRestrictions());
         return environment;
     }
 
@@ -151,6 +176,9 @@ public class EnvironmentServiceImpl extends TransactionalService implements Envi
         final EnvironmentEntity environmentEntity = new EnvironmentEntity();
         environmentEntity.setId(environment.getId());
         environmentEntity.setName(environment.getName());
+        environmentEntity.setDescription(environment.getDescription());
+        environmentEntity.setOrganizationId(environment.getOrganization());
+        environmentEntity.setDomainRestrictions(environment.getDomainRestrictions());
         return environmentEntity;
     }
 
@@ -159,11 +187,26 @@ public class EnvironmentServiceImpl extends TransactionalService implements Envi
         Environment defaultEnvironment = new Environment();
         defaultEnvironment.setId(GraviteeContext.getDefaultEnvironment());
         defaultEnvironment.setName("Default environment");
+        defaultEnvironment.setDescription("Default environment");
+        defaultEnvironment.setOrganization(GraviteeContext.getDefaultOrganization());
         try {
             environmentRepository.create(defaultEnvironment);
         } catch (TechnicalException ex) {
             LOGGER.error("An error occurs while trying to create default environment", ex);
             throw new TechnicalManagementException("An error occurs while trying to create default environment", ex);
+        }
+    }
+
+    @Override
+    public List<EnvironmentEntity> findByOrganization(String organizationId) {
+        try {
+            LOGGER.debug("Find all environments by organization");
+            return environmentRepository.findByOrganization(organizationId)
+                    .stream()
+                    .map(this::convert).collect(Collectors.toList());
+        } catch (TechnicalException ex) {
+            LOGGER.error("An error occurs while trying to find all environments by organization {}", organizationId, ex);
+            throw new TechnicalManagementException("An error occurs while trying to find all environments by organization " + organizationId, ex);
         }
     }
 }

gravitee-rest-api-service/src/test/java/io/gravitee/rest/api/service/OrganizationService_CreateTest.java
@@ -1,0 +1,83 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.rest.api.service;
+
+import io.gravitee.repository.exceptions.TechnicalException;
+import io.gravitee.repository.management.api.OrganizationRepository;
+import io.gravitee.repository.management.model.Organization;
+import io.gravitee.rest.api.model.NewOrganizationEntity;
+import io.gravitee.rest.api.model.OrganizationEntity;
+import io.gravitee.rest.api.service.impl.OrganizationServiceImpl;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.Mockito.*;
+
+/**
+ * @author Nicolas GERAUD (nicolas.geraud at graviteesource.com)
+ * @author Titouan COMPIEGNE (titouan.compiegne at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class OrganizationService_CreateTest {
+
+    @InjectMocks
+    private OrganizationServiceImpl organizationService = new OrganizationServiceImpl();
+
+    @Mock
+    private OrganizationRepository mockOrganizationRepository;
+
+    @Mock
+    private RoleService mockRoleService;
+
+    @Test
+    public void shouldCreateOrganization() throws TechnicalException {
+        when(mockOrganizationRepository.findById(any())).thenReturn(Optional.empty());
+
+        NewOrganizationEntity org1 = new NewOrganizationEntity();
+        org1.setName("org_name");
+        org1.setDescription("org_desc");
+        List<String> domainRestrictions = Arrays.asList("domain", "restriction");
+        org1.setDomainRestrictions(domainRestrictions);
+        
+        Organization createdOrganization = new Organization();
+        createdOrganization.setId("created_org");
+        when(mockOrganizationRepository.create(any())).thenReturn(createdOrganization);
+
+        OrganizationEntity organization = organizationService.create(org1);
+
+        assertNotNull("result is null", organization);
+        verify(mockOrganizationRepository, times(1))
+            .create(argThat(arg -> 
+                arg != null 
+                && arg.getName().equals("org_name")
+                && arg.getDescription().equals("org_desc")
+                && arg.getDomainRestrictions().equals(domainRestrictions)
+            ));
+        verify(mockRoleService, times(1)).createDefaultRoles("created_org");
+        verify(mockRoleService, times(1)).createOrUpdateSystemRoles("created_org");
+    }
+}

gravitee-io/gravitee-api-management

gravitee-rest-api-service/src/main/java/io/gravitee/rest/api/service/impl/EnvironmentServiceImpl.java
@@ -24,18 +24,16 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import io.gravitee.common.utils.IdGenerator;
+import io.gravitee.common.utils.UUID;
 import io.gravitee.repository.exceptions.TechnicalException;
 import io.gravitee.repository.management.api.EnvironmentRepository;
 import io.gravitee.repository.management.model.Environment;
 import io.gravitee.rest.api.model.EnvironmentEntity;
 import io.gravitee.rest.api.model.NewEnvironmentEntity;
 import io.gravitee.rest.api.model.UpdateEnvironmentEntity;
-import io.gravitee.rest.api.service.EnvironmentService;
+import io.gravitee.rest.api.service.*;
 import io.gravitee.rest.api.service.common.GraviteeContext;
-import io.gravitee.rest.api.service.exceptions.DuplicateEnvironmentNameException;
-import io.gravitee.rest.api.service.exceptions.EnvironmentNotFoundException;
-import io.gravitee.rest.api.service.exceptions.TechnicalManagementException;
+import io.gravitee.rest.api.service.exceptions.*;
 
 /**
  * @author Florent CHAMFROY (florent.chamfroy at graviteesource.com)
@@ -48,6 +46,18 @@ public class EnvironmentServiceImpl extends TransactionalService implements Envi
 
     @Autowired
     private EnvironmentRepository environmentRepository;
+
+    @Autowired
+    private OrganizationService organizationService;
+
+    @Autowired
+    private ApiHeaderService apiHeaderService;
+
+    @Autowired
+    private ViewService viewService;
+    
+    @Autowired
+    private PageService pageService;
 
     @Override
     public EnvironmentEntity findById(String environmentId) {
@@ -81,19 +91,26 @@ public class EnvironmentServiceImpl extends TransactionalService implements Envi
 
     @Override
     public EnvironmentEntity create(final NewEnvironmentEntity environmentEntity) {
-        // First we prevent the duplicate environment name
-        final List<String> environmentNames = this.findAll().stream()
-                .map(EnvironmentEntity::getName)
-                .collect(Collectors.toList());
-
-        if (environmentNames.contains(environmentEntity.getName())) {
-            throw new DuplicateEnvironmentNameException(environmentEntity.getName());
-        }
-
+        String organizationId = GraviteeContext.getCurrentOrganization();
+        // First we check that organization exist
+        this.organizationService.findById(organizationId);
+        
         try {
-            Environment environment = convert(environmentEntity);
-            EnvironmentEntity savedEnvironmentEntity = convert(environmentRepository.create(environment));
-            return savedEnvironmentEntity;
+            String id = UUID.toString(UUID.random());
+            Optional<Environment> checkEnvironment = environmentRepository.findById(id);
+            if (checkEnvironment.isPresent()) {
+                throw new EnvironmentAlreadyExistsException(id);
+            }
+            
+            Environment environment = convert(id, organizationId, environmentEntity);
+            EnvironmentEntity createdEnvironment = convert(environmentRepository.create(environment));
+            
+            //create Default items for environment
+            apiHeaderService.initialize(createdEnvironment.getId());
+            viewService.initialize(createdEnvironment.getId());
+            pageService.initialize(createdEnvironment.getId());
+            
+            return createdEnvironment;
         } catch (TechnicalException ex) {
             LOGGER.error("An error occurs while trying to create environment {}", environmentEntity.getName(), ex);
             throw new TechnicalManagementException("An error occurs while trying to create environment " + environmentEntity.getName(), ex);
@@ -126,6 +143,8 @@ public class EnvironmentServiceImpl extends TransactionalService implements Envi
             Optional<Environment> environmentOptional = environmentRepository.findById(environmentId);
             if (environmentOptional.isPresent()) {
                 environmentRepository.delete(environmentId);
+            } else {
+                throw new EnvironmentNotFoundException(environmentId);
             }
         } catch (TechnicalException ex) {
             LOGGER.error("An error occurs while trying to delete environment {}", environmentId, ex);
@@ -133,10 +152,13 @@ public class EnvironmentServiceImpl extends TransactionalService implements Envi
         }
     }
 
-    private Environment convert(final NewEnvironmentEntity environmentEntity) {
+    private Environment convert(final String id, final String organizationId, final NewEnvironmentEntity environmentEntity) {
         final Environment environment = new Environment();
-        environment.setId(IdGenerator.generate(environmentEntity.getName()));
+        environment.setId(id);
         environment.setName(environmentEntity.getName());
+        environment.setDescription(environmentEntity.getDescription());
+        environment.setOrganization(organizationId);
+        environment.setDomainRestrictions(environmentEntity.getDomainRestrictions());
         return environment;
     }
 
@@ -144,6 +166,9 @@ public class EnvironmentServiceImpl extends TransactionalService implements Envi
         final Environment environment = new Environment();
         environment.setId(environmentEntity.getId());
         environment.setName(environmentEntity.getName());
+        environment.setDescription(environmentEntity.getDescription());
+        environment.setOrganization(environmentEntity.getOrganizationId());
+        environment.setDomainRestrictions(environmentEntity.getDomainRestrictions());
         return environment;
     }
 
@@ -151,19 +176,37 @@ public class EnvironmentServiceImpl extends TransactionalService implements Envi
         final EnvironmentEntity environmentEntity = new EnvironmentEntity();
         environmentEntity.setId(environment.getId());
         environmentEntity.setName(environment.getName());
+        environmentEntity.setDescription(environment.getDescription());
+        environmentEntity.setOrganizationId(environment.getOrganization());
+        environmentEntity.setDomainRestrictions(environment.getDomainRestrictions());
         return environmentEntity;
     }
 
     @Override
-    public void createDefaultEnvironment() {
+    public void initialize() {
         Environment defaultEnvironment = new Environment();
         defaultEnvironment.setId(GraviteeContext.getDefaultEnvironment());
         defaultEnvironment.setName("Default environment");
+        defaultEnvironment.setDescription("Default environment");
+        defaultEnvironment.setOrganization(GraviteeContext.getDefaultOrganization());
         try {
             environmentRepository.create(defaultEnvironment);
         } catch (TechnicalException ex) {
             LOGGER.error("An error occurs while trying to create default environment", ex);
             throw new TechnicalManagementException("An error occurs while trying to create default environment", ex);
+        }
+    }
+
+    @Override
+    public List<EnvironmentEntity> findByOrganization(String organizationId) {
+        try {
+            LOGGER.debug("Find all environments by organization");
+            return environmentRepository.findByOrganization(organizationId)
+                    .stream()
+                    .map(this::convert).collect(Collectors.toList());
+        } catch (TechnicalException ex) {
+            LOGGER.error("An error occurs while trying to find all environments by organization {}", organizationId, ex);
+            throw new TechnicalManagementException("An error occurs while trying to find all environments by organization " + organizationId, ex);
         }
     }
 }

gravitee-rest-api-service/src/test/java/io/gravitee/rest/api/service/OrganizationService_CreateTest.java
@@ -1,0 +1,83 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.rest.api.service;
+
+import io.gravitee.repository.exceptions.TechnicalException;
+import io.gravitee.repository.management.api.OrganizationRepository;
+import io.gravitee.repository.management.model.Organization;
+import io.gravitee.rest.api.model.NewOrganizationEntity;
+import io.gravitee.rest.api.model.OrganizationEntity;
+import io.gravitee.rest.api.service.impl.OrganizationServiceImpl;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.Mockito.*;
+
+/**
+ * @author Nicolas GERAUD (nicolas.geraud at graviteesource.com)
+ * @author Titouan COMPIEGNE (titouan.compiegne at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class OrganizationService_CreateTest {
+
+    @InjectMocks
+    private OrganizationServiceImpl organizationService = new OrganizationServiceImpl();
+
+    @Mock
+    private OrganizationRepository mockOrganizationRepository;
+
+    @Mock
+    private RoleService mockRoleService;
+
+    @Test
+    public void shouldCreateOrganization() throws TechnicalException {
+        when(mockOrganizationRepository.findById(any())).thenReturn(Optional.empty());
+
+        NewOrganizationEntity org1 = new NewOrganizationEntity();
+        org1.setName("org_name");
+        org1.setDescription("org_desc");
+        List<String> domainRestrictions = Arrays.asList("domain", "restriction");
+        org1.setDomainRestrictions(domainRestrictions);
+        
+        Organization createdOrganization = new Organization();
+        createdOrganization.setId("created_org");
+        when(mockOrganizationRepository.create(any())).thenReturn(createdOrganization);
+
+        OrganizationEntity organization = organizationService.create(org1);
+
+        assertNotNull("result is null", organization);
+        verify(mockOrganizationRepository, times(1))
+            .create(argThat(arg -> 
+                arg != null 
+                && arg.getName().equals("org_name")
+                && arg.getDescription().equals("org_desc")
+                && arg.getDomainRestrictions().equals(domainRestrictions)
+            ));
+        verify(mockRoleService, times(1)).initialize("created_org");
+        verify(mockRoleService, times(1)).createOrUpdateSystemRoles("created_org");
+    }
+}