2246cb4898b78d27ff567d7b08f6dc130886b12e
David BRASSELY <brasseld@gmail.com>
2020-04-27 18:32:13 +0200

    fix(theme): Fallback to default theme if the custom one does not exist
    Closes gravitee-io/issues#3658

--- src/index.ts
+++ src/index.ts
@@ -82,7 +82,11 @@ function initTheme(constants:any) {
   return $http.get(`./themes/${constants.theme.name}-theme.json`, configNoCache)
     .then((response: any) => {
       angular.module('gravitee-portal').constant('Theme', response.data);
-    });
+    }).catch(() => {
+      return $http.get('./themes/default-theme.json', configNoCache).then((response: any) => {
+        angular.module('gravitee-portal').constant('Theme', response.data);
+      });
+    })
 }
 
 function bootstrapApplication() {
