db8b73721a4ffc0682c2073786f690e7e5c4ccda
Azize Elamrani <azize.elamrani@gmail.com>
2020-05-16 10:06:52 +0200

    fix: Errors when selecting a filter on an empty user agent
    fix gravitee-io/issues#3788

--- src/services/analytics.service.ts
+++ src/services/analytics.service.ts
@@ -71,7 +71,7 @@ class AnalyticsService {
     let q = this.$stateParams.q;
     if (q) {
       const queryFilters = {};
-      q.split(' ').forEach(q => {
+      q.split(/\s(OR|AND)\s/).forEach(q => {
         if (q.includes(':')) {
           let param = q.split(':');
           let keyParam = this.cleanParam(param[0]);
@@ -91,7 +91,7 @@ class AnalyticsService {
   buildQueryParam(queryParam, q: string) {
     queryParam = (q === 'body') ? ('*' + queryParam + '*') : queryParam;
     queryParam = (q === 'uri') ? (queryParam + '*') : queryParam;
-    queryParam = (q.includes('path')) ? ('\\"' + queryParam + '\\"') : queryParam;
+    queryParam = '\\"' + queryParam + '\\"';
     queryParam = queryParam.replace(/\//g, '\\\\/');
     return queryParam;
   }
