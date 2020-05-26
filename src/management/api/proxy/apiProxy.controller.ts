b4722fdb8cf9c3db7fd2cd47d880b994e5a13cbb
Nicolas Géraud <nicolas.geraud@gmail.com>
2020-05-27 01:09:16 +0200

    fix(cors): add regex support
    fix gravitee-io/issues#3840

--- src/management/api/proxy/apiProxy.controller.ts
+++ src/management/api/proxy/apiProxy.controller.ts
@@ -302,9 +302,8 @@ class ApiProxyController {
     return !_.includes(this.userTags, tag.id);
   }
 
-  controlAllowOrigin(chip, index, ev) {
+  controlAllowOrigin(chip, index) {
     if ("*" === chip) {
-      let that = this;
       this.$mdDialog.show({
         controller: 'DialogConfirmController',
         controllerAs: 'ctrl',
@@ -314,12 +313,27 @@ class ApiProxyController {
           title: 'Are you sure you want to remove all cross-origin restrictions?',
           confirmButton: 'Yes, I want to allow all origins.'
         }
-      }).then(function (response) {
+      }).then((response) => {
         if (!response) {
-          that.api.proxy.cors.allowOrigin.splice(index, 1);
+          this.api.proxy.cors.allowOrigin.splice(index, 1);
         }
       });
     }
+  }
+
+  isRegexValid() {
+    let isValid = true;
+    this.api.proxy.cors.allowOrigin.forEach(allowOrigin => {
+      if ("*" !== allowOrigin &&
+        (allowOrigin.includes("(") || allowOrigin.includes("[") || allowOrigin.includes("*"))) {
+        try {
+          new RegExp(allowOrigin);
+        } catch (e) {
+          isValid = false;
+        }
+      }
+    });
+    return isValid;
   }
 
   switchVirtualHostMode() {
