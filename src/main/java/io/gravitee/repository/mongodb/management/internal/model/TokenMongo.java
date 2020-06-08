05281443e10fc8e550e44add14ffcfe80f5f2318
Azize Elamrani <azize.elamrani@gmail.com>
2020-06-08 10:31:26 +0200

    feat: Allows to generate personal access tokens to access the management API
    fix gravitee-io/issues#2749

---
+++ src/main/java/io/gravitee/repository/mongodb/management/internal/model/TokenMongo.java
@@ -0,0 +1,131 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.repository.mongodb.management.internal.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+import java.util.Objects;
+
+/**
+ * @author Azize ELAMRANI (azize.elamrani at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+@Document(collection = "tokens")
+public class TokenMongo {
+
+	@Id
+	private String id;
+	private String token;
+	private String referenceType;
+	private String referenceId;
+	private String name;
+	private Date expiresAt;
+	private Date createdAt;
+	private Date lastUseAt;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	public String getReferenceType() {
+		return referenceType;
+	}
+
+	public void setReferenceType(String referenceType) {
+		this.referenceType = referenceType;
+	}
+
+	public String getReferenceId() {
+		return referenceId;
+	}
+
+	public void setReferenceId(String referenceId) {
+		this.referenceId = referenceId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getExpiresAt() {
+		return expiresAt;
+	}
+
+	public void setExpiresAt(Date expiresAt) {
+		this.expiresAt = expiresAt;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Date getLastUseAt() {
+		return lastUseAt;
+	}
+
+	public void setLastUseAt(Date lastUseAt) {
+		this.lastUseAt = lastUseAt;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (!(o instanceof TokenMongo)) return false;
+		TokenMongo tokenMongo = (TokenMongo) o;
+		return Objects.equals(id, tokenMongo.id);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public String toString() {
+		return "TokenMongo{" +
+				"id='" + id + '\'' +
+				", token='" + token + '\'' +
+				", referenceType='" + referenceType + '\'' +
+				", referenceId='" + referenceId + '\'' +
+				", name='" + name + '\'' +
+				", expiresAt=" + expiresAt +
+				", createdAt=" + createdAt +
+				", lastUseAt=" + lastUseAt +
+				'}';
+	}
+}
