6aa12f6c3d29f42c224c4ae4345745ed3a778cf5
Florent CHAMFROY <florent.chamfroy@graviteesource.com>
2020-06-26 10:37:09 +0200

    feat(repository): manage Org & Env in parameters
    Closes gravitee-io/issues#4642

--- src/main/java/io/gravitee/repository/management/api/ParameterRepository.java
+++ src/main/java/io/gravitee/repository/management/api/ParameterRepository.java
@@ -15,12 +15,12 @@
  */
 package io.gravitee.repository.management.api;
 
-import java.util.List;
-import java.util.Optional;
-
 import io.gravitee.repository.exceptions.TechnicalException;
 import io.gravitee.repository.management.model.Parameter;
 import io.gravitee.repository.management.model.ParameterReferenceType;
+
+import java.util.List;
+import java.util.Optional;
 
 /**
  * @author Azize ELAMRANI (azize.elamrani at graviteesource.com)
@@ -28,17 +28,17 @@ import io.gravitee.repository.management.model.ParameterReferenceType;
  * @author GraviteeSource Team
  */
 public interface ParameterRepository {
-    
-    Optional<Parameter> findById(String key) throws TechnicalException;
+
+    Optional<Parameter> findById(String key, String referenceId, ParameterReferenceType referenceType) throws TechnicalException;
 
     Parameter create(Parameter item) throws TechnicalException;
 
     Parameter update(Parameter item) throws TechnicalException;
 
-    void delete(String key) throws TechnicalException;
-    
-    List<Parameter> findAll(List<String> keys) throws TechnicalException;
-    
-    List<Parameter> findAllByReferenceIdAndReferenceType(List<String> keys, String referenceId, ParameterReferenceType referenceType) throws TechnicalException;
-    
+    void delete(String key, String referenceId, ParameterReferenceType referenceType) throws TechnicalException;
+
+    List<Parameter> findByKeys(List<String> keys, String referenceId, ParameterReferenceType referenceType) throws TechnicalException;
+
+    List<Parameter> findAll(String referenceId, ParameterReferenceType referenceType) throws TechnicalException;
+
 }
