6ba3e36f1a655209515a76f00349ab9acb27c592
eric <eric.leleu.dev@gmail.com>
2020-07-29 13:43:38 +0200

    feat(management-api/reporter): #146 addition of page revision
    fix gravitee-io/issues#146

---
+++ src/main/java/io/gravitee/repository/mongodb/management/MongoPageRevisionRepository.java
@@ -0,0 +1,107 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.repository.mongodb.management;
+
+import io.gravitee.repository.exceptions.TechnicalException;
+import io.gravitee.repository.management.api.PageRevisionRepository;
+import io.gravitee.repository.management.model.PageRevision;
+import io.gravitee.repository.mongodb.management.internal.model.PageRevisionMongo;
+import io.gravitee.repository.mongodb.management.internal.model.PageRevisionPkMongo;
+import io.gravitee.repository.mongodb.management.internal.page.revision.PageRevisionMongoRepository;
+import io.gravitee.repository.mongodb.management.mapper.GraviteeMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+/**
+ * @author Eric LELEU (eric.leleu at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+@Component
+public class MongoPageRevisionRepository implements PageRevisionRepository {
+
+    private static final Logger logger = LoggerFactory.getLogger(MongoPageRevisionRepository.class);
+
+    @Autowired
+    private PageRevisionMongoRepository internalPageRevisionRepo;
+
+    @Autowired
+    private GraviteeMapper mapper;
+
+    @Override
+    public Optional<PageRevision> findById(String pageId, int revision) throws TechnicalException {
+        logger.debug("Find page revision by ID [{}]", pageId);
+
+        PageRevisionMongo page = internalPageRevisionRepo.findById(new PageRevisionPkMongo(pageId, revision)).orElse(null);
+        PageRevision res = mapper.map(page, PageRevision.class);
+
+        logger.debug("Find page revision by ID [{}] - Done", pageId);
+        return Optional.ofNullable(res);
+    }
+
+    @Override
+    public PageRevision create(PageRevision page) throws TechnicalException {
+        logger.debug("Create revision for page [{}]", page.getName());
+
+        PageRevisionMongo pageMongo = mapper.map(page, PageRevisionMongo.class);
+        PageRevisionMongo createdPageMongo = internalPageRevisionRepo.insert(pageMongo);
+
+        PageRevision res = mapper.map(createdPageMongo, PageRevision.class);
+
+        logger.debug("Create revision for page [{}] - Done", page.getName());
+
+        return res;
+    }
+
+    @Override
+    public void deleteAllByPageId(String pageId) throws TechnicalException {
+        try {
+            internalPageRevisionRepo.deleteByPageId(pageId);
+        } catch (Exception e) {
+            logger.error("An error occurred when deleting all revisions for page [{}]", pageId, e);
+            throw new TechnicalException("An error occurred when deleting page revisions");
+        }
+    }
+
+    @Override
+    public List<PageRevision> findAllByPageId(String pageId) throws TechnicalException {
+        try {
+            return internalPageRevisionRepo.findAllByPageId(pageId)
+                    .stream()
+                    .map(rev -> mapper.map(rev, PageRevision.class))
+                    .collect(Collectors.toList());
+        } catch (Exception e) {
+            logger.error("An error occurred when querying all revisions for page [{}]", pageId, e);
+            throw new TechnicalException("An error occurred when querying page revisions");
+        }
+    }
+
+    @Override
+    public Optional<PageRevision> findLastByPageId(String pageId) throws TechnicalException {
+        try {
+            return internalPageRevisionRepo.findLastByPageId(pageId)
+                    .map(rev -> mapper.map(rev, PageRevision.class));
+        } catch (Exception e) {
+            logger.error("An error occurred when querying last revision for page [{}]", pageId, e);
+            throw new TechnicalException("An error occurred when querying the last page revision");
+        }
+    }
+}
