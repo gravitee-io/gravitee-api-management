cpuydebois/gravitee-management-rest-api

feat: Allows user to subscribe to the newsletter after login
fix: gravitee-io/issues#4096

gravitee-rest-api-management/gravitee-rest-api-management-rest/src/main/java/io/gravitee/rest/api/management/rest/resource/CurrentUserResource.java
@@ -62,6 +62,7 @@ import java.net.URI;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.*;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 import static io.gravitee.rest.api.management.rest.model.TokenType.BEARER;
@@ -136,6 +137,14 @@ public class CurrentUserResource extends AbstractResource {
                 userDetails.setEmail(details.getEmail());
             }
 
+            if (userEntity.getNewsletterSubscribed() == null) {
+                long diffInMs = Math.abs(new Date().getTime() - userEntity.getFirstConnectionAt().getTime());
+                long diff = TimeUnit.DAYS.convert(diffInMs, TimeUnit.MILLISECONDS);
+                userDetails.setNewsletterReminder(diff >= 7);
+            } else {
+                userDetails.setNewsletterReminder(false);
+            }
+
             //convert UserEntityRoles to UserDetailsRoles
             userDetails.setRoles(userEntity.getRoles().
                     stream().
@@ -173,6 +182,21 @@ public class CurrentUserResource extends AbstractResource {
         }
 
         return ok(userService.update(userEntity.getId(), user)).build();
+    }
+
+    @POST
+    @Path("/subscribeNewsletter")
+    @ApiOperation(value = "Subscribe to the newsletter the authenticated user")
+    @ApiResponses({
+        @ApiResponse(code = 200, message = "Updated user", response = UserEntity.class),
+        @ApiResponse(code = 400, message = "Invalid user profile"),
+        @ApiResponse(code = 404, message = "User not found"),
+        @ApiResponse(code = 500, message = "Internal server error")})
+    public Response subscribeNewsletter(@Valid @NotNull final String email) {
+        UserEntity userEntity = userService.findById(getAuthenticatedUser());
+        UpdateUserEntity user = new UpdateUserEntity(userEntity);
+        user.setNewsletter(true);
+        return ok(userService.update(userEntity.getId(), user, email)).build();
     }
 
     @GET

gravitee-rest-api-model/src/main/java/io/gravitee/rest/api/model/NewExternalUserEntity.java
@@ -54,7 +54,7 @@ public class NewExternalUserEntity {
      */
     private String sourceId;
 
-    private boolean newsletter;
+    private Boolean newsletter;
 
     public String getSource() {
         return source;
@@ -104,11 +104,11 @@ public class NewExternalUserEntity {
         this.email = email;
     }
 
-    public boolean isNewsletter() {
+    public Boolean isNewsletter() {
         return newsletter;
     }
 
-    public void setNewsletter(boolean newsletter) {
+    public void setNewsletter(Boolean newsletter) {
         this.newsletter = newsletter;
     }
 

gravitee-rest-api-service/src/main/java/io/gravitee/rest/api/service/impl/UserServiceImpl.java
@@ -168,11 +168,13 @@ public class UserServiceImpl extends AbstractService implements UserService {
 
             User user = checkUser.get();
             User previousUser = new User(user);
+
             // First connection: create default application for user & notify
-            if (user.getLastConnectionAt() == null) {
+            if (user.getLastConnectionAt() == null && user.getFirstConnectionAt() == null) {
                 notifierService.trigger(PortalHook.USER_FIRST_LOGIN, new NotificationParamsBuilder()
-                        .user(convert(user, false))
-                        .build());
+                    .user(convert(user, false))
+                    .build());
+                user.setFirstConnectionAt(new Date());
                 if (defaultApplicationForFirstConnection) {
                     LOGGER.debug("Create a default application for {}", userId);
                     NewApplicationEntity defaultApp = new NewApplicationEntity();
@@ -192,20 +194,22 @@ public class UserServiceImpl extends AbstractService implements UserService {
                     }
                 }
             }
-
             // Set date fields
             user.setLastConnectionAt(new Date());
+            if (user.getFirstConnectionAt() == null) {
+                user.setFirstConnectionAt(user.getLastConnectionAt());
+            }
             user.setUpdatedAt(user.getLastConnectionAt());
 
             user.setLoginCount(user.getLoginCount() + 1);
 
             User updatedUser = userRepository.update(user);
             auditService.createPortalAuditLog(
-                    Collections.singletonMap(USER, userId),
-                    User.AuditEvent.USER_CONNECTED,
-                    user.getUpdatedAt(),
-                    previousUser,
-                    user);
+                Collections.singletonMap(USER, userId),
+                User.AuditEvent.USER_CONNECTED,
+                user.getUpdatedAt(),
+                previousUser,
+                user);
 
             final UserEntity userEntity = convert(updatedUser, true);
             searchEngineService.index(userEntity, false);
@@ -581,7 +585,7 @@ public class UserServiceImpl extends AbstractService implements UserService {
         }
 
         if (newExternalUserEntity.isNewsletter()) {
-            newsletterService.subscribe(newExternalUserEntity);
+            newsletterService.subscribe(newExternalUserEntity.getEmail());
         }
 
         return userEntity;
@@ -644,6 +648,11 @@ public class UserServiceImpl extends AbstractService implements UserService {
 
     @Override
     public UserEntity update(String id, UpdateUserEntity updateUserEntity) {
+        return this.update(id, updateUserEntity, updateUserEntity.getEmail());
+    }
+
+    @Override
+    public UserEntity update(String id, UpdateUserEntity updateUserEntity, String newsletterEmail) {
         try {
             LOGGER.debug("Updating {}", updateUserEntity);
             Optional<User> checkUser = userRepository.findById(id);
@@ -673,8 +682,9 @@ public class UserServiceImpl extends AbstractService implements UserService {
                 user.setStatus(UserStatus.valueOf(updateUserEntity.getStatus()));
             }
 
-            if (updateUserEntity.isNewsletter() && user.getEmail() != null) {
-                newsletterService.subscribe(user);
+            user.setNewsletterSubscribed(updateUserEntity.isNewsletter());
+            if (updateUserEntity.isNewsletter() != null && updateUserEntity.isNewsletter() && newsletterEmail != null) {
+                newsletterService.subscribe(newsletterEmail);
             }
 
             User updatedUser = userRepository.update(user);
@@ -911,6 +921,7 @@ public class UserServiceImpl extends AbstractService implements UserService {
         user.setSourceId(newExternalUserEntity.getSourceId());
         user.setStatus(UserStatus.ACTIVE);
         user.setPicture(newExternalUserEntity.getPicture());
+        user.setNewsletterSubscribed(newExternalUserEntity.isNewsletter());
         return user;
     }
 
@@ -947,6 +958,7 @@ public class UserServiceImpl extends AbstractService implements UserService {
         userEntity.setCreatedAt(user.getCreatedAt());
         userEntity.setUpdatedAt(user.getUpdatedAt());
         userEntity.setLastConnectionAt(user.getLastConnectionAt());
+        userEntity.setFirstConnectionAt(user.getFirstConnectionAt());
         userEntity.setPicture(user.getPicture());
         if (user.getStatus() != null) {
             userEntity.setStatus(user.getStatus().name());
@@ -976,6 +988,7 @@ public class UserServiceImpl extends AbstractService implements UserService {
         }
 
         userEntity.setLoginCount(user.getLoginCount());
+        userEntity.setNewsletterSubscribed(user.getNewsletterSubscribed());
 
         return userEntity;
     }

gravitee-io/gravitee-api-management

gravitee-rest-api-management/gravitee-rest-api-management-rest/src/main/java/io/gravitee/rest/api/management/rest/resource/CurrentUserResource.java
@@ -62,6 +62,7 @@ import java.net.URI;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.*;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 import static io.gravitee.rest.api.management.rest.model.TokenType.BEARER;
@@ -136,6 +137,14 @@ public class CurrentUserResource extends AbstractResource {
                 userDetails.setEmail(details.getEmail());
             }
 
+            if (userEntity.getNewsletterSubscribed() == null) {
+                long diffInMs = Math.abs(new Date().getTime() - userEntity.getFirstConnectionAt().getTime());
+                long diff = TimeUnit.DAYS.convert(diffInMs, TimeUnit.MILLISECONDS);
+                userDetails.setDisplayNewsletterSubscription(diff >= 7);
+            } else {
+                userDetails.setDisplayNewsletterSubscription(false);
+            }
+
             //convert UserEntityRoles to UserDetailsRoles
             userDetails.setRoles(userEntity.getRoles().
                     stream().
@@ -173,6 +182,21 @@ public class CurrentUserResource extends AbstractResource {
         }
 
         return ok(userService.update(userEntity.getId(), user)).build();
+    }
+
+    @POST
+    @Path("/subscribeNewsletter")
+    @ApiOperation(value = "Subscribe to the newsletter the authenticated user")
+    @ApiResponses({
+        @ApiResponse(code = 200, message = "Updated user", response = UserEntity.class),
+        @ApiResponse(code = 400, message = "Invalid user profile"),
+        @ApiResponse(code = 404, message = "User not found"),
+        @ApiResponse(code = 500, message = "Internal server error")})
+    public Response subscribeNewsletter(@Valid @NotNull final String email) {
+        UserEntity userEntity = userService.findById(getAuthenticatedUser());
+        UpdateUserEntity user = new UpdateUserEntity(userEntity);
+        user.setNewsletter(true);
+        return ok(userService.update(userEntity.getId(), user, email)).build();
     }
 
     @GET

gravitee-rest-api-model/src/main/java/io/gravitee/rest/api/model/NewExternalUserEntity.java
@@ -54,7 +54,7 @@ public class NewExternalUserEntity {
      */
     private String sourceId;
 
-    private boolean newsletter;
+    private Boolean newsletter;
 
     public String getSource() {
         return source;
@@ -104,11 +104,11 @@ public class NewExternalUserEntity {
         this.email = email;
     }
 
-    public boolean isNewsletter() {
+    public Boolean getNewsletter() {
         return newsletter;
     }
 
-    public void setNewsletter(boolean newsletter) {
+    public void setNewsletter(Boolean newsletter) {
         this.newsletter = newsletter;
     }
 

gravitee-rest-api-service/src/main/java/io/gravitee/rest/api/service/impl/UserServiceImpl.java
@@ -168,11 +168,13 @@ public class UserServiceImpl extends AbstractService implements UserService {
 
             User user = checkUser.get();
             User previousUser = new User(user);
+
             // First connection: create default application for user & notify
-            if (user.getLastConnectionAt() == null) {
+            if (user.getLastConnectionAt() == null && user.getFirstConnectionAt() == null) {
                 notifierService.trigger(PortalHook.USER_FIRST_LOGIN, new NotificationParamsBuilder()
-                        .user(convert(user, false))
-                        .build());
+                    .user(convert(user, false))
+                    .build());
+                user.setFirstConnectionAt(new Date());
                 if (defaultApplicationForFirstConnection) {
                     LOGGER.debug("Create a default application for {}", userId);
                     NewApplicationEntity defaultApp = new NewApplicationEntity();
@@ -192,20 +194,22 @@ public class UserServiceImpl extends AbstractService implements UserService {
                     }
                 }
             }
-
             // Set date fields
             user.setLastConnectionAt(new Date());
+            if (user.getFirstConnectionAt() == null) {
+                user.setFirstConnectionAt(user.getLastConnectionAt());
+            }
             user.setUpdatedAt(user.getLastConnectionAt());
 
             user.setLoginCount(user.getLoginCount() + 1);
 
             User updatedUser = userRepository.update(user);
             auditService.createPortalAuditLog(
-                    Collections.singletonMap(USER, userId),
-                    User.AuditEvent.USER_CONNECTED,
-                    user.getUpdatedAt(),
-                    previousUser,
-                    user);
+                Collections.singletonMap(USER, userId),
+                User.AuditEvent.USER_CONNECTED,
+                user.getUpdatedAt(),
+                previousUser,
+                user);
 
             final UserEntity userEntity = convert(updatedUser, true);
             searchEngineService.index(userEntity, false);
@@ -580,8 +584,8 @@ public class UserServiceImpl extends AbstractService implements UserService {
             );
         }
 
-        if (newExternalUserEntity.isNewsletter()) {
-            newsletterService.subscribe(newExternalUserEntity);
+        if (newExternalUserEntity.getNewsletter() != null && newExternalUserEntity.getNewsletter()) {
+            newsletterService.subscribe(newExternalUserEntity.getEmail());
         }
 
         return userEntity;
@@ -644,6 +648,11 @@ public class UserServiceImpl extends AbstractService implements UserService {
 
     @Override
     public UserEntity update(String id, UpdateUserEntity updateUserEntity) {
+        return this.update(id, updateUserEntity, updateUserEntity.getEmail());
+    }
+
+    @Override
+    public UserEntity update(String id, UpdateUserEntity updateUserEntity, String newsletterEmail) {
         try {
             LOGGER.debug("Updating {}", updateUserEntity);
             Optional<User> checkUser = userRepository.findById(id);
@@ -673,8 +682,9 @@ public class UserServiceImpl extends AbstractService implements UserService {
                 user.setStatus(UserStatus.valueOf(updateUserEntity.getStatus()));
             }
 
-            if (updateUserEntity.isNewsletter() && user.getEmail() != null) {
-                newsletterService.subscribe(user);
+            user.setNewsletterSubscribed(updateUserEntity.isNewsletter());
+            if (updateUserEntity.isNewsletter() != null && updateUserEntity.isNewsletter() && newsletterEmail != null) {
+                newsletterService.subscribe(newsletterEmail);
             }
 
             User updatedUser = userRepository.update(user);
@@ -911,6 +921,7 @@ public class UserServiceImpl extends AbstractService implements UserService {
         user.setSourceId(newExternalUserEntity.getSourceId());
         user.setStatus(UserStatus.ACTIVE);
         user.setPicture(newExternalUserEntity.getPicture());
+        user.setNewsletterSubscribed(newExternalUserEntity.getNewsletter());
         return user;
     }
 
@@ -947,6 +958,7 @@ public class UserServiceImpl extends AbstractService implements UserService {
         userEntity.setCreatedAt(user.getCreatedAt());
         userEntity.setUpdatedAt(user.getUpdatedAt());
         userEntity.setLastConnectionAt(user.getLastConnectionAt());
+        userEntity.setFirstConnectionAt(user.getFirstConnectionAt());
         userEntity.setPicture(user.getPicture());
         if (user.getStatus() != null) {
             userEntity.setStatus(user.getStatus().name());
@@ -976,6 +988,7 @@ public class UserServiceImpl extends AbstractService implements UserService {
         }
 
         userEntity.setLoginCount(user.getLoginCount());
+        userEntity.setNewsletterSubscribed(user.getNewsletterSubscribed());
 
         return userEntity;
     }