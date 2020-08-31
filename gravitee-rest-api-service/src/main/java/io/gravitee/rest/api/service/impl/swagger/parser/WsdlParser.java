reconbug/gravitee-management-rest-api

feat(maganement): Create api from a WSDL
generate OpenAPI description based on WSDL file
generate Rest-To-Soap & XML-JSON policies if required by the user

fix gravitee-io/issues#322

gravitee-rest-api-service/src/main/java/io/gravitee/rest/api/service/impl/swagger/parser/WsdlParser.java
@@ -1,0 +1,47 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.rest.api.service.impl.swagger.parser;
+
+import io.gravitee.rest.api.spec.converter.wsdl.WsdlMapper;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.parser.core.models.AuthorizationValue;
+import io.swagger.v3.parser.util.RemoteUrl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+/**
+ * @author Eric LELEU (eric.leleu at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+public class WsdlParser extends AbstractSwaggerParser<OpenAPI> {
+    private static final Logger LOGGER = LoggerFactory.getLogger(WsdlParser.class);
+
+    @Override
+    public OpenAPI parse(String content) {
+        try {
+            if (isLocationUrl(content)) {
+                return new WsdlMapper().toOpenAPI(RemoteUrl.urlToString(content, (List<AuthorizationValue>) null));
+            } else {
+                return new WsdlMapper().toOpenAPI(content);
+            }
+        } catch (Exception e) {
+            LOGGER.info("Wsdl parsing failed : {}", e.getMessage());
+            return null;
+        }
+    }
+}

gravitee-rest-api-spec-converter/src/main/java/io/gravitee/rest/api/spec/converter/wsdl/binding/Soap12Binding.java
@@ -1,0 +1,75 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.rest.api.spec.converter.wsdl.binding;
+
+import javax.wsdl.BindingOperation;
+import javax.wsdl.extensions.soap12.SOAP12Body;
+import javax.wsdl.extensions.soap12.SOAP12Header;
+import javax.xml.namespace.QName;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static io.gravitee.rest.api.spec.converter.wsdl.WsdlMapperUtils.extractAllElements;
+import static io.gravitee.rest.api.spec.converter.wsdl.WsdlMapperUtils.extractFirstElement;
+import static java.util.Collections.emptyList;
+
+public class Soap12Binding extends AbstractBinding {
+    public static final String SOAP12_ENVELOPE_NS = "http://www.w3.org/2003/05/soap-envelope";
+    public static final String SOAP12_ENVELOPE_PREFIX = "soapenv";
+
+    public Soap12Binding(boolean rpc) {
+        super(SOAP12_ENVELOPE_NS, SOAP12_ENVELOPE_PREFIX, rpc);
+    }
+
+    @Override
+    public boolean hasBodyElement(List<Object> elements) {
+        Optional<SOAP12Body> optBody12 = extractFirstElement(elements, SOAP12Body.class);
+        return optBody12.isPresent();
+    }
+
+    @Override
+    public BobyParts extractBodyParts(List<Object> elements) {
+        Optional<SOAP12Body> optBody12 = extractFirstElement(elements, SOAP12Body.class);
+        final Optional<BobyParts> optionalBobyParts =
+                optBody12.map(body -> new BobyParts(body.getParts() == null ? emptyList() : body.getParts(), body.getUse()));
+        return optionalBobyParts.orElse(null);
+    }
+
+    @Override
+    public boolean hasHeadersElement(List<Object> elements) {
+        Optional<SOAP12Header> optHeader12 = extractFirstElement(elements, SOAP12Header.class);
+        return optHeader12.isPresent();
+    }
+
+    @Override
+    public List<HeaderDef> extractHeaderParts(List<Object> elements) {
+        return extractAllElements(elements, SOAP12Header.class)
+                .map(header -> new HeaderDef(header.getMessage(), header.getUse(), header.getPart(), header.getRequired()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getRpcNamespace(BindingOperation bindingOperation) {
+        Optional<SOAP12Body> optional = extractFirstElement(bindingOperation.getBindingInput().getExtensibilityElements(), SOAP12Body.class);
+        return optional.get().getNamespaceURI();
+    }
+
+    @Override
+    public QName buildAttribute(String localPart) {
+        return new QName(SOAP12_ENVELOPE_NS, localPart, SOAP12_ENVELOPE_PREFIX);
+    }
+}

gravitee-rest-api-spec-converter/src/main/java/io/gravitee/rest/api/spec/converter/wsdl/binding/SoapBinding.java
@@ -1,0 +1,71 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.rest.api.spec.converter.wsdl.binding;
+
+import javax.wsdl.BindingOperation;
+import javax.wsdl.extensions.soap.SOAPBody;
+import javax.wsdl.extensions.soap.SOAPHeader;
+import javax.xml.namespace.QName;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static io.gravitee.rest.api.spec.converter.wsdl.WsdlMapperUtils.extractAllElements;
+import static io.gravitee.rest.api.spec.converter.wsdl.WsdlMapperUtils.extractFirstElement;
+
+public class SoapBinding extends AbstractBinding {
+
+    public static final String SOAP_ENVELOPE_NS = "http://schemas.xmlsoap.org/soap/envelope/";
+    public static final String SOAP_ENVELOPE_PREFIXS = "soapenv";
+
+    public SoapBinding(boolean rpc) {
+       super(SOAP_ENVELOPE_NS, SOAP_ENVELOPE_PREFIXS, rpc);
+    }
+
+    @Override
+    public boolean hasBodyElement(List<Object> elements) {
+        Optional<SOAPBody> optBody = extractFirstElement(elements, SOAPBody.class);
+        return optBody.isPresent();
+    }
+    @Override
+    public BobyParts extractBodyParts(List<Object> elements) {
+        Optional<SOAPBody> optBody = extractFirstElement(elements, SOAPBody.class);
+        return new BobyParts(optBody.get().getParts() == null ? Collections.EMPTY_LIST : optBody.get().getParts(), optBody.get().getUse());
+    }
+    @Override
+    public boolean hasHeadersElement(List<Object> elements) {
+        Optional<SOAPHeader> optHeader = extractFirstElement(elements, SOAPHeader.class);
+        return optHeader.isPresent();
+    }
+    @Override
+    public List<HeaderDef> extractHeaderParts(List<Object> elements) {
+        return extractAllElements(elements, SOAPHeader.class)
+                .map(header -> new HeaderDef(header.getMessage(), header.getUse(), header.getPart(), header.getRequired()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getRpcNamespace(BindingOperation bindingOperation) {
+        Optional<SOAPBody> optional = extractFirstElement(bindingOperation.getBindingInput().getExtensibilityElements(), SOAPBody.class);
+        return optional.get().getNamespaceURI();
+    }
+
+    @Override
+    public QName buildAttribute(String localPart) {
+        return new QName(SOAP_ENVELOPE_NS, localPart, SOAP_ENVELOPE_PREFIXS);
+    }
+}

gravitee-rest-api-spec-converter/src/main/java/io/gravitee/rest/api/spec/converter/wsdl/soap/SoapHeadersBuilder.java
@@ -1,0 +1,82 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.rest.api.spec.converter.wsdl.soap;
+
+import io.gravitee.rest.api.spec.converter.wsdl.utils.SampleXmlUtil;
+import io.gravitee.rest.api.spec.converter.wsdl.binding.HeaderDef;
+import org.apache.xmlbeans.XmlCursor;
+
+import javax.wsdl.Definition;
+import javax.wsdl.Message;
+import javax.wsdl.Part;
+import javax.xml.namespace.QName;
+import java.util.List;
+
+import static io.gravitee.rest.api.spec.converter.wsdl.WsdlMapperUtils.formatQName;
+
+/**
+ * @author Eric LELEU (eric.leleu at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+public class SoapHeadersBuilder extends AbstractSoapBuilder {
+    private Definition wsdlDef;
+
+    public SoapHeadersBuilder withWsdlDef(Definition wsdlDef) {
+        this.wsdlDef = wsdlDef;
+        return this;
+    }
+
+    @Override
+    public XmlCursor build() {
+        if (version.hasHeadersElement(bindingOperation.getBindingInput().getExtensibilityElements())) {
+            List<HeaderDef> headers = version.extractHeaderParts(bindingOperation.getBindingInput().getExtensibilityElements());
+            for (HeaderDef header : headers) {
+                generateHeader(header);
+            }
+        }
+        return xmlCursor;
+    }
+
+    private void generateHeader(HeaderDef header) {
+        Message msg = wsdlDef.getMessage(header.getMessage());
+        Part part = msg.getPart(header.getPart());
+
+        xmlCursor.beginElement(version.getHeaderQName());
+        if (header.useEncoded()) {
+            xmlCursor.insertAttributeWithValue(SampleXmlUtil.XSI_TYPE, formatQName(version.getHeaderQName()));
+        }
+
+        xmlCursor.toFirstChild();
+        xmlCursor.beginElement(new QName(part.getName()));
+
+        QName mustUnderstandQName = version.buildAttribute("mustUnderstand");
+        Object mustUnderstand = part.getExtensionAttribute(mustUnderstandQName);
+        if (mustUnderstand != null && mustUnderstand instanceof String) {
+            xmlCursor.insertAttributeWithValue(mustUnderstandQName, (String)mustUnderstand);
+        }
+
+        QName actorQName = version.buildAttribute("actor");
+        Object actor = part.getExtensionAttribute(actorQName);
+        if (actor != null && actor instanceof String) {
+            xmlCursor.insertAttributeWithValue(actorQName, (String)actor);
+        }
+
+        generateXml(part, xmlCursor, header.useEncoded());
+        xmlCursor.toParent();
+        xmlCursor.toParent();
+    }
+
+}

gravitee-io/gravitee-api-management

gravitee-rest-api-service/src/main/java/io/gravitee/rest/api/service/impl/swagger/parser/WsdlParser.java
@@ -1,0 +1,47 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.rest.api.service.impl.swagger.parser;
+
+import io.gravitee.rest.api.spec.converter.wsdl.WSDLToOpenAPIConverter;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.parser.core.models.AuthorizationValue;
+import io.swagger.v3.parser.util.RemoteUrl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+/**
+ * @author Eric LELEU (eric.leleu at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+public class WsdlParser extends AbstractSwaggerParser<OpenAPI> {
+    private static final Logger LOGGER = LoggerFactory.getLogger(WsdlParser.class);
+
+    @Override
+    public OpenAPI parse(String content) {
+        try {
+            if (isLocationUrl(content)) {
+                return new WSDLToOpenAPIConverter().toOpenAPI(RemoteUrl.urlToString(content, (List<AuthorizationValue>) null));
+            } else {
+                return new WSDLToOpenAPIConverter().toOpenAPI(content);
+            }
+        } catch (Exception e) {
+            LOGGER.info("Wsdl parsing failed : {}", e.getMessage());
+            return null;
+        }
+    }
+}

gravitee-rest-api-spec-converter/src/main/java/io/gravitee/rest/api/spec/converter/wsdl/binding/Soap12Binding.java
@@ -1,0 +1,75 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.rest.api.spec.converter.wsdl.binding;
+
+import javax.wsdl.BindingOperation;
+import javax.wsdl.extensions.soap12.SOAP12Body;
+import javax.wsdl.extensions.soap12.SOAP12Header;
+import javax.xml.namespace.QName;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static io.gravitee.rest.api.spec.converter.wsdl.WSDLUtils.extractAllElements;
+import static io.gravitee.rest.api.spec.converter.wsdl.WSDLUtils.extractFirstElement;
+import static java.util.Collections.emptyList;
+
+public class Soap12Binding extends AbstractBinding {
+    public static final String SOAP12_ENVELOPE_NS = "http://www.w3.org/2003/05/soap-envelope";
+    public static final String SOAP12_ENVELOPE_PREFIX = "soapenv";
+
+    public Soap12Binding(boolean rpc) {
+        super(SOAP12_ENVELOPE_NS, SOAP12_ENVELOPE_PREFIX, rpc);
+    }
+
+    @Override
+    public boolean hasBodyElement(List<Object> elements) {
+        Optional<SOAP12Body> optBody12 = extractFirstElement(elements, SOAP12Body.class);
+        return optBody12.isPresent();
+    }
+
+    @Override
+    public BobyParts extractBodyParts(List<Object> elements) {
+        Optional<SOAP12Body> optBody12 = extractFirstElement(elements, SOAP12Body.class);
+        final Optional<BobyParts> optionalBobyParts =
+                optBody12.map(body -> new BobyParts(body.getParts() == null ? emptyList() : body.getParts(), body.getUse()));
+        return optionalBobyParts.orElse(null);
+    }
+
+    @Override
+    public boolean hasHeadersElement(List<Object> elements) {
+        Optional<SOAP12Header> optHeader12 = extractFirstElement(elements, SOAP12Header.class);
+        return optHeader12.isPresent();
+    }
+
+    @Override
+    public List<HeaderDef> extractHeaderParts(List<Object> elements) {
+        return extractAllElements(elements, SOAP12Header.class)
+                .map(header -> new HeaderDef(header.getMessage(), header.getUse(), header.getPart(), header.getRequired()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getRpcNamespace(BindingOperation bindingOperation) {
+        Optional<SOAP12Body> optional = extractFirstElement(bindingOperation.getBindingInput().getExtensibilityElements(), SOAP12Body.class);
+        return optional.get().getNamespaceURI();
+    }
+
+    @Override
+    public QName buildAttribute(String localPart) {
+        return new QName(SOAP12_ENVELOPE_NS, localPart, SOAP12_ENVELOPE_PREFIX);
+    }
+}

gravitee-rest-api-spec-converter/src/main/java/io/gravitee/rest/api/spec/converter/wsdl/binding/SoapBinding.java
@@ -1,0 +1,71 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.rest.api.spec.converter.wsdl.binding;
+
+import javax.wsdl.BindingOperation;
+import javax.wsdl.extensions.soap.SOAPBody;
+import javax.wsdl.extensions.soap.SOAPHeader;
+import javax.xml.namespace.QName;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static io.gravitee.rest.api.spec.converter.wsdl.WSDLUtils.extractAllElements;
+import static io.gravitee.rest.api.spec.converter.wsdl.WSDLUtils.extractFirstElement;
+
+public class SoapBinding extends AbstractBinding {
+
+    public static final String SOAP_ENVELOPE_NS = "http://schemas.xmlsoap.org/soap/envelope/";
+    public static final String SOAP_ENVELOPE_PREFIXS = "soapenv";
+
+    public SoapBinding(boolean rpc) {
+       super(SOAP_ENVELOPE_NS, SOAP_ENVELOPE_PREFIXS, rpc);
+    }
+
+    @Override
+    public boolean hasBodyElement(List<Object> elements) {
+        Optional<SOAPBody> optBody = extractFirstElement(elements, SOAPBody.class);
+        return optBody.isPresent();
+    }
+    @Override
+    public BobyParts extractBodyParts(List<Object> elements) {
+        Optional<SOAPBody> optBody = extractFirstElement(elements, SOAPBody.class);
+        return new BobyParts(optBody.get().getParts() == null ? Collections.EMPTY_LIST : optBody.get().getParts(), optBody.get().getUse());
+    }
+    @Override
+    public boolean hasHeadersElement(List<Object> elements) {
+        Optional<SOAPHeader> optHeader = extractFirstElement(elements, SOAPHeader.class);
+        return optHeader.isPresent();
+    }
+    @Override
+    public List<HeaderDef> extractHeaderParts(List<Object> elements) {
+        return extractAllElements(elements, SOAPHeader.class)
+                .map(header -> new HeaderDef(header.getMessage(), header.getUse(), header.getPart(), header.getRequired()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getRpcNamespace(BindingOperation bindingOperation) {
+        Optional<SOAPBody> optional = extractFirstElement(bindingOperation.getBindingInput().getExtensibilityElements(), SOAPBody.class);
+        return optional.get().getNamespaceURI();
+    }
+
+    @Override
+    public QName buildAttribute(String localPart) {
+        return new QName(SOAP_ENVELOPE_NS, localPart, SOAP_ENVELOPE_PREFIXS);
+    }
+}

gravitee-rest-api-spec-converter/src/main/java/io/gravitee/rest/api/spec/converter/wsdl/soap/SoapHeadersBuilder.java
@@ -1,0 +1,82 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.rest.api.spec.converter.wsdl.soap;
+
+import io.gravitee.rest.api.spec.converter.wsdl.utils.SampleXmlUtil;
+import io.gravitee.rest.api.spec.converter.wsdl.binding.HeaderDef;
+import org.apache.xmlbeans.XmlCursor;
+
+import javax.wsdl.Definition;
+import javax.wsdl.Message;
+import javax.wsdl.Part;
+import javax.xml.namespace.QName;
+import java.util.List;
+
+import static io.gravitee.rest.api.spec.converter.wsdl.WSDLUtils.formatQName;
+
+/**
+ * @author Eric LELEU (eric.leleu at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+public class SoapHeadersBuilder extends AbstractSoapBuilder {
+    private Definition wsdlDef;
+
+    public SoapHeadersBuilder withWsdlDef(Definition wsdlDef) {
+        this.wsdlDef = wsdlDef;
+        return this;
+    }
+
+    @Override
+    public XmlCursor build() {
+        if (version.hasHeadersElement(bindingOperation.getBindingInput().getExtensibilityElements())) {
+            List<HeaderDef> headers = version.extractHeaderParts(bindingOperation.getBindingInput().getExtensibilityElements());
+            for (HeaderDef header : headers) {
+                generateHeader(header);
+            }
+        }
+        return xmlCursor;
+    }
+
+    private void generateHeader(HeaderDef header) {
+        Message msg = wsdlDef.getMessage(header.getMessage());
+        Part part = msg.getPart(header.getPart());
+
+        xmlCursor.beginElement(version.getHeaderQName());
+        if (header.useEncoded()) {
+            xmlCursor.insertAttributeWithValue(SampleXmlUtil.XSI_TYPE, formatQName(version.getHeaderQName()));
+        }
+
+        xmlCursor.toFirstChild();
+        xmlCursor.beginElement(new QName(part.getName()));
+
+        QName mustUnderstandQName = version.buildAttribute("mustUnderstand");
+        Object mustUnderstand = part.getExtensionAttribute(mustUnderstandQName);
+        if (mustUnderstand != null && mustUnderstand instanceof String) {
+            xmlCursor.insertAttributeWithValue(mustUnderstandQName, (String)mustUnderstand);
+        }
+
+        QName actorQName = version.buildAttribute("actor");
+        Object actor = part.getExtensionAttribute(actorQName);
+        if (actor != null && actor instanceof String) {
+            xmlCursor.insertAttributeWithValue(actorQName, (String)actor);
+        }
+
+        generateXml(part, xmlCursor, header.useEncoded());
+        xmlCursor.toParent();
+        xmlCursor.toParent();
+    }
+
+}