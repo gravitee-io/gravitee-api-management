c180dc3d9203c7fa04e9970c2b4d9dc5f13453b9
eric <eric.leleu.dev@gmail.com>
2020-09-16 18:01:34 +0200

    fix(management): fix slf4j template
    fix gravitee-io/issues#4197

--- gravitee-management-api-service/src/main/java/io/gravitee/management/service/impl/PageServiceImpl.java
+++ gravitee-management-api-service/src/main/java/io/gravitee/management/service/impl/PageServiceImpl.java
@@ -174,7 +174,7 @@ public class PageServiceImpl extends TransactionalService implements PageService
                 descriptor = swaggerService.parse(pageEntity.getContent());
             } catch (SwaggerDescriptorException sde) {
                 if (apiId != null) {
-                    logger.error("Parsing error for API : %s", apiId);
+                    logger.error("Parsing error for API : {}", apiId);
                 }
                 throw sde;
             }
