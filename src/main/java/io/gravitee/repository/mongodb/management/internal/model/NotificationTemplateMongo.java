1995b9d4acf5e5c9d61c2ceb8c8c824aa25e9f2c
Florent CHAMFROY <florent.chamfroy@graviteesource.com>
2020-10-28 13:58:23 +0100

    feat(notification): manage notif template in database
    + upgrade gravitee dependancies
    
    Closes gravitee-io/issues#4297

---
+++ src/main/java/io/gravitee/repository/mongodb/management/internal/model/NotificationTemplateMongo.java
@@ -0,0 +1,153 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.repository.mongodb.management.internal.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+/**
+ * @author Florent CHAMFROY (florent.chamfroy at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+@Document(collection = "notificationTemplates")
+public class NotificationTemplateMongo extends Auditable {
+
+	@Id
+	private String id;
+
+	private String hook;
+
+	private String scope;
+
+	private String referenceId;
+
+	private String referenceType;
+
+	private String name;
+
+	private String description;
+
+	private String title;
+
+	private String content;
+
+	private String type;
+
+	private boolean enabled;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getHook() {
+		return hook;
+	}
+
+	public void setHook(String hook) {
+		this.hook = hook;
+	}
+
+	public String getScope() {
+		return scope;
+	}
+
+	public void setScope(String scope) {
+		this.scope = scope;
+	}
+
+	public String getReferenceId() {
+		return referenceId;
+	}
+
+	public void setReferenceId(String referenceId) {
+		this.referenceId = referenceId;
+	}
+
+	public String getReferenceType() {
+		return referenceType;
+	}
+
+	public void setReferenceType(String referenceType) {
+		this.referenceType = referenceType;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+
+		NotificationTemplateMongo that = (NotificationTemplateMongo) o;
+
+		return id.equals(that.id);
+	}
+
+	@Override
+	public int hashCode() {
+		return id.hashCode();
+	}
+}
---
+++ src/main/java/io/gravitee/repository/mongodb/management/internal/notification/NotificationTemplateMongoRepository.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.repository.mongodb.management.internal.notification;
+
+import io.gravitee.repository.mongodb.management.internal.model.NotificationTemplateMongo;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * @author Florent CHAMFROY (florent.chamfroy at graviteesource.com) 
+ * @author GraviteeSource Team
+ */
+@Repository
+public interface NotificationTemplateMongoRepository extends MongoRepository<NotificationTemplateMongo, String> {
+    @Query("{ 'hook': ?0, 'scope': ?1, 'referenceId': ?2, 'referenceType': ?3 }")
+    List<NotificationTemplateMongo> findByHookAndScopeAndReferenceIdAndReferenceType(String hook, String scope, String referenceId, String referenceType);
+
+    @Query("{ 'referenceId': ?0, 'referenceType': ?1 }")
+    List<NotificationTemplateMongo> findByReferenceIdAndReferenceType(String referenceId, String referenceType);
+
+    @Query("{ type: ?0, 'referenceId': ?1, 'referenceType': ?2 }")
+    List<NotificationTemplateMongo> findByType(String type, String referenceId, String referenceType);
+}
