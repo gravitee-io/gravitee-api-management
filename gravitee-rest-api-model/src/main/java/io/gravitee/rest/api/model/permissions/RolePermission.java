gravitee-io/gravitee-management-rest-api

feat(management): idp links with an env or an org are now stored
Closes gravitee-io/issues#3963

gravitee-rest-api-model/src/main/java/io/gravitee/rest/api/model/permissions/RolePermission.java
@@ -76,11 +76,13 @@ public enum RolePermission {
     ENVIRONMENT_API_HEADER(RoleScope.ENVIRONMENT, EnvironmentPermission.API_HEADER),
     ENVIRONMENT_CLIENT_REGISTRATION_PROVIDER(RoleScope.ENVIRONMENT, EnvironmentPermission.CLIENT_REGISTRATION_PROVIDER),
     ENVIRONMENT_THEME(RoleScope.ENVIRONMENT, EnvironmentPermission.THEME),
+    ENVIRONMENT_IDENTITY_PROVIDER_ACTIVATION(RoleScope.ENVIRONMENT, EnvironmentPermission.IDENTITY_PROVIDER_ACTIVATION),
 
     ORGANIZATION_USERS(RoleScope.ORGANIZATION, OrganizationPermission.USER),
     ORGANIZATION_ROLE(RoleScope.ORGANIZATION, OrganizationPermission.ROLE),
     ORGANIZATION_ENVIRONMENT(RoleScope.ORGANIZATION, OrganizationPermission.ENVIRONMENT),
     ORGANIZATION_IDENTITY_PROVIDER(RoleScope.ORGANIZATION, OrganizationPermission.IDENTITY_PROVIDER),
+    ORGANIZATION_IDENTITY_PROVIDER_ACTIVATION(RoleScope.ORGANIZATION, OrganizationPermission.IDENTITY_PROVIDER_ACTIVATION),
     ;
 
     RoleScope scope;

gravitee-io/gravitee-api-management

gravitee-rest-api-model/src/main/java/io/gravitee/rest/api/model/permissions/RolePermission.java
@@ -76,12 +76,14 @@ public enum RolePermission {
     ENVIRONMENT_API_HEADER(RoleScope.ENVIRONMENT, EnvironmentPermission.API_HEADER),
     ENVIRONMENT_CLIENT_REGISTRATION_PROVIDER(RoleScope.ENVIRONMENT, EnvironmentPermission.CLIENT_REGISTRATION_PROVIDER),
     ENVIRONMENT_THEME(RoleScope.ENVIRONMENT, EnvironmentPermission.THEME),
+    ENVIRONMENT_IDENTITY_PROVIDER_ACTIVATION(RoleScope.ENVIRONMENT, EnvironmentPermission.IDENTITY_PROVIDER_ACTIVATION),
 
     ORGANIZATION_USERS(RoleScope.ORGANIZATION, OrganizationPermission.USER),
     ORGANIZATION_ROLE(RoleScope.ORGANIZATION, OrganizationPermission.ROLE),
     ORGANIZATION_ENVIRONMENT(RoleScope.ORGANIZATION, OrganizationPermission.ENVIRONMENT),
     ORGANIZATION_CUSTOM_USER_FIELDS(RoleScope.ORGANIZATION, OrganizationPermission.CUSTOM_USER_FIELDS),
-    ORGANIZATION_IDENTITY_PROVIDER(RoleScope.ORGANIZATION, OrganizationPermission.IDENTITY_PROVIDER)
+    ORGANIZATION_IDENTITY_PROVIDER(RoleScope.ORGANIZATION, OrganizationPermission.IDENTITY_PROVIDER),
+    ORGANIZATION_IDENTITY_PROVIDER_ACTIVATION(RoleScope.ORGANIZATION, OrganizationPermission.IDENTITY_PROVIDER_ACTIVATION)
     ;
 
     RoleScope scope;