WongSir/gravitee-management-rest-api

feat: allows to define a custom api-key

gravitee-rest-api-management/gravitee-rest-api-management-rest/src/main/java/io/gravitee/rest/api/management/rest/resource/ApiKeysResource.java
@@ -22,6 +22,7 @@ import io.gravitee.rest.api.model.permissions.RolePermissionAction;
 import io.gravitee.rest.api.management.rest.security.Permission;
 import io.gravitee.rest.api.management.rest.security.Permissions;
 import io.gravitee.rest.api.service.ApiKeyService;
+import io.gravitee.rest.api.validator.CustomApiKey;
 import io.swagger.annotations.*;
 
 import javax.inject.Inject;
@@ -93,5 +94,27 @@ public class ApiKeysResource extends AbstractResource {
 
         ApiKeyEntity keyEntity = apiKeyService.update(apiKeyEntity);
         return Response.ok(keyEntity).build();
+    }
+
+    @GET
+    @Path("verify")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @ApiOperation(value = "Check if an API key is available",
+            notes = "User must have the API_SUBSCRIPTION:READ permission to use this service")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "API Key successfully checked", response = Boolean.class),
+            @ApiResponse(code = 400, message = "Bad API Key parameter"),
+            @ApiResponse(code = 500, message = "Internal server error")})
+    @Permissions({
+            @Permission(value = RolePermission.API_SUBSCRIPTION, acls = RolePermissionAction.READ)
+    })
+    public Response verifyApiKeyAvailability(
+            @ApiParam(name = "apiKey", required = true)
+            @CustomApiKey @NotNull @QueryParam("apiKey") String apiKey) {
+
+        return Response
+                .ok(!apiKeyService.exists(apiKey))
+                .build();
     }
 }

gravitee-rest-api-management/gravitee-rest-api-management-rest/src/test/java/io/gravitee/rest/api/management/rest/resource/ApiKeysResourceTest.java
@@ -1,0 +1,90 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.rest.api.management.rest.resource;
+
+import io.gravitee.common.http.HttpStatusCode;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.ws.rs.core.Response;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.when;
+
+/**
+ * @author Yann TAVERNIER (yann.tavernier at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+public class ApiKeysResourceTest extends AbstractResourceTest {
+
+    @Override
+    protected String contextPath() {
+        return "apis/my-api/keys/";
+    }
+
+    @Before
+    public void setUp() {
+        reset(apiKeyService);
+    }
+
+    @Test
+    public void shouldReturnApiKeyAvailability() {
+        when(apiKeyService.exists(anyString())).thenReturn(true);
+
+        Response response = envTarget("verify")
+                .queryParam("apiKey", "atLeast10CharsButLessThan64")
+                .request()
+                .get();
+
+        assertEquals(HttpStatusCode.OK_200, response.getStatus());
+        assertEquals(false, response.readEntity(Boolean.class));
+    }
+
+    @Test
+    public void shouldReturn500IfInternalError() {
+        when(apiKeyService.exists(anyString())).thenThrow(new InternalError());
+
+        Response response = envTarget("verify")
+                .queryParam("apiKey", "atLeast10CharsButLessThan64")
+                .request()
+                .get();
+
+        assertEquals(HttpStatusCode.INTERNAL_SERVER_ERROR_500, response.getStatus());
+    }
+
+    @Test
+    public void shouldReturn400IfBadParameter() {
+
+        Response response = envTarget("verify")
+                .queryParam("apiKey", "short/\\;")
+                .request()
+                .get();
+
+        assertEquals(HttpStatusCode.BAD_REQUEST_400, response.getStatus());
+    }
+
+    @Test
+    public void shouldReturn400IfNoApiKeyParam() {
+
+        Response response = envTarget("verify")
+                .request()
+                .get();
+
+        assertEquals(HttpStatusCode.BAD_REQUEST_400, response.getStatus());
+    }
+}

gravitee-io/gravitee-api-management

gravitee-rest-api-management/gravitee-rest-api-management-rest/src/main/java/io/gravitee/rest/api/management/rest/resource/ApiKeysResource.java
@@ -22,6 +22,7 @@ import io.gravitee.rest.api.model.ApiKeyEntity;
 import io.gravitee.rest.api.model.permissions.RolePermission;
 import io.gravitee.rest.api.model.permissions.RolePermissionAction;
 import io.gravitee.rest.api.service.ApiKeyService;
+import io.gravitee.rest.api.validator.CustomApiKey;
 import io.swagger.annotations.*;
 
 import javax.inject.Inject;
@@ -96,5 +97,27 @@ public class ApiKeysResource extends AbstractResource {
 
         ApiKeyEntity keyEntity = apiKeyService.update(apiKeyEntity);
         return Response.ok(keyEntity).build();
+    }
+
+    @POST
+    @Path("_verify")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @ApiOperation(value = "Check if an API key is available",
+            notes = "User must have the API_SUBSCRIPTION:READ permission to use this service")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "API Key successfully checked", response = Boolean.class),
+            @ApiResponse(code = 400, message = "Bad API Key parameter"),
+            @ApiResponse(code = 500, message = "Internal server error")})
+    @Permissions({
+            @Permission(value = RolePermission.API_SUBSCRIPTION, acls = RolePermissionAction.READ)
+    })
+    public Response verifyApiKeyAvailability(
+            @ApiParam(name = "apiKey", required = true)
+            @CustomApiKey @NotNull @QueryParam("apiKey") String apiKey) {
+
+        return Response
+                .ok(!apiKeyService.exists(apiKey))
+                .build();
     }
 }

gravitee-rest-api-management/gravitee-rest-api-management-rest/src/test/java/io/gravitee/rest/api/management/rest/resource/ApiKeysResourceTest.java
@@ -1,0 +1,90 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.rest.api.management.rest.resource;
+
+import io.gravitee.common.http.HttpStatusCode;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.ws.rs.core.Response;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.when;
+
+/**
+ * @author Yann TAVERNIER (yann.tavernier at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+public class ApiKeysResourceTest extends AbstractResourceTest {
+
+    @Override
+    protected String contextPath() {
+        return "apis/my-api/keys/";
+    }
+
+    @Before
+    public void setUp() {
+        reset(apiKeyService);
+    }
+
+    @Test
+    public void shouldReturnApiKeyAvailability() {
+        when(apiKeyService.exists(anyString())).thenReturn(true);
+
+        Response response = envTarget("_verify")
+                .queryParam("apiKey", "atLeast10CharsButLessThan64")
+                .request()
+                .post(null);
+
+        assertEquals(HttpStatusCode.OK_200, response.getStatus());
+        assertEquals(false, response.readEntity(Boolean.class));
+    }
+
+    @Test
+    public void shouldReturn500IfInternalError() {
+        when(apiKeyService.exists(anyString())).thenThrow(new InternalError());
+
+        Response response = envTarget("_verify")
+                .queryParam("apiKey", "atLeast10CharsButLessThan64")
+                .request()
+                .post(null);
+
+        assertEquals(HttpStatusCode.INTERNAL_SERVER_ERROR_500, response.getStatus());
+    }
+
+    @Test
+    public void shouldReturn400IfBadParameter() {
+
+        Response response = envTarget("_verify")
+                .queryParam("apiKey", "short/\\;")
+                .request()
+                .post(null);
+
+        assertEquals(HttpStatusCode.BAD_REQUEST_400, response.getStatus());
+    }
+
+    @Test
+    public void shouldReturn400IfNoApiKeyParam() {
+
+        Response response = envTarget("_verify")
+                .request()
+                .post(null);
+
+        assertEquals(HttpStatusCode.BAD_REQUEST_400, response.getStatus());
+    }
+}