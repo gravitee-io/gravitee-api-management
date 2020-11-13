WongSir/gravitee-management-rest-api

feat: support tickets history

gravitee-rest-api-service/src/main/java/io/gravitee/rest/api/service/impl/TicketServiceImpl.java
@@ -15,24 +15,38 @@
  */
 package io.gravitee.rest.api.service.impl;
 
+import io.gravitee.common.data.domain.Page;
+import io.gravitee.repository.exceptions.TechnicalException;
+import io.gravitee.repository.management.api.TicketRepository;
+import io.gravitee.repository.management.api.search.TicketCriteria;
+import io.gravitee.repository.management.api.search.builder.PageableBuilder;
+import io.gravitee.repository.management.model.Ticket;
 import io.gravitee.rest.api.model.*;
+import io.gravitee.rest.api.model.api.ApiEntity;
+import io.gravitee.rest.api.model.common.Pageable;
 import io.gravitee.rest.api.model.parameters.Key;
 import io.gravitee.rest.api.service.*;
 import io.gravitee.rest.api.service.builder.EmailNotificationBuilder;
+import io.gravitee.rest.api.service.common.RandomString;
 import io.gravitee.rest.api.service.exceptions.EmailRequiredException;
 import io.gravitee.rest.api.service.exceptions.SupportUnavailableException;
+import io.gravitee.rest.api.service.exceptions.TechnicalManagementException;
 import io.gravitee.rest.api.service.impl.upgrade.DefaultMetadataUpgrader;
 import io.gravitee.rest.api.service.notification.ApiHook;
 import io.gravitee.rest.api.service.notification.ApplicationHook;
 import io.gravitee.rest.api.service.notification.NotificationParamsBuilder;
 import io.gravitee.rest.api.service.notification.PortalHook;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import javax.inject.Inject;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import static io.gravitee.rest.api.service.builder.EmailNotificationBuilder.EmailTemplate.SUPPORT_TICKET;
 
@@ -59,70 +73,167 @@ public class TicketServiceImpl extends TransactionalService implements TicketSer
     private ParameterService parameterService;
     @Inject
     private NotifierService notifierService;
+    @Inject
+    private TicketRepository ticketRepository;
 
     private boolean isEnabled() {
         return parameterService.findAsBoolean(Key.PORTAL_SUPPORT_ENABLED);
     }
 
     @Override
-    public void create(final String userId, final NewTicketEntity ticketEntity) {
-        if (!isEnabled()) {
-            throw new SupportUnavailableException();
-        }
-        LOGGER.info("Creating a support ticket: {}", ticketEntity);
-
-        final Map<String, Object> parameters = new HashMap<>();
-
-        final UserEntity user = userService.findById(userId);
-        if (user.getEmail() == null) {
-            throw new EmailRequiredException(userId);
-        }
-        parameters.put("user", user);
-
-        final String emailTo;
-        final ApiModelEntity api;
-        final ApplicationEntity applicationEntity;
-        if (ticketEntity.getApi() == null || ticketEntity.getApi().isEmpty()) {
-            api = null;
-            final MetadataEntity emailMetadata = metadataService.findDefaultByKey(DefaultMetadataUpgrader.METADATA_EMAIL_SUPPORT_KEY);
-            if (emailMetadata == null) {
-                throw new IllegalStateException("The support email metadata has not been found");
+    public TicketEntity create(final String userId, final NewTicketEntity ticketEntity) {
+        try {
+            if (!isEnabled()) {
+                throw new SupportUnavailableException();
             }
-            emailTo = emailMetadata.getValue();
-        } else {
-            api = apiService.findByIdForTemplates(ticketEntity.getApi(), true);
-            final String apiMetadataEmailSupport = api.getMetadata().get(DefaultMetadataUpgrader.METADATA_EMAIL_SUPPORT_KEY);
-            if (apiMetadataEmailSupport == null) {
-                throw new IllegalStateException("The support email API metadata has not been found");
+            LOGGER.info("Creating a support ticket: {}", ticketEntity);
+
+            final Map<String, Object> parameters = new HashMap<>();
+
+            final UserEntity user = userService.findById(userId);
+            if (user.getEmail() == null) {
+                throw new EmailRequiredException(userId);
             }
-            emailTo = apiMetadataEmailSupport;
-            parameters.put("api", api);
-        }
+            parameters.put("user", user);
 
-        if (DefaultMetadataUpgrader.DEFAULT_METADATA_EMAIL_SUPPORT.equals(emailTo)) {
-            throw new IllegalStateException("The support email API metadata has not been changed");
-        }
+            final String emailTo;
+            final ApiModelEntity api;
+            final ApplicationEntity applicationEntity;
+            if (ticketEntity.getApi() == null || ticketEntity.getApi().isEmpty()) {
+                api = null;
+                final MetadataEntity emailMetadata = metadataService.findDefaultByKey(DefaultMetadataUpgrader.METADATA_EMAIL_SUPPORT_KEY);
+                if (emailMetadata == null) {
+                    throw new IllegalStateException("The support email metadata has not been found");
+                }
+                emailTo = emailMetadata.getValue();
+            } else {
+                api = apiService.findByIdForTemplates(ticketEntity.getApi(), true);
+                final String apiMetadataEmailSupport = api.getMetadata().get(DefaultMetadataUpgrader.METADATA_EMAIL_SUPPORT_KEY);
+                if (apiMetadataEmailSupport == null) {
+                    throw new IllegalStateException("The support email API metadata has not been found");
+                }
+                emailTo = apiMetadataEmailSupport;
+                parameters.put("api", api);
+            }
 
-        if (ticketEntity.getApplication() != null && !ticketEntity.getApplication().isEmpty()) {
-            applicationEntity = applicationService.findById(ticketEntity.getApplication());
-            parameters.put("application", applicationEntity);
-        } else {
-            applicationEntity = null;
-        }
+            if (DefaultMetadataUpgrader.DEFAULT_METADATA_EMAIL_SUPPORT.equals(emailTo)) {
+                throw new IllegalStateException("The support email API metadata has not been changed");
+            }
 
-        parameters.put("content", ticketEntity.getContent().replaceAll("(\r\n|\n)", "<br />"));
-        final String fromName = user.getFirstname() == null ? user.getEmail() : user.getFirstname() + ' ' + user.getLastname();
-        emailService.sendEmailNotification(
-                new EmailNotificationBuilder()
-                        .replyTo(user.getEmail())
-                        .fromName(fromName)
-                        .to(emailTo)
-                        .subject(ticketEntity.getSubject())
-                        .copyToSender(ticketEntity.isCopyToSender())
-                        .template(SUPPORT_TICKET)
-                        .params(parameters)
-                        .build());
-        sendUserNotification(user, api, applicationEntity);
+            if (ticketEntity.getApplication() != null && !ticketEntity.getApplication().isEmpty()) {
+                applicationEntity = applicationService.findById(ticketEntity.getApplication());
+                parameters.put("application", applicationEntity);
+            } else {
+                applicationEntity = null;
+            }
+
+            parameters.put("content", ticketEntity.getContent().replaceAll("(\r\n|\n)", "<br />"));
+            final String fromName = user.getFirstname() == null ? user.getEmail() : user.getFirstname() + ' ' + user.getLastname();
+            emailService.sendEmailNotification(
+                    new EmailNotificationBuilder()
+                            .replyTo(user.getEmail())
+                            .fromName(fromName)
+                            .to(emailTo)
+                            .subject(ticketEntity.getSubject())
+                            .copyToSender(ticketEntity.isCopyToSender())
+                            .template(SUPPORT_TICKET)
+                            .params(parameters)
+                            .build());
+            sendUserNotification(user, api, applicationEntity);
+
+            Ticket ticket = convert(ticketEntity);
+            ticket.setId(RandomString.generate());
+            ticket.setCreatedAt(new Date());
+            ticket.setFromUser(userId);
+
+            Ticket createdTicket = ticketRepository.create(ticket);
+
+            return convert(createdTicket);
+
+        } catch (TechnicalException ex) {
+            LOGGER.error("An error occurs while trying to create a ticket {}", ticketEntity, ex);
+            throw new TechnicalManagementException("An error occurs while trying to create a ticket " + ticketEntity, ex);
+        }
+    }
+
+    @Override
+    public Page<TicketEntity> search(TicketCriteria criteria, Pageable pageable) {
+        try {
+            LOGGER.debug("search tickets");
+
+            Page<Ticket> tickets = ticketRepository.search(
+                    criteria,
+                    new PageableBuilder()
+                            .pageNumber(pageable.getPageNumber() - 1)
+                            .pageSize(pageable.getPageSize())
+                            .build()
+            );
+
+            List<TicketEntity> entities = tickets.getContent()
+                    .stream()
+                    .map(t -> {
+                        final Ticket ticket = new Ticket(t);
+
+                        //Retrieve application name
+                        if (StringUtils.isNotEmpty(t.getApplication())) {
+                            ApplicationEntity application = applicationService.findById(t.getApplication());
+                            ticket.setApplication(application.getName());
+                        }
+
+                        //Retrieve api name
+                        if (StringUtils.isNotEmpty(t.getApi())) {
+                            ApiEntity api = apiService.findById(t.getApi());
+                            ticket.setApi(api.getName());
+                        }
+
+                        return ticket;
+                    })
+                    .map(this::convert)
+                    .collect(Collectors.toList());
+
+
+            LOGGER.debug("search tickets - Done with {} elements", entities.size());
+
+            return new Page<>(entities,
+                    tickets.getPageNumber() + 1,
+                    (int) tickets.getPageElements(),
+                    tickets.getTotalElements()
+            );
+        } catch (TechnicalException ex) {
+            LOGGER.error("An error occurs while trying to search tickets", ex);
+            throw new TechnicalManagementException("An error occurs while trying to search tickets", ex);
+        }
+    }
+
+    @Override
+    public TicketEntity findById(String ticketId) {
+        try {
+            return ticketRepository
+                    .findById(ticketId)
+                    .map(t -> {
+                        Ticket ticket = new Ticket(t);
+
+                        //Retrieve application name
+                        if (StringUtils.isNotEmpty(t.getApplication())) {
+                            ApplicationEntity application = applicationService.findById(t.getApplication());
+                            ticket.setApplication(application.getName());
+                        }
+
+                        //Retrieve api name
+                        if (StringUtils.isNotEmpty(t.getApi())) {
+                            ApiEntity api = apiService.findById(t.getApi());
+                            ticket.setApi(api.getName());
+                        }
+
+                        return ticket;
+                    })
+                    .map(this::convert)
+                    .orElse(null);
+
+        }  catch (TechnicalException ex) {
+            LOGGER.error("An error occurs while trying to search ticket {}", ticketId, ex);
+            throw new TechnicalManagementException("An error occurs while trying to search ticket " + ticketId , ex);
+        }
     }
 
     private void sendUserNotification(final UserEntity user, final ApiModelEntity api, final ApplicationEntity application) {
@@ -147,5 +258,36 @@ public class TicketServiceImpl extends TransactionalService implements TicketSer
                     .application(application)
                     .build());
         }
+    }
+
+    private TicketEntity convert(Ticket ticket) {
+        if (ticket == null) {
+            return null;
+        }
+
+        TicketEntity ticketEntity = new TicketEntity();
+        ticketEntity.setId(ticket.getId());
+        ticketEntity.setApi(ticket.getApi());
+        ticketEntity.setApplication(ticket.getApplication());
+        ticketEntity.setSubject(ticket.getSubject());
+        ticketEntity.setContent(ticket.getContent());
+        ticketEntity.setCreatedAt(ticket.getCreatedAt());
+        ticketEntity.setFromUser(ticket.getFromUser());
+
+        return ticketEntity;
+    }
+
+    private Ticket convert(NewTicketEntity ticketEntity) {
+        if (ticketEntity == null) {
+            return null;
+        }
+
+        Ticket ticket = new Ticket();
+        ticket.setApi(ticketEntity.getApi());
+        ticket.setApplication(ticketEntity.getApplication());
+        ticket.setSubject(ticketEntity.getSubject());
+        ticket.setContent(ticketEntity.getContent());
+
+        return ticket;
     }
 }

gravitee-io/gravitee-api-management

gravitee-rest-api-service/src/main/java/io/gravitee/rest/api/service/impl/TicketServiceImpl.java
@@ -15,26 +15,46 @@
  */
 package io.gravitee.rest.api.service.impl;
 
+import io.gravitee.common.data.domain.Page;
+import io.gravitee.repository.exceptions.TechnicalException;
+import io.gravitee.repository.management.api.TicketRepository;
+import io.gravitee.repository.management.api.search.Order;
+import io.gravitee.repository.management.api.search.TicketCriteria;
+import io.gravitee.repository.management.api.search.builder.PageableBuilder;
+import io.gravitee.repository.management.api.search.builder.SortableBuilder;
+import io.gravitee.repository.management.model.Ticket;
 import io.gravitee.rest.api.model.*;
+import io.gravitee.rest.api.model.api.ApiEntity;
+import io.gravitee.rest.api.model.api.TicketQuery;
+import io.gravitee.rest.api.model.common.Pageable;
+import io.gravitee.rest.api.model.common.Sortable;
 import io.gravitee.rest.api.model.parameters.Key;
 import io.gravitee.rest.api.service.*;
 import io.gravitee.rest.api.service.builder.EmailNotificationBuilder;
+import io.gravitee.rest.api.service.common.RandomString;
 import io.gravitee.rest.api.service.exceptions.EmailRequiredException;
 import io.gravitee.rest.api.service.exceptions.SupportUnavailableException;
+import io.gravitee.rest.api.service.exceptions.TechnicalManagementException;
+import io.gravitee.rest.api.service.exceptions.TicketNotFoundException;
 import io.gravitee.rest.api.service.impl.upgrade.DefaultMetadataUpgrader;
 import io.gravitee.rest.api.service.notification.ApiHook;
 import io.gravitee.rest.api.service.notification.ApplicationHook;
 import io.gravitee.rest.api.service.notification.NotificationParamsBuilder;
 import io.gravitee.rest.api.service.notification.PortalHook;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import javax.inject.Inject;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import static io.gravitee.rest.api.service.builder.EmailNotificationBuilder.EmailTemplate.TEMPLATES_FOR_ACTION_SUPPORT_TICKET;
+import static org.apache.commons.lang3.StringUtils.isBlank;
 
 /**
  * @author Azize ELAMRANI (azize at graviteesource.com)
@@ -59,70 +79,138 @@ public class TicketServiceImpl extends TransactionalService implements TicketSer
     private ParameterService parameterService;
     @Inject
     private NotifierService notifierService;
+    @Inject
+    private TicketRepository ticketRepository;
 
     private boolean isEnabled() {
         return parameterService.findAsBoolean(Key.PORTAL_SUPPORT_ENABLED);
     }
 
     @Override
-    public void create(final String userId, final NewTicketEntity ticketEntity) {
-        if (!isEnabled()) {
-            throw new SupportUnavailableException();
-        }
-        LOGGER.info("Creating a support ticket: {}", ticketEntity);
-
-        final Map<String, Object> parameters = new HashMap<>();
-
-        final UserEntity user = userService.findById(userId);
-        if (user.getEmail() == null) {
-            throw new EmailRequiredException(userId);
-        }
-        parameters.put("user", user);
-
-        final String emailTo;
-        final ApiModelEntity api;
-        final ApplicationEntity applicationEntity;
-        if (ticketEntity.getApi() == null || ticketEntity.getApi().isEmpty()) {
-            api = null;
-            final MetadataEntity emailMetadata = metadataService.findDefaultByKey(DefaultMetadataUpgrader.METADATA_EMAIL_SUPPORT_KEY);
-            if (emailMetadata == null) {
-                throw new IllegalStateException("The support email metadata has not been found");
+    public TicketEntity create(final String userId, final NewTicketEntity ticketEntity) {
+        try {
+            if (!isEnabled()) {
+                throw new SupportUnavailableException();
             }
-            emailTo = emailMetadata.getValue();
-        } else {
-            api = apiService.findByIdForTemplates(ticketEntity.getApi(), true);
-            final String apiMetadataEmailSupport = api.getMetadata().get(DefaultMetadataUpgrader.METADATA_EMAIL_SUPPORT_KEY);
-            if (apiMetadataEmailSupport == null) {
-                throw new IllegalStateException("The support email API metadata has not been found");
+            LOGGER.info("Creating a support ticket: {}", ticketEntity);
+
+            final Map<String, Object> parameters = new HashMap<>();
+
+            final UserEntity user = userService.findById(userId);
+            if (user.getEmail() == null) {
+                throw new EmailRequiredException(userId);
             }
-            emailTo = apiMetadataEmailSupport;
-            parameters.put("api", api);
-        }
+            parameters.put("user", user);
 
-        if (DefaultMetadataUpgrader.DEFAULT_METADATA_EMAIL_SUPPORT.equals(emailTo)) {
-            throw new IllegalStateException("The support email API metadata has not been changed");
-        }
+            final String emailTo;
+            final ApiModelEntity api;
+            final ApplicationEntity applicationEntity;
+            if (ticketEntity.getApi() == null || ticketEntity.getApi().isEmpty()) {
+                api = null;
+                final MetadataEntity emailMetadata = metadataService.findDefaultByKey(DefaultMetadataUpgrader.METADATA_EMAIL_SUPPORT_KEY);
+                if (emailMetadata == null) {
+                    throw new IllegalStateException("The support email metadata has not been found");
+                }
+                emailTo = emailMetadata.getValue();
+            } else {
+                api = apiService.findByIdForTemplates(ticketEntity.getApi(), true);
+                final String apiMetadataEmailSupport = api.getMetadata().get(DefaultMetadataUpgrader.METADATA_EMAIL_SUPPORT_KEY);
+                if (apiMetadataEmailSupport == null) {
+                    throw new IllegalStateException("The support email API metadata has not been found");
+                }
+                emailTo = apiMetadataEmailSupport;
+                parameters.put("api", api);
+            }
 
-        if (ticketEntity.getApplication() != null && !ticketEntity.getApplication().isEmpty()) {
-            applicationEntity = applicationService.findById(ticketEntity.getApplication());
-            parameters.put("application", applicationEntity);
-        } else {
-            applicationEntity = null;
-        }
+            if (DefaultMetadataUpgrader.DEFAULT_METADATA_EMAIL_SUPPORT.equals(emailTo)) {
+                throw new IllegalStateException("The support email API metadata has not been changed");
+            }
 
-        parameters.put("content", ticketEntity.getContent().replaceAll("(\r\n|\n)", "<br />"));
-        parameters.put("ticketSubject", ticketEntity.getSubject());
-        final String fromName = user.getFirstname() == null ? user.getEmail() : user.getFirstname() + ' ' + user.getLastname();
-        emailService.sendEmailNotification(
-                new EmailNotificationBuilder()
-                        .replyTo(user.getEmail())
-                        .fromName(fromName)
-                        .to(emailTo)
-                        .copyToSender(ticketEntity.isCopyToSender())
-                        .template(TEMPLATES_FOR_ACTION_SUPPORT_TICKET)
-                        .params(parameters)
-                        .build());
-        sendUserNotification(user, api, applicationEntity);
+            if (ticketEntity.getApplication() != null && !ticketEntity.getApplication().isEmpty()) {
+                applicationEntity = applicationService.findById(ticketEntity.getApplication());
+                parameters.put("application", applicationEntity);
+            } else {
+                applicationEntity = null;
+            }
+
+            parameters.put("content", ticketEntity.getContent().replaceAll("(\r\n|\n)", "<br />"));
+            parameters.put("ticketSubject", ticketEntity.getSubject());final String fromName = user.getFirstname() == null ? user.getEmail() : user.getFirstname() + ' ' + user.getLastname();
+            emailService.sendEmailNotification(
+                    new EmailNotificationBuilder()
+                            .replyTo(user.getEmail())
+                            .fromName(fromName)
+                            .to(emailTo)
+                            .copyToSender(ticketEntity.isCopyToSender())
+                            .template(TEMPLATES_FOR_ACTION_SUPPORT_TICKET)
+                            .params(parameters)
+                            .build());
+            sendUserNotification(user, api, applicationEntity);
+
+            Ticket ticket = convert(ticketEntity);
+            ticket.setId(RandomString.generate());
+            ticket.setCreatedAt(new Date());
+            ticket.setFromUser(userId);
+
+            Ticket createdTicket = ticketRepository.create(ticket);
+
+            return convert(createdTicket);
+
+        } catch (TechnicalException ex) {
+            LOGGER.error("An error occurs while trying to create a ticket {}", ticketEntity, ex);
+            throw new TechnicalManagementException("An error occurs while trying to create a ticket " + ticketEntity, ex);
+        }
+    }
+
+    @Override
+    public Page<TicketEntity> search(TicketQuery query, Sortable sortable, Pageable pageable) {
+        try {
+            LOGGER.debug("search tickets");
+
+            TicketCriteria criteria = queryToCriteriaBuilder(query).build();
+
+
+            Page<Ticket> tickets = ticketRepository.search(
+                    criteria,
+                    buildSortable(sortable),
+                    new PageableBuilder()
+                            .pageNumber(pageable.getPageNumber() - 1)
+                            .pageSize(pageable.getPageSize())
+                            .build()
+            );
+
+            List<TicketEntity> entities = tickets.getContent()
+                    .stream()
+                    .map(this::getApiNameAndApplicationName)
+                    .map(this::convert)
+                    .collect(Collectors.toList());
+
+
+            LOGGER.debug("search tickets - Done with {} elements", entities.size());
+
+            return new Page<>(entities,
+                    tickets.getPageNumber() + 1,
+                    (int) tickets.getPageElements(),
+                    tickets.getTotalElements()
+            );
+        } catch (TechnicalException ex) {
+            LOGGER.error("An error occurs while trying to search tickets", ex);
+            throw new TechnicalManagementException("An error occurs while trying to search tickets", ex);
+        }
+    }
+
+    @Override
+    public TicketEntity findById(String ticketId) {
+        try {
+            return ticketRepository
+                    .findById(ticketId)
+                    .map(this::getApiNameAndApplicationName)
+                    .map(this::convert)
+                    .orElseThrow(() -> new TicketNotFoundException(ticketId));
+
+        }  catch (TechnicalException ex) {
+            LOGGER.error("An error occurs while trying to search ticket {}", ticketId, ex);
+            throw new TechnicalManagementException("An error occurs while trying to search ticket " + ticketId , ex);
+        }
     }
 
     private void sendUserNotification(final UserEntity user, final ApiModelEntity api, final ApplicationEntity application) {
@@ -147,5 +235,79 @@ public class TicketServiceImpl extends TransactionalService implements TicketSer
                     .application(application)
                     .build());
         }
+    }
+
+    private TicketEntity convert(Ticket ticket) {
+        if (ticket == null) {
+            return null;
+        }
+
+        TicketEntity ticketEntity = new TicketEntity();
+        ticketEntity.setId(ticket.getId());
+        ticketEntity.setApi(ticket.getApi());
+        ticketEntity.setApplication(ticket.getApplication());
+        ticketEntity.setSubject(ticket.getSubject());
+        ticketEntity.setContent(ticket.getContent());
+        ticketEntity.setCreatedAt(ticket.getCreatedAt());
+        ticketEntity.setFromUser(ticket.getFromUser());
+
+        return ticketEntity;
+    }
+
+    private Ticket convert(NewTicketEntity ticketEntity) {
+        if (ticketEntity == null) {
+            return null;
+        }
+
+        Ticket ticket = new Ticket();
+        ticket.setApi(ticketEntity.getApi());
+        ticket.setApplication(ticketEntity.getApplication());
+        ticket.setSubject(ticketEntity.getSubject());
+        ticket.setContent(ticketEntity.getContent());
+
+        return ticket;
+    }
+
+    private Ticket getApiNameAndApplicationName(Ticket ticket) {
+
+        //Retrieve application name
+        if (StringUtils.isNotEmpty(ticket.getApplication())) {
+            ApplicationEntity application = applicationService.findById(ticket.getApplication());
+            ticket.setApplication(application.getName());
+        }
+
+        //Retrieve api name
+        if (StringUtils.isNotEmpty(ticket.getApi())) {
+            ApiEntity api = apiService.findById(ticket.getApi());
+            ticket.setApi(api.getName());
+        }
+
+        return ticket;
+    }
+
+    private TicketCriteria.Builder queryToCriteriaBuilder(TicketQuery query) {
+        final TicketCriteria.Builder builder = new TicketCriteria.Builder().fromUser(query.getFromUser());
+        if (query == null) {
+            return builder;
+        }
+
+        if (!isBlank(query.getApi())) {
+            builder.api(query.getApi());
+        }
+        if (!isBlank(query.getApplication())) {
+            builder.application(query.getApplication());
+        }
+
+        return builder;
+    }
+
+    private io.gravitee.repository.management.api.search.Sortable buildSortable(Sortable sortable) {
+        if (sortable == null) {
+            return null;
+        }
+        return new SortableBuilder()
+                .field(sortable.getField())
+                .order(sortable.isAscOrder() ? Order.ASC : Order.DESC)
+                .build();
     }
 }