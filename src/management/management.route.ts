WongSir/gravitee-management-webui

feat: support tickets history

src/management/management.route.ts
@@ -21,8 +21,9 @@ import {User} from '../entities/user';
 import RoleService from '../services/role.service';
 import DashboardService from '../services/dashboard.service';
 import {StateParams} from '@uirouter/core';
-import TenantService from '../services/tenant.service';
 import AnalyticsService from '../services/analytics.service';
+import TicketsListController from './support/tickets-list.controller';
+import TicketService from '../services/ticket.service';
 
 function managementRouterConfig($stateProvider) {
   'ngInject';
@@ -263,11 +264,43 @@ function managementRouterConfig($stateProvider) {
       }
     })
     .state('management.support', {
+      template: '<div ui-view></div>',
+    })
+    .state('management.support.create', {
       url: '/support',
       template: require('./support/ticket.html'),
       controller: 'SupportTicketController',
       controllerAs: 'supportTicketCtrl'
-    });
+    })
+    .state('management.support.tickets', {
+      url: '/support/tickets?page&size&field',
+      template: require('./support/tickets-list.html'),
+      controller: TicketsListController,
+      controllerAs: 'ticketsListCtrl',
+      params: {
+        page: {
+          type: 'int',
+          dynamic: true
+        },
+        size: {
+          type: 'int',
+          dynamic: true
+        },
+        field: {
+          type: 'string',
+          dynamic: true
+        }
+      },
+    })
+    .state('management.support.ticket', {
+      url: '/support/tickets/:ticketId?page&size&field',
+      component: 'ticketDetail',
+      resolve: {
+        ticket: ($stateParams: StateParams, TicketService: TicketService) =>
+          TicketService.getTicket($stateParams.ticketId).then(response => response.data)
+      }
+    })
+  ;
 }
 
 export default managementRouterConfig;

src/management/support/ticket-detail.component.ts
@@ -1,0 +1,35 @@
+import {StateService} from '@uirouter/core';
+
+/*
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+const TicketDetailComponent: ng.IComponentOptions = {
+  template: require('./ticket-detail.html'),
+  bindings: {
+    ticket: '<'
+  },
+  controller: function($state: StateService) {
+    'ngInject';
+
+    this.backStateParams = {
+      page: $state.params.page,
+      size: $state.params.size,
+      field: $state.params.field
+    };
+  }
+
+};
+
+export default TicketDetailComponent;

src/management/support/tickets-list.controller.ts
@@ -1,0 +1,71 @@
+/*
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import TicketService, {TicketsQuery} from '../../services/ticket.service';
+import {StateService} from '@uirouter/core';
+import {IScope} from 'angular';
+
+class TicketsListController {
+
+  private query: TicketsQuery;
+  private tickets: {totalElements: number, content: any[]};
+
+  constructor(
+    private TicketService: TicketService,
+    private $scope: IScope,
+    private $state: StateService) {
+    'ngInject';
+  }
+
+  $onInit() {
+
+    this.onPaginate = this.onPaginate.bind(this);
+
+    this.query = new TicketsQuery();
+    this.query.page = this.$state.params.page || 1;
+    this.query.size = this.$state.params.size || 15;
+    this.query.field = this.$state.params.field || '-created_at';
+    this.query.order = false;
+
+    this.$scope.$watch('ticketsListCtrl.query.field', (field) => {
+      if (field) {
+        this.refresh();
+      }
+    });
+  }
+
+  onPaginate(page, size) {
+    this.query.page = page;
+    this.query.size = size;
+    this.refresh();
+  }
+
+  refresh() {
+    this.$state.transitionTo(
+      this.$state.current,
+      {
+        page: this.query.page,
+        size: this.query.size,
+        field: this.query.field,
+        order: this.query.order,
+      },
+      {notify: false});
+    this.TicketService.search(this.query).then((tickets) => {
+      this.tickets = tickets.data;
+    });
+  }
+}
+
+export default TicketsListController;

gravitee-io/gravitee-api-management

src/management/management.route.ts
@@ -21,8 +21,9 @@ import {User} from '../entities/user';
 import RoleService from '../services/role.service';
 import DashboardService from '../services/dashboard.service';
 import {StateParams} from '@uirouter/core';
-import TenantService from '../services/tenant.service';
 import AnalyticsService from '../services/analytics.service';
+import TicketsListController from './support/tickets-list.controller';
+import TicketService from '../services/ticket.service';
 
 function managementRouterConfig($stateProvider) {
   'ngInject';
@@ -263,11 +264,43 @@ function managementRouterConfig($stateProvider) {
       }
     })
     .state('management.support', {
+      template: '<div ui-view></div>',
+    })
+    .state('management.support.create', {
       url: '/support',
       template: require('./support/ticket.html'),
       controller: 'SupportTicketController',
       controllerAs: 'supportTicketCtrl'
-    });
+    })
+    .state('management.support.tickets', {
+      url: '/support/tickets?page&size&order',
+      template: require('./support/tickets-list.html'),
+      controller: TicketsListController,
+      controllerAs: 'ticketsListCtrl',
+      params: {
+        page: {
+          type: 'int',
+          dynamic: true
+        },
+        size: {
+          type: 'int',
+          dynamic: true
+        },
+        order: {
+          type: 'string',
+          dynamic: true
+        }
+      },
+    })
+    .state('management.support.ticket', {
+      url: '/support/tickets/:ticketId?page&size&order',
+      component: 'ticketDetail',
+      resolve: {
+        ticket: ($stateParams: StateParams, TicketService: TicketService) =>
+          TicketService.getTicket($stateParams.ticketId).then(response => response.data)
+      }
+    })
+  ;
 }
 
 export default managementRouterConfig;

src/management/support/ticket-detail.component.ts
@@ -1,0 +1,35 @@
+import {StateService} from '@uirouter/core';
+
+/*
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+const TicketDetailComponent: ng.IComponentOptions = {
+  template: require('./ticket-detail.html'),
+  bindings: {
+    ticket: '<'
+  },
+  controller: function($state: StateService) {
+    'ngInject';
+
+    this.backStateParams = {
+      page: $state.params.page,
+      size: $state.params.size,
+      order: $state.params.order
+    };
+  }
+
+};
+
+export default TicketDetailComponent;

src/management/support/tickets-list.controller.ts
@@ -1,0 +1,69 @@
+/*
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import TicketService, {TicketsQuery} from '../../services/ticket.service';
+import {StateService} from '@uirouter/core';
+import {IScope} from 'angular';
+
+class TicketsListController {
+
+  private query: TicketsQuery;
+  private tickets: {totalElements: number, content: any[]};
+
+  constructor(
+    private TicketService: TicketService,
+    private $scope: IScope,
+    private $state: StateService) {
+    'ngInject';
+  }
+
+  $onInit() {
+
+    this.onPaginate = this.onPaginate.bind(this);
+
+    this.query = new TicketsQuery();
+    this.query.page = this.$state.params.page || 1;
+    this.query.size = this.$state.params.size || 15;
+    this.query.order = this.$state.params.order || '-created_at';
+
+    this.$scope.$watch('ticketsListCtrl.query.order', (order) => {
+      if (order) {
+        this.refresh();
+      }
+    });
+  }
+
+  onPaginate(page, size) {
+    this.query.page = page;
+    this.query.size = size;
+    this.refresh();
+  }
+
+  refresh() {
+    this.$state.transitionTo(
+      this.$state.current,
+      {
+        page: this.query.page,
+        size: this.query.size,
+        order: this.query.order,
+      },
+      {notify: false});
+    this.TicketService.search(this.query).then((tickets) => {
+      this.tickets = tickets.data;
+    });
+  }
+}
+
+export default TicketsListController;