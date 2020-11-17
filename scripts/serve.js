e7bffb575a4dc826b747470d622677683671b73b
Guillaume Cusnieux <guillaume.cusnieux@graviteesource.com>
2020-11-17 14:52:32 +0100

    chore: add proxy middleware for testing app in production mode
    fix gravitee-io/issues#3895

--- scripts/serve.js
+++ scripts/serve.js
@@ -1,9 +1,11 @@
 const compression = require('compression');
 const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 
 app.use(compression());
 app.use(express.static('dist/'));
+app.use('/portal', createProxyMiddleware({ target: 'http://localhost:8083', changeOrigin: true }));
 
 app.all("/*", function(req, res) {
   res.sendFile('index.html', { root: 'dist/' });
