751a6ac26d8f6b3766eadd17ae2add96c4eb0ea4
Yann Tavernier <yann.tavernier@graviteesource.com>
2021-01-07 18:22:37 +0100

    feat: console dashboard page

--- src/main/java/io/gravitee/repository/management/model/DashboardReferenceType.java
+++ src/main/java/io/gravitee/repository/management/model/DashboardReferenceType.java
@@ -20,5 +20,5 @@ package io.gravitee.repository.management.model;
  * @author GraviteeSource Team
  */
 public enum DashboardReferenceType {
-    PLATFORM, API, APPLICATION
+    PLATFORM, API, APPLICATION, HOME
 }
