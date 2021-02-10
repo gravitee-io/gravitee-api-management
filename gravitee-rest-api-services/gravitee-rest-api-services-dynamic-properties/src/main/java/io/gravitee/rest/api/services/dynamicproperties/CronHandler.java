b16391c59d41d42609483febe39d2eeb40428b80
Gaëtan Maisse <gaetanmaisse@gmail.com>
2021-02-10 17:10:14 +0100

    fix: update the correct timer id in CronHandler to be able to stop it

--- gravitee-rest-api-services/gravitee-rest-api-services-dynamic-properties/src/main/java/io/gravitee/rest/api/services/dynamicproperties/CronHandler.java
+++ gravitee-rest-api-services/gravitee-rest-api-services-dynamic-properties/src/main/java/io/gravitee/rest/api/services/dynamicproperties/CronHandler.java
@@ -55,9 +55,9 @@ public class CronHandler implements Handler<Long>, Serializable {
     }
 
     @Override
-    public void handle(Long timerId) {
+    public void handle(final Long timerId) {
         final long delay = getDelay(expression);
-        timerId = vertx.setTimer(delay, this);
+        this.timerId = vertx.setTimer(delay, this);
         handler.handle(timerId);
     }
 
