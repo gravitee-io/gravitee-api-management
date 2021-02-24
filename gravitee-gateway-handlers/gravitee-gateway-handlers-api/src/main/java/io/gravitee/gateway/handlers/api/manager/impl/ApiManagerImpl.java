e5431ca208012d44ad5b4c99c0e8c491d734e95d
Guillaume Cusnieux <guillaume.cusnieux@graviteesource.com>
2021-02-24 20:12:35 +0100

    chore: use hazelcast version from parent pom
    fix gravitee-io/issues#599

--- gravitee-gateway-handlers/gravitee-gateway-handlers-api/src/main/java/io/gravitee/gateway/handlers/api/manager/impl/ApiManagerImpl.java
+++ gravitee-gateway-handlers/gravitee-gateway-handlers-api/src/main/java/io/gravitee/gateway/handlers/api/manager/impl/ApiManagerImpl.java
@@ -18,7 +18,7 @@ package io.gravitee.gateway.handlers.api.manager.impl;
 import com.hazelcast.core.EntryEvent;
 import com.hazelcast.core.EntryEventType;
 import com.hazelcast.core.HazelcastInstance;
-import com.hazelcast.core.IMap;
+import com.hazelcast.map.IMap;
 import com.hazelcast.map.impl.MapListenerAdapter;
 import io.gravitee.common.event.EventManager;
 import io.gravitee.definition.model.Plan;
