5f6c403590fc3c12bc5b0a52f1f8ea4e701d4176
Michael Sogos <michaelsogos@users.noreply.github.com>
2021-03-05 11:46:49 +0100

    Update ManagedEndpointFactoryTest.java

--- gravitee-gateway-http/src/test/java/io/gravite/gateway/http/endpoint/ManagedEndpointFactoryTest.java
+++ gravitee-gateway-http/src/test/java/io/gravite/gateway/http/endpoint/ManagedEndpointFactoryTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.gravite.gateway.http.endpoint;
+package io.gravitee.gateway.http.endpoint;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
