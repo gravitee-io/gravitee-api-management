257b84334662f221149012fc56f4ab2509bd1740
David BRASSELY <brasseld@gmail.com>
2021-04-07 15:47:59 +0200

    feat(cockpit): Propagate monitoring data to cockpit
    Closes gravitee-io/issues#5104

---
+++ src/main/java/io/gravitee/repository/mongodb/management/MongoNodeMonitoringRepository.java
@@ -0,0 +1,92 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.repository.mongodb.management;
+
+import io.gravitee.node.api.Monitoring;
+import io.gravitee.node.api.NodeMonitoringRepository;
+import io.gravitee.repository.management.model.Plan;
+import io.gravitee.repository.mongodb.management.internal.model.MonitoringMongo;
+import io.gravitee.repository.mongodb.management.internal.model.PlanMongo;
+import io.gravitee.repository.mongodb.management.internal.node.NodeMonitoringMongoRepository;
+import io.gravitee.repository.mongodb.management.mapper.GraviteeMapper;
+import io.reactivex.Flowable;
+import io.reactivex.Maybe;
+import io.reactivex.Single;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.stream.Collectors;
+
+/**
+ * @author David BRASSELY (david.brassely at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+@Component
+public class MongoNodeMonitoringRepository implements NodeMonitoringRepository {
+
+    @Autowired
+    private GraviteeMapper mapper;
+
+    @Autowired
+    private NodeMonitoringMongoRepository internalNodeMonitoringRepository;
+
+    @Override
+    public Maybe<Monitoring> findByNodeIdAndType(String nodeId, String type) {
+        MonitoringMongo monitoring = internalNodeMonitoringRepository.findByNodeIdAndType(nodeId, type);
+        return monitoring != null ? Maybe.just(map(monitoring)) : Maybe.empty();
+    }
+
+    @Override
+    public Single<Monitoring> create(Monitoring monitoring) {
+        MonitoringMongo monitoringMongo = internalNodeMonitoringRepository.insert(map(monitoring));
+        return Single.just(map(monitoringMongo));
+    }
+
+    @Override
+    public Single<Monitoring> update(Monitoring monitoring) {
+        if (monitoring == null || monitoring.getId() == null) {
+            return Single.error(new IllegalStateException("Node monitoring to update must have an id"));
+        }
+
+        MonitoringMongo monitoringMongo = internalNodeMonitoringRepository.findById(monitoring.getId()).orElse(null);
+
+        if (monitoringMongo == null) {
+            return Single.error(new IllegalStateException(
+                    String.format("No node monitoring found with id [%s]", monitoring.getId())));
+        }
+
+        monitoringMongo = map(monitoring);
+        monitoringMongo = internalNodeMonitoringRepository.save(monitoringMongo);
+        return Single.just(map(monitoringMongo));
+    }
+
+    @Override
+    public Flowable<Monitoring> findByTypeAndTimeFrame(String type, long from, long to) {
+        return Flowable.fromIterable(
+                internalNodeMonitoringRepository.findByTypeAndTimeFrame(type, from, to)
+                        .stream()
+                        .map(this::map)
+                        .collect(Collectors.toList()));
+    }
+
+    private MonitoringMongo map(Monitoring item) {
+        return (item == null) ? null : mapper.map(item, MonitoringMongo.class);
+    }
+
+    private Monitoring map(MonitoringMongo item){
+        return (item == null) ? null : mapper.map(item, Monitoring.class);
+    }
+}
---
+++ src/main/java/io/gravitee/repository/mongodb/management/internal/node/NodeMonitoringMongoRepositoryImpl.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.repository.mongodb.management.internal.node;
+
+import io.gravitee.repository.mongodb.management.internal.model.MonitoringMongo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @author David BRASSELY (david.brassely at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+public class NodeMonitoringMongoRepositoryImpl implements NodeMonitoringMongoRepositoryCustom {
+
+    private static final String FIELD_TYPE = "type";
+    private static final String FIELD_UPDATED_AT = "updatedAt";
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    @Override
+    public List<MonitoringMongo> findByTypeAndTimeFrame(String type, long from, long to) {
+        final Query query = new Query();
+        query.addCriteria(Criteria.where(FIELD_TYPE).is(type));
+
+        final Criteria updatedAtCriteria = Criteria.where(FIELD_UPDATED_AT).gte(new Date(from));
+        if (to > from) {
+            updatedAtCriteria.lte(new Date(to));
+        }
+
+        query.addCriteria(updatedAtCriteria);
+
+        return mongoTemplate.find(query, MonitoringMongo.class);
+    }
+}
