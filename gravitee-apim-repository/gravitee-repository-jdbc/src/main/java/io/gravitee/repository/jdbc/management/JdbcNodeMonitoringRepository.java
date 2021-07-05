a7cf13c5a2fdacb37d6311d980a5a8cda11100c9
Gaëtan Maisse <gaetanmaisse@gmail.com>
2021-07-05 16:25:33 +0200

    chore: move gravitee-apim-repository/gravitee-repository-jdbc to its own directory

---
+++ gravitee-apim-repository/gravitee-repository-jdbc/src/main/java/io/gravitee/repository/jdbc/management/JdbcNodeMonitoringRepository.java
@@ -0,0 +1,164 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.repository.jdbc.management;
+
+import io.gravitee.node.api.Monitoring;
+import io.gravitee.node.api.NodeMonitoringRepository;
+import io.gravitee.repository.exceptions.TechnicalException;
+import io.gravitee.repository.jdbc.orm.JdbcObjectMapper;
+import io.reactivex.Flowable;
+import io.reactivex.Maybe;
+import io.reactivex.Single;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.Timestamp;
+import java.sql.Types;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * @author David BRASSELY (david.brassely at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+@Repository
+public class JdbcNodeMonitoringRepository extends JdbcAbstractRepository<Monitoring> implements NodeMonitoringRepository {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(JdbcNodeMonitoringRepository.class);
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    JdbcNodeMonitoringRepository(@Value("${management.jdbc.prefix:}") String tablePrefix) {
+        super(tablePrefix, "node_monitoring");
+    }
+
+    @Override
+    protected JdbcObjectMapper<Monitoring> buildOrm() {
+        return JdbcObjectMapper.builder(Monitoring.class, this.tableName, "id")
+                .addColumn("id", Types.NVARCHAR, String.class)
+                .addColumn("node_id", Types.NVARCHAR, String.class)
+                .addColumn("type", Types.NVARCHAR, String.class)
+                .addColumn("payload", Types.NVARCHAR, String.class)
+                .addColumn("created_at", Types.TIMESTAMP, Date.class)
+                .addColumn("evaluated_at", Types.TIMESTAMP, Date.class)
+                .addColumn("updated_at", Types.TIMESTAMP, Date.class)
+                .build();
+    }
+
+    @Override
+    public Maybe<Monitoring> findByNodeIdAndType(String nodeId, String type) {
+        LOGGER.debug("JdbcNodeMonitoringRepository.findByNodeIdAndType({}, {})", nodeId, type);
+        try {
+            List<Monitoring> monitoringEvents = jdbcTemplate.query(
+                    getOrm().getSelectAllSql() + " where node_id = ? and type = ?"
+                    , getOrm().getRowMapper()
+                    , nodeId
+                    , type
+            );
+
+            return monitoringEvents.stream().findFirst().map(Maybe::just).orElseGet(Maybe::empty);
+        } catch (final Exception ex) {
+            LOGGER.error("Failed to find node monitoring by node_id and type:", ex);
+            return Maybe.error(new TechnicalException("Failed to find node monitoring by node_id and type", ex));
+        }
+    }
+
+
+    @Override
+    public Single<Monitoring> create(Monitoring monitoring) {
+        LOGGER.debug("JdbcNodeMonitoringRepository.create({})", monitoring);
+        try {
+            jdbcTemplate.update(getOrm().buildInsertPreparedStatementCreator(monitoring));
+
+            return findById(monitoring.getId()).map(Maybe::just).orElseGet(Maybe::empty).toSingle();
+        } catch (final Exception ex) {
+            LOGGER.error("Failed to create node monitoring:", ex);
+            return Single.error(new TechnicalException("Failed to create node monitoring", ex));
+        }
+    }
+
+    public Optional<Monitoring> findById(String id) throws TechnicalException {
+        LOGGER.debug("JdbcNodeMonitoringRepository<{}>.findById({})", getOrm().getTableName(), id);
+        try {
+            List<Monitoring> items = jdbcTemplate.query(getOrm().getSelectByIdSql()
+                    , getRowMapper()
+                    , id
+            );
+            return items.stream().findFirst();
+        } catch (final Exception ex) {
+            LOGGER.error("Failed to find {} items by id:", getOrm().getTableName(), ex);
+            throw new TechnicalException("Failed to find " + getOrm().getTableName() + " items by id", ex);
+        }
+    }
+
+    @Override
+    public Single<Monitoring> update(Monitoring monitoring) {
+        LOGGER.debug("JdbcNodeMonitoringRepository.update({})", monitoring);
+        try {
+            jdbcTemplate.update(getOrm().buildUpdatePreparedStatementCreator(monitoring, monitoring.getId()));
+
+            return findById(monitoring.getId())
+                    .map(Single::just)
+                    .orElseGet(() -> Single.error(new IllegalStateException(
+                        String.format("No node monitoring found with id [%s]", monitoring.getId()))));
+
+        } catch (final Exception ex) {
+            LOGGER.error("Failed to update node monitoring:", ex);
+            return Single.error(new TechnicalException("Failed to update node monitoring", ex));
+        }
+    }
+
+    @Override
+    public Flowable<Monitoring> findByTypeAndTimeFrame(String type, long from, long to) {
+        if (LOGGER.isDebugEnabled()) {
+            LOGGER.debug("JdbcNodeMonitoringRepository.findByTypeAndTimeFrame({}, {}, {})", type, from, to);
+        }
+
+        final List<Object> args = new ArrayList<>();
+        final StringBuilder builder = new StringBuilder("select nm.* from " + this.tableName + " nm ");
+        builder.append("where type = ? and updated_at >= ?");
+        args.add(type);
+        args.add(new Date(from));
+
+        if (to > from) {
+            builder.append(" and updated_at <= ?");
+            args.add(new Date(to));
+        }
+
+        return Flowable.fromIterable(jdbcTemplate.query((Connection cnctn) -> {
+            PreparedStatement stmt = cnctn.prepareStatement(builder.toString());
+            int idx = 1;
+            for (final Object arg : args) {
+                if (arg instanceof Date) {
+                    final Date date = (Date) arg;
+                    stmt.setTimestamp(idx++, new Timestamp(date.getTime()));
+                } else {
+                    stmt.setObject(idx++, arg);
+                }
+            }
+            return stmt;
+        }, getRowMapper()));
+    }
+}
