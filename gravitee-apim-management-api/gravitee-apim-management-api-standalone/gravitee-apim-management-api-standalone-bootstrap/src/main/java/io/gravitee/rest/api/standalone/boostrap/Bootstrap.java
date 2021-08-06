ee965f5705d64553e35410756ebb6c5543411c4d
Florent CHAMFROY <florent.chamfroy@graviteesource.com>
2021-08-06 10:32:12 +0200

    fix: update folders name in some files
    makefile, executable files and config files

--- gravitee-apim-management-api/gravitee-apim-management-api-standalone/gravitee-apim-management-api-standalone-bootstrap/src/main/java/io/gravitee/rest/api/standalone/boostrap/Bootstrap.java
+++ gravitee-apim-management-api/gravitee-apim-management-api-standalone/gravitee-apim-management-api-standalone-bootstrap/src/main/java/io/gravitee/rest/api/standalone/boostrap/Bootstrap.java
@@ -133,7 +133,7 @@ public class Bootstrap {
 
         File graviteeLibDir = new File(graviteeHomeDir, LIB_DIRECTORY);
 
-        File[] files = graviteeLibDir.listFiles(pathname -> pathname.getName().startsWith("gravitee-rest-api-standalone-bootstrap"));
+        File[] files = graviteeLibDir.listFiles(pathname -> pathname.getName().startsWith("gravitee-apim-rest-api-standalone-bootstrap"));
 
         if (files == null || files.length == 0 || files.length > 1) {
             throw new RuntimeException(
