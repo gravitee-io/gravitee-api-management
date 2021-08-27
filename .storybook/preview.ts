5d89aee594aeacc5b167e9c666a56d74de2ecc78
Gaëtan Maisse <gaetanmaisse@gmail.com>
2021-08-27 17:37:11 +0200

    chore: add and configure Storybook
    Also create a dummy story

---
+++ .storybook/preview.ts
@@ -0,0 +1,5 @@
+// Import all styles of the app
+import '../src/index.scss';
+
+// Set material classes to the Storybook root div
+window.document.getElementById('root').classList.add('mat', 'mat-typography');
