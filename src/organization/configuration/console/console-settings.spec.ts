9ffb547a3c7a145b81615d744689114d517b4a2c
Thibaud Av <thibaud.avenier@gmail.com>
2021-09-02 17:15:14 +0200

    test: for ConsoleSettings  `cors` part

--- src/organization/configuration/console/console-settings.spec.ts
+++ src/organization/configuration/console/console-settings.spec.ts
@@ -20,8 +20,13 @@ import { MatInputHarness } from '@angular/material/input/testing';
 import { MatFormFieldHarness } from '@angular/material/form-field/testing';
 import { MatButtonHarness } from '@angular/material/button/testing';
 import { MatCheckboxHarness } from '@angular/material/checkbox/testing';
+import { MatChipInputHarness, MatChipListHarness } from '@angular/material/chips/testing';
+import { MatSelectHarness } from '@angular/material/select/testing';
+import { MatAutocompleteHarness } from '@angular/material/autocomplete/testing';
+import { MatDialogHarness } from '@angular/material/dialog/testing';
 import { HttpTestingController } from '@angular/common/http/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { InteractivityChecker } from '@angular/cdk/a11y';
 
 import { ConsoleSettingsComponent } from './console-settings';
 
@@ -32,18 +37,26 @@ import { ConsoleSettings } from '../../../entities/consoleSettings';
 describe('ConsoleSettingsComponent', () => {
   let fixture: ComponentFixture<ConsoleSettingsComponent>;
   let loader: HarnessLoader;
+  let rootLoader: HarnessLoader;
 
   let httpTestingController: HttpTestingController;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [NoopAnimationsModule, GioHttpTestingModule, OrganizationSettingsModule],
-    }).compileComponents();
+    })
+      .overrideProvider(InteractivityChecker, {
+        useValue: {
+          isFocusable: () => true, // This checks focus trap, set it to true to  avoid the warning
+        },
+      })
+      .compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ConsoleSettingsComponent);
     loader = TestbedHarnessEnvironment.loader(fixture);
+    rootLoader = TestbedHarnessEnvironment.documentRootLoader(fixture);
 
     httpTestingController = TestBed.inject(HttpTestingController);
     fixture.detectChanges();
@@ -283,6 +296,147 @@ describe('ConsoleSettingsComponent', () => {
       expectConsoleSettingsSendRequest({
         alert: {
           enabled: true,
+        },
+      });
+    });
+  });
+
+  describe('cors', () => {
+    it('should disable field when setting is readonly', async () => {
+      expectConsoleSettingsGetRequest({
+        cors: {
+          allowOrigin: undefined,
+          allowMethods: ['GET', 'POST'],
+          allowHeaders: ['a', 'b'],
+          exposedHeaders: [],
+          maxAge: 42,
+        },
+        metadata: {
+          readonly: ['cors.allowOrigin', 'cors.allowMethods', 'cors.allowHeaders', 'cors.exposedHeaders', 'cors.maxAge'],
+        },
+      });
+
+      const allowOriginFormField = await loader.getHarness(MatFormFieldHarness.with({ floatingLabelText: 'Allow-Origin' }));
+      expect(await allowOriginFormField.isDisabled()).toEqual(true);
+
+      const allowMethodsFormField = await loader.getHarness(
+        MatFormFieldHarness.with({ floatingLabelText: 'Access-Control-Allow-Methods' }),
+      );
+      expect(await allowMethodsFormField.isDisabled()).toEqual(true);
+
+      const allowHeadersFormField = await loader.getHarness(MatFormFieldHarness.with({ floatingLabelText: 'Allow-Headers' }));
+      expect(await allowHeadersFormField.isDisabled()).toEqual(true);
+
+      const exposedHeadersFormField = await loader.getHarness(MatFormFieldHarness.with({ floatingLabelText: 'Exposed-Headers' }));
+      expect(await exposedHeadersFormField.isDisabled()).toEqual(true);
+
+      const maxAgeFormField = await loader.getHarness(MatFormFieldHarness.with({ floatingLabelText: /^Max age/ }));
+      expect(await maxAgeFormField.isDisabled()).toEqual(true);
+    });
+
+    it('should save cors settings', async () => {
+      expectConsoleSettingsGetRequest({
+        cors: {
+          allowOrigin: undefined,
+          allowMethods: ['GET', 'POST'],
+          allowHeaders: ['x-foo', 'x-bar'],
+          exposedHeaders: [],
+          maxAge: 42,
+        },
+      });
+
+      // # Allow-Origin
+      const allowOriginFormField = await loader.getHarness(MatFormFieldHarness.with({ floatingLabelText: 'Allow-Origin' }));
+      expect(await (await allowOriginFormField.getControl(MatChipListHarness)).getChips()).toEqual([]);
+      // Add valid RegExp
+      const allowOriginChipListInput = await allowOriginFormField.getControl(MatChipInputHarness);
+      await allowOriginChipListInput.setValue('(Valid|RegExp)');
+      await allowOriginChipListInput.blur();
+
+      // # Access-Control-Allow-Methods
+      const allowMethodsFormField = await loader.getHarness(
+        MatFormFieldHarness.with({ floatingLabelText: 'Access-Control-Allow-Methods' }),
+      );
+      // Open select and select DELETE option
+      const allowMethodsSelect = await allowMethodsFormField.getControl(MatSelectHarness);
+      await allowMethodsSelect.open();
+      await (await allowMethodsSelect.getOptions({ text: 'DELETE' }))[0].click();
+      await allowMethodsSelect.close();
+      // Check selected options
+      expect(await allowMethodsSelect.getValueText()).toEqual('GET, DELETE, POST');
+
+      // # Allow-Headers
+      const allowHeadersFormField = await loader.getHarness(MatFormFieldHarness.with({ floatingLabelText: 'Allow-Headers' }));
+      const allowHeadersChipList = await allowHeadersFormField.getControl(MatChipListHarness);
+      const allowHeadersChipListInput = await allowHeadersChipList.getInput();
+      // Add new custom header
+      await allowHeadersChipListInput.setValue('x-ray');
+      await allowHeadersChipListInput.blur();
+      // Remove x-foo header
+      await (await allowHeadersChipList.getChips({ text: 'x-foo' }))[0].remove();
+      // Check headers
+      const allowHeadersChips = await Promise.all(await (await allowHeadersChipList.getChips()).map((c) => c.getText()));
+      expect(allowHeadersChips).toEqual(['x-bar', 'x-ray']);
+
+      // # Exposed-Headers
+      const exposedHeadersFormField = await loader.getHarness(MatFormFieldHarness.with({ floatingLabelText: 'Exposed-Headers' }));
+      const exposedHeadersChipList = await exposedHeadersFormField.getControl(MatChipListHarness);
+      const exposedHeadersAutocomplete = await exposedHeadersFormField.getControl(MatAutocompleteHarness);
+      // Select `Cache-Control` with autocomplete
+      await exposedHeadersAutocomplete.focus();
+      await exposedHeadersAutocomplete.selectOption({ text: 'Cache-Control' });
+      // Check headers
+      const exposedHeadersChips = await Promise.all(await (await exposedHeadersChipList.getChips()).map((c) => c.getText()));
+      expect(exposedHeadersChips).toEqual(['Cache-Control']);
+
+      // # Max age
+      const maxAgeFormField = await loader.getHarness(MatFormFieldHarness.with({ floatingLabelText: /^Max age/ }));
+      await (await maxAgeFormField.getControl(MatInputHarness)).setValue('666');
+
+      const saveButton = await loader.getHarness(MatButtonHarness.with({ text: 'Save' }));
+      await saveButton.click();
+
+      expectConsoleSettingsSendRequest({
+        cors: {
+          allowOrigin: ['(Valid|RegExp)'],
+          allowMethods: ['GET', 'DELETE', 'POST'],
+          allowHeaders: ['x-bar', 'x-ray'],
+          exposedHeaders: ['Cache-Control'],
+          maxAge: 666,
+        },
+      });
+    });
+
+    it('should open confirm dialog for the addition of a Allow-Origin with `*`', async () => {
+      expectConsoleSettingsGetRequest({
+        cors: {
+          allowOrigin: undefined,
+        },
+      });
+
+      // # Allow-Origin
+      const allowOriginFormField = await loader.getHarness(MatFormFieldHarness.with({ floatingLabelText: 'Allow-Origin' }));
+      expect(await (await allowOriginFormField.getControl(MatChipListHarness)).getChips()).toEqual([]);
+      // Add `*` and confirm dialog
+      const allowOriginChipListInput = await allowOriginFormField.getControl(MatChipInputHarness);
+
+      await allowOriginChipListInput.setValue('*');
+      await allowOriginChipListInput.blur();
+      const dialogOne = await rootLoader.getHarness(MatDialogHarness);
+      expect(await dialogOne.getId()).toEqual('allowAllOriginsConfirmDialog');
+      await dialogOne.close();
+
+      await allowOriginChipListInput.setValue('*');
+      await allowOriginChipListInput.blur();
+      const dialogTwo = await rootLoader.getHarness(MatDialogHarness);
+      await (await dialogTwo.getHarness(MatButtonHarness.with({ text: /^Yes,/ }))).click();
+
+      const saveButton = await loader.getHarness(MatButtonHarness.with({ text: 'Save' }));
+      await saveButton.click();
+
+      expectConsoleSettingsSendRequest({
+        cors: {
+          allowOrigin: ['*'],
         },
       });
     });
