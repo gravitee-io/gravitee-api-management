1cdc67b8c22b707a2f44449a5b0a398b952a5654
Thibaud Av <thibaud.avenier@gmail.com>
2021-09-22 18:18:36 +0200

    chore: fix storybook with mat typography

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -3,6 +3,7 @@ import '../src/index.scss';
 import { withDesign } from "storybook-addon-designs";
 
 // Set material classes to the Storybook root div
-window.document.getElementById('root').classList.add('mat', 'mat-typography');
+window.document.body.classList.add('mat');
+window.document.getElementById('root').classList.add( 'mat-typography');
 
 export const decorators = [withDesign]
