5347718ad89ff85c48dafc9878437de7c1ebef1c
Gaëtan Maisse <gaetanmaisse@gmail.com>
2021-10-21 10:21:30 +0200

    chore: update .prettierignore

--- gravitee-apim-console-webui/.storybook/preview.ts
+++ gravitee-apim-console-webui/.storybook/preview.ts
@@ -1,9 +1,9 @@
 // Import all styles of the app
 import '../src/index.scss';
-import { withDesign } from "storybook-addon-designs";
+import { withDesign } from 'storybook-addon-designs';
 
 // Set material classes to the Storybook root div
 window.document.body.classList.add('mat');
-window.document.getElementById('root').classList.add( 'mat-typography');
+window.document.getElementById('root').classList.add('mat-typography');
 
-export const decorators = [withDesign]
+export const decorators = [withDesign];
