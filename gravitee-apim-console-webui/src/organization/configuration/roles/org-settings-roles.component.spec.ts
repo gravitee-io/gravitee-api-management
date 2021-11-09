2b61ea971f5bc1f9450e29e65959d76a9d63b775
Gaëtan Maisse <gaetanmaisse@gmail.com>
2021-11-09 10:03:20 +0100

    refactor: start implementation of new OrgSettingsRoles component

--- gravitee-apim-console-webui/src/organization/configuration/roles/org-settings-roles.component.spec.ts
+++ gravitee-apim-console-webui/src/organization/configuration/roles/org-settings-roles.component.spec.ts
@@ -20,10 +20,12 @@ import { HttpTestingController } from '@angular/common/http/testing';
 import { OrgSettingsRolesComponent } from './org-settings-roles.component';
 
 import { OrganizationSettingsModule } from '../organization-settings.module';
-import { GioHttpTestingModule } from '../../../shared/testing';
+import { CONSTANTS_TESTING, GioHttpTestingModule } from '../../../shared/testing';
+import { fakeRole } from '../../../entities/role/role.fixture';
 
 describe('OrgSettingsRolesComponent', () => {
   let fixture: ComponentFixture<OrgSettingsRolesComponent>;
+  let component: OrgSettingsRolesComponent;
   let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
@@ -32,11 +34,119 @@ describe('OrgSettingsRolesComponent', () => {
     });
     httpTestingController = TestBed.inject(HttpTestingController);
     fixture = TestBed.createComponent(OrgSettingsRolesComponent);
+    component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should init properly', () => {
-    expect(fixture.componentInstance).toBeDefined();
+    httpTestingController.expectOne(`${CONSTANTS_TESTING.org.baseURL}/configuration/rolescopes/ORGANIZATION/roles`).flush([
+      fakeRole({
+        id: 'role-1',
+        name: 'Role 1',
+        description: 'Role 1 description',
+        scope: 'ORGANIZATION',
+      }),
+    ]);
+    httpTestingController.expectOne(`${CONSTANTS_TESTING.org.baseURL}/configuration/rolescopes/ENVIRONMENT/roles`).flush([
+      fakeRole({
+        id: 'role-2',
+        name: 'Role 2',
+        description: 'Role 2 description',
+        scope: 'ENVIRONMENT',
+      }),
+    ]);
+    httpTestingController.expectOne(`${CONSTANTS_TESTING.org.baseURL}/configuration/rolescopes/API/roles`).flush([
+      fakeRole({
+        id: 'role-3',
+        name: 'Role 3',
+        description: 'Role 3 description',
+        scope: 'API',
+        default: false,
+        system: false,
+      }),
+    ]);
+    httpTestingController.expectOne(`${CONSTANTS_TESTING.org.baseURL}/configuration/rolescopes/APPLICATION/roles`).flush([
+      fakeRole({
+        id: 'role-4',
+        name: 'Role 4',
+        description: 'Role 4 description',
+        scope: 'APPLICATION',
+      }),
+      fakeRole({
+        id: 'role-5',
+        name: 'Role 5',
+        description: 'Role 5 description',
+        scope: 'APPLICATION',
+      }),
+    ]);
+
+    expect(component.rolesByScope).toStrictEqual([
+      {
+        scope: 'Organization',
+        roles: [
+          {
+            canBeDeleted: false,
+            description: 'Role 1 description',
+            hasUserRoleManagement: true,
+            icon: 'corporate_fare',
+            isDefault: true,
+            isSystem: false,
+            name: 'Role 1',
+          },
+        ],
+      },
+      {
+        scope: 'Environment',
+        roles: [
+          {
+            canBeDeleted: false,
+            description: 'Role 2 description',
+            hasUserRoleManagement: false,
+            icon: 'dns',
+            isDefault: true,
+            isSystem: false,
+            name: 'Role 2',
+          },
+        ],
+      },
+      {
+        scope: 'API',
+        roles: [
+          {
+            canBeDeleted: true,
+            description: 'Role 3 description',
+            hasUserRoleManagement: false,
+            icon: 'dashboard',
+            isDefault: false,
+            isSystem: false,
+            name: 'Role 3',
+          },
+        ],
+      },
+      {
+        scope: 'Application',
+        roles: [
+          {
+            canBeDeleted: false,
+            description: 'Role 4 description',
+            hasUserRoleManagement: false,
+            icon: 'list',
+            isDefault: true,
+            isSystem: false,
+            name: 'Role 4',
+          },
+          {
+            canBeDeleted: false,
+            description: 'Role 5 description',
+            hasUserRoleManagement: false,
+            icon: 'list',
+            isDefault: true,
+            isSystem: false,
+            name: 'Role 5',
+          },
+        ],
+      },
+    ]);
   });
 
   afterEach(() => {
--- gravitee-apim-console-webui/src/organization/configuration/roles/org-settings-roles.component.ts
+++ gravitee-apim-console-webui/src/organization/configuration/roles/org-settings-roles.component.ts
@@ -15,6 +15,21 @@
  */
 
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject, combineLatest } from 'rxjs';
+import { takeUntil, tap } from 'rxjs/operators';
+
+import { RoleService } from '../../../services-ngx/role.service';
+import { Role, RoleScope } from '../../../entities/role/role';
+
+interface RoleVM {
+  name: string;
+  description: string;
+  isDefault: boolean;
+  isSystem: boolean;
+  canBeDeleted: boolean;
+  icon: string;
+  hasUserRoleManagement: boolean;
+}
 
 @Component({
   selector: 'org-settings-roles',
@@ -22,9 +37,76 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
   styles: [require('./org-settings-roles.component.scss')],
 })
 export class OrgSettingsRolesComponent implements OnInit, OnDestroy {
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
-  ngOnDestroy(): void {}
+  rolesByScope: Array<{ scope: string; roles: RoleVM[] }>;
+  loading = true;
 
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
-  ngOnInit(): void {}
+  constructor(private readonly roleService: RoleService) {}
+
+  private readonly unsubscribe$ = new Subject<boolean>();
+
+  ngOnInit(): void {
+    combineLatest([
+      this.roleService.list('ORGANIZATION'),
+      this.roleService.list('ENVIRONMENT'),
+      this.roleService.list('API'),
+      this.roleService.list('APPLICATION'),
+    ])
+      .pipe(
+        takeUntil(this.unsubscribe$),
+        tap(([orgRoles, envRoles, apiRoles, appRoles]) => {
+          this.rolesByScope = [
+            { scope: 'Organization', roles: this.convertToRoleVMs(orgRoles) },
+            { scope: 'Environment', roles: this.convertToRoleVMs(envRoles) },
+            { scope: 'API', roles: this.convertToRoleVMs(apiRoles) },
+            { scope: 'Application', roles: this.convertToRoleVMs(appRoles) },
+          ];
+          this.loading = false;
+        }),
+      )
+      .subscribe();
+  }
+
+  ngOnDestroy() {
+    this.unsubscribe$.next(true);
+    this.unsubscribe$.unsubscribe();
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars,@typescript-eslint/no-empty-function
+  onAddARoleClicked(scope: string) {}
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars,@typescript-eslint/no-empty-function
+  onEditRoleClicked(role: RoleVM) {}
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars,@typescript-eslint/no-empty-function
+  onDeleteRoleClicked(role: RoleVM) {}
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars,@typescript-eslint/no-empty-function
+  onMembersClicked(role: RoleVM) {}
+
+  private convertToRoleVMs(roles: Role[]): RoleVM[] {
+    return roles.map((role) => ({
+      name: role.name,
+      description: role.description,
+      isDefault: role.default,
+      isSystem: role.system,
+      hasUserRoleManagement: role.scope === 'ORGANIZATION',
+      canBeDeleted: !role.default && !role.system,
+      icon: OrgSettingsRolesComponent.getScopeIcon(role.scope),
+    }));
+  }
+
+  private static getScopeIcon(scope: RoleScope): string {
+    switch (scope) {
+      case 'API':
+        return 'dashboard';
+      case 'APPLICATION':
+        return 'list';
+      case 'ENVIRONMENT':
+        return 'dns';
+      case 'ORGANIZATION':
+        return 'corporate_fare';
+      default:
+        return '';
+    }
+  }
 }
