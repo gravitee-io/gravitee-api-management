390185b9bdc7c58be69ff2dbf67f7e8d11913b29
Kamiel Ahmadpour <kamiel.ahmadpour@graviteesource.com>
2021-11-22 17:16:59 +0000

    Add Map event listener

--- gravitee-gateway-services/gravitee-gateway-services-sync/src/main/java/io/gravitee/gateway/services/sync/cache/ApiKeysCacheService.java
+++ gravitee-gateway-services/gravitee-gateway-services-sync/src/main/java/io/gravitee/gateway/services/sync/cache/ApiKeysCacheService.java
@@ -117,7 +117,7 @@ public class ApiKeysCacheService extends AbstractService implements EventListene
         LOGGER.debug("Register API key repository implementation {}", ApiKeyRepositoryWrapper.class.getName());
         beanFactory.registerSingleton(
             ApiKeyRepository.class.getName(),
-            new ApiKeyRepositoryWrapper(this.apiKeyRepository, new ApiKeysCache(cacheManager.getCache(API_KEY_CACHE_NAME)))
+            new ApiKeyRepositoryWrapper(this.apiKeyRepository, new ApiKeysCache(cacheManager.getMap(API_KEY_CACHE_NAME)))
         );
 
         LOGGER.info("Associate a new HTTP handler on {}", PATH);
@@ -165,7 +165,7 @@ public class ApiKeysCacheService extends AbstractService implements EventListene
                                     chunk
                                 );
                                 refresher.setApiKeyRepository(apiKeyRepository);
-                                refresher.setCache(new ApiKeysCache(cacheManager.getCache(API_KEY_CACHE_NAME)));
+                                refresher.setCache(new ApiKeysCache(cacheManager.getMap(API_KEY_CACHE_NAME)));
 
                                 return refresher;
                             }
@@ -280,7 +280,7 @@ public class ApiKeysCacheService extends AbstractService implements EventListene
             if (clusterManager.isMasterNode() || (!clusterManager.isMasterNode() && !distributed)) {
                 final FullApiKeyRefresher refresher = new FullApiKeyRefresher(planIds);
                 refresher.setApiKeyRepository(apiKeyRepository);
-                refresher.setCache(new ApiKeysCache(cacheManager.getCache(API_KEY_CACHE_NAME)));
+                refresher.setCache(new ApiKeysCache(cacheManager.getMap(API_KEY_CACHE_NAME)));
 
                 CompletableFuture
                     .supplyAsync(refresher::call, executorService)
