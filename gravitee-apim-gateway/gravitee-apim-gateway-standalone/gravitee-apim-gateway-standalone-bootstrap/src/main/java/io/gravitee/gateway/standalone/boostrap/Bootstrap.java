92c5d2ad14da19e984cbfbb8542671e7d3774ca4
Florent CHAMFROY <florent.chamfroy@graviteesource.com>
2021-11-25 12:49:02 +0100

    fix: fix missed renaming

--- gravitee-apim-gateway/gravitee-apim-gateway-standalone/gravitee-apim-gateway-standalone-bootstrap/src/main/java/io/gravitee/gateway/standalone/boostrap/Bootstrap.java
+++ gravitee-apim-gateway/gravitee-apim-gateway-standalone/gravitee-apim-gateway-standalone-bootstrap/src/main/java/io/gravitee/gateway/standalone/boostrap/Bootstrap.java
@@ -142,7 +142,7 @@ public class Bootstrap {
         File graviteeLibDir = new File(graviteeHomeDir, LIB_DIRECTORY);
 
         File [] files = graviteeLibDir.listFiles(pathname ->
-                pathname.getName().startsWith("gravitee-gateway-standalone-bootstrap"));
+                pathname.getName().startsWith("gravitee-apim-gateway-standalone-bootstrap"));
 
         if (files == null || files.length == 0 || files.length > 1) {
             throw new RuntimeException("Invalid Gravitee.io Node home directory. No bootstrapable jar can be found in "
