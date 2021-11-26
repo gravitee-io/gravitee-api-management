2e4ce320d60cfbac902859862f9c93d1090bfbf9
Jeoffrey Haeyaert <jeoffrey.haeyaert@gmail.com>
2021-11-26 16:19:14 +0100

    feat(ssl): support native openssl on http client
    Closes gravitee-io/issues#6640

--- gravitee-apim-gateway/gravitee-apim-gateway-http/src/main/java/io/gravitee/gateway/http/connector/AbstractConnector.java
+++ gravitee-apim-gateway/gravitee-apim-gateway-http/src/main/java/io/gravitee/gateway/http/connector/AbstractConnector.java
@@ -193,6 +193,10 @@ public abstract class AbstractConnector<T extends HttpEndpoint> extends Abstract
             options.setSsl(true)
                     .setUseAlpn(true);
 
+            if (environment.getProperty("http.ssl.openssl", Boolean.class, false)) {
+                options.setSslEngineOptions(new OpenSSLEngineOptions());
+            }
+
             if (sslOptions != null) {
                 options
                         .setVerifyHost(sslOptions.isHostnameVerifier())
