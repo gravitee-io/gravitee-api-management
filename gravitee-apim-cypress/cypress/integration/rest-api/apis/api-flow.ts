1079ffa154a363389cfed075f0807f470f5d7e2e
Christian Tschacher <15231507+ctschacher@users.noreply.github.com>
2021-12-15 18:28:38 +0100

    test: added API flow

---
+++ gravitee-apim-cypress/cypress/integration/rest-api/apis/api-flow.ts
@@ -0,0 +1,150 @@
+/*
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { ADMIN_USER, API_PUBLISHER_USER, LOW_PERMISSION_USER } from 'fixtures/fakers/users/users';
+import { ApiFakers } from 'fixtures/fakers/apis';
+import { PlanFakers } from 'fixtures/fakers/plans';
+import { createApi, deleteApi, deployApi, publishApi, startApi, stopApi } from 'commands/management/api-management-commands';
+import { createPlan, publishPlan, deletePlan } from 'commands/management/api-plan-management-commands';
+import { Api } from 'model/apis';
+import { NewPlanEntity, SecurityType } from 'model/plan';
+
+context('Create an API flow', () => {
+  let api: Api;
+  let plan: NewPlanEntity;
+
+  describe('Create an API', function () {
+    it('should create an API as admin user', function () {
+      const fakeApi: Api = ApiFakers.api();
+      createApi(ADMIN_USER, fakeApi).should((response) => {
+        expect(response.body.state).equal('STOPPED');
+        expect(response.body.visibility).equal('PRIVATE');
+        expect(response.body.lifecycle_state).equal('CREATED');
+        api = response.body;
+      });
+    });
+
+    it('should fail to create an API if user lacks required permssions', function () {
+      const fakeApi: Api = ApiFakers.api();
+      createApi(LOW_PERMISSION_USER, fakeApi).should((response) => {
+        expect(response.status).to.equal(403);
+        expect(response.body.message).to.equal('You do not have sufficient rights to access this resource');
+      });
+    });
+  });
+
+  describe('Create a plan', () => {
+    it('should create a keyless plan as admin user', function () {
+      const fakePlan = PlanFakers.plan({ security: SecurityType.KEY_LESS });
+      createPlan(ADMIN_USER, api.id, fakePlan).should(function (response) {
+        expect(response.status).to.equal(201);
+        expect(response.body).to.have.all.keys(
+          'name',
+          'id',
+          'description',
+          'validation',
+          'security',
+          'type',
+          'status',
+          'api',
+          'order',
+          'created_at',
+          'updated_at',
+          'paths',
+          'flows',
+          'comment_required',
+        );
+        expect(response.body.security).to.equal('KEY_LESS');
+        expect(response.body.status).to.equal('STAGING');
+        plan = response.body;
+      });
+    });
+  });
+
+  describe('Publish plan', function () {
+    it('should publish a plan as admin user', function () {
+      publishPlan(ADMIN_USER, api.id, plan.id).should(function (response) {
+        expect(response.status).to.equal(200);
+        expect(response.body.status).to.equal('PUBLISHED');
+      });
+    });
+  });
+
+  describe('Publish an API', function () {
+    it('should publish an API as admin user', function () {
+      publishApi(ADMIN_USER, api).should(function (response) {
+        expect(response.status).to.equal(200);
+        expect(response.body.lifecycle_state).to.equal('PUBLISHED');
+        expect(response.body.state).to.equal('STOPPED');
+        expect(response.body.visibility).to.equal('PRIVATE');
+      });
+    });
+  });
+
+  describe('Start an API', function () {
+    it('should start an API as admin user', function () {
+      startApi(ADMIN_USER, api.id).should(function (response) {
+        expect(response.status).to.equal(204);
+      });
+    });
+  });
+
+  describe('Test newly created API', function () {
+    it(
+      'should get a positive response when calling the new API endpoint',
+      {
+        retries: 20,
+      },
+      function () {
+        cy.request({
+          url: `${Cypress.env('gatewayServer')}${api.context_path}?teststring=${api.id}`,
+          retryOnStatusCodeFailure: true,
+        }).should((response) => {
+          expect(response.body.query_params.teststring).to.be.equal(api.id);
+          expect(response.status).to.be.equal(200);
+        });
+      },
+    );
+  });
+
+  describe('Stop an API', function () {
+    it('should delete an API as admin user', function () {
+      stopApi(ADMIN_USER, api.id).its('status').should('equal', 204);
+    });
+  });
+
+  describe('Delete a plan', function () {
+    it('should delete a plan as admin user', function () {
+      deletePlan(ADMIN_USER, api.id, plan.id).its('status').should('equal', 204);
+    });
+  });
+
+  describe('Delete an API', function () {
+    it('should fail to delete an API as low permission user', function () {
+      deleteApi(LOW_PERMISSION_USER, api.id).should((response) => {
+        expect(response.status).to.be.equal(403);
+        expect(response.body.message).to.be.equal('You do not have sufficient rights to access this resource');
+      });
+    });
+
+    it('should delete an API as admin user', function () {
+      deleteApi(ADMIN_USER, api.id).its('status').should('equal', 204);
+    });
+
+    it('should fail to delete a non-existing API as admin user', function () {
+      deleteApi(ADMIN_USER, api.id).its('status').should('equal', 404);
+    });
+  });
+});
---
+++ gravitee-apim-cypress/cypress/integration/rest-api/apis/plan-tests.ts
@@ -0,0 +1,86 @@
+/*
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { ADMIN_USER, API_PUBLISHER_USER, LOW_PERMISSION_USER } from 'fixtures/fakers/users/users';
+import { ApiFakers } from 'fixtures/fakers/apis';
+import { PlanFakers } from 'fixtures/fakers/plans';
+import { createApi, deleteApi, stopApi } from 'commands/management/api-management-commands';
+import { createPlan, deletePlan, publishPlan } from 'commands/management/api-plan-management-commands';
+import { SecurityType, NewPlanEntity } from '@model/plan';
+
+context('Plan tests', () => {
+  describe('Create a Plan', function () {
+    beforeEach(function () {
+      const fakeApi = ApiFakers.api();
+      createApi(ADMIN_USER, fakeApi, true).its('body.id').as('apiId');
+    });
+
+    afterEach(function () {
+      deletePlan(ADMIN_USER, this.apiId, this.planId).its('status').should('equal', 204);
+      deleteApi(ADMIN_USER, this.apiId).its('status').should('equal', 204);
+    });
+
+    it('should create a keyless plan as admin user', function () {
+      const fakePlan = PlanFakers.plan({ security: SecurityType.KEY_LESS });
+      createPlan(ADMIN_USER, this.apiId, fakePlan).should(function (response) {
+        expect(response.status).to.equal(201);
+        expect(response.body).to.have.all.keys(
+          'name',
+          'id',
+          'description',
+          'validation',
+          'security',
+          'type',
+          'status',
+          'api',
+          'order',
+          'created_at',
+          'updated_at',
+          'paths',
+          'flows',
+          'comment_required',
+        );
+        expect(response.body.security).to.equal('KEY_LESS');
+        expect(response.body.status).to.equal('STAGING');
+        cy.wrap(response.body.id).as('planId');
+      });
+    });
+  });
+
+  describe('Publish plan', function () {
+    beforeEach(function () {
+      const fakeApi = ApiFakers.api();
+      const fakePlan = PlanFakers.plan();
+      createApi(ADMIN_USER, fakeApi).should(function (response) {
+        expect(response.status).to.equal(201);
+        cy.wrap(response.body.id).as('apiId');
+        createPlan(ADMIN_USER, response.body.id, fakePlan, true).its('body.id').as('planId');
+      });
+    });
+
+    afterEach(function () {
+      deletePlan(ADMIN_USER, this.apiId, this.planId).its('status').should('equal', 204);
+      deleteApi(ADMIN_USER, this.apiId).its('status').should('equal', 204);
+    });
+
+    it('should publish a plan as admin user', function () {
+      publishPlan(ADMIN_USER, this.apiId, this.planId).should(function (response) {
+        expect(response.status).to.equal(200);
+        expect(response.body.status).to.equal('PUBLISHED');
+        cy.wrap(response.body.id).as('planId');
+      });
+    });
+  });
+});
