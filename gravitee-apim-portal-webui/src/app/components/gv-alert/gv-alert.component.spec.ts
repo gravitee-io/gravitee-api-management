c2e645110900ff37494cd0d29d4278063d54e610
Thibaud Av <thibaud.avenier@gmail.com>
2021-12-28 13:49:13 +0100

    feat(portal): add description filed into alert form

--- gravitee-apim-portal-webui/src/app/components/gv-alert/gv-alert.component.spec.ts
+++ gravitee-apim-portal-webui/src/app/components/gv-alert/gv-alert.component.spec.ts
@@ -13,33 +13,86 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { createComponentFactory, mockProvider, Spectator } from '@ngneat/spectator';
+import { createComponentFactory, mockProvider, Spectator, createRoutingFactory } from '@ngneat/spectator';
 import { CurrentUserService } from '../../services/current-user.service';
 import { TranslateTestingModule } from '../../test/translate-testing-module';
 
-import { GvAlertComponent } from './gv-alert.component';
+import { AlertMode, GvAlertComponent } from './gv-alert.component';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 describe('GvAlertComponent', () => {
-  const createComponent = createComponentFactory({
+  const createComponent = createRoutingFactory({
     component: GvAlertComponent,
     schemas: [CUSTOM_ELEMENTS_SCHEMA],
     imports: [RouterTestingModule, TranslateTestingModule, FormsModule, ReactiveFormsModule, HttpClientTestingModule],
     providers: [mockProvider(CurrentUserService)],
+    data: { application: { id: 'appId', name: 'Admin Default application' } },
   });
 
   let spectator: Spectator<GvAlertComponent>;
-  let component;
+  let component: GvAlertComponent;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
     spectator = createComponent();
     component = spectator.component;
+    httpTestingController = spectator.inject(HttpTestingController);
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  afterEach(() => {
+    httpTestingController.verify();
   });
+
+  it('should create alert', () => {
+    spectator.detectChanges();
+    expectGetApplicationPermission();
+
+    component.mode = AlertMode.CREATION;
+    component.status = {
+      available_plugins: 1,
+      enabled: true,
+    };
+    component.alert = {
+      id: 'c8b67a64-ba8a-453e-b67a-64ba8a553e83',
+      enabled: true,
+      type: 'RESPONSE_TIME',
+      description: 'Yo ',
+      response_time: 1,
+      duration: 1,
+      time_unit: 'MINUTES',
+    };
+
+    component.alertForm.controls.type.setValue('STATUS');
+    component.alertForm.controls.duration.setValue(1);
+    component.alertForm.controls.timeUnit.setValue('MINUTES');
+    component.alertForm.controls.description.setValue('My description');
+
+    component.addAlert();
+
+    const req = httpTestingController.expectOne({
+      method: 'POST',
+      url: 'http://localhost:8083/portal/environments/DEFAULT/applications/appId/alerts',
+    });
+    expect(req.request.body).toEqual({
+      description: 'My description',
+      duration: 1,
+      enabled: true,
+      status_code: '4xx',
+      status_percent: '1',
+      time_unit: 'MINUTES',
+      type: 'STATUS',
+    });
+  });
+
+  function expectGetApplicationPermission() {
+    httpTestingController
+      .expectOne({ method: 'GET', url: 'http://localhost:8083/portal/environments/DEFAULT/permissions?applicationId=appId' })
+      .flush({
+        SUBSCRIPTION: ['R', 'C', 'D', 'U'],
+        ALERT: ['R', 'C', 'D', 'U'],
+      });
+  }
 });
