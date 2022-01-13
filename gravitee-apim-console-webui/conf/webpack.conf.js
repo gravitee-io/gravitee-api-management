9b653006c15b2ca932f84e67a9ad66e8ea97daab
Thibaud Av <thibaud.avenier@gmail.com>
2022-01-13 13:58:21 +0100

    feat: use new design tokens from ui-particles
    - bump ui-particles
    - manages the breaking changes
    - add new Inter & Fira Mono fonts from google fonts

--- gravitee-apim-console-webui/conf/webpack.conf.js
+++ gravitee-apim-console-webui/conf/webpack.conf.js
@@ -114,6 +114,10 @@ module.exports = {
           to: 'icons',
         },
         {
+          from: './node_modules/@gravitee/ui-particles-angular/assets',
+          to: 'assets',
+        },
+        {
           from: './src/assets',
           to: 'assets',
         },
