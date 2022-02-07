29aeb1d343bf93825f18a414ba09fdfa41f71891
Thibaud Av <thibaud.avenier@gmail.com>
2022-02-07 16:10:35 +0100

    chore(console): fix storybook to have right mat style and ui-particles assets

--- gravitee-apim-console-webui/.storybook/preview.ts
+++ gravitee-apim-console-webui/.storybook/preview.ts
@@ -3,7 +3,7 @@ import '../src/index.scss';
 import { withDesign } from 'storybook-addon-designs';
 
 // Set material classes to the Storybook root div
-window.document.body.classList.add('mat');
+window.document.body.parentElement.classList.add('mat');
 window.document.body.classList.add('mat-typography');
 window.document.body.classList.add('mat-app-background');
 
