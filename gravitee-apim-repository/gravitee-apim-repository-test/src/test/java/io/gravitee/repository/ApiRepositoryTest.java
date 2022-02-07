7741473d60a3b003fc7d02ebc8f8319022500135
Guillaume Cusnieux <guillaume.cusnieux@graviteesource.com>
2022-02-07 11:58:56 +0100

    fix(repository): be aware that api criteria may not have a group or label
    gravitee-io/issues#7060

--- gravitee-apim-repository/gravitee-apim-repository-test/src/test/java/io/gravitee/repository/ApiRepositoryTest.java
+++ gravitee-apim-repository/gravitee-apim-repository-test/src/test/java/io/gravitee/repository/ApiRepositoryTest.java
@@ -222,12 +222,13 @@ public class ApiRepositoryTest extends AbstractRepositoryTest {
     public void shouldFindIdsWithMultipleApiCriteria() {
         List<String> apis = apiRepository.searchIds(
             new ApiCriteria.Builder().ids("api-to-delete", "api-to-update", "unknown").build(),
-            new ApiCriteria.Builder().environments(Arrays.asList("DEV", "DEVS")).build()
+            new ApiCriteria.Builder().environments(Arrays.asList("DEV", "DEVS")).build(),
+            new ApiCriteria.Builder().groups("api-group", "unknown").build()
         );
         assertNotNull(apis);
         assertFalse(apis.isEmpty());
-        assertEquals(3, apis.size());
-        assertTrue(apis.containsAll(asList("api-to-delete", "api-to-update", "big-name")));
+        assertEquals(4, apis.size());
+        assertTrue(apis.containsAll(asList("api-to-delete", "api-to-update", "big-name", "grouped-api")));
     }
 
     @Test(expected = IllegalStateException.class)
