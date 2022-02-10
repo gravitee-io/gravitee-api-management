7479ea070fbd76faaf0f42df4593617188e1d3fb
Guillaume Cusnieux <guillaume.cusnieux@graviteesource.com>
2022-02-10 16:05:44 +0100

    feat(console): create empty debug inspector component
    gravitee-io/issues#6765

---
+++ gravitee-apim-console-webui/src/management/api/policy-studio/debug/components/policy-studio-debug-inspector/policy-studio-debug-inspector.component.stories.ts
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Meta, moduleMetadata } from '@storybook/angular';
+import { MatIconModule } from '@angular/material/icon';
+import { Story } from '@storybook/angular/dist/ts3.9/client/preview/types-7-0';
+import '@gravitee/ui-components/wc/gv-code';
+import '@gravitee/ui-components/wc/gv-icon';
+import { GioIconsModule } from '@gravitee/ui-particles-angular';
+
+import { PolicyStudioDebugInspectorComponent } from './policy-studio-debug-inspector.component';
+
+export default {
+  title: 'APIM / Policy Studio / Debug / Components / Inspector ',
+  component: PolicyStudioDebugInspectorComponent,
+  decorators: [
+    moduleMetadata({
+      declarations: [PolicyStudioDebugInspectorComponent],
+      imports: [CommonModule, BrowserAnimationsModule, MatIconModule, GioIconsModule],
+    }),
+  ],
+  render: () => ({
+    template: `
+    <policy-studio-debug-inspector>
+    </policy-studio-debug-inspector>
+    `,
+  }),
+} as Meta;
+
+export const Empty: Story = {};
