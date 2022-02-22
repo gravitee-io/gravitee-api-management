e077611ba9683b5d816039d1b2a5d6e4fdaa370b
Antoine CORDIER <antoine.cordier@graviteesource.com>
2022-02-22 10:29:18 +0100

    fix: admin role check in transfer ownerwhip component

--- gravitee-apim-console-webui/src/management/api/apiAdmin.controller.ts
+++ gravitee-apim-console-webui/src/management/api/apiAdmin.controller.ts
@@ -230,7 +230,11 @@ class ApiAdminController {
   }
 
   canTransferOwnership(): boolean {
-    return this.UserService.currentUser.isAdmin() || this.UserService.isApiPrimaryOwner(this.api, this.resolvedApiGroups);
+    return this.isAdmin() || this.UserService.isApiPrimaryOwner(this.api, this.resolvedApiGroups);
+  }
+
+  isAdmin(): boolean {
+    return this.UserService.currentUser.roles['ORGANIZATION'] === 'ADMIN' || this.UserService.currentUser.roles['ENVIRONMENT'] === 'ADMIN';
   }
 
   isRequestForChanges(): boolean {
