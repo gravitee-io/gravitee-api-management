868831f828e4bc0ae2164017cc12ff564294d318
Marc <marc.cambier@graviteesource.com>
2022-03-08 08:33:40 +0100

    feat: forbid usage of subscription's apiKey endpoints on shared Api Key mode
    When an application uses the shared ApiKey mode,
    Api key update actions (renewal, revoke, update, reactivate) have to be done using the Application's ApiKey endpoint, cause it will impact multiple subscriptions.

--- gravitee-apim-rest-api/gravitee-apim-rest-api-management/gravitee-apim-rest-api-management-rest/src/test/java/io/gravitee/rest/api/management/rest/resource/ApiSubscriptionApikeyResourceTest.java
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-management/gravitee-apim-rest-api-management-rest/src/test/java/io/gravitee/rest/api/management/rest/resource/ApiSubscriptionApikeyResourceTest.java
@@ -21,10 +21,11 @@ import static org.mockito.Mockito.*;
 
 import io.gravitee.common.http.HttpStatusCode;
 import io.gravitee.rest.api.model.ApiKeyEntity;
+import io.gravitee.rest.api.model.ApiKeyMode;
+import io.gravitee.rest.api.model.ApplicationEntity;
 import io.gravitee.rest.api.model.SubscriptionEntity;
 import io.gravitee.rest.api.service.common.GraviteeContext;
 import io.gravitee.rest.api.service.exceptions.TechnicalManagementException;
-import java.util.List;
 import java.util.Set;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.Response;
@@ -40,6 +41,7 @@ public class ApiSubscriptionApikeyResourceTest extends AbstractResourceTest {
     private static final String API_ID = "my-api";
     private static final String APIKEY_ID = "my-apikey";
     private static final String SUBSCRIPTION_ID = "my-subscription";
+    private static final String APPLICATION_ID = "my-application";
 
     @Override
     protected String contextPath() {
@@ -48,7 +50,7 @@ public class ApiSubscriptionApikeyResourceTest extends AbstractResourceTest {
 
     @Before
     public void setUp() {
-        reset(apiKeyService);
+        reset(apiKeyService, subscriptionService, applicationService);
         GraviteeContext.cleanContext();
     }
 
@@ -59,6 +61,8 @@ public class ApiSubscriptionApikeyResourceTest extends AbstractResourceTest {
 
     @Test
     public void delete_should_call_revoke_service_and_return_http_204() {
+        mockExistingSubscriptionWithApplication(ApiKeyMode.EXCLUSIVE);
+
         ApiKeyEntity existingApiKey = new ApiKeyEntity();
         SubscriptionEntity subscription = new SubscriptionEntity();
         subscription.setId(SUBSCRIPTION_ID);
@@ -73,6 +77,8 @@ public class ApiSubscriptionApikeyResourceTest extends AbstractResourceTest {
 
     @Test
     public void delete_should_return_http_400_when_apikey_on_another_subscription() {
+        mockExistingSubscriptionWithApplication(ApiKeyMode.EXCLUSIVE);
+
         ApiKeyEntity existingApiKey = new ApiKeyEntity();
         SubscriptionEntity subscription = new SubscriptionEntity();
         subscription.setId("another-subscription");
@@ -88,6 +94,8 @@ public class ApiSubscriptionApikeyResourceTest extends AbstractResourceTest {
 
     @Test
     public void delete_should_return_http_500_on_exception() {
+        mockExistingSubscriptionWithApplication(ApiKeyMode.EXCLUSIVE);
+
         doThrow(TechnicalManagementException.class).when(apiKeyService).revoke(any(String.class), any(Boolean.class));
 
         Response response = envTarget().request().delete();
@@ -97,6 +105,8 @@ public class ApiSubscriptionApikeyResourceTest extends AbstractResourceTest {
 
     @Test
     public void put_should_return_http_400_if_entity_id_does_not_match() {
+        mockExistingSubscriptionWithApplication(ApiKeyMode.EXCLUSIVE);
+
         ApiKeyEntity apiKey = new ApiKeyEntity();
         apiKey.setId("another-api-key-id");
 
@@ -107,6 +117,8 @@ public class ApiSubscriptionApikeyResourceTest extends AbstractResourceTest {
 
     @Test
     public void put_should_call_service_update_and_return_http_200() {
+        mockExistingSubscriptionWithApplication(ApiKeyMode.EXCLUSIVE);
+
         ApiKeyEntity apiKey = new ApiKeyEntity();
         apiKey.setId(APIKEY_ID);
 
@@ -118,6 +130,8 @@ public class ApiSubscriptionApikeyResourceTest extends AbstractResourceTest {
 
     @Test
     public void put_should_return_http_500_on_exception() {
+        mockExistingSubscriptionWithApplication(ApiKeyMode.EXCLUSIVE);
+
         ApiKeyEntity apiKey = new ApiKeyEntity();
         apiKey.setId(APIKEY_ID);
 
@@ -129,7 +143,31 @@ public class ApiSubscriptionApikeyResourceTest extends AbstractResourceTest {
     }
 
     @Test
+    public void put_should_return_http_404_when_subscription_not_found() {
+        ApiKeyEntity apiKey = new ApiKeyEntity();
+        apiKey.setId(APIKEY_ID);
+
+        Response response = envTarget().request().put(Entity.json(apiKey));
+
+        assertEquals(HttpStatusCode.NOT_FOUND_404, response.getStatus());
+    }
+
+    @Test
+    public void put_should_return_http_400_when_its_a_shared_api_key() {
+        mockExistingSubscriptionWithApplication(ApiKeyMode.SHARED);
+
+        ApiKeyEntity apiKey = new ApiKeyEntity();
+        apiKey.setId(APIKEY_ID);
+
+        Response response = envTarget().request().put(Entity.json(apiKey));
+
+        assertEquals(HttpStatusCode.BAD_REQUEST_400, response.getStatus());
+    }
+
+    @Test
     public void post_on_reactivate_should_call_reactivate_service_and_return_http_200() {
+        mockExistingSubscriptionWithApplication(ApiKeyMode.EXCLUSIVE);
+
         ApiKeyEntity existingApiKey = new ApiKeyEntity();
         SubscriptionEntity subscription = new SubscriptionEntity();
         subscription.setId(SUBSCRIPTION_ID);
@@ -144,6 +182,8 @@ public class ApiSubscriptionApikeyResourceTest extends AbstractResourceTest {
 
     @Test
     public void post_on_reactivate_should_return_http_400_when_apikey_on_another_subscription() {
+        mockExistingSubscriptionWithApplication(ApiKeyMode.EXCLUSIVE);
+
         ApiKeyEntity existingApiKey = new ApiKeyEntity();
         SubscriptionEntity subscription = new SubscriptionEntity();
         subscription.setId("another-subscription");
@@ -155,5 +195,15 @@ public class ApiSubscriptionApikeyResourceTest extends AbstractResourceTest {
 
         verify(apiKeyService, never()).reactivate(any(ApiKeyEntity.class));
         assertEquals(HttpStatusCode.BAD_REQUEST_400, response.getStatus());
+    }
+
+    private void mockExistingSubscriptionWithApplication(ApiKeyMode apiKeyMode) {
+        SubscriptionEntity subscription = new SubscriptionEntity();
+        subscription.setApplication(APPLICATION_ID);
+        when(subscriptionService.findById(SUBSCRIPTION_ID)).thenReturn(subscription);
+
+        ApplicationEntity application = new ApplicationEntity();
+        application.setApiKeyMode(apiKeyMode);
+        when(applicationService.findById(GraviteeContext.getCurrentEnvironment(), APPLICATION_ID)).thenReturn(application);
     }
 }
