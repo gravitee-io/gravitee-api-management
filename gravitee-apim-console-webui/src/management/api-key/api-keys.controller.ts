a0094ad7bed13d22fb4ed1f3662a89c08db8c7f0
Marc <marc.cambier@graviteesource.com>
2022-03-08 18:07:54 +0100

    feat: manage shared API key in application subscriptions screen
    If there are shared API keys for the application,
    This add the shared API key management part at the bottom of the application subscriptions screen.
    
    It externalize the application API key list in a dedicated component,
    As it's used to manage the application API keys in :
    - the application subscriptions list screen (for shared API key)
    - the application subscription screen

---
+++ gravitee-apim-console-webui/src/management/api-key/api-keys.controller.ts
@@ -0,0 +1,114 @@
+/*
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { StateParams, StateService } from '@uirouter/core';
+import { Observable } from 'rxjs';
+
+import NotificationService from '../../services/notification.service';
+import ApplicationService from '../../services/application.service';
+
+class ApiKeysController {
+  private subscription: any;
+  private keys: any[];
+  private application: any;
+  private listLabel: string;
+  private listEvent: Observable<void>;
+  private backStateParams: StateParams;
+
+  constructor(
+    private $mdDialog: angular.material.IDialogService,
+    private NotificationService: NotificationService,
+    private ApplicationService: ApplicationService,
+    private $state: StateService,
+  ) {
+    'ngInject';
+    this.backStateParams = $state.params;
+  }
+
+  $onInit() {
+    this.listApiKeys();
+    if (this.listEvent) {
+      this.listEvent.subscribe(() => this.listApiKeys());
+    }
+  }
+
+  listApiKeys(): void {
+    this.ApplicationService.listApiKeys(this.application, this.subscription).then((response) => {
+      this.keys = response.data;
+    });
+  }
+
+  renewApiKey(): void {
+    this.$mdDialog
+      .show({
+        controller: 'DialogConfirmController',
+        controllerAs: 'ctrl',
+        template: require('../../components/dialog/confirmWarning.dialog.html'),
+        clickOutsideToClose: true,
+        locals: {
+          title: 'Are you sure you want to renew your API Key?',
+          msg: 'Your previous API Key will be no longer valid in 2 hours!',
+          confirmButton: 'Renew',
+        },
+      })
+      .then((response) => {
+        if (response) {
+          this.ApplicationService.renewApiKey(this.application, this.subscription).then(() => {
+            this.NotificationService.show('A new API Key has been generated');
+            this.listApiKeys();
+          });
+        }
+      });
+  }
+
+  isValid(key): boolean {
+    return !key.revoked && !key.expired;
+  }
+
+  // keys are editable if subscription is accepted or if application uses shared API key mode
+  areKeysEditable(): boolean {
+    return (this.subscription && this.subscription.status === 'ACCEPTED') || this.application.api_key_mode === 'SHARED';
+  }
+
+  revokeApiKey(apiKey): void {
+    this.$mdDialog
+      .show({
+        controller: 'DialogConfirmController',
+        controllerAs: 'ctrl',
+        template: require('../../components/dialog/confirmWarning.dialog.html'),
+        clickOutsideToClose: true,
+        locals: {
+          title: "Are you sure you want to revoke API Key '" + apiKey.key + "'?",
+          confirmButton: 'Revoke',
+        },
+      })
+      .then((response) => {
+        if (response) {
+          this.ApplicationService.revokeApiKey(this.application, this.subscription, apiKey.id).then(() => {
+            this.NotificationService.show('API Key ' + apiKey.key + ' has been revoked!');
+            this.listApiKeys();
+          });
+        }
+      });
+  }
+
+  onCopyApiKeySuccess(e): void {
+    this.NotificationService.show('API Key has been copied to clipboard');
+    e.clearSelection();
+  }
+}
+
+export default ApiKeysController;
