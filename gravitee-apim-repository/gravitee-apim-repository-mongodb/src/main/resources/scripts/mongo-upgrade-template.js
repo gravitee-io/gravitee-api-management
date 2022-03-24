781a4d9e19c017013df8c7a402be7c1b1f915853
Antoine CORDIER <antoine.cordier@graviteesource.com>
2022-03-24 16:26:38 +0100

    chore: add mongo upgrade template to script dir

---
+++ gravitee-apim-repository/gravitee-apim-repository-mongodb/src/main/resources/scripts/mongo-upgrade-template.js
@@ -0,0 +1,7 @@
+print('Title of the upgrade operation');
+// Override this variable if you use prefix
+const prefix = "";
+
+// ... your instructions goes here
+// You can access a collection with
+db.getCollection(`${prefix}myCollection`)
