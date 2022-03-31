a3959a2d64dd6c79ea81f76e53120e0d609ecc95
Antoine CORDIER <antoine.cordier@graviteesource.com>
2022-03-31 14:01:01 +0200

    fix: build conditional logging expression properly
    The conditional logging expression computed by the condition editor
    was wrong, leading to error logs at the proxy level and condition
    not being honored.
    
    Additionally, the `[GET] /subscribers` endpoint was returning applications
    linked to closed subscriptions and the condition type input was wiped when
    hitting the `Add` button in the editor.
    
    see https://github.com/gravitee-io/issues/issues/7329

--- gravitee-apim-rest-api/gravitee-apim-rest-api-management/gravitee-apim-rest-api-management-rest/src/main/java/io/gravitee/rest/api/management/rest/resource/ApiSubscribersResource.java
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-management/gravitee-apim-rest-api-management-rest/src/main/java/io/gravitee/rest/api/management/rest/resource/ApiSubscribersResource.java
@@ -15,9 +15,12 @@
  */
 package io.gravitee.rest.api.management.rest.resource;
 
+import static io.gravitee.rest.api.model.SubscriptionStatus.*;
+
 import io.gravitee.common.http.MediaType;
 import io.gravitee.rest.api.model.ApplicationEntity;
 import io.gravitee.rest.api.model.SubscriptionEntity;
+import io.gravitee.rest.api.model.SubscriptionStatus;
 import io.gravitee.rest.api.model.permissions.RolePermission;
 import io.gravitee.rest.api.model.permissions.RolePermissionAction;
 import io.gravitee.rest.api.model.subscription.SubscriptionQuery;
@@ -27,6 +30,7 @@ import io.gravitee.rest.api.service.common.GraviteeContext;
 import io.gravitee.rest.api.service.exceptions.ForbiddenAccessException;
 import io.swagger.annotations.*;
 import java.util.Collection;
+import java.util.Set;
 import java.util.stream.Collectors;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
@@ -80,8 +84,10 @@ public class ApiSubscribersResource extends AbstractResource {
 
         SubscriptionQuery subscriptionQuery = new SubscriptionQuery();
         subscriptionQuery.setApi(api);
+        subscriptionQuery.setStatuses(Set.of(PENDING, ACCEPTED));
 
         Collection<SubscriptionEntity> subscriptions = subscriptionService.search(subscriptionQuery);
+
         return subscriptions
             .stream()
             .map(SubscriptionEntity::getApplication)
