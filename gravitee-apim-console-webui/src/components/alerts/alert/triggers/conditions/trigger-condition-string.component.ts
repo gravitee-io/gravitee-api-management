af34fbc09a57bb3403939279ff4ce8c9561939a1
Marc <marc.cambier@graviteesource.com>
2022-04-12 08:50:43 +0200

    fix: fix 'not equals' alert filter condition field type
    not_equals condition should behave like equals condition :
    - When the condition contains values, display a list of values, let the user select one of them
    - When the condition doesn't contain any value, display an input field, let the user enter his own value

--- gravitee-apim-console-webui/src/components/alerts/alert/triggers/conditions/trigger-condition-string.component.ts
+++ gravitee-apim-console-webui/src/components/alerts/alert/triggers/conditions/trigger-condition-string.component.ts
@@ -50,7 +50,7 @@ const AlertTriggerConditionStringComponent: ng.IComponentOptions = {
     };
 
     this.displaySelect = () => {
-      return (this.values !== undefined && this.condition.operator === 'EQUALS') || this.condition.operator === 'NOT_EQUALS';
+      return this.values !== undefined && (this.condition.operator === 'EQUALS' || this.condition.operator === 'NOT_EQUALS');
     };
   },
 };
