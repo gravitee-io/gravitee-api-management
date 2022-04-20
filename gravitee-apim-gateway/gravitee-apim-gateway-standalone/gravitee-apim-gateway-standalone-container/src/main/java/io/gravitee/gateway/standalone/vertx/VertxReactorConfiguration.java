b62a05f5a55aaa6d40ced41aa991b4a1f1368df1
Guillaume Lamirand <guillaume.lamirand@graviteesource.com>
2022-04-20 11:37:30 +0200

    format: format code and headers

--- gravitee-apim-gateway/gravitee-apim-gateway-standalone/gravitee-apim-gateway-standalone-container/src/main/java/io/gravitee/gateway/standalone/vertx/VertxReactorConfiguration.java
+++ gravitee-apim-gateway/gravitee-apim-gateway-standalone/gravitee-apim-gateway-standalone-container/src/main/java/io/gravitee/gateway/standalone/vertx/VertxReactorConfiguration.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
- * <p>
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -44,18 +44,20 @@ public class VertxReactorConfiguration {
     @Bean("gatewayHttpServer")
     //    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
     public VertxHttpServerFactory vertxHttpServerFactory(
-            Vertx vertx,
-            @Qualifier("httpServerConfiguration") HttpServerConfiguration httpServerConfiguration,
-            KeyStoreLoaderManager keyStoreLoaderManager
-                                                        ) {
+        Vertx vertx,
+        @Qualifier("httpServerConfiguration") HttpServerConfiguration httpServerConfiguration,
+        KeyStoreLoaderManager keyStoreLoaderManager
+    ) {
         return new VertxHttpServerFactory(vertx, httpServerConfiguration, keyStoreLoaderManager);
     }
 
     @Bean
     @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
-    public HttpProtocolVerticle graviteeVerticle(@Qualifier("gatewayHttpServer") HttpServer httpServer,
-                                                 @Qualifier("httpServerConfiguration") HttpServerConfiguration httpServerConfiguration,
-                                                 @Qualifier("httpRequestDispatcher") HttpRequestDispatcher requestDispatcher) {
+    public HttpProtocolVerticle graviteeVerticle(
+        @Qualifier("gatewayHttpServer") HttpServer httpServer,
+        @Qualifier("httpServerConfiguration") HttpServerConfiguration httpServerConfiguration,
+        @Qualifier("httpRequestDispatcher") HttpRequestDispatcher requestDispatcher
+    ) {
         return new HttpProtocolVerticle(httpServer, httpServerConfiguration, requestDispatcher);
     }
 
