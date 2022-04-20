5206650e0c4a17cbd02f8de9659c8d11f9faae6c
Guillaume Cusnieux <guillaume.cusnieux@graviteesource.com>
2022-04-20 17:00:34 +0200

    test(e2e): setup jest for API integration tests
    gravitee-io/issues#7528

---
+++ gravitee-apim-cypress/lib/configuration.ts
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { Configuration } from './management-webclient-sdk/src/lib';
+import 'dotenv/config';
+import fetchApi from 'node-fetch';
+import { BasicAuthentication } from '@model/users';
+
+export const ADMIN_USER = {
+  username: process.env.ADMIN_USERNAME,
+  password: process.env.ADMIN_PASSWORD,
+};
+
+export const API_USER = {
+  username: process.env.API_USERNAME,
+  password: process.env.API_PASSWORD,
+};
+
+export const APP_USER = {
+  username: process.env.APP_USERNAME,
+  password: process.env.APP_PASSWORD,
+};
+
+export const SIMPLE_USER = {
+  username: process.env.SIMPLE_USERNAME,
+  password: process.env.SIMPLE_PASSWORD,
+};
+
+export const buildConfiguration = (auth: BasicAuthentication = ADMIN_USER) => {
+  return new Configuration({
+    basePath: process.env.MANAGEMENT_BASE_PATH,
+    fetchApi,
+    ...auth,
+  });
+};
