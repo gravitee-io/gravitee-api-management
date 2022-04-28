6ba501878c1fa9aec83419cc225aa1494a06e101
Guillaume Cusnieux <guillaume.cusnieux@graviteesource.com>
2022-04-28 08:14:22 +0200

    chore(e2e): generate client for portal api

---
+++ gravitee-apim-e2e/lib/portal-webclient-sdk/src/lib/apis/index.ts
@@ -0,0 +1,11 @@
+
+export * from './ApiApi';
+export * from './ApplicationApi';
+export * from './AuthenticationApi';
+export * from './GroupApi';
+export * from './NotifiersApi';
+export * from './PermissionsApi';
+export * from './PortalApi';
+export * from './SubscriptionApi';
+export * from './UserApi';
+export * from './UsersApi';
