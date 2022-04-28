f6a2c5925c6e935ec62363f1ae71dd882c78f54c
Guillaume Lamirand <guillaume.lamirand@graviteesource.com>
2022-04-28 10:53:12 +0200

    fix: remove execution mode from new entity, and list item

--- gravitee-apim-rest-api/gravitee-apim-rest-api-model/src/main/java/io/gravitee/rest/api/model/api/ApiQuery.java
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-model/src/main/java/io/gravitee/rest/api/model/api/ApiQuery.java
@@ -34,7 +34,6 @@ public class ApiQuery {
     private String label;
     private String state;
     private Visibility visibility;
-    private ExecutionMode executionMode;
     private String version;
     private String name;
     private String tag;
@@ -201,13 +200,5 @@ public class ApiQuery {
             crossId +
             '}'
         );
-    }
-
-    public ExecutionMode getExecutionMode() {
-        return executionMode;
-    }
-
-    public void setExecutionMode(final ExecutionMode executionMode) {
-        this.executionMode = executionMode;
     }
 }
