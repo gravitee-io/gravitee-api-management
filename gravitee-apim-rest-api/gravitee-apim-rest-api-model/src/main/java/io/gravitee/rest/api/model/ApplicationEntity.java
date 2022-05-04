23bdddb10948e91e3ead9d6dcd74e7e51880cc22
Marc <marc.cambier@graviteesource.com>
2022-05-04 07:29:32 +0200

    refactor: use lombok in ApplicationEntity

--- gravitee-apim-rest-api/gravitee-apim-rest-api-model/src/main/java/io/gravitee/rest/api/model/ApplicationEntity.java
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-model/src/main/java/io/gravitee/rest/api/model/ApplicationEntity.java
@@ -19,17 +19,25 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import io.gravitee.rest.api.model.application.ApplicationSettings;
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.Date;
-import java.util.Objects;
 import java.util.Set;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 /**
  * @author David BRASSELY (david.brassely at graviteesource.com)
  * @author Nicolas GERAUD (nicolas.geraud at graviteesource.com)
  * @author GraviteeSource Team
  */
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class ApplicationEntity {
 
     @Schema(description = "Application's uuid.", example = "00f8c9e7-78fc-4907-b8c9-e778fc790750")
+    @EqualsAndHashCode.Include
     private String id;
 
     @Schema(description = "Application's name. Duplicate names can exists.", example = "My App")
@@ -79,151 +87,7 @@ public class ApplicationEntity {
 
     private String background;
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public Date getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Date createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Date getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(Date updatedAt) {
-        this.updatedAt = updatedAt;
-    }
-
-    public PrimaryOwnerEntity getPrimaryOwner() {
-        return primaryOwner;
-    }
-
-    public void setPrimaryOwner(PrimaryOwnerEntity primaryOwner) {
-        this.primaryOwner = primaryOwner;
-    }
-
-    public Set<String> getGroups() {
-        return groups;
-    }
-
-    public void setGroups(Set<String> groups) {
-        this.groups = groups;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public ApplicationSettings getSettings() {
-        return settings;
-    }
-
-    public void setSettings(ApplicationSettings settings) {
-        this.settings = settings;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public String getPicture() {
-        return picture;
-    }
-
-    public void setPicture(String picture) {
-        this.picture = picture;
-    }
-
-    public boolean isDisableMembershipNotifications() {
-        return disableMembershipNotifications;
-    }
-
-    public void setDisableMembershipNotifications(boolean disableMembershipNotifications) {
-        this.disableMembershipNotifications = disableMembershipNotifications;
-    }
-
-    public String getBackground() {
-        return background;
-    }
-
-    public void setBackground(String background) {
-        this.background = background;
-    }
-
-    public String getDomain() {
-        return domain;
-    }
-
-    public void setDomain(String domain) {
-        this.domain = domain;
-    }
-
-    public ApiKeyMode getApiKeyMode() {
-        return apiKeyMode;
-    }
-
     public boolean hasApiKeySharedMode() {
         return apiKeyMode == ApiKeyMode.SHARED;
-    }
-
-    public void setApiKeyMode(ApiKeyMode apiKeyMode) {
-        this.apiKeyMode = apiKeyMode;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        ApplicationEntity that = (ApplicationEntity) o;
-        return Objects.equals(id, that.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
-
-    @Override
-    public String toString() {
-        final StringBuilder sb = new StringBuilder("Application{");
-        sb.append("id='").append(id).append('\'');
-        sb.append(", name='").append(name).append('\'');
-        sb.append(", createdAt=").append(createdAt).append('\'');
-        sb.append(", disableMembershipNotifications='").append(disableMembershipNotifications);
-        sb.append('}');
-        return sb.toString();
     }
 }
