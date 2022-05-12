3bd13a7938528d66836314173d328638f273f95e
Guillaume Lamirand <guillaume.lamirand@graviteesource.com>
2022-05-12 17:02:42 +0200

    feat(processor): migrate platform processors

---
+++ gravitee-apim-gateway/gravitee-apim-gateway-reactor/src/main/java/io/gravitee/gateway/reactive/reactor/processor/transaction/TransactionProcessorFactory.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.gateway.reactive.reactor.processor.transaction;
+
+import io.gravitee.gateway.reactive.core.processor.Processor;
+import org.springframework.beans.factory.annotation.Value;
+
+/**
+ * @author David BRASSELY (david.brassely at graviteesource.com)
+ * @author Guillaume LAMIRAND (guillaume.lamirand at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+public class TransactionProcessorFactory {
+
+    static final String DEFAULT_TRANSACTION_ID_HEADER = "X-Gravitee-Transaction-Id";
+    static final String DEFAULT_REQUEST_ID_HEADER = "X-Gravitee-Request-Id";
+
+    private final String transactionHeader;
+    private final String requestHeader;
+
+    public TransactionProcessorFactory(
+        @Value("${handlers.request.transaction.header:" + DEFAULT_TRANSACTION_ID_HEADER + "}") String transactionHeader,
+        @Value("${handlers.request.request.header:" + DEFAULT_REQUEST_ID_HEADER + "}") String requestHeader
+    ) {
+        this.transactionHeader = transactionHeader;
+        this.requestHeader = requestHeader;
+    }
+
+    public TransactionProcessor create() {
+        return new TransactionProcessor(transactionHeader, requestHeader);
+    }
+}
