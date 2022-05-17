15cf5f639a108f805cb2065fc8c19908abe092b3
Guillaume Lamirand <guillaume.lamirand@graviteesource.com>
2022-05-17 12:00:45 +0200

    fix(jupiter): few change after review

--- gravitee-apim-gateway/gravitee-apim-gateway-handlers/gravitee-apim-gateway-handlers-api/src/main/java/io/gravitee/gateway/reactive/handlers/api/processor/error/SimpleFailureProcessor.java
+++ gravitee-apim-gateway/gravitee-apim-gateway-handlers/gravitee-apim-gateway-handlers-api/src/main/java/io/gravitee/gateway/reactive/handlers/api/processor/error/SimpleFailureProcessor.java
@@ -15,31 +15,6 @@
  */
 package io.gravitee.gateway.reactive.handlers.api.processor.error;
 
-import static io.gravitee.gateway.reactive.api.context.ExecutionContext.ATTR_INTERNAL_EXECUTION_FAILURE;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import io.gravitee.common.http.HttpHeadersValues;
-import io.gravitee.common.http.MediaType;
-import io.gravitee.gateway.api.buffer.Buffer;
-import io.gravitee.gateway.api.http.HttpHeaderNames;
-import io.gravitee.gateway.api.processor.ProcessorFailure;
-import io.gravitee.gateway.handlers.api.processor.error.ProcessorFailureAsJson;
-import io.gravitee.gateway.reactive.api.ExecutionFailure;
-import io.gravitee.gateway.reactive.api.context.ExecutionContext;
-import io.gravitee.gateway.reactive.api.context.Request;
-import io.gravitee.gateway.reactive.api.context.RequestExecutionContext;
-import io.gravitee.gateway.reactive.api.context.Response;
-import io.gravitee.gateway.reactive.core.processor.Processor;
-import io.gravitee.gateway.reactive.handlers.api.processor.cors.CorsSimpleRequestProcessor;
-import io.gravitee.gateway.reactive.handlers.api.processor.forward.XForwardedPrefixProcessor;
-import io.netty.handler.codec.http.HttpResponseStatus;
-import io.reactivex.Completable;
-import io.reactivex.Maybe;
-import java.util.List;
-import org.bouncycastle.asn1.x509.Holder;
-
 /**
  * @author David BRASSELY (david.brassely at graviteesource.com)
  * @author Guillaume LAMIRAND (guillaume.lamirand at graviteesource.com)
