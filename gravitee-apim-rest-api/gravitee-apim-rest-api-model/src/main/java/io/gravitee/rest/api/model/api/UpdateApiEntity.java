2fe72b8abcba534aabc8f0675b757443feb5f192
Marc <marc.cambier@graviteesource.com>
2022-05-30 16:29:45 +0200

    refactor: use lombok for ApiEntity RollbackApiEntity and UpdateApiEntity

--- gravitee-apim-rest-api/gravitee-apim-rest-api-model/src/main/java/io/gravitee/rest/api/model/api/UpdateApiEntity.java
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-model/src/main/java/io/gravitee/rest/api/model/api/UpdateApiEntity.java
@@ -16,6 +16,7 @@
 package io.gravitee.rest.api.model.api;
 
 import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonSetter;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
@@ -29,10 +30,14 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.*;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
 
 /**
  * @author David BRASSELY (brasseld at gmail.com)
  */
+@Getter
+@Setter
 public class UpdateApiEntity {
 
     @Schema(description = "API's crossId. Identifies API across environments.", example = "00f8c9e7-78fc-4907-b8c9-e778fc790750")
@@ -142,66 +147,7 @@ public class UpdateApiEntity {
     @Schema(description = "the API background URL")
     private String backgroundUrl;
 
-    public Visibility getVisibility() {
-        return visibility;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setVisibility(Visibility visibility) {
-        this.visibility = visibility;
-    }
-
-    public String getVersion() {
-        return version;
-    }
-
-    public void setVersion(String version) {
-        this.version = version;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Proxy getProxy() {
-        return proxy;
-    }
-
-    public void setProxy(Proxy proxy) {
-        this.proxy = proxy;
-    }
-
-    public Map<String, List<Rule>> getPaths() {
-        return paths;
-    }
-
-    public void setPaths(Map<String, List<Rule>> paths) {
-        this.paths = paths;
-    }
-
-    public Services getServices() {
-        return services;
-    }
-
-    public void setServices(Services services) {
-        this.services = services;
-    }
-
-    public PropertiesEntity getProperties() {
-        return properties;
-    }
-
+    @JsonIgnore
     public void setProperties(PropertiesEntity properties) {
         this.properties = properties;
     }
@@ -218,157 +164,5 @@ public class UpdateApiEntity {
             return properties.getProperties();
         }
         return Collections.emptyList();
-    }
-
-    public Set<String> getTags() {
-        return tags;
-    }
-
-    public void setTags(Set<String> tags) {
-        this.tags = tags;
-    }
-
-    public String getPicture() {
-        return picture;
-    }
-
-    public void setPicture(String picture) {
-        this.picture = picture;
-    }
-
-    public List<Resource> getResources() {
-        return resources;
-    }
-
-    public void setResources(List<Resource> resources) {
-        this.resources = resources;
-    }
-
-    public Set<String> getCategories() {
-        return categories;
-    }
-
-    public void setCategories(Set<String> categories) {
-        this.categories = categories;
-    }
-
-    public List<String> getLabels() {
-        return labels;
-    }
-
-    public void setLabels(List<String> labels) {
-        this.labels = labels;
-    }
-
-    public Set<String> getGroups() {
-        return groups;
-    }
-
-    public void setGroups(Set<String> groups) {
-        this.groups = groups;
-    }
-
-    public Set<String> getPathMappings() {
-        return pathMappings;
-    }
-
-    public void setPathMappings(Set<String> pathMappings) {
-        this.pathMappings = pathMappings;
-    }
-
-    public Map<String, Map<String, ResponseTemplate>> getResponseTemplates() {
-        return responseTemplates;
-    }
-
-    public void setResponseTemplates(Map<String, Map<String, ResponseTemplate>> responseTemplates) {
-        this.responseTemplates = responseTemplates;
-    }
-
-    public List<ApiMetadataEntity> getMetadata() {
-        return metadata;
-    }
-
-    public void setMetadata(List<ApiMetadataEntity> metadata) {
-        this.metadata = metadata;
-    }
-
-    public ApiLifecycleState getLifecycleState() {
-        return lifecycleState;
-    }
-
-    public void setLifecycleState(ApiLifecycleState lifecycleState) {
-        this.lifecycleState = lifecycleState;
-    }
-
-    public boolean isDisableMembershipNotifications() {
-        return disableMembershipNotifications;
-    }
-
-    public void setDisableMembershipNotifications(boolean disableMembershipNotifications) {
-        this.disableMembershipNotifications = disableMembershipNotifications;
-    }
-
-    public String getBackground() {
-        return background;
-    }
-
-    public void setBackground(String background) {
-        this.background = background;
-    }
-
-    public String getBackgroundUrl() {
-        return backgroundUrl;
-    }
-
-    public void setBackgroundUrl(String backgroundUrl) {
-        this.backgroundUrl = backgroundUrl;
-    }
-
-    public String getPictureUrl() {
-        return pictureUrl;
-    }
-
-    public void setPictureUrl(String pictureUrl) {
-        this.pictureUrl = pictureUrl;
-    }
-
-    public List<Flow> getFlows() {
-        return flows;
-    }
-
-    public void setFlows(List<Flow> flows) {
-        this.flows = flows;
-    }
-
-    public Set<PlanEntity> getPlans() {
-        return plans;
-    }
-
-    public void setPlans(Set<PlanEntity> plans) {
-        this.plans = plans;
-    }
-
-    public String getGraviteeDefinitionVersion() {
-        return graviteeDefinitionVersion;
-    }
-
-    public void setGraviteeDefinitionVersion(String graviteeDefinitionVersion) {
-        this.graviteeDefinitionVersion = graviteeDefinitionVersion;
-    }
-
-    public FlowMode getFlowMode() {
-        return flowMode;
-    }
-
-    public void setFlowMode(FlowMode flowMode) {
-        this.flowMode = flowMode;
-    }
-
-    public String getCrossId() {
-        return crossId;
-    }
-
-    public void setCrossId(String crossId) {
-        this.crossId = crossId;
     }
 }
