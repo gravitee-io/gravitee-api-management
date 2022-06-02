3ad45d7bb008d59a996bcca79530dc0dc8bbe0c0
Thibaud Av <thibaud.avenier@gmail.com>
2022-06-02 16:03:51 +0200

    feat(console): init new ApiCreationGetStartedComponent

---
+++ gravitee-apim-console-webui/src/management/api/creation/api-creation-get-started.component.spec.ts
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpTestingController } from '@angular/common/http/testing';
+import { InteractivityChecker } from '@angular/cdk/a11y';
+import { ApiCreationGetStartedComponent } from './api-creation-get-started.component';
+import { GioHttpTestingModule } from '../../../shared/testing';
+import { ApiCreationModule } from './api-creation.module';
+
+describe('ApiCreationGetStartedComponent', () => {
+  let fixture: ComponentFixture<ApiCreationGetStartedComponent>;
+  let component: ApiCreationGetStartedComponent;
+  let httpTestingController: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [NoopAnimationsModule, GioHttpTestingModule, ApiCreationModule],
+    })
+      .overrideProvider(InteractivityChecker, {
+        useValue: {
+          isFocusable: () => true, // This traps focus checks and so avoid warnings when dealing with
+        },
+      })
+      .compileComponents();
+
+    fixture = TestBed.createComponent(ApiCreationGetStartedComponent);
+    component = fixture.componentInstance;
+
+    httpTestingController = TestBed.inject(HttpTestingController);
+
+    fixture.detectChanges();
+  });
+
+  it('should work', async () => {
+    expect(component).toBeTruthy();
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
+  });
+});
