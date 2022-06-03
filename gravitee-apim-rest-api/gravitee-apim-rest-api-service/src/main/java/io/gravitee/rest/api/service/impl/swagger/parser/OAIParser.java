93fd448b30e2bfe249cd07385229f8a26aec2d77
Antoine CORDIER <antoine.cordier@graviteesource.com>
2022-06-03 15:17:19 +0200

    fix: make sure publicly writable directories are used safely
    see https://sonarcloud.io/organizations/gravitee-io/rules?open=java%3AS5443&rule_key=java%3AS5443

--- gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/rest/api/service/impl/swagger/parser/OAIParser.java
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/rest/api/service/impl/swagger/parser/OAIParser.java
@@ -15,18 +15,23 @@
  */
 package io.gravitee.rest.api.service.impl.swagger.parser;
 
+import io.gravitee.rest.api.service.common.UuidString;
 import io.gravitee.rest.api.service.exceptions.SwaggerDescriptorException;
 import io.gravitee.rest.api.service.exceptions.TechnicalManagementException;
 import io.gravitee.rest.api.service.swagger.OAIDescriptor;
 import io.swagger.parser.OpenAPIParser;
 import io.swagger.v3.parser.core.models.ParseOptions;
 import io.swagger.v3.parser.core.models.SwaggerParseResult;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.FileAttribute;
+import java.nio.file.attribute.PosixFilePermission;
+import java.nio.file.attribute.PosixFilePermissions;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
+import org.apache.commons.lang3.SystemUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -45,20 +50,24 @@ public class OAIParser extends AbstractDescriptorParser<OAIDescriptor> {
     public OAIDescriptor parse(String content, ParseOptions options) {
         OpenAPIParser parser = new OpenAPIParser();
         SwaggerParseResult parseResult;
+
         String path = content;
-        File temp = null;
+        Path tmp = null;
+
         if (!isLocationUrl(content)) {
             // Swagger v1 supports only a URL to read swagger: create temporary file for Swagger parser
-            temp = createTempFile(content);
-            path = temp.getAbsolutePath();
+            try {
+                tmp = createTempFile(content);
+                path = tmp.toAbsolutePath().toString();
+            } catch (RuntimeException e) {
+                logger.warn("Unable to create temporary file, raw content will be passed to OAI parser: {}", e.getMessage());
+            }
         }
 
         parseResult = parser.readLocation(path, null, options);
 
-        if (temp != null) {
-            if (!temp.delete()) {
-                // Do nothing if failed
-            }
+        if (tmp != null) {
+            deleteTempFile(tmp);
         }
 
         /* Hack due to swagger v1 converting issue
@@ -87,19 +96,65 @@ public class OAIParser extends AbstractDescriptorParser<OAIDescriptor> {
         return parse(content, null);
     }
 
-    private File createTempFile(String content) {
-        String fileName = "gio_swagger_" + System.currentTimeMillis();
-        File temp;
-        try {
-            temp = File.createTempFile(fileName, ".tmp");
-        } catch (IOException ioe) {
-            throw new TechnicalManagementException("An error occurs while trying to create temp file", ioe);
-        }
-        try (FileWriter out = new FileWriter(temp); BufferedWriter bw = new BufferedWriter(out)) {
+    private Path createTempFile(String content) {
+        Path temp = createSecuredTempFile();
+
+        try (FileWriter out = new FileWriter(temp.toFile()); BufferedWriter bw = new BufferedWriter(out)) {
             bw.write(content);
         } catch (IOException ioe) {
             throw new TechnicalManagementException("An error occurs while trying to create temp file", ioe);
         }
+
         return temp;
+    }
+
+    private Path createSecuredTempFile() {
+        String fileName = "gio_swagger_" + UuidString.generateRandom() + "_" + System.currentTimeMillis();
+        if (SystemUtils.IS_OS_UNIX) {
+            return createPosixSecuredFile(fileName);
+        } else {
+            return createNonPosixSecuredFile(fileName);
+        }
+    }
+
+    private Path createPosixSecuredFile(String fileName) {
+        FileAttribute<Set<PosixFilePermission>> attr = PosixFilePermissions.asFileAttribute(PosixFilePermissions.fromString("rw-------"));
+        try {
+            return Files.createTempFile(fileName, ".tmp", attr);
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    /*
+     * Ignoring security-sensitive usage of publicly writable directories as:
+     *   - file name is not predictable
+     *   - secured permissions will not be set only if something goes wrong system wise in setNonPosixPermissions
+     *
+     * see https://sonarcloud.io/organizations/gravitee-io/rules?open=java%3AS5443&rule_key=java%3AS5443
+     */
+    @SuppressWarnings("java:S5443")
+    private Path createNonPosixSecuredFile(String fileName) {
+        try {
+            Path path = Files.createTempFile(fileName, ".tmp");
+            setNonPosixPermissions(path.toFile());
+            return path;
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    private void setNonPosixPermissions(File file) {
+        if (!file.setReadable(true, true) || !file.setWritable(true, true) || !file.setExecutable(false)) {
+            logger.warn("Unable to set permissions on file {}, using default permissions", file.getAbsolutePath());
+        }
+    }
+
+    private void deleteTempFile(Path path) {
+        try {
+            Files.delete(path);
+        } catch (IOException e) {
+            logger.warn("Unable to delete temporary file {}", path);
+        }
     }
 }
