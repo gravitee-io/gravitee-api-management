c914050630f8542dfb42d7226b407022473cabc2
Thibaud Av <thibaud.avenier@gmail.com>
2022-06-21 15:07:17 +0200

    test(e2e): impl "Create an API with JWT plan and use it" e2e test

--- gravitee-apim-e2e/api-test/use-case-test/src/management/publisher/plans/create-an-api-with-jwt-plan-and-use-it.spec.ts
+++ gravitee-apim-e2e/api-test/use-case-test/src/management/publisher/plans/create-an-api-with-jwt-plan-and-use-it.spec.ts
@@ -13,8 +13,170 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { describe } from '@jest/globals';
+import { afterAll, beforeAll, describe } from '@jest/globals';
+import { APIsApi } from '@management-apis/APIsApi';
+import { forManagementAsApiUser } from '@client-conf/*';
+import { ApplicationsApi } from '@management-apis/ApplicationsApi';
+import { ApplicationSubscriptionsApi } from '@management-apis/ApplicationSubscriptionsApi';
+import { APIPlansApi } from '@management-apis/APIPlansApi';
+import { ApiEntity } from '@management-models/ApiEntity';
+import { ApplicationEntity, ApplicationEntityToJSON } from '@management-models/ApplicationEntity';
+import { PlanEntity, PlanEntityToJSON } from '@management-models/PlanEntity';
+import { Subscription } from '@management-models/Subscription';
+import { ApisFaker } from '@management-fakers/ApisFaker';
+import { PlansFaker } from '@management-fakers/PlansFaker';
+import { PlanSecurityType } from '@management-models/PlanSecurityType';
+import { PlanStatus } from '@management-models/PlanStatus';
+import { UpdatePlanEntityFromJSON } from '@management-models/UpdatePlanEntity';
+import { ApplicationsFaker } from '@management-fakers/ApplicationsFaker';
+import { LifecycleAction } from '@management-models/LifecycleAction';
+import { fetchGatewaySuccess, fetchGatewayUnauthorized } from '@lib/gateway';
+import jwt from 'jsonwebtoken';
+
+const orgId = 'DEFAULT';
+const envId = 'DEFAULT';
+
+const apisResource = new APIsApi(forManagementAsApiUser());
+const applicationsResource = new ApplicationsApi(forManagementAsApiUser());
+const applicationSubscriptionsResource = new ApplicationSubscriptionsApi(forManagementAsApiUser());
+const apiPlansResource = new APIPlansApi(forManagementAsApiUser());
 
 describe('Create an API with JWT plan and use it', () => {
-  test.skip('To complete', async () => {});
+  const MY_SECURE_GIVEN_KEY = 'MY_SECURE_GIVEN_KEY_eyJpc3MiOiJncmF2aXRl';
+
+  let createdApi: ApiEntity;
+  let createdApplication: ApplicationEntity;
+  let createdJWTPlan: PlanEntity;
+  let createdSubscription: Subscription;
+
+  beforeAll(async () => {
+    // Create new API
+    createdApi = await apisResource.createApi({
+      orgId,
+      envId,
+      newApiEntity: ApisFaker.newApi({
+        gravitee: '2.0.0',
+      }),
+    });
+
+    // Create JWT Plan with GIVEN_KEY resolver
+    const securityDefinitionJWTPlan = {
+      signature: 'HMAC_HS256',
+      publicKeyResolver: 'GIVEN_KEY',
+      useSystemProxy: false,
+      extractClaims: false,
+      propagateAuthHeader: true,
+      userClaim: 'sub',
+      resolverParameter: MY_SECURE_GIVEN_KEY,
+    };
+    createdJWTPlan = await apisResource.createApiPlan({
+      orgId,
+      envId,
+      api: createdApi.id,
+      newPlanEntity: PlansFaker.newPlan({
+        security: PlanSecurityType.JWT,
+        status: PlanStatus.PUBLISHED,
+        securityDefinition: JSON.stringify(securityDefinitionJWTPlan),
+      }),
+    });
+
+    // Create an application
+    createdApplication = await applicationsResource.createApplication({
+      envId,
+      orgId,
+      newApplicationEntity: ApplicationsFaker.newApplication({}),
+    });
+
+    createdApplication = await applicationsResource.updateApplication({
+      envId,
+      orgId,
+      application: createdApplication.id,
+      updateApplicationEntity: {
+        ...UpdatePlanEntityFromJSON(ApplicationEntityToJSON(createdApplication)),
+        settings: {
+          app: {
+            client_id: `MY_CLIENT_ID_${createdJWTPlan.id}`,
+          },
+        },
+      },
+    });
+
+    // Subscribe application to JWT plan
+    createdSubscription = await applicationSubscriptionsResource.createSubscriptionWithApplication({
+      envId,
+      orgId,
+      plan: createdJWTPlan.id,
+      application: createdApplication.id,
+    });
+
+    // Start it
+    await apisResource.doApiLifecycleAction({
+      envId,
+      orgId,
+      api: createdApi.id,
+      action: LifecycleAction.START,
+    });
+  });
+
+  describe('Call on the Jwt plan', () => {
+    test('Should return 200 Ok with valid jwt token', async () => {
+      const token = jwt.sign({ foo: 'bar', client_id: createdApplication.settings.app.client_id }, MY_SECURE_GIVEN_KEY, {
+        algorithm: 'HS256',
+      });
+
+      await fetchGatewaySuccess({
+        contextPath: `${createdApi.context_path}`,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+    });
+
+    test('Should return 401 Unauthorized with invalid jwt token', async () => {
+      const token = "I'm not a valid token (°_0)";
+
+      await fetchGatewayUnauthorized({
+        contextPath: `${createdApi.context_path}`,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+    });
+  });
+
+  afterAll(async () => {
+    if (createdApi) {
+      // Stop API
+      await apisResource.doApiLifecycleAction({
+        envId,
+        orgId,
+        api: createdApi.id,
+        action: LifecycleAction.STOP,
+      });
+
+      // Close JWT plan
+      await apiPlansResource.closeApiPlan({
+        envId,
+        orgId,
+        plan: createdJWTPlan.id,
+        api: createdApi.id,
+      });
+
+      // Delete API
+      await apisResource.deleteApi({
+        envId,
+        orgId,
+        api: createdApi.id,
+      });
+    }
+
+    // Delete application
+    if (createdApplication) {
+      await applicationsResource.deleteApplication({
+        envId,
+        orgId,
+        application: createdApplication.id,
+      });
+    }
+  });
 });
