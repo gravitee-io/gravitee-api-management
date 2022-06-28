gravitee-io/gravitee-api-management

commit 7a45634ba514abfc01e98c3ce64de1b9c7394a3f

refactor: show transfer ownership link based on permissions

gravitee-apim-console-webui/src/management/api/portal/userGroupAccess/transferOwnership/transferOwnership.controller.ts
@@ -122,7 +122,7 @@ class ApiTransferOwnershipController {
   }
 
   isAllowedToTransferOwnership(): boolean {
-    return this.UserService.currentUser.isAdmin() || this.UserService.isApiPrimaryOwner(this.api, this.groupMembers);
+    return this.UserService.currentUser.isOrganizationAdmin() || this.UserService.currentUser.allowedTo(['api-members-u']);
   }
 
   canUseGroupAsPrimaryOwner(): boolean {

gravitee-apim-console-webui/src/management/application/details/members/application-members.controller.ts
@@ -196,7 +196,7 @@ class ApplicationMembersController {
   }
 
   isAllowedToTransferOwnership() {
-    return this.UserService.currentUser.isAdmin() || this.isPrimaryOwner();
+    return this.UserService.currentUser.isOrganizationAdmin() || this.isPrimaryOwner();
   }
 
   isPrimaryOwner() {

gravitee-apim-console-webui/src/entities/user.ts
@@ -76,12 +76,7 @@ export class User {
     return _.difference(permissions, allPermissions).length === 0;
   }
 
-  isAdmin(): boolean {
-    if (!this.userPermissions && !this.userEnvironmentPermissions) {
-      return false;
-    }
-    return this.userEnvironmentPermissions.some((userEnvironmentPermission) => {
-      return userEnvironmentPermission.startsWith('environment-instance') || userEnvironmentPermission.startsWith('environment-platform');
-    });
+  isOrganizationAdmin(): boolean {
+    return this.roles?.some((role) => role.scope === 'ORGANIZATION' && role.name === 'ADMIN');
   }
 }

gravitee-apim-console-webui/src/management/api/apiAdmin.controller.ts
@@ -230,7 +230,7 @@ class ApiAdminController {
   }
 
   canTransferOwnership(): boolean {
-    return this.UserService.currentUser.isAdmin() || this.UserService.isApiPrimaryOwner(this.api, this.resolvedApiGroups);
+    return this.UserService.currentUser.isOrganizationAdmin() || this.UserService.currentUser.allowedTo(['api-member-u']);
   }
 
   isAdmin(): boolean {

gravitee-apim-console-webui/src/management/api/proxy/backend/healthcheck/healthcheck.controller.ts
@@ -248,7 +248,7 @@ class ApiHealthCheckController {
   }
 
   displayGatewayHC() {
-    return this.UserService.currentUser.isAdmin();
+    return this.UserService.currentUser.isOrganizationAdmin();
   }
 }
 

commit a2945c7decd902047460b05d8d0ccd54df0f0699

fix: use correct permission to access transfer ownership screen
Wrong permission was introduced with 7a45634b

https://github.com/gravitee-io/issues/issues/7859

gravitee-apim-console-webui/src/management/api/portal/userGroupAccess/transferOwnership/transferOwnership.controller.ts
@@ -122,7 +122,7 @@ class ApiTransferOwnershipController {
   }
 
   isAllowedToTransferOwnership(): boolean {
-    return this.UserService.currentUser.isOrganizationAdmin() || this.UserService.currentUser.allowedTo(['api-members-u']);
+    return this.UserService.currentUser.isOrganizationAdmin() || this.UserService.currentUser.allowedTo(['api-member-u']);
   }
 
   canUseGroupAsPrimaryOwner(): boolean {

gravitee-apim-console-webui/src/management/application/details/members/application-members.controller.ts
@@ -196,7 +196,7 @@ class ApplicationMembersController {
   }
 
   isAllowedToTransferOwnership() {
-    return this.UserService.currentUser.isOrganizationAdmin() || this.UserService.currentUser.allowedTo(['api-members-u']);
+    return this.UserService.currentUser.isOrganizationAdmin() || this.UserService.currentUser.allowedTo(['api-member-u']);
   }
 
   toggleDisableMembershipNotifications() {