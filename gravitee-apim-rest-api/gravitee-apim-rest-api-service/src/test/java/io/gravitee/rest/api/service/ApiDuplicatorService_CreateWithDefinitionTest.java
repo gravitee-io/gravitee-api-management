gravitee-io/gravitee-api-management

commit 49a3f4462782ed0dd38cc44a94e1c39eab45b277

fix: create pages before system folder
gravitee-io/issues#7876

gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/rest/api/service/impl/ApiDuplicatorServiceImpl.java
@@ -129,7 +129,7 @@ public class ApiDuplicatorServiceImpl extends AbstractService implements ApiDupl
 
             UpdateApiEntity importedApi = convertToEntity(apiDefinition, jsonNode, environmentId);
             ApiEntity createdApiEntity = apiService.createWithApiDefinition(importedApi, userId, jsonNode);
-            createMediaAndSystemFolder(createdApiEntity, jsonNode, environmentId);
+            createMediaAndPages(createdApiEntity, jsonNode, environmentId);
             updateApiReferences(createdApiEntity.getId(), jsonNode, organizationId, environmentId, false);
             return createdApiEntity;
         } catch (IOException e) {
@@ -326,8 +326,7 @@ public class ApiDuplicatorServiceImpl extends AbstractService implements ApiDupl
         return importedApi;
     }
 
-    private void createMediaAndSystemFolder(ApiEntity createdApiEntity, JsonNode jsonNode, String environmentId)
-        throws JsonProcessingException {
+    private void createMediaAndPages(ApiEntity createdApiEntity, JsonNode jsonNode, String environmentId) throws JsonProcessingException {
         final JsonNode apiMedia = jsonNode.path("apiMedia");
         if (apiMedia != null && apiMedia.isArray()) {
             for (JsonNode media : apiMedia) {
@@ -335,18 +334,7 @@ public class ApiDuplicatorServiceImpl extends AbstractService implements ApiDupl
             }
         }
 
-        List<PageEntity> search = pageService.search(
-            new PageQuery.Builder()
-                .api(createdApiEntity.getId())
-                .name(SystemFolderType.ASIDE.folderName())
-                .type(PageType.SYSTEM_FOLDER)
-                .build(),
-            environmentId
-        );
-
-        if (search.isEmpty()) {
-            pageService.createAsideFolder(createdApiEntity.getId(), environmentId);
-        }
+        updatePages(createdApiEntity.getId(), jsonNode, environmentId);
     }
 
     private String fetchApiDefinitionContentFromURL(String apiDefinitionOrURL) {
@@ -367,8 +355,10 @@ public class ApiDuplicatorServiceImpl extends AbstractService implements ApiDupl
         // Members
         updateMembers(apiId, jsonNode, organizationId, environmentId);
 
-        //Pages
-        updatePages(apiId, jsonNode, environmentId);
+        if (isUpdate) {
+            //Pages
+            updatePages(apiId, jsonNode, environmentId);
+        }
 
         //Plans
         updatePlans(apiId, jsonNode, environmentId, isUpdate);

gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/test/java/io/gravitee/rest/api/service/ApiDuplicatorService_CreateWithDefinitionTest.java
@@ -254,7 +254,6 @@ public class ApiDuplicatorService_CreateWithDefinitionTest {
         );
 
         verify(apiService, times(1)).createWithApiDefinition(any(), eq("admin"), any());
-        verify(pageService, times(1)).createAsideFolder(eq(API_ID), eq(GraviteeContext.getCurrentEnvironment()));
         verify(membershipService, times(1))
             .addRoleToMemberOnReference(
                 GraviteeContext.getCurrentOrganization(),
@@ -333,7 +332,6 @@ public class ApiDuplicatorService_CreateWithDefinitionTest {
         );
 
         verify(apiService, times(1)).createWithApiDefinition(any(), eq("admin"), any());
-        verify(pageService, times(1)).createAsideFolder(eq(API_ID), eq(GraviteeContext.getCurrentEnvironment()));
     }
 
     @Test
@@ -363,7 +361,6 @@ public class ApiDuplicatorService_CreateWithDefinitionTest {
         );
 
         verify(apiService, times(1)).createWithApiDefinition(any(), eq("admin"), any());
-        verify(pageService, times(1)).createAsideFolder(eq(API_ID), eq(GraviteeContext.getCurrentEnvironment()));
     }
 
     @Test
@@ -393,7 +390,6 @@ public class ApiDuplicatorService_CreateWithDefinitionTest {
         );
 
         verify(apiService, times(1)).createWithApiDefinition(any(), eq("admin"), any());
-        verify(pageService, times(1)).createAsideFolder(eq(API_ID), eq(GraviteeContext.getCurrentEnvironment()));
     }
 
     @Test
@@ -419,7 +415,6 @@ public class ApiDuplicatorService_CreateWithDefinitionTest {
         );
 
         verify(apiService, times(1)).createWithApiDefinition(any(), eq("admin"), any());
-        verify(pageService, times(1)).createAsideFolder(eq(API_ID), eq(GraviteeContext.getCurrentEnvironment()));
     }
 
     @Test
@@ -555,7 +550,6 @@ public class ApiDuplicatorService_CreateWithDefinitionTest {
         );
 
         verify(apiService, times(1)).createWithApiDefinition(any(), eq("admin"), any());
-        verify(pageService, times(1)).createAsideFolder(eq(API_ID), eq(GraviteeContext.getCurrentEnvironment()));
         verify(membershipService, times(1))
             .addRoleToMemberOnReference(
                 GraviteeContext.getCurrentOrganization(),

commit c23cf0bdb1f386007170a2e10fcb3b9443fa4d5d

fix: create api with system folder
gravitee-io/issues#7876. This commit fix a regression on the api creation introduced in the commit 49a3f44

gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/rest/api/service/impl/ApiDuplicatorServiceImpl.java
@@ -335,6 +335,19 @@ public class ApiDuplicatorServiceImpl extends AbstractService implements ApiDupl
         }
 
         updatePages(createdApiEntity.getId(), jsonNode, environmentId);
+
+        List<PageEntity> search = pageService.search(
+            new PageQuery.Builder()
+                .api(createdApiEntity.getId())
+                .name(SystemFolderType.ASIDE.folderName())
+                .type(PageType.SYSTEM_FOLDER)
+                .build(),
+            environmentId
+        );
+
+        if (search.isEmpty()) {
+            pageService.createAsideFolder(createdApiEntity.getId(), environmentId);
+        }
     }
 
     private String fetchApiDefinitionContentFromURL(String apiDefinitionOrURL) {

gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/test/java/io/gravitee/rest/api/service/ApiDuplicatorService_CreateWithDefinitionTest.java
@@ -254,6 +254,7 @@ public class ApiDuplicatorService_CreateWithDefinitionTest {
         );
 
         verify(apiService, times(1)).createWithApiDefinition(any(), eq("admin"), any());
+        verify(pageService, times(1)).createAsideFolder(eq(API_ID), eq(GraviteeContext.getCurrentEnvironment()));
         verify(membershipService, times(1))
             .addRoleToMemberOnReference(
                 GraviteeContext.getCurrentOrganization(),
@@ -332,6 +333,7 @@ public class ApiDuplicatorService_CreateWithDefinitionTest {
         );
 
         verify(apiService, times(1)).createWithApiDefinition(any(), eq("admin"), any());
+        verify(pageService, times(1)).createAsideFolder(eq(API_ID), eq(GraviteeContext.getCurrentEnvironment()));
     }
 
     @Test
@@ -361,6 +363,7 @@ public class ApiDuplicatorService_CreateWithDefinitionTest {
         );
 
         verify(apiService, times(1)).createWithApiDefinition(any(), eq("admin"), any());
+        verify(pageService, times(1)).createAsideFolder(eq(API_ID), eq(GraviteeContext.getCurrentEnvironment()));
     }
 
     @Test
@@ -390,6 +393,7 @@ public class ApiDuplicatorService_CreateWithDefinitionTest {
         );
 
         verify(apiService, times(1)).createWithApiDefinition(any(), eq("admin"), any());
+        verify(pageService, times(1)).createAsideFolder(eq(API_ID), eq(GraviteeContext.getCurrentEnvironment()));
     }
 
     @Test
@@ -415,6 +419,7 @@ public class ApiDuplicatorService_CreateWithDefinitionTest {
         );
 
         verify(apiService, times(1)).createWithApiDefinition(any(), eq("admin"), any());
+        verify(pageService, times(1)).createAsideFolder(eq(API_ID), eq(GraviteeContext.getCurrentEnvironment()));
     }
 
     @Test
@@ -550,6 +555,7 @@ public class ApiDuplicatorService_CreateWithDefinitionTest {
         );
 
         verify(apiService, times(1)).createWithApiDefinition(any(), eq("admin"), any());
+        verify(pageService, times(1)).createAsideFolder(eq(API_ID), eq(GraviteeContext.getCurrentEnvironment()));
         verify(membershipService, times(1))
             .addRoleToMemberOnReference(
                 GraviteeContext.getCurrentOrganization(),