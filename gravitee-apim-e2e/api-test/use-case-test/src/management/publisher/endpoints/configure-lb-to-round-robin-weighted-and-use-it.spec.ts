0207019d3199e7639ddd8e572f61c4f29610b0c2
Christian Tschacher <15231507+ctschacher@users.noreply.github.com>
2022-07-11 19:50:16 +0200

    test: added test for weighted round-robin lb
    gravitee-io/issues#8079

--- gravitee-apim-e2e/api-test/use-case-test/src/management/publisher/endpoints/configure-lb-to-round-robin-weighted-and-use-it.spec.ts
+++ gravitee-apim-e2e/api-test/use-case-test/src/management/publisher/endpoints/configure-lb-to-round-robin-weighted-and-use-it.spec.ts
@@ -13,8 +13,91 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { describe } from '@jest/globals';
+import { APIsApi } from '@management-apis/APIsApi';
+import { forManagementAsApiUser } from '@client-conf/*';
+import { afterAll, beforeAll, describe, expect } from '@jest/globals';
+import { succeed } from '@lib/jest-utils';
+import { ApisFaker } from '@management-fakers/ApisFaker';
+import { ApiEntity } from '@management-models/ApiEntity';
+import { PlansFaker } from '@management-fakers/PlansFaker';
+import { LifecycleAction } from '@management-models/LifecycleAction';
+import { PlanStatus } from '@management-models/PlanStatus';
+import { fetchGatewaySuccess } from '@lib/gateway';
+import { LoadBalancerTypeEnum } from '@management-models/LoadBalancer';
+import { teardownApisAndApplications } from '@lib/management';
 
-describe('Configure LB to round robin weighted and use it ', () => {
-  test.skip('To complete', async () => {});
+const orgId = 'DEFAULT';
+const envId = 'DEFAULT';
+const apisResourceAsPublisher = new APIsApi(forManagementAsApiUser());
+
+describe('Configure LB to round robin weighted and use it', () => {
+  let createdApi: ApiEntity;
+
+  beforeAll(async () => {
+    // create an API with a published free plan and 2 endpoints in one endpoint group (lb: weighted round robin)
+    createdApi = await succeed(
+      apisResourceAsPublisher.importApiDefinitionRaw({
+        envId,
+        orgId,
+        body: ApisFaker.apiImport({
+          plans: [PlansFaker.plan({ status: PlanStatus.PUBLISHED })],
+          proxy: ApisFaker.proxy({
+            groups: [
+              {
+                name: 'default-group',
+                endpoints: [
+                  {
+                    inherit: true,
+                    name: 'default',
+                    target: `${process.env.WIREMOCK_BASE_URL}/hello?name=endpoint1`,
+                    weight: 1,
+                    backup: false,
+                    type: 'http',
+                  },
+                  {
+                    inherit: true,
+                    name: 'endpoint2',
+                    target: `${process.env.WIREMOCK_BASE_URL}/hello?name=endpoint2`,
+                    weight: 2,
+                    backup: false,
+                    type: 'http',
+                  },
+                ],
+                load_balancing: {
+                  type: LoadBalancerTypeEnum.WEIGHTEDROUNDROBIN,
+                },
+              },
+            ],
+          }),
+        }),
+      }),
+    );
+
+    // start it
+    await apisResourceAsPublisher.doApiLifecycleAction({
+      envId,
+      orgId,
+      api: createdApi.id,
+      action: LifecycleAction.START,
+    });
+  });
+
+  test('Should switch between three configured endpoints using round-robin', async () => {
+    const contextPath = createdApi.context_path;
+    let endpoint1 = 0;
+    let endpoint2 = 0;
+
+    for (let i = 0; i < 6; i++) {
+      const response = await fetchGatewaySuccess({ contextPath }).then((res) => res.json());
+      if (response.message.match(/Endpoint1/)) endpoint1++;
+      if (response.message.match(/Endpoint2/)) endpoint2++;
+    }
+
+    expect(endpoint1).toBe(2);
+    expect(endpoint2).toBe(4);
+  });
+
+  afterAll(async () => {
+    await teardownApisAndApplications(orgId, envId, [createdApi.id]);
+  });
 });
