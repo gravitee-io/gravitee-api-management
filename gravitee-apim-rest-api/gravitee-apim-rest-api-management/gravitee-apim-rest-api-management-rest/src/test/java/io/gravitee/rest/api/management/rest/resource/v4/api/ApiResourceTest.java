1acaa7caa0deadb81ac90a4670dcf82b927fda08
Florent CHAMFROY <florent.chamfroy@graviteesource.com>
2022-08-02 10:37:53 +0200

    test(definition): increase coverage for GET API

--- gravitee-apim-rest-api/gravitee-apim-rest-api-management/gravitee-apim-rest-api-management-rest/src/test/java/io/gravitee/rest/api/management/rest/resource/v4/api/ApiResourceTest.java
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-management/gravitee-apim-rest-api-management-rest/src/test/java/io/gravitee/rest/api/management/rest/resource/v4/api/ApiResourceTest.java
@@ -20,18 +20,27 @@ import static io.gravitee.common.http.HttpStatusCode.NO_CONTENT_204;
 import static io.gravitee.common.http.HttpStatusCode.OK_200;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.reset;
 
 import io.gravitee.definition.model.v4.listener.http.ListenerHttp;
 import io.gravitee.definition.model.v4.listener.http.Path;
+import io.gravitee.definition.model.v4.property.Property;
+import io.gravitee.definition.model.v4.resource.Resource;
+import io.gravitee.definition.model.v4.service.ApiServices;
 import io.gravitee.rest.api.management.rest.resource.AbstractResourceTest;
+import io.gravitee.rest.api.model.permissions.RolePermission;
+import io.gravitee.rest.api.model.permissions.RolePermissionAction;
 import io.gravitee.rest.api.model.v4.api.ApiEntity;
 import io.gravitee.rest.api.service.common.GraviteeContext;
 import io.gravitee.rest.api.service.exceptions.ApiNotFoundException;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import javax.ws.rs.core.Response;
 import org.junit.After;
 import org.junit.Before;
@@ -61,8 +70,13 @@ public class ApiResourceTest extends AbstractResourceTest {
         apiEntity.setId(API);
         apiEntity.setName(API);
         ListenerHttp listenerHttp = new ListenerHttp();
-        listenerHttp.setPaths(List.of(new Path("/test")));
+        listenerHttp.setPaths(List.of(new Path("my.fake.host", "/test")));
+        listenerHttp.setPathMappings(Set.of("/test"));
         apiEntity.setListeners(List.of(listenerHttp));
+        apiEntity.setProperties(List.of(new Property()));
+        apiEntity.setServices(new ApiServices());
+        apiEntity.setResources(List.of(new Resource()));
+        apiEntity.setResponseTemplates(Map.of("key", new HashMap<>()));
         apiEntity.setUpdatedAt(new Date());
         doReturn(apiEntity).when(apiServiceV4).findById(GraviteeContext.getExecutionContext(), API);
         doThrow(ApiNotFoundException.class).when(apiServiceV4).findById(GraviteeContext.getExecutionContext(), UNKNOWN_API);
@@ -83,6 +97,45 @@ public class ApiResourceTest extends AbstractResourceTest {
         final ApiEntity responseApi = response.readEntity(ApiEntity.class);
         assertNotNull(responseApi);
         assertEquals(API, responseApi.getName());
+        assertNotNull(responseApi.getPictureUrl());
+        assertNotNull(responseApi.getBackgroundUrl());
+        assertNotNull(responseApi.getProperties());
+        assertEquals(1, responseApi.getProperties().size());
+        assertNotNull(responseApi.getServices());
+        assertNotNull(responseApi.getResources());
+        assertEquals(1, responseApi.getResources().size());
+        assertNotNull(responseApi.getResponseTemplates());
+        assertEquals(1, responseApi.getResponseTemplates().size());
+        assertNotNull(responseApi.getListeners());
+        assertNotNull(((ListenerHttp) responseApi.getListeners().get(0)).getPathMappings());
+        assertNotNull(((ListenerHttp) responseApi.getListeners().get(0)).getPaths().get(0).getHost());
+    }
+
+    @Test
+    public void shouldGetFilteredApi() {
+        doReturn(false)
+            .when(permissionService)
+            .hasPermission(GraviteeContext.getExecutionContext(), RolePermission.API_DEFINITION, API, RolePermissionAction.READ);
+
+        final Response response = envTarget(API).request().get();
+
+        assertEquals(OK_200, response.getStatus());
+
+        final ApiEntity responseApi = response.readEntity(ApiEntity.class);
+        assertNotNull(responseApi);
+        assertEquals(API, responseApi.getName());
+        assertNull(responseApi.getPictureUrl());
+        assertNull(responseApi.getBackgroundUrl());
+        assertNotNull(responseApi.getProperties());
+        assertEquals(0, responseApi.getProperties().size());
+        assertNull(responseApi.getServices());
+        assertNotNull(responseApi.getResources());
+        assertEquals(0, responseApi.getResources().size());
+        assertNotNull(responseApi.getResponseTemplates());
+        assertEquals(0, responseApi.getResponseTemplates().size());
+        assertNotNull(responseApi.getListeners());
+        assertNull(((ListenerHttp) responseApi.getListeners().get(0)).getPathMappings());
+        assertNull(((ListenerHttp) responseApi.getListeners().get(0)).getPaths().get(0).getHost());
     }
 
     @Test
