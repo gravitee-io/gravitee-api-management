06113b3efde78e7f853f7408cbcf4cca471b86f9
Florent CHAMFROY <florent.chamfroy@graviteesource.com>
2022-08-30 10:36:44 +0200

    feat(entrypoint): add a new http-get entrypoint
    gravitee-i/issues#8037

---
+++ gravitee-apim-plugin/gravitee-apim-plugin-entrypoint/gravitee-apim-plugin-entrypoint-http-get/src/main/java/io/gravitee/plugin/entrypoint/http/get/HttpGetEntrypointConnectorFactory.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.plugin.entrypoint.http.get;
+
+import io.gravitee.gateway.jupiter.api.ConnectorMode;
+import io.gravitee.gateway.jupiter.api.connector.ConnectorFactoryHelper;
+import io.gravitee.gateway.jupiter.api.connector.entrypoint.async.EntrypointAsyncConnectorFactory;
+import io.gravitee.gateway.jupiter.api.exception.PluginConfigurationException;
+import io.gravitee.plugin.entrypoint.http.get.configuration.HttpGetEntrypointConnectorConfiguration;
+import java.util.Set;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author Florent CHAMFROY (florent.chamfroy at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+@Slf4j
+@AllArgsConstructor
+public class HttpGetEntrypointConnectorFactory implements EntrypointAsyncConnectorFactory {
+
+    private ConnectorFactoryHelper connectorFactoryHelper;
+
+    @Override
+    public Set<ConnectorMode> supportedModes() {
+        return HttpGetEntrypointConnector.SUPPORTED_MODES;
+    }
+
+    @Override
+    public HttpGetEntrypointConnector createConnector(final String configuration) {
+        try {
+            return new HttpGetEntrypointConnector(
+                connectorFactoryHelper.getConnectorConfiguration(HttpGetEntrypointConnectorConfiguration.class, configuration)
+            );
+        } catch (PluginConfigurationException e) {
+            log.error("Can't create connector cause no valid configuration", e);
+            return null;
+        }
+    }
+}
