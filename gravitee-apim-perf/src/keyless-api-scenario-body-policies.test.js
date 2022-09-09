e7c320d76c6222489ac44c0a27f928d41f3e26aa
Guillaume Cusnieux <guillaume.cusnieux@graviteesource.com>
2022-09-09 09:18:17 +0200

    feat(perf): add keyless api body policies scenario

---
+++ gravitee-apim-perf/src/keyless-api-scenario-body-policies.test.js
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { check } from 'k6';
+import http from 'k6/http';
+import { k6Options } from '../config/k6-options.js';
+
+export const options = k6Options;
+
+const data = JSON.parse(open(__ENV.TEST_DATA_PATH));
+const url = `${__ENV.GATEWAY_BASE_URL}${data.api.context_path}`;
+
+export default () => {
+  const res = http.get(url);
+  check(res, {
+    'status is 200': () => res.status === 200,
+    'contains header': () => res.headers['Content-Type'] === 'application/xml;charset=UTF-8',
+  });
+};
