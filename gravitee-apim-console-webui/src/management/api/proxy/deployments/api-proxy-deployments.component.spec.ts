b7f6f7c5bd648f9bc5cd5b013961de3f7d612d09
ThibaudAv <thibaud.avenier@gmail.com>
2022-09-09 15:41:30 +0200

    feat(console): impl ApiProxyDeploymentsComponent form

--- gravitee-apim-console-webui/src/management/api/proxy/deployments/api-proxy-deployments.component.spec.ts
+++ gravitee-apim-console-webui/src/management/api/proxy/deployments/api-proxy-deployments.component.spec.ts
@@ -16,36 +16,103 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpTestingController } from '@angular/common/http/testing';
+import { HarnessLoader } from '@angular/cdk/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { GioSaveBarHarness } from '@gravitee/ui-particles-angular';
+import { MatSelectHarness } from '@angular/material/select/testing';
+import { MatSnackBarHarness } from '@angular/material/snack-bar/testing';
 
 import { ApiProxyDeploymentsModule } from './api-proxy-deployments.module';
 import { ApiProxyDeploymentsComponent } from './api-proxy-deployments.component';
 
-import { GioHttpTestingModule } from '../../../../shared/testing';
+import { CONSTANTS_TESTING, GioHttpTestingModule } from '../../../../shared/testing';
+import { UIRouterStateParams, CurrentUserService } from '../../../../ajs-upgraded-providers';
+import { User } from '../../../../entities/user';
+import { Api } from '../../../../entities/api';
+import { fakeTag } from '../../../../entities/tag/tag.fixture';
+import { Tag } from '../../../../entities/tag/tag';
+import { fakeApi } from '../../../../entities/api/Api.fixture';
 
 describe('ApiProxyDeploymentsComponent', () => {
+  const API_ID = 'apiId';
+
   let fixture: ComponentFixture<ApiProxyDeploymentsComponent>;
-  let component: ApiProxyDeploymentsComponent;
+  let loader: HarnessLoader;
+  let rootLoader: HarnessLoader;
   let httpTestingController: HttpTestingController;
+
+  const currentUser = new User();
+  currentUser.userPermissions = ['api-definition-u'];
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [NoopAnimationsModule, GioHttpTestingModule, ApiProxyDeploymentsModule],
+      providers: [
+        { provide: UIRouterStateParams, useValue: { apiId: API_ID } },
+        { provide: CurrentUserService, useValue: { currentUser } },
+      ],
     });
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ApiProxyDeploymentsComponent);
-    component = fixture.componentInstance;
+    loader = TestbedHarnessEnvironment.loader(fixture);
+    rootLoader = TestbedHarnessEnvironment.documentRootLoader(fixture);
 
     httpTestingController = TestBed.inject(HttpTestingController);
     fixture.detectChanges();
   });
 
-  it('should work', async () => {
-    expect(component).toBeTruthy();
-  });
-
   afterEach(() => {
     httpTestingController.verify();
   });
+
+  it('should update deployment', async () => {
+    const api = fakeApi({
+      id: API_ID,
+      tags: ['tag2'],
+    });
+    expectApiGetRequest(api);
+    expectTagGetRequest([
+      fakeTag({ id: 'tag1', name: 'tag1' }),
+      fakeTag({ id: 'tag2', name: 'tag2' }),
+      fakeTag({ id: 'tag3', name: 'tag3' }),
+    ]);
+
+    const saveBar = await loader.getHarness(GioSaveBarHarness);
+    expect(await saveBar.isVisible()).toBe(false);
+
+    const tagsInput = await loader.getHarness(MatSelectHarness.with({ selector: '[formControlName="tags"]' }));
+    expect(await tagsInput.isDisabled()).toEqual(false);
+
+    await tagsInput.clickOptions({ text: /tag1/ });
+    await tagsInput.clickOptions({ text: /tag2/ });
+    await tagsInput.clickOptions({ text: /tag3/ });
+
+    const saveButton = await loader.getHarness(GioSaveBarHarness);
+    await saveButton.clickSubmit();
+
+    // Expect fetch api and update
+    expectApiGetRequest(api);
+    const req = httpTestingController.expectOne({ method: 'PUT', url: `${CONSTANTS_TESTING.env.baseURL}/apis/${API_ID}` });
+    expect(req.request.body.tags).toStrictEqual(['tag1', 'tag3']);
+    req.flush(api);
+
+    const snackBars = await rootLoader.getAllHarnesses(MatSnackBarHarness);
+    expect(snackBars.length).toBe(1);
+
+    // No flush to stop on new call of ngOnInit
+    httpTestingController.expectOne({ url: `${CONSTANTS_TESTING.env.baseURL}/apis/${api.id}`, method: 'GET' });
+    httpTestingController.expectOne({ url: `${CONSTANTS_TESTING.org.baseURL}/configuration/tags`, method: 'GET' });
+  });
+
+  function expectApiGetRequest(api: Api) {
+    httpTestingController.expectOne({ url: `${CONSTANTS_TESTING.env.baseURL}/apis/${api.id}`, method: 'GET' }).flush(api);
+    fixture.detectChanges();
+  }
+
+  function expectTagGetRequest(tags: Tag[]) {
+    httpTestingController.expectOne({ url: `${CONSTANTS_TESTING.org.baseURL}/configuration/tags`, method: 'GET' }).flush(tags);
+    fixture.detectChanges();
+  }
 });
