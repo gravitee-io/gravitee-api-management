b61ab0731f33dcdf80906b1cec39c7f1689ef0ba
Gaëtan Maisse <gaetanmaisse@gmail.com>
2022-09-14 11:22:40 +0200

    feat: use bridgeWorkerExecutor in all handlers

--- gravitee-apim-repository/gravitee-apim-repository-gateway-bridge-http/gravitee-apim-repository-gateway-bridge-http-server/src/main/java/io/gravitee/repository/bridge/server/handler/EnvironmentsHandler.java
+++ gravitee-apim-repository/gravitee-apim-repository-gateway-bridge-http/gravitee-apim-repository-gateway-bridge-http-server/src/main/java/io/gravitee/repository/bridge/server/handler/EnvironmentsHandler.java
@@ -19,6 +19,7 @@ import io.gravitee.repository.management.api.EnvironmentRepository;
 import io.gravitee.repository.management.model.Environment;
 import io.vertx.core.Handler;
 import io.vertx.core.Promise;
+import io.vertx.core.WorkerExecutor;
 import io.vertx.ext.web.RoutingContext;
 import java.util.Collection;
 import java.util.Set;
@@ -34,6 +35,10 @@ public class EnvironmentsHandler extends AbstractHandler {
     @Autowired
     private EnvironmentRepository environmentRepository;
 
+    public EnvironmentsHandler(WorkerExecutor bridgeWorkerExecutor) {
+        super(bridgeWorkerExecutor);
+    }
+
     public void findByOrganizationsAndHrids(RoutingContext ctx) {
         final String organizationsIdsParam = ctx.request().getParam("organizationsIds");
         final String hridsParam = ctx.request().getParam("hrids");
@@ -41,34 +46,30 @@ public class EnvironmentsHandler extends AbstractHandler {
         final Set<String> organizationsIds = readListParam(organizationsIdsParam);
         final Set<String> hrids = readListParam(hridsParam);
 
-        ctx
-            .vertx()
-            .executeBlocking(
-                (Handler<Promise<Set<Environment>>>) promise -> {
-                    try {
-                        promise.complete(environmentRepository.findByOrganizationsAndHrids(organizationsIds, hrids));
-                    } catch (Exception ex) {
-                        LOGGER.error("Unable to search for environments", ex);
-                        promise.fail(ex);
-                    }
-                },
-                event -> handleResponse(ctx, event)
-            );
+        bridgeWorkerExecutor.executeBlocking(
+            (Handler<Promise<Set<Environment>>>) promise -> {
+                try {
+                    promise.complete(environmentRepository.findByOrganizationsAndHrids(organizationsIds, hrids));
+                } catch (Exception ex) {
+                    LOGGER.error("Unable to search for environments", ex);
+                    promise.fail(ex);
+                }
+            },
+            event -> handleResponse(ctx, event)
+        );
     }
 
     public void findAll(RoutingContext ctx) {
-        ctx
-            .vertx()
-            .executeBlocking(
-                (Handler<Promise<Collection<Environment>>>) promise -> {
-                    try {
-                        promise.complete(environmentRepository.findAll());
-                    } catch (Exception ex) {
-                        LOGGER.error("Unable to search for environments", ex);
-                        promise.fail(ex);
-                    }
-                },
-                event -> handleResponse(ctx, event)
-            );
+        bridgeWorkerExecutor.executeBlocking(
+            (Handler<Promise<Collection<Environment>>>) promise -> {
+                try {
+                    promise.complete(environmentRepository.findAll());
+                } catch (Exception ex) {
+                    LOGGER.error("Unable to search for environments", ex);
+                    promise.fail(ex);
+                }
+            },
+            event -> handleResponse(ctx, event)
+        );
     }
 }
