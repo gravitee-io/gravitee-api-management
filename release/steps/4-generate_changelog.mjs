0ad8f39c7d4112ed979b3bf6df40343f5c0362f7
Okhelifi <ouahid.khelifi@gmail.com>
2022-09-19 17:26:41 +0200

    ci: add changelog generation in ci

--- release/steps/4-generate_changelog.mjs
+++ release/steps/4-generate_changelog.mjs
@@ -37,7 +37,7 @@ if (tagSteps !== 'y') {
   process.exit();
 }
 
-const response = await fetch('https://circleci.com/api/v2/project/gh/gravitee-io/issues/pipeline', {
+const response = await fetch('https://circleci.com/api/v2/project/gh/gravitee-io/gravitee-api-management/pipeline', {
   method: 'post',
   body: JSON.stringify(body),
   headers: {
@@ -50,7 +50,7 @@ const data = await response.json();
 
 if (response.status === 201) {
   console.log(chalk.green(`Pipeline created with number: ${data.number}`));
-  echo`Follow its progress on: https://app.circleci.com/pipelines/github/gravitee-io/issues/${data.number}`;
+  echo`Follow its progress on: https://app.circleci.com/pipelines/github/gravitee-io/gravitee-api-management/${data.number}`;
   console.log(chalk.greenBright(`When it's done, run 'npm run nexus_sync -- --version=${releasingVersion}'`));
 } else {
   console.log(chalk.yellow('Something went wrong'));
