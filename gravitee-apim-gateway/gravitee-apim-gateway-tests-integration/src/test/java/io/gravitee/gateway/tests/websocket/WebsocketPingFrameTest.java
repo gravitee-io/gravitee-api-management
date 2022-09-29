6b8a8142f420c504929d47bb1a8f014d3c7e364c
Okhelifi <ouahid.khelifi@gmail.com>
2022-09-29 18:07:02 +0200

    test: migrate websocket ping frame test

---
+++ gravitee-apim-gateway/gravitee-apim-gateway-tests-integration/src/test/java/io/gravitee/gateway/tests/websocket/WebsocketPingFrameTest.java
@@ -0,0 +1,72 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.gateway.tests.websocket;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.gravitee.apim.gateway.tests.sdk.AbstractWebsocketGatewayTest;
+import io.gravitee.apim.gateway.tests.sdk.annotations.DeployApi;
+import io.gravitee.apim.gateway.tests.sdk.annotations.GatewayTest;
+import io.vertx.junit5.VertxTestContext;
+import io.vertx.reactivex.core.buffer.Buffer;
+import io.vertx.reactivex.core.http.WebSocket;
+import java.util.concurrent.TimeUnit;
+import org.junit.jupiter.api.Test;
+
+@GatewayTest
+@DeployApi({ "/apis/http/api.json" })
+public class WebsocketPingFrameTest extends AbstractWebsocketGatewayTest {
+
+    @Test
+    public void websocket_ping_request(VertxTestContext testContext) throws Throwable {
+        httpServer
+            .webSocketHandler(
+                serverWebSocket -> {
+                    serverWebSocket.exceptionHandler(testContext::failNow);
+                    serverWebSocket.accept();
+                    serverWebSocket.frameHandler(
+                        frame -> {
+                            if (frame.isPing()) {
+                                testContext.verify(() -> assertThat(frame.textData()).isEqualTo("PING"));
+                                testContext.completeNow();
+                            }
+                        }
+                    );
+                }
+            )
+            .listen(
+                websocketPort,
+                ar ->
+                    httpClient.webSocket(
+                        "/test",
+                        event -> {
+                            if (event.failed()) {
+                                testContext.failNow(event.cause());
+                            } else {
+                                final WebSocket webSocket = event.result();
+                                webSocket.exceptionHandler(testContext::failNow);
+                                webSocket.writePing(Buffer.buffer("PING"));
+                            }
+                        }
+                    )
+            );
+
+        testContext.awaitCompletion(10, TimeUnit.SECONDS);
+        if (testContext.failed()) {
+            throw testContext.causeOfFailure();
+        }
+    }
+}
