be058af490ab4b0a03901dfe2fce4fc3b3ace4a7
ThibaudAv <thibaud.avenier@gmail.com>
2022-11-04 17:30:05 +0100

    feat(console): support api v1.0.0 in migrated portal details page

--- gravitee-apim-console-webui/src/shared/components/gio-api-import-dialog/gio-api-import-dialog.component.ts
+++ gravitee-apim-console-webui/src/shared/components/gio-api-import-dialog/gio-api-import-dialog.component.ts
@@ -32,6 +32,8 @@ type FileExtension = typeof allowedFileExtensions[number];
 export type GioApiImportDialogData = {
   policies?: PolicyListItem[];
   apiId?: string;
+  // Default to 2.0.0
+  definitionVersion?: string; 
 };
 
 @Component({
@@ -70,6 +72,8 @@ export class GioApiImportDialogComponent implements OnDestroy {
 
   private unsubscribe$: Subject<void> = new Subject<void>();
 
+  private definitionVersion: string;
+
   constructor(
     private readonly dialogRef: MatDialogRef<GioApiImportDialogData>,
     @Inject(MAT_DIALOG_DATA) dialogData: GioApiImportDialogData,
@@ -79,6 +83,7 @@ export class GioApiImportDialogComponent implements OnDestroy {
     this.policies = dialogData?.policies ?? [];
     this.isUpdateMode = !!dialogData?.apiId ?? false;
     this.updateModeApiId = dialogData?.apiId ?? undefined;
+    this.definitionVersion = dialogData?.definitionVersion ?? '2.0.0';
 
     this.configsForm = new FormGroup({
       importDocumentation: new FormControl(false),
@@ -246,7 +251,7 @@ export class GioApiImportDialogComponent implements OnDestroy {
             with_policies: this.policies.filter((policy) => configsFormValue.importPolicies[policy.id]).map((policy) => policy.id),
             with_policy_paths: configsFormValue.importPolicyPaths,
           },
-          '2.0.0',
+          this.definitionVersion,
           this.updateModeApiId,
         );
         break;
@@ -262,7 +267,7 @@ export class GioApiImportDialogComponent implements OnDestroy {
             with_policies: this.policies.filter((policy) => configsFormValue.importPolicies[policy.id]).map((policy) => policy.id),
             with_policy_paths: configsFormValue.importPolicyPaths,
           },
-          '2.0.0',
+          this.definitionVersion,
           this.updateModeApiId,
         );
         break;
@@ -271,7 +276,7 @@ export class GioApiImportDialogComponent implements OnDestroy {
         importRequest$ = this.apiService.importApiDefinition(
           isFile ? 'graviteeJson' : 'graviteeUrl',
           payload,
-          '2.0.0',
+          this.definitionVersion,
           this.updateModeApiId,
         );
         break;
