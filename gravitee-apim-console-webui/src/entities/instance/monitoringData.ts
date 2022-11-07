110a112610b84d29a7b0c1c6749a8a9b4c958b38
Florent CHAMFROY <florent.chamfroy@graviteesource.com>
2022-11-07 11:26:59 +0100

    feat: create new service for instance management

---
+++ gravitee-apim-console-webui/src/entities/instance/monitoringData.ts
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the License);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an AS IS BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+export interface MonitoringData {
+  cpu: {
+    percent_use: number;
+    load_average: any;
+  };
+  process: {
+    cpu_percent: number;
+    open_file_descriptors: number;
+    max_file_descriptors: number;
+  };
+  jvm: {
+    timestamp: Date;
+    uptime_in_millis: number;
+    heap_used_in_bytes: number;
+    heap_used_percent: number;
+    heap_committed_in_bytes: number;
+    heap_max_in_bytes: number;
+    non_heap_used_in_bytes: number;
+    non_heap_committed_in_bytes: number;
+    young_pool_used_in_bytes: number;
+    young_pool_max_in_bytes: number;
+    young_pool_peak_used_in_bytes: number;
+    young_pool_peak_max_in_bytes: number;
+    survivor_pool_used_in_bytes: number;
+    survivor_pool_max_in_bytes: number;
+    survivor_pool_peak_used_in_bytes: number;
+    survivor_pool_peak_max_in_bytes: number;
+    old_pool_used_in_bytes: number;
+    old_pool_max_in_bytes: number;
+    old_pool_peak_used_in_bytes: number;
+    old_pool_peak_max_in_bytes: number;
+  };
+  thread: {
+    count: number;
+    peak_count: number;
+  };
+  gc: {
+    young_collection_count: number;
+    young_collection_time_in_millis: number;
+    old_collection_count: number;
+    old_collection_time_in_millis: number;
+  };
+}
---
+++ gravitee-apim-console-webui/src/services-ngx/instance.service.spec.ts
@@ -0,0 +1,98 @@
+/*
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { HttpTestingController } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+
+import { InstanceService } from './instance.service';
+
+import { CONSTANTS_TESTING, GioHttpTestingModule } from '../shared/testing';
+import { fakeInstance, fakeMonitoringData, fakeSearchResult } from '../entities/instance/instance.fixture';
+
+describe('InstanceService', () => {
+  let httpTestingController: HttpTestingController;
+  let instanceService: InstanceService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [GioHttpTestingModule],
+    });
+
+    httpTestingController = TestBed.inject(HttpTestingController);
+    instanceService = TestBed.inject<InstanceService>(InstanceService);
+  });
+
+  describe('get', () => {
+    it('should call the API', (done) => {
+      const instanceId = '5bc17c57-b350-460d-817c-57b350060db3';
+      const fakeInstanceObject = fakeInstance();
+
+      instanceService.get(instanceId).subscribe((instance) => {
+        expect(instance).toMatchObject(fakeInstanceObject);
+        done();
+      });
+
+      const req = httpTestingController.expectOne(`${CONSTANTS_TESTING.env.baseURL}/instances/${instanceId}`);
+      expect(req.request.method).toEqual('GET');
+
+      req.flush(fakeInstanceObject);
+    });
+  });
+
+  describe('getMonitoringData', () => {
+    it('should call the API', (done) => {
+      const instanceId = '5bc17c57-b350-460d-817c-57b350060db3';
+      const monitoringId = '74bf7316-6475-416f-bf73-166475816fc5';
+      const fakeMonitoringDataObject = fakeMonitoringData();
+
+      instanceService.getMonitoringData(instanceId, monitoringId).subscribe((monitoringData) => {
+        expect(monitoringData).toMatchObject(fakeMonitoringDataObject);
+        done();
+      });
+
+      const req = httpTestingController.expectOne(`${CONSTANTS_TESTING.env.baseURL}/instances/${instanceId}/monitoring/${monitoringId}`);
+      expect(req.request.method).toEqual('GET');
+
+      req.flush(fakeMonitoringDataObject);
+    });
+  });
+
+  describe('search', () => {
+    it('should call the API', (done) => {
+      const includeStopped = false;
+      const from = 0;
+      const to = 0;
+      const page = 0;
+      const size = 100;
+      const fakeSearchResults = [fakeSearchResult()];
+
+      instanceService.search(includeStopped, from, to, page, size).subscribe((searchResults) => {
+        expect(searchResults).toMatchObject(fakeSearchResults);
+        done();
+      });
+
+      const req = httpTestingController.expectOne(
+        `${CONSTANTS_TESTING.env.baseURL}/instances/?includeStopped=${includeStopped}&from=${from}&to=${to}&page=${page}&size=${size}`,
+      );
+      expect(req.request.method).toEqual('GET');
+
+      req.flush(fakeSearchResults);
+    });
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
+  });
+});
