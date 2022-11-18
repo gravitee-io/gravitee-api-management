eae3a8ab7687f7aa543539757a39ae6f2b7bb315
Antoine Mouliere <notprovided@notprovided.home>
2022-11-18 15:58:14 +0100

    fix(apim): remove metadata name placeholder

--- gravitee-apim-portal-webui/src/app/pages/application/application-metadata/application-metadata.component.ts
+++ gravitee-apim-portal-webui/src/app/pages/application/application-metadata/application-metadata.component.ts
@@ -155,7 +155,6 @@ export class ApplicationMetadataComponent implements OnInit {
       label: nameLabel,
       type: (item) => (this._isAddFormLine(item) ? 'gv-input' : 'div'),
       attributes: {
-        placeholder: 'Nom de la donnée',
         required: true,
         innerText: (item) => (this._isAddFormLine(item) ? '' : item.name),
         'ongv-input:input': this._onInput.bind(this),
