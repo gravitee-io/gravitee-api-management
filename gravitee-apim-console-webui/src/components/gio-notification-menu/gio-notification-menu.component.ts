01353a4dd6b2c9b55124a1a09311697d0ffc68bc
Guillaume Cusnieux <guillaume.cusnieux@graviteesource.com>
2022-11-21 18:20:22 +0100

    feat: add new notification menu

---
+++ gravitee-apim-console-webui/src/components/gio-notification-menu/gio-notification-menu.component.ts
@@ -0,0 +1,89 @@
+/*
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { EMPTY, Subject } from 'rxjs';
+import { catchError, takeUntil, tap } from 'rxjs/operators';
+
+import { UserNotificationService } from '../../services-ngx/user-notification.service';
+import { UserNotification } from '../../entities/user-notification/userNotification';
+import { SnackBarService } from '../../services-ngx/snack-bar.service';
+
+@Component({
+  selector: 'gio-notification-menu',
+  template: require('./gio-notification-menu.component.html'),
+  styles: [require('./gio-notification-menu.component.scss')],
+})
+export class GioNotificationMenuComponent implements OnInit, OnDestroy {
+  private unsubscribe$ = new Subject();
+  public hasNotifications = false;
+  public userNotificationsCount = 0;
+  public userNotifications: UserNotification[];
+  public isOpen: boolean;
+
+  constructor(public readonly userNotificationService: UserNotificationService, private readonly snackBarService: SnackBarService) {}
+
+  public ngOnInit(): void {
+    this.userNotificationService
+      .getNotificationsAutoFetch()
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe((notificationPagedResult) => {
+        this.userNotificationsCount = notificationPagedResult.page.total_elements;
+        this.hasNotifications = this.userNotificationsCount > 0;
+        this.userNotifications = notificationPagedResult.data;
+      });
+  }
+
+  public ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.unsubscribe();
+  }
+
+  public deleteAll() {
+    this.userNotificationService
+      .getNotifications()
+      .pipe(
+        takeUntil(this.unsubscribe$),
+        tap(() => {
+          this.snackBarService.success('Notifications successfully deleted!');
+        }),
+        catchError(({ error }) => {
+          this.snackBarService.error(error.message);
+          return EMPTY;
+        }),
+      )
+      .subscribe(() => this.unsubscribe$.next());
+  }
+
+  public delete(userNotification: UserNotification) {
+    this.userNotificationService
+      .delete(userNotification.id)
+      .pipe(
+        takeUntil(this.unsubscribe$),
+        tap(() => {
+          this.snackBarService.success('Notification successfully deleted!');
+        }),
+        catchError(({ error }) => {
+          this.snackBarService.error(error.message);
+          return EMPTY;
+        }),
+      )
+      .subscribe(() => this.unsubscribe$.next());
+  }
+
+  onOutsideClick() {
+    this.isOpen = false;
+  }
+}
---
+++ gravitee-apim-console-webui/src/services-ngx/user-notification.service.spec.ts
@@ -0,0 +1,71 @@
+/*
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { HttpTestingController } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+
+import { UserNotificationService } from './user-notification.service';
+
+import { CONSTANTS_TESTING, GioHttpTestingModule } from '../shared/testing';
+import { fakeUserNotification } from '../entities/user-notification/userNotification.fixture';
+
+describe('UserNotification', () => {
+  let httpTestingController: HttpTestingController;
+  let userNotificationService: UserNotificationService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [GioHttpTestingModule],
+    });
+
+    httpTestingController = TestBed.inject(HttpTestingController);
+    userNotificationService = TestBed.inject<UserNotificationService>(UserNotificationService);
+  });
+
+  describe('getNotifications', () => {
+    it('should call the API', (done) => {
+      const userNotifications = [fakeUserNotification()];
+
+      userNotificationService.getNotifications().subscribe((response) => {
+        expect(response).toStrictEqual(userNotifications);
+        done();
+      });
+
+      httpTestingController
+        .expectOne({ method: 'GET', url: `${CONSTANTS_TESTING.org.baseURL}/user/notifications` })
+        .flush(userNotifications);
+    });
+  });
+
+  describe('delete', () => {
+    it('should call the API', (done) => {
+      userNotificationService.delete('12345').subscribe(() => done());
+
+      httpTestingController.expectOne({ method: 'DELETE', url: `${CONSTANTS_TESTING.org.baseURL}/user/notifications/12345` }).flush(null);
+    });
+  });
+
+  describe('deleteAll', () => {
+    it('should call the API', (done) => {
+      userNotificationService.deleteAll().subscribe(() => done());
+
+      httpTestingController.expectOne({ method: 'DELETE', url: `${CONSTANTS_TESTING.org.baseURL}/user/notifications` }).flush(null);
+    });
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
+  });
+});
