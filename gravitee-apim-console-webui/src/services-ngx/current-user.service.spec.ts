c94478c21a6b4d7ca630975aecb6557eba85f6ed
ThibaudAv <thibaud.avenier@gmail.com>
2022-11-22 20:26:00 +0100

    feat(console): disable shading tags when needed in plan form
    Same rules as before

---
+++ gravitee-apim-console-webui/src/services-ngx/current-user.service.spec.ts
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { HttpTestingController } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+
+import { CurrentUserService } from './current-user.service';
+
+import { CONSTANTS_TESTING, GioHttpTestingModule } from '../shared/testing';
+
+describe('CurrentUserService', () => {
+  let httpTestingController: HttpTestingController;
+  let currentUserService: CurrentUserService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [GioHttpTestingModule],
+    });
+
+    httpTestingController = TestBed.inject(HttpTestingController);
+    currentUserService = TestBed.inject<CurrentUserService>(CurrentUserService);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
+  });
+
+  describe('getTags', () => {
+    it('should return users', (done) => {
+      const fakeTags = ['tag1', 'tag2'];
+
+      currentUserService.getTags().subscribe((tags) => {
+        expect(tags).toEqual(fakeTags);
+        done();
+      });
+
+      httpTestingController
+        .expectOne({
+          method: 'GET',
+          url: `${CONSTANTS_TESTING.org.baseURL}/user/tags`,
+        })
+        .flush(fakeTags);
+    });
+  });
+});
