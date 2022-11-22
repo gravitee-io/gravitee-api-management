67b72fa465d80de1ddb39bc015f441fadc6601e6
Okhelifi <ouahid.khelifi@gmail.com>
2022-11-22 11:08:04 +0100

    feat(console): close plan

--- gravitee-apim-console-webui/src/management/api/portal/plans/api-portal-plan-list/api-portal-plan-list.component.ts
+++ gravitee-apim-console-webui/src/management/api/portal/plans/api-portal-plan-list/api-portal-plan-list.component.ts
@@ -13,22 +13,29 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
-import { catchError, filter, map, switchMap, takeUntil, tap } from 'rxjs/operators';
-import { combineLatest, EMPTY, Subject } from 'rxjs';
-import { StateService } from '@uirouter/core';
-import { CdkDragDrop } from '@angular/cdk/drag-drop';
-import { orderBy } from 'lodash';
-import { GioConfirmDialogComponent, GioConfirmDialogData } from '@gravitee/ui-particles-angular';
-import { MatDialog } from '@angular/material/dialog';
-import { IRootScopeService } from 'angular';
+import { Component, Inject, OnDestroy, OnInit } from "@angular/core";
+import { catchError, filter, map, switchMap, takeUntil, tap } from "rxjs/operators";
+import { combineLatest, EMPTY, Subject } from "rxjs";
+import { StateService } from "@uirouter/core";
+import { CdkDragDrop } from "@angular/cdk/drag-drop";
+import { orderBy } from "lodash";
+import {
+  GioConfirmAndValidateDialogComponent,
+  GioConfirmAndValidateDialogData,
+  GioConfirmDialogComponent,
+  GioConfirmDialogData
+} from "@gravitee/ui-particles-angular";
+import { MatDialog } from "@angular/material/dialog";
+import { IRootScopeService } from "angular";
 
-import { AjsRootScope, UIRouterState, UIRouterStateParams } from '../../../../../ajs-upgraded-providers';
-import { PlanService } from '../../../../../services-ngx/plan.service';
-import { Api, API_PLAN_STATUS, ApiPlan, ApiPlanStatus } from '../../../../../entities/api';
-import { ApiService } from '../../../../../services-ngx/api.service';
-import { SnackBarService } from '../../../../../services-ngx/snack-bar.service';
-import { GioPermissionService } from '../../../../../shared/components/gio-permission/gio-permission.service';
+import { AjsRootScope, UIRouterState, UIRouterStateParams } from "../../../../../ajs-upgraded-providers";
+import { PlanService } from "../../../../../services-ngx/plan.service";
+import { Api, API_PLAN_STATUS, ApiPlan, ApiPlanStatus } from "../../../../../entities/api";
+import { ApiService } from "../../../../../services-ngx/api.service";
+import { SubscriptionService } from "../../../../../services-ngx/subscription.service";
+import { GioPermissionService } from "../../../../../shared/components/gio-permission/gio-permission.service";
+import { SnackBarService } from "../../../../../services-ngx/snack-bar.service";
+import { PlanSecurityType } from "../../../../../entities/plan/plan";
 
 @Component({
   selector: 'api-portal-plan-list',
@@ -52,6 +59,7 @@ export class ApiPortalPlanListComponent implements OnInit, OnDestroy {
     @Inject(AjsRootScope) private readonly ajsRootScope: IRootScopeService,
     private readonly plansService: PlanService,
     private readonly apiService: ApiService,
+    private readonly subscriptionService: SubscriptionService,
     private readonly permissionService: GioPermissionService,
     private readonly matDialog: MatDialog,
     private readonly snackBarService: SnackBarService,
@@ -183,6 +191,62 @@ export class ApiPortalPlanListComponent implements OnInit, OnDestroy {
           this.snackBarService.success(`The plan ${plan.name} has been deprecated with success.`);
           this.ajsRootScope.$broadcast('planChangeSuccess', { state: 'DEPRECATED' });
           this.searchPlansByStatus('DEPRECATED');
+        }),
+      )
+      .subscribe();
+  }
+
+  public closePlan(plan: ApiPlan): void {
+    this.subscriptionService
+      .getApiSubscriptionsByPlan(plan.api, plan.id)
+      .pipe(
+        takeUntil(this.unsubscribe$),
+        switchMap((subscriptions) => {
+          let content = '';
+          if (plan.security === PlanSecurityType.KEY_LESS) {
+            content = 'A keyless plan may have consumers. <br/>' + 'By closing this plan you will remove free access to this API.';
+          } else {
+            if (subscriptions.page.size === 0) {
+              content = 'No subscription is associated to this plan. You can delete it safely.';
+            } else if (subscriptions.page.size > 0) {
+              content = `There are <code>subscriptions</code> subscription(s) associated to this plan.<br/> By closing this plan, all relative active subscriptions will also be closed.`;
+            }
+          }
+          let confirmButton = 'Yes, close this plan.';
+          if (subscriptions.page.size === 0 && plan.security === PlanSecurityType.API_KEY) {
+            confirmButton = 'Yes, delete this plan';
+          }
+          return this.matDialog
+            .open<GioConfirmAndValidateDialogComponent, GioConfirmAndValidateDialogData>(GioConfirmAndValidateDialogComponent, {
+              width: '500px',
+              data: {
+                title: `Close plan`,
+                warning: `This operation is irreversible.`,
+                validationMessage: `Please, type in the name of the plan <code>${plan.name}</code> to confirm.`,
+                validationValue: plan.name,
+                content,
+                confirmButton,
+              },
+              role: 'alertdialog',
+              id: 'closePlanDialog',
+            })
+            .afterClosed();
+        }),
+        filter((confirm) => confirm === true),
+        switchMap(() =>
+          this.plansService.close(this.api, {
+            ...plan,
+            status: 'CLOSED',
+          }),
+        ),
+        catchError(({ error }) => {
+          this.snackBarService.error(error.message);
+          return EMPTY;
+        }),
+        map((plan) => {
+          this.snackBarService.success(`The plan ${plan.name} has been closed with success.`);
+          this.ajsRootScope.$broadcast('planChangeSuccess', { state: 'CLOSED' });
+          this.searchPlansByStatus('CLOSED');
         }),
       )
       .subscribe();
