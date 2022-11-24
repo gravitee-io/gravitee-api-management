86ab344c426fef1dd7614d48c824dbb9cde04eb3
ThibaudAv <thibaud.avenier@gmail.com>
2022-11-24 12:14:42 +0100

    feat(console): impl secure step in plan page

--- gravitee-apim-console-webui/src/management/api/portal/plans/plan/edit/2-secure-step/plan-edit-secure-step.component.ts
+++ gravitee-apim-console-webui/src/management/api/portal/plans/plan/edit/2-secure-step/plan-edit-secure-step.component.ts
@@ -13,8 +13,41 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, Inject, Input, OnDestroy, OnInit } from '@angular/core';
+import { camelCase } from 'lodash';
 import { Subject } from 'rxjs';
+import { FormControl, FormGroup } from '@angular/forms';
+import { filter, map, switchMap, takeUntil, tap } from 'rxjs/operators';
+import '@gravitee/ui-components/wc/gv-schema-form';
+
+import { Constants } from '../../../../../../../entities/Constants';
+import { PlanSecurityType } from '../../../../../../../entities/plan/plan';
+import { PolicyService } from '../../../../../../../services-ngx/policy.service';
+import { ResourceService } from '../../../../../../../services-ngx/resource.service';
+import { ResourceListItem } from '../../../../../../../entities/resource/resourceListItem';
+import { Api } from '../../../../../../../entities/api';
+
+const allSecurityTypes = [
+  {
+    id: PlanSecurityType.OAUTH2,
+    name: 'OAuth2',
+    policy: 'oauth2',
+  },
+  {
+    id: PlanSecurityType.JWT,
+    name: 'JWT',
+    policy: 'jwt',
+  },
+  {
+    id: PlanSecurityType.API_KEY,
+    name: 'API Key',
+    policy: 'api-key',
+  },
+  {
+    id: PlanSecurityType.KEY_LESS,
+    name: 'Keyless (public)',
+  },
+];
 
 @Component({
   selector: 'plan-edit-secure-step',
@@ -24,14 +57,81 @@ import { Subject } from 'rxjs';
 export class PlanEditSecureStepComponent implements OnInit, OnDestroy {
   private unsubscribe$: Subject<boolean> = new Subject<boolean>();
 
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
-  constructor() {}
+  public secureForm: FormGroup;
 
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
-  ngOnInit(): void {}
+  public securityTypes = allSecurityTypes.filter((securityType) => {
+    return Object.entries(this.constants.env?.settings?.plan?.security ?? {}).map(([key, value]) => {
+      return camelCase(securityType.id) === camelCase(key) && value?.enabled;
+    });
+  });
+
+  public securityConfigSchema: unknown;
+
+  private resourceTypes: ResourceListItem[];
+
+  @Input()
+  public api: Api;
+
+  constructor(
+    @Inject('Constants') private readonly constants: Constants,
+    private readonly policyService: PolicyService,
+    private readonly resourceService: ResourceService,
+  ) {}
+
+  ngOnInit(): void {
+    this.secureForm = new FormGroup({
+      securityTypes: new FormControl(),
+      securityConfig: new FormControl({
+        value: {},
+        disabled: false,
+      }),
+      selectionRule: new FormControl(),
+    });
+
+    this.secureForm
+      .get('securityTypes')
+      .valueChanges.pipe(
+        takeUntil(this.unsubscribe$),
+        tap(() => (this.securityConfigSchema = undefined)),
+        filter((securityType) => securityType !== PlanSecurityType.KEY_LESS),
+        map((securityType) => this.securityTypes.find((type) => type.id === securityType).policy),
+        switchMap((securityTypePolicy) => this.policyService.getSchema(securityTypePolicy)),
+      )
+      .subscribe((schema) => {
+        this.securityConfigSchema = schema;
+      });
+
+    this.resourceService
+      .list({ expandSchema: false, expandIcon: true })
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe((resources) => {
+        this.resourceTypes = resources;
+      });
+  }
 
   ngOnDestroy() {
     this.unsubscribe$.next(true);
     this.unsubscribe$.unsubscribe();
+  }
+
+  onFetchResources(event) {
+    if (this.resourceTypes && this.api?.resources) {
+      const { currentTarget, regexTypes } = event.detail;
+      const options = this.api.resources
+        .filter((resource) => regexTypes == null || new RegExp(regexTypes).test(resource.type))
+        .map((resource) => {
+          const resourceType = this.resourceTypes.find((type) => type.id === resource.type);
+          const row = document.createElement('gv-row');
+          const picture = resourceType.icon ? resourceType.icon : null;
+          (row as any).item = { picture, name: resource.name };
+          return {
+            element: row,
+            value: resource.name,
+            id: resource.type,
+          };
+        });
+
+      currentTarget.options = options;
+    }
   }
 }
