c9f5a67a297362d33161f7e22e45cdee0da8739f
ThibaudAv <thibaud.avenier@gmail.com>
2022-12-09 12:35:12 +0100

    ci: fix run generate release notes with right version branch

--- release/steps/4-generate_release_notes.mjs
+++ release/steps/4-generate_release_notes.mjs
@@ -13,7 +13,7 @@ console.log(chalk.blue(`Triggering Release Notes generation Pipeline`));
 // Use the preconfigured payload from config folder with the good parameters
 // For release-notes generation, always work on master
 const body = {
-  branch: 'master',
+  branch: versions.branch,
   parameters: {
     gio_action: 'release_notes_apim',
     dry_run: isDryRun(),
