d552bf4de24345aa6154377d4548f2e930155a79
Okhelifi <ouahid.khelifi@gmail.com>
2022-12-12 17:17:44 +0100

    feat(console): list all path mapping

--- gravitee-apim-console-webui/src/management/api/analytics/pathMappings/api-path-mappings.component.ts
+++ gravitee-apim-console-webui/src/management/api/analytics/pathMappings/api-path-mappings.component.ts
@@ -13,11 +13,51 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { Component } from '@angular/core';
+import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { takeUntil, tap } from 'rxjs/operators';
+import { sortBy } from 'lodash';
+
+import { UIRouterStateParams } from '../../../../ajs-upgraded-providers';
+import { ApiService } from '../../../../services-ngx/api.service';
+import { Api } from '../../../../entities/api';
+
+interface PathMappingDS {
+  path: string;
+}
 
 @Component({
   selector: 'api-path-mappings',
   template: require('./api-path-mappings.component.html'),
   styles: [require('./api-path-mappings.component.scss')],
 })
-export class ApiPathMappingsComponent {}
+export class ApiPathMappingsComponent implements OnInit, OnDestroy {
+  private unsubscribe$: Subject<boolean> = new Subject<boolean>();
+  public displayedColumns = ['path', 'actions'];
+  public pathMappingsDS: PathMappingDS[] = [];
+  public isLoadingData = true;
+
+  constructor(@Inject(UIRouterStateParams) private readonly ajsStateParams, private readonly apiService: ApiService) {}
+
+  public ngOnInit(): void {
+    this.apiService
+      .get(this.ajsStateParams.apiId)
+      .pipe(
+        takeUntil(this.unsubscribe$),
+        tap((api) => {
+          this.pathMappingsDS = this.toPathMappingDS(api);
+          this.isLoadingData = false;
+        }),
+      )
+      .subscribe();
+  }
+
+  public ngOnDestroy(): void {
+    this.unsubscribe$.next(true);
+    this.unsubscribe$.unsubscribe();
+  }
+
+  private toPathMappingDS(api: Api): PathMappingDS[] {
+    return sortBy(api.path_mappings).map((path) => ({ path }));
+  }
+}
