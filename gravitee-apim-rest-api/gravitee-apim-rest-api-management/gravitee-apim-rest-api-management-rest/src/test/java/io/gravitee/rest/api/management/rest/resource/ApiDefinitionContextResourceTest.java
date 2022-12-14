b719486a9fe99e7ea3e6ae6ca3d6dfae11d6822a
Antoine CORDIER <antoine.cordier@graviteesource.com>
2022-12-14 12:04:19 +0100

    feat: add resource to set api definition context
    see https://gravitee.atlassian.net/browse/APIM-15

---
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-management/gravitee-apim-rest-api-management-rest/src/test/java/io/gravitee/rest/api/management/rest/resource/ApiDefinitionContextResourceTest.java
@@ -0,0 +1,108 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.rest.api.management.rest.resource;
+
+import static io.gravitee.definition.model.DefinitionContext.*;
+import static javax.ws.rs.client.Entity.*;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
+
+import io.gravitee.common.http.HttpStatusCode;
+import io.gravitee.rest.api.model.api.DefinitionContextEntity;
+import io.gravitee.rest.api.service.exceptions.TechnicalManagementException;
+import javax.ws.rs.core.Response;
+import org.junit.Test;
+
+/**
+ * @author GraviteeSource Team
+ */
+public class ApiDefinitionContextResourceTest extends AbstractResourceTest {
+
+    private static final String API_ID = "d635e8d0-8b19-40f6-8cc3-e53633109e08";
+    private static final String DEFINITION_CONTEXT_PATH = "definition-context";
+
+    @Override
+    protected String contextPath() {
+        return "apis/";
+    }
+
+    @Test
+    public void shouldReturn500IfTechnicalException() {
+        doThrow(new TechnicalManagementException()).when(definitionContextService).setDefinitionContext(eq(API_ID), any());
+
+        Response response = envTarget(API_ID).path(DEFINITION_CONTEXT_PATH).request().put(json(newKubernetesContext()));
+
+        assertEquals(HttpStatusCode.INTERNAL_SERVER_ERROR_500, response.getStatus());
+    }
+
+    @Test
+    public void shouldReturn403IfNotGranted() {
+        when(permissionService.hasPermission(any(), any(), any(), any())).thenReturn(false);
+        Response response = envTarget(API_ID).path(DEFINITION_CONTEXT_PATH).request().put(json(newKubernetesContext()));
+        assertEquals(HttpStatusCode.FORBIDDEN_403, response.getStatus());
+    }
+
+    @Test
+    public void shouldReturn200AndSetDefinitionContext() {
+        Response response = envTarget(API_ID).path(DEFINITION_CONTEXT_PATH).request().put(json(newKubernetesContext()));
+        assertEquals(HttpStatusCode.OK_200, response.getStatus());
+        verify(definitionContextService, times(1))
+            .setDefinitionContext(
+                eq(API_ID),
+                argThat(
+                    context -> {
+                        assertThat(context).usingRecursiveComparison().isEqualTo(newKubernetesContext());
+                        return true;
+                    }
+                )
+            );
+    }
+
+    @Test
+    public void shouldReturn400WithNullOrigin() {
+        DefinitionContextEntity definitionContextEntity = new DefinitionContextEntity(null, MODE_FULLY_MANAGED);
+        Response response = envTarget(API_ID).path(DEFINITION_CONTEXT_PATH).request().put(json(definitionContextEntity));
+        assertEquals(HttpStatusCode.BAD_REQUEST_400, response.getStatus());
+    }
+
+    @Test
+    public void shouldReturn400WithUnknownOrigin() {
+        DefinitionContextEntity definitionContextEntity = new DefinitionContextEntity("unknown", MODE_FULLY_MANAGED);
+        Response response = envTarget(API_ID).path(DEFINITION_CONTEXT_PATH).request().put(json(definitionContextEntity));
+        assertEquals(HttpStatusCode.BAD_REQUEST_400, response.getStatus());
+    }
+
+    @Test
+    public void shouldReturn400WithNullMode() {
+        DefinitionContextEntity definitionContextEntity = new DefinitionContextEntity(ORIGIN_KUBERNETES, null);
+        Response response = envTarget(API_ID).path(DEFINITION_CONTEXT_PATH).request().put(json(definitionContextEntity));
+        assertEquals(HttpStatusCode.BAD_REQUEST_400, response.getStatus());
+    }
+
+    @Test
+    public void shouldReturn400WithUnknownMode() {
+        DefinitionContextEntity definitionContextEntity = new DefinitionContextEntity(ORIGIN_KUBERNETES, "unknown");
+        Response response = envTarget(API_ID).path(DEFINITION_CONTEXT_PATH).request().put(json(definitionContextEntity));
+        assertEquals(HttpStatusCode.BAD_REQUEST_400, response.getStatus());
+    }
+
+    private static DefinitionContextEntity newKubernetesContext() {
+        return new DefinitionContextEntity(ORIGIN_KUBERNETES, MODE_FULLY_MANAGED);
+    }
+}
---
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-model/src/main/java/io/gravitee/rest/api/model/api/DefinitionContextEntity.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.rest.api.model.api;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import lombok.*;
+
+/**
+ * @author GraviteeSource Team
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class DefinitionContextEntity {
+
+    @NotNull
+    @Pattern(regexp = "(kubernetes|management)")
+    private String origin;
+
+    @NotNull
+    @Pattern(regexp = "(fully_managed|api_definition_only)")
+    private String mode;
+}
