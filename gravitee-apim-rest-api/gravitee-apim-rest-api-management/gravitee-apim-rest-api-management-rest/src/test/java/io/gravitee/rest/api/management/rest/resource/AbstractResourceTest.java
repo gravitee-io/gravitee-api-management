efeeba746c4788208d5061123d12c63af8bde2bc
Antoine CORDIER <antoine.cordier@graviteesource.com>
2022-12-14 12:04:19 +0100

    feat: add resource to set api definition context
    see https://gravitee.atlassian.net/browse/APIM-15

--- gravitee-apim-rest-api/gravitee-apim-rest-api-management/gravitee-apim-rest-api-management-rest/src/test/java/io/gravitee/rest/api/management/rest/resource/AbstractResourceTest.java
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-management/gravitee-apim-rest-api-management-rest/src/test/java/io/gravitee/rest/api/management/rest/resource/AbstractResourceTest.java
@@ -29,52 +29,7 @@ import io.gravitee.rest.api.model.permissions.RolePermission;
 import io.gravitee.rest.api.security.authentication.AuthenticationProvider;
 import io.gravitee.rest.api.security.cookies.CookieGenerator;
 import io.gravitee.rest.api.security.utils.AuthoritiesProvider;
-import io.gravitee.rest.api.service.AccessControlService;
-import io.gravitee.rest.api.service.AlertAnalyticsService;
-import io.gravitee.rest.api.service.AlertService;
-import io.gravitee.rest.api.service.AnalyticsService;
-import io.gravitee.rest.api.service.ApiDuplicatorService;
-import io.gravitee.rest.api.service.ApiExportService;
-import io.gravitee.rest.api.service.ApiKeyService;
-import io.gravitee.rest.api.service.ApiMetadataService;
-import io.gravitee.rest.api.service.ApiService;
-import io.gravitee.rest.api.service.ApplicationMetadataService;
-import io.gravitee.rest.api.service.ApplicationService;
-import io.gravitee.rest.api.service.AuditService;
-import io.gravitee.rest.api.service.CategoryService;
-import io.gravitee.rest.api.service.ConfigService;
-import io.gravitee.rest.api.service.CustomUserFieldService;
-import io.gravitee.rest.api.service.DebugApiService;
-import io.gravitee.rest.api.service.EnvironmentService;
-import io.gravitee.rest.api.service.FetcherService;
-import io.gravitee.rest.api.service.GroupService;
-import io.gravitee.rest.api.service.InstallationService;
-import io.gravitee.rest.api.service.JsonPatchService;
-import io.gravitee.rest.api.service.LogsService;
-import io.gravitee.rest.api.service.MediaService;
-import io.gravitee.rest.api.service.MembershipService;
-import io.gravitee.rest.api.service.MessageService;
-import io.gravitee.rest.api.service.NotifierService;
-import io.gravitee.rest.api.service.OrganizationService;
-import io.gravitee.rest.api.service.PageService;
-import io.gravitee.rest.api.service.ParameterService;
-import io.gravitee.rest.api.service.PermissionService;
-import io.gravitee.rest.api.service.PlanService;
-import io.gravitee.rest.api.service.PolicyService;
-import io.gravitee.rest.api.service.QualityMetricsService;
-import io.gravitee.rest.api.service.RatingService;
-import io.gravitee.rest.api.service.RoleService;
-import io.gravitee.rest.api.service.SocialIdentityProviderService;
-import io.gravitee.rest.api.service.SubscriptionService;
-import io.gravitee.rest.api.service.SwaggerService;
-import io.gravitee.rest.api.service.TagService;
-import io.gravitee.rest.api.service.TaskService;
-import io.gravitee.rest.api.service.TicketService;
-import io.gravitee.rest.api.service.TokenService;
-import io.gravitee.rest.api.service.TopApiService;
-import io.gravitee.rest.api.service.UserService;
-import io.gravitee.rest.api.service.VirtualHostService;
-import io.gravitee.rest.api.service.WorkflowService;
+import io.gravitee.rest.api.service.*;
 import io.gravitee.rest.api.service.common.ExecutionContext;
 import io.gravitee.rest.api.service.configuration.application.ApplicationTypeService;
 import io.gravitee.rest.api.service.configuration.application.ClientRegistrationService;
@@ -315,6 +270,9 @@ public abstract class AbstractResourceTest extends JerseySpringTest {
 
     @Autowired
     protected MediaService mediaService;
+
+    @Autowired
+    protected ApiDefinitionContextService definitionContextService;
 
     @Autowired
     protected LogsService logsService;
@@ -681,6 +639,11 @@ public abstract class AbstractResourceTest extends JerseySpringTest {
         @Bean
         public LogsService logsService() {
             return mock(LogsService.class);
+        }
+
+        @Bean
+        public ApiDefinitionContextService definitionContextService() {
+            return mock(ApiDefinitionContextService.class);
         }
     }
 
