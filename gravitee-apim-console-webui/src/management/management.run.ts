2d8e6673c04d82a510b1b4487d3081188dfb8657
Florent CHAMFROY <florent.chamfroy@graviteesource.com>
2022-12-15 09:06:00 +0100

    fix: load bootstrap on legacy angularJS pages
    https://gravitee.atlassian.net/browse/APIM-397

--- gravitee-apim-console-webui/src/management/management.run.ts
+++ gravitee-apim-console-webui/src/management/management.run.ts
@@ -250,7 +250,7 @@ function runBlock(
   $transitions.onSuccess({}, function (trans) {
     const toState = trans.to();
     const useAngularMaterial = toState.data && toState.data.useAngularMaterial;
-    const mainContainer = angular.element(document.querySelector('.gv-main-container, .gravitee-no-sidenav-container'));
+    const mainContainer = angular.element(document.querySelector('.gravitee-no-sidenav-container, .gv-sub-content'));
     if (useAngularMaterial) {
       mainContainer.removeClass('bootstrap');
     } else {
