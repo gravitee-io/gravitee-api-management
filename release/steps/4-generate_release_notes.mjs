de3e5967590cf6244718aa01e2799672da23e224
ThibaudAv <thibaud.avenier@gmail.com>
2023-01-03 11:07:11 +0100

    ci(release): fix release changelog pr comment for doc

--- release/steps/4-generate_release_notes.mjs
+++ release/steps/4-generate_release_notes.mjs
@@ -1,12 +1,13 @@
 #!/usr/bin/env zx
 
 import { checkToken } from '../helpers/circleci-helper.mjs';
-import { extractVersion } from '../helpers/version-helper.mjs';
+import { computeVersion, extractVersion } from '../helpers/version-helper.mjs';
 import { isDryRun } from '../helpers/option-helper.mjs';
 
 await checkToken();
 
 const releasingVersion = await extractVersion();
+const versions = computeVersion(releasingVersion);
 
 console.log(chalk.blue(`Triggering Release Notes generation Pipeline`));
 
