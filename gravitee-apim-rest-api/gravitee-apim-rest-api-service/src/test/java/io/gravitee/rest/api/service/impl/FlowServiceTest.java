ec1abe6c8560ff5da7284191ff72e4e54b7630e3
Guillaume Cusnieux <guillaume.cusnieux@graviteesource.com>
2023-01-19 17:17:10 +0100

    fix: use id to save flows

--- gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/test/java/io/gravitee/rest/api/service/impl/FlowServiceTest.java
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/test/java/io/gravitee/rest/api/service/impl/FlowServiceTest.java
@@ -17,10 +17,29 @@ package io.gravitee.rest.api.service.impl;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.gravitee.definition.model.flow.Flow;
+import io.gravitee.definition.model.flow.Operator;
+import io.gravitee.repository.exceptions.TechnicalException;
+import io.gravitee.repository.management.api.FlowRepository;
+import io.gravitee.repository.management.api.GroupRepository;
+import io.gravitee.repository.management.model.flow.FlowOperator;
+import io.gravitee.repository.management.model.flow.FlowReferenceType;
+import io.gravitee.rest.api.service.converter.FlowConverter;
 import io.gravitee.rest.api.service.impl.configuration.flow.FlowServiceImpl;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
 
 /**
@@ -30,7 +49,14 @@ import org.mockito.junit.MockitoJUnitRunner;
 @RunWith(MockitoJUnitRunner.class)
 public class FlowServiceTest {
 
+    @InjectMocks
     private FlowServiceImpl flowService = new FlowServiceImpl();
+
+    @Mock
+    private FlowRepository flowRepository;
+
+    @Spy
+    private FlowConverter flowConverter = new FlowConverter(new ObjectMapper());
 
     @Test
     public void shouldGetConfigurationSchemaForm() {
@@ -129,5 +155,88 @@ public class FlowServiceTest {
             "}\n",
             flowService.getConfigurationSchemaForm()
         );
+    }
+
+    @Test
+    public void shouldFindByReference() throws TechnicalException {
+        List<io.gravitee.repository.management.model.flow.Flow> flows = new ArrayList<>();
+        flows.addAll(generateRepoFlows());
+
+        when(flowRepository.findByReference(FlowReferenceType.API, "api-id")).thenReturn(flows);
+
+        List<Flow> byReference = flowService.findByReference(FlowReferenceType.API, "api-id");
+
+        assertNotNull(byReference);
+        assertEquals(byReference.stream().map(f -> f.getId()).collect(Collectors.joining(", ", "[", "]")), "[flow-0, flow-1, flow-2]");
+    }
+
+    @Test
+    public void shouldSave() throws TechnicalException {
+        List<io.gravitee.repository.management.model.flow.Flow> flows = new ArrayList<>();
+        flows.addAll(generateRepoFlows());
+
+        when(flowRepository.findByReference(FlowReferenceType.API, "api-id")).thenReturn(flows);
+
+        io.gravitee.repository.management.model.flow.Flow updatedFlow = createRepoFlow(1);
+        when(flowRepository.update(any())).thenReturn(updatedFlow);
+
+        io.gravitee.repository.management.model.flow.Flow createdFlow4 = createRepoFlow(4);
+        io.gravitee.repository.management.model.flow.Flow createdFlow5 = createRepoFlow(5);
+        when(flowRepository.create(any())).thenReturn(createdFlow4, createdFlow5);
+
+        List<Flow> savedFlows = new ArrayList<>();
+        savedFlows.add(createFlow("flow-1"));
+        savedFlows.add(createFlow("ignore"));
+        savedFlows.add(createFlow(null));
+
+        List<Flow> byReference = flowService.save(FlowReferenceType.API, "api-id", savedFlows);
+
+        verify(flowRepository, times(1)).delete("flow-0");
+        verify(flowRepository, times(1)).delete("flow-2");
+        verify(flowRepository, times(1)).update(any());
+        verify(flowRepository, times(2)).create(any());
+        assertNotNull(byReference);
+        assertEquals(3, byReference.size());
+    }
+
+    @Test
+    public void shouldSaveWhenNoFlowInDb() throws TechnicalException {
+        when(flowRepository.findByReference(FlowReferenceType.API, "api-id")).thenReturn(List.of());
+
+        io.gravitee.repository.management.model.flow.Flow createdFlow1 = createRepoFlow(1);
+        io.gravitee.repository.management.model.flow.Flow createdFlow2 = createRepoFlow(2);
+        when(flowRepository.create(any())).thenReturn(createdFlow1, createdFlow2);
+
+        List<Flow> savedFlows = new ArrayList<>();
+        savedFlows.add(createFlow("flow-1"));
+        savedFlows.add(createFlow(null));
+
+        List<Flow> byReference = flowService.save(FlowReferenceType.API, "api-id", savedFlows);
+
+        verify(flowRepository, never()).delete(any());
+        verify(flowRepository, never()).update(any());
+        verify(flowRepository, times(2)).create(any());
+        assertNotNull(byReference);
+        assertEquals(2, byReference.size());
+    }
+
+    private Flow createFlow(String id) {
+        Flow flow = new Flow();
+        if (id != null) {
+            flow.setId(id);
+        }
+        return flow;
+    }
+
+    private Collection<? extends io.gravitee.repository.management.model.flow.Flow> generateRepoFlows() {
+        return List.of(createRepoFlow(1), createRepoFlow(2), createRepoFlow(0));
+    }
+
+    private io.gravitee.repository.management.model.flow.Flow createRepoFlow(int order) {
+        io.gravitee.repository.management.model.flow.Flow flow = mock(io.gravitee.repository.management.model.flow.Flow.class);
+        when(flow.getId()).thenReturn("flow-" + order);
+        when(flow.getOrder()).thenReturn(order);
+        when(flow.getOperator()).thenReturn(FlowOperator.STARTS_WITH);
+        return flow;
     }
 }
