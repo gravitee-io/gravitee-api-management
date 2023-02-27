42ab9ecd0ecfb70cc0f74326803f90fc33a109ba
ThibaudAv <thibaud.avenier@gmail.com>
2023-02-27 15:12:13 +0100

    chore(console): add decorator to use GioMatConfigModule

--- gravitee-apim-console-webui/.storybook/preview.ts
+++ gravitee-apim-console-webui/.storybook/preview.ts
@@ -2,10 +2,12 @@
 import '../src/index.scss';
 import { withDesign } from 'storybook-addon-designs';
 import '@angular/compiler';
+import { moduleMetadata } from '@storybook/angular';
+import { GioMatConfigModule } from '@gravitee/ui-particles-angular';
 
 // Set material classes to the Storybook root div
 window.document.body.parentElement.classList.add('mat');
 window.document.body.classList.add('mat-typography');
 window.document.body.classList.add('mat-app-background');
 
-export const decorators = [withDesign];
+export const decorators = [withDesign, moduleMetadata({ imports: [GioMatConfigModule] })];
