0ec9ba97ce49ccf4330f62ea3c95dc68d5922755
Florent CHAMFROY <florent.chamfroy@graviteesource.com>
2023-03-08 18:05:13 +0100

    fix: use JSON SCHEMA to convert YAML to JSON
    https://gravitee.atlassian.net/browse/APIM-840
    https://github.com/gravitee-io/issues/issues/7099

--- gravitee-apim-portal-webui/src/app/utils/yaml-parser.spec.ts
+++ gravitee-apim-portal-webui/src/app/utils/yaml-parser.spec.ts
@@ -32,10 +32,10 @@ describe('yamlToJson', () => {
           example: john@doe.com
       home_phone:
           type: integer
-          example: 0606060606
+          example: "0606060606"
       mobile_phone:
           type: string
-          example: +33606060606
+          example: "+33606060606"
       budget_balance:
           type: integer
           example: -300
@@ -47,18 +47,18 @@ describe('yamlToJson', () => {
           example: -0.1
       rank:
           type: number
-          example: 05
+          example: "05"
     `;
 
     const expected = JSON.stringify({
       birthdate: { type: 'string', format: 'date', example: '1980-12-12' },
-      active: { type: 'boolean', example: 'TRUE' },
+      active: { type: 'boolean', example: true },
       email: { type: 'string', format: 'email', example: 'john@doe.com' },
       home_phone: { type: 'integer', example: '0606060606' },
       mobile_phone: { type: 'string', example: '+33606060606' },
-      budget_balance: { type: 'integer', example: '-300' },
-      error_rate: { type: 'number', example: '0.8' },
-      forecast_error_rate: { type: 'number', example: '-0.1' },
+      budget_balance: { type: 'integer', example: -300 },
+      error_rate: { type: 'number', example: 0.8 },
+      forecast_error_rate: { type: 'number', example: -0.1 },
       rank: { type: 'number', example: '05' },
     });
 
