6fa292bb968c8dd50411d96e896c28959aa58e56
Yann Tavernier <yann.tavernier@graviteesource.com>
2023-03-17 11:14:20 +0100

    feat(api): expose endpoint group schema in rest resources

--- gravitee-apim-rest-api/gravitee-apim-rest-api-management/gravitee-apim-rest-api-management-rest/src/test/java/io/gravitee/rest/api/management/rest/resource/v4/endpoint/EndpointsResourceTest.java
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-management/gravitee-apim-rest-api-management-rest/src/test/java/io/gravitee/rest/api/management/rest/resource/v4/endpoint/EndpointsResourceTest.java
@@ -15,19 +15,16 @@
  */
 package io.gravitee.rest.api.management.rest.resource.v4.endpoint;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.when;
 
 import io.gravitee.common.http.HttpStatusCode;
 import io.gravitee.definition.model.v4.ApiType;
 import io.gravitee.definition.model.v4.ConnectorMode;
-import io.gravitee.plugin.core.api.PluginMoreInformation;
 import io.gravitee.rest.api.management.rest.resource.AbstractResourceTest;
 import io.gravitee.rest.api.model.v4.connector.ConnectorPluginEntity;
 import io.gravitee.rest.api.service.common.GraviteeContext;
-import io.gravitee.rest.api.service.exceptions.TechnicalManagementException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -63,17 +60,17 @@ public class EndpointsResourceTest extends AbstractResourceTest {
         when(endpointConnectorPluginService.findAll()).thenReturn(Set.of(connectorPlugin));
 
         final Response response = envTarget().request().get();
-        assertEquals(HttpStatusCode.OK_200, response.getStatus());
-        final List<Map<String, String>> pluginEntities = response.readEntity(List.class);
-        assertEquals(1, pluginEntities.size());
-        Map<String, String> pluginEntity = pluginEntities.get(0);
-        assertEquals("id", pluginEntity.get("id"));
-        assertEquals("name", pluginEntity.get("name"));
-        assertEquals("1.0", pluginEntity.get("version"));
-        assertEquals(ApiType.MESSAGE.getLabel(), pluginEntity.get("supportedApiType"));
-        ArrayList<String> arrayList = new ArrayList<>();
-        arrayList.add(ConnectorMode.SUBSCRIBE.getLabel());
-        assertEquals(arrayList, pluginEntity.get("supportedModes"));
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatusCode.OK_200);
+        final List<Map<String, Object>> pluginEntities = response.readEntity(List.class);
+        assertThat(pluginEntities).hasSize(1);
+        Map<String, Object> pluginEntity = pluginEntities.get(0);
+        assertThat(pluginEntity)
+            .containsEntry("id", "id")
+            .containsEntry("name", "name")
+            .containsEntry("version", "1.0")
+            .containsEntry("supportedApiType", ApiType.MESSAGE.getLabel())
+            .containsEntry("supportedModes", List.of(ConnectorMode.SUBSCRIBE.getLabel()));
     }
 
     @Test
@@ -85,55 +82,57 @@ public class EndpointsResourceTest extends AbstractResourceTest {
         connectorPlugin.setSupportedApiType(ApiType.MESSAGE);
         connectorPlugin.setSupportedModes(Set.of(ConnectorMode.SUBSCRIBE));
         when(endpointConnectorPluginService.findAll()).thenReturn(Set.of(connectorPlugin));
-        when(endpointConnectorPluginService.getSchema(connectorPlugin.getId())).thenReturn("schema");
-        when(endpointConnectorPluginService.getIcon(connectorPlugin.getId())).thenReturn("icon");
+        when(endpointConnectorPluginService.getSchema(connectorPlugin.getId())).thenReturn("schemaResponse");
+        when(endpointConnectorPluginService.getIcon(connectorPlugin.getId())).thenReturn("iconResponse");
 
         final Response response = envTarget().queryParam("expand", "schema").queryParam("expand", "icon").request().get();
-        assertEquals(HttpStatusCode.OK_200, response.getStatus());
-        final List<Map<String, String>> pluginEntities = response.readEntity(List.class);
-        assertEquals(1, pluginEntities.size());
-        Map<String, String> pluginEntity = pluginEntities.get(0);
-        assertEquals("id", pluginEntity.get("id"));
-        assertEquals("name", pluginEntity.get("name"));
-        assertEquals("1.0", pluginEntity.get("version"));
-        assertEquals(ApiType.MESSAGE.getLabel(), pluginEntity.get("supportedApiType"));
-        ArrayList<String> arrayList = new ArrayList<>();
-        arrayList.add(ConnectorMode.SUBSCRIBE.getLabel());
-        assertEquals(arrayList, pluginEntity.get("supportedModes"));
-        assertEquals("schema", pluginEntity.get("schema"));
-        assertEquals("icon", pluginEntity.get("icon"));
+        assertThat(response.getStatus()).isEqualTo(HttpStatusCode.OK_200);
+        final List<Map<String, Object>> pluginEntities = response.readEntity(List.class);
+        assertThat(pluginEntities).hasSize(1);
+        Map<String, Object> pluginEntity = pluginEntities.get(0);
+        assertThat(pluginEntity)
+            .containsEntry("id", "id")
+            .containsEntry("name", "name")
+            .containsEntry("version", "1.0")
+            .containsEntry("supportedApiType", ApiType.MESSAGE.getLabel())
+            .containsEntry("supportedModes", List.of(ConnectorMode.SUBSCRIBE.getLabel()))
+            .containsEntry("schema", "schemaResponse")
+            .containsEntry("icon", "iconResponse");
     }
 
     @Test
-    public void shouldReturnMoreInformation() {
+    public void shouldReturnAllEndpointsWithSchemaAndEndpointSharedConfigurationSchema() {
         ConnectorPluginEntity connectorPlugin = new ConnectorPluginEntity();
         connectorPlugin.setId("id");
         connectorPlugin.setName("name");
         connectorPlugin.setVersion("1.0");
         connectorPlugin.setSupportedApiType(ApiType.MESSAGE);
         connectorPlugin.setSupportedModes(Set.of(ConnectorMode.SUBSCRIBE));
-        when(endpointConnectorPluginService.findById("id")).thenReturn(connectorPlugin);
+        when(endpointConnectorPluginService.findAll()).thenReturn(Set.of(connectorPlugin));
+        when(endpointConnectorPluginService.getSchema(connectorPlugin.getId())).thenReturn("schemaResponse");
+        when(endpointConnectorPluginService.getIcon(connectorPlugin.getId())).thenReturn("iconResponse");
+        when(endpointConnectorPluginService.getSharedConfigurationSchema(connectorPlugin.getId()))
+            .thenReturn("sharedConfigurationSchemaResponse");
 
-        var moreInformation = new PluginMoreInformation();
-        moreInformation.setDescription("A nice description");
-        moreInformation.setSchemaImg("A nice schema");
-        moreInformation.setDocumentationUrl("foobar");
-        when(endpointConnectorPluginService.getMoreInformation("id")).thenReturn(moreInformation);
+        final Response response = envTarget()
+            .queryParam("expand", "schema")
+            .queryParam("expand", "sharedConfigurationSchema")
+            .queryParam("expand", "icon")
+            .request()
+            .get();
 
-        final Response response = envTarget().path("id").path("/moreInformation").request().get();
-
-        assertEquals(HttpStatusCode.OK_200, response.getStatus());
-        var body = response.readEntity(PluginMoreInformation.class);
-        assertEquals("A nice description", body.getDescription());
-        assertEquals("A nice schema", body.getSchemaImg());
-        assertEquals("foobar", body.getDocumentationUrl());
-    }
-
-    @Test
-    public void shouldReturn500IfMoreInformationError() {
-        doThrow(new TechnicalManagementException()).when(endpointConnectorPluginService).getMoreInformation("id");
-
-        final Response response = envTarget().path("id").path("/moreInformation").request().get();
-        assertEquals(HttpStatusCode.INTERNAL_SERVER_ERROR_500, response.getStatus());
+        assertThat(response.getStatus()).isEqualTo(HttpStatusCode.OK_200);
+        final List<Map<String, Object>> pluginEntities = response.readEntity(List.class);
+        assertThat(pluginEntities).hasSize(1);
+        Map<String, Object> pluginEntity = pluginEntities.get(0);
+        assertThat(pluginEntity)
+            .containsEntry("id", "id")
+            .containsEntry("name", "name")
+            .containsEntry("version", "1.0")
+            .containsEntry("supportedApiType", ApiType.MESSAGE.getLabel())
+            .containsEntry("supportedModes", List.of(ConnectorMode.SUBSCRIBE.getLabel()))
+            .containsEntry("schema", "schemaResponse")
+            .containsEntry("icon", "iconResponse")
+            .containsEntry("sharedConfigurationSchema", "sharedConfigurationSchemaResponse");
     }
 }
