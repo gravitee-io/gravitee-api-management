87eaeb04d391dd68b42edfc3e119ef67518530ca
jourdiw <jourdi.waller@graviteesource.com>
2023-03-23 15:58:32 +0100

    feat(console): create gio request stats component

---
+++ gravitee-apim-console-webui/src/management/home/widgets/gio-request-stats/gio-request-stats.component.ts
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { Component, Input, OnInit } from '@angular/core';
+
+export interface RequestStatsSource {
+  min: number;
+  max: number;
+  avg: number;
+  rps: number;
+  total: number;
+}
+
+@Component({
+  selector: 'gio-request-stats',
+  template: require('./gio-request-stats.component.html'),
+  styles: [require('./gio-request-stats.component.scss')],
+})
+export class GioRequestStatsComponent implements OnInit {
+  @Input()
+  public source: RequestStatsSource;
+
+  public data: RequestStatsSource;
+
+  ngOnInit() {
+    if (this.source) {
+      this.data = this.source;
+    }
+  }
+}
---
+++ gravitee-apim-console-webui/src/management/home/widgets/gio-request-stats/gio-request-stats.harness.ts
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { ComponentHarness } from '@angular/cdk/testing';
+
+export class GioRequestStatsHarness extends ComponentHarness {
+  static hostSelector = '.gio-request-stats';
+  protected getRows = this.locatorForAll('.gio-request-stats__body__row');
+
+  async getMin(): Promise<string> {
+    return this.getValueByLabel('Min');
+  }
+
+  async getMax(): Promise<string> {
+    return this.getValueByLabel('Max');
+  }
+
+  async getAverage(): Promise<string> {
+    return this.getValueByLabel('Average');
+  }
+
+  async getRequestsPerSecond(): Promise<string> {
+    return this.getValueByLabel('Requests per second');
+  }
+
+  async getTotalRequests(): Promise<string> {
+    return this.getValueByLabel('Total number of requests');
+  }
+
+  private async getValueByLabel(label: string): Promise<string> {
+    return this.getRows()
+      .then((rows) => Promise.all(rows.map((t) => t.text())))
+      .then((res) => res.find((t) => t.includes(label)))
+      .then((found) => found.replace(label, ''));
+  }
+}
