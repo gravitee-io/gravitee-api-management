b6740b1d2f724bfafb33d49bc18060ec3e5832eb
Gaëtan Maisse <gaetanmaisse@gmail.com>
2023-03-28 17:38:19 +0200

    fix: allow use of personal token on the Portal API
    Dirty hack due to ec1abe6c8560ff5da7284191ff72e4e54b7630e3,
    after this change the payloadEntity doesn't contain the flow ids yet
    as there were no upgrader to update the last publish_api event.
    So we need to remove the flow ids before comparing the deployed API
    and the current one.
    
    https://gravitee.atlassian.net/browse/APIM-1147
    https://github.com/gravitee-io/issues/issues/8954

--- gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/rest/api/service/impl/ApiServiceImpl.java
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/rest/api/service/impl/ApiServiceImpl.java
@@ -2160,6 +2160,13 @@ public class ApiServiceImpl extends AbstractService implements ApiService {
                     removeDescriptionFromPolicies(api);
                     removeDescriptionFromPolicies(deployedApi);
 
+                    // FIXME: Dirty hack due to ec1abe6c8560ff5da7284191ff72e4e54b7630e3, after this change the
+                    //  payloadEntity doesn't contain the flow ids yet as there were no upgrader to update the last
+                    //  publish_api event. So we need to remove the flow ids before comparing the deployed API and the
+                    //  current one.
+                    removeIdsFromFlows(api);
+                    removeIdsFromFlows(deployedApi);
+
                     sync = apiSynchronizationProcessor.processCheckSynchronization(deployedApi, api);
 
                     // 2_ If API definition is synchronized, check if there is any modification for API's plans
@@ -2182,6 +2189,11 @@ public class ApiServiceImpl extends AbstractService implements ApiService {
         }
 
         return false;
+    }
+
+    private void removeIdsFromFlows(ApiEntity api) {
+        api.getFlows().forEach(flow -> flow.setId(null));
+        api.getPlans().forEach(plan -> plan.getFlows().forEach(flow -> flow.setId(null)));
     }
 
     private void removeDescriptionFromPolicies(final ApiEntity api) {
