522f2af1f6c1cd3592bb66f14e5dcb5cc47b3d85
Antoine CORDIER <antoine.cordier@graviteesource.com>
2023-04-20 17:19:59 +0200

    fix: index apis and applications groups using proper field name

--- gravitee-apim-repository/gravitee-apim-repository-mongodb/src/main/resources/scripts/create-index.js
+++ gravitee-apim-repository/gravitee-apim-repository-mongodb/src/main/resources/scripts/create-index.js
@@ -3,13 +3,13 @@ const prefix = "";
 // "apis" collection
 db.getCollection(`${prefix}apis`).dropIndexes();
 db.getCollection(`${prefix}apis`).createIndex({ visibility: 1 }, { name: "v1" });
-db.getCollection(`${prefix}apis`).createIndex({ group: 1 }, { name: "g1" });
+db.getCollection(`${prefix}apis`).createIndex({ groups: 1 }, { name: "g1" });
 db.getCollection(`${prefix}apis`).createIndex({ name: 1 }, { name: "n1" });
 db.getCollection(`${prefix}apis`).reIndex();
 
 // "applications" collection
 db.getCollection(`${prefix}applications`).dropIndexes();
-db.getCollection(`${prefix}applications`).createIndex({ group: 1 }, { name: "g1" });
+db.getCollection(`${prefix}applications`).createIndex({ groups: 1 }, { name: "g1" });
 db.getCollection(`${prefix}applications`).createIndex({ name: 1 }, { name: "n1" });
 db.getCollection(`${prefix}applications`).createIndex({ status: 1 }, { name: "s1" });
 db.getCollection(`${prefix}applications`).reIndex();
