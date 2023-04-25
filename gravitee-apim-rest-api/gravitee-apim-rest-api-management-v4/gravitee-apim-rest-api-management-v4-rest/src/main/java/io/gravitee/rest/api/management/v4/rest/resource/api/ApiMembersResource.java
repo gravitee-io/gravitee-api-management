a0cc760299fb7e022ee75017aafbad91bd88e19b
Lorie Pisicchio <lorie.pisicchio@graviteesource.com>
2023-04-25 16:19:59 +0200

    feat: implement POST and PUT for API members

--- gravitee-apim-rest-api/gravitee-apim-rest-api-management-v4/gravitee-apim-rest-api-management-v4-rest/src/main/java/io/gravitee/rest/api/management/v4/rest/resource/api/ApiMembersResource.java
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-management-v4/gravitee-apim-rest-api-management-v4-rest/src/main/java/io/gravitee/rest/api/management/v4/rest/resource/api/ApiMembersResource.java
@@ -15,10 +15,14 @@
  */
 package io.gravitee.rest.api.management.v4.rest.resource.api;
 
+import static io.gravitee.rest.api.model.permissions.SystemRole.PRIMARY_OWNER;
+
 import io.gravitee.common.http.MediaType;
 import io.gravitee.rest.api.management.v4.rest.mapper.MemberMapper;
+import io.gravitee.rest.api.management.v4.rest.model.CreateApiMembership;
 import io.gravitee.rest.api.management.v4.rest.model.Member;
 import io.gravitee.rest.api.management.v4.rest.model.MembersResponse;
+import io.gravitee.rest.api.management.v4.rest.model.UpdateApiMembership;
 import io.gravitee.rest.api.management.v4.rest.resource.AbstractResource;
 import io.gravitee.rest.api.management.v4.rest.resource.param.PaginationParam;
 import io.gravitee.rest.api.management.v4.rest.security.Permission;
@@ -27,13 +31,17 @@ import io.gravitee.rest.api.model.MembershipMemberType;
 import io.gravitee.rest.api.model.MembershipReferenceType;
 import io.gravitee.rest.api.model.permissions.RolePermission;
 import io.gravitee.rest.api.model.permissions.RolePermissionAction;
+import io.gravitee.rest.api.model.permissions.RoleScope;
 import io.gravitee.rest.api.service.common.GraviteeContext;
+import io.gravitee.rest.api.service.exceptions.InvalidDataException;
+import io.gravitee.rest.api.service.exceptions.SinglePrimaryOwnerException;
 import java.util.Comparator;
 import java.util.stream.Collectors;
 import javax.validation.Valid;
 import javax.ws.rs.*;
 import javax.ws.rs.container.ResourceContext;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
 
 @Path("/environments/{envId}/apis/{apiId}/members")
 public class ApiMembersResource extends AbstractResource {
@@ -59,5 +67,48 @@ public class ApiMembersResource extends AbstractResource {
             .data(computePaginationData(members, paginationParam))
             .pagination(computePaginationInfo(members.size(), 1, paginationParam))
             .links(computePaginationLinks(members.size(), paginationParam));
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Permissions({ @Permission(value = RolePermission.API_MEMBER, acls = RolePermissionAction.CREATE) })
+    public Response createApiMembership(CreateApiMembership apiMembership) {
+        checkRoleIsNotPrimaryOwner(apiMembership.getRoleName());
+
+        if (apiMembership.getUserId() == null && apiMembership.getExternalReference() == null) {
+            throw new InvalidDataException("Request must specify either userId or externalReference");
+        }
+
+        var createdMember = membershipService.createNewMembershipForApi(
+            GraviteeContext.getExecutionContext(),
+            apiId,
+            apiMembership.getUserId(),
+            apiMembership.getExternalReference(),
+            apiMembership.getRoleName()
+        );
+        return Response.status(Response.Status.CREATED).entity(MemberMapper.INSTANCE.convert(createdMember)).build();
+    }
+
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Permissions({ @Permission(value = RolePermission.API_MEMBER, acls = RolePermissionAction.UPDATE) })
+    public Response updateApiMembership(UpdateApiMembership apiMembership) {
+        checkRoleIsNotPrimaryOwner(apiMembership.getRoleName());
+
+        var updatedMembership = membershipService.updateMembershipForApi(
+            GraviteeContext.getExecutionContext(),
+            apiId,
+            apiMembership.getMemberId(),
+            apiMembership.getRoleName()
+        );
+        return Response.ok().entity(MemberMapper.INSTANCE.convert(updatedMembership)).build();
+    }
+
+    private void checkRoleIsNotPrimaryOwner(String roleId) {
+        if (PRIMARY_OWNER.name().equals(roleId)) {
+            throw new SinglePrimaryOwnerException(RoleScope.API);
+        }
     }
 }
---
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/test/java/io/gravitee/rest/api/service/impl/MembershipService_CreateNewMembershipForApiTest.java
@@ -0,0 +1,224 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.rest.api.service.impl;
+
+import static io.gravitee.rest.api.model.permissions.RolePermissionAction.CREATE;
+import static io.gravitee.rest.api.model.permissions.RolePermissionAction.DELETE;
+import static io.gravitee.rest.api.model.permissions.RolePermissionAction.READ;
+import static io.gravitee.rest.api.model.permissions.RolePermissionAction.UPDATE;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.when;
+
+import io.gravitee.repository.management.api.MembershipRepository;
+import io.gravitee.repository.management.model.Membership;
+import io.gravitee.rest.api.model.MemberEntity;
+import io.gravitee.rest.api.model.RoleEntity;
+import io.gravitee.rest.api.model.UserEntity;
+import io.gravitee.rest.api.model.permissions.OrganizationPermission;
+import io.gravitee.rest.api.model.permissions.Permission;
+import io.gravitee.rest.api.model.permissions.RoleScope;
+import io.gravitee.rest.api.model.providers.User;
+import io.gravitee.rest.api.model.v4.api.ApiEntity;
+import io.gravitee.rest.api.service.AuditService;
+import io.gravitee.rest.api.service.IdentityService;
+import io.gravitee.rest.api.service.MembershipService;
+import io.gravitee.rest.api.service.RoleService;
+import io.gravitee.rest.api.service.UserService;
+import io.gravitee.rest.api.service.common.ExecutionContext;
+import io.gravitee.rest.api.service.common.GraviteeContext;
+import io.gravitee.rest.api.service.exceptions.MembershipAlreadyExistsException;
+import io.gravitee.rest.api.service.v4.ApiSearchService;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class MembershipService_CreateNewMembershipForApiTest {
+
+    private static final String API_ID = "api-id-1";
+
+    @InjectMocks
+    private MembershipService membershipService = new MembershipServiceImpl();
+
+    @Mock
+    private MembershipRepository membershipRepository;
+
+    @Mock
+    private ApiSearchService apiSearchService;
+
+    @Mock
+    private UserService userService;
+
+    @Mock
+    private AuditService auditService;
+
+    @Mock
+    private RoleService roleService;
+
+    @Mock
+    private IdentityService identityService;
+
+    @Before
+    public void setUp() throws Exception {
+        reset(membershipRepository, apiSearchService, userService, auditService, roleService, identityService);
+        mockRole();
+        mockApi();
+    }
+
+    @Test
+    public void should_create_membership_for_existing_user_without_existing_membership() throws Exception {
+        String existingUserId = "existing-user-id";
+        mockExistingUser(existingUserId);
+
+        Membership newMembership = new Membership();
+        newMembership.setReferenceType(io.gravitee.repository.management.model.MembershipReferenceType.API);
+        newMembership.setRoleId("API_OWNER");
+        newMembership.setReferenceId(API_ID);
+        newMembership.setMemberId(existingUserId);
+        newMembership.setMemberType(io.gravitee.repository.management.model.MembershipMemberType.USER);
+        when(
+            membershipRepository.findByMemberIdAndMemberTypeAndReferenceTypeAndReferenceId(
+                existingUserId,
+                io.gravitee.repository.management.model.MembershipMemberType.USER,
+                io.gravitee.repository.management.model.MembershipReferenceType.API,
+                API_ID
+            )
+        )
+            .thenReturn(Collections.emptySet(), Set.of(newMembership));
+
+        MemberEntity createdMember = membershipService.createNewMembershipForApi(
+            GraviteeContext.getExecutionContext(),
+            API_ID,
+            existingUserId,
+            null,
+            "OWNER"
+        );
+
+        assertNotNull(createdMember);
+    }
+
+    @Test(expected = MembershipAlreadyExistsException.class)
+    public void should_not_create_membership_for_existing_user_with_existing_membership() throws Exception {
+        String existingUserId = "existing-user-id";
+        mockExistingUser(existingUserId);
+
+        Membership existingMembership = new Membership();
+        existingMembership.setReferenceType(io.gravitee.repository.management.model.MembershipReferenceType.API);
+        existingMembership.setRoleId("API_OWNER");
+        existingMembership.setReferenceId(API_ID);
+        existingMembership.setMemberId(existingUserId);
+        existingMembership.setMemberType(io.gravitee.repository.management.model.MembershipMemberType.USER);
+        when(
+            membershipRepository.findByMemberIdAndMemberTypeAndReferenceTypeAndReferenceId(
+                existingUserId,
+                io.gravitee.repository.management.model.MembershipMemberType.USER,
+                io.gravitee.repository.management.model.MembershipReferenceType.API,
+                API_ID
+            )
+        )
+            .thenReturn(Set.of(existingMembership));
+
+        membershipService.createNewMembershipForApi(GraviteeContext.getExecutionContext(), API_ID, existingUserId, null, "OWNER");
+    }
+
+    @Test
+    public void should_create_membership_for_external_reference() throws Exception {
+        String externalReference = "external-reference";
+        mockExternalUser(externalReference);
+
+        Membership newMembership = new Membership();
+        newMembership.setReferenceType(io.gravitee.repository.management.model.MembershipReferenceType.API);
+        newMembership.setRoleId("API_OWNER");
+        newMembership.setReferenceId(API_ID);
+        newMembership.setMemberId("external-user-id");
+        newMembership.setMemberType(io.gravitee.repository.management.model.MembershipMemberType.USER);
+        when(
+            membershipRepository.findByMemberIdAndMemberTypeAndReferenceTypeAndReferenceId(
+                "external-user-id",
+                io.gravitee.repository.management.model.MembershipMemberType.USER,
+                io.gravitee.repository.management.model.MembershipReferenceType.API,
+                API_ID
+            )
+        )
+            .thenReturn(Set.of(newMembership));
+
+        MemberEntity createdMember = membershipService.createNewMembershipForApi(
+            GraviteeContext.getExecutionContext(),
+            API_ID,
+            null,
+            externalReference,
+            "OWNER"
+        );
+
+        assertNotNull(createdMember);
+    }
+
+    private void mockExistingUser(String existingUserId) {
+        UserEntity userEntity = new UserEntity();
+        userEntity.setId(existingUserId);
+        when(userService.findById(GraviteeContext.getExecutionContext(), existingUserId)).thenReturn(userEntity);
+    }
+
+    private void mockRole() {
+        Map<String, char[]> perms = new HashMap<>();
+        for (Permission perm : OrganizationPermission.values()) {
+            perms.put(perm.getName(), new char[] { CREATE.getId(), READ.getId(), UPDATE.getId(), DELETE.getId() });
+        }
+        RoleEntity role = new RoleEntity();
+        role.setScope(RoleScope.API);
+        role.setName("OWNER");
+        role.setId("API_OWNER");
+        role.setPermissions(perms);
+        when(roleService.findByScopeAndName(RoleScope.API, "OWNER", GraviteeContext.getCurrentOrganization()))
+            .thenReturn(Optional.of(role));
+        when(roleService.findById("API_OWNER")).thenReturn(role);
+    }
+
+    private void mockApi() {
+        ApiEntity apiEntity = new ApiEntity();
+        apiEntity.setId(API_ID);
+        apiEntity.setGroups(Collections.emptySet());
+        when(apiSearchService.findGenericById(GraviteeContext.getExecutionContext(), API_ID)).thenReturn(apiEntity);
+    }
+
+    private void mockExternalUser(String externalReference) {
+        User externalUser = new User();
+        externalUser.setSource("source");
+        externalUser.setSourceId(externalReference);
+        externalUser.setId("external-user-id");
+        when(identityService.findByReference(externalReference)).thenReturn(Optional.of(externalUser));
+
+        UserEntity userEntity = new UserEntity();
+        userEntity.setId("external-user-id");
+        userEntity.setSource("source");
+        userEntity.setSourceId(externalReference);
+        when(userService.findBySource(any(ExecutionContext.class), anyString(), anyString(), eq(false))).thenReturn(userEntity);
+
+        mockExistingUser("external-user-id");
+    }
+}
