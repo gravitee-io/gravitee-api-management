42b2218f054db7aeb7d27d8e6dc97144e02e62c0
Julien Giovaresco <dev@giovaresco.fr>
2023-04-28 08:47:11 +0200

    fix: wrong failure when execution interrupt while publishing a message
    When a policy interrupts the execution in the onMessageRequest, the
    connector plugins may catch the exception and replace it with another
    one; it was the case of the Kafka plugin, where we catch errors to
    return an unknown error.
    
    To prevent that, I've created another processor chain handling failure.
    This chain is executed before the endpoint and will catch any error to
    return the actual error in the response.
    Errors thrown by the connectors are processed with the original chain
    after the API execution.
    
    https://gravitee.atlassian.net/browse/APIM-1098

---
+++ gravitee-apim-integration-tests/src/test/java/io/gravitee/apim/integration/tests/fake/InterruptMessageRequestPhasePolicy.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.apim.integration.tests.fake;
+
+import io.gravitee.gateway.reactive.api.ExecutionFailure;
+import io.gravitee.gateway.reactive.api.context.MessageExecutionContext;
+import io.gravitee.gateway.reactive.api.policy.Policy;
+import io.reactivex.rxjava3.core.Completable;
+
+/**
+ * Policy that interrupts the publish phase.
+ */
+public class InterruptMessageRequestPhasePolicy implements Policy {
+
+    @Override
+    public String id() {
+        return "interrupt-message-request-phase";
+    }
+
+    @Override
+    public Completable onMessageRequest(MessageExecutionContext ctx) {
+        return ctx
+            .request()
+            .onMessage(message -> ctx.interruptMessageWith(new ExecutionFailure(412).key("FAKE_KEY").message("An error occurred")));
+    }
+}
