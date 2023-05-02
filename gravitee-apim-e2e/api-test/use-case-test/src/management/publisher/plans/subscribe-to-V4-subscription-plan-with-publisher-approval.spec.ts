94051f97e89cea3b6b27fa06c376325f6d021b98
Yann Tavernier <yann.tavernier@graviteesource.com>
2023-05-02 14:12:38 +0200

    test:  skip webhook e2e test

--- gravitee-apim-e2e/api-test/use-case-test/src/management/publisher/plans/subscribe-to-V4-subscription-plan-with-publisher-approval.spec.ts
+++ gravitee-apim-e2e/api-test/use-case-test/src/management/publisher/plans/subscribe-to-V4-subscription-plan-with-publisher-approval.spec.ts
@@ -47,7 +47,8 @@ const apiSubscriptionResource = new APISubscriptionsApi(forManagementAsApiUser()
 const portalApplicationResource = new ApplicationApi(forPortalAsAppUser());
 const portalSubscriptionResource = new SubscriptionApi(forPortalAsAppUser());
 
-describeIfJupiter('V4 subscription plan subscription and approval workflow', () => {
+// TODO: describeIfJupiter.skip('V4 subscription plan subscription and approval workflow', () => {
+describe.skip('V4 subscription plan subscription and approval workflow', () => {
   let api: ApiEntityV4;
   let plan: PlanEntityV4;
   let application: Application;
