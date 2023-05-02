891fb469d244c7aeda4d0c7cfc8df6d905d28ba4
Yann Tavernier <yann.tavernier@graviteesource.com>
2023-05-02 09:53:56 +0200

    refactor: move gateway integration tests to new module with new profile

---
+++ gravitee-apim-integration-tests/src/test/java/io/gravitee/apim/integration/tests/http/HttpRequestTimeoutV3CompatibilityIntegrationTest.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.apim.integration.tests.http;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.gravitee.apim.gateway.tests.sdk.configuration.GatewayConfigurationBuilder;
+import io.gravitee.apim.integration.tests.fake.AddHeaderPolicy;
+import io.gravitee.definition.model.Api;
+import io.gravitee.definition.model.ExecutionMode;
+import io.vertx.rxjava3.core.http.HttpClientResponse;
+
+/**
+ * @author Yann TAVERNIER (yann.tavernier at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+public class HttpRequestTimeoutV3CompatibilityIntegrationTest extends HttpRequestTimeoutIntegrationTest {
+
+    @Override
+    protected void configureGateway(GatewayConfigurationBuilder gatewayConfigurationBuilder) {
+        super.configureGateway(gatewayConfigurationBuilder);
+        gatewayConfigurationBuilder.set("api.jupiterMode.enabled", "true");
+    }
+
+    @Override
+    public void configureApi(Api api) {
+        super.configureApi(api);
+        api.setExecutionMode(ExecutionMode.V3);
+    }
+
+    // IN V3 mode, if an exception is thrown during api flows, then platform response flow is not executed.
+    @Override
+    protected void assertPlatformHeaders(HttpClientResponse response) {
+        assertThat(response.headers().contains(AddHeaderPolicy.HEADER_NAME)).isFalse();
+    }
+}
