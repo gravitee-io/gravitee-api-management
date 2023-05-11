712a29a684d237aa5dafa735013ca6caa5b3b00f
Yann Tavernier <yann.tavernier@graviteesource.com>
2023-05-11 06:56:05 +0200

    ci: add a one-step release workflow
    - remove release scripts to merge them in one and only
    - We lose the list of PRs to merge for documentation, because we display log before the end of workflow. Since it sends a notification in slack, it's not a real problem.
    - Add a question at the beginning of the process to ensure everything is done on jira side.
    - Don't lint and test ui during release
    - jobs are now prefixed by "job-" and name contains only hyphens
    - commands are now prefixed by "cmd-" and name contains only hyphens
    - workflows continue to have underscore to differentiate them
    - run zx in quiet mode

--- release/steps/full_release.mjs
+++ release/steps/full_release.mjs
@@ -9,13 +9,32 @@ await checkToken();
 const releasingVersion = await extractVersion();
 const versions = computeVersion(releasingVersion);
 
-console.log(chalk.blue(`Triggering Package Zip Pipeline`));
+console.log(chalk.green(`💪 Triggering Release Pipeline!\n`));
 
-// Use the preconfigured payload from config folder with the good parameters
+const isReadyToRelease = await question(chalk.blue(`📝 Ensure Release list is good for ${releasingVersion} in JIRA. Should we continue? (y/n)\n`));
+if (isReadyToRelease === 'n') {
+  try {
+    await $`exit 1`;
+  } catch (p) {
+    console.log(chalk.yellow(`🚦 Release process interrupted. Verify JIRA release for ${releasingVersion} and try again!`))
+  }
+}
+
+let isLatest = false;
+if (argv.latest) {
+  isLatest = true;
+} else {
+  const shouldBeLatest = await question(chalk.blue(`⚠️ No '--latest' parameter. Should this version be flagged as 'latest'? (y/n)\n`));
+  if (shouldBeLatest === 'y') {
+    isLatest = true;
+  }
+}
+
 const body = {
   branch: versions.branch,
   parameters: {
-    gio_action: 'package_bundle',
+    gio_action: 'release',
+    docker_tag_as_latest: isLatest,
     dry_run: isDryRun(),
     graviteeio_version: releasingVersion,
   },
@@ -35,7 +54,14 @@ const data = await response.json();
 if (response.status === 201) {
   console.log(chalk.green(`Pipeline created with number: ${data.number}`));
   echo`Follow its progress on: https://app.circleci.com/pipelines/github/gravitee-io/gravitee-api-management/${data.number}`;
-  console.log(chalk.greenBright(`When it's done, run 'npm run docker_rpms -- --version=${releasingVersion}'`));
+
+  console.log(
+      chalk.greenBright(
+          'Just wait for the end of the workflow and release is done! Congrats 🏆\n' +
+          'When it\'s done, do not forget to:\n' +
+          `- merge gravitee-docs PR if it exists for ${releasingVersion}`
+      ),
+  );
 } else {
   console.log(chalk.yellow('Something went wrong'));
 }
