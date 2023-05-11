14e18ccd01f7ec89e316d36ad8f61fec42482ad0
Julien Giovaresco <dev@giovaresco.fr>
2023-05-11 10:58:06 +0200

    feat: improve consistency dealing with invalid cors preflight requests
    BREAKING CHANGE: policies are no longer executed on preflight requests
    when the checks are not fulfilled in Jupiter and V4 engine.
    
    In V3 engine, when the preflight request is invalid:
    - the status is 200 when the APIs were configured to run policies
    - the status is 400 otherwise
    - the policies are executed if configured
    
    This behavior is fixed in Jupiter and V4 engine. Now, when the preflight
    request is invalid:
    - the status is 400
    - the policies are not executed
    
    A new failure key `CORS_PREFLIGHT_FAILED` was introduced to override the
    default behavior by changing the status code or defining a response body
    using response templates.

---
+++ gravitee-apim-integration-tests/src/test/java/io/gravitee/apim/integration/tests/http/cors/AbstractCorsIntegrationTest.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.apim.integration.tests.http.cors;
+
+import io.gravitee.apim.gateway.tests.sdk.AbstractGatewayTest;
+import io.gravitee.apim.gateway.tests.sdk.connector.EndpointBuilder;
+import io.gravitee.apim.gateway.tests.sdk.connector.EntrypointBuilder;
+import io.gravitee.apim.gateway.tests.sdk.policy.PolicyBuilder;
+import io.gravitee.apim.integration.tests.fake.AddHeaderPolicy;
+import io.gravitee.plugin.endpoint.EndpointConnectorPlugin;
+import io.gravitee.plugin.endpoint.http.proxy.HttpProxyEndpointConnectorFactory;
+import io.gravitee.plugin.entrypoint.EntrypointConnectorPlugin;
+import io.gravitee.plugin.entrypoint.http.proxy.HttpProxyEntrypointConnectorFactory;
+import io.gravitee.plugin.policy.PolicyPlugin;
+import io.vertx.rxjava3.core.http.HttpClientResponse;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class AbstractCorsIntegrationTest extends AbstractGatewayTest {
+
+    @Override
+    public void configureEntrypoints(Map<String, EntrypointConnectorPlugin<?, ?>> entrypoints) {
+        entrypoints.putIfAbsent("http-proxy", EntrypointBuilder.build("http-proxy", HttpProxyEntrypointConnectorFactory.class));
+    }
+
+    @Override
+    public void configureEndpoints(Map<String, EndpointConnectorPlugin<?, ?>> endpoints) {
+        endpoints.putIfAbsent("http-proxy", EndpointBuilder.build("http-proxy", HttpProxyEndpointConnectorFactory.class));
+    }
+
+    @Override
+    public void configurePolicies(Map<String, PolicyPlugin> policies) {
+        policies.put("add-header", PolicyBuilder.build("add-header", AddHeaderPolicy.class));
+    }
+
+    protected static Map<String, String> extractHeaders(HttpClientResponse response) {
+        return response.headers().entries().stream().collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
+}
---
+++ gravitee-apim-integration-tests/src/test/java/io/gravitee/apim/integration/tests/http/cors/CorsIntegrationTest.java
@@ -0,0 +1,224 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.apim.integration.tests.http.cors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.gravitee.apim.gateway.tests.sdk.annotations.DeployApi;
+import io.gravitee.apim.gateway.tests.sdk.annotations.GatewayTest;
+import io.gravitee.apim.gateway.tests.sdk.configuration.GatewayMode;
+import io.gravitee.apim.integration.tests.fake.AddHeaderPolicy;
+import io.vertx.core.http.HttpMethod;
+import io.vertx.rxjava3.core.http.HttpClient;
+import java.util.Map;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+public class CorsIntegrationTest {
+
+    @Nested
+    @GatewayTest(mode = GatewayMode.JUPITER)
+    @DeployApi(
+        {
+            "/apis/http/cors-running-policies.json",
+            "/apis/http/cors-not-running-policies.json",
+            "/apis/http/cors-with-response-template.json",
+        }
+    )
+    class CheckingResponseStatus extends AbstractCorsIntegrationTest {
+
+        @ParameterizedTest
+        @ValueSource(strings = { "/api-cors-running-policies", "/api-cors-not-running-policies" })
+        void should_return_200_status_when_preflight_request_is_valid(String path, HttpClient httpClient) throws InterruptedException {
+            httpClient
+                .rxRequest(HttpMethod.OPTIONS, path)
+                .flatMap(httpClientRequest ->
+                    httpClientRequest.putHeader("Origin", "https://mydomain.com").putHeader("Access-Control-Request-Method", "GET").rxSend()
+                )
+                .flatMapPublisher(response -> {
+                    assertThat(response.statusCode()).isEqualTo(200);
+                    assertThat(extractHeaders(response))
+                        .contains(
+                            Map.entry("Access-Control-Allow-Origin", "https://mydomain.com"),
+                            Map.entry("Access-Control-Allow-Methods", "POST, GET"),
+                            Map.entry("Access-Control-Allow-Headers", "x-gravitee-test")
+                        );
+                    return response.toFlowable();
+                })
+                .test()
+                .await()
+                .assertComplete()
+                .assertNoErrors();
+        }
+
+        @ParameterizedTest
+        @ValueSource(strings = { "/api-cors-running-policies", "/api-cors-not-running-policies" })
+        void should_return_400_status_when_preflight_request_is_invalid_and_policies_configured_to_run(String path, HttpClient httpClient)
+            throws InterruptedException {
+            httpClient
+                .rxRequest(HttpMethod.OPTIONS, path)
+                .flatMap(httpClientRequest ->
+                    httpClientRequest.putHeader("Origin", "https://unknown.com").putHeader("Access-Control-Request-Method", "GET").rxSend()
+                )
+                .flatMapPublisher(response -> {
+                    assertThat(response.statusCode()).isEqualTo(400);
+                    return response.toFlowable();
+                })
+                .test()
+                .await()
+                .assertComplete()
+                .assertNoErrors();
+        }
+
+        @Test
+        void should_return_apply_response_template_when_preflight_request_is_invalid(HttpClient httpClient) throws InterruptedException {
+            httpClient
+                .rxRequest(HttpMethod.OPTIONS, "/api-cors-with-response-template")
+                .flatMap(httpClientRequest ->
+                    httpClientRequest.putHeader("Origin", "https://unknown.com").putHeader("Access-Control-Request-Method", "GET").rxSend()
+                )
+                .flatMapPublisher(response -> {
+                    assertThat(response.statusCode()).isEqualTo(412);
+                    return response.toFlowable();
+                })
+                .test()
+                .await()
+                .assertComplete()
+                .assertNoErrors()
+                .assertValue(body -> {
+                    assertThat(body.toString()).contains("Custom CORS error message");
+                    return true;
+                });
+        }
+    }
+
+    @Nested
+    @GatewayTest(mode = GatewayMode.JUPITER)
+    @DeployApi({ "/apis/http/cors-running-policies.json", "/apis/http/cors-not-running-policies.json" })
+    class CheckingPoliciesExecution extends AbstractCorsIntegrationTest {
+
+        @Test
+        void should_apply_policy_on_preflight_request_when_valid(HttpClient httpClient) throws InterruptedException {
+            httpClient
+                .rxRequest(HttpMethod.OPTIONS, "/api-cors-running-policies")
+                .flatMap(httpClientRequest ->
+                    httpClientRequest.putHeader("Origin", "https://mydomain.com").putHeader("Access-Control-Request-Method", "GET").rxSend()
+                )
+                .flatMapPublisher(response -> {
+                    assertThat(response.statusCode()).isEqualTo(200);
+                    assertThat(extractHeaders(response)).contains(Map.entry(AddHeaderPolicy.HEADER_NAME, AddHeaderPolicy.RESPONSE_HEADER));
+                    return response.toFlowable();
+                })
+                .test()
+                .await()
+                .assertComplete()
+                .assertNoErrors();
+        }
+
+        @ParameterizedTest
+        @ValueSource(strings = { "/api-cors-running-policies", "/api-cors-not-running-policies" })
+        void should_not_apply_policy_on_preflight_request_when_invalid(String path, HttpClient httpClient) throws InterruptedException {
+            httpClient
+                .rxRequest(HttpMethod.OPTIONS, path)
+                .flatMap(httpClientRequest ->
+                    httpClientRequest.putHeader("Origin", "https://unknown.com").putHeader("Access-Control-Request-Method", "GET").rxSend()
+                )
+                .flatMapPublisher(response -> {
+                    assertThat(response.statusCode()).isEqualTo(400);
+                    assertThat(extractHeaders(response)).doesNotContainKeys(AddHeaderPolicy.HEADER_NAME);
+                    return response.toFlowable();
+                })
+                .test()
+                .await()
+                .assertComplete()
+                .assertNoErrors();
+        }
+    }
+
+    @Nested
+    @GatewayTest(mode = GatewayMode.JUPITER)
+    @DeployApi({ "/apis/http/cors-running-policies.json", "/apis/http/cors-not-running-policies.json" })
+    class CheckingRejection extends AbstractCorsIntegrationTest {
+
+        @Test
+        void should_reject_preflight_request_when_forbidden_origin(HttpClient httpClient) throws InterruptedException {
+            httpClient
+                .rxRequest(HttpMethod.OPTIONS, "/api-cors-running-policies")
+                .flatMap(httpClientRequest ->
+                    httpClientRequest.putHeader("Origin", "https://unknown.com").putHeader("Access-Control-Request-Method", "GET").rxSend()
+                )
+                .flatMapPublisher(response -> {
+                    assertThat(extractHeaders(response))
+                        .doesNotContainKeys("Access-Control-Allow-Origin", "Access-Control-Allow-Methods", "Access-Control-Allow-Headers");
+
+                    return response.toFlowable();
+                })
+                .test()
+                .await()
+                .assertComplete()
+                .assertNoErrors();
+        }
+
+        @Test
+        void should_reject_preflight_request_when_forbidden_header(HttpClient httpClient) throws InterruptedException {
+            httpClient
+                .rxRequest(HttpMethod.OPTIONS, "/api-cors-running-policies")
+                .flatMap(httpClientRequest ->
+                    httpClientRequest
+                        .putHeader("Origin", "https://mydomain.com")
+                        .putHeader("Access-Control-Request-Method", "GET")
+                        .putHeader("Access-Control-Request-Headers", "x-gravitee-test, x-gravitee-dev")
+                        .rxSend()
+                )
+                .flatMapPublisher(response -> {
+                    assertThat(extractHeaders(response)).doesNotContainKeys("Access-Control-Allow-Methods", "Access-Control-Allow-Headers");
+
+                    return response.toFlowable();
+                })
+                .test()
+                .await()
+                .assertComplete()
+                .assertNoErrors();
+        }
+
+        @Test
+        void should_reject_preflight_request_when_forbidden_method(HttpClient httpClient) throws InterruptedException {
+            httpClient
+                .rxRequest(HttpMethod.OPTIONS, "/api-cors-running-policies")
+                .flatMap(httpClientRequest ->
+                    httpClientRequest
+                        .putHeader("Origin", "https://mydomain.com")
+                        .putHeader("Access-Control-Request-Method", "PUT")
+                        .putHeader("Access-Control-Request-Headers", "x-gravitee-test")
+                        .rxSend()
+                )
+                .flatMapPublisher(response -> {
+                    assertThat(extractHeaders(response)).doesNotContainKeys("Access-Control-Allow-Methods", "Access-Control-Allow-Headers");
+
+                    return response.toFlowable();
+                })
+                .test()
+                .await()
+                .assertComplete()
+                .assertNoErrors();
+        }
+    }
+}
