a948ddd1e239192ecd9c1b8cfb0404c5ccd8a735
Azize Elamrani <azizeelamrani@macbook-pro.home>
2023-05-23 10:43:19 +0200

    fix: apply the status filter when getting the list of applications
    (cherry picked from commit 871e4dea01474992d211b0c940ac21a1cb4d646b)
    
    # Conflicts:
    #	gravitee-apim-e2e/api-test/src/applications/application-searching-paged.spec.ts

--- gravitee-apim-e2e/api-test/src/applications/application-searching-paged.spec.ts
+++ gravitee-apim-e2e/api-test/src/applications/application-searching-paged.spec.ts
@@ -15,20 +15,32 @@
  */
 import { afterAll, beforeAll, expect } from '@jest/globals';
 import faker from '@faker-js/faker';
-import { forbidden, succeed } from '@lib/jest-utils';
-import { ApplicationsApi } from '@gravitee/management-webclient-sdk/src/lib/apis/ApplicationsApi';
-import { ApplicationEntity } from '@gravitee/management-webclient-sdk/src/lib/models/ApplicationEntity';
+import { find } from 'lodash';
 import {
-  APP_USER,
   ADMIN_USER,
+  API_USER,
+  APP_USER,
   forManagementAsAdminUser,
   forManagementAsApiUser,
   forManagementAsAppUser,
 } from '@gravitee/utils/configuration';
 
+import { forbidden, succeed } from '@lib/jest-utils';
+import { ApplicationsApi } from '@gravitee/management-webclient-sdk/src/lib/apis/ApplicationsApi';
+import { ApplicationEntity } from '@gravitee/management-webclient-sdk/src/lib/models/ApplicationEntity';
+import { GroupsApi } from '@gravitee/management-webclient-sdk/src/lib/apis/GroupsApi';
+import { GroupEntity } from '@gravitee/management-webclient-sdk/src/lib/models/GroupEntity';
+import { GroupMembershipsApi } from '@gravitee/management-webclient-sdk/src/lib/apis/GroupMembershipsApi';
+import { MemberRoleEntityFromJSON } from '@gravitee/management-webclient-sdk/src/lib/models/MemberRoleEntity';
+import { SearchableUser } from '@gravitee/management-webclient-sdk/src/lib/models/SearchableUser';
+import { UsersApi } from '@gravitee/management-webclient-sdk/src/lib/apis/UsersApi';
+
 const applicationManagementApiAsAdminUser = new ApplicationsApi(forManagementAsAdminUser());
 const applicationManagementApiAsAppUser = new ApplicationsApi(forManagementAsAppUser());
 const applicationManagementApiAsApiUser = new ApplicationsApi(forManagementAsApiUser());
+const groupsApi = new GroupsApi(forManagementAsAdminUser());
+const usersResourceAsApiUser = new UsersApi(forManagementAsApiUser());
+const groupMembershipsApi = new GroupMembershipsApi(forManagementAsAdminUser());
 
 const orgId = 'DEFAULT';
 const envId = 'DEFAULT';
@@ -37,6 +49,8 @@ let createdApplication1: ApplicationEntity;
 let createdApplication2: ApplicationEntity;
 let createdApplication3: ApplicationEntity;
 let createdApplication4: ApplicationEntity;
+let createdApplication5: ApplicationEntity;
+let createdGroup: GroupEntity;
 
 describe('Applications - Searching - Paged', () => {
   beforeAll(async () => {
@@ -56,10 +70,26 @@ describe('Applications - Searching - Paged', () => {
 
     [createdApplication1, createdApplication2, createdApplication3] = await Promise.all(createdAppsAsAppUser);
 
-    const createdAppsAsApiUser = [4]
+    [createdGroup] = await Promise.all(
+      [
+        {
+          name: `${faker.random.word()}-${faker.datatype.uuid()}`,
+        },
+      ].map(
+        async (newGroupEntity) =>
+          await groupsApi.createGroup({
+            orgId,
+            envId,
+            newGroupEntity,
+          }),
+      ),
+    );
+
+    const createdAppsAsApiUser = [4, 5]
       .map((num) => ({
         name: `${faker.random.word()}-${faker.datatype.uuid()}`,
         description: `Application ${num} for "Applications searching scenario"`,
+        groups: [createdGroup.id],
       }))
       .map(
         async (newApplicationEntity) =>
@@ -70,7 +100,43 @@ describe('Applications - Searching - Paged', () => {
           }),
       );
 
-    [createdApplication4] = await Promise.all(createdAppsAsApiUser);
+    [createdApplication4, createdApplication5] = await Promise.all(createdAppsAsApiUser);
+
+    // get user member
+    const users: SearchableUser[] = await usersResourceAsApiUser.searchUsers({
+      envId,
+      orgId,
+      q: process.env.API_USERNAME,
+    });
+    const userMember = find(users, (user) => user.displayName === process.env.API_USERNAME);
+
+    // add member user to the group
+    await groupMembershipsApi.addOrUpdateGroupMember({
+      envId,
+      orgId,
+      group: createdGroup.id,
+      groupMembership: [
+        {
+          id: userMember.id,
+          reference: userMember.reference,
+          roles: [
+            { scope: 'API', name: 'USER' },
+            { scope: 'APPLICATION', name: 'USER' },
+          ].map(MemberRoleEntityFromJSON),
+        },
+      ],
+    });
+
+    // archive the application5
+    const deletedAppsAsApiUser = [createdApplication5].map(
+      async (application) =>
+        await applicationManagementApiAsApiUser.deleteApplication({
+          orgId,
+          envId,
+          application: application.id,
+        }),
+    );
+    await Promise.all(deletedAppsAsApiUser);
   });
 
   test(`Search all for ${APP_USER.username}`, async () => {
@@ -120,6 +186,12 @@ describe('Applications - Searching - Paged', () => {
     expect(applications.page.total_pages).toBeGreaterThanOrEqual(2);
   });
 
+  test(`Search all ACTIVE for ${API_USER.username}`, async () => {
+    const applications = await succeed(applicationManagementApiAsApiUser.getApplicationsPagedRaw({ orgId, envId, status: 'ACTIVE' }));
+    const archivedApplications = applications.data.filter((a) => a.status === 'ARCHIVED');
+    expect(archivedApplications.length).toEqual(0);
+  });
+
   afterAll(async () => {
     const deletedAppsAsAppUser = [createdApplication1, createdApplication2, createdApplication3].map(
       async (application) =>
@@ -139,5 +211,14 @@ describe('Applications - Searching - Paged', () => {
         }),
     );
     await Promise.all(deletedAppsAsApiUser);
+    const deletedGroup = [createdGroup].map(
+      async (group) =>
+        await groupsApi.deleteGroup({
+          orgId,
+          envId,
+          group: group.id,
+        }),
+    );
+    await Promise.all(deletedGroup);
   });
 });
