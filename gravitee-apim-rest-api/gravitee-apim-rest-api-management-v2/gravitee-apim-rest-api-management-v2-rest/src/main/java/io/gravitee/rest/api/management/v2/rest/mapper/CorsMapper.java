e138d0a2db9c62ef7e7434a4d1214a0aff7ffd16
Florent CHAMFROY <florent.chamfroy@graviteesource.com>
2023-05-30 09:00:49 +0200

    feat(rest-api-v2): add new mappers - Cors

---
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-management-v2/gravitee-apim-rest-api-management-v2-rest/src/main/java/io/gravitee/rest/api/management/v2/rest/mapper/CorsMapper.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.rest.api.management.v2.rest.mapper;
+
+import io.gravitee.definition.model.v4.listener.Listener;
+import io.gravitee.definition.model.v4.listener.http.HttpListener;
+import io.gravitee.definition.model.v4.listener.subscription.SubscriptionListener;
+import io.gravitee.definition.model.v4.listener.tcp.TcpListener;
+import io.gravitee.rest.api.management.v2.rest.model.Cors;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface CorsMapper {
+    CorsMapper INSTANCE = Mappers.getMapper(CorsMapper.class);
+
+    @Mapping(target = "accessControlAllowCredentials", source = "allowCredentials")
+    @Mapping(target = "accessControlAllowHeaders", source = "allowHeaders")
+    @Mapping(target = "accessControlAllowMethods", source = "allowMethods")
+    @Mapping(target = "accessControlAllowOrigin", source = "allowOrigin")
+    @Mapping(target = "accessControlExposeHeaders", source = "exposeHeaders")
+    @Mapping(target = "accessControlMaxAge", source = "maxAge")
+    io.gravitee.definition.model.Cors mapToCorsEntity(Cors cors);
+
+    @Mapping(target = "allowCredentials", source = "accessControlAllowCredentials")
+    @Mapping(target = "allowHeaders", source = "accessControlAllowHeaders")
+    @Mapping(target = "allowMethods", source = "accessControlAllowMethods")
+    @Mapping(target = "allowOrigin", source = "accessControlAllowOrigin")
+    @Mapping(target = "exposeHeaders", source = "accessControlExposeHeaders")
+    @Mapping(target = "maxAge", source = "accessControlMaxAge")
+    Cors mapFromCorsEntity(io.gravitee.definition.model.Cors cors);
+}
---
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-management-v2/gravitee-apim-rest-api-management-v2-rest/src/test/java/fixtures/CorsFixtures.java
@@ -0,0 +1,57 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package fixtures;
+
+import io.gravitee.rest.api.management.v2.rest.model.Cors;
+import java.util.Set;
+
+/**
+ * @author Jeoffrey HAEYAERT (jeoffrey.haeyaert at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+@SuppressWarnings("ALL")
+public class CorsFixtures {
+
+    private static final io.gravitee.definition.model.Cors.CorsBuilder BASE_MODEL_CORS = io.gravitee.definition.model.Cors
+        .builder()
+        .accessControlAllowCredentials(true)
+        .accessControlAllowHeaders(Set.of("header1", "header2"))
+        .accessControlAllowMethods(Set.of("method1", "method2"))
+        .accessControlAllowOrigin(Set.of("origin1", "origin2"))
+        .enabled(true)
+        .accessControlExposeHeaders(Set.of("exposeHeader1", "exposeHeader2"))
+        .accessControlMaxAge(10)
+        .runPolicies(true);
+
+    private static final Cors.CorsBuilder BASE_CORS = Cors
+        .builder()
+        .allowCredentials(true)
+        .allowHeaders(Set.of("header1", "header2"))
+        .allowMethods(Set.of("method1", "method2"))
+        .allowOrigin(Set.of("origin1", "origin2"))
+        .enabled(true)
+        .exposeHeaders(Set.of("exposeHeader1", "exposeHeader2"))
+        .maxAge(10)
+        .runPolicies(true);
+
+    public static io.gravitee.definition.model.Cors aModelCors() {
+        return BASE_MODEL_CORS.build();
+    }
+
+    public static Cors aCors() {
+        return BASE_CORS.build();
+    }
+}
