e8a62a21d15f5538aaf117c37615868e527fcf25
Julien Giovaresco <dev@giovaresco.fr>
2023-05-30 13:18:47 +0200

    feat: extract http-get entrypoint in its own repository
    https://gravitee.atlassian.net/browse/APIM-1357

--- gravitee-apim-integration-tests/src/test/java/io/gravitee/apim/integration/tests/messages/HttpGetEntrypointKafkaEndpointIntegrationTest.java
+++ gravitee-apim-integration-tests/src/test/java/io/gravitee/apim/integration/tests/messages/HttpGetEntrypointKafkaEndpointIntegrationTest.java
@@ -17,6 +17,7 @@ package io.gravitee.apim.integration.tests.messages;/**
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.graviteesource.endpoint.kafka.KafkaEndpointConnectorFactory;
+import com.graviteesource.entrypoint.http.get.HttpGetEntrypointConnectorFactory;
 import com.graviteesource.reactor.message.MessageApiReactorFactory;
 import io.gravitee.apim.gateway.tests.sdk.AbstractGatewayTest;
 import io.gravitee.apim.gateway.tests.sdk.annotations.DeployApi;
@@ -32,7 +33,6 @@ import io.gravitee.gateway.reactive.reactor.v4.reactor.ReactorFactory;
 import io.gravitee.gateway.reactor.ReactableApi;
 import io.gravitee.plugin.endpoint.EndpointConnectorPlugin;
 import io.gravitee.plugin.entrypoint.EntrypointConnectorPlugin;
-import io.gravitee.plugin.entrypoint.http.get.HttpGetEntrypointConnectorFactory;
 import io.vertx.core.http.HttpMethod;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
