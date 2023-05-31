be56f4ef56315046567653c7e1f472b6bd01d5ae
ThibaudAv <thibaud.avenier@gmail.com>
2023-05-31 16:07:46 +0200

    feat(console): impl plans input for gio-policy-studio

--- gravitee-apim-console-webui/src/management/api/policy-studio-v4/design/api-v4-policy-studio-design.component.ts
+++ gravitee-apim-console-webui/src/management/api/policy-studio-v4/design/api-v4-policy-studio-design.component.ts
@@ -14,16 +14,17 @@
  * limitations under the License.
  */
 
-import {Component, Inject, OnDestroy, OnInit} from '@angular/core';
-import {combineLatest, Subject} from 'rxjs';
-import {takeUntil, map} from "rxjs/operators";
-import {ConnectorsInfo, Flow as PSFlow} from "@gravitee/ui-policy-studio-angular";
+import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
+import { combineLatest, Subject } from 'rxjs';
+import { takeUntil, map } from 'rxjs/operators';
+import { ConnectorsInfo, Flow as PSFlow, Plan as PSPlan } from '@gravitee/ui-policy-studio-angular';
 
-import { UIRouterStateParams} from "../../../../ajs-upgraded-providers";
-import {ApiV2Service} from "../../../../services-ngx/api-v2.service";
-import { ApiType, ApiV4} from "../../../../entities/management-api-v2";
-import {IconService} from "../../../../services-ngx/icon.service";
-import {ConnectorPluginsV2Service} from "../../../../services-ngx/connector-plugins-v2.service";
+import { UIRouterStateParams } from '../../../../ajs-upgraded-providers';
+import { ApiV2Service } from '../../../../services-ngx/api-v2.service';
+import { ApiType, ApiV4 } from '../../../../entities/management-api-v2';
+import { IconService } from '../../../../services-ngx/icon.service';
+import { ConnectorPluginsV2Service } from '../../../../services-ngx/connector-plugins-v2.service';
+import { ApiPlanV2Service } from '../../../../services-ngx/api-plan-v2.service';
 
 @Component({
   selector: 'api-v4-policy-studio-design',
@@ -33,52 +34,70 @@ import {ConnectorPluginsV2Service} from "../../../../services-ngx/connector-plug
 export class ApiV4PolicyStudioDesignComponent implements OnInit, OnDestroy {
   private unsubscribe$ = new Subject<boolean>();
 
-
   public apiType: ApiType;
   public entrypointsInfo: ConnectorsInfo[];
-  public endpointsInfo: ConnectorsInfo[]
-  public commonFlows: PSFlow[]
-
+  public endpointsInfo: ConnectorsInfo[];
+  public commonFlows: PSFlow[];
+  public plans: PSPlan[];
 
   constructor(
     @Inject(UIRouterStateParams) private readonly ajsStateParams,
     private readonly connectorPluginsV2Service: ConnectorPluginsV2Service,
     private readonly iconService: IconService,
-    private readonly apiV2Service: ApiV2Service,) {}
+    private readonly apiV2Service: ApiV2Service,
+    private readonly apiPlanV2Service: ApiPlanV2Service,
+  ) {}
 
   ngOnInit(): void {
-
     combineLatest([
       this.apiV2Service.get(this.ajsStateParams.apiId).pipe(map((api) => api as ApiV4)),
       this.connectorPluginsV2Service.listEntrypointPlugins(),
       this.connectorPluginsV2Service.listEndpointPlugins(),
+      this.apiPlanV2Service
+        .list(
+          this.ajsStateParams.apiId,
+          undefined,
+          ['PUBLISHED'],
+          1,
+          // No pagination here. Policy Studio doesn't support it for now.
+          9999,
+        )
+        .pipe(map((apiPlansResponse) => apiPlansResponse.data)),
     ])
       .pipe(takeUntil(this.unsubscribe$))
-      .subscribe(([api,  entrypoints, endpoints]) => {
+      .subscribe(([api, entrypoints, endpoints, plans]) => {
         this.apiType = api.type;
 
-        this.entrypointsInfo = api.listeners.flatMap(listener =>
-          listener.entrypoints.map(entrypoint => {
-            const entrypointPlugin = entrypoints.find(entrypointPlugin => entrypointPlugin.id === entrypoint.type);
+        this.entrypointsInfo = api.listeners.flatMap((listener) =>
+          listener.entrypoints.map((entrypoint) => {
+            const entrypointPlugin = entrypoints.find((entrypointPlugin) => entrypointPlugin.id === entrypoint.type);
             return {
               type: entrypoint.type,
-              icon: this.iconService.registerSvg(entrypoint.type, entrypointPlugin.icon),
-            }
-          }
-        ));
-
-        this.endpointsInfo = api.endpointGroups.flatMap(endpointGroup =>
-          endpointGroup.endpoints.map(endpoint => {
-            const endpointPlugin = endpoints.find(endpointPlugin => endpointPlugin.id === endpoint.type);
-            return {
-              type: endpoint.type,
-              icon: this.iconService.registerSvg(endpoint.type, endpointPlugin.icon),
-            }
-          })
+              icon:
+                entrypointPlugin && entrypointPlugin.icon
+                  ? this.iconService.registerSvg(entrypoint.type, entrypointPlugin.icon)
+                  : 'gio:language',
+            };
+          }),
         );
 
-        this.commonFlows = api.flows
+        this.endpointsInfo = api.endpointGroups.flatMap((endpointGroup) =>
+          endpointGroup.endpoints.map((endpoint) => {
+            const endpointPlugin = endpoints.find((endpointPlugin) => endpointPlugin.id === endpoint.type);
+            return {
+              type: endpoint.type,
+              icon:
+                endpointPlugin && endpointPlugin.icon ? this.iconService.registerSvg(endpoint.type, endpointPlugin.icon) : 'gio:language',
+            };
+          }),
+        );
 
+        this.commonFlows = api.flows;
+
+        this.plans = plans.map((plan) => ({
+          name: plan.name,
+          flows: plan.flows,
+        }));
       });
   }
 
