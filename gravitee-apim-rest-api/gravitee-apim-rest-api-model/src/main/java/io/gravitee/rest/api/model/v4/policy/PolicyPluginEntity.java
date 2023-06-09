8adcfb0b05e57fdbe34d941a025d7dd524f93632
Florent CHAMFROY <florent.chamfroy@graviteesource.com>
2023-06-09 08:13:33 +0200

    feat: create new PolicyPluginService

---
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-model/src/main/java/io/gravitee/rest/api/model/v4/policy/PolicyPluginEntity.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.rest.api.model.v4.policy;
+
+import io.gravitee.definition.model.v4.ApiType;
+import io.gravitee.definition.model.v4.ConnectorFeature;
+import io.gravitee.definition.model.v4.ConnectorMode;
+import io.gravitee.definition.model.v4.listener.ListenerType;
+import io.gravitee.definition.model.v4.listener.entrypoint.Qos;
+import io.gravitee.rest.api.model.platform.plugin.PlatformPluginEntity;
+import io.swagger.v3.oas.annotations.media.Schema;
+import java.util.Set;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+@Schema(name = "PolicyPluginEntityV4")
+public class PolicyPluginEntity extends PlatformPluginEntity {
+
+    private Set<ExecutionPhase> proxy;
+
+    private Set<ExecutionPhase> message;
+}
---
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/rest/api/service/v4/impl/PolicyPluginServiceImpl.java
@@ -0,0 +1,98 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.rest.api.service.v4.impl;
+
+import io.gravitee.plugin.core.api.ConfigurablePluginManager;
+import io.gravitee.plugin.core.api.Plugin;
+import io.gravitee.plugin.policy.PolicyPlugin;
+import io.gravitee.rest.api.model.v4.policy.ExecutionPhase;
+import io.gravitee.rest.api.model.v4.policy.PolicyPluginEntity;
+import io.gravitee.rest.api.service.JsonSchemaService;
+import io.gravitee.rest.api.service.impl.AbstractPluginService;
+import io.gravitee.rest.api.service.v4.PolicyPluginService;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PolicyPluginServiceImpl extends AbstractPluginService<PolicyPlugin<?>, PolicyPluginEntity> implements PolicyPluginService {
+
+    protected PolicyPluginServiceImpl(
+        final JsonSchemaService jsonSchemaService,
+        final ConfigurablePluginManager<PolicyPlugin<?>> policyManager
+    ) {
+        super(jsonSchemaService, policyManager);
+    }
+
+    @Override
+    public Set<PolicyPluginEntity> findAll() {
+        // Need to preserve list order in Set
+        return super.list().stream().map(this::convert).collect(Collectors.toCollection(LinkedHashSet::new));
+    }
+
+    @Override
+    public PolicyPluginEntity findById(String policyPluginId) {
+        PolicyPlugin resourceDefinition = super.get(policyPluginId);
+        return convert(resourceDefinition);
+    }
+
+    @Override
+    protected PolicyPluginEntity convert(Plugin plugin) {
+        PolicyPluginEntity entity = new PolicyPluginEntity();
+
+        entity.setId(plugin.id());
+        entity.setDescription(plugin.manifest().description());
+        entity.setName(plugin.manifest().name());
+        entity.setIcon(getIcon(plugin.id()));
+        entity.setVersion(plugin.manifest().version());
+        entity.setCategory(plugin.manifest().category());
+
+        entity.setProxy(getExecutionPhase(plugin, "proxy"));
+        entity.setMessage(getExecutionPhase(plugin, "message"));
+
+        return entity;
+    }
+
+    private static Set<ExecutionPhase> getExecutionPhase(Plugin plugin, String property) {
+        if (
+            plugin.manifest().properties() != null &&
+            plugin.manifest().properties().get(property) != null &&
+            !plugin.manifest().properties().get(property).isEmpty()
+        ) {
+            return Arrays
+                .stream(plugin.manifest().properties().get(property).split(","))
+                .map(String::trim)
+                .map(ExecutionPhase::valueOf)
+                .collect(Collectors.toSet());
+        } else {
+            return Collections.emptySet();
+        }
+    }
+
+    @Override
+    public String validatePolicyConfiguration(final String policyPluginId, final String configuration) {
+        PolicyPluginEntity policyPluginEntity = this.findById(policyPluginId);
+        return validateConfiguration(policyPluginEntity.getId(), configuration);
+    }
+
+    @Override
+    public String validatePolicyConfiguration(final PolicyPluginEntity policyPluginEntity, final String configuration) {
+        return validateConfiguration(policyPluginEntity.getId(), configuration);
+    }
+}
