e0788fbb47b1797c52e1a524f0f856971a4d2711
Gaëtan Maisse <gaetanmaisse@gmail.com>
2023-06-16 17:41:06 +0200

    fix: sanitize Asciidoc doc page before rendering them

--- gravitee-apim-portal-webui/src/app/components/gv-page-asciidoc/gv-page-asciidoc.component.spec.ts
+++ gravitee-apim-portal-webui/src/app/components/gv-page-asciidoc/gv-page-asciidoc.component.spec.ts
@@ -15,9 +15,11 @@
  */
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { createComponentFactory, Spectator } from '@ngneat/spectator/jest';
+import { createComponentFactory, mockProvider, Spectator } from '@ngneat/spectator/jest';
 
 import { SafePipe } from '../../pipes/safe.pipe';
+import { Page } from '../../../../projects/portal-webclient-sdk/src/lib';
+import { PageService } from '../../services/page.service';
 
 import { GvPageAsciiDocComponent } from './gv-page-asciidoc.component';
 
@@ -27,18 +29,27 @@ describe('GvPageAsciiDocComponent', () => {
     schemas: [CUSTOM_ELEMENTS_SCHEMA],
     declarations: [SafePipe],
     imports: [HttpClientTestingModule],
+    providers: [
+      mockProvider(PageService, {
+        getCurrentPage: () => docPage,
+      }),
+    ],
   });
 
   let spectator: Spectator<GvPageAsciiDocComponent>;
-  let component;
+  let component: GvPageAsciiDocComponent;
+  const docPage: Page = { name: 'A Page', id: '86de4f08-aa02-40f0-aa73-4b3e0e97fef4', content: '', type: 'ASCIIDOC', order: 1 };
 
   beforeEach(() => {
     spectator = createComponent();
     component = spectator.component;
-    component.pageContent = null;
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should sanitize Asciidoc with JavaScript in it', async () => {
+    docPage.content = '```test"><img src=x onerror=alert(1)></img>';
+    await component.ngOnInit();
+    expect(component.pageContent).toEqual(
+      '<div class="listingblock">&#10;<div class="content">&#10;<pre class="highlight"><code class="language-test"><img src="x">&#34; data-lang=&#34;test&#34;&gt;<img src="x">&#34;&gt;</code></pre>&#10;</div>&#10;</div>',
+    );
   });
 });
