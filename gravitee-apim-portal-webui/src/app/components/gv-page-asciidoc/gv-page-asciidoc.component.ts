f017cf8938f0a6378bca8c74ae6385ce025fa604
Gaëtan Maisse <gaetanmaisse@gmail.com>
2023-06-16 17:41:06 +0200

    fix: sanitize Asciidoc doc page before rendering them

--- gravitee-apim-portal-webui/src/app/components/gv-page-asciidoc/gv-page-asciidoc.component.ts
+++ gravitee-apim-portal-webui/src/app/components/gv-page-asciidoc/gv-page-asciidoc.component.ts
@@ -13,8 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, SecurityContext } from '@angular/core';
 import { toDom } from '@gravitee/ui-components/src/lib/text-format';
+import { DomSanitizer } from '@angular/platform-browser';
 
 import { Page } from '../../../../projects/portal-webclient-sdk/src/lib';
 import { PageService } from '../../services/page.service';
@@ -28,20 +29,19 @@ export class GvPageAsciiDocComponent implements OnInit {
   page: Page;
   pageContent: string;
 
-  constructor(private pageService: PageService) {}
+  constructor(private readonly pageService: PageService, private readonly sanitizer: DomSanitizer) {}
 
   ngOnInit() {
     this.page = this.pageService.getCurrentPage();
     const asiidocContainer = document.querySelector('.gv-page-container');
 
     if (this.page && this.page.content && asiidocContainer) {
-      toDom(this.page.content).then(({ element }) => {
+      return toDom(this.page.content).then(({ element }) => {
         element.style.maxWidth = 'inherit';
-        this.pageContent = element.innerHTML;
+        this.pageContent = this.sanitizer.sanitize(SecurityContext.HTML, element.innerHTML);
       });
     }
   }
-
   openMedia(link: string) {
     window.open(link, '_blank');
   }
