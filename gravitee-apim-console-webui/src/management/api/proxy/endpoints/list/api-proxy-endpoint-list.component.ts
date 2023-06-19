3f871cedebbb3611b82922c5a626f0f1990697c3
Ouahid Khelifi <ouahid.khelifi@gmail.com>
2023-06-19 11:35:21 +0200

    refactor: remove endpoint in a group

--- gravitee-apim-console-webui/src/management/api/proxy/endpoints/list/api-proxy-endpoint-list.component.ts
+++ gravitee-apim-console-webui/src/management/api/proxy/endpoints/list/api-proxy-endpoint-list.component.ts
@@ -20,14 +20,14 @@ import { catchError, filter, map, switchMap, takeUntil, tap } from 'rxjs/operato
 import { MatDialog } from '@angular/material/dialog';
 import { GioConfirmDialogComponent, GioConfirmDialogData } from '@gravitee/ui-particles-angular';
 import { UIRouterGlobals } from '@uirouter/core';
+import { remove, find } from 'lodash';
 
 import { UIRouterState, UIRouterStateParams } from '../../../../../ajs-upgraded-providers';
 import { GioPermissionService } from '../../../../../shared/components/gio-permission/gio-permission.service';
 import { EndpointGroup, toEndpoints } from '../endpoint.adapter';
 import { SnackBarService } from '../../../../../services-ngx/snack-bar.service';
-import { ApiV2, EndpointGroupV2, UpdateApi } from '../../../../../entities/management-api-v2';
+import { ApiV2, EndpointGroupV2, EndpointV2, UpdateApi } from '../../../../../entities/management-api-v2';
 import { ApiV2Service } from '../../../../../services-ngx/api-v2.service';
-import { remove } from 'lodash';
 
 @Component({
   selector: 'api-proxy-endpoint-list',
@@ -116,13 +116,13 @@ export class ApiProxyEndpointListComponent implements OnInit {
         takeUntil(this.unsubscribe$),
         filter((confirm) => confirm === true),
         switchMap(() => this.apiService.get(this.api.id)),
-        /*
-        // TODO: make it work with UpdateApi type
-        switchMap((api: Api) => {
-          _.remove(_.find(api.proxy.groups, (g) => g.name === groupName).endpoints, (e) => e.name === endpointName);
-          return this.apiService.update(api.id, api);
+        switchMap((api: ApiV2) => {
+          remove(
+            find(api.proxy.groups, (g: EndpointGroupV2) => g.name === groupName).endpoints,
+            (e: EndpointV2) => e.name === endpointName,
+          );
+          return this.apiService.update(api.id, { ...api } as UpdateApi);
         }),
-        */
         catchError(({ error }) => {
           this.snackBarService.error(error.message);
           return EMPTY;
