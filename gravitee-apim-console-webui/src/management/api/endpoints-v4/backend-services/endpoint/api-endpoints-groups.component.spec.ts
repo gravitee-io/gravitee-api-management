958ecd7f4ea8ef0de5ef44b2055c5cec2df4c3e8
Ouahid Khelifi <ouahid.khelifi@gmail.com>
2023-06-20 18:54:40 +0200

    feat: delete an API V4 endpoint group

--- gravitee-apim-console-webui/src/management/api/endpoints-v4/backend-services/endpoint/api-endpoints-groups.component.spec.ts
+++ gravitee-apim-console-webui/src/management/api/endpoints-v4/backend-services/endpoint/api-endpoints-groups.component.spec.ts
@@ -19,30 +19,106 @@ import { HttpTestingController } from '@angular/common/http/testing';
 import { MatIconTestingModule } from '@angular/material/icon/testing';
 import { HarnessLoader } from '@angular/cdk/testing';
 import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
-import { MatTableHarness } from '@angular/material/table/testing';
+import { Component, ViewChild } from '@angular/core';
+import { InteractivityChecker } from '@angular/cdk/a11y';
 
-import { GioHttpTestingModule } from '../../../../../shared/testing';
+import { CONSTANTS_TESTING, GioHttpTestingModule } from '../../../../../shared/testing';
 import { ApiV4, fakeApiV4 } from '../../../../../entities/management-api-v2';
 import { ApiEndpointsGroupsComponent } from './api-endpoints-groups.component';
-import { ApiProxyEndpointModule } from '../../../proxy/endpoints/api-proxy-endpoints.module';
+import { ApiEndpointsGroupsHarness } from './api-endpoints-groups.harness';
+import { ApiEndpointsGroupsModule } from './api-endpoints-groups.module';
+
+@Component({
+  template: ` <api-endpoints-groups #apiPortalEndpoints [api]="api"></api-endpoints-groups> `,
+})
+class TestComponent {
+  @ViewChild('apiPortalEndpoints') apiPortalEndpoints: ApiEndpointsGroupsComponent;
+  api?: ApiV4;
+}
 
 describe('ApiEndpointsGroupsComponent', () => {
   const API_ID = 'apiId';
-  let fixture: ComponentFixture<ApiEndpointsGroupsComponent>;
+  const apiV4 = fakeApiV4({
+    id: API_ID,
+    endpointGroups: [
+      {
+        name: 'default-group',
+        type: 'kafka',
+        endpoints: [
+          {
+            name: 'a kafka',
+            type: 'kafka',
+            weight: 1,
+            inheritConfiguration: false,
+            configuration: {
+              bootstrapServers: 'localhost:9092',
+            },
+          },
+          {
+            name: 'another kafka',
+            type: 'kafka',
+            weight: 5,
+            inheritConfiguration: false,
+            configuration: {
+              bootstrapServers: 'localhost:9093',
+            },
+          },
+        ],
+      },
+      {
+        name: 'Another Mocked Kafka',
+        type: 'mock',
+        loadBalancer: {
+          type: 'ROUND_ROBIN',
+        },
+        sharedConfiguration: {},
+        endpoints: [
+          {
+            name: 'a mock',
+            type: 'mock',
+            weight: 1,
+            inheritConfiguration: true,
+            configuration: {
+              headers: [],
+              metadata: [],
+              messageCount: 10,
+              messageInterval: 1000,
+              messageContent: 'mock message',
+            },
+            services: {},
+            secondary: false,
+          },
+        ],
+        services: {},
+      },
+    ],
+  });
+
+  let fixture: ComponentFixture<TestComponent>;
   let httpTestingController: HttpTestingController;
   let loader: HarnessLoader;
+  let rootLoader: HarnessLoader;
+  let componentHarness: ApiEndpointsGroupsHarness;
 
-  const initComponent = (api: ApiV4): void => {
+  const initComponent = async (api: ApiV4) => {
     TestBed.configureTestingModule({
-      imports: [NoopAnimationsModule, GioHttpTestingModule, ApiProxyEndpointModule, MatIconTestingModule],
+      declarations: [TestComponent],
+      imports: [NoopAnimationsModule, GioHttpTestingModule, ApiEndpointsGroupsModule, MatIconTestingModule],
+    }).overrideProvider(InteractivityChecker, {
+      useValue: {
+        isFocusable: () => true,
+      },
     });
 
-    fixture = TestBed.createComponent(ApiEndpointsGroupsComponent);
+    fixture = TestBed.createComponent(TestComponent);
     fixture.componentInstance.api = api;
 
     loader = TestbedHarnessEnvironment.loader(fixture);
+    rootLoader = TestbedHarnessEnvironment.documentRootLoader(fixture);
     httpTestingController = TestBed.inject(HttpTestingController);
     fixture.detectChanges();
+
+    componentHarness = await loader.getHarness(ApiEndpointsGroupsHarness);
   };
 
   afterEach(() => {
@@ -52,45 +128,53 @@ describe('ApiEndpointsGroupsComponent', () => {
 
   describe('table display tests', () => {
     it('should display the endpoint groups tables', async () => {
-      initComponent(
-        fakeApiV4({
-          id: API_ID,
-          endpointGroups: [
-            {
-              name: 'default-group',
-              type: 'kafka',
-              endpoints: [
-                {
-                  name: 'a kafka',
-                  type: 'kafka',
-                  weight: 1,
-                  inheritConfiguration: false,
-                  configuration: {
-                    bootstrapServers: 'localhost:9092',
-                  },
-                },
-                {
-                  name: 'another kafka',
-                  type: 'kafka',
-                  weight: 5,
-                  inheritConfiguration: false,
-                  configuration: {
-                    bootstrapServers: 'localhost:9093',
-                  },
-                },
-              ],
-            },
-          ],
-        }),
-      );
+      await initComponent(apiV4);
 
-      const rtTable0 = await loader.getHarness(MatTableHarness.with({ selector: '#groupsTable-0' }));
-      const rtTableRows0 = await rtTable0.getCellTextByIndex();
-
-      expect(rtTableRows0).toEqual([
+      expect(await componentHarness.getTableRows(0)).toEqual([
         ['a kafka', '', '1', ''],
         ['another kafka', '', '5', ''],
       ]);
     });
   });
+
+  describe('deleteGroup', () => {
+    it('should delete the endpoint group', async () => {
+      await initComponent(apiV4);
+      await componentHarness.deleteEndpointGroup(0, rootLoader);
+
+      expectApiGetRequest(apiV4);
+      expectApiPutRequest({
+        ...apiV4,
+        endpointGroups: [
+          {
+            name: 'default-mock',
+            type: 'kafka',
+            endpoints: [
+              {
+                name: 'a kafka',
+                type: 'mock',
+                weight: 1,
+                inheritConfiguration: false,
+                configuration: {},
+              },
+            ],
+          },
+        ],
+      });
+    });
+  });
+
+  function expectApiGetRequest(api: ApiV4) {
+    httpTestingController
+      .expectOne({
+        url: `${CONSTANTS_TESTING.env.v2BaseURL}/apis/${api.id}`,
+        method: 'GET',
+      })
+      .flush(api);
+  }
+
+  function expectApiPutRequest(api: ApiV4) {
+    httpTestingController.expectOne({ url: `${CONSTANTS_TESTING.env.v2BaseURL}/apis/${api.id}`, method: 'PUT' }).flush(api);
+    fixture.detectChanges();
+  }
 });
---
+++ gravitee-apim-console-webui/src/management/api/endpoints-v4/backend-services/endpoint/api-endpoints-groups.harness.ts
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { ComponentHarness, HarnessLoader } from '@angular/cdk/testing';
+import { MatTableHarness } from '@angular/material/table/testing';
+import { MatDialogHarness } from '@angular/material/dialog/testing';
+import { MatButtonHarness } from '@angular/material/button/testing';
+
+export class ApiEndpointsGroupsHarness extends ComponentHarness {
+  static hostSelector = 'api-endpoints-groups';
+
+  private getDeleteEndpointGroupButtons = this.locatorForAll(MatButtonHarness.with({ selector: '[aria-label="Delete endpoints group"]' }));
+
+  public async getTableRows(index: number) {
+    const table = this.locatorFor(MatTableHarness.with({ selector: `#groupsTable-${index}` }));
+    return await table().then((t) => t.getCellTextByIndex());
+  }
+
+  public async deleteEndpointGroup(index: number, rootLoader: HarnessLoader) {
+    const button = (await this.getDeleteEndpointGroupButtons())[index];
+    await button.click();
+    return await rootLoader
+      .getHarness(MatDialogHarness)
+      .then((dialog) => dialog.getHarness(MatButtonHarness.with({ text: /Delete/ })))
+      .then((element) => element.click());
+  }
+}
