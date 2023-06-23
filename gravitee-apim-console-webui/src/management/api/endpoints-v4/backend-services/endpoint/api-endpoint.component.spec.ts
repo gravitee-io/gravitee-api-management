0bfd2f6e9a1e958292cb01d3fcb283c26aedb468
Ouahid Khelifi <ouahid.khelifi@gmail.com>
2023-06-23 15:40:04 +0200

    feat: edit existing endpoint

--- gravitee-apim-console-webui/src/management/api/endpoints-v4/backend-services/endpoint/api-endpoint.component.spec.ts
+++ gravitee-apim-console-webui/src/management/api/endpoints-v4/backend-services/endpoint/api-endpoint.component.spec.ts
@@ -21,13 +21,14 @@ import { HarnessLoader } from '@angular/cdk/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconTestingModule } from '@angular/material/icon/testing';
 import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { MatInputHarness } from '@angular/material/input/testing';
 
 import { ApiEndpointComponent } from './api-endpoint.component';
 import { ApiEndpointModule } from './api-endpoint.module';
 import { ApiEndpointHarness } from './api-endpoint.harness';
 
 import { CONSTANTS_TESTING, GioHttpTestingModule } from '../../../../../shared/testing';
-import { ApiV4, fakeApiV4 } from '../../../../../entities/management-api-v2';
+import { ApiV4, fakeApiV4, fakeConnectorPlugin } from '../../../../../entities/management-api-v2';
 import { UIRouterState, UIRouterStateParams } from '../../../../../ajs-upgraded-providers';
 
 @Component({
@@ -52,13 +53,13 @@ describe('ApiEndpointComponent', () => {
   let loader: HarnessLoader;
   let componentHarness: ApiEndpointHarness;
 
-  const initComponent = async (api: ApiV4) => {
+  const initComponent = async (api: ApiV4, routerParams: unknown = { apiId: API_ID, groupIndex: 0 }) => {
     TestBed.configureTestingModule({
       declarations: [TestComponent],
       imports: [NoopAnimationsModule, GioHttpTestingModule, ApiEndpointModule, MatIconTestingModule],
       providers: [
         { provide: UIRouterState, useValue: fakeAjsState },
-        { provide: UIRouterStateParams, useValue: { apiId: API_ID, groupIndex: 0 } },
+        { provide: UIRouterStateParams, useValue: routerParams },
       ],
     });
 
@@ -70,6 +71,11 @@ describe('ApiEndpointComponent', () => {
     fixture.detectChanges();
 
     componentHarness = await loader.getHarness(ApiEndpointHarness);
+
+    expectApiGetRequest(api);
+    expectEndpointSchemaGetRequest(api.endpointGroups[0].type);
+    expectEndpointsSharedConfigurationSchemaGetRequest(api.endpointGroups[0].type);
+    expectEndpointPluginGetRequest(api.endpointGroups[0].type);
   };
 
   afterEach(() => {
@@ -80,18 +86,10 @@ describe('ApiEndpointComponent', () => {
   describe('add endpoints in a group', () => {
     it('should load kafka endpoint form dynamically', async () => {
       await initComponent(apiV4);
-
-      expectApiGetRequest(apiV4);
-      expectEndpointSchemaGetRequest(apiV4.endpointGroups[0].type);
-      expectEndpointsSharedConfigurationSchemaGetRequest(apiV4.endpointGroups[0].type);
     });
 
     it('should edit and save the endpoint', async () => {
       await initComponent(apiV4);
-
-      expectApiGetRequest(apiV4);
-      expectEndpointSchemaGetRequest(apiV4.endpointGroups[0].type);
-      expectEndpointsSharedConfigurationSchemaGetRequest(apiV4.endpointGroups[0].type);
 
       expect(await componentHarness.isSaveButtonDisabled()).toBeTruthy();
 
@@ -134,15 +132,95 @@ describe('ApiEndpointComponent', () => {
       expectApiPutRequest(updatedApi);
       expect(fakeAjsState.go).toHaveBeenCalledWith('management.apis.ng.endpoints');
     });
+
+    it('should edit and save an existing endpoint', async () => {
+      await initComponent(apiV4, { apiId: API_ID, groupIndex: 0, endpointIndex: 0 });
+
+      fixture.detectChanges();
+      expect(await componentHarness.getEndpointName()).toStrictEqual('default');
+
+      await componentHarness.fillInputName('endpoint-name updated');
+      fixture.detectChanges();
+
+      expect(await componentHarness.getEndpointName()).toStrictEqual('endpoint-name updated');
+
+      await componentHarness.clickSaveButton();
+
+      expectApiGetRequest(apiV4);
+
+      const updatedApi: ApiV4 = {
+        ...apiV4,
+        endpointGroups: [
+          {
+            name: 'default-group',
+            type: 'kafka',
+            loadBalancer: {
+              type: 'ROUND_ROBIN',
+            },
+            endpoints: [
+              {
+                name: 'default',
+                type: 'kafka',
+                weight: 1,
+                inheritConfiguration: false,
+                configuration: {
+                  bootstrapServers: 'localhost:9092',
+                },
+              },
+              {
+                name: 'endpoint-name updated',
+                type: 'kafka',
+              },
+            ],
+          },
+        ],
+      };
+      expectApiPutRequest(updatedApi);
+      expect(fakeAjsState.go).toHaveBeenCalledWith('management.apis.ng.endpoints');
+    });
+
+    it('should inherit configuration from parent', async () => {
+      const anApi = fakeApiV4({
+        id: API_ID,
+        endpointGroups: [
+          {
+            name: 'a group',
+            type: 'kafka',
+            loadBalancer: {
+              type: 'ROUND_ROBIN',
+            },
+            sharedConfiguration: {
+              test: 'test',
+            },
+            endpoints: [
+              {
+                name: 'a kafka endpoint',
+                type: 'kafka',
+                weight: 1,
+                inheritConfiguration: true,
+              },
+            ],
+          },
+        ],
+      });
+
+      await initComponent(anApi, { apiId: API_ID, groupIndex: 0, endpointIndex: 0 });
+
+      expect(await componentHarness.isConfigurationButtonToggled()).toBeTruthy();
+      fixture.detectChanges();
+
+      const inputHarness = await loader.getHarness(MatInputHarness.with({ selector: '[id*="test"]' }));
+      expect(await inputHarness.isDisabled()).toBeTruthy();
+      expect(await inputHarness.getValue()).toStrictEqual('test');
+
+      await componentHarness.toggleConfigurationButton();
+      fixture.detectChanges();
+    });
   });
 
   describe('onPreviousClick', () => {
     it('should go back to endpoints groups list page', async () => {
       await initComponent(apiV4);
-
-      expectApiGetRequest(apiV4);
-      expectEndpointSchemaGetRequest(apiV4.endpointGroups[0].type);
-      expectEndpointsSharedConfigurationSchemaGetRequest(apiV4.endpointGroups[0].type);
 
       await componentHarness.clickPreviousButton();
 
@@ -153,17 +231,21 @@ describe('ApiEndpointComponent', () => {
   function expectEndpointsSharedConfigurationSchemaGetRequest(id: string) {
     httpTestingController
       .expectOne({ url: `${CONSTANTS_TESTING.v2BaseURL}/plugins/endpoints/${id}/shared-configuration-schema`, method: 'GET' })
-      .flush(
-        '{"$schema":"http://json-schema.org/draft-07/schema#","type":"object","definitions":{"security":{"type":"object","title":"Security configuration","oneOf":[{"title":"Protocol PLAINTEXT","properties":{"protocol":{"const":"PLAINTEXT"}},"additionalProperties":false},{"title":"Protocol SASL_PLAINTEXT","properties":{"protocol":{"const":"SASL_PLAINTEXT"},"sasl":{"$ref":"#/definitions/securityProtocolSasl"}},"additionalProperties":false,"required":["sasl"]},{"title":"Protocol SASL_SSL","properties":{"protocol":{"const":"SASL_SSL"},"sasl":{"$ref":"#/definitions/securityProtocolSasl"},"ssl":{"$ref":"#/definitions/securityProtocolSsl"}},"additionalProperties":false,"required":["sasl","ssl"]},{"title":"Protocol SSL","properties":{"protocol":{"const":"SSL"},"ssl":{"$ref":"#/definitions/securityProtocolSsl"}},"additionalProperties":false,"required":["ssl"]}]}},"oneOf":[{"title":"Use Consumer","properties":{"security":{"$ref":"#/definitions/security"},"consumer":{"$ref":"#/definitions/consumer"}},"required":["consumer"],"additionalProperties":false},{"title":"Use Producer","properties":{"security":{"$ref":"#/definitions/security"},"producer":{"$ref":"#/definitions/producer"}},"required":["producer"],"additionalProperties":false},{"title":"Use Consumer and Producer","properties":{"security":{"$ref":"#/definitions/security"},"producer":{"$ref":"#/definitions/producer"},"consumer":{"$ref":"#/definitions/consumer"}},"required":["producer","consumer"],"additionalProperties":false}]}',
-      );
+      .flush({
+        $schema: 'http://json-schema.org/draft-07/schema#',
+        type: 'object',
+        properties: { test: { title: 'test', description: 'test', type: 'string' } },
+      });
   }
 
   function expectEndpointSchemaGetRequest(id: string) {
-    httpTestingController
-      .expectOne({ url: `${CONSTANTS_TESTING.v2BaseURL}/plugins/endpoints/${id}/schema`, method: 'GET' })
-      .flush(
-        '{"$schema":"http://json-schema.org/draft-07/schema#","type":"object","definitions":{"bootstrapServers":{"title":"Bootstrap servers (bootstrap.servers)","description":"This list should be in the form host1:port1,host2:port2,...","type":"string","gioConfig":{"banner":{"title":"Bootstrap servers","text":"A list of host/port pairs, separated by a comma, to use for establishing the initial connection to the Kafka cluster. The client will make use of all servers irrespective of which servers are specified here for bootstrapping—this list only impacts the initial hosts used to discover the full set of servers. "}}}},"properties":{"bootstrapServers":{"$ref":"#/definitions/bootstrapServers"}},"required":["bootstrapServers"],"additionalProperties":false}',
-      );
+    httpTestingController.expectOne({ url: `${CONSTANTS_TESTING.v2BaseURL}/plugins/endpoints/${id}/schema`, method: 'GET' }).flush({
+      $schema: 'http://json-schema.org/draft-07/schema#',
+      type: 'object',
+      properties: {
+        bootstrapServers: { title: 'bootstrapServers', description: 'The list of kafka bootstrap servers.', type: 'string' },
+      },
+    });
     fixture.detectChanges();
   }
 
@@ -175,5 +257,11 @@ describe('ApiEndpointComponent', () => {
   function expectApiPutRequest(api: ApiV4) {
     httpTestingController.expectOne({ url: `${CONSTANTS_TESTING.env.v2BaseURL}/apis/${api.id}`, method: 'PUT' }).flush(api);
     fixture.detectChanges();
+  }
+
+  function expectEndpointPluginGetRequest(pluginId: string) {
+    httpTestingController
+      .expectOne({ url: `${CONSTANTS_TESTING.v2BaseURL}/plugins/endpoints/${pluginId}`, method: 'GET' })
+      .flush([fakeConnectorPlugin({ id: pluginId, name: pluginId })]);
   }
 });
