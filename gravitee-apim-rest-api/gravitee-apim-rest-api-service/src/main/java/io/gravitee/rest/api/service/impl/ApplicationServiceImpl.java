9327232b5a466d7a0dabd99fdae350e922060a90
Ouahid Khelifi <ouahid.khelifi@gmail.com>
2023-07-12 10:30:44 +0200

    fix: filter subscriptions when validate application client_id

--- gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/rest/api/service/impl/ApplicationServiceImpl.java
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/rest/api/service/impl/ApplicationServiceImpl.java
@@ -643,6 +643,11 @@ public class ApplicationServiceImpl extends AbstractService implements Applicati
             Set<String> planIds = subscriptionService
                 .findByApplicationAndPlan(executionContext, applicationId, null)
                 .stream()
+                .filter(subscriptionEntity ->
+                    Set
+                        .of(SubscriptionStatus.ACCEPTED, SubscriptionStatus.PAUSED, SubscriptionStatus.PENDING)
+                        .contains(subscriptionEntity.getStatus())
+                )
                 .map(SubscriptionEntity::getPlan)
                 .collect(Collectors.toSet());
 
