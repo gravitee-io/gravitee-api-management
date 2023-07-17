ea5b7d4d4573ec9269916a1af6c8b54de8d3db44
Guillaume Cusnieux <guillaume.cusnieux@graviteesource.com>
2023-07-17 16:59:50 +0200

    fix: use applicationType from db instead of optional metadata
    In openId spec, the application_type property is optional (https://openid.net/specs/openid-connect-registration-1_0.html\#ClientMetadata)
    
    (cherry picked from commit 413b0c9181757c80163168fb5c6efcb5167dc68b)

--- gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/test/java/io/gravitee/rest/api/service/impl/ApplicationService_UpdateTest.java
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/test/java/io/gravitee/rest/api/service/impl/ApplicationService_UpdateTest.java
@@ -479,7 +479,7 @@ public class ApplicationService_UpdateTest {
         ApplicationSettings settings = new ApplicationSettings();
         OAuthClientSettings oAuthClientSettings = new OAuthClientSettings();
         oAuthClientSettings.setGrantTypes(List.of("application-grant-type"));
-        oAuthClientSettings.setApplicationType("application-type");
+        oAuthClientSettings.setApplicationType(ApplicationType.BROWSER.name());
         settings.setoAuthClient(oAuthClientSettings);
         when(updateApplication.getSettings()).thenReturn(settings);
 
@@ -489,7 +489,7 @@ public class ApplicationService_UpdateTest {
         applicationGrantTypeEntity.setResponse_types(List.of("response-type"));
         applicationTypeEntity.setAllowed_grant_types(List.of(applicationGrantTypeEntity));
         applicationTypeEntity.setRequires_redirect_uris(false);
-        when(applicationTypeService.getApplicationType("application-type")).thenReturn(applicationTypeEntity);
+        when(applicationTypeService.getApplicationType(ApplicationType.BROWSER.name())).thenReturn(applicationTypeEntity);
 
         // mock response from DCR with a new client ID
         ClientRegistrationResponse clientRegistrationResponse = new ClientRegistrationResponse();
@@ -540,7 +540,7 @@ public class ApplicationService_UpdateTest {
         ApplicationSettings settings = new ApplicationSettings();
         OAuthClientSettings oAuthClientSettings = new OAuthClientSettings();
         oAuthClientSettings.setGrantTypes(List.of("application-grant-type"));
-        oAuthClientSettings.setApplicationType("application-type");
+        oAuthClientSettings.setApplicationType(ApplicationType.BROWSER.name());
         settings.setoAuthClient(oAuthClientSettings);
         when(updateApplication.getSettings()).thenReturn(settings);
 
@@ -550,7 +550,7 @@ public class ApplicationService_UpdateTest {
         applicationGrantTypeEntity.setResponse_types(List.of("response-type"));
         applicationTypeEntity.setAllowed_grant_types(List.of(applicationGrantTypeEntity));
         applicationTypeEntity.setRequires_redirect_uris(false);
-        when(applicationTypeService.getApplicationType("application-type")).thenReturn(applicationTypeEntity);
+        when(applicationTypeService.getApplicationType(ApplicationType.BROWSER.name())).thenReturn(applicationTypeEntity);
 
         // DCR throws exception
         when(clientRegistrationService.update(any(), any(), same(updateApplication))).thenThrow(RuntimeException.class);
