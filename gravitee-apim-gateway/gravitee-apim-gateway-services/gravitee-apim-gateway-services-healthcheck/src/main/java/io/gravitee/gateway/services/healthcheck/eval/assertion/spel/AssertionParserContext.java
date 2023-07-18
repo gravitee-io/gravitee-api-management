07cf31c8f2bb346ab9c070c0d1a9e7b5860d734c
David Brassely <brasseld@gmail.com>
2023-07-18 12:30:06 +0200

    fix: Add support for secured EL by extending the parser context
    https://gravitee.atlassian.net/browse/APIM-2193

---
+++ gravitee-apim-gateway/gravitee-apim-gateway-services/gravitee-apim-gateway-services-healthcheck/src/main/java/io/gravitee/gateway/services/healthcheck/eval/assertion/spel/AssertionParserContext.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright © 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.gateway.services.healthcheck.eval.assertion.spel;
+
+import io.reactivex.rxjava3.annotations.NonNull;
+import org.springframework.expression.ParserContext;
+
+/**
+ * @author David BRASSELY (david.brassely at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+public class AssertionParserContext implements ParserContext {
+
+    private final String prefix;
+    private final String suffix;
+
+    public AssertionParserContext(String prefix, String suffix) {
+        this.prefix = prefix;
+        this.suffix = suffix;
+    }
+
+    @NonNull
+    @Override
+    public String getExpressionPrefix() {
+        return prefix;
+    }
+
+    @NonNull
+    @Override
+    public String getExpressionSuffix() {
+        return suffix;
+    }
+
+    @Override
+    public boolean isTemplate() {
+        return false;
+    }
+}
