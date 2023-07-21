50a4e901a1d994dc05e2daf868f50a4ee97a8b80
Julian Robotham <julian.robotham@graviteesource.com>
2023-07-21 12:35:54 +0100

    fix: [APIM-1701] add liecence header

--- gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/test/java/io/gravitee/rest/api/service/processor/SynchronizationServiceTest.java
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/test/java/io/gravitee/rest/api/service/processor/SynchronizationServiceTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright © 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.gravitee.rest.api.service.processor;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -29,9 +44,9 @@ class SynchronizationServiceTest {
 
     @Test
     @DisplayName(
-        "GIVEN an entity" +
-        "WHEN the required entity fields are retrieved" +
-        "THEN only the correct amount of required fields should be returned"
+            "GIVEN an entity" +
+                    "WHEN the required entity fields are retrieved" +
+                    "THEN only the correct amount of required fields should be returned"
     )
     void thenTheRequiredFieldsShouldBeReturned() {
         ApiEntity entity = new ApiEntity();
