e9bf5c9dd369cc8a5fd9c63c92f49cefff1b43ba
Gaëtan Maisse <gaetanmaisse@gmail.com>
2023-08-03 10:43:28 +0200

    chore: update changelog generation process for APIM 4.0+

--- release/helpers/changelog-helper.mjs
+++ release/helpers/changelog-helper.mjs
@@ -1,23 +1,28 @@
 /**
  * Get the Ascii doc formatted changelog for input issues
  *
- * @param issues {Array<{id: string, fields: Array<{customfield_10115: string,summary: string}>}>}
+ * @param issues {Array<{id: string, githubIssue: string, fields: Array<{summary: string}>}>}
  */
 export function getChangelogFor(issues) {
   return issues
     .sort((issue1, issue2) => {
       // if null or undefined, put it at the end
-      if (!issue1.fields.customfield_10115) {
+      if (!issue1.githubIssue) {
         return 1;
-      } else if (!issue2.fields.customfield_10115) {
+      } else if (!issue2.githubIssue) {
         return -1;
       } else {
-        return issue1.fields.customfield_10115 - issue2.fields.customfield_10115;
+        return issue1.githubIssue - issue2.githubIssue;
       }
     })
     .map((issue) => {
-      const githubLink = `https://github.com/gravitee-io/issues/issues/${issue.fields.customfield_10115}`;
-      return `* ${issue.fields.summary} ${githubLink}[#${issue.fields.customfield_10115}]`;
+      if (issue.githubIssue && issue.githubIssue !== '') {
+        const githubLink = `https://github.com/gravitee-io/issues/issues/${issue.githubIssue}`;
+        return `* ${issue.summary} [#${issue.githubIssue}](${githubLink})`;
+      }
+
+      return `* ${issue.summary}`;
+
     })
     .join('\n');
 }
