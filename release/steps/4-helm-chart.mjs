e1e383cac1dd0e84130d538015408938f5ba6829
Gaëtan Maisse <gaetanmaisse@gmail.com>
2023-08-04 08:57:17 +0200

    ci: add step to trigger release of helm charts
    (cherry picked from commit edfa89997932d9390c175d2196adc77903b77af7)

--- release/steps/4-helm-chart.mjs
+++ release/steps/4-helm-chart.mjs
@@ -9,24 +9,12 @@ await checkToken();
 const releasingVersion = await extractVersion();
 const versions = computeVersion(releasingVersion);
 
-let isLatest = false;
-if (argv.latest) {
-  isLatest = true;
-} else {
-  const shouldBeLatest = await question(chalk.blue(`No '--latest' parameter. Should this version be flagged as 'latest'? (y/n)\n`));
-  if (shouldBeLatest === 'y') {
-    isLatest = true;
-  }
-}
+console.log(chalk.blue(`Triggering Helm Chart generation Pipeline`));
 
-console.log(chalk.blue(`Triggering Docker & RPMs Pipeline`));
-
-// Use the preconfigured payload from config folder with the good parameters
 const body = {
   branch: versions.branch,
   parameters: {
-    gio_action: 'build_rpm_&_docker_images',
-    docker_tag_as_latest: isLatest,
+    gio_action: 'release_helm',
     dry_run: isDryRun(),
     graviteeio_version: releasingVersion,
   },
@@ -46,7 +34,7 @@ const data = await response.json();
 if (response.status === 201) {
   console.log(chalk.green(`Pipeline created with number: ${data.number}`));
   echo`Follow its progress on: https://app.circleci.com/pipelines/github/gravitee-io/gravitee-api-management/${data.number}`;
-  console.log(chalk.greenBright(`When it's done, run 'npm run helm_chart -- --version=${releasingVersion}'`));
+  console.log(chalk.greenBright(`When it's done, run 'npm run release_notes -- --version=${releasingVersion}'`));
 } else {
   console.log(chalk.yellow('Something went wrong'));
 }
