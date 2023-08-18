3041f7e8b68caac946ff97764bcc237145bceebf
Julien Giovaresco <dev@giovaresco.fr>
2023-08-18 14:17:29 +0200

    test: add custom assertion for Response

---
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-management-v2/gravitee-apim-rest-api-management-v2-rest/src/test/java/assertions/ErrorAssert.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright © 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package assertions;
+
+import io.gravitee.rest.api.management.v2.rest.model.Error;
+import java.util.Objects;
+import org.assertj.core.api.AbstractObjectAssert;
+
+public class ErrorAssert extends AbstractObjectAssert<ErrorAssert, Error> {
+
+    public ErrorAssert(Error error) {
+        super(error, ErrorAssert.class);
+    }
+
+    public ErrorAssert hasHttpStatus(int status) {
+        isNotNull();
+
+        if (!Objects.equals(actual.getHttpStatus(), status)) {
+            failWithMessage("Expected error http status to be <%s> but was <%s>", status, actual.getHttpStatus());
+        }
+        return this;
+    }
+
+    public ErrorAssert hasMessage(String message) {
+        isNotNull();
+
+        if (!Objects.equals(actual.getMessage(), message)) {
+            failWithMessage("Expected error message to be <%s> but was <%s>", message, actual.getMessage());
+        }
+        return this;
+    }
+}
---
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-management-v2/gravitee-apim-rest-api-management-v2-rest/src/test/java/assertions/ResponseAssert.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright © 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package assertions;
+
+import io.gravitee.rest.api.management.v2.rest.model.Error;
+import jakarta.ws.rs.core.Response;
+import org.assertj.core.api.AbstractObjectAssert;
+import org.assertj.core.api.AssertionsForClassTypes;
+import org.assertj.core.api.ObjectAssert;
+
+public class ResponseAssert extends AbstractObjectAssert<ResponseAssert, Response> {
+
+    public ResponseAssert(Response apiEntity) {
+        super(apiEntity, ResponseAssert.class);
+    }
+
+    public ResponseAssert hasStatus(int status) {
+        isNotNull();
+        if (actual.getStatus() != status) {
+            failWithMessage("Expected response status to be <%s> but was <%s>", status, actual.getStatus());
+        }
+        return this;
+    }
+
+    public ErrorAssert asError() {
+        isNotNull();
+        var error = actual.readEntity(Error.class);
+        return new ErrorAssert(error);
+    }
+
+    public <T> ObjectAssert<T> asEntity(Class<T> clazz) {
+        isNotNull();
+        T entity = actual.readEntity(clazz);
+        return AssertionsForClassTypes.assertThat(entity);
+    }
+}
