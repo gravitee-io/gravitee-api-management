64d6dc2d8e459b7bde0b2710e74da9f076c7bc7e
Julien Giovaresco <dev@giovaresco.fr>
2023-08-21 10:34:17 +0200

    feat: design rest resource fetching logs

---
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-management-v2/gravitee-apim-rest-api-management-v2-rest/src/test/java/io/gravitee/rest/api/management/v2/rest/resource/api/ApiLogsResourceTest.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright © 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.rest.api.management.v2.rest.resource.api;
+
+import static assertions.MAPIAssertions.assertThat;
+import static io.gravitee.common.http.HttpStatusCode.FORBIDDEN_403;
+import static io.gravitee.common.http.HttpStatusCode.OK_200;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+
+import io.gravitee.rest.api.management.v2.rest.model.ApiLog;
+import io.gravitee.rest.api.management.v2.rest.model.ApiLogsResponse;
+import io.gravitee.rest.api.management.v2.rest.model.BasePlan;
+import io.gravitee.rest.api.management.v2.rest.model.Links;
+import io.gravitee.rest.api.management.v2.rest.model.Pagination;
+import io.gravitee.rest.api.model.permissions.RolePermission;
+import io.gravitee.rest.api.model.permissions.RolePermissionAction;
+import io.gravitee.rest.api.service.common.GraviteeContext;
+import jakarta.ws.rs.client.WebTarget;
+import jakarta.ws.rs.core.Response;
+import java.time.Instant;
+import java.time.ZoneOffset;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ApiLogsResourceTest extends ApiResourceTest {
+
+    WebTarget target;
+
+    @Override
+    protected String contextPath() {
+        return "/environments/" + ENVIRONMENT + "/apis/" + API + "/logs";
+    }
+
+    @BeforeEach
+    public void setup() {
+        target = rootTarget();
+
+        GraviteeContext.setCurrentEnvironment(ENVIRONMENT);
+        GraviteeContext.setCurrentOrganization(ORGANIZATION);
+    }
+
+    @Test
+    public void should_return_403_if_incorrect_permissions() {
+        when(
+            permissionService.hasPermission(
+                eq(GraviteeContext.getExecutionContext()),
+                eq(RolePermission.API_LOG),
+                eq(API),
+                eq(RolePermissionAction.READ)
+            )
+        )
+            .thenReturn(false);
+
+        final Response response = target.request().get();
+
+        assertThat(response)
+            .hasStatus(FORBIDDEN_403)
+            .asError()
+            .hasHttpStatus(FORBIDDEN_403)
+            .hasMessage("You do not have sufficient rights to access this resource");
+    }
+
+    @Test
+    public void should_return_the_1st_page_of_logs() {
+        final Response response = target.request().get();
+
+        assertThat(response)
+            .hasStatus(OK_200)
+            .asEntity(ApiLogsResponse.class)
+            .isEqualTo(
+                ApiLogsResponse
+                    .builder()
+                    .data(
+                        List.of(
+                            ApiLog
+                                .builder()
+                                .id("log-id")
+                                .application(null)
+                                .plan(BasePlan.builder().id("id-keyless").name("Keyless").apiId(API).build())
+                                .status(200)
+                                .clientIdentifier("client-id")
+                                .requestEnded(true)
+                                .requestId("request-id")
+                                .transactionId("transaction-id")
+                                .timestamp(Instant.parse("2020-01-01T00:00:00.00Z").atOffset(ZoneOffset.UTC))
+                                .build()
+                        )
+                    )
+                    .pagination(Pagination.builder().page(1).perPage(10).pageCount(1).pageItemsCount(1).totalCount(1).build())
+                    .links(Links.builder().self(target.getUri().toString()).build())
+                    .build()
+            );
+    }
+}
