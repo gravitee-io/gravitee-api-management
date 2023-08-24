f99e9cac30f4d1c7f3e9b72b436cc6909a0916b1
Ouahid Khelifi <ouahid.khelifi@gmail.com>
2023-08-24 17:44:24 +0200

    fix: map sampling in API V4 analytics
    (cherry picked from commit 47ac49f14aa8f541c32fba95844fddaf76e61d1a)

---
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-management-v2/gravitee-apim-rest-api-management-v2-rest/src/main/java/io/gravitee/rest/api/management/v2/rest/mapper/AnalyticsMapper.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright © 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.rest.api.management.v2.rest.mapper;
+
+import io.gravitee.definition.model.v4.analytics.sampling.SamplingType;
+import io.gravitee.rest.api.management.v2.rest.model.Sampling;
+import org.mapstruct.Mapper;
+import io.gravitee.rest.api.management.v2.rest.model.Analytics;
+import org.mapstruct.Mapping;
+import org.mapstruct.ValueMapping;
+
+@Mapper
+public interface AnalyticsMapper {
+
+    @Mapping(target = "sampling", source = "messageSampling")
+    Analytics toAnalytics(io.gravitee.definition.model.v4.analytics.Analytics analytics);
+
+    @Mapping(target = "messageSampling", source = "sampling")
+    io.gravitee.definition.model.v4.analytics.Analytics fromAnalytics(Analytics analytics);
+}
---
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-management-v2/gravitee-apim-rest-api-management-v2-rest/src/test/java/io/gravitee/rest/api/management/v2/rest/mapper/AnalyticsMapperTest.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright © 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.rest.api.management.v2.rest.mapper;
+
+import fixtures.AnalyticsFixtures;
+import io.gravitee.definition.model.v4.analytics.Analytics;
+import io.gravitee.definition.model.v4.analytics.sampling.SamplingType;
+import io.gravitee.rest.api.management.v2.rest.model.Sampling;
+import org.junit.jupiter.api.Test;
+import org.mapstruct.factory.Mappers;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class AnalyticsMapperTest {
+
+    private final AnalyticsMapper analyticsMapper = Mappers.getMapper(AnalyticsMapper.class);
+
+    @Test
+    void shouldMapDefinitionModelToRestModel() {
+        Analytics analytics = AnalyticsFixtures.anAnalytics();
+        var mappedAnalytics = analyticsMapper.toAnalytics(analytics);
+        assertThat(mappedAnalytics).isNotNull();
+        assertThat(mappedAnalytics.getEnabled()).isEqualTo(analytics.isEnabled());
+        assertThat(mappedAnalytics.getSampling()).isNotNull();
+        assertThat("10").isEqualTo(mappedAnalytics.getSampling().getValue());
+        assertThat(Sampling.TypeEnum.COUNT).isEqualTo(mappedAnalytics.getSampling().getType());
+        assertThat(mappedAnalytics.getLogging()).isNotNull();
+    }
+
+    @Test
+    void shouldMapRestModelToDefinitionModel() {
+        io.gravitee.rest.api.management.v2.rest.model.Analytics analytics = AnalyticsFixtures.aBasicAnalytics();
+        var mappedAnalytics = analyticsMapper.fromAnalytics(analytics);
+        assertThat(mappedAnalytics).isNotNull();
+        assertThat(mappedAnalytics.isEnabled()).isEqualTo(analytics.getEnabled());
+        assertThat(mappedAnalytics.getMessageSampling()).isNotNull();
+        assertThat("10").isEqualTo(mappedAnalytics.getMessageSampling().getValue());
+        assertThat(Sampling.TypeEnum.COUNT.toString()).isEqualTo(mappedAnalytics.getMessageSampling().getType().toString());
+        assertThat(mappedAnalytics.getLogging()).isNotNull();
+    }
+}
