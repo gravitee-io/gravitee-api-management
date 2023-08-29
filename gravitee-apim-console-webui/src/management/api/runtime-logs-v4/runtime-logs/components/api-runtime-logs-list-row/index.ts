7573e8830a06e2fdfe40f669c73f46cf7fb20181
Julien Giovaresco <dev@giovaresco.fr>
2023-08-29 16:16:21 +0200

    chore: add api-runtime-logs-list-row component

--- gravitee-apim-console-webui/src/management/api/runtime-logs-v4/runtime-logs/components/api-runtime-logs-list-row/index.ts
+++ gravitee-apim-console-webui/src/management/api/runtime-logs-v4/runtime-logs/components/api-runtime-logs-list-row/index.ts
@@ -13,5 +13,5 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-export * from './api-runtime-logs-empty';
-export * from './api-runtime-logs-list-row';
+export * from './api-runtime-logs-list-row.harness';
+export * from './api-runtime-logs-list-row.module';
