7d2422b8f6639023336faa6871550282846730fa
Julien Giovaresco <dev@giovaresco.fr>
2023-08-29 17:05:11 +0200

    chore: implement api-logs-v2.service.ts
    (cherry picked from commit cdf308fa856e938c85bcd52c56019ebe0980c1a4)

---
+++ gravitee-apim-console-webui/src/services-ngx/api-logs-v2.service.spec.ts
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { HttpTestingController } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+
+import { ApiLogsV2Service } from './api-logs-v2.service';
+
+import { CONSTANTS_TESTING, GioHttpTestingModule } from '../shared/testing';
+import { ApiLogsResponse } from '../entities/management-api-v2';
+import { fakeConnectionLog } from '../entities/management-api-v2/log/connectionLog.fixture';
+
+describe('ApiLogsV2Service', () => {
+  let httpTestingController: HttpTestingController;
+  let apiPlanV2Service: ApiLogsV2Service;
+  const API_ID = 'api-id';
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [GioHttpTestingModule],
+    });
+
+    httpTestingController = TestBed.inject(HttpTestingController);
+    apiPlanV2Service = TestBed.inject<ApiLogsV2Service>(ApiLogsV2Service);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
+  });
+
+  describe('searchConnectionLogs', () => {
+    it('should call the API', (done) => {
+      const fakeResponse: ApiLogsResponse = {
+        data: [fakeConnectionLog({ apiId: API_ID })],
+      };
+
+      apiPlanV2Service.searchConnectionLogs(API_ID).subscribe((apiPlansResponse) => {
+        expect(apiPlansResponse.data).toEqual(fakeResponse.data);
+        done();
+      });
+
+      const req = httpTestingController.expectOne({
+        url: `${CONSTANTS_TESTING.env.v2BaseURL}/apis/${API_ID}/logs?page=1&perPage=10`,
+        method: 'GET',
+      });
+
+      req.flush(fakeResponse);
+    });
+  });
+});
