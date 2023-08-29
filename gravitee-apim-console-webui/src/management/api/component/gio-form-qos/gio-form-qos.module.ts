c161dd4e582761c4d1715cfce0a283a22783722a
Florent CHAMFROY <florent.chamfroy@graviteesource.com>
2023-08-29 15:43:33 +0200

    feat(qos): create a shared component to handle qos

---
+++ gravitee-apim-console-webui/src/management/api/component/gio-form-qos/gio-form-qos.module.ts
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { GioBannerModule, GioIconsModule } from '@gravitee/ui-particles-angular';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatOptionModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+
+import { GioFormQosComponent } from './gio-form-qos.component';
+
+import { GioConnectorListModule } from '../../../../shared/components/gio-connector-list-option/gio-connector-list.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatListModule,
+    GioConnectorListModule,
+    GioIconsModule,
+    MatButtonModule,
+    FormsModule,
+    GioBannerModule,
+    MatCardModule,
+    MatOptionModule,
+    MatSelectModule,
+  ],
+  declarations: [GioFormQosComponent],
+  exports: [GioFormQosComponent],
+})
+export class GioFormQosModule {}
