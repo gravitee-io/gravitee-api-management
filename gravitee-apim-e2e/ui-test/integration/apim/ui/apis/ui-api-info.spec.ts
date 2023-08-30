30ed1ea1675781d67a49a329cc00f7e2f4c6b789
Kyle Kibbey <kkibbey1996@gmail.com>
2023-08-30 10:09:22 +0100

    test(ui): info dangerzone buttons in progress

--- gravitee-apim-e2e/ui-test/integration/apim/ui/apis/ui-api-info.spec.ts
+++ gravitee-apim-e2e/ui-test/integration/apim/ui/apis/ui-api-info.spec.ts
@@ -13,14 +13,33 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { ADMIN_USER } from '@fakers/users/users';
+import { ADMIN_USER, API_PUBLISHER_USER } from '@fakers/users/users';
 import { ApisFaker } from '@gravitee/fixtures/management/ApisFaker';
 import { Visibility } from '@gravitee/management-webclient-sdk/src/lib/models';
 import faker from '@faker-js/faker';
 import ApiDetails from 'ui-test/support/PageObjects/Apis/ApiDetails';
 import { Api } from '@model/apis';
+import { MAPIV2PlansFaker } from '@gravitee/fixtures/management/MAPIV2PlansFaker';
+import { MAPIV2ApisFaker } from '@gravitee/fixtures/management/MAPIV2ApisFaker';
+import { ApiType, ApiV4, FlowV4, HttpListener } from '@gravitee/management-v2-webclient-sdk/src/lib';
+
+const envId = 'DEFAULT';
 
 describe('API Info Page functionality', () => {
+  let api: Api;
+  let duplicateApi: ApiV4;
+  let v4dangerzoneApi: ApiV4;
+  let v4infoApi: ApiV4;
+  const apiDetails = new ApiDetails();
+  const apiName = faker.commerce.productName();
+  const apiVersion = faker.datatype.number();
+  const apiDescription = faker.lorem.words(10);
+  const apiFileName = `${apiName.replace(/ /g, '-')}-${apiVersion}`;
+  const path = require('path');
+  const httpListener1: HttpListener = MAPIV2ApisFaker.newHttpListener();
+  const httpListener2: HttpListener = MAPIV2ApisFaker.newHttpListener();
+  const commonFlow: FlowV4 = MAPIV2ApisFaker.newFlow();
+
   beforeEach(() => {
     cy.loginInAPIM(ADMIN_USER.username, ADMIN_USER.password);
     cy.visit('/#!/environments/default/apis/');
@@ -28,7 +47,7 @@ describe('API Info Page functionality', () => {
   });
 
   before(() => {
-    cy.log('Import (create) v2 API');
+    cy.log('Import (create) v2 API for Info');
     cy.request({
       method: 'POST',
       url: `${Cypress.env('managementApi')}${Cypress.env('defaultOrgEnv')}/apis/import`,
@@ -38,19 +57,64 @@ describe('API Info Page functionality', () => {
       expect(response.status).to.eq(200);
       api = { ...response.body };
     });
+
+    cy.log('Create v4 API for Info');
+    cy.request({
+      method: 'POST',
+      url: `${Cypress.env('managementApi')}/management/v2/environments/DEFAULT/apis`,
+      auth: { username: API_PUBLISHER_USER.username, password: API_PUBLISHER_USER.password },
+      body: MAPIV2ApisFaker.newApi({
+        type: ApiType.PROXY,
+        listeners: [httpListener1],
+        endpointGroups: [MAPIV2ApisFaker.newHttpEndpointGroup()],
+      }),
+    }).then((response) => {
+      expect(response.status).to.eq(201);
+      v4infoApi = response.body;
+    });
+
+    cy.log('Create v4 API for Dangerzone');
+    cy.request({
+      method: 'POST',
+      url: `${Cypress.env('managementApi')}/management/v2/environments/DEFAULT/apis`,
+      auth: { username: API_PUBLISHER_USER.username, password: API_PUBLISHER_USER.password },
+      body: MAPIV2ApisFaker.newApi({
+        type: ApiType.PROXY,
+        listeners: [httpListener2],
+        endpointGroups: [MAPIV2ApisFaker.newHttpEndpointGroup()],
+        flows: [commonFlow],
+      }),
+    }).then((response) => {
+      expect(response.status).to.eq(201);
+      v4dangerzoneApi = response.body;
+
+      cy.log('Create a plan with a flow');
+      cy.request({
+        method: 'POST',
+        url: `${Cypress.env('managementApi')}/management/v2/environments/${envId}/apis/${v4dangerzoneApi.id}/plans`,
+        auth: { username: ADMIN_USER.username, password: ADMIN_USER.password },
+        body: MAPIV2PlansFaker.newPlanV4(),
+      })
+        .then((response) => {
+          expect(response.status).to.eq(201);
+          return response.body.id;
+        })
+        .then((planId) => {
+          cy.log('Publish Plan');
+          cy.request({
+            method: 'POST',
+            url: `${Cypress.env('managementApi')}/management/v2/environments/DEFAULT/apis/${v4dangerzoneApi.id}/plans/${planId}/_publish`,
+            auth: { username: ADMIN_USER.username, password: ADMIN_USER.password },
+          }).then((response) => {
+            expect(response.status).to.eq(200);
+          });
+        });
+    });
   });
 
-  const apiDetails = new ApiDetails();
-  const apiName = faker.commerce.productName();
-  const apiVersion = faker.datatype.number();
-  const apiDescription = faker.lorem.words(10);
-  const apiFileName = `${apiName.replace(/ /g, '-')}-${apiVersion}`;
-  const path = require('path');
-  let api: Api;
-  let duplicateApi: Api;
-
   it('Verify Info Page Elements', () => {
-    cy.getByDataTestId('api_list_edit_button', { timeout: 5000 }).first().click();
+    cy.visit(`/#!/environments/DEFAULT/apis/ng/${v4infoApi.id}/general`);
+    apiDetails.infoMenuItem().click();
     apiDetails.policyStudioMenuItem().should('be.visible');
     apiDetails.infoMenuItem().should('be.visible');
     apiDetails.plansMenuItem().should('be.visible');
@@ -64,8 +128,7 @@ describe('API Info Page functionality', () => {
   });
 
   it('Edit Info Page and Verify changes', () => {
-    cy.getByDataTestId('api_list_edit_button').first().click();
-    apiDetails.infoMenuItem().click();
+    cy.visit(`/#!/environments/DEFAULT/apis/ng/${v4infoApi.id}/general`);
     cy.getByDataTestId('api_info_namefield').scrollIntoView().clear().type(`${apiName}`);
     cy.getByDataTestId('api_info_versionfield').clear().type(`${apiVersion}`);
     cy.getByDataTestId('api_info_descriptionfield').clear().type(`${apiDescription}`);
@@ -78,22 +141,18 @@ describe('API Info Page functionality', () => {
     cy.contains(apiName).should('be.visible');
   });
 
-  it('Export API and verify json download', () => {
-    cy.getByDataTestId('api_list_edit_button').first().click();
+  it('Export API and verify json download (v4)', () => {
+    cy.visit(`/#!/environments/DEFAULT/apis/ng/${v4infoApi.id}/general`);
     cy.getByDataTestId('api_info_export_menu').click();
-    cy.getByDataTestId('api_info_export_api').click();
     const downloadsFolder = Cypress.config('downloadsFolder');
-    cy.readFile(path.join(downloadsFolder, `${apiFileName}.json`)).should('exist');
-    cy.readFile(path.join(downloadsFolder, `${apiFileName}.json`))
-      .its('name')
-      .should('eq', `${apiName}`);
-    cy.readFile(path.join(downloadsFolder, `${apiFileName}.json`))
-      .its('description')
-      .should('eq', `${apiDescription}`);
+    cy.readFile(path.join(downloadsFolder, `${apiFileName}.json`)).should((file) => {
+      expect(file.api.name).to.equal(apiName);
+      expect(file.api.description).to.equal(apiDescription);
+    });
   });
 
   it('Duplicate API and verify duplicate', () => {
-    cy.getByDataTestId('api_list_edit_button').first().click();
+    cy.visit(`/#!/environments/DEFAULT/apis/ng/${v4infoApi.id}/general`);
     cy.getByDataTestId('api_info_duplicate_menu').click();
     cy.getByDataTestId('api_info_duplicate_path').type(`${apiFileName}-duplicate`);
     cy.getByDataTestId('api_info_duplicate_version').type(`${apiVersion}`);
@@ -102,7 +161,7 @@ describe('API Info Page functionality', () => {
     cy.wait('@duplicateApi').then((interception) => {
       expect(interception.response.statusCode).to.eq(200);
       expect(interception.response.body).to.have.property('id');
-      expect(interception.response.body).to.have.property('name', apiName);
+      expect(interception.response.body).to.have.property('name', `${apiName}`);
       expect(interception.response.body).to.have.property('apiVersion', `${apiVersion}`);
       duplicateApi = interception.response.body;
     });
@@ -111,17 +170,76 @@ describe('API Info Page functionality', () => {
     cy.contains(`${apiFileName}-duplicate`).should('be.visible');
   });
 
-  it('Verify Promote pop-up on info page', () => {
-    cy.getByDataTestId('api_list_edit_button').first().click();
+  it('Verify Promote pop-up on info page (v2)', () => {
+    cy.visit(`/#!/environments/DEFAULT/apis/ng/${api.id}/general`);
     cy.getByDataTestId('api_info_promote').click();
     cy.contains('Meet Cockpit').should('be.visible');
     cy.getByDataTestId('api_info_promote_ok').click();
+  });
+
+  // Danger Zone
+
+  it('Danger Zone - Start and Stop the API (without and with published plan)', () => {
+    cy.visit(`/#!/environments/DEFAULT/apis/ng/${v4infoApi.id}/general`);
+    cy.getByDataTestId('api_info_dangerzone_start_api').click();
+    cy.getByDataTestId('confirm-dialog').click();
+    cy.contains('can not be started without at least one published plan');
+    cy.visit(`/#!/environments/DEFAULT/apis/ng/${v4dangerzoneApi.id}/general`);
+    cy.getByDataTestId('api_info_dangerzone_start_api').click();
+    cy.getByDataTestId('confirm-dialog').click();
+    cy.contains('The API has been started with success.');
+    cy.getByDataTestId('api_info_dangerzone_stop_api').click();
+    cy.getByDataTestId('confirm-dialog').click();
+    cy.contains('The API has been stopped with success.');
+  });
+
+  it('Danger Zone - Publish and Unpublish the API)', () => {
+    cy.visit(`/#!/environments/DEFAULT/apis/ng/${v4dangerzoneApi.id}/general`);
+    cy.getByDataTestId('api_info_dangerzone_publish_api').click();
+    cy.getByDataTestId('confirm-dialog').click();
+    cy.contains('The API has been publish with success.');
+    cy.getByDataTestId('api_info_dangerzone_unpublish_api').click();
+    cy.getByDataTestId('confirm-dialog').click();
+    cy.contains('The API has been unpublish with success.');
+  });
+
+  it('Danger Zone - Make Public and Make Private the API)', () => {
+    cy.visit(`/#!/environments/DEFAULT/apis/ng/${v4dangerzoneApi.id}/general`);
+    cy.getByDataTestId('api_info_dangerzone_make_public').click();
+    cy.getByDataTestId('confirm-dialog').click();
+    cy.contains('The API has been Make Public with success.');
+    cy.getByDataTestId('api_info_dangerzone_make_private').click();
+    cy.getByDataTestId('confirm-dialog').click();
+    cy.contains('The API has been Make Private with success.');
+  });
+
+  it('Danger Zone - Deprecate the API)', () => {
+    cy.visit(`/#!/environments/DEFAULT/apis/ng/${v4dangerzoneApi.id}/general`);
+    cy.getByDataTestId('api_info_dangerzone_deprecate_api').click();
+    cy.getByDataTestId('confirm-dialog').click();
+    cy.contains('The API has been deprecate with success.');
+    cy.contains('This API is deprecated.');
+    cy.getByDataTestId('api_info_dangerzone_publish_api').should('not.exist');
+    cy.getByDataTestId('api_info_dangerzone_unpublish_api').should('not.exist');
+    cy.getByDataTestId('api_info_dangerzone_make_private').should('not.exist');
+    cy.getByDataTestId('api_info_dangerzone_make_public').should('not.exist');
+    cy.getByDataTestId('api_info_dangerzone_deprecate_api').should('not.exist');
+  });
+
+  it('Danger Zone - Delete the API)', () => {
+    cy.visit(`/#!/environments/DEFAULT/apis/ng/${v4dangerzoneApi.id}/general`);
+    cy.getByDataTestId('api_dangerzone_delete_api').click();
+    cy.getByDataTestId('confirm-input-dialog').type(`${v4dangerzoneApi.name}`);
+    cy.getByDataTestId('confirm-dialog').click();
+    cy.contains('The API has been deleted.');
+    cy.contains(`${v4dangerzoneApi.name}`).should('not.exist');
   });
 
   after(() => {
     cy.clearCookie('Auth-Graviteeio-APIM');
     cy.log('Clean up APIs');
     cy.teardownApi(api);
-    cy.teardownApi(duplicateApi);
+    cy.teardownV4Api(v4infoApi.id);
+    cy.teardownV4Api(duplicateApi.id);
   });
 });
