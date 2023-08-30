a824e11ed4ac923055553e2a3f2dabe565446eb4
Julien Giovaresco <dev@giovaresco.fr>
2023-08-30 14:24:26 +0200

    refactor: extract ApiRuntimeLogsEmptyComponent

---
+++ gravitee-apim-console-webui/src/management/api/runtime-logs-v4/runtime-logs/components/api-runtime-logs-empty/api-runtime-logs-empty.component.ts
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'api-runtime-logs-empty',
+  template: require('./api-runtime-logs-empty.component.html'),
+  styles: [require('./api-runtime-logs-empty.component.scss')],
+})
+export class ApiRuntimeLogsEmptyComponent {
+  @Output()
+  navigateToSettings = new EventEmitter<void>();
+}
