036aa8af5d784a73d5dd1bb799a943424218cca8
Ouahid Khelifi <ouahid.khelifi@gmail.com>
2023-08-31 08:17:46 +0200

    feat: add dedicated component for v4 proxy APIs logs settings

---
+++ gravitee-apim-console-webui/src/management/api/runtime-logs-v4/runtime-logs-settings/runtime-logs-proxy-settings/api-runtime-logs-proxy-settings.module.ts
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+
+import { ApiRuntimeLogsProxySettingsComponent } from './api-runtime-logs-proxy-settings.component';
+
+@NgModule({
+  declarations: [ApiRuntimeLogsProxySettingsComponent],
+  exports: [ApiRuntimeLogsProxySettingsComponent],
+  imports: [CommonModule, MatButtonModule, MatCardModule, MatIconModule],
+})
+export class ApiRuntimeLogsProxySettingsModule {}
