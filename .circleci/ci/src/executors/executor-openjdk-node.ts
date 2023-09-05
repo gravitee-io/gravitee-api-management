dc1727a5534588fdffd168c7557d1ae50d9c3a57
Ouahid Khelifi <ouahid.khelifi@gmail.com>
2023-09-05 15:31:26 +0200

    refactor(ci): avoid to reuse executors [skip ci]

--- .circleci/ci/src/executors/executor-openjdk-node.ts
+++ .circleci/ci/src/executors/executor-openjdk-node.ts
@@ -1,4 +1,4 @@
-import { executors, reusable } from '@circleci/circleci-config-sdk';
+import { executors } from '@circleci/circleci-config-sdk';
 import { config } from '../config';
 import { Executor } from '@circleci/circleci-config-sdk/dist/src/lib/Components/Executors';
 import { DockerResourceClass } from '@circleci/circleci-config-sdk/dist/src/lib/Components/Executors/types/DockerExecutor.types';
@@ -7,9 +7,5 @@ export class OpenJdkNodeExecutor {
   public static create(resource: DockerResourceClass = config.executor.openjdk.resource): Executor {
     const image = `${config.executor.openjdk.image}:${config.executor.openjdk.version}-node`;
     return new executors.DockerExecutor(image, resource);
-  }
-
-  public static get(resource: DockerResourceClass = config.executor.openjdk.resource): reusable.ReusableExecutor {
-    return new reusable.ReusableExecutor('openjdk-node', OpenJdkNodeExecutor.create(resource));
   }
 }
