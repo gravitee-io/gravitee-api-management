92ba83e6e6befe17747c23ecdc29867d0ec79dc5
ThibaudAv <thibaud.avenier@gmail.com>
2023-09-06 14:51:10 +0200

    feat(console): add API Lifecycle State widgets to home overview

---
+++ gravitee-apim-console-webui/src/management/home/widgets/gio-api-lifecycle-state/gio-api-lifecycle-state.component.ts
@@ -0,0 +1,80 @@
+/*
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { isEmpty } from 'lodash';
+
+import { GioChartPieInput } from '../../../../shared/components/widgets/gio-chart-pie/gio-chart-pie.component';
+
+export type ApiLifecycleStateData = {
+  values: { [key: string]: number };
+};
+
+const LIFECYCLE_STATE_DISPLAYABLE = {
+  CREATED: {
+    label: 'Created',
+    color: '#6978ff',
+  },
+  DEPRECATED: {
+    label: 'Deprecated',
+    color: '#bf3f0e',
+  },
+  PUBLISHED: {
+    label: 'Published',
+    color: '#02c37f',
+  },
+  UNPUBLISHED: {
+    label: 'Unpublished',
+    color: '#d3d5dc',
+  },
+};
+
+@Component({
+  selector: 'gio-api-lifecycle-state',
+  template: require('./gio-api-lifecycle-state.component.html'),
+})
+export class GioApiLifecycleStateComponent implements OnChanges {
+  @Input()
+  data: ApiLifecycleStateData;
+
+  chartPieInput: GioChartPieInput[];
+  isEmpty = true;
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.data) {
+      this.buildDataSource();
+    }
+  }
+
+  private buildDataSource() {
+    this.chartPieInput = Object.entries(this.data.values)
+      .map(([key, value]) => {
+        const status = LIFECYCLE_STATE_DISPLAYABLE[key];
+        if (!status) {
+          // Best effort: Ignore unknown key
+          return null;
+        }
+
+        return {
+          label: `${status.label}`,
+          color: `${status.color}`,
+          value,
+        };
+      })
+      .filter((v) => v != null);
+
+    this.isEmpty = isEmpty(this.chartPieInput) || !this.chartPieInput.some((v) => v.value > 0);
+  }
+}
---
+++ gravitee-apim-console-webui/src/management/home/widgets/gio-api-lifecycle-state/gio-api-lifecycle-state.stories.ts
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { Meta, moduleMetadata, componentWrapperDecorator } from '@storybook/angular';
+import { Story } from '@storybook/angular/dist/ts3.9/client/preview/types-7-0';
+import { action } from '@storybook/addon-actions';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { GioApiLifecycleStateComponent } from './gio-api-lifecycle-state.component';
+import { GioApiLifecycleStateModule } from './gio-api-lifecycle-state.module';
+
+import { UIRouterState } from '../../../../ajs-upgraded-providers';
+
+export default {
+  title: 'Home / Widgets / API Lifecycle State',
+  component: GioApiLifecycleStateComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [GioApiLifecycleStateModule, BrowserAnimationsModule],
+      providers: [{ provide: UIRouterState, useValue: { go: (...args) => action('Ajs state go')(args) } }],
+    }),
+    componentWrapperDecorator((story) => `<div style="height:400px;width: 400px">${story}</div>`),
+  ],
+  render: ({ data }) => ({
+    props: { data },
+    template: `<gio-api-lifecycle-state [data]="data"></gio-api-lifecycle-state>`,
+  }),
+} as Meta;
+
+export const Default: Story = {};
+Default.args = {
+  data: {
+    values: {
+      CREATED: 83,
+      PUBLISHED: 24,
+      UNPUBLISHED: 2,
+      DEPRECATED: 2,
+    },
+  },
+};
