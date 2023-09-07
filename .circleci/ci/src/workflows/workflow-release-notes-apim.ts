fb10ae9ff9e672a9fc5d0906659e823fe8e5e6db
jourdiw <jourdi.waller@graviteesource.com>
2023-09-07 17:22:06 +0200

    fix(ci): add full info to release notes apim pipeline [skip ci]

--- .circleci/ci/src/workflows/workflow-release-notes-apim.ts
+++ .circleci/ci/src/workflows/workflow-release-notes-apim.ts
@@ -1,12 +1,18 @@
-import { Config, Workflow } from '@circleci/circleci-config-sdk';
+import { Config, workflow, Workflow } from '@circleci/circleci-config-sdk';
 import { CircleCIEnvironment } from '../pipelines';
 import { ReleaseNotesApimJob } from '../jobs';
+import { config } from '../config';
 
 export class ReleaseNotesApimWorkflow {
   static create(dynamicConfig: Config, environment: CircleCIEnvironment) {
     const releaseNotesApimJob = ReleaseNotesApimJob.create(dynamicConfig, environment);
     dynamicConfig.addJob(releaseNotesApimJob);
 
-    return new Workflow('release-notes-apim', [releaseNotesApimJob]);
+    return new Workflow('release-notes-apim', [
+      new workflow.WorkflowJob(releaseNotesApimJob, {
+        context: config.jobContext,
+        name: 'Generate release note and create PR',
+      }),
+    ]);
   }
 }
