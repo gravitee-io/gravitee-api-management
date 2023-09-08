0d405ee2afaa01e7df5803967493098fe7a0999f
Yann Tavernier <yann.tavernier@graviteesource.com>
2023-09-08 16:08:28 +0200

    test(arch): add adapter package to persistence's infra layer https://gravitee.atlassian.net/browse/APIM-2637

--- gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/test/java/architecture/AbstractApimArchitectureTest.java
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/test/java/architecture/AbstractApimArchitectureTest.java
@@ -39,6 +39,7 @@ public class AbstractApimArchitectureTest {
     public final String USECASE_PACKAGE = "usecase";
     public final String CRUD_SERVICE_PACKAGE = "crud_service";
     public final String DOMAIN_SERVICE_PACKAGE = "domain_service";
+    public final String ADAPTER_PACKAGE = "adapter";
     public final String MODEL_PACKAGE = "model";
 
     public JavaClasses apimClassesWithoutTests() {
