a2888a56046f6c6a022677b50bb62ba9fa19e6a3
“Jan <jan.sobczynski@mhrglobal.com>
2023-09-12 14:44:32 +0200

    feat(console): creating notification service, fetch data into a table

--- gravitee-apim-console-webui/src/management/api/notifications/notifications-list/notifications-list.component.ts
+++ gravitee-apim-console-webui/src/management/api/notifications/notifications-list/notifications-list.component.ts
@@ -13,11 +13,73 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { Component } from '@angular/core';
+
+import { Component, Inject, OnInit } from '@angular/core';
+import { takeUntil, tap } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+
+import { NotificationSettingsService } from '../../../../services-ngx/notification-settings.service';
+import { UIRouterStateParams } from '../../../../ajs-upgraded-providers';
+import { GioTableWrapperFilters } from '../../../../shared/components/gio-table-wrapper/gio-table-wrapper.component';
+import { gioTableFilterCollection } from '../../../../shared/components/gio-table-wrapper/gio-table-wrapper.util';
+
+type NotificationSettingsTable = {
+  name: string;
+  id: string;
+  configType: string;
+};
 
 @Component({
   selector: 'notifications-list',
   template: require('./notifications-list.component.html'),
   styles: [require('./notifications-list.component.scss')],
 })
-export class NotificationsListComponent {}
+export class NotificationsListComponent implements OnInit {
+  public notificationsSettingsTable: NotificationSettingsTable[] = [];
+  public isLoadingData = true;
+  public displayedColumns = ['name'];
+  public notificationUnpaginatedLength = 0;
+  public filteredNotificationsSettingsTable = [];
+
+  private unsubscribe$: Subject<boolean> = new Subject<boolean>();
+
+  constructor(
+    @Inject(UIRouterStateParams) private readonly ajsStateParams,
+    private readonly notificationSettingsService: NotificationSettingsService,
+  ) {}
+
+  public ngOnInit() {
+    this.isLoadingData = true;
+    this.filteredNotificationsSettingsTable = [];
+    this.notificationSettingsService
+      .getNotificationSettings(this.ajsStateParams.apiId)
+      .pipe(
+        tap((settings) => {
+          this.notificationsSettingsTable = settings.map((notificationSettings) => {
+            return {
+              id: notificationSettings.id,
+              configType: notificationSettings.config_type,
+              name: notificationSettings.name,
+            };
+          });
+          this.filteredNotificationsSettingsTable = this.notificationsSettingsTable;
+          this.notificationUnpaginatedLength = this.filteredNotificationsSettingsTable.length;
+        }),
+        takeUntil(this.unsubscribe$),
+      )
+      .subscribe(() => {
+        this.isLoadingData = false;
+      });
+  }
+
+  ngOnDestroy() {
+    this.unsubscribe$.next(true);
+    this.unsubscribe$.unsubscribe();
+  }
+
+  onPropertiesFiltersChanged(filters: GioTableWrapperFilters) {
+    const filtered = gioTableFilterCollection(this.notificationsSettingsTable, filters);
+    this.filteredNotificationsSettingsTable = filtered.filteredCollection;
+    this.notificationUnpaginatedLength = filtered.unpaginatedLength;
+  }
+}
--- gravitee-apim-console-webui/src/management/api/notifications/notifications-list/notifications-list.module.ts
+++ gravitee-apim-console-webui/src/management/api/notifications/notifications-list/notifications-list.module.ts
@@ -17,15 +17,31 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { GioLoaderModule } from '@gravitee/ui-particles-angular';
+import { MatTableModule } from '@angular/material/table';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { UIRouterModule } from '@uirouter/angular';
+import { HttpClientModule } from '@angular/common/http';
 
 import { NotificationsListComponent } from './notifications-list.component';
 
-import { GioTableWrapperModule } from '../../../../shared/components/gio-table-wrapper/gio-table-wrapper.module';
 import { GioPermissionModule } from '../../../../shared/components/gio-permission/gio-permission.module';
+import { GioTableWrapperModule } from '../../../../shared/components/gio-table-wrapper/gio-table-wrapper.module';
 
 @NgModule({
   declarations: [NotificationsListComponent],
   exports: [NotificationsListComponent],
-  imports: [CommonModule, GioTableWrapperModule, GioPermissionModule, MatButtonModule, MatIconModule],
+  imports: [
+    CommonModule,
+    GioTableWrapperModule,
+    GioPermissionModule,
+    MatButtonModule,
+    MatIconModule,
+    GioLoaderModule,
+    MatTableModule,
+    MatTooltipModule,
+    UIRouterModule,
+    HttpClientModule,
+  ],
 })
 export class NotificationsListModule {}
