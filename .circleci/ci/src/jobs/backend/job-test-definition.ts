c0997c4ed619805e41617737e2426ea3f1aae8f0
Florent CHAMFROY <florent.chamfroy@graviteesource.com>
2023-09-13 14:25:03 +0200

    test(ci): split backend tests to improve execution time
    (cherry picked from commit 0a609d53525d5af38d3ef84e96be2ceef6bd8267)

--- .circleci/ci/src/jobs/backend/job-test-definition.ts
+++ .circleci/ci/src/jobs/backend/job-test-definition.ts
@@ -15,24 +15,20 @@
  */
 import { commands, Config } from '@circleci/circleci-config-sdk';
 import { config } from '../../config';
-import { AbstractTestJob } from './abstract-job-test';
 import { OpenJdkExecutor } from '../../executors';
+import { AbstractTestJob } from './abstract-job-test';
 
-export class TestBackendJob extends AbstractTestJob {
+export class TestDefinitionJob extends AbstractTestJob {
   public static create(dynamicConfig: Config) {
     return super.create(
       dynamicConfig,
-      'job-test-backend',
+      'job-test-definition',
       new commands.Run({
-        name: `Run backend tests`,
-        command: `mvn --fail-fast -s ${config.maven.settingsFile} test --no-transfer-progress -Dmain-modules -Dskip.validation=true -T 2C`,
+        name: `Run definition tests`,
+        command: `mvn --fail-fast -s ${config.maven.settingsFile} test --no-transfer-progress -Ddefinition-modules -Dskip.validation=true -T 2C`,
       }),
-      OpenJdkExecutor.create('medium+'),
-      [
-        'gravitee-apim-gateway/gravitee-apim-gateway-coverage/target/site/jacoco-aggregate/',
-        'gravitee-apim-rest-api/gravitee-apim-rest-api-coverage/target/site/jacoco-aggregate/',
-        'gravitee-apim-definition/gravitee-apim-definition-coverage/target/site/jacoco-aggregate/',
-      ],
+      OpenJdkExecutor.create('small'),
+      ['gravitee-apim-definition/gravitee-apim-definition-coverage/target/site/jacoco-aggregate/'],
     );
   }
 }
