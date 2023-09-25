b9595bb49cdcb96617d445059ec52a6a3a6f5a82
Julien Giovaresco <dev@giovaresco.fr>
2023-09-25 16:23:51 +0200

    refactor: init SubscriptionCrudService

--- gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/apim/core/subscription/model/SubscriptionEntity.java
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/apim/core/subscription/model/SubscriptionEntity.java
@@ -15,7 +15,8 @@
  */
 package io.gravitee.apim.core.subscription.model;
 
-import java.util.Date;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import java.time.ZonedDateTime;
 import java.util.Map;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -39,7 +40,7 @@ public class SubscriptionEntity {
     /** The clientId linked to the subscription */
     private String clientId;
     /** When the subscription has been processed. */
-    private Date processedAt;
+    private ZonedDateTime processedAt;
     /** Give a request message to the api owner why a user wants to subscribe */
     private String requestMessage;
     /** Give a reason to the developer if the subscription is accepted or not. */
@@ -58,22 +59,22 @@ public class SubscriptionEntity {
     private String processedBy;
     /** The username of the user who has subscribed to the plan. */
     private String subscribedBy;
-    private Date startingAt;
-    private Date endingAt;
+    private ZonedDateTime startingAt;
+    private ZonedDateTime endingAt;
     /** Subscription creation date */
-    private Date createdAt;
+    private ZonedDateTime createdAt;
     /** Subscription last update date */
-    private Date updatedAt;
-    private Date closedAt;
-    private Date pausedAt;
-    private Date consumerPausedAt;
+    private ZonedDateTime updatedAt;
+    private ZonedDateTime closedAt;
+    private ZonedDateTime pausedAt;
+    private ZonedDateTime consumerPausedAt;
 
     private Integer generalConditionsContentRevision;
     private String generalConditionsContentPageId;
     private Boolean generalConditionsAccepted;
     /** Number of days before the expiration of this subscription when the last pre-expiration notification was sent */
     private Integer daysToExpirationOnLastNotification;
-    private String configuration;
+    private SubscriptionConfiguration configuration;
     private Map<String, String> metadata;
 
     @Builder.Default
