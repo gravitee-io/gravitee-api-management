edef86bc2f51883fb61d7ad07b6d3f9f96f6caa6
Lorie Pisicchio <lorie.pisicchio@graviteesource.com>
2023-09-25 18:00:38 +0200

    feat: create EnvironmentCrudService

---
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/apim/infra/crud_service/environment/EnvironmentCrudServiceImpl.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright © 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.apim.infra.crud_service.environment;
+
+import io.gravitee.apim.core.environment.crud_service.EnvironmentCrudService;
+import io.gravitee.apim.core.environment.model.Environment;
+import io.gravitee.apim.infra.adapter.EnvironmentAdapter;
+import io.gravitee.repository.exceptions.TechnicalException;
+import io.gravitee.repository.management.api.EnvironmentRepository;
+import io.gravitee.rest.api.service.exceptions.EnvironmentNotFoundException;
+import io.gravitee.rest.api.service.exceptions.TechnicalManagementException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class EnvironmentCrudServiceImpl implements EnvironmentCrudService {
+
+    private final EnvironmentRepository environmentRepository;
+
+    public EnvironmentCrudServiceImpl(@Lazy final EnvironmentRepository environmentRepository) {
+        this.environmentRepository = environmentRepository;
+    }
+
+    @Override
+    public Environment get(String environmentId) {
+        try {
+            log.debug("Find environment by id: {}", environmentId);
+
+            return this.environmentRepository.findById(environmentId)
+                .map(EnvironmentAdapter.INSTANCE::toModel)
+                .orElseThrow(() -> new EnvironmentNotFoundException(environmentId));
+        } catch (TechnicalException e) {
+            throw new TechnicalManagementException(e);
+        }
+    }
+}
---
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/test/java/io/gravitee/apim/infra/crud_service/environment/EnvironmentCrudServiceImplTest.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright © 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.apim.infra.crud_service.environment;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import io.gravitee.apim.core.environment.crud_service.EnvironmentCrudService;
+import io.gravitee.apim.core.environment.model.Environment;
+import io.gravitee.apim.infra.adapter.EnvironmentAdapter;
+import io.gravitee.repository.management.api.EnvironmentRepository;
+import io.gravitee.rest.api.service.exceptions.EnvironmentNotFoundException;
+import java.util.Optional;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class EnvironmentCrudServiceImplTest {
+
+    EnvironmentRepository environmentRepository;
+    EnvironmentCrudService service;
+
+    @BeforeEach
+    void setUp() {
+        environmentRepository = mock(EnvironmentRepository.class);
+        service = new EnvironmentCrudServiceImpl(environmentRepository);
+    }
+
+    @Test
+    void should_find_environment_by_id() {
+        Environment environment = anEnvironment();
+        givenEnvironment(environment);
+
+        assertThat(service.get(environment.getId())).isEqualTo(environment);
+    }
+
+    @Test
+    void should_throw_exception_if_environment_not_found() {
+        var throwable = catchThrowable(() -> service.get("unknown"));
+        assertThat(throwable).isInstanceOf(EnvironmentNotFoundException.class);
+    }
+
+    @SneakyThrows
+    private void givenEnvironment(Environment environment) {
+        when(environmentRepository.findById(environment.getId()))
+            .thenReturn(Optional.of(EnvironmentAdapter.INSTANCE.toRepository(environment)));
+    }
+
+    private Environment anEnvironment() {
+        return Environment.builder().id("environment-id").build();
+    }
+}
