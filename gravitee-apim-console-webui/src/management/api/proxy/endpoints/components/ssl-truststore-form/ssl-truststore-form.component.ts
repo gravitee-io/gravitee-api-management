b2359f28da5c302e540e169acd6a95537e12024a
ThibaudAv <thibaud.avenier@gmail.com>
2023-09-26 09:58:08 +0200

    feat(console): impl ssl-truststore-form component

---
+++ gravitee-apim-console-webui/src/management/api/proxy/endpoints/components/ssl-truststore-form/ssl-truststore-form.component.ts
@@ -0,0 +1,296 @@
+/*
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Component, Input, OnDestroy, OnInit, forwardRef } from '@angular/core';
+import {
+  AbstractControl,
+  ControlContainer,
+  ControlValueAccessor,
+  FormControl,
+  FormGroup,
+  NG_VALIDATORS,
+  NG_VALUE_ACCESSOR,
+  ValidationErrors,
+  Validator,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
+import { Subject } from 'rxjs';
+import { isEmpty } from 'lodash';
+import { filter, take, map, takeUntil } from 'rxjs/operators';
+
+import { JKSTrustStore, PEMTrustStore, PKCS12TrustStore, TrustStore } from '../../../../../../entities/management-api-v2';
+
+type InternalFormValue = {
+  type: TrustStore['type'];
+  jksPath?: string;
+  jksContent?: string;
+  jksPassword?: string;
+  pkcs12Path?: string;
+  pkcs12Content?: string;
+  pkcs12Password?: string;
+  pemPath?: string;
+  pemContent?: string;
+};
+
+export const TRUSTSTORE_TYPE_LABELS: { label: string; value: TrustStore['type'] }[] = [
+  {
+    label: 'None',
+    value: 'NONE',
+  },
+  {
+    label: 'Java Trust Store (.jks)',
+    value: 'JKS',
+  },
+  {
+    label: 'PKCS#12 (.p12) / PFX (.pfx)',
+    value: 'PKCS12',
+  },
+  {
+    label: 'PEM (.pem)',
+    value: 'PEM',
+  },
+];
+
+@Component({
+  selector: 'ssl-truststore-form',
+  template: require('./ssl-truststore-form.component.html'),
+  styles: [require('./ssl-truststore-form.component.scss')],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      multi: true,
+      useExisting: forwardRef(() => SslTrustStoreFormComponent),
+    },
+    {
+      provide: NG_VALIDATORS,
+      useExisting: forwardRef(() => SslTrustStoreFormComponent),
+      multi: true,
+    },
+  ],
+})
+export class SslTrustStoreFormComponent implements OnInit, OnDestroy, ControlValueAccessor, Validator {
+  private unsubscribe$ = new Subject<boolean>();
+
+  private _onChange: (value: TrustStore) => void = () => ({});
+
+  private _onTouched: () => void = () => ({});
+
+  @Input() formControlName!: string;
+  @Input() formGroupName!: string;
+
+  public types = TRUSTSTORE_TYPE_LABELS;
+
+  public internalFormGroup = new FormGroup({
+    // Base fields
+    type: new FormControl('NONE'),
+
+    // JKS fields
+    jksPath: new FormControl(),
+    jksContent: new FormControl(),
+    jksPassword: new FormControl(),
+
+    // PKCS12 fields
+    pkcs12Path: new FormControl(),
+    pkcs12Content: new FormControl(),
+    pkcs12Password: new FormControl(),
+
+    // PEM fields
+    pemPath: new FormControl(),
+    pemContent: new FormControl(),
+  });
+  public isDisabled = false;
+
+  constructor(private controlContainer: ControlContainer) {}
+
+  ngOnInit(): void {
+    this.internalFormGroup
+      .get('type')
+      .valueChanges.pipe(takeUntil(this.unsubscribe$))
+      .subscribe((type) => {
+        // Clear all validators
+        this.internalFormGroup.clearValidators();
+        this.internalFormGroup.get('jksPassword').clearValidators();
+        this.internalFormGroup.get('pkcs12Password').clearValidators();
+
+        switch (type) {
+          case 'JKS': {
+            this.internalFormGroup.get('jksPassword').setValidators([Validators.required]);
+            this.internalFormGroup.setValidators([pathOrContentRequired('jksPath', 'jksContent')]);
+            break;
+          }
+          case 'PKCS12': {
+            this.internalFormGroup.get('pkcs12Password').setValidators([Validators.required]);
+            this.internalFormGroup.setValidators([pathOrContentRequired('pkcs12Path', 'pkcs12Content')]);
+            break;
+          }
+          case 'PEM': {
+            this.internalFormGroup.setValidators([pathOrContentRequired('pemPath', 'pemContent')]);
+            break;
+          }
+        }
+
+        // Update validators
+        this.internalFormGroup.get('jksPassword').updateValueAndValidity();
+        this.internalFormGroup.get('pkcs12Password').updateValueAndValidity();
+        this.internalFormGroup.updateValueAndValidity();
+      });
+
+    this.internalFormGroup.valueChanges.pipe(takeUntil(this.unsubscribe$)).subscribe((value) => {
+      this._onChange(internalFormValueToTrustStore(value));
+    });
+
+    this.internalFormGroup.statusChanges
+      .pipe(
+        map(() => this.internalFormGroup?.touched),
+        filter((touched) => touched === true && this.isDisabled === false),
+        takeUntil(this.unsubscribe$),
+      )
+      .subscribe(() => {
+        this._onTouched();
+      });
+
+    // If parent form control is touched, mark all internal fields as touched
+    const parentFormControl = this.controlContainer?.control?.get(this.formControlName);
+    if (parentFormControl) {
+      parentFormControl.statusChanges
+        .pipe(
+          map(() => parentFormControl?.touched),
+          filter((touched) => touched === true && this.isDisabled === false),
+          take(1),
+          takeUntil(this.unsubscribe$),
+        )
+        .subscribe(() => {
+          this.internalFormGroup.markAllAsTouched();
+        });
+    }
+  }
+
+  ngOnDestroy() {
+    this.unsubscribe$.next(true);
+    this.unsubscribe$.unsubscribe();
+  }
+
+  registerOnChange(fn: (value: TrustStore) => void): void {
+    this._onChange = fn;
+  }
+
+  registerOnTouched(fn: () => void): void {
+    this._onTouched = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    this.isDisabled = isDisabled;
+    isDisabled ? this.internalFormGroup.disable() : this.internalFormGroup.enable();
+  }
+
+  validate(_: AbstractControl): ValidationErrors | null {
+    return this.internalFormGroup.valid
+      ? null
+      : {
+          invalidTrustStore: true,
+        };
+  }
+
+  writeValue(value: TrustStore): void {
+    this.internalFormGroup.reset(trustStoreTypeToInternalFormValue(value));
+  }
+}
+
+const pathOrContentRequired: (pathControlName: string, contentControlName: string) => ValidatorFn = (
+  pathControlName: string,
+  contentControlName: string,
+) => {
+  return (formGroup: FormGroup): ValidationErrors | null => {
+    const pathControl = formGroup.get(pathControlName);
+    const contentControl = formGroup.get(contentControlName);
+
+    if (isEmpty(pathControl.value) && isEmpty(contentControl.value)) {
+      pathControl.setErrors({ pathOrContentRequired: true }, { emitEvent: false });
+      contentControl.setErrors({ pathOrContentRequired: true }, { emitEvent: false });
+      return { pathOrContentRequired: true };
+    }
+
+    pathControl.setErrors(null, { emitEvent: false });
+    contentControl.setErrors(null, { emitEvent: false });
+    return null;
+  };
+};
+
+const trustStoreTypeToInternalFormValue: (trustStore?: TrustStore) => InternalFormValue = (trustStore) => {
+  switch (trustStore?.type) {
+    case 'JKS': {
+      const jksTrustStore = trustStore as JKSTrustStore;
+      return {
+        type: trustStore.type,
+        jksPath: jksTrustStore.path,
+        jksContent: jksTrustStore.content,
+        jksPassword: jksTrustStore.password,
+      };
+    }
+    case 'PKCS12': {
+      const pkcs12TrustStore = trustStore as PKCS12TrustStore;
+      return {
+        type: trustStore.type,
+        pkcs12Path: pkcs12TrustStore.path,
+        pkcs12Content: pkcs12TrustStore.content,
+        pkcs12Password: pkcs12TrustStore.password,
+      };
+    }
+    case 'PEM': {
+      const pemTrustStore = trustStore as PEMTrustStore;
+      return {
+        type: trustStore.type,
+        pemPath: pemTrustStore.path,
+        pemContent: pemTrustStore.content,
+      };
+    }
+    default: {
+      return {
+        type: 'NONE',
+      };
+    }
+  }
+};
+
+const internalFormValueToTrustStore: (internalFormValue: InternalFormValue) => TrustStore = (internalFormValue) => {
+  switch (internalFormValue.type) {
+    case 'JKS':
+      return {
+        type: internalFormValue.type,
+        path: internalFormValue.jksPath,
+        content: internalFormValue.jksContent,
+        password: internalFormValue.jksPassword,
+      };
+    case 'PKCS12':
+      return {
+        type: internalFormValue.type,
+        path: internalFormValue.pkcs12Path,
+        content: internalFormValue.pkcs12Content,
+        password: internalFormValue.pkcs12Password,
+      };
+    case 'PEM':
+      return {
+        type: internalFormValue.type,
+        path: internalFormValue.pemPath,
+        content: internalFormValue.pemContent,
+      };
+    default:
+      return {
+        type: internalFormValue.type,
+      };
+  }
+};
---
+++ gravitee-apim-console-webui/src/management/api/proxy/endpoints/components/ssl-truststore-form/ssl-truststore-form.harness.ts
@@ -0,0 +1,106 @@
+/*
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { ComponentHarness } from '@angular/cdk/testing';
+import { MatInputHarness } from '@angular/material/input/testing';
+import { MatSelectHarness } from '@angular/material/select/testing';
+import { keyBy } from 'lodash';
+
+import { TRUSTSTORE_TYPE_LABELS } from './ssl-truststore-form.component';
+
+import { JKSTrustStore, TrustStore } from '../../../../../../entities/management-api-v2';
+
+const TRUSTSTORE_TYPE_LABELS_TO_KEY = keyBy(TRUSTSTORE_TYPE_LABELS, 'value');
+
+export class SslTrustStoreFormHarness extends ComponentHarness {
+  static hostSelector = 'ssl-truststore-form';
+
+  protected getInputHarness = (formControlName: string) =>
+    this.locatorFor(MatInputHarness.with({ selector: `[formControlName="${formControlName}"]` }))();
+
+  protected getType = () => this.locatorFor(MatSelectHarness.with({ selector: `[formControlName="type"]` }))();
+
+  async setJksTrustStore(jks: JKSTrustStore) {
+    const type = await this.getType();
+    await type.clickOptions({ text: TRUSTSTORE_TYPE_LABELS_TO_KEY['JKS'].label });
+
+    const passwordInput = await this.getInputHarness('jksPassword');
+    await passwordInput.setValue(jks.password);
+
+    if (jks.path) {
+      const pathInput = await this.getInputHarness('jksPath');
+      await pathInput.setValue(jks.path);
+    }
+
+    if (jks.content) {
+      const contentInput = await this.getInputHarness('jksContent');
+      await contentInput.setValue(jks.content);
+    }
+  }
+
+  async getValues(): Promise<TrustStore> {
+    const type = await this.getType();
+    const typeValue = await type.getValueText();
+
+    switch (typeValue) {
+      case TRUSTSTORE_TYPE_LABELS_TO_KEY['JKS'].label: {
+        const passwordInput = await this.getInputHarness('jksPassword');
+        const password = await passwordInput.getValue();
+        const pathInput = await this.getInputHarness('jksPath');
+        const path = await pathInput.getValue();
+        const contentInput = await this.getInputHarness('jksContent');
+        const content = await contentInput.getValue();
+        return {
+          type: TRUSTSTORE_TYPE_LABELS_TO_KEY['JKS'].value,
+          password,
+          path,
+          content,
+        };
+      }
+      case TRUSTSTORE_TYPE_LABELS_TO_KEY['PEM'].label: {
+        const pemPathInput = await this.getInputHarness('pemPath');
+        const pemPath = await pemPathInput.getValue();
+        const pemContentInput = await this.getInputHarness('pemContent');
+        const pemContent = await pemContentInput.getValue();
+        return {
+          type: TRUSTSTORE_TYPE_LABELS_TO_KEY['PEM'].value,
+          path: pemPath,
+          content: pemContent,
+        };
+      }
+      case TRUSTSTORE_TYPE_LABELS_TO_KEY['PKCS12'].label: {
+        const pkcs12PasswordInput = await this.getInputHarness('pkcs12Password');
+        const pkcs12Password = await pkcs12PasswordInput.getValue();
+        const pkcs12PathInput = await this.getInputHarness('pkcs12Path');
+        const pkcs12Path = await pkcs12PathInput.getValue();
+        const pkcs12ContentInput = await this.getInputHarness('pkcs12Content');
+        const pkcs12Content = await pkcs12ContentInput.getValue();
+        return {
+          type: TRUSTSTORE_TYPE_LABELS_TO_KEY['PKCS12'].value,
+          password: pkcs12Password,
+          path: pkcs12Path,
+          content: pkcs12Content,
+        };
+      }
+      case TRUSTSTORE_TYPE_LABELS_TO_KEY['NONE'].label: {
+        return {
+          type: TRUSTSTORE_TYPE_LABELS_TO_KEY['NONE'].value,
+        };
+      }
+      default:
+        throw new Error(`Unknown type ${typeValue}`);
+    }
+  }
+}
