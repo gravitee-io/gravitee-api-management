1d8354dee589ac2c4e8fe84159bfe42a54b665c6
Florent CHAMFROY <florent.chamfroy@graviteesource.com>
2023-10-06 00:19:41 +0200

    ci: remove elastic and redis tests

--- .circleci/ci/src/jobs/test-container/index.ts
+++ .circleci/ci/src/jobs/test-container/index.ts
@@ -13,7 +13,5 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-export * from './job-elastic-test-container';
 export * from './job-jdbc-test-container';
 export * from './job-mongo-test-container';
-export * from './job-redis-test-container';
