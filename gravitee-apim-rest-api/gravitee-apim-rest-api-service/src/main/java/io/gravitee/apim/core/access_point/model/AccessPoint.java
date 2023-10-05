e379b0be7874988b1189fdd4515fd586b1bb062b
Julien Giovaresco <dev@giovaresco.fr>
2023-10-05 17:20:05 +0200

    refactor: implement AccessPointCrudService

---
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/apim/core/access_point/model/AccessPoint.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright © 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.apim.core.access_point.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author Guillaume LAMIRAND (guillaume.lamirand at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+@Data
+public class AccessPoint {
+
+    private String id;
+    private AccessPointReferenceType referenceType;
+    private String referenceId;
+    private AccessPointTarget target;
+    private String host;
+    private boolean secured;
+    private boolean overriding;
+
+    public enum AccessPointReferenceType {
+        ENVIRONMENT,
+        ORGANIZATION,
+    }
+
+    public enum AccessPointTarget {
+        CONSOLE,
+        CONSOLE_API,
+        PORTAL,
+        PORTAL_API,
+        GATEWAY,
+    }
+}
---
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/test/java/io/gravitee/apim/infra/crud_service/access_point/AccessPointCrudServiceImplTest.java
@@ -0,0 +1,162 @@
+/*
+ * Copyright © 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.apim.infra.crud_service.access_point;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import io.gravitee.apim.core.access_point.model.AccessPoint;
+import io.gravitee.apim.infra.adapter.AccessPointAdapter;
+import io.gravitee.repository.management.api.AccessPointRepository;
+import io.gravitee.repository.management.model.AccessPointTarget;
+import io.gravitee.rest.api.service.common.UuidString;
+import java.util.Arrays;
+import java.util.List;
+import lombok.SneakyThrows;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.mockito.ArgumentCaptor;
+
+class AccessPointCrudServiceImplTest {
+
+    AccessPointRepository accessPointRepository;
+    AccessPointCrudServiceImpl service;
+
+    @BeforeAll
+    static void beforeAll() {
+        UuidString.overrideGenerator(() -> "random-id");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        UuidString.reset();
+    }
+
+    @BeforeEach
+    void setUp() {
+        accessPointRepository = mock(AccessPointRepository.class);
+        service = new AccessPointCrudServiceImpl(accessPointRepository);
+    }
+
+    @Nested
+    class UpdateAccessPoints {
+
+        @ParameterizedTest
+        @EnumSource(AccessPoint.AccessPointReferenceType.class)
+        @SneakyThrows
+        void should_delete_existing_access_points_of_the_reference(AccessPoint.AccessPointReferenceType referenceType) {
+            // Given
+            var accessPoints = List.<AccessPoint>of();
+
+            // When
+            service.updateAccessPoints(referenceType, "ref-id", accessPoints);
+
+            // Then
+            verify(accessPointRepository).deleteByReference(AccessPointAdapter.INSTANCE.fromEntity(referenceType), "ref-id");
+        }
+
+        @ParameterizedTest
+        @EnumSource(AccessPoint.AccessPointReferenceType.class)
+        @SneakyThrows
+        void should_create_all_access_points_provided(AccessPoint.AccessPointReferenceType referenceType) {
+            // Given
+
+            var accessPoints = Arrays
+                .stream(AccessPoint.AccessPointTarget.values())
+                .map(target ->
+                    AccessPoint
+                        .builder()
+                        .referenceType(referenceType)
+                        .referenceId("my-ref")
+                        .host("my-host")
+                        .secured(true)
+                        .overriding(true)
+                        .target(target)
+                        .build()
+                )
+                .toList();
+
+            // When
+            service.updateAccessPoints(referenceType, "ref-id", accessPoints);
+
+            // Then
+            var captor = ArgumentCaptor.forClass(io.gravitee.repository.management.model.AccessPoint.class);
+            verify(accessPointRepository, times(accessPoints.size())).create(captor.capture());
+
+            Assertions
+                .assertThat(captor.getAllValues())
+                .hasSameSizeAs(accessPoints)
+                .containsOnly(
+                    io.gravitee.repository.management.model.AccessPoint
+                        .builder()
+                        .id("random-id")
+                        .referenceType(AccessPointAdapter.INSTANCE.fromEntity(referenceType))
+                        .referenceId("my-ref")
+                        .host("my-host")
+                        .secured(true)
+                        .overriding(true)
+                        .target(AccessPointTarget.GATEWAY)
+                        .build(),
+                    io.gravitee.repository.management.model.AccessPoint
+                        .builder()
+                        .id("random-id")
+                        .referenceType(AccessPointAdapter.INSTANCE.fromEntity(referenceType))
+                        .referenceId("my-ref")
+                        .host("my-host")
+                        .secured(true)
+                        .overriding(true)
+                        .target(AccessPointTarget.CONSOLE_API)
+                        .build(),
+                    io.gravitee.repository.management.model.AccessPoint
+                        .builder()
+                        .id("random-id")
+                        .referenceType(AccessPointAdapter.INSTANCE.fromEntity(referenceType))
+                        .referenceId("my-ref")
+                        .host("my-host")
+                        .secured(true)
+                        .overriding(true)
+                        .target(AccessPointTarget.PORTAL_API)
+                        .build(),
+                    io.gravitee.repository.management.model.AccessPoint
+                        .builder()
+                        .id("random-id")
+                        .referenceType(AccessPointAdapter.INSTANCE.fromEntity(referenceType))
+                        .referenceId("my-ref")
+                        .host("my-host")
+                        .secured(true)
+                        .overriding(true)
+                        .target(AccessPointTarget.CONSOLE)
+                        .build(),
+                    io.gravitee.repository.management.model.AccessPoint
+                        .builder()
+                        .id("random-id")
+                        .referenceType(AccessPointAdapter.INSTANCE.fromEntity(referenceType))
+                        .referenceId("my-ref")
+                        .host("my-host")
+                        .secured(true)
+                        .overriding(true)
+                        .target(AccessPointTarget.PORTAL)
+                        .build()
+                );
+        }
+    }
+}
