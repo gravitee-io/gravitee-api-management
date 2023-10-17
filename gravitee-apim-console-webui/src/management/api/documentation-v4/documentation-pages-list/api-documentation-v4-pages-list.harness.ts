518b132017f50bb4ef9d50a07beb977c527a6356
jourdiw <jourdi.waller@graviteesource.com>
2023-10-17 18:45:39 +0200

    feat(console): add list of pages to documentation

---
+++ gravitee-apim-console-webui/src/management/api/documentation-v4/documentation-pages-list/api-documentation-v4-pages-list.harness.ts
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { ComponentHarness } from '@angular/cdk/testing';
+import { MatTableHarness } from '@angular/material/table/testing';
+import { MatButtonHarness } from '@angular/material/button/testing';
+
+export class ApiDocumentationV4PagesListHarness extends ComponentHarness {
+  static hostSelector = 'api-documentation-v4-pages-list';
+
+  protected tableLocator = this.locatorFor(MatTableHarness);
+  protected addNewPageButtonLocator = this.locatorFor(MatButtonHarness.with({ text: 'Add new page' }));
+
+  public getNameByRowIndex(idx: number): Promise<string> {
+    return this.getColumnTextByIndex('name', idx);
+  }
+
+  public getVisibilityByRowIndex(idx: number): Promise<string> {
+    return this.getColumnTextByIndex('visibility', idx);
+  }
+
+  public async clickAddNewPage() {
+    return this.addNewPageButtonLocator().then((btn) => btn.click());
+  }
+
+  private async getColumnTextByIndex(columnName: string, idx: number): Promise<string> {
+    const table = await this.tableLocator();
+    const rows = await table.getRows();
+    const cells = await rows[idx].getCellTextByIndex({ columnName });
+    return cells[0];
+  }
+}
