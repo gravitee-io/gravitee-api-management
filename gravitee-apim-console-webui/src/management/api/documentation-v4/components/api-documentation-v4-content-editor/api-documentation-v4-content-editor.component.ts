56dd03a0ea2c1f15fb9851f729fd41b025cef5d1
Lorie Pisicchio <lorie.pisicchio@graviteesource.com>
2023-10-23 12:07:35 +0200

    refactor: move documentation content edition to a component

---
+++ gravitee-apim-console-webui/src/management/api/documentation-v4/components/api-documentation-v4-content-editor/api-documentation-v4-content-editor.component.ts
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { Component, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+@Component({
+  selector: 'api-documentation-content',
+  template: require('./api-documentation-v4-content-editor.component.html'),
+  styles: [require('./api-documentation-v4-content-editor.component.scss')],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => ApiDocumentationV4ContentEditorComponent),
+      multi: true,
+    },
+  ],
+})
+export class ApiDocumentationV4ContentEditorComponent implements ControlValueAccessor {
+  preview = true;
+  _value: string;
+  protected _onChange: (_selection: string) => void = () => ({});
+
+  protected _onTouched: () => void = () => ({});
+
+  // From ControlValueAccessor interface
+  public registerOnChange(fn: (value: string) => void): void {
+    this._onChange = fn;
+  }
+
+  // From ControlValueAccessor interface
+  public registerOnTouched(fn: () => void): void {
+    this._onTouched = fn;
+  }
+
+  writeValue(content: string): void {
+    this._value = content;
+  }
+}
