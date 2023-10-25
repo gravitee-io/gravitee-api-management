10c943847db9d1712663afd7092876710da15b45
Gaëtan Maisse <gaetanmaisse@gmail.com>
2023-10-25 10:10:58 +0200

    refactor: rework Application pages header and remove unneeded read-more lib
    (cherry picked from commit 9fed953879b78de908f6988eb4f460887623cb73)
    
    # Conflicts:
    #	gravitee-apim-console-webui/package-lock.json
    #	gravitee-apim-console-webui/package.json

--- gravitee-apim-console-webui/src/management/management.module.ajs.ts
+++ gravitee-apim-console-webui/src/management/management.module.ajs.ts
@@ -400,7 +400,6 @@ require('../libraries/angular-ui-codemirror/ui-codemirror');
 
 require('ngclipboard');
 require('angular-ui-validate');
-require('read-more/js/directives/readmore.js');
 require('angular-timeline');
 require('angular-utf8-base64');
 require('ng-file-upload');
@@ -474,7 +473,6 @@ angular.module('gravitee-management', [
   'md.data.table',
   'ngCookies',
   'dragularModule',
-  'readMore',
   'ngMessages',
   'vAccordion',
   'schemaForm',
