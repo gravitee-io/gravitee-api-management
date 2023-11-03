e35d46f5517c4f0dceee27b93ff80fee5ed8287b
ytvnr <yann.tavernier@graviteesource.com>
2023-11-03 16:14:14 +0100

    test: move auth tests

--- gravitee-apim-e2e/api-test/src/authentication/mapi-v1/basic-authentication.spec.ts
+++ gravitee-apim-e2e/api-test/src/authentication/mapi-v1/basic-authentication.spec.ts
@@ -13,8 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { APIsApi } from '@gravitee/management-webclient-sdk/src/lib/apis/APIsApi';
-import { ApiApi } from '@gravitee/portal-webclient-sdk/src/lib/apis/ApiApi';
+import { APIsApi } from '../../../../lib/management-webclient-sdk/src/lib/apis/APIsApi';
+import { ApiApi } from '../../../../lib/portal-webclient-sdk/src/lib/apis/ApiApi';
 import {
   forManagementAsApiUser,
   forManagementWithWrongPassword,
