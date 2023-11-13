5540b6f72e79c82a2511783a35250caced58241e
Florent CHAMFROY <florent.chamfroy@graviteesource.com>
2023-11-13 08:14:01 +0100

    chore: update portal SDK
    (cherry picked from commit 32e713a7679b6b1a94541414ad32da582d254cf7)

--- gravitee-apim-portal-webui/projects/portal-webclient-sdk/src/lib/model/apiKeyModeEnum.ts
+++ gravitee-apim-portal-webui/projects/portal-webclient-sdk/src/lib/model/apiKeyModeEnum.ts
@@ -9,13 +9,15 @@
  * Do not edit the class manually.
  */
 
+
 /**
- * The API Key mode to use for this application.   - The `SHARED` API Key mode allows consumer to use the same API Key across all the subscriptions   - The `EXCLUSIVE` API Key mode will result to a new API Key being generated for each subscription   - The `UNSPECIFIED` API Key mode is a marker value informing that no choice as been made yet regarding     the API Key mode to use for the application.
+ * The API Key mode to use for this application.   - The `SHARED` API Key mode allows consumer to use the same API Key across all the subscriptions   - The `EXCLUSIVE` API Key mode will result to a new API Key being generated for each subscription   - The `UNSPECIFIED` API Key mode is a marker value informing that no choice as been made yet regarding     the API Key mode to use for the application. 
  */
 export type ApiKeyModeEnum = 'SHARED' | 'EXCLUSIVE' | 'UNSPECIFIED';
 
 export const ApiKeyModeEnum = {
-  SHARED: 'SHARED' as ApiKeyModeEnum,
-  EXCLUSIVE: 'EXCLUSIVE' as ApiKeyModeEnum,
-  UNSPECIFIED: 'UNSPECIFIED' as ApiKeyModeEnum,
+    SHARED: 'SHARED' as ApiKeyModeEnum,
+    EXCLUSIVE: 'EXCLUSIVE' as ApiKeyModeEnum,
+    UNSPECIFIED: 'UNSPECIFIED' as ApiKeyModeEnum
 };
+
