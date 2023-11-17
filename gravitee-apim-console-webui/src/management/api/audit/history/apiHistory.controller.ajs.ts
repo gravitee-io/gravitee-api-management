1f286589878ce231e173817c9eed7f2b329c69c2
thibaudAv <thibaud.avenier@gmail.com>
2023-11-17 17:21:43 +0100

    fix(console): make history screen rework

--- gravitee-apim-console-webui/src/management/api/audit/history/apiHistory.controller.ajs.ts
+++ gravitee-apim-console-webui/src/management/api/audit/history/apiHistory.controller.ajs.ts
@@ -192,7 +192,6 @@ class ApiHistoryControllerAjs {
 
     this.ApiService.get(this.$state.params.apiId).then((api) => {
       this.api = api.data;
-      this.$scope.$parent.apiCtrl.checkAPISynchronization(this.api);
 
       this.eventPage = -1;
       this.events = [];
