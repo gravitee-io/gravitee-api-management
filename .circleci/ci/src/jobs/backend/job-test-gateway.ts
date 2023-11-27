af45cdc86a574d8752e672ed0ef867d84b80e23e
Florent CHAMFROY <florent.chamfroy@graviteesource.com>
2023-11-27 14:09:32 +0100

    ci: remove parallelisation for gateway and rest-api tests
    https://gravitee.atlassian.net/browse/APIM-3387
    (cherry picked from commit 2f6f62b81c20eefa9b869df799832bd238925422)

--- .circleci/ci/src/jobs/backend/job-test-gateway.ts
+++ .circleci/ci/src/jobs/backend/job-test-gateway.ts
@@ -24,32 +24,11 @@ export class TestGatewayJob extends AbstractTestJob {
       dynamicConfig,
       'job-test-gateway',
       new commands.Run({
-        name: 'Run tests',
-        command: `cd gravitee-apim-gateway
-# List all tests
-circleci tests glob "**/src/test/java/**/*Test.java" | sed -e 's#^.*/src/test/java/\\(.*\\)\\.java#\\1#' | tr "/" "." > all-tests
-
-# List all tests to run on this executor
-cat all-tests | circleci tests split --split-by=timings --timings-type=classname --time-default=10s > tests-to-run
-
-# Compute exclusion list (use grep to invert the include list to an exclude list)
-cat all-tests | grep -xvf tests-to-run > /tmp/ignore_list
-
-# Add * add the end of each line of ignore_list to also exclude all inner classes
-sed -i 's/$/*/' /tmp/ignore_list
-
-# Display tests to run on this executor
-echo "Following test files will run on this executor:"
-cat tests-to-run
-
-# Run tests
-mvn --fail-fast -s ../${config.maven.settingsFile} test --no-transfer-progress -Dskip.validation=true -Dsurefire.excludesFile=/tmp/ignore_list`,
+        name: `Run gateway tests`,
+        command: `mvn --fail-fast -s ${config.maven.settingsFile} test --no-transfer-progress -Dgateway-modules -Dskip.validation=true -T 2C`,
       }),
       OpenJdkExecutor.create('medium'),
       ['gravitee-apim-gateway/gravitee-apim-gateway-coverage/target/site/jacoco-aggregate/'],
-      {
-        parallelism: 4,
-      },
     );
   }
 }
