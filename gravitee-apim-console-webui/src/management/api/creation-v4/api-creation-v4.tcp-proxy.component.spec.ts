7069f8f8509d5589078c0577b4f46904c82c88fc
ytvnr <yann.tavernier@graviteesource.com>
2023-12-12 14:27:02 +0100

    fix: fix bad replace in license header https://gravitee.atlassian.net/browse/APIM-3498

--- gravitee-apim-console-webui/src/management/api/creation-v4/api-creation-v4.tcp-proxy.component.spec.ts
+++ gravitee-apim-console-webui/src/management/api/creation-v4/api-creation-v4.tcp-proxy.component.spec.ts
@@ -14,13 +14,13 @@
  * limitations under the License.
  */
 /*
- * Copyright (C) 2015 The Gravitee team (tcp://gravitee.io)
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *         tcp://www.apache.org/licenses/LICENSE-2.0
+ *         http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
