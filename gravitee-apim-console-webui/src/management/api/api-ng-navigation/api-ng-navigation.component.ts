3d3ce8e5384b3a96807b0b75a7e103f9ef5e3d70
thibaudAv <thibaud.avenier@gmail.com>
2023-12-13 13:11:22 +0100

    fix(console): improve & fix height for policy studio (design & debug)

--- gravitee-apim-console-webui/src/management/api/api-ng-navigation/api-ng-navigation.component.ts
+++ gravitee-apim-console-webui/src/management/api/api-ng-navigation/api-ng-navigation.component.ts
@@ -17,7 +17,7 @@ import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
 import { StateService } from '@uirouter/core';
 import { IScope } from 'angular';
 import { castArray, flatMap } from 'lodash';
-import { takeUntil, map } from 'rxjs/operators';
+import { map, shareReplay, takeUntil } from 'rxjs/operators';
 import { GIO_DIALOG_WIDTH, GioBannerTypes, GioMenuService } from '@gravitee/ui-particles-angular';
 import { Observable, Subject } from 'rxjs';
 import { MatDialog } from '@angular/material/dialog';
@@ -225,7 +225,9 @@ export class ApiNgNavigationComponent implements OnInit, OnDestroy {
 
       return banners;
     }),
+    shareReplay(1),
   );
+  public hasBanner$ = this.banners$.pipe(map((banners) => banners.length > 0));
 
   private unsubscribe$ = new Subject();
   constructor(
