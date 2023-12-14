0cb227b9a5abac9f0012937556df47d7a4c2d7cd
“Jan <jan.sobczynski@graviteesource.com>
2023-12-14 13:58:53 +0100

    feat(console): migrate application general part7

--- gravitee-apim-console-webui/src/management/application/details/user-group-access/groups/application-general-groups.component.ts
+++ gravitee-apim-console-webui/src/management/application/details/user-group-access/groups/application-general-groups.component.ts
@@ -13,11 +13,78 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { combineLatest, EMPTY, Subject } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { catchError, takeUntil, tap } from 'rxjs/operators';
+import { FormControl, FormGroup } from '@angular/forms';
+
+import { GroupService } from '../../../../../services-ngx/group.service';
+import { ApplicationService } from '../../../../../services-ngx/application.service';
+import { Group } from '../../../../../entities/management-api-v2';
+import { Application } from '../../../../../entities/application/application';
+import { SnackBarService } from '../../../../../services-ngx/snack-bar.service';
 
 @Component({
   selector: 'application-general-access-groups',
   templateUrl: './application-general-groups.component.html',
   styleUrls: ['./application-general-groups.component.scss'],
 })
-export class ApplicationGeneralGroupsComponent {}
+export class ApplicationGeneralGroupsComponent implements OnInit, OnDestroy {
+  public groups: Group[];
+  public application: Application;
+  public form: FormGroup;
+  public initialFormValue: unknown;
+
+  private unsubscribe$: Subject<void> = new Subject<void>();
+
+  constructor(
+    private readonly groupService: GroupService,
+    private readonly applicationService: ApplicationService,
+    private readonly activatedRoute: ActivatedRoute,
+    private readonly snackBarService: SnackBarService,
+  ) {}
+
+  ngOnInit() {
+    combineLatest([this.applicationService.getById(this.activatedRoute.snapshot.params.applicationId), this.groupService.list()])
+      .pipe(
+        tap(([application, groups]) => {
+          this.groups = groups;
+          this.application = application;
+        }),
+        takeUntil(this.unsubscribe$),
+      )
+      .subscribe(() => {
+        const userGroupList: Group[] = this.groups.filter((group) => this.application.groups?.includes(group.id));
+        this.form = new FormGroup({
+          selectedGroups: new FormControl(userGroupList.map((group) => group.id)),
+        });
+
+        this.initialFormValue = this.form.getRawValue();
+      });
+  }
+
+  ngOnDestroy() {
+    this.unsubscribe$.next();
+    this.unsubscribe$.unsubscribe();
+  }
+
+  save() {
+    this.applicationService
+      .update({
+        ...this.application,
+        groups: this.form.getRawValue()?.selectedGroups ?? this.initialFormValue,
+      })
+      .pipe(
+        tap(() => {
+          this.snackBarService.success('Changes successfully saved!');
+        }),
+        catchError(({ error }) => {
+          this.snackBarService.error(error.message);
+          return EMPTY;
+        }),
+        takeUntil(this.unsubscribe$),
+      )
+      .subscribe(() => this.ngOnInit());
+  }
+}
