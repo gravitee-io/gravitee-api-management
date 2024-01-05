cd6c788f497e8a5e8d8a0dfdbff80fe9076944bb
ThibaudAv <thibaud.avenier@gmail.com>
2024-01-05 10:39:37 +0100

    chore(portal): improve angular proxy configuration
    Like in console we can now start portal on spesific apim env. ex:
    `BACKEND_ENV=apim-4-1-x-api.team-apim.gravitee.dev npm run serve`
    
    (cherry picked from commit b336864e971e734afad0f3b9dd2d4e6e0ac1eb8b)

--- gravitee-apim-portal-webui/src/proxy.conf.mjs
+++ gravitee-apim-portal-webui/src/proxy.conf.mjs
@@ -1,26 +1,28 @@
 /*
  * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
- *
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *         http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-module.exports = {
+const env = process.env.BACKEND_ENV;
+const target = `${env ? `https://${env}` : 'http://localhost:8083'}`
+export default {
   '/portal/environments/DEFAULT': {
-    target: 'https://apim-master-api.team-apim.gravitee.dev/',
-    logLevel: 'debug',
+    target,
     secure: false,
     changeOrigin: true,
     onProxyReq: function (proxyReq, req, res) {
-      proxyReq.setHeader('origin', 'https://apim-master-portal.team-apim.gravitee.dev');
+      proxyReq.setHeader('origin', target.replace("-api", "-portal"));
     },
+    logLevel: 'debug',
   },
 };
