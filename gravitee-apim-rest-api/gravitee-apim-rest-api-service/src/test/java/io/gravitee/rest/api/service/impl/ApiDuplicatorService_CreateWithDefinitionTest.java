fb4b7e8209d133772ff00f13004b774155d25d26
Florent CHAMFROY <florent.chamfroy@graviteesource.com>
2024-01-08 11:52:25 +0100

    fix(import): use group name in page access control groups
    https://gravitee.atlassian.net/browse/APIM-2139
    (cherry picked from commit 9d8bb01cf8d738bb82ee9d780000d512e8da75d8)
    
    # Conflicts:
    #	gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/test/java/io/gravitee/rest/api/service/impl/ApiDuplicatorService_CreateWithDefinitionTest.java
    #	gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/test/java/io/gravitee/rest/api/service/impl/ApiDuplicatorService_UpdateWithDefinitionTest.java
    #	gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/test/resources/io/gravitee/rest/api/management/service/export-convertAsJsonForExportWithoutPlans-1_15.json
    #	gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/test/resources/io/gravitee/rest/api/management/service/export-convertAsJsonForExportWithoutPlans-1_20.json
    #	gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/test/resources/io/gravitee/rest/api/management/service/export-convertAsJsonForExportWithoutPlans-1_25.json
    #	gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/test/resources/io/gravitee/rest/api/management/service/export-convertAsJsonForExportWithoutPlans-3_0.json
    #	gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/test/resources/io/gravitee/rest/api/management/service/export-convertAsJsonForExportWithoutPlans-3_7.json
    #	gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/test/resources/io/gravitee/rest/api/management/service/export-gRPC-convertAsJsonForExport-3_7.json

--- gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/test/java/io/gravitee/rest/api/service/impl/ApiDuplicatorService_CreateWithDefinitionTest.java
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/test/java/io/gravitee/rest/api/service/impl/ApiDuplicatorService_CreateWithDefinitionTest.java
@@ -33,7 +33,20 @@ import com.google.common.base.Charsets;
 import com.google.common.io.Resources;
 import io.gravitee.repository.management.model.Api;
 import io.gravitee.rest.api.idp.api.authentication.UserDetails;
+<<<<<<< HEAD
 import io.gravitee.rest.api.model.*;
+=======
+import io.gravitee.rest.api.model.AccessControlEntity;
+import io.gravitee.rest.api.model.GroupEntity;
+import io.gravitee.rest.api.model.MemberEntity;
+import io.gravitee.rest.api.model.MembershipMemberType;
+import io.gravitee.rest.api.model.MembershipReferenceType;
+import io.gravitee.rest.api.model.PageEntity;
+import io.gravitee.rest.api.model.PlanEntity;
+import io.gravitee.rest.api.model.RoleEntity;
+import io.gravitee.rest.api.model.UpdateApiMetadataEntity;
+import io.gravitee.rest.api.model.UserEntity;
+>>>>>>> 9d8bb01cf8 (fix(import): use group name in page access control groups)
 import io.gravitee.rest.api.model.api.ApiEntity;
 import io.gravitee.rest.api.model.permissions.RoleScope;
 import io.gravitee.rest.api.service.ApiIdsCalculatorService;
@@ -341,13 +354,43 @@ public class ApiDuplicatorService_CreateWithDefinitionTest {
         user.setSource(SOURCE);
         user.setSourceId("ref-user");
 
+<<<<<<< HEAD
         when(apiIdsCalculatorService.recalculateApiDefinitionIds(any(), any())).then(AdditionalAnswers.returnsSecondArg());
+=======
+        GroupEntity knownGroup = new GroupEntity();
+        knownGroup.setId("known_group_id");
+        knownGroup.setName("known group name");
+        when(groupService.findByName(GraviteeContext.getExecutionContext().getEnvironmentId(), "known group name"))
+            .thenReturn(List.of(knownGroup));
+        when(groupService.findByName(GraviteeContext.getExecutionContext().getEnvironmentId(), "group_id")).thenReturn(List.of());
+>>>>>>> 9d8bb01cf8 (fix(import): use group name in page access control groups)
 
         apiDuplicatorService.createWithImportedDefinition(GraviteeContext.getExecutionContext(), toBeImport);
 
         verify(apiService, times(1)).createWithApiDefinition(eq(GraviteeContext.getExecutionContext()), any(), eq("admin"), any());
+        verify(groupService, times(1)).findByName(GraviteeContext.getExecutionContext().getEnvironmentId(), "known group name");
+        verify(groupService, times(1)).findByName(GraviteeContext.getExecutionContext().getEnvironmentId(), "group_id");
         verify(pageService, times(1))
-            .createOrUpdatePages(eq(GraviteeContext.getExecutionContext()), argThat(pagesList -> pagesList.size() == 2), eq(API_ID));
+            .createOrUpdatePages(
+                eq(GraviteeContext.getExecutionContext()),
+                argThat(pagesList -> {
+                    boolean pageSizeOk = pagesList.size() == 4;
+                    boolean accessControlOk = true;
+                    for (PageEntity pageEntity : pagesList) {
+                        if (pageEntity.getOrder() == 3) {
+                            accessControlOk =
+                                accessControlOk &&
+                                pageEntity.getAccessControls().contains(new AccessControlEntity("known_group_id", "GROUP"));
+                        } else if (pageEntity.getOrder() == 4) {
+                            accessControlOk =
+                                accessControlOk && pageEntity.getAccessControls().contains(new AccessControlEntity("group_id", "GROUP"));
+                        }
+                    }
+
+                    return pageSizeOk && accessControlOk;
+                }),
+                eq(API_ID)
+            );
     }
 
     @Test
