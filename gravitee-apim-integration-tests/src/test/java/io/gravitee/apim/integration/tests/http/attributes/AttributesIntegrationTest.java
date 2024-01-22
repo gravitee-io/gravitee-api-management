9b310ffed8f9fdee033972196d3ddc49158974f8
Antoine CORDIER <antoine.cordier@graviteesource.com>
2024-01-22 14:57:17 +0000

    fix: properly build regex  mapping pattern for v4 APIs
    see https://gravitee.atlassian.net/browse/APIM-3494

--- gravitee-apim-integration-tests/src/test/java/io/gravitee/apim/integration/tests/http/attributes/AttributesIntegrationTest.java
+++ gravitee-apim-integration-tests/src/test/java/io/gravitee/apim/integration/tests/http/attributes/AttributesIntegrationTest.java
@@ -196,4 +196,29 @@ public class AttributesIntegrationTest {
                 });
         }
     }
+
+    @Nested
+    @GatewayTest
+    @DeployApi({ "/apis/v4/http/mapped-path-attribute.json" })
+    class CheckingMappedPathAttributeV4 extends AbstractAttributesIntegrationTest {
+
+        @Test
+        void should_set_mapped_path_attribute(HttpClient httpClient) throws InterruptedException {
+            httpClient
+                .rxRequest(HttpMethod.GET, "/mapped/test/mock")
+                .flatMap(HttpClientRequest::rxSend)
+                .flatMapPublisher(response -> {
+                    assertThat(response.statusCode()).isEqualTo(200);
+                    return response.toFlowable();
+                })
+                .test()
+                .await()
+                .assertComplete()
+                .assertNoErrors()
+                .assertValue(body -> {
+                    assertThat(body.toString()).contains("/test/:testId");
+                    return true;
+                });
+        }
+    }
 }
