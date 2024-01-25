471d3d12ab130fa88d892dafa4f731bdb30db6d8
ThibaudAv <thibaud.avenier@gmail.com>
2024-01-25 12:22:55 +0100

    chore(console): enable angular `strictInjectionParameters`

--- gravitee-apim-console-webui/src/app.module.ts
+++ gravitee-apim-console-webui/src/app.module.ts
@@ -71,6 +71,10 @@ import { AuthModule } from './auth/auth.module';
       },
       deps: ['Constants'],
     },
+    {
+      provide: 'isFactory',
+      useValue: true,
+    },
   ],
 })
 export class AppModule implements DoBootstrap {
