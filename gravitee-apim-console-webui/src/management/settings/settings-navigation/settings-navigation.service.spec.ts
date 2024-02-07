b423c0623a4fd28db33430dbdebc2d7f1c86a7a1
Ouahid Khelifi <ouahid.khelifi@gmail.com>
2024-02-07 17:20:36 +0100

    feat: add search for environment settings menu

---
+++ gravitee-apim-console-webui/src/management/settings/settings-navigation/settings-navigation.service.spec.ts
@@ -0,0 +1,84 @@
+/*
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { TestBed } from '@angular/core/testing';
+
+import { SettingsNavigationService } from './settings-navigation.service';
+
+import { GioPermissionService } from '../../../shared/components/gio-permission/gio-permission.service';
+import { GioHttpTestingModule } from '../../../shared/testing';
+
+describe('SettingsNavigationService', () => {
+  let service: SettingsNavigationService;
+  const envId = 'envId';
+
+  const init = (hasAnyMatching = true) => {
+    TestBed.configureTestingModule({
+      imports: [GioHttpTestingModule],
+      providers: [
+        {
+          provide: GioPermissionService,
+          useValue: {
+            hasAnyMatching: () => hasAnyMatching,
+          },
+        },
+      ],
+    });
+    service = TestBed.inject(SettingsNavigationService);
+  };
+
+  it('should map routes to search items', () => {
+    init();
+
+    const menuSearchItems = service.getSettingsNavigationSearchItems(envId);
+
+    expect(menuSearchItems).toHaveLength(16);
+    expect(menuSearchItems).toEqual(
+      expect.arrayContaining(
+        [
+          'Analytics',
+          'API Portal Information',
+          'API Quality',
+          'Authentication',
+          'Categories',
+          'Client Registration',
+          'Documentation',
+          'Metadata',
+          'Settings',
+          'Theme',
+          'Top APIs',
+          'API Logging',
+          'Dictionaries',
+          'User Fields',
+          'Groups',
+          'Notification settings',
+        ].map((name) =>
+          expect.objectContaining({
+            name,
+            routerLink: expect.not.stringContaining('./') && expect.stringContaining(`${envId}/settings`),
+          }),
+        ),
+      ),
+    );
+  });
+
+  it('should not map any route to search items', () => {
+    init(false);
+
+    const menuSearchItems = service.getSettingsNavigationSearchItems(envId);
+
+    expect(menuSearchItems).toStrictEqual([]);
+  });
+});
---
+++ gravitee-apim-console-webui/src/management/settings/settings-navigation/settings-navigation.service.ts
@@ -0,0 +1,161 @@
+/*
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { Injectable } from '@angular/core';
+import { MenuSearchItem } from '@gravitee/ui-particles-angular';
+
+import { GioPermissionService } from '../../../shared/components/gio-permission/gio-permission.service';
+import { cleanRouterLink } from '../../../util/router-link.util';
+
+export interface MenuItem {
+  routerLink?: string;
+  displayName: string;
+  permissions?: string[];
+  category?: string;
+}
+
+export interface GroupItem {
+  title: string;
+  items: MenuItem[];
+}
+
+@Injectable({ providedIn: 'root' })
+export class SettingsNavigationService {
+  constructor(private readonly permissionService: GioPermissionService) {}
+
+  public getSettingsNavigationRoutes(): GroupItem[] {
+    const items: GroupItem[] = [
+      {
+        title: 'Portal',
+        items: [
+          {
+            displayName: 'Analytics',
+            routerLink: './analytics',
+            permissions: ['environment-dashboard-r'],
+          },
+          {
+            displayName: 'API Portal Information',
+            routerLink: './api-portal-header',
+            permissions: ['environment-api_header-r'],
+          },
+          {
+            displayName: 'API Quality',
+            routerLink: './api-quality-rules',
+            permissions: ['environment-quality_rule-r'],
+          },
+          {
+            displayName: 'Authentication',
+            routerLink: './identity-providers',
+            permissions: ['organization-identity_provider-r', 'environment-identity_provider_activation-r'],
+          },
+          {
+            displayName: 'Categories',
+            routerLink: './categories',
+            permissions: ['environment-category-r'],
+          },
+          {
+            displayName: 'Client Registration',
+            routerLink: './client-registration-providers',
+            permissions: ['environment-client_registration_provider-r'],
+          },
+          {
+            displayName: 'Documentation',
+            routerLink: './documentation',
+            permissions: ['environment-documentation-c', 'environment-documentation-u', 'environment-documentation-d'],
+          },
+          {
+            displayName: 'Metadata',
+            routerLink: './metadata',
+            permissions: ['environment-metadata-r'],
+          },
+          {
+            displayName: 'Settings',
+            routerLink: './portal',
+            permissions: ['environment-settings-r'],
+          },
+          {
+            displayName: 'Theme',
+            routerLink: './theme',
+            permissions: ['environment-theme-r'],
+          },
+          {
+            displayName: 'Top APIs',
+            routerLink: './top-apis',
+            permissions: ['environment-top_apis-r'],
+          },
+        ],
+      },
+      {
+        title: 'Gateway',
+        items: [
+          {
+            displayName: 'API Logging',
+            routerLink: './api-logging',
+            permissions: ['organization-settings-r'],
+          },
+          {
+            displayName: 'Dictionaries',
+            routerLink: './dictionaries',
+            permissions: ['environment-dictionary-r'],
+          },
+        ],
+      },
+      {
+        title: 'User Management',
+        items: [
+          {
+            displayName: 'User Fields',
+            routerLink: './custom-user-fields',
+            permissions: ['organization-custom_user_fields-r'],
+          },
+          {
+            displayName: 'Groups',
+            routerLink: './groups',
+            permissions: ['environment-group-r'],
+          },
+        ],
+      },
+      {
+        title: 'Notifications',
+        items: [
+          {
+            displayName: 'Notification settings',
+            routerLink: './notifications',
+            permissions: ['environment-notification-r'],
+          },
+        ],
+      },
+    ];
+
+    items.forEach((groupItem) => {
+      groupItem.items = groupItem.items.filter((item) => !item.permissions || this.permissionService.hasAnyMatching(item.permissions));
+    });
+
+    return items;
+  }
+
+  public getSettingsNavigationSearchItems(environmentId: string): MenuSearchItem[] {
+    return this.getSettingsNavigationRoutes().flatMap((groupItem) =>
+      groupItem.items.map((item) => {
+        return {
+          name: item.displayName,
+          routerLink: `/${environmentId}/settings/${cleanRouterLink(item.routerLink)}`,
+          category: `Environment / ${groupItem.title}`,
+          groupIds: [environmentId],
+        };
+      }),
+    );
+  }
+}
