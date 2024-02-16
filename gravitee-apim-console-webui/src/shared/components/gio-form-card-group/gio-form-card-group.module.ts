f4418126206947963b74549f9db51db743262e9b
ThibaudAv <thibaud.avenier@gmail.com>
2024-02-16 16:21:57 +0100

    refactor(console): form-card-group - migrate material to MDC-based component

--- gravitee-apim-console-webui/src/shared/components/gio-form-card-group/gio-form-card-group.module.ts
+++ gravitee-apim-console-webui/src/shared/components/gio-form-card-group/gio-form-card-group.module.ts
@@ -15,7 +15,7 @@
  */
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatLegacyCardModule as MatCardModule } from '@angular/material/legacy-card';
+import { MatCardModule } from '@angular/material/card';
 import { MatRippleModule } from '@angular/material/core';
 import { MatIconModule } from '@angular/material/icon';
 
