dd15f7b19973318b91c149632ef20b8ea72a9e34
ThibaudAv <thibaud.avenier@gmail.com>
2024-02-16 13:15:35 +0100

    refactor(console): metadata - migrate material to MDC-based component

--- gravitee-apim-console-webui/src/components/gio-metadata/gio-metadata.module.ts
+++ gravitee-apim-console-webui/src/components/gio-metadata/gio-metadata.module.ts
@@ -15,20 +15,20 @@
  */
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatLegacyTableModule as MatTableModule } from '@angular/material/legacy-table';
+import { MatTableModule } from '@angular/material/table';
 import { MatSortModule } from '@angular/material/sort';
 import { MatIconModule } from '@angular/material/icon';
-import { MatLegacyTooltipModule as MatTooltipModule } from '@angular/material/legacy-tooltip';
-import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
-import { MatLegacyDialogModule } from '@angular/material/legacy-dialog';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { ReactiveFormsModule } from '@angular/forms';
-import { MatLegacyInputModule as MatInputModule } from '@angular/material/legacy-input';
-import { MatLegacySelectModule as MatSelectModule } from '@angular/material/legacy-select';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatMomentDateModule, MAT_MOMENT_DATE_ADAPTER_OPTIONS } from '@angular/material-moment-adapter';
-import { MatLegacyCardModule as MatCardModule } from '@angular/material/legacy-card';
-import { MatLegacySnackBarModule as MatSnackBarModule } from '@angular/material/legacy-snack-bar';
+import { MatCardModule } from '@angular/material/card';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatLegacySnackBarModule } from '@angular/material/legacy-snack-bar';
 
 import { GioMetadataDialogComponent } from './dialog/gio-metadata-dialog.component';
 import { GioMetadataComponent } from './gio-metadata.component';
@@ -45,12 +45,12 @@ import { GioPermissionModule } from '../../shared/components/gio-permission/gio-
     MatCardModule,
     MatDatepickerModule,
     MatDialogModule,
-    MatLegacyDialogModule,
     MatInputModule,
     MatIconModule,
     MatMomentDateModule,
     MatSelectModule,
     MatSnackBarModule,
+    MatLegacySnackBarModule,
     MatSortModule,
     MatTableModule,
     MatTooltipModule,
