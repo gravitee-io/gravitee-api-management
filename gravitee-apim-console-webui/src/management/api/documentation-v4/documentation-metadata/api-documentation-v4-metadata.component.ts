42052e2d4ea1228c62c86a15208def0fc22ec99c
jourdiw <jourdi.waller@graviteesource.com>
2024-02-19 16:59:07 +0100

    feat(console): add metadata page for v4 apis

---
+++ gravitee-apim-console-webui/src/management/api/documentation-v4/documentation-metadata/api-documentation-v4-metadata.component.ts
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { MetadataSaveServices } from '../../../../components/gio-metadata/gio-metadata.component';
+import { ApiService } from '../../../../services-ngx/api.service';
+
+@Component({
+  selector: 'api-documentation-v4-metadata',
+  templateUrl: './api-documentation-v4-metadata.component.html',
+  styleUrls: ['./api-documentation-v4-metadata.component.scss'],
+})
+export class ApiDocumentationV4MetadataComponent implements OnInit {
+  metadataSaveServices: MetadataSaveServices;
+  description: string;
+
+  constructor(private readonly apiService: ApiService, private readonly activatedRoute: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.metadataSaveServices = {
+      type: 'API',
+      list: () => this.apiService.listMetadata(this.activatedRoute.snapshot.params.apiId),
+      create: (newMetadata) => this.apiService.createMetadata(this.activatedRoute.snapshot.params.apiId, newMetadata),
+      update: (updateMetadata) => this.apiService.updateMetadata(this.activatedRoute.snapshot.params.apiId, updateMetadata),
+      delete: (metadataKey) => this.apiService.deleteMetadata(this.activatedRoute.snapshot.params.apiId, metadataKey),
+    };
+    this.description = `Set metadata information on the API that can be easily accessed through Markdown templating`;
+  }
+}
