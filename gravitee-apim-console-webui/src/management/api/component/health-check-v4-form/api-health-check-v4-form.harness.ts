c10f6c358e5574f0c4b3292562a13bdff3f011ed
Ouahid Khelifi <ouahid.khelifi@gmail.com>
2024-02-20 12:44:25 +0100

    fix: allow user to disable health check configuration

--- gravitee-apim-console-webui/src/management/api/component/health-check-v4-form/api-health-check-v4-form.harness.ts
+++ gravitee-apim-console-webui/src/management/api/component/health-check-v4-form/api-health-check-v4-form.harness.ts
@@ -14,8 +14,8 @@
  * limitations under the License.
  */
 import { ComponentHarness } from '@angular/cdk/testing';
-import { MatLegacyInputHarness as MatInputHarness } from '@angular/material/legacy-input/testing';
 import { MatSlideToggleHarness } from '@angular/material/slide-toggle/testing';
+import { MatInputHarness } from '@angular/material/input/testing';
 
 export class ApiHealthCheckV4FormHarness extends ComponentHarness {
   static hostSelector = 'api-health-check-v4-form';
