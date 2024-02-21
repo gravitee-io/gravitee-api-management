45a2756addd00bf2fd1725af3a0490f54bb30cbf
“Jan <jan.sobczynski@graviteesource.com>
2024-02-21 10:12:43 +0100

    feat(console): migrating settings identity providers tab

---
+++ gravitee-apim-console-webui/src/services-ngx/environment-identity-provider.service.spec.ts
@@ -0,0 +1,76 @@
+/*
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { HttpTestingController } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+
+import { EnvironmentIdentityProviderService } from './environment-identity-provider.service';
+
+import { CONSTANTS_TESTING, GioHttpTestingModule } from '../shared/testing';
+import { fakeIdentityProviderActivation, IdentityProviderActivation } from '../entities/identity-provider';
+
+describe('IdentityProviderService', () => {
+  let httpTestingController: HttpTestingController;
+  let environmentIdentityProviderService: EnvironmentIdentityProviderService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [GioHttpTestingModule],
+    });
+
+    httpTestingController = TestBed.inject(HttpTestingController);
+    environmentIdentityProviderService = TestBed.inject<EnvironmentIdentityProviderService>(EnvironmentIdentityProviderService);
+  });
+
+  describe('list activated', () => {
+    it('should return a list of activated identity providers', (done) => {
+      const activatedIdentityProviders: IdentityProviderActivation[] = [
+        fakeIdentityProviderActivation({ identityProvider: 'gravitee-am' }),
+      ];
+
+      environmentIdentityProviderService.list().subscribe((activatedIdentityProviders: IdentityProviderActivation[]) => {
+        expect(activatedIdentityProviders).toEqual(activatedIdentityProviders);
+        done();
+      });
+
+      const req = httpTestingController.expectOne(`${CONSTANTS_TESTING.env.baseURL}/identities`);
+      expect(req.request.method).toEqual('GET');
+
+      req.flush(activatedIdentityProviders);
+    });
+
+    it('should update a list of activated identity providers', (done) => {
+      const activatedIdentityProviders: IdentityProviderActivation[] = [
+        fakeIdentityProviderActivation({ identityProvider: 'gravitee-am' }),
+      ];
+
+      environmentIdentityProviderService
+        .update(activatedIdentityProviders)
+        .subscribe((activatedIdentityProviders: IdentityProviderActivation[]) => {
+          expect(activatedIdentityProviders).toEqual(activatedIdentityProviders);
+          done();
+        });
+
+      const req = httpTestingController.expectOne(`${CONSTANTS_TESTING.env.baseURL}/identities`);
+      expect(req.request.method).toEqual('PUT');
+
+      req.flush(activatedIdentityProviders);
+    });
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
+  });
+});
