8e8bd85f8e6fd268107a59266f46a6cbdab14104
ytvnr <yann.tavernier@graviteesource.com>
2024-02-21 09:06:46 +0100

    refactor: improve consistency of api-endpoint-group test style https://gravitee.atlassian.net/browse/APIM-3915

--- gravitee-apim-console-webui/src/management/api/endpoints-v4/endpoint-group/api-endpoint-group.component.spec.ts
+++ gravitee-apim-console-webui/src/management/api/endpoints-v4/endpoint-group/api-endpoint-group.component.spec.ts
@@ -29,33 +29,9 @@ import { ApiEndpointGroupModule } from './api-endpoint-group.module';
 import { CONSTANTS_TESTING, GioHttpTestingModule } from '../../../../shared/testing';
 import { CurrentUserService, UIRouterState, UIRouterStateParams } from '../../../../ajs-upgraded-providers';
 import { User as DeprecatedUser } from '../../../../entities/user';
-import { ApiV4, EndpointGroupV4, fakeApiV4 } from '../../../../entities/management-api-v2';
+import { ApiV4, fakeApiV4 } from '../../../../entities/management-api-v2';
 import { SnackBarService } from '../../../../services-ngx/snack-bar.service';
 import { fakeEndpointGroupV4 } from '../../../../entities/management-api-v2/api/v4/endpointGroupV4.fixture';
-
-/**
- * Test data
- */
-const API_ID = 'apiId';
-const DEFAULT_GROUP_NAME = 'default-group';
-const VALID_ENDPOINT_GROUP_NAME = 'New Endpoint Group Name';
-const INVALID_ENDPOINT_GROUP_NAME = '';
-const DEFAULT_LOAD_BALANCER_TYPE = 'ROUND_ROBIN';
-const ALTERNATE_LOAD_BALANCER_TYPE = 'RANDOM';
-const ALTERNATE_LOAD_BALANCER_TYPE_2 = 'WEIGHTED_RANDOM';
-const GROUP_INDEX = 0;
-const FAKE_UI_ROUTER = { go: jest.fn() };
-
-/**
- * Given the api and the updated endpoint group attributes
- * return the newly modified endpoint group
- *
- * @param api api object
- * @param endpointGroupAttributes endpoint group attributes that will be updated
- */
-function getExpectedEndpoints(api: ApiV4, endpointGroupAttributes): EndpointGroupV4[] {
-  return [{ ...api.endpointGroups[GROUP_INDEX], ...endpointGroupAttributes }];
-}
 
 /**
  * Expect that a single GET request has been made which matches the specified URL
@@ -93,19 +69,17 @@ function expectApiPutRequest(api: ApiV4, fixture: ComponentFixture<any>, httpTes
 }
 
 describe('ApiEndpointGroupComponent', () => {
+  const API_ID = 'apiId';
+  const FAKE_UI_ROUTER = { go: jest.fn() };
   let fixture: ComponentFixture<ApiEndpointGroupComponent>;
   let httpTestingController: HttpTestingController;
   let componentHarness: ApiEndpointGroupHarness;
   let api: ApiV4;
 
-  const initComponent = async (testApi: ApiV4) => {
+  const initComponent = async (api: ApiV4, routerParams: unknown = { apiId: API_ID, groupIndex: 0 }) => {
     const currentUser = new DeprecatedUser();
 
     currentUser.userPermissions = ['api-definition-u', 'api-definition-c', 'api-definition-r'];
-
-    const routerParams: unknown = { apiId: API_ID, groupIndex: GROUP_INDEX };
-
-    api = testApi;
 
     TestBed.configureTestingModule({
       imports: [NoopAnimationsModule, GioHttpTestingModule, ApiEndpointGroupModule, MatIconTestingModule, FormsModule],
@@ -123,6 +97,7 @@ describe('ApiEndpointGroupComponent', () => {
 
     await TestBed.compileComponents();
     fixture = TestBed.createComponent(ApiEndpointGroupComponent);
+    fixture.componentInstance.api = api;
     httpTestingController = TestBed.inject(HttpTestingController);
     componentHarness = await TestbedHarnessEnvironment.harnessForFixture(fixture, ApiEndpointGroupHarness);
 
@@ -136,140 +111,67 @@ describe('ApiEndpointGroupComponent', () => {
     httpTestingController.verify();
   });
 
-  describe('GIVEN the current page is the endpoint group page', () => {
-    beforeEach(async () => {
+  describe('endpoint group update - configuration', () => {
+    it('should not update endpoint group with invalid name', async () => {
       api = fakeApiV4({ id: API_ID, endpointGroups: [fakeEndpointGroupV4()] });
       await initComponent(api);
+      await componentHarness.clickGeneralTab();
+      expectApiSchemaGetRequests(api, fixture, httpTestingController);
+
+      expect(await componentHarness.readEndpointGroupNameInput()).toEqual('default-group');
+
+      // empty string is not a valid name
+      await componentHarness.writeToEndpointGroupNameInput('');
+      expect(await componentHarness.isGeneralTabSaveButtonInvalid()).toBeTruthy();
+
+      // reset change will restore initial value
+      await componentHarness.clickEndpointGroupDismissButton();
+      expect(await componentHarness.readEndpointGroupNameInput()).toEqual('default-group');
+      expect(await componentHarness.configurationTabIsVisible()).toEqual(true);
     });
 
-    // General Tab
-    describe('WHEN the general tab is selected', () => {
-      beforeEach(async () => {
-        await componentHarness.clickGeneralTab();
-        expectApiSchemaGetRequests(api, fixture, httpTestingController);
-      });
+    it('should update endpoint group with valid name', async () => {
+      api = fakeApiV4({ id: API_ID, endpointGroups: [fakeEndpointGroupV4()] });
+      await initComponent(api);
+      await componentHarness.clickGeneralTab();
+      expectApiSchemaGetRequests(api, fixture, httpTestingController);
 
-      // General Tab -> Endpoint Group Name
-      it('THEN the endpoint group name field should be visible with a default value', async () => {
-        expect(await componentHarness.readEndpointGroupNameInput()).toEqual(DEFAULT_GROUP_NAME);
-      });
+      expect(await componentHarness.readEndpointGroupNameInput()).toEqual('default-group');
 
-      describe('AND WHEN the endpoint group name is modified with a valid value', () => {
-        beforeEach(async () => {
-          await componentHarness.writeToEndpointGroupNameInput(VALID_ENDPOINT_GROUP_NAME);
-        });
-        it('THEN the endpoint group name field should be updated with the expected value', async () => {
-          expect(await componentHarness.readEndpointGroupNameInput()).toEqual(VALID_ENDPOINT_GROUP_NAME);
-        });
+      // change to a valid name
+      await componentHarness.writeToEndpointGroupNameInput('default-group updated');
+      expect(await componentHarness.isGeneralTabSaveButtonInvalid()).toBeFalsy();
 
-        it('THEN the save button should be valid', async () => {
-          expect(await componentHarness.isGeneralTabSaveButtonInvalid()).toBeFalsy();
-        });
-
-        describe('AND WHEN the save button is clicked', () => {
-          it('THEN the endpoint group name field should be saved', async () => {
-            await componentHarness.clickEndpointGroupSaveButton();
-            expectApiGetRequest(api, fixture, httpTestingController);
-            expect(expectApiPutRequest(api, fixture, httpTestingController).request.body.endpointGroups).toEqual(
-              getExpectedEndpoints(api, { name: VALID_ENDPOINT_GROUP_NAME }),
-            );
-          });
-        });
-
-        describe('AND WHEN the endpoint group is modified with an invalid value', () => {
-          beforeEach(async () => {
-            await componentHarness.writeToEndpointGroupNameInput(INVALID_ENDPOINT_GROUP_NAME);
-          });
-
-          it('THEN the save button should be invalid', async () => {
-            expect(await componentHarness.isGeneralTabSaveButtonInvalid()).toBeTruthy();
-          });
-        });
-
-        describe('AND WHEN the changes made have been dismissed', () => {
-          beforeEach(async () => {
-            await componentHarness.writeToEndpointGroupNameInput('TEST');
-
-            await componentHarness.clickEndpointGroupDismissButton();
-          });
-
-          it('THEN the endpoint group name should reset back to the original value', async () => {
-            expect(await componentHarness.readEndpointGroupNameInput()).toEqual(DEFAULT_GROUP_NAME);
-          });
-        });
-      });
-
-      // General Tab -> Endpoint Group Load Balancer Type
-      it('THEN the endpoint group load balancer field should be visible with a default value', async () => {
-        expect(await componentHarness.readEndpointGroupLoadBalancerSelector()).toEqual(DEFAULT_LOAD_BALANCER_TYPE);
-      });
-
-      describe('AND WHEN the endpoint group load balancer type is modified', () => {
-        beforeEach(async () => {
-          await componentHarness.writeToEndpointGroupLoadBalancerSelector(ALTERNATE_LOAD_BALANCER_TYPE);
-        });
-
-        it('THEN the endpoint group name field should be updated with the expected value', async () => {
-          expect(await componentHarness.readEndpointGroupLoadBalancerSelector()).toEqual(ALTERNATE_LOAD_BALANCER_TYPE);
-        });
-
-        describe('AND WHEN the save button is clicked', () => {
-          it('THEN the endpoint group load balancer type field should be saved', async () => {
-            await componentHarness.clickEndpointGroupSaveButton();
-            expectApiGetRequest(api, fixture, httpTestingController);
-            expect(expectApiPutRequest(api, fixture, httpTestingController).request.body.endpointGroups).toEqual(
-              getExpectedEndpoints(api, {
-                name: DEFAULT_GROUP_NAME,
-                loadBalancer: { type: ALTERNATE_LOAD_BALANCER_TYPE },
-              }),
-            );
-          });
-        });
-
-        describe('AND WHEN the changes made have been dismissed', () => {
-          beforeEach(async () => {
-            await componentHarness.writeToEndpointGroupLoadBalancerSelector(ALTERNATE_LOAD_BALANCER_TYPE_2);
-
-            await componentHarness.clickEndpointGroupDismissButton();
-          });
-
-          it('THEN the endpoint group load balancer type should reset back to the original value', async () => {
-            expect(await componentHarness.readEndpointGroupLoadBalancerSelector()).toEqual(DEFAULT_LOAD_BALANCER_TYPE);
-          });
-        });
-      });
-      it('THEN should show the configuration tab', async () => {
-        expect(await componentHarness.configurationTabIsVisible()).toEqual(true);
-      });
+      // save the correct value
+      await componentHarness.clickEndpointGroupSaveButton();
+      expectApiGetRequest(api, fixture, httpTestingController);
+      const endpointsGroup = expectApiPutRequest(api, fixture, httpTestingController).request.body.endpointGroups;
+      expect(endpointsGroup[0].name).toEqual('default-group updated');
+      expect(await componentHarness.configurationTabIsVisible()).toEqual(true);
     });
 
-    // Back Button
-    describe('WHEN the back button is clicked', () => {
-      const routerSpy = jest.spyOn(FAKE_UI_ROUTER, 'go');
+    it('should update endpoint group load balancing', async () => {
+      api = fakeApiV4({ id: API_ID, endpointGroups: [fakeEndpointGroupV4()] });
+      await initComponent(api);
+      await componentHarness.clickGeneralTab();
+      expectApiSchemaGetRequests(api, fixture, httpTestingController);
 
-      beforeEach(async () => {
-        await componentHarness.clickBackButton();
-        expectApiSchemaGetRequests(api, fixture, httpTestingController);
-      });
+      expect(await componentHarness.readEndpointGroupLoadBalancerSelector()).toEqual('ROUND_ROBIN');
 
-      it('THEN the endpoint groups list page should be the next page expected to be shown', async () => {
-        expect(routerSpy).toHaveBeenCalledWith('management.apis.endpoint-groups', undefined, undefined);
-      });
+      // change load balancing strategy
+      await componentHarness.writeToEndpointGroupLoadBalancerSelector('RANDOM');
+      expect(await componentHarness.isGeneralTabSaveButtonInvalid()).toBeFalsy();
+
+      // save the new strategy
+      await componentHarness.clickEndpointGroupSaveButton();
+      expectApiGetRequest(api, fixture, httpTestingController);
+      const endpointsGroup = expectApiPutRequest(api, fixture, httpTestingController).request.body.endpointGroups;
+      expect(endpointsGroup[0].name).toEqual('default-group');
+      expect(endpointsGroup[0].loadBalancer.type).toEqual('RANDOM');
+      expect(await componentHarness.configurationTabIsVisible()).toEqual(true);
     });
   });
-
-  describe('GIVEN the current page is a mock endpoint group page', () => {
-    beforeEach(async () => {
-      const apiWithMockEndpointGroup = fakeApiV4({ id: API_ID, endpointGroups: [fakeEndpointGroupV4({ type: 'mock' })] });
-      await initComponent(apiWithMockEndpointGroup);
-    });
-
-    it('THEN the configuration tab is not visible', async () => {
-      expect(await componentHarness.configurationTabIsVisible()).toEqual(false);
-    });
-  });
-
-  describe('Endpoint group name validation', () => {
+  describe('endpoint group name validation', () => {
     const TRIM_ENDPOINT_GROUP_NAME = 'Neat and trim';
     const SPACEY_ENDPOINT_GROUP_NAME = 'Space after ';
     const TRIM_ENDPOINT_NAME = 'Trim and neat';
