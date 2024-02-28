011d67100945bc7236ebf883bec2e83f3532cab8
pes-spyro-soft-com <pawel.lesniowski@graviteesource.com>
2024-02-28 15:12:59 +0100

    refactor(console): settings top-apis

---
+++ gravitee-apim-console-webui/src/management/settings/top-apis/migrated/add-top-apis-dialog/add-top-apis-dialog.component.ts
@@ -0,0 +1,100 @@
+/*
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Component, Inject, OnInit, DestroyRef, inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { FormControl } from '@angular/forms';
+import { forkJoin, Observable } from 'rxjs';
+import { filter, map, startWith, tap } from 'rxjs/operators';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { filter as lodashFilter, includes as lodashIncludes, map as lodashMap } from 'lodash';
+
+import { TopApi } from '../top-apis.model';
+import { ApiService } from '../../../../../services-ngx/api.service';
+import { Api } from '../../../../../entities/api';
+import { TopApiService } from '../../../../../services-ngx/top-api.service';
+
+export interface AddTopApisDialogData {
+  title: string;
+}
+export type AddTopApisDialogResult = Api;
+
+@Component({
+  selector: 'add-top-apis-dialog',
+  templateUrl: './add-top-apis-dialog.component.html',
+  styleUrls: ['./add-top-apis-dialog.component.scss'],
+})
+export class AddTopApisDialogComponent implements OnInit {
+  public searchApiControl: FormControl<string | Api> = new FormControl('');
+  public apis: Api[] = [];
+  public filteredOptions$: Observable<Api[]>;
+  public isApiSelected = false;
+  private destroyRef = inject(DestroyRef);
+
+  constructor(
+    public dialogRef: MatDialogRef<AddTopApisDialogComponent>,
+    private apiService: ApiService,
+    private topApiService: TopApiService,
+    @Inject(MAT_DIALOG_DATA) public data: AddTopApisDialogData,
+  ) {}
+
+  ngOnInit(): void {
+    forkJoin([this.apiService.getAll(), this.topApiService.getList()])
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe(([apis, topApis]: [Api[], TopApi[]]) => {
+        this.apis = this.removeTopApis(apis, topApis);
+      });
+
+    this.filteredOptions$ = this.searchApiControl.valueChanges.pipe(
+      startWith(''),
+      filter((v) => typeof v === 'string'),
+      tap(() => (this.isApiSelected = false)),
+      map((value: string): Api[] => (value ? this._filter(value) : [])),
+      takeUntilDestroyed(this.destroyRef),
+    );
+  }
+
+  removeTopApis(apis, topApis): Api[] {
+    return lodashFilter(apis, (api: Api) => !lodashIncludes(lodashMap(topApis, 'api'), api.id));
+  }
+
+  private _filter(value: string): Api[] {
+    const filterValue = value.toLowerCase();
+    return this.apis.filter((api: Api) => {
+      return api.name.toLowerCase().includes(filterValue) || api.description.toLowerCase().includes(filterValue);
+    });
+  }
+
+  public displayFn(option: Api): string {
+    return option.name;
+  }
+
+  resetSearchTerm() {
+    this.searchApiControl.setValue('');
+  }
+
+  onCancelClick(): void {
+    this.dialogRef.close();
+  }
+
+  selectTopAPI(): void {
+    this.isApiSelected = true;
+  }
+
+  public submit() {
+    this.dialogRef.close(this.searchApiControl.getRawValue());
+  }
+}
---
+++ gravitee-apim-console-webui/src/management/settings/top-apis/migrated/top-apis.module.ts
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { GioAvatarModule, GioConfirmDialogModule, GioIconsModule } from '@gravitee/ui-particles-angular';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDialogModule } from '@angular/material/dialog';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatOptionModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatInputModule } from '@angular/material/input';
+
+import { TopApisComponent } from './top-apis.component';
+import { AddTopApisDialogComponent } from './add-top-apis-dialog/add-top-apis-dialog.component';
+
+import { GioTableWrapperModule } from '../../../../shared/components/gio-table-wrapper/gio-table-wrapper.module';
+import { GioPermissionModule } from '../../../../shared/components/gio-permission/gio-permission.module';
+
+@NgModule({
+  declarations: [TopApisComponent, AddTopApisDialogComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatDialogModule,
+    MatIconModule,
+    MatTooltipModule,
+    MatSnackBarModule,
+    MatCardModule,
+    MatTableModule,
+    MatButtonModule,
+    GioIconsModule,
+    GioConfirmDialogModule,
+    GioTableWrapperModule,
+    GioPermissionModule,
+    MatFormFieldModule,
+    MatOptionModule,
+    MatSelectModule,
+    ReactiveFormsModule,
+    MatAutocompleteModule,
+    MatInputModule,
+    GioAvatarModule,
+  ],
+  exports: [TopApisComponent],
+})
+export class TopApisModule {}
