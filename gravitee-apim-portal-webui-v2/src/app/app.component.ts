cbfac4c14f1cb1291e246ccd80b2150f5bbd7db1
Florent CHAMFROY <florent.chamfroy@graviteesource.com>
2024-03-12 23:32:46 +0100

    style: apply prettier

--- gravitee-apim-portal-webui-v2/src/app/app.component.ts
+++ gravitee-apim-portal-webui-v2/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'gravitee-apim-portal-webui-v2';
