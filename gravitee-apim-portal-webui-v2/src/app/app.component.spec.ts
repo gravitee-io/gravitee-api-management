a4cafddec04e11d197033304cf94e0b7ce85973b
Florent CHAMFROY <florent.chamfroy@graviteesource.com>
2024-03-12 23:48:38 +0100

    chore: simplify init page

--- gravitee-apim-portal-webui-v2/src/app/app.component.spec.ts
+++ gravitee-apim-portal-webui-v2/src/app/app.component.spec.ts
@@ -24,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, gravitee-apim-portal-webui-v2');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello there');
   });
 });
