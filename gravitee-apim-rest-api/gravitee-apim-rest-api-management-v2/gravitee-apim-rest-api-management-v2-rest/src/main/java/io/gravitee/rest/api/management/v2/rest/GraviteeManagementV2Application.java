e774d2dfa14dbdcbc63ee7c65ec5b43a95b7f0df
Jedrzej Janasiak <jedrzej.janasiak@graviteesource.com>
2024-03-15 17:06:12 +0100

    feat(console): get integrations list and create integrations with stub response provided

--- gravitee-apim-rest-api/gravitee-apim-rest-api-management-v2/gravitee-apim-rest-api-management-v2-rest/src/main/java/io/gravitee/rest/api/management/v2/rest/GraviteeManagementV2Application.java
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-management-v2/gravitee-apim-rest-api-management-v2-rest/src/main/java/io/gravitee/rest/api/management/v2/rest/GraviteeManagementV2Application.java
@@ -35,6 +35,7 @@ import io.gravitee.rest.api.management.v2.rest.resource.api.ApisResource;
 import io.gravitee.rest.api.management.v2.rest.resource.installation.EnvironmentsResource;
 import io.gravitee.rest.api.management.v2.rest.resource.installation.GraviteeLicenseResource;
 import io.gravitee.rest.api.management.v2.rest.resource.installation.OrganizationResource;
+import io.gravitee.rest.api.management.v2.rest.resource.integration.IntegrationsResource;
 import io.gravitee.rest.api.management.v2.rest.resource.plugin.ApiServicesResource;
 import io.gravitee.rest.api.management.v2.rest.resource.plugin.EndpointsResource;
 import io.gravitee.rest.api.management.v2.rest.resource.plugin.EntrypointsResource;
@@ -71,6 +72,7 @@ public class GraviteeManagementV2Application extends ResourceConfig {
         register(EntrypointsResource.class);
         register(ApiServicesResource.class);
         register(PoliciesResource.class);
+        register(IntegrationsResource.class);
 
         register(MultiPartFeature.class);
 
