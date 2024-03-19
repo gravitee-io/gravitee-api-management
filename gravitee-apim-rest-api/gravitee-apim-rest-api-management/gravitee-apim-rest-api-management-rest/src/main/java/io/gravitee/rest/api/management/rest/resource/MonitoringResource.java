212a058acdb535ad289bdf5cc0960714013ca674
Florent CHAMFROY <florent.chamfroy@graviteesource.com>
2024-03-19 15:10:23 +0100

    fix: consistency checks on environment instances

--- gravitee-apim-rest-api/gravitee-apim-rest-api-management/gravitee-apim-rest-api-management-rest/src/main/java/io/gravitee/rest/api/management/rest/resource/MonitoringResource.java
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-management/gravitee-apim-rest-api-management-rest/src/main/java/io/gravitee/rest/api/management/rest/resource/MonitoringResource.java
@@ -16,18 +16,28 @@
 package io.gravitee.rest.api.management.rest.resource;
 
 import io.gravitee.common.http.MediaType;
+import io.gravitee.rest.api.model.InstanceEntity;
+import io.gravitee.rest.api.model.OrganizationEntity;
 import io.gravitee.rest.api.model.monitoring.MonitoringData;
 import io.gravitee.rest.api.model.permissions.RolePermission;
 import io.gravitee.rest.api.model.permissions.RolePermissionAction;
 import io.gravitee.rest.api.rest.annotation.Permission;
 import io.gravitee.rest.api.rest.annotation.Permissions;
+import io.gravitee.rest.api.service.InstanceService;
 import io.gravitee.rest.api.service.MonitoringService;
+import io.gravitee.rest.api.service.OrganizationService;
+import io.gravitee.rest.api.service.common.GraviteeContext;
+import io.gravitee.rest.api.service.exceptions.InstanceNotFoundException;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 /**
  * @author Azize ELAMRANI (azize.elamrani at graviteesource.com)
@@ -40,11 +50,43 @@ public class MonitoringResource extends AbstractResource {
     @Inject
     private MonitoringService monitoringService;
 
+    @Inject
+    private InstanceService instanceService;
+
+    @Inject
+    private OrganizationService organizationService;
+
+    @PathParam("instance")
+    @Parameter(name = "instance", hidden = true)
+    private String instance;
+
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Operation(summary = "Get monitoring metrics for a gateway instance")
     @Permissions({ @Permission(value = RolePermission.ENVIRONMENT_PLATFORM, acls = RolePermissionAction.READ) })
     public MonitoringData getInstanceMonitoring(@PathParam("gatewayId") String gatewayId) {
+        InstanceEntity instanceEntity = instanceService.findByEvent(GraviteeContext.getExecutionContext(), this.instance);
+        if (
+            !isInstanceAccessibleByEnv(instanceEntity.getEnvironments(), GraviteeContext.getCurrentEnvironment()) ||
+            !isInstanceAccessibleByOrga(instanceEntity.getOrganizationsHrids(), GraviteeContext.getCurrentOrganization())
+        ) {
+            throw new InstanceNotFoundException(instance);
+        }
         return monitoringService.findMonitoring(gatewayId);
+    }
+
+    private boolean isInstanceAccessibleByOrga(List<String> organizationsHrids, String currentOrganization) {
+        if (organizationsHrids == null || organizationsHrids.isEmpty()) {
+            return true;
+        }
+        return organizationService
+            .findByHrids(new HashSet<>(organizationsHrids))
+            .stream()
+            .map(OrganizationEntity::getId)
+            .anyMatch(id -> id.equalsIgnoreCase(currentOrganization));
+    }
+
+    private boolean isInstanceAccessibleByEnv(Set<String> environments, String currentEnvironment) {
+        return environments == null || environments.isEmpty() || environments.contains(currentEnvironment);
     }
 }
