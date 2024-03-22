b03798c839ee060bc77c3c014cdf0d032431a07e
Ouahid Khelifi <ouahid.khelifi@gmail.com>
2024-03-22 16:41:51 +0100

    fix: handle json mapping exception to throw only detail message without stack trace
    (cherry picked from commit 48ad60c635a5faee52c4b59312f2ab93d7d0851b)
    
    # Conflicts:
    #	gravitee-apim-rest-api/gravitee-apim-rest-api-management/gravitee-apim-rest-api-management-rest/src/main/java/io/gravitee/rest/api/management/rest/resource/GraviteeManagementApplication.java

---
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-management/gravitee-apim-rest-api-management-rest/src/main/java/io/gravitee/rest/api/management/rest/provider/JsonMappingExceptionMapper.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright © 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.rest.api.management.rest.provider;
+
+import com.fasterxml.jackson.databind.JsonMappingException;
+import io.gravitee.common.http.HttpStatusCode;
+import io.gravitee.rest.api.management.rest.model.ErrorEntity;
+import javax.annotation.Priority;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+@Priority(1)
+public class JsonMappingExceptionMapper extends AbstractExceptionMapper<JsonMappingException> {
+
+    @Override
+    public Response toResponse(JsonMappingException exception) {
+        return Response
+            .status(Response.Status.BAD_REQUEST)
+            .type(MediaType.APPLICATION_JSON_TYPE)
+            .entity(new ErrorEntity(exception.getOriginalMessage(), HttpStatusCode.BAD_REQUEST_400))
+            .build();
+    }
+}
