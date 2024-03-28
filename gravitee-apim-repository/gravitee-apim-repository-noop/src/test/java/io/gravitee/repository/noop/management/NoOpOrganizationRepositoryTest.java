9e3c3e8bce205213fb71826990ed0eb854619b42
Jeoffrey Haeyaert <jeoffrey.haeyaert@gmail.com>
2024-03-28 17:49:20 +0100

    fix: support default env and org on noop repository
    https://gravitee.atlassian.net/browse/GKO-51

--- gravitee-apim-repository/gravitee-apim-repository-noop/src/test/java/io/gravitee/repository/noop/management/NoOpOrganizationRepositoryTest.java
+++ gravitee-apim-repository/gravitee-apim-repository-noop/src/test/java/io/gravitee/repository/noop/management/NoOpOrganizationRepositoryTest.java
@@ -19,8 +19,11 @@ import static org.junit.Assert.*;
 
 import io.gravitee.repository.exceptions.TechnicalException;
 import io.gravitee.repository.management.api.OrganizationRepository;
+import io.gravitee.repository.management.model.Environment;
 import io.gravitee.repository.management.model.Organization;
 import io.gravitee.repository.noop.AbstractNoOpRepositoryTest;
+import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,17 +38,56 @@ public class NoOpOrganizationRepositoryTest extends AbstractNoOpRepositoryTest {
     private OrganizationRepository cut;
 
     @Test
-    public void count() throws TechnicalException {
-        Long count = cut.count();
+    public void should_return_default_organization_when_finding_by_default_organization_hrid() throws TechnicalException {
+        Set<Organization> organizations = cut.findByHrids(new HashSet<>(Organization.DEFAULT.getHrids()));
 
-        assertNull(count);
+        assertNotNull(organizations);
+        assertEquals(Set.of(Organization.DEFAULT), organizations);
     }
 
     @Test
-    public void findByHrids() throws TechnicalException {
-        Set<Organization> organizations = cut.findByHrids(Set.of("test_hird"));
+    public void should_return_empty_when_finding_by_an_organization_hrid_other_than_default() throws TechnicalException {
+        Set<Organization> organizations = cut.findByHrids(Set.of("test_org"));
 
         assertNotNull(organizations);
         assertTrue(organizations.isEmpty());
+    }
+
+    @Test
+    public void should_return_default_organization_when_finding_all() throws TechnicalException {
+        Set<Organization> organizations = cut.findAll();
+
+        assertNotNull(organizations);
+        assertEquals(Set.of(Organization.DEFAULT), organizations);
+    }
+
+    @Test
+    public void should_return_default_organization_when_finding_by_default_organization() throws TechnicalException {
+        Optional<Organization> organizationOpt = cut.findById(Organization.DEFAULT.getId());
+
+        assertNotNull(organizationOpt);
+        assertTrue(organizationOpt.isPresent());
+        assertEquals(Organization.DEFAULT, organizationOpt.get());
+    }
+
+    @Test
+    public void should_return_empty_when_finding_by_an_organization() throws TechnicalException {
+        Optional<Organization> organizationOpt = cut.findById("test_id");
+
+        assertNotNull(organizationOpt);
+        assertFalse(organizationOpt.isPresent());
+    }
+
+    @Test
+    public void should_return_empty_when_finding_cockpit_id() throws TechnicalException {
+        Optional<Organization> organization = cut.findByCockpitId("test_id");
+
+        assertNotNull(organization);
+        assertTrue(organization.isEmpty());
+    }
+
+    @Test
+    public void should_count_1_organization() throws TechnicalException {
+        assertEquals(1L, (long) cut.count());
     }
 }
