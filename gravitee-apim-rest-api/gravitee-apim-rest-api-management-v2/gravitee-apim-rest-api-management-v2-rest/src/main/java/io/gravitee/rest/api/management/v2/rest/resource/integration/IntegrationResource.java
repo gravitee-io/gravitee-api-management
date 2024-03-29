59e04119daeb3bd911adc3588e1af6960db7dfde
Julien Giovaresco <dev@giovaresco.fr>
2024-03-29 09:05:40 +0100

    chore: add a new endpoint to trigger Integration APIs ingestion
    For now the endpoint returns an hardcoded response that can be called by
    the frontend to parallelize work.

--- gravitee-apim-rest-api/gravitee-apim-rest-api-management-v2/gravitee-apim-rest-api-management-v2-rest/src/main/java/io/gravitee/rest/api/management/v2/rest/resource/integration/IntegrationResource.java
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-management-v2/gravitee-apim-rest-api-management-v2-rest/src/main/java/io/gravitee/rest/api/management/v2/rest/resource/integration/IntegrationResource.java
@@ -18,9 +18,13 @@ package io.gravitee.rest.api.management.v2.rest.resource.integration;
 import io.gravitee.apim.core.integration.use_case.GetIntegrationUseCase;
 import io.gravitee.common.http.MediaType;
 import io.gravitee.rest.api.management.v2.rest.mapper.IntegrationMapper;
+import io.gravitee.rest.api.management.v2.rest.model.IngestionStatus;
+import io.gravitee.rest.api.management.v2.rest.model.IntegrationIngestionResponse;
 import io.gravitee.rest.api.management.v2.rest.resource.AbstractResource;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.Response;
@@ -44,5 +48,12 @@ public class IntegrationResource extends AbstractResource {
             .integration();
 
         return Response.ok(IntegrationMapper.INSTANCE.map(integration)).build();
+    }
+
+    @POST
+    @Path("/_ingest")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response ingestApis(@PathParam("integrationId") String integrationId) {
+        return Response.ok(IntegrationIngestionResponse.builder().status(IngestionStatus.SUCCESS).build()).build();
     }
 }
