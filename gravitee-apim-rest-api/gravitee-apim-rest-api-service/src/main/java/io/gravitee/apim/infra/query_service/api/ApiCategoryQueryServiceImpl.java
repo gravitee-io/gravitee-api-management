286876785134ce0f20d98c39ea0e95316cb1f0f9
Julien Giovaresco <dev@giovaresco.fr>
2024-04-02 10:49:41 +0200

    chore: implement ApiCategoryQueryService

---
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/apim/infra/query_service/api/ApiCategoryQueryServiceImpl.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright © 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.apim.infra.query_service.api;
+
+import io.gravitee.apim.core.api.model.Api;
+import io.gravitee.apim.core.api.query_service.ApiCategoryQueryService;
+import io.gravitee.apim.core.exception.TechnicalDomainException;
+import io.gravitee.repository.exceptions.TechnicalException;
+import io.gravitee.repository.management.api.CategoryRepository;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.stream.Collectors;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class ApiCategoryQueryServiceImpl implements ApiCategoryQueryService {
+
+    private final CategoryRepository categoryRepository;
+
+    public ApiCategoryQueryServiceImpl(@Lazy CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @Override
+    public Collection<String> findApiCategoryKeys(Api api) {
+        try {
+            var categoriesIds = api.getCategories();
+            if (categoriesIds == null || categoriesIds.isEmpty()) {
+                return Collections.emptySet();
+            }
+
+            // Uncomment when https://gravitee.atlassian.net/browse/APIM-4437 is fixed
+            //            return categoryRepository
+            //                .findByEnvironmentIdAndIdIn(api.getEnvironmentId(), categoriesIds)
+            //                .stream()
+            //                .map(io.gravitee.repository.management.model.Category::getKey)
+            //                .collect(Collectors.toSet());
+
+            return categoryRepository
+                .findAllByEnvironment(api.getEnvironmentId())
+                .stream()
+                .filter(category -> categoriesIds.contains(category.getId()) || categoriesIds.contains(category.getKey()))
+                .map(io.gravitee.repository.management.model.Category::getKey)
+                .collect(Collectors.toSet());
+        } catch (TechnicalException e) {
+            throw new TechnicalDomainException("An error occurs while trying to find Categories for the api: " + api.getId(), e);
+        }
+    }
+}
---
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/test/java/io/gravitee/apim/infra/query_service/api/ApiCategoryQueryServiceImplTest.java
@@ -0,0 +1,140 @@
+/*
+ * Copyright © 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.apim.infra.query_service.api;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.when;
+
+import fixtures.core.model.ApiFixtures;
+import io.gravitee.apim.core.api.model.Api;
+import io.gravitee.apim.core.exception.TechnicalDomainException;
+import io.gravitee.repository.exceptions.TechnicalException;
+import io.gravitee.repository.management.api.CategoryRepository;
+import io.gravitee.repository.management.model.Category;
+import jakarta.validation.constraints.Null;
+import java.util.Set;
+import java.util.stream.Collectors;
+import lombok.SneakyThrows;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.NullSource;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class ApiCategoryQueryServiceImplTest {
+
+    private static final String CATEGORY_ID_2 = "category2";
+    private static final String CATEGORY_ID_1 = "category1";
+    private static final String ENVIRONMENT_ID = "environment-id";
+    private static final Api API = ApiFixtures
+        .aProxyApiV4()
+        .toBuilder()
+        .environmentId(ENVIRONMENT_ID)
+        .categories(Set.of(CATEGORY_ID_1, CATEGORY_ID_2))
+        .build();
+
+    @Mock
+    CategoryRepository categoryRepository;
+
+    ApiCategoryQueryServiceImpl service;
+
+    @BeforeEach
+    void setUp() {
+        service = new ApiCategoryQueryServiceImpl(categoryRepository);
+    }
+
+    @Nested
+    class FindApiCategoryKeys {
+
+        @Test
+        @SneakyThrows
+        void should_query_only_categories_of_given_api() {
+            // when
+            service.findApiCategoryKeys(API);
+
+            // then
+            verify(categoryRepository).findAllByEnvironment(ENVIRONMENT_ID);
+        }
+
+        @Test
+        @SneakyThrows
+        void should_return_category_keys() {
+            // given
+            // Uncomment when https://gravitee.atlassian.net/browse/APIM-4437 is fixed
+            //            when(categoryRepository.findByEnvironmentIdAndIdIn(any(), any()))
+            //                .thenAnswer(invocation -> {
+            //                    Set<String> categoryIds = invocation.getArgument(1);
+            //
+            //                    return categoryIds
+            //                        .stream()
+            //                        .map(categoryId -> Category.builder().id(categoryId).key("key-" + categoryId).build())
+            //                        .collect(Collectors.toSet());
+            //                });
+
+            when(categoryRepository.findAllByEnvironment(ENVIRONMENT_ID))
+                .thenAnswer(invocation -> {
+                    return Set.of(
+                        Category.builder().id(CATEGORY_ID_1).key("key-" + CATEGORY_ID_1).build(),
+                        Category.builder().id(CATEGORY_ID_2).key("key-" + CATEGORY_ID_2).build()
+                    );
+                });
+
+            // when
+            var categories = service.findApiCategoryKeys(API);
+
+            // then
+            Assertions.assertThat(categories).containsExactlyInAnyOrder("key-" + CATEGORY_ID_1, "key-" + CATEGORY_ID_2);
+        }
+
+        @ParameterizedTest
+        @NullSource
+        @EmptySource
+        @SneakyThrows
+        void should_return_empty_list_when_api_has_no_category_defined(Set<String> categoryIds) {
+            // when
+            var categories = service.findApiCategoryKeys(API.toBuilder().categories(categoryIds).build());
+
+            // then
+            Assertions.assertThat(categories).isEmpty();
+            verifyNoInteractions(categoryRepository);
+        }
+
+        @Test
+        @SneakyThrows
+        void should_throw_when_fail_to_fetch_categories() {
+            // given
+            when(categoryRepository.findAllByEnvironment(any())).thenThrow(new TechnicalException("error"));
+
+            Throwable throwable = catchThrowable(() -> service.findApiCategoryKeys(API));
+
+            assertThat(throwable)
+                .isInstanceOf(TechnicalDomainException.class)
+                .hasMessageContaining("An error occurs while trying to find Categories for the api: " + API.getId())
+                .hasCauseInstanceOf(TechnicalException.class)
+                .hasRootCauseMessage("error");
+        }
+    }
+}
