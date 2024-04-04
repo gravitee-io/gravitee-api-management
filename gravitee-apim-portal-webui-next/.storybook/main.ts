31a9c1bb8ff43f7ae07e72cef3fba87fa04573f2
“Jan <jan.sobczynski@graviteesource.com>
2024-04-04 11:22:45 +0200

    feat(portal-next): initialize storybook

---
+++ gravitee-apim-portal-webui-next/.storybook/main.ts
@@ -0,0 +1,14 @@
+import type { StorybookConfig } from '@storybook/angular';
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@chromatic-com/storybook', '@storybook/addon-interactions'],
+  framework: {
+    name: '@storybook/angular',
+    options: {},
+  },
+  docs: {
+    autodocs: 'tag',
+  },
+};
+export default config;
