115beae738914bda75957b2c2d7a79b2eab023ce
ytvnr <yann.tavernier@graviteesource.com>
2024-04-10 16:17:38 +0200

    test(sdk): introduce connection latency mock endpoint connector - poll delays to apply from configuration connectionLatencies field - if Deque is empty or no more element, then apply a delay of 0 - add a Mock-Connection-Attempts to the response's headers with the counter of connection attempts https://gravitee.atlassian.net/browse/APIM-4209

---
+++ gravitee-apim-gateway/gravitee-apim-gateway-tests-sdk/src/main/java/io/gravitee/apim/gateway/tests/sdk/connector/fakes/ConnectionLatencyMockEndpointConnector.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright © 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.apim.gateway.tests.sdk.connector.fakes;
+
+import io.gravitee.gateway.reactive.api.context.ExecutionContext;
+import io.gravitee.plugin.endpoint.mock.MockEndpointConnector;
+import io.reactivex.rxjava3.core.Completable;
+import io.reactivex.rxjava3.core.Observable;
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * This endpoint allows to apply some latency during the connection to the endpoint
+ * For repeatability, different latencies can be provided through a Deque in the configuration: connectionLatencies.
+ * The connector will apply a delay taking the first element of the Deque, or 0 if no element.
+ * @author Yann TAVERNIER (yann.tavernier at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+@Getter
+@Setter
+public class ConnectionLatencyMockEndpointConnector extends MockEndpointConnector {
+
+    public static final String MOCK_CONNECTION_ATTEMPTS = "Mock-Connection-Attempts";
+    final ConnectionLatencyMockEndpointConnectorConfiguration latencyConfiguration;
+    final AtomicInteger connectAttempts = new AtomicInteger(0);
+
+    public ConnectionLatencyMockEndpointConnector(ConnectionLatencyMockEndpointConnectorConfiguration latencyConfiguration) {
+        super(latencyConfiguration);
+        this.latencyConfiguration = latencyConfiguration;
+    }
+
+    @Override
+    public Completable connect(ExecutionContext ctx) {
+        Long latency = Optional.ofNullable(latencyConfiguration.connectionLatencies.pollFirst()).orElse(0L);
+        // Return the connection attempts counter as a header to simplify the assertions
+        ctx.response().headers().set(MOCK_CONNECTION_ATTEMPTS, Integer.toString(connectAttempts.incrementAndGet()));
+        return Observable.just(new Object()).delay(latency, TimeUnit.MILLISECONDS).ignoreElements().andThen(super.connect(ctx));
+    }
+}
---
+++ gravitee-apim-gateway/gravitee-apim-gateway-tests-sdk/src/main/java/io/gravitee/apim/gateway/tests/sdk/connector/fakes/ConnectionLatencyMockEndpointConnectorFactory.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright © 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.apim.gateway.tests.sdk.connector.fakes;
+
+import io.gravitee.gateway.reactive.api.ConnectorMode;
+import io.gravitee.gateway.reactive.api.connector.endpoint.async.EndpointAsyncConnectorFactory;
+import io.gravitee.gateway.reactive.api.context.DeploymentContext;
+import io.gravitee.gateway.reactive.api.exception.PluginConfigurationException;
+import io.gravitee.gateway.reactive.api.helper.PluginConfigurationHelper;
+import io.gravitee.gateway.reactive.api.qos.Qos;
+import java.util.Set;
+
+/**
+ * @author Yann TAVERNIER (yann.tavernier at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+public class ConnectionLatencyMockEndpointConnectorFactory
+    implements EndpointAsyncConnectorFactory<ConnectionLatencyMockEndpointConnector> {
+
+    private final PluginConfigurationHelper pluginConfigurationHelper;
+
+    public ConnectionLatencyMockEndpointConnectorFactory(PluginConfigurationHelper pluginConfigurationHelper) {
+        this.pluginConfigurationHelper = pluginConfigurationHelper;
+    }
+
+    @Override
+    public Set<ConnectorMode> supportedModes() {
+        return Set.of(ConnectorMode.PUBLISH, ConnectorMode.SUBSCRIBE);
+    }
+
+    @Override
+    public Set<Qos> supportedQos() {
+        return Set.of(Qos.NONE, Qos.AUTO, Qos.AT_LEAST_ONCE, Qos.AT_MOST_ONCE);
+    }
+
+    @Override
+    public ConnectionLatencyMockEndpointConnector createConnector(
+        DeploymentContext deploymentContext,
+        String configuration,
+        String sharedConfiguration
+    ) {
+        try {
+            return new ConnectionLatencyMockEndpointConnector(
+                pluginConfigurationHelper.readConfiguration(ConnectionLatencyMockEndpointConnectorConfiguration.class, configuration)
+            );
+        } catch (PluginConfigurationException e) {
+            return null;
+        }
+    }
+}
