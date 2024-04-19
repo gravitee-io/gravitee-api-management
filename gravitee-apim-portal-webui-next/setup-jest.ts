9150ab9e0da4b6f018104aeb6fb643c0a0f35dc5
jourdiw <jourdi.waller@graviteesource.com>
2024-04-19 17:46:59 +0200

    feat(portal-next): add swagger ui as openapi renderer

--- gravitee-apim-portal-webui-next/setup-jest.ts
+++ gravitee-apim-portal-webui-next/setup-jest.ts
@@ -1,2 +1,6 @@
 import 'jest-preset-angular/setup-jest';
 import '@angular/localize/init';
+
+// mocking swagger-ui for tests as it contains some JS incompatible with Jest
+// This means we will not be able to test Swagger in our components tests
+jest.mock('swagger-ui', () => jest.fn(() => ({ initOAuth: () => jest.fn() })));
---
+++ gravitee-apim-portal-webui-next/src/components/page/page-swagger/page-swagger.component.ts
@@ -0,0 +1,127 @@
+/*
+ * Copyright (C) 2024 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { PlatformLocation } from '@angular/common';
+import { Component, inject, Input, OnInit } from '@angular/core';
+import SwaggerUI, { SwaggerUIOptions, SwaggerUIPlugin } from 'swagger-ui';
+
+import { readYaml } from '../../../app/helpers/yaml-parser';
+import { Page } from '../../../entities/page/page';
+import { CurrentUserService } from '../../../services/current-user.service';
+
+@Component({
+  selector: 'app-page-swagger',
+  standalone: true,
+  template: `<div id="swagger"></div>`,
+})
+export class PageSwaggerComponent implements OnInit {
+  @Input() page!: Page;
+  private platformLocation: PlatformLocation = inject(PlatformLocation);
+
+  constructor(private currentUser: CurrentUserService) {}
+
+  ngOnInit() {
+    SwaggerUI({
+      domNode: document.getElementById('swagger'),
+      spec: this.readSpec() ?? '',
+      ...this.buildConfig(),
+    }).initOAuth({ usePkceWithAuthorizationCodeGrant: this.page.configuration?.use_pkce });
+  }
+
+  private buildConfig(): SwaggerUIOptions {
+    const plugins = this.buildPlugins();
+
+    const config: SwaggerUIOptions = {
+      defaultModelsExpandDepth: 0,
+      layout: 'BaseLayout',
+      plugins,
+      requestInterceptor: req => {
+        if (req['loadSpec']) {
+          req['credentials'] = 'include';
+        }
+        return req;
+      },
+      oauth2RedirectUrl: `${window.location.origin}${this.platformLocation.getBaseHrefFromDOM()}oauth2-redirect.html`,
+    };
+
+    if (this.page.configuration) {
+      const pageConfiguration = this.page.configuration;
+      if (pageConfiguration.show_url) {
+        config.url = this.page._links?.content;
+        config.spec = undefined;
+      }
+      config.docExpansion = this.page.configuration?.doc_expansion ?? 'none';
+      config.displayOperationId = pageConfiguration.display_operation_id || false;
+      config.filter = pageConfiguration.enable_filtering || false;
+      config.showExtensions = pageConfiguration.show_extensions || false;
+      config.showCommonExtensions = pageConfiguration.show_common_extensions || false;
+      config.maxDisplayedTags =
+        pageConfiguration.max_displayed_tags && pageConfiguration.max_displayed_tags >= 0
+          ? pageConfiguration.max_displayed_tags
+          : undefined;
+    }
+
+    return config;
+  }
+
+  private buildPlugins(): SwaggerUIPlugin[] {
+    const plugins: SwaggerUIPlugin[] = [];
+    if (!this.isTryItEnabled()) {
+      plugins.push(this.disabledTryItOutPlugin);
+      plugins.push(this.disabledAuthorizationPlugin);
+    }
+    return plugins;
+  }
+
+  private readSpec(): { [propName: string]: unknown } | unknown {
+    const content = this.page.content;
+    if (!content) {
+      return undefined;
+    }
+    try {
+      return JSON.parse(content);
+    } catch (_) {
+      return readYaml(content);
+    }
+  }
+
+  private disabledTryItOutPlugin() {
+    return {
+      statePlugins: {
+        spec: {
+          wrapSelectors: {
+            allowTryItOutFor: () => () => false,
+          },
+        },
+      },
+    };
+  }
+
+  private disabledAuthorizationPlugin() {
+    return {
+      wrapComponents: {
+        authorizeBtn: () => () => null,
+      },
+    };
+  }
+
+  private isTryItEnabled() {
+    return this.page.configuration?.try_it && this.isTryItAllowed();
+  }
+
+  private isTryItAllowed() {
+    return this.currentUser.isAuthenticated() || this.page.configuration?.try_it_anonymous;
+  }
+}
