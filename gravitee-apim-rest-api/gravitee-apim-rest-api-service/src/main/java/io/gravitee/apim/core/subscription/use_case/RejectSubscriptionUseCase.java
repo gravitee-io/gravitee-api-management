2f680aef53b943dbb31d858dfe6a334d10f7eeb8
Julien Giovaresco <dev@giovaresco.fr>
2024-04-23 09:16:52 +0200

    refactor: implement RejectSubscriptionUseCase

---
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/apim/core/subscription/use_case/RejectSubscriptionUseCase.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright © 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.apim.core.subscription.use_case;
+
+import io.gravitee.apim.core.UseCase;
+import io.gravitee.apim.core.audit.model.AuditInfo;
+import io.gravitee.apim.core.subscription.crud_service.SubscriptionCrudService;
+import io.gravitee.apim.core.subscription.domain_service.RejectSubscriptionDomainService;
+import io.gravitee.apim.core.subscription.model.SubscriptionEntity;
+import io.gravitee.rest.api.service.exceptions.SubscriptionNotFoundException;
+import lombok.Builder;
+
+@UseCase
+public class RejectSubscriptionUseCase {
+
+    private final SubscriptionCrudService subscriptionCrudService;
+    private final RejectSubscriptionDomainService rejectSubscriptionDomainService;
+
+    public RejectSubscriptionUseCase(
+        SubscriptionCrudService subscriptionCrudService,
+        RejectSubscriptionDomainService rejectSubscriptionDomainService
+    ) {
+        this.subscriptionCrudService = subscriptionCrudService;
+        this.rejectSubscriptionDomainService = rejectSubscriptionDomainService;
+    }
+
+    public Output execute(Input input) {
+        var subscription = subscriptionCrudService.get(input.subscriptionId);
+        if (!input.apiId.equalsIgnoreCase(subscription.getApiId())) {
+            throw new SubscriptionNotFoundException(input.subscriptionId);
+        }
+
+        return new Output(rejectSubscriptionDomainService.reject(subscription, input.reasonMessage, input.auditInfo));
+    }
+
+    @Builder
+    public record Input(String apiId, String subscriptionId, String reasonMessage, AuditInfo auditInfo) {}
+
+    public record Output(SubscriptionEntity subscription) {}
+}
