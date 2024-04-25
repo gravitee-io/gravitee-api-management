a2e200c48819a7c4b0f45469a6d77e3545af94fa
jourdiw <jourdi.waller@graviteesource.com>
2024-04-25 16:13:04 +0200

    feat(portal-next): create asciidoc page component

--- gravitee-apim-portal-webui-next/jest.config.js
+++ gravitee-apim-portal-webui-next/jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  transformIgnorePatterns: ['/node_modules/(?!(.*\\.mjs$)|(@asciidoctor/.*?\\.js)$)'],
 };
