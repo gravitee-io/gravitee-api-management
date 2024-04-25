c97d3e8f92da261b3346e470a519dfffffda74cd
ytvnr <yann.tavernier@graviteesource.com>
2024-04-25 10:08:34 +0200

    feat(usecase): get total number of requests for v4 analytics https://gravitee.atlassian.net/browse/APIM-4211

---
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/apim/core/analytics/use_case/SearchRequestsCountAnalyticsUseCase.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright © 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.apim.core.analytics.use_case;
+
+import io.gravitee.apim.core.UseCase;
+import io.gravitee.apim.core.analytics.query_service.AnalyticsQueryService;
+import io.gravitee.apim.core.api.crud_service.ApiCrudService;
+import io.gravitee.apim.core.api.exception.ApiInvalidDefinitionVersionException;
+import io.gravitee.apim.core.api.exception.ApiNotFoundException;
+import io.gravitee.apim.core.api.model.Api;
+import io.gravitee.definition.model.DefinitionVersion;
+import io.gravitee.rest.api.model.v4.analytics.RequestsCount;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+@UseCase
+public class SearchRequestsCountAnalyticsUseCase {
+
+    private final AnalyticsQueryService analyticsQueryService;
+    private final ApiCrudService apiCrudService;
+
+    public Output execute(Input input) {
+        validateApiRequirements(input);
+
+        // Verify v4 api
+        return analyticsQueryService.searchRequestsCount(input.apiId()).map(Output::new).orElse(new Output());
+    }
+
+    private void validateApiRequirements(Input input) {
+        final Api api = apiCrudService.get(input.apiId);
+        validateApiDefinitionVersion(api.getDefinitionVersion(), input.apiId);
+        validateApiMultiTenancyAccess(api, input.environmentId);
+    }
+
+    private static void validateApiMultiTenancyAccess(Api api, String environmentId) {
+        if (!api.belongsToEnvironment(environmentId)) {
+            throw new ApiNotFoundException(api.getId());
+        }
+    }
+
+    private static void validateApiDefinitionVersion(DefinitionVersion definitionVersion, String apiId) {
+        if (!DefinitionVersion.V4.equals(definitionVersion)) {
+            throw new ApiInvalidDefinitionVersionException(apiId);
+        }
+    }
+
+    public record Input(String apiId, String environmentId) {}
+
+    public record Output(Optional<RequestsCount> requestsCount) {
+        Output(RequestsCount requestsCount) {
+            this(Optional.of(requestsCount));
+        }
+
+        Output() {
+            this(Optional.empty());
+        }
+    }
+}
