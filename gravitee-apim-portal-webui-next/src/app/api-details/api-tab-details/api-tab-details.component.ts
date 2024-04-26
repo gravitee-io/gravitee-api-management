8f11be0c566dc3c261897dd1b96b219d5875f192
jourdiw <jourdi.waller@graviteesource.com>
2024-04-26 11:08:37 +0200

    feat(portal-next): handle error + display loader for homepage in api details

--- gravitee-apim-portal-webui-next/src/app/api-details/api-tab-details/api-tab-details.component.ts
+++ gravitee-apim-portal-webui-next/src/app/api-details/api-tab-details/api-tab-details.component.ts
@@ -15,18 +15,24 @@
  */
 import { AsyncPipe, DatePipe } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
-import { Observable, of, switchMap } from 'rxjs';
+import { catchError, map, Observable, of, switchMap } from 'rxjs';
 
+import { LoaderComponent } from '../../../components/loader/loader.component';
 import { PageComponent } from '../../../components/page/page.component';
 import { MarkdownDescriptionPipe } from '../../../components/pipe/markdown-description.pipe';
 import { Api } from '../../../entities/api/api';
 import { Page } from '../../../entities/page/page';
 import { PageService } from '../../../services/page.service';
 
+interface HomepageData {
+  result?: Page;
+  error?: unknown;
+}
+
 @Component({
   selector: 'app-api-tab-details',
   standalone: true,
-  imports: [MarkdownDescriptionPipe, AsyncPipe, DatePipe, PageComponent],
+  imports: [MarkdownDescriptionPipe, AsyncPipe, DatePipe, PageComponent, LoaderComponent],
   templateUrl: './api-tab-details.component.html',
   styleUrl: './api-tab-details.component.scss',
 })
@@ -35,17 +41,20 @@ export class ApiTabDetailsComponent implements OnInit {
   api!: Api;
   @Input()
   pages!: Page[];
-  homepage$: Observable<Page> = of();
+  homepageData$: Observable<HomepageData> = of();
 
   constructor(private pageService: PageService) {}
 
   ngOnInit(): void {
-    this.homepage$ = this.pageService.listByApiId(this.api.id, true).pipe(
+    this.homepageData$ = this.pageService.listByApiId(this.api.id, true).pipe(
       switchMap(pageResponse => {
         if (pageResponse.data?.length) {
-          return this.pageService.content(this.api.id, pageResponse.data[0].id);
+          return this.pageService.content(this.api.id, pageResponse.data[0].id).pipe(
+            map(result => ({ result })),
+            catchError(error => of({ error })),
+          );
         } else {
-          return of();
+          return of({});
         }
       }),
     );
