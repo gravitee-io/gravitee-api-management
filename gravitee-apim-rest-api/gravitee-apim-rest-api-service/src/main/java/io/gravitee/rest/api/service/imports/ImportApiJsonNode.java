b7f537cfc9b24d987b37fa84c683b99c46f23d80
Kamiel Ahmadpour <kamiel.ahmadpour@graviteesource.com>
2024-05-07 14:39:54 +0100

    fix: allow removing members from an API see: https://gravitee.atlassian.net/browse/GKO-7
    (cherry picked from commit fc9a52d74384b9bed60077f44b0d0c729e992b3c)

--- gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/rest/api/service/imports/ImportApiJsonNode.java
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/rest/api/service/imports/ImportApiJsonNode.java
@@ -19,7 +19,7 @@ import static java.util.stream.Collectors.toList;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ArrayNode;
-import io.swagger.util.Json;
+import io.gravitee.definition.model.DefinitionContext;
 import java.util.List;
 
 public class ImportApiJsonNode extends ImportJsonNodeWithIds {
@@ -60,6 +60,10 @@ public class ImportApiJsonNode extends ImportJsonNodeWithIds {
 
     public String getDefinitionContextOrigin() {
         return getJsonNode().findPath(DEFINITION_CONTEXT).findPath(DEFINITION_CONTEXT_ORIGIN).asText();
+    }
+
+    public boolean isKubernetesOrigin() {
+        return DefinitionContext.ORIGIN_KUBERNETES.equalsIgnoreCase(getDefinitionContextOrigin());
     }
 
     public List<ImportJsonNodeWithIds> getPages() {
