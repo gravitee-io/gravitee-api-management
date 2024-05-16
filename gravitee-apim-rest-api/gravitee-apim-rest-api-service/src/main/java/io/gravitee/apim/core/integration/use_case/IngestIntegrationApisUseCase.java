4c72b2943ceb8c7779ca2a6dad38aa287fece7e2
Julien Giovaresco <dev@giovaresco.fr>
2024-05-16 09:49:10 +0200

    Revert "feat: check license before ingestion federated API"
    This reverts commit 9c4ce7d8492cbb38d809e8b13cf03df025eaf6d0.

--- gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/apim/core/integration/use_case/IngestIntegrationApisUseCase.java
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/apim/core/integration/use_case/IngestIntegrationApisUseCase.java
@@ -24,12 +24,10 @@ import io.gravitee.apim.core.api.model.factory.ApiModelFactory;
 import io.gravitee.apim.core.audit.model.AuditInfo;
 import io.gravitee.apim.core.documentation.domain_service.CreateApiDocumentationDomainService;
 import io.gravitee.apim.core.documentation.model.Page;
-import io.gravitee.apim.core.exception.NotAllowedDomainException;
 import io.gravitee.apim.core.integration.crud_service.IntegrationCrudService;
 import io.gravitee.apim.core.integration.exception.IntegrationNotFoundException;
 import io.gravitee.apim.core.integration.model.IntegrationApi;
 import io.gravitee.apim.core.integration.service_provider.IntegrationAgent;
-import io.gravitee.apim.core.license.domain_service.LicenseDomainService;
 import io.gravitee.apim.core.membership.domain_service.ApiPrimaryOwnerFactory;
 import io.gravitee.apim.core.membership.model.PrimaryOwnerEntity;
 import io.gravitee.apim.core.plan.domain_service.CreatePlanDomainService;
@@ -41,7 +39,6 @@ import io.gravitee.definition.model.v4.plan.PlanStatus;
 import io.gravitee.rest.api.model.v4.plan.PlanSecurityType;
 import io.gravitee.rest.api.service.common.UuidString;
 import io.reactivex.rxjava3.core.Completable;
-import io.reactivex.rxjava3.core.Maybe;
 import io.reactivex.rxjava3.core.Single;
 import java.util.Collections;
 import java.util.Date;
@@ -61,7 +58,6 @@ public class IngestIntegrationApisUseCase {
     private final CreatePlanDomainService createPlanDomainService;
     private final IntegrationAgent integrationAgent;
     private final CreateApiDocumentationDomainService createApiDocumentationDomainService;
-    private final LicenseDomainService licenseDomainService;
 
     public IngestIntegrationApisUseCase(
         IntegrationCrudService integrationCrudService,
@@ -71,8 +67,7 @@ public class IngestIntegrationApisUseCase {
         CreateApiDomainService createApiDomainService,
         CreatePlanDomainService createPlanDomainService,
         IntegrationAgent integrationAgent,
-        CreateApiDocumentationDomainService createApiDocumentationDomainService,
-        LicenseDomainService licenseDomainService
+        CreateApiDocumentationDomainService createApiDocumentationDomainService
     ) {
         this.integrationCrudService = integrationCrudService;
         this.apiPrimaryOwnerFactory = apiPrimaryOwnerFactory;
@@ -82,7 +77,6 @@ public class IngestIntegrationApisUseCase {
         this.createPlanDomainService = createPlanDomainService;
         this.integrationAgent = integrationAgent;
         this.createApiDocumentationDomainService = createApiDocumentationDomainService;
-        this.licenseDomainService = licenseDomainService;
     }
 
     public Completable execute(Input input) {
@@ -91,16 +85,12 @@ public class IngestIntegrationApisUseCase {
         var organizationId = auditInfo.organizationId();
         var environmentId = auditInfo.environmentId();
 
-        return Maybe
-            .fromOptional(licenseDomainService.getLicenseByOrganizationId(organizationId))
-            .switchIfEmpty(Maybe.error(NotAllowedDomainException.noLicenseForFederation()))
-            .flatMapSingle(license ->
-                Single.fromCallable(() ->
-                    integrationCrudService
-                        .findById(integrationId)
-                        .filter(integration -> integration.getEnvironmentId().equals(environmentId))
-                        .orElseThrow(() -> new IntegrationNotFoundException(integrationId))
-                )
+        return Single
+            .fromCallable(() ->
+                integrationCrudService
+                    .findById(integrationId)
+                    .filter(integration -> integration.getEnvironmentId().equals(environmentId))
+                    .orElseThrow(() -> new IntegrationNotFoundException(integrationId))
             )
             .flatMapPublisher(integration -> {
                 var primaryOwner = apiPrimaryOwnerFactory.createForNewApi(organizationId, environmentId, input.auditInfo.actor().userId());
