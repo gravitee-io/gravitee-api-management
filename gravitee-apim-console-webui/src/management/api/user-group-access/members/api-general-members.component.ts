2f8c247f4fea95dd8868bf74222cd657c981c859
Kamiel Ahmadpour <kamiel.ahmadpour@graviteesource.com>
2024-05-20 15:14:07 +0100

    fix: make members read-only see: https://gravitee.atlassian.net/browse/GKO-214
    (cherry picked from commit d29513cb3cedbe1628fc8a74313081a24c84315f)

--- gravitee-apim-console-webui/src/management/api/user-group-access/members/api-general-members.component.ts
+++ gravitee-apim-console-webui/src/management/api/user-group-access/members/api-general-members.component.ts
@@ -218,6 +218,7 @@ export class ApiGeneralMembersComponent implements OnInit {
         data: {
           api: this.api,
           groups: this.groups,
+          isKubernetesOrigin: this.isKubernetesOrigin,
         },
       })
       .afterClosed()
@@ -226,7 +227,7 @@ export class ApiGeneralMembersComponent implements OnInit {
           return combineLatest([of(apiDialogResult), this.apiService.get(this.activatedRoute.snapshot.params.apiId)]);
         }),
         switchMap(([apiDialogResult, api]) => {
-          return api.definitionVersion === 'V1' || this.isKubernetesOrigin
+          return api.definitionVersion === 'V1'
             ? throwError({ message: `You cannot modify this API. version ${api.definitionVersion}.` })
             : this.apiService.update(api.id, { ...api, groups: apiDialogResult?.groups });
         }),
