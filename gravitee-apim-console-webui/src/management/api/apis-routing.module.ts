d1fa313e12782683b769134d2fac3603930c40b2
ThibaudAv <thibaud.avenier@gmail.com>
2024-05-28 10:55:12 +0200

    feat(console): impl "HTTP Proxy Entrypoint - Request Stats" card for Proxy V4 API

--- gravitee-apim-console-webui/src/management/api/apis-routing.module.ts
+++ gravitee-apim-console-webui/src/management/api/apis-routing.module.ts
@@ -82,7 +82,7 @@ import { ApiDocumentationV4MetadataComponent } from './documentation-v4/document
 import { ApiHistoryV4Component } from './history-v4/api-history-v4.component';
 import { ApiFailoverV4Component } from './failover-v4/api-failover-v4.component';
 import { ApiImportV4Component } from './import-v4/api-import-v4.component';
-import { ApiAnalyticsComponent } from './api-traffic-v4/analytics/api-analytics.component';
+import { ApiAnalyticsProxyComponent } from './api-traffic-v4/analytics/api-analytics-proxy.component';
 
 import { DocumentationManagementComponent } from '../../components/documentation/documentation-management.component';
 import { DocumentationNewPageComponent } from '../../components/documentation/new-page.component';
@@ -868,7 +868,7 @@ const apisRoutes: Routes = [
             anyOf: ['api-analytics-r'],
           },
         },
-        component: ApiAnalyticsComponent,
+        component: ApiAnalyticsProxyComponent,
       },
       {
         path: 'v4/runtime-logs',
