457fd796161b3666f4ecf13b95666bff2f278bfb
ThibaudAv <thibaud.avenier@gmail.com>
2024-06-03 10:40:45 +0200

    feat(console): display entrypoints with analytics but not configured at API lvl
    (cherry picked from commit 169997984adfe3fd88b20c6202b010e03565d18b)

--- gravitee-apim-console-webui/src/management/api/api-traffic-v4/analytics/api-analytics-message/api-analytics-message.component.harness.ts
+++ gravitee-apim-console-webui/src/management/api/api-traffic-v4/analytics/api-analytics-message/api-analytics-message.component.harness.ts
@@ -13,7 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { ComponentHarness } from '@angular/cdk/testing';
+import { ComponentHarness, parallel } from '@angular/cdk/testing';
+import { DivHarness } from '@gravitee/ui-particles-angular/testing';
 
 import { ApiAnalyticsRequestStatsHarness } from '../components/api-analytics-requests-stats/api-analytics-request-stats.component.harness';
 import { ApiAnalyticsFiltersBarHarness } from '../components/api-analytics-filters-bar/api-analytics-filters-bar.component.harness';
@@ -23,8 +24,11 @@ export class ApiAnalyticsMessageHarness extends ComponentHarness {
 
   protected emptyPanelHarness = this.locatorForOptional('gio-card-empty-state');
   protected loaderElement = this.locatorForOptional('.loader gio-loader');
+
   protected requestStats = (testId: string) =>
     this.locatorForOptional(ApiAnalyticsRequestStatsHarness.with({ selector: `[data-testId="${testId}"]` }));
+
+  protected entrypointsRows = this.locatorForAll(DivHarness.with({ selector: `.entrypoints__rows__row` }));
 
   getFiltersBarHarness = this.locatorForOptional(ApiAnalyticsFiltersBarHarness);
 
@@ -38,5 +42,20 @@ export class ApiAnalyticsMessageHarness extends ComponentHarness {
 
   async getRequestStatsHarness(testId: string): Promise<ApiAnalyticsRequestStatsHarness> {
     return this.requestStats(testId)();
+  }
+
+  async getEntrypointRow(): Promise<
+    {
+      name: string;
+      isNotConfigured: boolean;
+    }[]
+  > {
+    const rows = await this.entrypointsRows();
+    return parallel(() =>
+      rows.map(async (row) => ({
+        name: await row.getText({ childSelector: '.gio-badge-primary' }),
+        isNotConfigured: (await row.getText()).includes('Not configured'),
+      })),
+    );
   }
 }
