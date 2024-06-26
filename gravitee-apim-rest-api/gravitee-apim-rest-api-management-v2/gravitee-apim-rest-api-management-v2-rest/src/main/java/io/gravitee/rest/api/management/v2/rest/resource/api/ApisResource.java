7f57abebc76c628fbebbbdd62c1ec1f634cd19e5
Ouahid Khelifi <ouahid.khelifi@gmail.com>
2024-06-26 18:38:32 +0200

    fix: map primary owner using expand in api service

--- gravitee-apim-rest-api/gravitee-apim-rest-api-management-v2/gravitee-apim-rest-api-management-v2-rest/src/main/java/io/gravitee/rest/api/management/v2/rest/resource/api/ApisResource.java
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-management-v2/gravitee-apim-rest-api-management-v2-rest/src/main/java/io/gravitee/rest/api/management/v2/rest/resource/api/ApisResource.java
@@ -110,6 +110,7 @@ public class ApisResource extends AbstractResource {
             GraviteeContext.getExecutionContext(),
             getAuthenticatedUser(),
             isAdmin(),
+            expands,
             paginationParam.toPageable()
         );
 
