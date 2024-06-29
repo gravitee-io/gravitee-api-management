fdc5d59e435e41994377f49b009836b4cd4b0404
Florent CHAMFROY <florent.chamfroy@graviteesource.com>
2024-06-29 08:15:48 +0200

    refactor: remove old webui build job

--- .circleci/ci/src/jobs/frontend/index.ts
+++ .circleci/ci/src/jobs/frontend/index.ts
@@ -18,6 +18,5 @@ export * from './job-console-webui-build';
 export * from './job-storybook-console';
 export * from './job-portal-webui-build';
 export * from './job-portal-webui-next-build';
-export * from './job-webui-build';
 export * from './job-webui-lint-test';
 export * from './job-webui-publish-artifactory';
