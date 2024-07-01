98cbb1c63816bad1768e98af102052a86aa2c02c
Florent CHAMFROY <florent.chamfroy@graviteesource.com>
2024-07-01 16:29:30 +0200

    ci: add a "run e2e" dedicated workflow
    (cherry picked from commit d874656bb9f9ea093ed0b6e3959ae76935e947bc)

---
+++ .circleci/ci/src/workflows/workflow-run-e2e-tests.ts
@@ -0,0 +1,112 @@
+/*
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { Config, workflow, Workflow } from '@circleci/circleci-config-sdk';
+import {
+  BuildBackendImagesJob,
+  BuildBackendJob,
+  E2ECypressJob,
+  E2EGenerateSDKJob,
+  E2ELintBuildJob,
+  E2ETestJob,
+  SetupJob,
+  WebuiBuildJob,
+} from '../jobs';
+import { config } from '../config';
+import { CircleCIEnvironment } from '../pipelines';
+
+export class RunE2ETestsWorkflow {
+  static create(dynamicConfig: Config, environment: CircleCIEnvironment) {
+    const setupJob = SetupJob.create(dynamicConfig);
+    dynamicConfig.addJob(setupJob);
+
+    const buildBackendJob = BuildBackendJob.create(dynamicConfig, environment);
+    dynamicConfig.addJob(buildBackendJob);
+
+    const buildBackendImagesJob = BuildBackendImagesJob.create(dynamicConfig, environment);
+    dynamicConfig.addJob(buildBackendImagesJob);
+
+    const webuiBuildJob = WebuiBuildJob.create(dynamicConfig, environment);
+    dynamicConfig.addJob(webuiBuildJob);
+
+    const e2eGenerateSdkJob = E2EGenerateSDKJob.create(dynamicConfig);
+    dynamicConfig.addJob(e2eGenerateSdkJob);
+
+    const e2eLintBuildJob = E2ELintBuildJob.create(dynamicConfig);
+    dynamicConfig.addJob(e2eLintBuildJob);
+
+    const e2eTestJob = E2ETestJob.create(dynamicConfig, environment);
+    dynamicConfig.addJob(e2eTestJob);
+
+    const e2eCypressJob = E2ECypressJob.create(dynamicConfig, environment);
+    dynamicConfig.addJob(e2eCypressJob);
+
+    const jobs = [
+      new workflow.WorkflowJob(setupJob, { context: config.jobContext, name: 'Setup' }),
+      new workflow.WorkflowJob(buildBackendJob, { context: config.jobContext, requires: ['Setup'], name: 'Build backend' }),
+      new workflow.WorkflowJob(buildBackendImagesJob, {
+        context: config.jobContext,
+        requires: ['Build backend'],
+        name: 'Build and push rest api and gateway images',
+      }),
+      new workflow.WorkflowJob(e2eGenerateSdkJob, {
+        context: config.jobContext,
+        requires: ['Build backend'],
+        name: 'Generate e2e tests SDK',
+      }),
+      new workflow.WorkflowJob(e2eLintBuildJob, {
+        context: config.jobContext,
+        requires: ['Generate e2e tests SDK'],
+        name: 'Lint & Build APIM e2e',
+      }),
+      new workflow.WorkflowJob(webuiBuildJob, {
+        context: config.jobContext,
+        requires: ['Setup'],
+        name: 'Build APIM Console and publish image',
+        'apim-ui-project': config.dockerImages.console.project,
+        'docker-image-name': config.dockerImages.console.image,
+      }),
+      new workflow.WorkflowJob(webuiBuildJob, {
+        context: config.jobContext,
+        requires: ['Setup'],
+        name: 'Build APIM Portal and publish image',
+        'apim-ui-project': config.dockerImages.portal.project,
+        'docker-image-name': config.dockerImages.portal.image,
+        node_version: '20.9',
+      }),
+      new workflow.WorkflowJob(e2eTestJob, {
+        context: config.jobContext,
+        name: 'E2E - << matrix.execution_mode >> - << matrix.database >>',
+        requires: ['Lint & Build APIM e2e', 'Build and push rest api and gateway images'],
+        matrix: {
+          execution_mode: ['v3', 'v4-emulation-engine'],
+          database: ['mongo', 'jdbc', 'bridge'],
+        },
+      }),
+      new workflow.WorkflowJob(e2eCypressJob, {
+        context: config.jobContext,
+        name: 'Run Cypress UI tests',
+        requires: [
+          'Lint & Build APIM e2e',
+          'Build and push rest api and gateway images',
+          'Build APIM Console and publish image',
+          'Build APIM Portal and publish image',
+        ],
+      }),
+    ];
+
+    return new Workflow('run_e2e_tests', jobs);
+  }
+}
