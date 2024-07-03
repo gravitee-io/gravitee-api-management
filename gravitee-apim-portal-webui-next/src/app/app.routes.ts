d77637af8211a12c5db3f562b605c5345ef38eeb
jourdiw <jourdi.waller@graviteesource.com>
2024-07-03 17:22:16 +0200

    fix(portal-next): remove hanging subscription id in breadcrumb
    (cherry picked from commit 76b7a564212f5ac774dcf71a100dab49651e43af)

--- gravitee-apim-portal-webui-next/src/app/app.routes.ts
+++ gravitee-apim-portal-webui-next/src/app/app.routes.ts
@@ -78,6 +78,7 @@ export const routes: Routes = [
                   {
                     path: ':subscriptionId',
                     component: SubscriptionsDetailsComponent,
+                    data: { breadcrumb: { skip: true } },
                   },
                 ],
               },
