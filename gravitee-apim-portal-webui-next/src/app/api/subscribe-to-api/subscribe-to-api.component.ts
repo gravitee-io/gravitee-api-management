78024a673e10d0dd2806cc92576616ee6728055a
jourdiw <jourdi.waller@graviteesource.com>
2024-07-04 15:13:22 +0200

    refactor(portal-next): move services to parent, remove form + add signals for store
    (cherry picked from commit dc28936c55ca4d37a0b8819660ed60a39c26760c)

--- gravitee-apim-portal-webui-next/src/app/api/subscribe-to-api/subscribe-to-api.component.ts
+++ gravitee-apim-portal-webui-next/src/app/api/subscribe-to-api/subscribe-to-api.component.ts
@@ -14,48 +14,91 @@
  * limitations under the License.
  */
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { MatCard } from '@angular/material/card';
+import { Component, computed, Input, OnInit, Signal, signal } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { MatCard, MatCardActions, MatCardContent, MatCardHeader } from '@angular/material/card';
+import { catchError, map, Observable } from 'rxjs';
+import { of } from 'rxjs/internal/observable/of';
 
 import { SubscribeToApiCheckoutComponent } from './subscribe-to-api-checkout/subscribe-to-api-checkout.component';
 import { SubscribeToApiChooseApplicationComponent } from './subscribe-to-api-choose-application/subscribe-to-api-choose-application.component';
 import { SubscribeToApiChoosePlanComponent } from './subscribe-to-api-choose-plan/subscribe-to-api-choose-plan.component';
-import { BreadcrumbNavigationComponent } from '../../../components/breadcrumb-navigation/breadcrumb-navigation.component';
+import { LoaderComponent } from '../../../components/loader/loader.component';
+import { Api } from '../../../entities/api/api';
+import { Application } from '../../../entities/application/application';
+import { Plan } from '../../../entities/plan/plan';
+import { ApiService } from '../../../services/api.service';
+import { PlanService } from '../../../services/plan.service';
 
-export interface SubscribePlan {
-  id: string;
-  header: string;
-  plan: string[];
-  footer: string;
-  isDisabled: boolean;
-}
-
-export interface SubscribeToApiForm {
-  step: FormControl<number | null>;
-  plan: FormControl<SubscribePlan>;
-}
 @Component({
   selector: 'app-subscribe-to-api',
   imports: [
     CommonModule,
-    ReactiveFormsModule,
     MatCard,
-    BreadcrumbNavigationComponent,
     SubscribeToApiCheckoutComponent,
     SubscribeToApiChoosePlanComponent,
     SubscribeToApiChooseApplicationComponent,
+    MatCardActions,
+    MatCardHeader,
+    MatCardContent,
+    MatButton,
+    LoaderComponent,
   ],
   templateUrl: './subscribe-to-api.component.html',
   styleUrl: './subscribe-to-api.component.scss',
   standalone: true,
 })
-export class SubscribeToApiComponent {
+export class SubscribeToApiComponent implements OnInit {
   @Input()
   apiId!: string;
 
-  subscribeToApiForm: FormGroup<SubscribeToApiForm> = new FormGroup({
-    step: new FormControl(1),
-    plan: new FormControl(),
+  currentStep = signal(1);
+  currentPlan = signal<Plan | undefined>(undefined);
+  currentApplication = signal<Application | undefined>(undefined);
+
+  stepIsInvalid: Signal<boolean> = computed(() => {
+    if (this.currentStep() === 1) {
+      return this.currentPlan() === undefined;
+    } else if (this.currentStep() === 2) {
+      return this.currentApplication() === undefined;
+    }
+
+    return false;
   });
+
+  api$: Observable<Api> = of();
+  plans$: Observable<Plan[]> = of();
+
+  constructor(
+    private planService: PlanService,
+    private apiService: ApiService,
+  ) {}
+
+  ngOnInit(): void {
+    this.plans$ = this.planService.list(this.apiId).pipe(
+      map(({ data }) => data ?? []),
+      catchError(_ => of([])),
+    );
+    this.api$ = this.apiService.details(this.apiId).pipe(catchError(_ => of()));
+  }
+
+  goToNextStep(): void {
+    if (this.currentPlan()?.security === 'KEY_LESS' && this.currentStep() === 1) {
+      this.currentStep.set(3);
+    } else if (this.currentStep() < 3) {
+      this.currentStep.update(currentStep => currentStep + 1);
+    }
+  }
+
+  goToPreviousStep(): void {
+    if (this.currentPlan()?.security === 'KEY_LESS' && this.currentStep() === 3) {
+      this.currentStep.set(1);
+    } else if (this.currentStep() > 1) {
+      this.currentStep.update(currentStep => currentStep - 1);
+    }
+  }
+
+  subscribe() {
+    // TODO: To be implemented
+  }
 }
