93741c8000f7f8d79fad3aef78d2182c69ecf335
jourdiw <jourdi.waller@graviteesource.com>
2024-07-04 15:06:33 +0200

    feat(portal-next): subscribe button disabled if no plans for an api
    (cherry picked from commit a6c81525f7f2571b35b9902b1f9b5d02e94f57f8)

--- gravitee-apim-portal-webui-next/src/app/api/api-details/api-details.component.ts
+++ gravitee-apim-portal-webui-next/src/app/api/api-details/api-details.component.ts
@@ -14,20 +14,24 @@
  * limitations under the License.
  */
 import { AsyncPipe } from '@angular/common';
-import { Component, inject, Input } from '@angular/core';
+import { Component, inject, Input, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButton } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTabsModule } from '@angular/material/tabs';
 import { RouterModule } from '@angular/router';
+import { catchError, map, Observable } from 'rxjs';
+import { of } from 'rxjs/internal/observable/of';
 
 import { ApiCardComponent } from '../../../components/api-card/api-card.component';
 import { ApiPictureComponent } from '../../../components/api-picture/api-picture.component';
 import { BannerComponent } from '../../../components/banner/banner.component';
 import { BreadcrumbNavigationComponent } from '../../../components/breadcrumb-navigation/breadcrumb-navigation.component';
 import { Api } from '../../../entities/api/api';
+import { Plan } from '../../../entities/plan/plan';
 import { CurrentUserService } from '../../../services/current-user.service';
+import { PlanService } from '../../../services/plan.service';
 
 @Component({
   selector: 'app-api-details',
@@ -48,9 +52,20 @@ import { CurrentUserService } from '../../../services/current-user.service';
   templateUrl: './api-details.component.html',
   styleUrl: './api-details.component.scss',
 })
-export class ApiDetailsComponent {
+export class ApiDetailsComponent implements OnInit {
   @Input() api!: Api;
   isAuthenticated = inject(CurrentUserService).isUserAuthenticated;
 
-  constructor() {}
+  plans$: Observable<Plan[]> = of([]);
+
+  constructor(private planService: PlanService) {}
+
+  ngOnInit() {
+    if (this.isAuthenticated()) {
+      this.plans$ = this.planService.list(this.api.id).pipe(
+        map(({ data }) => data ?? []),
+        catchError(_ => of([])),
+      );
+    }
+  }
 }
