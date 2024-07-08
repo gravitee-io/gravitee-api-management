3a3875004d2bc819c092453958e81c6f92d7b83c
ThibaudAv <thibaud.avenier@gmail.com>
2024-07-08 11:09:20 +0200

    feat(console): impl add, edit, remove resource

--- gravitee-apim-console-webui/src/management/api/resources/api-resources.component.spec.ts
+++ gravitee-apim-console-webui/src/management/api/resources/api-resources.component.spec.ts
@@ -19,19 +19,26 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconTestingModule } from '@angular/material/icon/testing';
 import { ActivatedRoute } from '@angular/router';
 import { HttpTestingController } from '@angular/common/http/testing';
+import { HarnessLoader } from '@angular/cdk/testing';
+import { InteractivityChecker } from '@angular/cdk/a11y';
+import { GioConfirmDialogHarness } from '@gravitee/ui-particles-angular';
 
 import { ApiResourcesHarness } from './api-resources.harness';
 import { ApiResourcesComponent } from './api-resources.component';
+import { ApiResourcesAddDialogHarness } from './api-resources-add-dialog/api-resources-add-dialog.harness';
+import { ApiResourcesEditDialogHarness } from './api-resources-edit-dialog/api-resources-edit-dialog.harness';
 
 import { ApiV2, ApiV4, fakeApiV4 } from '../../../entities/management-api-v2';
 import { CONSTANTS_TESTING, GioTestingModule } from '../../../shared/testing';
 import { ResourceListItem } from '../../../entities/resource/resourceListItem';
+import { GioTestingPermissionProvider } from '../../../shared/components/gio-permission/gio-permission.service';
 
 describe('ApiResourcesComponent', () => {
   const API_ID = 'api-id';
 
   let fixture: ComponentFixture<ApiResourcesComponent>;
   let componentHarness: ApiResourcesHarness;
+  let rootLoader: HarnessLoader;
   let httpTestingController: HttpTestingController;
 
   beforeEach(async () => {
@@ -48,11 +55,22 @@ describe('ApiResourcesComponent', () => {
             },
           },
         },
+        {
+          provide: GioTestingPermissionProvider,
+          useValue: ['api-definition-c', 'api-definition-r', 'api-definition-u', 'api-definition-d'],
+        },
       ],
-    }).compileComponents();
+    })
+      .overrideProvider(InteractivityChecker, {
+        useValue: {
+          isFocusable: () => true, // This traps focus checks and so avoid warnings when dealing with
+        },
+      })
+      .compileComponents();
 
     fixture = TestBed.createComponent(ApiResourcesComponent);
     httpTestingController = TestBed.inject(HttpTestingController);
+    rootLoader = TestbedHarnessEnvironment.documentRootLoader(fixture);
     componentHarness = await TestbedHarnessEnvironment.harnessForFixture(fixture, ApiResourcesHarness);
     fixture.detectChanges();
   });
@@ -81,9 +99,81 @@ describe('ApiResourcesComponent', () => {
     ]);
 
     expect(await table.getCellTextByIndex()).toStrictEqual([
-      ['MyCache', 'Cache', ''],
-      ['MyOAuth2 Disabled', 'OAuth2', ''],
+      ['MyCache', 'Cache', 'toggle_on'],
+      ['MyOAuth2 Disabled', 'OAuth2', 'toggle_off'],
     ]);
+  });
+
+  it('should add a resource', async () => {
+    expectApiGetRequest(fakeApiV4({ id: API_ID, resources: [] }));
+    expectListResourcesRequest([{ id: 'cache', name: 'Cache', version: '', description: '', icon: '', schema: '' }]);
+
+    // Open Add dialog
+    await componentHarness.clickAddResource();
+    const resourceAddDialog = await rootLoader.getHarness(ApiResourcesAddDialogHarness);
+    expect(await resourceAddDialog.getTitleText()).toEqual('Add API Resource');
+
+    // Select Cache resource
+    await resourceAddDialog.select('Cache');
+
+    // Configure Cache resource
+    const resourceEditDialog = await rootLoader.getHarness(ApiResourcesEditDialogHarness);
+    expect(await resourceEditDialog.getTitleText()).toEqual('Configure Cache resource');
+
+    await resourceEditDialog.setResourceName('My Cache');
+
+    // Save
+    await resourceEditDialog.save();
+
+    // Expect the resource to be added
+    expectApiGetRequest(fakeApiV4({ id: API_ID, resources: [] }));
+    const updateApiReq = httpTestingController.expectOne({
+      url: `${CONSTANTS_TESTING.env.v2BaseURL}/apis/${API_ID}`,
+      method: 'PUT',
+    });
+    expect(updateApiReq.request.body.resources).toStrictEqual([{ name: 'My Cache', type: 'cache', configuration: null }]);
+  });
+
+  it('should edit a resource', async () => {
+    expectApiGetRequest(fakeApiV4({ id: API_ID, resources: [{ name: 'MyCache', type: 'cache', configuration: '' }] }));
+    expectListResourcesRequest([{ id: 'cache', name: 'Cache', version: '', description: '', icon: '', schema: '' }]);
+
+    // Open Edit dialog
+    await componentHarness.clickEditResource(0);
+    const resourceEditDialog = await rootLoader.getHarness(ApiResourcesEditDialogHarness);
+
+    expect(await resourceEditDialog.getTitleText()).toEqual('Configure Cache resource');
+
+    await resourceEditDialog.setResourceName('Renamed Cache');
+
+    // Save
+    await resourceEditDialog.save();
+
+    // Expect the resource to be saved
+    expectApiGetRequest(fakeApiV4({ id: API_ID, resources: [{ name: 'Renamed Cache', type: 'cache', configuration: '' }] }));
+    const updateApiReq = httpTestingController.expectOne({
+      url: `${CONSTANTS_TESTING.env.v2BaseURL}/apis/${API_ID}`,
+      method: 'PUT',
+    });
+    expect(updateApiReq.request.body.resources).toStrictEqual([{ name: 'Renamed Cache', type: 'cache', configuration: '' }]);
+  });
+
+  it('should remove a resource', async () => {
+    expectApiGetRequest(fakeApiV4({ id: API_ID, resources: [{ name: 'MyCache', type: 'cache', configuration: '' }] }));
+    expectListResourcesRequest([{ id: 'cache', name: 'Cache', version: '', description: '', icon: '', schema: '' }]);
+
+    // Remove resource
+    await componentHarness.clickRemoveButton(0);
+    const confirmDialog = await rootLoader.getHarness(GioConfirmDialogHarness);
+    await confirmDialog.confirm();
+
+    // Expect the resource to be removed
+    expectApiGetRequest(fakeApiV4({ id: API_ID, resources: [{ name: 'MyCache', type: 'cache', configuration: '' }] }));
+    const updateApiReq = httpTestingController.expectOne({
+      url: `${CONSTANTS_TESTING.env.v2BaseURL}/apis/${API_ID}`,
+      method: 'PUT',
+    });
+    expect(updateApiReq.request.body.resources).toStrictEqual([]);
   });
 
   function expectApiGetRequest(api: ApiV2 | ApiV4) {
