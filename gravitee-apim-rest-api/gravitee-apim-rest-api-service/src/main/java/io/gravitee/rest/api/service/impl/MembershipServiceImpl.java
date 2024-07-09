396e3c73b12a7b946854a4a6c66cba6e9377c54b
Ouahid Khelifi <ouahid.khelifi@gmail.com>
2024-07-09 12:25:46 +0200

    fix: allow user to be redirected on portal when smtp server is not reachable

--- gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/rest/api/service/impl/MembershipServiceImpl.java
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/rest/api/service/impl/MembershipServiceImpl.java
@@ -319,7 +319,17 @@ public class MembershipServiceImpl extends AbstractService implements Membership
                             reference.getId()
                         );
                         if (emailNotification != null) {
-                            emailService.sendAsyncEmailNotification(executionContext, emailNotification);
+                            try {
+                                emailService.sendAsyncEmailNotification(executionContext, emailNotification);
+                            } catch (Exception e) {
+                                LOGGER.error(
+                                    "An error occurs while trying to send email notification for {} {} {}",
+                                    reference.getType(),
+                                    reference.getId(),
+                                    userEntity.getId(),
+                                    e
+                                );
+                            }
                         }
                     }
 
