4a8527ee3f2f5a26f5809739126b012093643414
ThibaudAv <thibaud.avenier@gmail.com>
2024-07-10 14:19:00 +0200

    feat(console): impl filters into resource add dialog

--- gravitee-apim-console-webui/src/management/api/resources/api-resources-add-dialog/api-resources-add-dialog.harness.ts
+++ gravitee-apim-console-webui/src/management/api/resources/api-resources-add-dialog/api-resources-add-dialog.harness.ts
@@ -13,10 +13,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { BaseHarnessFilters, ComponentHarness, HarnessPredicate } from '@angular/cdk/testing';
+import { BaseHarnessFilters, ComponentHarness, HarnessPredicate, parallel } from '@angular/cdk/testing';
 import { MatDialogSection } from '@angular/material/dialog/testing';
 import { MatButtonHarness } from '@angular/material/button/testing';
 import { MatRadioGroupHarness } from '@angular/material/radio/testing';
+import { MatChipListboxHarness } from '@angular/material/chips/testing';
+import { MatInputHarness } from '@angular/material/input/testing';
 
 export interface ApiResourcesAddDialogHarnessOptions extends BaseHarnessFilters {}
 
@@ -30,6 +32,10 @@ export class ApiResourcesAddDialogHarness extends ComponentHarness {
   protected _title = this.locatorForOptional(MatDialogSection.TITLE);
   protected _content = this.locatorForOptional(MatDialogSection.CONTENT);
   protected _actions = this.locatorForOptional(MatDialogSection.ACTIONS);
+
+  protected getRadioGroup = this.locatorFor(MatRadioGroupHarness);
+  protected getFilterCategoryListbox = this.locatorForOptional(MatChipListboxHarness.with({ selector: '[formControlName="category"]' }));
+  protected getFilterSearchInput = this.locatorFor(MatInputHarness.with({ selector: '[formControlName="search"]' }));
 
   public async getTitleText(): Promise<string> {
     return (await this._title())?.text() ?? '';
@@ -50,11 +56,37 @@ export class ApiResourcesAddDialogHarness extends ComponentHarness {
 
   public async select(resourceName: string): Promise<void> {
     // Select the radio button with the given label
-    const radioGroup = await this.locatorFor(MatRadioGroupHarness)();
+    const radioGroup = await this.getRadioGroup();
     await radioGroup.checkRadioButton({ label: resourceName });
 
     // Confirm the selection
     const confirmButton = await this.locatorFor(MatButtonHarness.with({ text: /Select/ }))();
     await confirmButton.click();
+  }
+
+  public async getDisplayedResources(): Promise<string[]> {
+    const radioGroup = await this.getRadioGroup();
+    const buttons = await radioGroup.getRadioButtons();
+
+    return parallel(() => buttons.map((button) => button.getLabelText()));
+  }
+
+  public async toggleFilterByCategory(category: string): Promise<void> {
+    const filtersCategoryListbox = await this.getFilterCategoryListbox();
+
+    if (filtersCategoryListbox) {
+      const chip = await filtersCategoryListbox.getChips({
+        text: category,
+      });
+      if (chip && chip[0]) {
+        const isSelected = await chip[0].isSelected();
+        isSelected ? await chip[0].deselect() : await chip[0].select();
+      }
+    }
+  }
+
+  public async searchFilter(search: string): Promise<void> {
+    const searchInput = await this.getFilterSearchInput();
+    await searchInput.setValue(search);
   }
 }
