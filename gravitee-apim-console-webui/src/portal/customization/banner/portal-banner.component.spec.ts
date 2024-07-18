7b371e85a2cbb5d9bcf46accc3cf04bbdde12de1
jourdiw <jourdi.waller@graviteesource.com>
2024-07-18 17:56:10 +0200

    feat(console): create portal section

---
+++ gravitee-apim-console-webui/src/portal/customization/banner/portal-banner.component.spec.ts
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+import { PortalBannerComponent } from './portal-banner.component';
+import { PortalBannerHarness } from './portal-banner.harness';
+
+describe('DeveloperPortalBannerComponent', () => {
+  // let component: DeveloperPortalBannerComponent;
+  let fixture: ComponentFixture<PortalBannerComponent>;
+  let componentHarness: PortalBannerHarness;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [NoopAnimationsModule, PortalBannerComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(PortalBannerComponent);
+    // component = fixture.componentInstance;
+    componentHarness = await TestbedHarnessEnvironment.harnessForFixture(fixture, PortalBannerHarness);
+    fixture.detectChanges();
+  });
+
+  it('should fill form and submit', async () => {
+    await componentHarness.setName('name');
+    await componentHarness.reset();
+    expect(await componentHarness.getName()).toStrictEqual('');
+
+    await componentHarness.setName('name');
+    await componentHarness.submit();
+    expect(await componentHarness.getName()).toStrictEqual('name');
+  });
+
+  it('should not be able to save', async () => {
+    await componentHarness.setName('name');
+    await componentHarness.setName(null);
+    expect(await componentHarness.isSubmitInvalid()).toBeTruthy();
+  });
+});
---
+++ gravitee-apim-console-webui/src/portal/portal-settings-routing.module.ts
@@ -0,0 +1,72 @@
+/*
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { PortalSettingsModule } from './portal-settings.module';
+import { PortalNavigationComponent } from './navigation/portal-navigation.component';
+import { PortalCustomizationComponent } from './customization/portal-customization.component';
+import { PortalBannerComponent } from './customization/banner/portal-banner.component';
+import { PortalThemeComponent } from './customization/theme/portal-theme.component';
+
+import { PermissionGuard } from '../shared/components/gio-permission/gio-permission.guard';
+import { HasLicenseGuard } from '../shared/components/gio-license/has-license.guard';
+import { EnvironmentGuard } from '../management/environment.guard';
+
+const portalRoutes: Routes = [
+  {
+    path: '',
+    component: PortalNavigationComponent,
+    canActivate: [PermissionGuard.checkRouteDataPermissions, HasLicenseGuard, EnvironmentGuard.initEnvConfigAndLoadPermissions],
+    children: [
+      {
+        path: 'customization',
+        component: PortalCustomizationComponent,
+        data: {
+          permissions: {
+            anyOf: ['organization-settings-r'],
+          },
+        },
+        children: [
+          {
+            path: 'banner',
+            component: PortalBannerComponent,
+          },
+          {
+            path: 'theme',
+            component: PortalThemeComponent,
+          },
+          {
+            path: '',
+            pathMatch: 'full',
+            redirectTo: 'theme',
+          },
+        ],
+      },
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'customization',
+      },
+    ],
+  },
+];
+
+@NgModule({
+  imports: [PortalSettingsModule, RouterModule.forChild(portalRoutes)],
+  exports: [RouterModule],
+})
+export class PortalSettingsRoutingModule {}
