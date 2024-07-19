09e9a3fdb01d952d5a344cd2c2d6e10e18291b76
jourdiw <jourdi.waller@graviteesource.com>
2024-07-19 14:40:03 +0200

    feat(service): add find by theme type and env id to theme query service

--- gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/test/java/io/gravitee/apim/infra/query_service/theme/ThemeQueryServiceImplTest.java
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/test/java/io/gravitee/apim/infra/query_service/theme/ThemeQueryServiceImplTest.java
@@ -27,7 +27,9 @@ import io.gravitee.repository.management.model.Theme;
 import io.gravitee.repository.management.model.ThemeType;
 import io.gravitee.rest.api.model.common.PageableImpl;
 import io.gravitee.rest.api.model.theme.portal.ThemeDefinition;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
@@ -41,6 +43,7 @@ public class ThemeQueryServiceImplTest {
 
     private final String PORTAL_THEME_ID = "portal-id";
     private final String PORTAL_NEXT_THEME_ID = "portal-next-id";
+    private final String ENV_ID = "env-id";
 
     @Mock
     ThemeRepository themeRepository;
@@ -179,6 +182,40 @@ public class ThemeQueryServiceImplTest {
                 .build();
 
             Assertions.assertThat(result).isNotNull().hasFieldOrPropertyWithValue("content", List.of(portalTheme));
+        }
+    }
+
+    @Nested
+    class FindByThemeTypeAndEnvironmentId {
+
+        @Test
+        void should_return_empty_results() throws Throwable {
+            when(themeRepository.findByReferenceIdAndReferenceTypeAndType(eq(ENV_ID), eq("ENVIRONMENT"), eq(ThemeType.PORTAL)))
+                .thenAnswer(invocation -> new HashSet<Theme>());
+
+            var result = service.findByThemeTypeAndEnvironmentId(io.gravitee.apim.core.theme.model.ThemeType.PORTAL, ENV_ID);
+
+            Assertions.assertThat(result).isNotNull().isEmpty();
+        }
+
+        @Test
+        void should_return_portal_theme() throws Throwable {
+            when(themeRepository.findByReferenceIdAndReferenceTypeAndType(eq(ENV_ID), eq("ENVIRONMENT"), eq(ThemeType.PORTAL)))
+                .thenAnswer(invocation -> Set.of(aPortalRepoTheme()));
+
+            var result = service.findByThemeTypeAndEnvironmentId(io.gravitee.apim.core.theme.model.ThemeType.PORTAL, ENV_ID);
+
+            var portalDefinition = new ThemeDefinition();
+            portalDefinition.setData(List.of());
+            var portalTheme = io.gravitee.apim.core.theme.model.Theme
+                .builder()
+                .id(PORTAL_THEME_ID)
+                .name(PORTAL_THEME_ID)
+                .definitionPortal(portalDefinition)
+                .type(io.gravitee.apim.core.theme.model.ThemeType.PORTAL)
+                .build();
+
+            Assertions.assertThat(result).isNotNull().hasSize(1).contains(portalTheme);
         }
     }
 
