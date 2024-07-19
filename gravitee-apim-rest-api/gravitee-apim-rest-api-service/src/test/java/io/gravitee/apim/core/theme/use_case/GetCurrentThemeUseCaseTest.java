8690cc4a84b14e169ebd39bb089a7cf994a717a2
jourdiw <jourdi.waller@graviteesource.com>
2024-07-19 15:30:59 +0200

    feat(service): add get current theme use case

--- gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/test/java/io/gravitee/apim/core/theme/use_case/GetCurrentThemeUseCaseTest.java
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/test/java/io/gravitee/apim/core/theme/use_case/GetCurrentThemeUseCaseTest.java
@@ -21,12 +21,14 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import inmemory.InMemoryAlternative;
 import inmemory.ParametersDomainServiceInMemory;
 import inmemory.ThemeCrudServiceInMemory;
+import inmemory.ThemeQueryServiceInMemory;
 import inmemory.ThemeServiceLegacyWrapperInMemory;
 import io.gravitee.apim.core.theme.domain_service.DefaultThemeDomainService;
 import io.gravitee.apim.core.theme.domain_service.ThemeDomainService;
 import io.gravitee.apim.core.theme.exception.ThemeTypeNotSupportedException;
 import io.gravitee.apim.core.theme.model.Theme;
 import io.gravitee.apim.core.theme.model.ThemeType;
+import io.gravitee.apim.core.theme.query_service.ThemeQueryService;
 import io.gravitee.repository.management.model.Parameter;
 import io.gravitee.repository.management.model.ParameterReferenceType;
 import io.gravitee.rest.api.model.parameters.Key;
@@ -37,10 +39,9 @@ import java.util.List;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-public class GetDefaultThemeUseCaseTest {
+public class GetCurrentThemeUseCaseTest {
 
     private final ExecutionContext EXECUTION_CONTEXT = GraviteeContext.getExecutionContext();
 
@@ -60,19 +61,12 @@ public class GetDefaultThemeUseCaseTest {
         .customCss(".style { }")
         .build();
 
-    private final Theme EXPECTED_DEFAULT_PORTAL_NEXT_THEME = Theme
-        .builder()
-        .type(ThemeType.PORTAL_NEXT)
-        .referenceType(Theme.ReferenceType.ENVIRONMENT)
-        .referenceId(EXECUTION_CONTEXT.getEnvironmentId())
-        .name("Default Portal Next Theme")
-        .definitionPortalNext(EXPECTED_PORTAL_NEXT_THEME_DEFINITION)
-        .build();
-
     ThemeCrudServiceInMemory themeCrudService = new ThemeCrudServiceInMemory();
     ParametersDomainServiceInMemory parametersDomainService = new ParametersDomainServiceInMemory();
     ThemeServiceLegacyWrapperInMemory themeServiceLegacyWrapper = new ThemeServiceLegacyWrapperInMemory();
-    GetDefaultThemeUseCase cut;
+    ThemeQueryServiceInMemory themeQueryServiceInMemory = new ThemeQueryServiceInMemory(themeCrudService);
+    DefaultThemeDomainService defaultThemeDomainService;
+    GetCurrentThemeUseCase cut;
 
     @BeforeEach
     void setUp() {
@@ -136,29 +130,94 @@ public class GetDefaultThemeUseCaseTest {
                     .build()
             )
         );
-        var defaultThemeDomainService = new DefaultThemeDomainService(
-            parametersDomainService,
-            new ThemeDomainService(themeCrudService),
-            themeServiceLegacyWrapper
-        );
 
-        cut = new GetDefaultThemeUseCase(defaultThemeDomainService);
+        defaultThemeDomainService =
+            new DefaultThemeDomainService(parametersDomainService, new ThemeDomainService(themeCrudService), themeServiceLegacyWrapper);
+        cut = new GetCurrentThemeUseCase(themeQueryServiceInMemory, defaultThemeDomainService);
     }
 
     @AfterEach
     void tearDown() {
-        Stream.of(parametersDomainService).forEach(InMemoryAlternative::reset);
+        Stream
+            .of(parametersDomainService, themeCrudService, themeServiceLegacyWrapper, themeQueryServiceInMemory)
+            .forEach(InMemoryAlternative::reset);
     }
 
     @Test
-    void should_return_portal_next_theme() {
-        var result = cut.execute(new GetDefaultThemeUseCase.Input(ThemeType.PORTAL_NEXT, EXECUTION_CONTEXT));
-        assertThat(result).extracting(GetDefaultThemeUseCase.Output::result).isEqualTo(EXPECTED_DEFAULT_PORTAL_NEXT_THEME);
+    void should_return_existing_portal_next_theme() {
+        themeCrudService.initWith(List.of(aPortalNextTheme(true)));
+
+        var result = cut.execute(new GetCurrentThemeUseCase.Input(ThemeType.PORTAL_NEXT, EXECUTION_CONTEXT));
+        assertThat(result).extracting(GetCurrentThemeUseCase.Output::result).isEqualTo(aPortalNextTheme(true));
     }
 
     @Test
-    void should_throw_error_for_portal_theme() {
-        assertThatThrownBy(() -> cut.execute(new GetDefaultThemeUseCase.Input(ThemeType.PORTAL, EXECUTION_CONTEXT)))
-            .isInstanceOf(ThemeTypeNotSupportedException.class);
+    void should_return_existing_portal_theme() {
+        themeCrudService.initWith(List.of(aPortalTheme(true)));
+
+        var result = cut.execute(new GetCurrentThemeUseCase.Input(ThemeType.PORTAL, EXECUTION_CONTEXT));
+        assertThat(result).extracting(GetCurrentThemeUseCase.Output::result).isEqualTo(aPortalTheme(true));
+    }
+
+    @Test
+    void should_return_default_portal_next_theme() {
+        themeCrudService.initWith(List.of(aPortalNextTheme(false)));
+
+        var result = cut.execute(new GetCurrentThemeUseCase.Input(ThemeType.PORTAL_NEXT, EXECUTION_CONTEXT));
+        assertThat(result)
+            .extracting(GetCurrentThemeUseCase.Output::result)
+            .satisfies(res -> {
+                var defaultPortalTheme = defaultThemeDomainService.getDefaultTheme(ThemeType.PORTAL_NEXT, EXECUTION_CONTEXT);
+
+                assertThat(res.getId()).isNotBlank();
+                assertThat(res.getName()).isEqualTo(defaultPortalTheme.getName());
+                assertThat(res.getDefinitionPortalNext()).isEqualTo(defaultPortalTheme.getDefinitionPortalNext());
+                assertThat(res.isEnabled()).isEqualTo(true);
+                assertThat(res.getCreatedAt()).isNotNull();
+                assertThat(res.getUpdatedAt()).isNotNull();
+            });
+
+        assertThat(themeCrudService.storage()).hasSize(2).contains(result.result());
+    }
+
+    @Test
+    void should_return_default_portal_theme() {
+        themeCrudService.initWith(List.of(aPortalTheme(false)));
+
+        var result = cut.execute(new GetCurrentThemeUseCase.Input(ThemeType.PORTAL, EXECUTION_CONTEXT));
+        assertThat(result)
+            .extracting(GetCurrentThemeUseCase.Output::result)
+            .satisfies(res -> {
+                assertThat(res.getId()).isEqualTo("portal-default-theme");
+            });
+    }
+
+    private Theme aPortalTheme(boolean enabled) {
+        var portalDefinition = new io.gravitee.rest.api.model.theme.portal.ThemeDefinition();
+        portalDefinition.setData(List.of());
+
+        return Theme
+            .builder()
+            .id("portal-theme")
+            .name("a portal theme")
+            .type(ThemeType.PORTAL)
+            .referenceId(EXECUTION_CONTEXT.getEnvironmentId())
+            .referenceType(Theme.ReferenceType.ENVIRONMENT)
+            .definitionPortal(portalDefinition)
+            .enabled(enabled)
+            .build();
+    }
+
+    private Theme aPortalNextTheme(boolean enabled) {
+        return Theme
+            .builder()
+            .id("portal-next-theme")
+            .name("a portal next theme")
+            .type(ThemeType.PORTAL_NEXT)
+            .referenceId(EXECUTION_CONTEXT.getEnvironmentId())
+            .referenceType(Theme.ReferenceType.ENVIRONMENT)
+            .definitionPortalNext(ThemeDefinition.builder().color(ThemeDefinition.Color.builder().primary("#fff").build()).build())
+            .enabled(enabled)
+            .build();
     }
 }
