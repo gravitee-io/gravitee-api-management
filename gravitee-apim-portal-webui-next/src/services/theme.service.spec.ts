842919adb493631f41a821a66010fd5a34d0c806
jourdiw <jourdi.waller@graviteesource.com>
2024-07-24 12:32:41 +0200

    refactor(portal-next): load theme from backend response + remove default values

--- gravitee-apim-portal-webui-next/src/services/theme.service.spec.ts
+++ gravitee-apim-portal-webui-next/src/services/theme.service.spec.ts
@@ -13,19 +13,70 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import { HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
-import { ThemeService } from './theme.service';
+import { Theme, ThemeService } from './theme.service';
+import { AppTestingModule, TESTING_BASE_URL } from '../testing/app-testing.module';
 
 describe('ThemeService', () => {
   let service: ThemeService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [AppTestingModule],
+    });
     service = TestBed.inject(ThemeService);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  afterEach(() => {
+    httpTestingController.verify();
+  });
+
+  it('load theme', done => {
+    const themeResponse: Theme = {
+      definition: {
+        color: {
+          primary: '#000000',
+          secondary: '#111111',
+          tertiary: '#222222',
+          error: '#333333',
+          background: {
+            page: '#444444',
+            card: '#555555',
+          },
+        },
+        font: { fontFamily: '"Roboto", sans serif' },
+        customCss: 'app-root { background: blue; }',
+      },
+    };
+
+    service.loadTheme().subscribe({
+      next: _ => {
+        expect(document.documentElement.style.getPropertyValue('--gio-app-background-color')).toEqual(
+          themeResponse.definition.color?.background?.page,
+        );
+        expect(document.documentElement.style.getPropertyValue('--gio-app-card-background-color')).toEqual(
+          themeResponse.definition.color?.background?.card,
+        );
+        expect(document.documentElement.style.getPropertyValue('--gio-app-primary-main-color')).toEqual('hsl(0, 0%, 0%)');
+        expect(document.documentElement.style.getPropertyValue('--gio-app-secondary-main-color')).toEqual(
+          'hsl(0, 0%, 0.06666666666666667%)',
+        );
+        expect(document.documentElement.style.getPropertyValue('--gio-app-tertiary-main-color')).toEqual(
+          'hsl(0, 0%, 0.13333333333333333%)',
+        );
+        expect(document.documentElement.style.getPropertyValue('--gio-app-error-main-color')).toEqual('hsl(0, 0%, 0.2%)');
+        expect(document.documentElement.style.getPropertyValue('--gio-app-font-family')).toEqual(themeResponse.definition.font.fontFamily);
+        const style: HTMLStyleElement = document.getElementsByTagName('style')[0];
+        expect(style.innerText).toEqual(themeResponse.definition.customCss);
+        done();
+      },
+      error: _ => fail(),
+    });
+
+    httpTestingController.expectOne(`${TESTING_BASE_URL}/theme?type=PORTAL_NEXT`).flush(themeResponse);
   });
 });
