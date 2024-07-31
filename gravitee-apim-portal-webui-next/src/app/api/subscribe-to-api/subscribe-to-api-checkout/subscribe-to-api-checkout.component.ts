d01792677b89d48f47982492412a1d365e9871a8
jourdiw <jourdi.waller@graviteesource.com>
2024-07-31 11:02:23 +0200

    feat(portal-next): add api key mode selection
    (cherry picked from commit 0f3b5b4e7c7ff362c8cad331867a6ef3ba81a02b)

--- gravitee-apim-portal-webui-next/src/app/api/subscribe-to-api/subscribe-to-api-checkout/subscribe-to-api-checkout.component.ts
+++ gravitee-apim-portal-webui-next/src/app/api/subscribe-to-api/subscribe-to-api-checkout/subscribe-to-api-checkout.component.ts
@@ -17,8 +17,10 @@ import { Component, Input, WritableSignal } from '@angular/core';
 import { MatCard, MatCardContent } from '@angular/material/card';
 import { MatFormField, MatLabel } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
+import { MatRadioGroup } from '@angular/material/radio';
 
 import { ApiAccessComponent } from '../../../../components/api-access/api-access.component';
+import { RadioCardComponent } from '../../../../components/radio-card/radio-card.component';
 import { SubscriptionInfoComponent } from '../../../../components/subscription-info/subscription-info.component';
 import { Api } from '../../../../entities/api/api';
 import { Application } from '../../../../entities/application/application';
@@ -26,7 +28,17 @@ import { Plan } from '../../../../entities/plan/plan';
 
 @Component({
   selector: 'app-subscribe-to-api-checkout',
-  imports: [SubscriptionInfoComponent, ApiAccessComponent, MatCard, MatCardContent, MatFormField, MatInput, MatLabel],
+  imports: [
+    SubscriptionInfoComponent,
+    ApiAccessComponent,
+    MatCard,
+    MatCardContent,
+    MatFormField,
+    MatInput,
+    MatLabel,
+    MatRadioGroup,
+    RadioCardComponent,
+  ],
   templateUrl: './subscribe-to-api-checkout.component.html',
   styleUrl: './subscribe-to-api-checkout.component.scss',
   providers: [],
@@ -41,6 +53,15 @@ export class SubscribeToApiCheckoutComponent {
 
   @Input()
   message!: WritableSignal<string>;
+
+  @Input()
+  showApiKeyModeSelection!: boolean;
+
+  @Input()
+  sharedApiKeyModeDisabled: boolean = false;
+
+  @Input()
+  apiKeyMode!: WritableSignal<'EXCLUSIVE' | 'SHARED' | 'UNSPECIFIED' | null>;
 
   @Input()
   application?: Application;
