84c40a2b560671145691c633bf861f8ca1f0d22b
jourdiw <jourdi.waller@graviteesource.com>
2024-08-02 10:51:24 +0200

    feat(portal-next): initialize applications list + application components + subcomponents
    (cherry picked from commit 99cd87541077198c581a3120d94f329c1ede9d9b)

---
+++ gravitee-apim-portal-webui-next/src/resolvers/application.resolver.spec.ts
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2024 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { TestBed } from '@angular/core/testing';
+import { ResolveFn } from '@angular/router';
+
+import { applicationResolver } from './application.resolver';
+import { Application } from '../entities/application/application';
+
+describe('applicationResolver', () => {
+  const executeResolver: ResolveFn<Application> = (...resolverParameters) =>
+    TestBed.runInInjectionContext(() => applicationResolver(...resolverParameters));
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+  });
+
+  it('should be created', () => {
+    expect(executeResolver).toBeTruthy();
+  });
+});
