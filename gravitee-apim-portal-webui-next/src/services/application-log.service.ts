af52de87cf481ded51a1e8a01a6fc8fbd85a3180
jourdiw <jourdi.waller@graviteesource.com>
2024-08-05 15:57:35 +0200

    feat(portal-next): add application log service to get page of logs
    (cherry picked from commit 7673724bac89dcec385330d5b6dbbbcb14e20e83)

---
+++ gravitee-apim-portal-webui-next/src/services/application-log.service.ts
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2024 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { ConfigService } from './config.service';
+import { LogsResponse } from '../entities/log/log';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApplicationLogService {
+  constructor(
+    private readonly http: HttpClient,
+    private configService: ConfigService,
+  ) {}
+
+  list(
+    applicationId: string,
+    params: {
+      page?: number;
+      size?: number;
+      to?: number;
+      from?: number;
+      order?: 'ASC' | 'DESC';
+      field?: string;
+    },
+  ): Observable<LogsResponse> {
+    const paramsList = [];
+    paramsList.push(`page=${params.page ?? 1}`);
+    paramsList.push(`size=${params.size ?? 10}`);
+
+    const currentDate = new Date();
+    const yesterdayDate = new Date();
+    yesterdayDate.setDate(yesterdayDate.getDate() - 1);
+
+    paramsList.push(`from=${params.from ?? yesterdayDate.getTime()}`);
+    paramsList.push(`to=${params.to ?? currentDate.getTime()}`);
+    paramsList.push(`order=${params.order ?? 'DESC'}`);
+
+    paramsList.push(`field=@${params.field ?? 'timestamp'}`);
+
+    return this.http.get<LogsResponse>(`${this.configService.baseURL}/applications/${applicationId}/logs?${paramsList.join('&')}`);
+  }
+}
