cffd8a1f2d8a7386a53bca6e93db68d1cacb7ebd
jourdiw <jourdi.waller@graviteesource.com>
2024-08-06 17:15:52 +0200

    feat(portal-next): add api select multiple filter to logs list

--- gravitee-apim-portal-webui-next/src/app/applications/application/application-tab-logs/application-tab-logs.component.spec.ts
+++ gravitee-apim-portal-webui-next/src/app/applications/application/application-tab-logs/application-tab-logs.component.spec.ts
@@ -19,6 +19,8 @@ import { HttpTestingController } from '@angular/common/http/testing';
 import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatButtonHarness } from '@angular/material/button/testing';
+import { MatChipHarness } from '@angular/material/chips/testing';
+import { MatSelectHarness } from '@angular/material/select/testing';
 import { MatTableHarness } from '@angular/material/table/testing';
 import { By } from '@angular/platform-browser';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -28,7 +30,11 @@ import { ApplicationTabLogsComponent } from './application-tab-logs.component';
 import { fakeApplication } from '../../../../entities/application/application.fixture';
 import { LogsResponse } from '../../../../entities/log/log';
 import { fakeLog, fakeLogsResponse } from '../../../../entities/log/log.fixture';
+import { fakeSubscription, fakeSubscriptionResponse } from '../../../../entities/subscription/subscription.fixture';
+import { SubscriptionsResponse } from '../../../../entities/subscription/subscriptions-response';
 import { AppTestingModule, TESTING_BASE_URL } from '../../../../testing/app-testing.module';
+
+/* eslint-disable no-useless-escape */
 
 describe('ApplicationTabLogsComponent', () => {
   let component: ApplicationTabLogsComponent;
@@ -78,6 +84,7 @@ describe('ApplicationTabLogsComponent', () => {
 
     it('should show empty message when no logs', async () => {
       expectGetApplicationLogs(fakeLogsResponse({ data: [], metadata: { data: { total: 0 } } }));
+      expectGetSubscriptions(fakeSubscriptionResponse());
       fixture.detectChanges();
 
       expect(getNoLogsMessageSection()).toBeTruthy();
@@ -98,6 +105,8 @@ describe('ApplicationTabLogsComponent', () => {
           },
         }),
       );
+      expectGetSubscriptions(fakeSubscriptionResponse());
+
       fixture.detectChanges();
 
       expect(getNoLogsMessageSection()).toBeFalsy();
@@ -136,6 +145,7 @@ describe('ApplicationTabLogsComponent', () => {
               },
             }),
           );
+          expectGetSubscriptions(fakeSubscriptionResponse());
           fixture.detectChanges();
         });
         it('should not allow previous page on load', async () => {
@@ -170,6 +180,7 @@ describe('ApplicationTabLogsComponent', () => {
               },
             }),
           );
+          expectGetSubscriptions(fakeSubscriptionResponse());
           fixture.detectChanges();
         });
         it('should not allow previous page on load', async () => {
@@ -272,6 +283,7 @@ describe('ApplicationTabLogsComponent', () => {
           }),
           3,
         );
+        expectGetSubscriptions(fakeSubscriptionResponse());
       });
       it('should allow previous page', async () => {
         const previousPageButton = await getPreviousPageButton();
@@ -327,14 +339,160 @@ describe('ApplicationTabLogsComponent', () => {
     });
   });
 
-  function expectGetApplicationLogs(logsResponse: LogsResponse, page: number = 1) {
+  describe('Filters', () => {
+    describe('No filters in query params', () => {
+      beforeEach(async () => {
+        await init({});
+
+        expectGetApplicationLogs(
+          fakeLogsResponse({
+            data: [
+              fakeLog({ api: 'my-api', plan: 'my-plan', status: 201, timestamp: 1466424490000 }),
+              fakeLog({ api: 'my-api-2', plan: 'my-plan', status: 204, timestamp: 1566424490000 }),
+            ],
+            metadata: {
+              'my-api': { name: 'My API', version: '1.0' },
+              'my-api-2': { name: 'My API 2', version: '2.0' },
+              'my-plan': { name: 'My Plan' },
+              data: { total: 3 },
+            },
+          }),
+        );
+        expectGetSubscriptions(fakeSubscriptionResponse());
+        fixture.detectChanges();
+      });
+
+      it('should have no filters selected', async () => {
+        expect(await noChipFiltersDisplayed()).toEqual(true);
+      });
+      it('should have search and reset buttons disabled', async () => {
+        const resetButton = await getResetFilterButton();
+        expect(await resetButton.isDisabled()).toEqual(true);
+
+        const searchButton = await getSearchButton();
+        expect(await searchButton.isDisabled()).toEqual(true);
+      });
+    });
+    describe('APIs', () => {
+      describe('One API in query params', () => {
+        const API_ID = 'api-id';
+        beforeEach(async () => {
+          await init({ apis: [API_ID] });
+
+          expectGetApplicationLogs(
+            fakeLogsResponse({
+              data: [
+                fakeLog({ api: 'my-api', plan: 'my-plan', status: 201, timestamp: 1466424490000 }),
+                fakeLog({ api: 'my-api-2', plan: 'my-plan', status: 204, timestamp: 1566424490000 }),
+              ],
+              metadata: {
+                'my-api': { name: 'My API', version: '1.0' },
+                'my-api-2': { name: 'My API 2', version: '2.0' },
+                'my-plan': { name: 'My Plan' },
+                data: { total: 3 },
+              },
+            }),
+            1,
+            `(api:\\"${API_ID}\\")`,
+          );
+          expectGetSubscriptions(
+            fakeSubscriptionResponse({
+              data: [fakeSubscription({ api: API_ID })],
+              metadata: { [API_ID]: { name: 'API 1', apiVersion: '99' } },
+            }),
+          );
+          fixture.detectChanges();
+        });
+
+        it('should have api pre-selected', async () => {
+          expect(await noChipFiltersDisplayed()).toEqual(false);
+          const apiFilter = await getApiSelection();
+          expect(await apiFilter.isEmpty()).toEqual(false);
+          expect(await apiFilter.getValueText()).toEqual('API 1 (99)');
+        });
+
+        it('should have search button disabled on load', async () => {
+          const searchButton = await getSearchButton();
+          expect(await searchButton.isDisabled()).toEqual(true);
+        });
+
+        it('should reset filter', async () => {
+          const resetButton = await getResetFilterButton();
+          expect(await resetButton.isDisabled()).toEqual(false);
+          await resetButton.click();
+
+          expectGetApplicationLogs(fakeLogsResponse());
+          fixture.detectChanges();
+        });
+      });
+      describe('Two APIs in query params', () => {
+        const API_ID_1 = 'api-id-1';
+        const API_ID_2 = 'api-id-2';
+        const API_ID_3 = 'api-id-3';
+        beforeEach(async () => {
+          await init({ apis: [API_ID_1, API_ID_2] });
+
+          expectGetApplicationLogs(
+            fakeLogsResponse({
+              data: [
+                fakeLog({ api: 'my-api', plan: 'my-plan', status: 201, timestamp: 1466424490000 }),
+                fakeLog({ api: 'my-api-2', plan: 'my-plan', status: 204, timestamp: 1566424490000 }),
+              ],
+              metadata: {
+                'my-api': { name: 'My API', version: '1.0' },
+                'my-api-2': { name: 'My API 2', version: '2.0' },
+                'my-plan': { name: 'My Plan' },
+                data: { total: 3 },
+              },
+            }),
+            1,
+            `(api:\\"${API_ID_1}\\" OR \\"${API_ID_2}\\")`,
+          );
+          expectGetSubscriptions(
+            fakeSubscriptionResponse({
+              data: [fakeSubscription({ api: API_ID_1 }), fakeSubscription({ api: API_ID_2 }), fakeSubscription({ api: API_ID_3 })],
+              metadata: {
+                [API_ID_1]: { name: 'API 1', apiVersion: '99' },
+                [API_ID_2]: { name: 'API 2', apiVersion: '1' },
+                [API_ID_3]: { name: 'API 3', apiVersion: '24' },
+              },
+            }),
+          );
+          fixture.detectChanges();
+        });
+
+        it('should have two apis pre-selected', async () => {
+          expect(await harnessLoader.getAllHarnesses(MatChipHarness)).toHaveLength(2);
+
+          const api1Chip = await getChipFilter('API 1 (99)');
+          expect(api1Chip).toBeTruthy();
+
+          const api2Chip = await getChipFilter('API 2 (1)');
+          expect(api2Chip).toBeTruthy();
+
+          const apiFilter = await getApiSelection();
+          expect(await apiFilter.isEmpty()).toEqual(false);
+          expect(await apiFilter.getValueText()).toEqual('API 1 (99), API 2 (1)');
+        });
+      });
+    });
+  });
+
+  function expectGetApplicationLogs(logsResponse: LogsResponse, page: number = 1, query?: string) {
     const currentDateInMilliseconds = MOCK_DATE.getTime();
     const yesterdayInMilliseconds = currentDateInMilliseconds - 86400000;
     httpTestingController
       .expectOne(
-        `${TESTING_BASE_URL}/applications/${APP_ID}/logs?page=${page}&size=10&from=${yesterdayInMilliseconds}&to=${currentDateInMilliseconds}&order=DESC&field=@timestamp`,
+        `${TESTING_BASE_URL}/applications/${APP_ID}/logs?page=${page}&size=10&from=${yesterdayInMilliseconds}&to=${currentDateInMilliseconds}&order=DESC&field=@timestamp` +
+          `${query ? '&query=' + query : ''}`,
       )
       .flush(logsResponse);
+  }
+
+  function expectGetSubscriptions(subscriptionsResponse: SubscriptionsResponse) {
+    httpTestingController
+      .expectOne(`${TESTING_BASE_URL}/subscriptions?applicationId=${APP_ID}&statuses=ACCEPTED&statuses=PAUSED&size=-1`)
+      .flush(subscriptionsResponse);
   }
 
   function getNoLogsMessageSection(): DebugElement {
@@ -363,5 +521,24 @@ describe('ApplicationTabLogsComponent', () => {
 
   async function getCurrentPaginationPage(): Promise<MatButtonHarness> {
     return await harnessLoader.getHarness(MatButtonHarness.with({ selector: '[aria-label="Current page of logs"]' }));
+  }
+
+  async function noChipFiltersDisplayed(): Promise<boolean> {
+    return await harnessLoader.getAllHarnesses(MatChipHarness).then(harnesses => harnesses.length === 0);
+  }
+
+  async function getChipFilter(text: string): Promise<MatChipHarness> {
+    return await harnessLoader.getHarness(MatChipHarness.with({ text }));
+  }
+
+  async function getApiSelection(): Promise<MatSelectHarness> {
+    return await harnessLoader.getHarness(MatSelectHarness.with({ selector: '[aria-label="Filter by API"]' }));
+  }
+
+  async function getResetFilterButton(): Promise<MatButtonHarness> {
+    return await harnessLoader.getHarness(MatButtonHarness.with({ selector: '[aria-label="Reset filters"]' }));
+  }
+  async function getSearchButton(): Promise<MatButtonHarness> {
+    return await harnessLoader.getHarness(MatButtonHarness.with({ selector: '[aria-label="Apply filters"]' }));
   }
 });
