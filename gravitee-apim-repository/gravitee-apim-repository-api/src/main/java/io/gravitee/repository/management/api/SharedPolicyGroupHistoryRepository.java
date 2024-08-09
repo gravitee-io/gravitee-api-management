127925ba20e7598f71d1dcb10fa318aea4e1d019
ThibaudAv <thibaud.avenier@gmail.com>
2024-08-09 15:06:57 +0200

    feat(repository): impl SharedPolicyGroupHistoryRepository with search & searchLastDeployedAtGroupById

---
+++ gravitee-apim-repository/gravitee-apim-repository-api/src/main/java/io/gravitee/repository/management/api/SharedPolicyGroupHistoryRepository.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright © 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.repository.management.api;
+
+import io.gravitee.common.data.domain.Page;
+import io.gravitee.repository.exceptions.TechnicalException;
+import io.gravitee.repository.management.api.search.Pageable;
+import io.gravitee.repository.management.api.search.SharedPolicyGroupHistoryCriteria;
+import io.gravitee.repository.management.api.search.Sortable;
+import io.gravitee.repository.management.model.SharedPolicyGroup;
+
+public interface SharedPolicyGroupHistoryRepository extends CrudRepository<SharedPolicyGroup, String> {
+    Page<SharedPolicyGroup> search(SharedPolicyGroupHistoryCriteria criteria, Pageable pageable, Sortable sortable)
+        throws TechnicalException;
+
+    Page<SharedPolicyGroup> searchLatestBySharedPolicyPolicyGroupId(String environmentId, Pageable pageable) throws TechnicalException;
+}
