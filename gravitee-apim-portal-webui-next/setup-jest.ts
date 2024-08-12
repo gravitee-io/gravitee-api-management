e36e4219c73e77bf5913385c41e7bd519ba7acc7
jourdiw <jourdi.waller@graviteesource.com>
2024-08-12 10:33:53 +0200

    test(portal-next): centralize system time to setup
    (cherry picked from commit 01d38dad057338cd63db86184dab7df743f87355)

--- gravitee-apim-portal-webui-next/setup-jest.ts
+++ gravitee-apim-portal-webui-next/setup-jest.ts
@@ -4,3 +4,7 @@ import '@angular/localize/init';
 // mocking swagger-ui for tests as it contains some JS incompatible with Jest
 // This means we will not be able to test Swagger in our components tests
 jest.mock('swagger-ui', () => jest.fn(() => ({ initOAuth: () => jest.fn() })));
+
+// Set the mock date globally for all tests
+const MOCK_DATE = new Date(1466424490000); // UTC Time: Mon Jun 20 2016 12:08:10.000
+jest.useFakeTimers({ advanceTimers: true }).setSystemTime(MOCK_DATE);
