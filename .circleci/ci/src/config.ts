8966e7dc7993f636912b62a3da2e99f52aabf502
Julien Giovaresco <dev@giovaresco.fr>
2024-08-14 14:13:35 +0200

    ci: configure Aqua filesystem scans on pull-requests pipeline
    (cherry picked from commit 1da58c7cec14160784feacd82307bb6b10cfca12)

--- .circleci/ci/src/config.ts
+++ .circleci/ci/src/config.ts
@@ -83,6 +83,7 @@ const maven = {
 };
 
 const orbs = {
+  aquasec: '1.0.0',
   artifactory: '1.0.1',
   awsCli: '2.0.6',
   awsS3: '3.0.0',
@@ -94,6 +95,9 @@ const orbs = {
 };
 
 const secrets = {
+  aquaKey: 'keeper://QeHHkvALPob4pgs1hMd9Gw/custom_field/API Key',
+  aquaSecret: 'keeper://QeHHkvALPob4pgs1hMd9Gw/custom_field/Secret',
+  aquaScannerKey: 'keeper://QeHHkvALPob4pgs1hMd9Gw/custom_field/ScannerToken',
   artifactoryApiKey: 'keeper://R7NuqoW0KD-8l-kjx0-PgQ/field/password',
   artifactoryUser: 'keeper://R7NuqoW0KD-8l-kjx0-PgQ/field/login',
   awsAccessKeyId: 'keeper://Mqmplmfu17bDR5XRLmO1mQ/field/password',
