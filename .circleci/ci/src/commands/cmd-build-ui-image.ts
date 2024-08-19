d3fca00d95e5ab3a8c378632c3a56c367140ae10
Julien Giovaresco <dev@giovaresco.fr>
2024-08-19 08:11:09 +0200

    ci: configure Aqua docker scans on pull-requests pipeline
    (cherry picked from commit 0c8dee7210f81f74c12ccf285cf387c4f2803571)

--- .circleci/ci/src/commands/cmd-build-ui-image.ts
+++ .circleci/ci/src/commands/cmd-build-ui-image.ts
@@ -57,8 +57,43 @@ export class BuildUiImageCommand {
     ];
 
     if (isSupportBranchOrMaster(environment.branch)) {
-      dynamicConfig.importOrb(orbs.keeper);
-
+      dynamicConfig.importOrb(orbs.keeper).importOrb(orbs.aquasec);
+      steps.push(
+        new reusable.ReusedCommand(orbs.keeper.commands['env-export'], {
+          'secret-url': config.secrets.aquaKey,
+          'var-name': 'AQUA_KEY',
+        }),
+        new reusable.ReusedCommand(orbs.keeper.commands['env-export'], {
+          'secret-url': config.secrets.aquaSecret,
+          'var-name': 'AQUA_SECRET',
+        }),
+        new reusable.ReusedCommand(orbs.keeper.commands['env-export'], {
+          'secret-url': config.secrets.aquaRegistryUsername,
+          'var-name': 'AQUA_USERNAME',
+        }),
+        new reusable.ReusedCommand(orbs.keeper.commands['env-export'], {
+          'secret-url': config.secrets.aquaRegistryPassword,
+          'var-name': 'AQUA_PASSWORD',
+        }),
+        new reusable.ReusedCommand(orbs.keeper.commands['env-export'], {
+          'secret-url': config.secrets.aquaScannerKey,
+          'var-name': 'SCANNER_TOKEN',
+        }),
+        new reusable.ReusedCommand(orbs.keeper.commands['env-export'], {
+          'secret-url': config.secrets.githubApiToken,
+          'var-name': 'GITHUB_TOKEN',
+        }),
+        new reusable.ReusedCommand(orbs.aquasec.commands['install_billy']),
+        new reusable.ReusedCommand(orbs.aquasec.commands['pull_aqua_scanner_image']),
+        new reusable.ReusedCommand(orbs.aquasec.commands['register_artifact'], {
+          artifact_to_register: `graviteeio.azurecr.io/<< parameters.docker-image-name >>:${tag}`,
+          debug: true,
+        }),
+        new reusable.ReusedCommand(orbs.aquasec.commands['scan_docker_image'], {
+          docker_image_to_scan: `graviteeio.azurecr.io/<< parameters.docker-image-name >>:${tag}`,
+          scanner_url: config.aqua.scannerUrl,
+        }),
+      );
       const addDockerImageInSnykCommand = AddDockerImageInSnykCommand.get();
       dynamicConfig.addReusableCommand(addDockerImageInSnykCommand);
 
