0ade6a4abb9ad82cad5ffea13e6ae522caa4feb4
Ouahid Khelifi <ouahid.khelifi@gmail.com>
2024-08-19 16:33:24 +0200

    fix: decrypt API properties when we use the debug mode

--- gravitee-apim-gateway/gravitee-apim-gateway-services/gravitee-apim-gateway-services-debug/src/main/java/io/gravitee/gateway/reactive/debug/DebugReactorEventListener.java
+++ gravitee-apim-gateway/gravitee-apim-gateway-services/gravitee-apim-gateway-services-debug/src/main/java/io/gravitee/gateway/reactive/debug/DebugReactorEventListener.java
@@ -18,7 +18,10 @@ package io.gravitee.gateway.reactive.debug;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.gravitee.common.event.Event;
 import io.gravitee.common.event.EventManager;
+import io.gravitee.common.util.DataEncryptor;
 import io.gravitee.definition.model.HttpRequest;
+import io.gravitee.definition.model.Properties;
+import io.gravitee.definition.model.Property;
 import io.gravitee.definition.model.v4.plan.PlanStatus;
 import io.gravitee.gateway.api.http.HttpHeaderNames;
 import io.gravitee.gateway.debug.definition.DebugApi;
@@ -36,11 +39,11 @@ import io.gravitee.repository.management.model.ApiDebugStatus;
 import io.vertx.core.MultiMap;
 import io.vertx.core.http.HttpClientOptions;
 import io.vertx.core.http.HttpMethod;
-import io.vertx.core.http.HttpServer;
 import io.vertx.core.http.RequestOptions;
 import io.vertx.core.http.impl.headers.HeadersMultiMap;
 import io.vertx.core.net.OpenSSLEngineOptions;
 import io.vertx.rxjava3.core.Vertx;
+import java.security.GeneralSecurityException;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -62,6 +65,7 @@ public class DebugReactorEventListener extends ReactorEventListener {
     private final VertxDebugHttpClientConfiguration debugHttpClientConfiguration;
     private final ReactorHandlerRegistry reactorHandlerRegistry;
     private final OrganizationManager organizationManager;
+    private final DataEncryptor dataEncryptor;
 
     public DebugReactorEventListener(
         final Vertx vertx,
@@ -70,7 +74,8 @@ public class DebugReactorEventListener extends ReactorEventListener {
         final ObjectMapper objectMapper,
         final VertxDebugHttpClientConfiguration debugHttpClientConfiguration,
         final ReactorHandlerRegistry reactorHandlerRegistry,
-        OrganizationManager organizationManager
+        OrganizationManager organizationManager,
+        DataEncryptor dataEncryptor
     ) {
         super(eventManager, reactorHandlerRegistry);
         this.vertx = vertx;
@@ -80,6 +85,7 @@ public class DebugReactorEventListener extends ReactorEventListener {
         this.debugHttpClientConfiguration = debugHttpClientConfiguration;
         this.reactorHandlerRegistry = reactorHandlerRegistry;
         this.organizationManager = organizationManager;
+        this.dataEncryptor = dataEncryptor;
     }
 
     @Override
@@ -151,6 +157,10 @@ public class DebugReactorEventListener extends ReactorEventListener {
                 event.getPayload(),
                 io.gravitee.definition.model.debug.DebugApi.class
             );
+
+            if (null != eventPayload.getProperties()) {
+                decryptProperties(eventPayload.getProperties());
+            }
 
             eventPayload.setPlans(
                 eventPayload
@@ -245,5 +255,19 @@ public class DebugReactorEventListener extends ReactorEventListener {
             .getProperties()
             .put(io.gravitee.repository.management.model.Event.EventProperties.API_DEBUG_STATUS.getValue(), apiDebugStatus.name());
         eventRepository.update(debugEvent);
+    }
+
+    private void decryptProperties(final Properties properties) {
+        for (Property property : properties.getProperties()) {
+            if (property.isEncrypted()) {
+                try {
+                    property.setValue(dataEncryptor.decrypt(property.getValue()));
+                    property.setEncrypted(false);
+                    properties.getValues().put(property.getKey(), property.getValue());
+                } catch (GeneralSecurityException e) {
+                    logger.error("Error decrypting API property value for key {}", property.getKey(), e);
+                }
+            }
+        }
     }
 }
