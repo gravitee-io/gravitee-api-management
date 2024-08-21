b72a99bb5424999364702cd61d3b43db2d47caa9
Antoine CORDIER <antoine.cordier@graviteesource.com>
2024-08-21 11:09:52 +0200

    refactor: share CRD members validator class

--- gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/apim/core/member/domain_service/ValidateCRDMembersDomainService.java
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/apim/core/member/domain_service/ValidateCRDMembersDomainService.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.gravitee.apim.core.api.domain_service;
+package io.gravitee.apim.core.member.domain_service;
 
 import io.gravitee.apim.core.DomainService;
 import io.gravitee.apim.core.api.model.crd.MemberCRD;
