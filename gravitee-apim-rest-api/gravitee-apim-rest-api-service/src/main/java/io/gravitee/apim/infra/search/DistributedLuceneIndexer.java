cf5e6e72d64b71e09727fb728cb44102f9d490c8
Florent CHAMFROY <florent.chamfroy@graviteesource.com>
2024-08-30 16:50:51 +0200

    fix: explicitly declare lazy & autowired on a dependency

--- gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/apim/infra/search/DistributedLuceneIndexer.java
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/apim/infra/search/DistributedLuceneIndexer.java
@@ -20,17 +20,22 @@ import io.gravitee.rest.api.model.search.Indexable;
 import io.gravitee.rest.api.service.common.ExecutionContext;
 import io.gravitee.rest.api.service.search.SearchEngineService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
 /**
  * @author Antoine CORDIER (antoine.cordier at graviteesource.com)
  * @author GraviteeSource Team
  */
-@RequiredArgsConstructor
 @Service
 public class DistributedLuceneIndexer implements Indexer {
 
     private final SearchEngineService searchEngineService;
+
+    public DistributedLuceneIndexer(@Lazy SearchEngineService searchEngineService) {
+        this.searchEngineService = searchEngineService;
+    }
 
     @Override
     public void index(IndexationContext context, Indexable indexable) {
