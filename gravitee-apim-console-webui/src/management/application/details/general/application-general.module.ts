b7e54578094d5cbab4ff4e1f9ed82bc51949e63b
Marek Mazur <marek.mazur@graviteesource.com>
2024-09-09 09:28:24 +0200

    fix(console): add information on OAuth settings update
    (cherry picked from commit 69476f641122eb8aef08d3ccad78daea3e04bbf1)

--- gravitee-apim-console-webui/src/management/application/details/general/application-general.module.ts
+++ gravitee-apim-console-webui/src/management/application/details/general/application-general.module.ts
@@ -19,6 +19,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
 import {
   GioAvatarModule,
+  GioBannerModule,
   GioClipboardModule,
   GioFormFilePickerModule,
   GioFormFocusInvalidModule,
@@ -72,6 +73,7 @@ import { GioPermissionModule } from '../../../../shared/components/gio-permissio
 
     ReactiveFormsModule,
     RouterModule,
+    GioBannerModule,
   ],
 })
 export class ApplicationGeneralModule {}
