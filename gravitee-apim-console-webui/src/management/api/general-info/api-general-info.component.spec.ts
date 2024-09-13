eb02f4583868c8085ef4a1b182aa99f192301cca
mukul-tyagi08 <mukul.tyagi@graviteesource.com>
2024-09-13 11:54:20 +0530

    fix: info page refresh when duplicating the API

--- gravitee-apim-console-webui/src/management/api/general-info/api-general-info.component.spec.ts
+++ gravitee-apim-console-webui/src/management/api/general-info/api-general-info.component.spec.ts
@@ -33,6 +33,7 @@ import { MatButtonHarness } from '@angular/material/button/testing';
 import { MatDialogHarness } from '@angular/material/dialog/testing';
 import { MatCheckboxHarness } from '@angular/material/checkbox/testing';
 import { ActivatedRoute, Router } from '@angular/router';
+import { of } from 'rxjs';
 
 import { ApiGeneralInfoModule } from './api-general-info.module';
 import { ApiGeneralInfoComponent } from './api-general-info.component';
@@ -59,9 +60,7 @@ describe('ApiGeneralInfoComponent', () => {
         {
           provide: ActivatedRoute,
           useValue: {
-            snapshot: {
-              params: { apiId: API_ID },
-            },
+            params: of({ apiId: API_ID }),
           },
         },
         {
