a4aeeab0d6371fced0d153024aa4da97a16f7dde
mukul-tyagi08 <mukul.tyagi@graviteesource.com>
2024-09-16 13:47:48 +0530

    fix: api history showing warning for all policies

--- gravitee-apim-console-webui/src/management/api/audit/history/apiHistory.controller.ajs.ts
+++ gravitee-apim-console-webui/src/management/api/audit/history/apiHistory.controller.ajs.ts
@@ -163,21 +163,6 @@ class ApiHistoryControllerAjs {
 
   $onInit() {
     this.studio = document.querySelector('gv-policy-studio');
-    if (this.hasDesign()) {
-      Promise.all([
-        this.PolicyService.list(true, true),
-        this.ResourceService.list(true, true),
-        this.ApiService.getFlowSchemaForm(),
-        this.FlowService.getConfigurationSchema(),
-      ]).then(([policies, resources, flowSchema, configurationSchema]) => {
-        this.studio.policies = policies.data;
-        this.studio.resourceTypes = resources.data;
-        this.studio.flowSchema = flowSchema.data;
-        this.studio.configurationSchema = configurationSchema.data;
-        this.studio.propertyProviders = propertyProviders;
-      });
-    }
-
     Promise.all([
       this.ApiService.get(this.activatedRoute.snapshot.params.apiId),
       this.ApiService.isAPISynchronized(this.activatedRoute.snapshot.params.apiId),
@@ -198,6 +183,20 @@ class ApiHistoryControllerAjs {
           }
         : undefined;
       this.appendNextPage(toDeployEventTimeline);
+      if (this.hasDesign()) {
+        Promise.all([
+          this.PolicyService.list(true, true),
+          this.ResourceService.list(true, true),
+          this.ApiService.getFlowSchemaForm(),
+          this.FlowService.getConfigurationSchema(),
+        ]).then(([policies, resources, flowSchema, configurationSchema]) => {
+          this.studio.policies = policies.data;
+          this.studio.resourceTypes = resources.data;
+          this.studio.flowSchema = flowSchema.data;
+          this.studio.configurationSchema = configurationSchema.data;
+          this.studio.propertyProviders = propertyProviders;
+        });
+      }
     });
   }
 
