d93a29f8565dafd43df00b5e5e147b0b12ecf172
Guillaume Cusnieux <guillaume.cusnieux@graviteesource.com>
2024-09-17 11:56:46 +0200

    refactor: make default notification are linked to the environment

--- gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/rest/api/service/cockpit/command/handler/DeleteEnvironmentCommandHandler.java
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/rest/api/service/cockpit/command/handler/DeleteEnvironmentCommandHandler.java
@@ -68,7 +68,6 @@ import io.gravitee.repository.management.model.MetadataReferenceType;
 import io.gravitee.repository.management.model.NotificationReferenceType;
 import io.gravitee.repository.management.model.PageReferenceType;
 import io.gravitee.repository.management.model.ParameterReferenceType;
-import io.gravitee.repository.management.model.PortalNotificationDefaultReferenceId;
 import io.gravitee.repository.management.model.RatingReferenceType;
 import io.gravitee.repository.management.model.RoleReferenceType;
 import io.gravitee.repository.management.model.ThemeReferenceType;
@@ -326,11 +325,8 @@ public class DeleteEnvironmentCommandHandler implements CommandHandler<DeleteEnv
         eventService.deleteOrUpdateEventsByEnvironment(environment.getId());
 
         // Always default for environment
-        portalNotificationConfigRepository.deleteByReferenceIdAndReferenceType(
-            PortalNotificationDefaultReferenceId.DEFAULT.name(),
-            NotificationReferenceType.PORTAL
-        );
-        genericNotificationConfigRepository.deleteByReferenceIdAndReferenceType(environment.getId(), NotificationReferenceType.PORTAL);
+        portalNotificationConfigRepository.deleteByReferenceIdAndReferenceType(environment.getId(), NotificationReferenceType.ENVIRONMENT);
+        genericNotificationConfigRepository.deleteByReferenceIdAndReferenceType(environment.getId(), NotificationReferenceType.ENVIRONMENT);
         themeRepository.deleteByReferenceIdAndReferenceType(environment.getId(), ThemeReferenceType.ENVIRONMENT);
         identityProviderActivationRepository.deleteByReferenceIdAndReferenceType(
             environment.getId(),
