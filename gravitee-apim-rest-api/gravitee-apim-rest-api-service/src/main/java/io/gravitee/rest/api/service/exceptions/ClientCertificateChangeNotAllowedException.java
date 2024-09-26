4ef906e1d3f683337141a4d23481627918b34fd5
Marek Mazur <marek.mazur@graviteesource.com>
2024-09-26 13:01:01 +0200

    fix: prevent user from changing app tls certificate

---
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/rest/api/service/exceptions/ClientCertificateChangeNotAllowedException.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright © 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.rest.api.service.exceptions;
+
+import io.gravitee.common.http.HttpStatusCode;
+import java.util.Map;
+
+public class ClientCertificateChangeNotAllowedException extends AbstractManagementException {
+
+    @Override
+    public String getMessage() {
+        return "Client certificate change is not allowed.";
+    }
+
+    @Override
+    public int getHttpStatusCode() {
+        return HttpStatusCode.BAD_REQUEST_400;
+    }
+
+    @Override
+    public String getTechnicalCode() {
+        return "application.certificate.changeNotAllowed";
+    }
+
+    @Override
+    public Map<String, String> getParameters() {
+        return null;
+    }
+}
