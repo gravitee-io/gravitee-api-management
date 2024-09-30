5857fa638d45723e15f857f90db5e0c06804bd01
Ouahid Khelifi <ouahid.khelifi@gmail.com>
2024-09-30 17:35:53 +0200

    fix: introduce organization user token permission
    (cherry picked from commit a72daadc6bf70f32424b58478ff903e485e0aa4d)
    (cherry picked from commit d373607edc759a4b3ffa44712e973cb293286b61)

--- gravitee-apim-rest-api/gravitee-apim-rest-api-model/src/main/java/io/gravitee/rest/api/model/permissions/OrganizationPermission.java
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-model/src/main/java/io/gravitee/rest/api/model/permissions/OrganizationPermission.java
@@ -37,7 +37,8 @@ public enum OrganizationPermission implements Permission {
     ENTRYPOINT("ENTRYPOINT", 2100),
     POLICIES("POLICIES", 2200),
     AUDIT("AUDIT", 2300),
-    LICENSE_MANAGEMENT("LICENSE_MANAGEMENT", 2400);
+    LICENSE_MANAGEMENT("LICENSE_MANAGEMENT", 2400),
+    USER_TOKEN("USER_TOKEN", 2500);
 
     String name;
     int mask;
