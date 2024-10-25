c2415b18e1fa53a523b6376e57584c0ccd14b316
Rémi Sultan <remi.sultan@gmail.com>
2024-10-25 14:46:29 +0200

    feat: spec gen response command handler (#9680)
    * feat: spec gen response command handler
    
    * test: oas provider returning null in test
    
    * chore: bump dependency

---
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/apim/core/specgen/use_case/BuildSpecGenPageResponseUseCase.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright © 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.apim.core.specgen.use_case;
+
+import static io.gravitee.apim.core.documentation.model.Page.ReferenceType.API;
+import static io.gravitee.apim.core.documentation.model.Page.Visibility.*;
+import static io.gravitee.rest.api.service.common.GraviteeContext.getExecutionContext;
+import static java.time.LocalDateTime.now;
+import static java.time.format.DateTimeFormatter.ofPattern;
+import static org.apache.commons.lang3.RandomStringUtils.randomAlphanumeric;
+
+import io.gravitee.apim.core.UseCase;
+import io.gravitee.apim.core.documentation.model.Page;
+import io.gravitee.apim.core.documentation.model.Page.Type;
+import io.gravitee.apim.core.specgen.model.ApiSpecGen;
+import io.gravitee.apim.core.specgen.query_service.ApiSpecGenQueryService;
+import io.gravitee.apim.core.specgen.service_provider.OasProvider;
+import io.gravitee.definition.model.v4.ApiType;
+import io.reactivex.rxjava3.core.Maybe;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author Rémi SULTAN (remi.sultan at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+@Slf4j
+@RequiredArgsConstructor
+@UseCase
+public class BuildSpecGenPageResponseUseCase {
+
+    public static final String POWERED_BY = "poweredBy";
+    public static final String NEWT_AI = "NewtAI";
+    public static final Map<String, String> METADATA = Map.of(POWERED_BY, NEWT_AI);
+    public static final String DATE_FORMAT_PATTERN = "yyyy-MM-dd HH:mm:ss";
+
+    private final ApiSpecGenQueryService queryService;
+    private final OasProvider oasProvider;
+
+    public Maybe<Page> execute(Input input) {
+        return queryService
+            .rxFindByIdAndType(getExecutionContext(), input.apiId(), ApiType.PROXY)
+            .observeOn(Schedulers.computation())
+            .map(api -> buildPage(api, oasProvider.decorateSpecification(api, input.content())))
+            .observeOn(Schedulers.io());
+    }
+
+    private static Page buildPage(ApiSpecGen api, String rawSpec) {
+        String now = now().format(ofPattern(DATE_FORMAT_PATTERN));
+        return Page
+            .builder()
+            .referenceId(api.id())
+            .referenceType(API)
+            .name(api.name() + " by NewtAI - " + now)
+            .content(rawSpec)
+            .type(Type.SWAGGER)
+            .visibility(PRIVATE)
+            .homepage(false)
+            .published(false)
+            .metadata(METADATA)
+            .build();
+    }
+
+    public record Input(String apiId, String content) {}
+}
---
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/test/java/io/gravitee/rest/api/service/cockpit/command/handler/SpecGenResponseCommandHandlerTest.java
@@ -0,0 +1,170 @@
+/*
+ * Copyright © 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.rest.api.service.cockpit.command.handler;
+
+import static io.gravitee.apim.core.documentation.model.Page.ReferenceType.API;
+import static io.gravitee.apim.core.documentation.model.Page.Visibility.PRIVATE;
+import static io.gravitee.apim.core.specgen.use_case.BuildSpecGenPageResponseUseCase.METADATA;
+import static io.gravitee.cockpit.api.command.v1.CockpitCommandType.SPEC_GEN_RESPONSE;
+import static io.gravitee.exchange.api.command.CommandStatus.ERROR;
+import static io.gravitee.exchange.api.command.CommandStatus.SUCCEEDED;
+import static io.gravitee.rest.api.service.common.UuidString.generateRandom;
+import static java.util.Objects.nonNull;
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import io.gravitee.apim.core.documentation.model.Page;
+import io.gravitee.apim.core.documentation.model.Page.Type;
+import io.gravitee.apim.core.documentation.use_case.ApiCreateDocumentationPageUseCase;
+import io.gravitee.apim.core.documentation.use_case.ApiCreateDocumentationPageUseCase.Output;
+import io.gravitee.apim.core.specgen.use_case.BuildSpecGenPageResponseUseCase;
+import io.gravitee.apim.core.specgen.use_case.BuildSpecGenPageResponseUseCase.Input;
+import io.gravitee.cockpit.api.command.v1.CockpitCommandType;
+import io.gravitee.cockpit.api.command.v1.specgen.SpecGenCommandPayload;
+import io.gravitee.cockpit.api.command.v1.specgen.response.SpecGenResponseCommand;
+import io.gravitee.exchange.api.command.CommandStatus;
+import io.gravitee.rest.api.service.common.GraviteeContext;
+import io.gravitee.rest.api.service.common.UuidString;
+import io.gravitee.spec.gen.api.SpecGenResponse;
+import io.reactivex.rxjava3.core.Maybe;
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+/**
+ * @author Rémi SULTAN (remi.sultan at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+@ExtendWith(MockitoExtension.class)
+class SpecGenResponseCommandHandlerTest {
+
+    @Mock
+    BuildSpecGenPageResponseUseCase buildSpecGenPageResponseUseCase;
+
+    @Mock
+    ApiCreateDocumentationPageUseCase createDocumentationPageUseCase;
+
+    SpecGenResponseCommandHandler specGenResponseCommandHandler;
+
+    @BeforeEach
+    void setUp() {
+        specGenResponseCommandHandler = new SpecGenResponseCommandHandler(buildSpecGenPageResponseUseCase, createDocumentationPageUseCase);
+    }
+
+    @Test
+    void must_not_save_page_due_to_build_page_error() {
+        final String errorMessage = "An error has occurred";
+        when(buildSpecGenPageResponseUseCase.execute(any())).thenReturn(Maybe.error(new IllegalArgumentException(errorMessage)));
+
+        assertThat(specGenResponseCommandHandler.supportType()).isEqualTo(SPEC_GEN_RESPONSE.name());
+        specGenResponseCommandHandler
+            .handle(new SpecGenResponseCommand(generatePayload()))
+            .test()
+            .awaitDone(2, SECONDS)
+            .assertComplete()
+            .assertNoErrors()
+            .assertValue(reply -> nonNull(reply.getCommandId()))
+            .assertValue(reply -> ERROR.equals(reply.getCommandStatus()))
+            .assertValue(reply -> errorMessage.equals(reply.getErrorDetails()));
+    }
+
+    @NotNull
+    private static SpecGenCommandPayload<SpecGenResponse> generatePayload() {
+        return new SpecGenCommandPayload<>(
+            generateRandom(),
+            generateRandom(),
+            generateRandom(),
+            generateRandom(),
+            new SpecGenResponse(generateRandom(), "openapi: 3.0.3", generateRandom())
+        );
+    }
+
+    @Test
+    void must_not_save_page_due_to_create_documentation_page_error() {
+        final String errorMessage = "An error has occurred";
+        when(buildSpecGenPageResponseUseCase.execute(any())).thenReturn(Maybe.just(getPage()));
+
+        var payload = generatePayload();
+
+        GraviteeContext.setCurrentEnvironment(payload.environmentId());
+        GraviteeContext.setCurrentOrganization(payload.organizationId());
+
+        when(createDocumentationPageUseCase.execute(any())).thenThrow(new IllegalArgumentException(errorMessage));
+
+        assertThat(specGenResponseCommandHandler.supportType()).isEqualTo(SPEC_GEN_RESPONSE.name());
+        specGenResponseCommandHandler
+            .handle(new SpecGenResponseCommand(payload))
+            .test()
+            .awaitDone(2, SECONDS)
+            .assertComplete()
+            .assertNoErrors()
+            .assertValue(reply -> nonNull(reply.getCommandId()))
+            .assertValue(reply -> ERROR.equals(reply.getCommandStatus()))
+            .assertValue(reply -> errorMessage.equals(reply.getErrorDetails()));
+    }
+
+    @Test
+    void must_save_page_due_to_create_documentation_page() {
+        final Page page = getPage();
+        when(buildSpecGenPageResponseUseCase.execute(any())).thenReturn(Maybe.just(page));
+
+        var payload = generatePayload();
+
+        GraviteeContext.setCurrentEnvironment(payload.environmentId());
+        GraviteeContext.setCurrentOrganization(payload.organizationId());
+
+        when(createDocumentationPageUseCase.execute(any())).thenReturn(new Output(page));
+
+        assertThat(specGenResponseCommandHandler.supportType()).isEqualTo(SPEC_GEN_RESPONSE.name());
+        specGenResponseCommandHandler
+            .handle(new SpecGenResponseCommand(payload))
+            .test()
+            .awaitDone(2, SECONDS)
+            .assertComplete()
+            .assertNoErrors()
+            .assertValue(reply -> nonNull(reply.getCommandId()))
+            .assertValue(reply -> SUCCEEDED.equals(reply.getCommandStatus()));
+    }
+
+    private static Page getPage() {
+        return Page
+            .builder()
+            .referenceId(generateRandom())
+            .referenceType(API)
+            .name("Api Name")
+            .content("openapi: 3.0.3")
+            .type(Type.SWAGGER)
+            .visibility(PRIVATE)
+            .homepage(false)
+            .hidden(true)
+            .published(false)
+            .metadata(METADATA)
+            .build();
+    }
+
+    @AfterEach
+    void after() {
+        GraviteeContext.cleanContext();
+    }
+}
