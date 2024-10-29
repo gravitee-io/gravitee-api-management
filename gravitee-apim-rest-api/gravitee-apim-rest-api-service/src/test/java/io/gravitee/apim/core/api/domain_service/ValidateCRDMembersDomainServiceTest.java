43a1979f9d8a0687f0c0f2d9f31e577217e3265c
Kamiel Ahmadpour <kamiel.ahmadpour@graviteesource.com>
2024-10-29 22:23:32 +0000

    fix: remove warning when role id exist
    see: https://gravitee.atlassian.net/browse/GKO-744

--- gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/test/java/io/gravitee/apim/core/api/domain_service/ValidateCRDMembersDomainServiceTest.java
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/test/java/io/gravitee/apim/core/api/domain_service/ValidateCRDMembersDomainServiceTest.java
@@ -15,7 +15,6 @@
  */
 package io.gravitee.apim.core.api.domain_service;
 
-import static io.gravitee.apim.core.member.model.SystemRole.PRIMARY_OWNER;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import inmemory.RoleQueryServiceInMemory;
@@ -30,6 +29,7 @@ import io.gravitee.apim.core.user.model.BaseUserEntity;
 import io.gravitee.apim.core.validation.Validator;
 import java.util.List;
 import java.util.Set;
+import java.util.UUID;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -42,6 +42,7 @@ class ValidateCRDMembersDomainServiceTest {
     static final String USER_SOURCE = "MEMORY";
     static final String USER_ID = "USER";
     static final String ACTOR_USER_ID = "ACTOR";
+    static final String ROLE_ID = UUID.randomUUID().toString();
 
     static final AuditInfo AUDIT_INFO = AuditInfo
         .builder()
@@ -70,7 +71,7 @@ class ValidateCRDMembersDomainServiceTest {
                     .name("USER")
                     .referenceType(Role.ReferenceType.ORGANIZATION)
                     .referenceId(ORG_ID)
-                    .id("user_role_id")
+                    .id(ROLE_ID)
                     .scope(Role.Scope.APPLICATION)
                     .build(),
                 Role
@@ -78,7 +79,7 @@ class ValidateCRDMembersDomainServiceTest {
                     .name("USER")
                     .referenceType(Role.ReferenceType.ORGANIZATION)
                     .referenceId(ORG_ID)
-                    .id("user_role_id")
+                    .id(ROLE_ID)
                     .scope(Role.Scope.API)
                     .build(),
                 Role
@@ -146,6 +147,19 @@ class ValidateCRDMembersDomainServiceTest {
         result.peek(
             sanitized -> assertThat(Set.copyOf(sanitized.members())).isEqualTo(expectedMembers),
             errors -> assertThat(errors).isEqualTo(List.of(Validator.Error.warning("member role [UNKNOWN] doesn't exist")))
+        );
+    }
+
+    @ParameterizedTest
+    @EnumSource(value = MembershipReferenceType.class, names = { "APPLICATION", "API" })
+    void should_not_return_warning_with_known_member_role(MembershipReferenceType referenceType) {
+        var members = Set.of(MemberCRD.builder().source(USER_SOURCE).sourceId(USER_ID).role(ROLE_ID).build());
+        var expectedMembers = Set.copyOf(members);
+        var result = cut.validateAndSanitize(new ValidateCRDMembersDomainService.Input(AUDIT_INFO, "", referenceType, members));
+
+        result.peek(
+            sanitized -> assertThat(Set.copyOf(sanitized.members())).isEqualTo(expectedMembers),
+            errors -> assertThat(errors).isEmpty()
         );
     }
 
