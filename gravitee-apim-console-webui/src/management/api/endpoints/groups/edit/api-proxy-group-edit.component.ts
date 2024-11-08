e23aa5405c050beff92b541389115a377093bac1
mukul-tyagi08 <mukul.tyagi@graviteesource.com>
2024-11-08 12:34:56 +0530

    fix: hide create bar on success
    (cherry picked from commit f54d1b0c779d771916b292a557f3864e2dd1df3c)

--- gravitee-apim-console-webui/src/management/api/endpoints/groups/edit/api-proxy-group-edit.component.ts
+++ gravitee-apim-console-webui/src/management/api/endpoints/groups/edit/api-proxy-group-edit.component.ts
@@ -108,7 +108,12 @@ export class ApiProxyGroupEditComponent implements OnInit, OnDestroy {
           groupIndex !== -1 ? api.proxy.groups.splice(groupIndex, 1, updatedGroup) : api.proxy.groups.push(updatedGroup);
           return this.apiService.update(api.id, api);
         }),
-        tap(() => this.snackBarService.success('Configuration successfully saved!')),
+        tap(() => {
+          this.snackBarService.success('Configuration successfully saved!');
+          this.initialGroupFormValue = this.groupForm.getRawValue();
+          this.groupForm.markAsPristine();
+          this.mode = 'edit';
+        }),
         catchError(({ error }) => {
           this.snackBarService.error(error.message);
           return EMPTY;
