vikrantgravitee/gravitee-api-management_vk

feat: add support for gko subscriptions
see https://gravitee.atlassian.net/browse/GKO-359
see https://gravitee.atlassian.net/browse/GKO-360
see https://gravitee.atlassian.net/browse/GKO-361

gravitee-apim-rest-api/gravitee-apim-rest-api-management-v2/gravitee-apim-rest-api-management-v2-rest/src/main/java/io/gravitee/rest/api/management/v2/rest/resource/api/ApiSubscriptionsResource.java
@@ -20,8 +20,11 @@ import static java.lang.String.format;
 import io.gravitee.apim.core.api_key.use_case.RevokeApiSubscriptionApiKeyUseCase;
 import io.gravitee.apim.core.audit.model.AuditActor;
 import io.gravitee.apim.core.audit.model.AuditInfo;
+import io.gravitee.apim.core.subscription.model.crd.SubscriptionSpec;
 import io.gravitee.apim.core.subscription.use_case.AcceptSubscriptionUseCase;
 import io.gravitee.apim.core.subscription.use_case.CloseSubscriptionUseCase;
+import io.gravitee.apim.core.subscription.use_case.DeleteSubscriptionSpecUseCase;
+import io.gravitee.apim.core.subscription.use_case.ImportSubscriptionSpecUseCase;
 import io.gravitee.apim.core.subscription.use_case.RejectSubscriptionUseCase;
 import io.gravitee.common.data.domain.Page;
 import io.gravitee.common.http.MediaType;
@@ -59,6 +62,7 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import jakarta.ws.rs.BeanParam;
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.DefaultValue;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
@@ -129,6 +133,12 @@ public class ApiSubscriptionsResource extends AbstractResource {
 
     @Inject
     private SecurityContext securityContext;
+
+    @Inject
+    private ImportSubscriptionSpecUseCase importSubscriptionSpecUseCase;
+
+    @Inject
+    private DeleteSubscriptionSpecUseCase deleteSubscriptionSpecUseCase;
 
     @PathParam("apiId")
     private String apiId;
@@ -303,6 +313,21 @@ public class ApiSubscriptionsResource extends AbstractResource {
         expandData(subscription, expands);
 
         return Response.ok(subscription).build();
+    }
+
+    @PUT
+    @Path("/spec/_import")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response importSubscriptionSpec(@Valid SubscriptionSpec spec) {
+        return Response.ok(importSubscriptionSpecUseCase.execute(new ImportSubscriptionSpecUseCase.Input(getAuditInfo(), spec))).build();
+    }
+
+    @DELETE
+    @Path("/spec/{id}")
+    public Response deleteSubscriptionSpec(@PathParam("id") String id) {
+        deleteSubscriptionSpecUseCase.execute(new DeleteSubscriptionSpecUseCase.Input(getAuditInfo(), id));
+        return Response.noContent().build();
     }
 
     @PUT

gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/apim/core/subscription/use_case/ImportSubscriptionSpecUseCase.java
@@ -1,0 +1,47 @@
+/*
+ * Copyright © 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.apim.core.subscription.use_case;
+
+import io.gravitee.apim.core.UseCase;
+import io.gravitee.apim.core.audit.model.AuditInfo;
+import io.gravitee.apim.core.subscription.crud_service.SubscriptionCrudService;
+import io.gravitee.apim.core.subscription.domain_service.SubscriptionSpecDomainService;
+import io.gravitee.apim.core.subscription.model.SubscriptionEntity;
+import io.gravitee.apim.core.subscription.model.crd.SubscriptionSpec;
+import io.gravitee.rest.api.service.exceptions.SubscriptionNotFoundException;
+import java.time.ZonedDateTime;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * @author Antoine CORDIER (antoine.cordier at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+@UseCase
+@RequiredArgsConstructor
+public class ImportSubscriptionSpecUseCase {
+
+    public record Input(AuditInfo auditInfo, SubscriptionSpec spec) {}
+
+    public record Output(String id, String status, ZonedDateTime startingAt, ZonedDateTime endingAt) {}
+
+    private final SubscriptionSpecDomainService domainService;
+
+    public Output execute(Input input) {
+        var subscription = domainService.createOrUpdate(input.auditInfo, input.spec);
+        return new Output(subscription.getId(), subscription.getStatus().name(), subscription.getStartingAt(), subscription.getEndingAt());
+    }
+}

gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/apim/infra/adapter/SubscriptionAdapter.java
@@ -23,6 +23,7 @@ import io.gravitee.apim.core.json.JsonProcessingException;
 import io.gravitee.apim.core.json.JsonSerializer;
 import io.gravitee.apim.core.subscription.model.SubscriptionConfiguration;
 import io.gravitee.apim.core.subscription.model.SubscriptionEntity;
+import io.gravitee.apim.core.subscription.model.crd.SubscriptionSpec;
 import io.gravitee.repository.management.model.Subscription;
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
@@ -82,6 +83,44 @@ public abstract class SubscriptionAdapter {
     @Mapping(source = "requestMessage", target = "request")
     @Mapping(source = "reasonMessage", target = "reason")
     public abstract io.gravitee.rest.api.model.SubscriptionEntity map(SubscriptionEntity subscription);
+
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "type", ignore = true)
+    @Mapping(target = "subscribedBy", ignore = true)
+    @Mapping(target = "requestMessage", ignore = true)
+    @Mapping(target = "reasonMessage", ignore = true)
+    @Mapping(target = "processedBy", ignore = true)
+    @Mapping(target = "processedAt", ignore = true)
+    @Mapping(target = "pausedAt", ignore = true)
+    @Mapping(target = "metadata", ignore = true)
+    @Mapping(target = "generalConditionsContentRevision", ignore = true)
+    @Mapping(target = "generalConditionsContentPageId", ignore = true)
+    @Mapping(target = "generalConditionsAccepted", ignore = true)
+    @Mapping(target = "failureCause", ignore = true)
+    @Mapping(target = "environmentId", ignore = true)
+    @Mapping(target = "daysToExpirationOnLastNotification", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "consumerStatus", ignore = true)
+    @Mapping(target = "consumerPausedAt", ignore = true)
+    @Mapping(target = "configuration", ignore = true)
+    @Mapping(target = "closedAt", ignore = true)
+    @Mapping(target = "clientId", ignore = true)
+    @Mapping(target = "clientCertificate", ignore = true)
+    public abstract SubscriptionEntity fromSpec(SubscriptionSpec spec);
+
+    @Mapping(target = "metadata", ignore = true)
+    @Mapping(target = "generalConditionsContentRevision", ignore = true)
+    @Mapping(target = "generalConditionsAccepted", ignore = true)
+    @Mapping(target = "configuration", ignore = true)
+    @Mapping(target = "request", ignore = true)
+    @Mapping(target = "apiKeyMode", ignore = true)
+    @Mapping(source = "applicationId", target = "application")
+    @Mapping(source = "planId", target = "plan")
+    public abstract io.gravitee.rest.api.model.NewSubscriptionEntity fromSpecForCreate(SubscriptionSpec spec);
+
+    @Mapping(target = "metadata", ignore = true)
+    @Mapping(target = "configuration", ignore = true)
+    public abstract io.gravitee.rest.api.model.UpdateSubscriptionEntity fromSpecForUpdate(SubscriptionSpec spec);
 
     @Named("deserializeConfiguration")
     public SubscriptionConfiguration deserializeConfiguration(String configuration) {

gravitee-io/gravitee-api-management

gravitee-apim-rest-api/gravitee-apim-rest-api-management-v2/gravitee-apim-rest-api-management-v2-rest/src/main/java/io/gravitee/rest/api/management/v2/rest/resource/api/ApiSubscriptionsResource.java
@@ -20,8 +20,11 @@ import static java.lang.String.format;
 import io.gravitee.apim.core.api_key.use_case.RevokeApiSubscriptionApiKeyUseCase;
 import io.gravitee.apim.core.audit.model.AuditActor;
 import io.gravitee.apim.core.audit.model.AuditInfo;
+import io.gravitee.apim.core.subscription.model.crd.SubscriptionCRDSpec;
 import io.gravitee.apim.core.subscription.use_case.AcceptSubscriptionUseCase;
 import io.gravitee.apim.core.subscription.use_case.CloseSubscriptionUseCase;
+import io.gravitee.apim.core.subscription.use_case.DeleteSubscriptionSpecUseCase;
+import io.gravitee.apim.core.subscription.use_case.ImportSubscriptionSpecUseCase;
 import io.gravitee.apim.core.subscription.use_case.RejectSubscriptionUseCase;
 import io.gravitee.common.data.domain.Page;
 import io.gravitee.common.http.MediaType;
@@ -59,6 +62,7 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import jakarta.ws.rs.BeanParam;
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.DefaultValue;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
@@ -129,6 +133,12 @@ public class ApiSubscriptionsResource extends AbstractResource {
 
     @Inject
     private SecurityContext securityContext;
+
+    @Inject
+    private ImportSubscriptionSpecUseCase importSubscriptionSpecUseCase;
+
+    @Inject
+    private DeleteSubscriptionSpecUseCase deleteSubscriptionSpecUseCase;
 
     @PathParam("apiId")
     private String apiId;
@@ -303,6 +313,21 @@ public class ApiSubscriptionsResource extends AbstractResource {
         expandData(subscription, expands);
 
         return Response.ok(subscription).build();
+    }
+
+    @PUT
+    @Path("/spec/_import")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response importSubscriptionSpec(@Valid SubscriptionCRDSpec spec) {
+        return Response.ok(importSubscriptionSpecUseCase.execute(new ImportSubscriptionSpecUseCase.Input(getAuditInfo(), spec))).build();
+    }
+
+    @DELETE
+    @Path("/spec/{id}")
+    public Response deleteSubscriptionSpec(@PathParam("id") String id) {
+        deleteSubscriptionSpecUseCase.execute(new DeleteSubscriptionSpecUseCase.Input(getAuditInfo(), id));
+        return Response.noContent().build();
     }
 
     @PUT

gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/apim/core/subscription/use_case/ImportSubscriptionSpecUseCase.java
@@ -1,0 +1,43 @@
+/*
+ * Copyright © 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.apim.core.subscription.use_case;
+
+import io.gravitee.apim.core.UseCase;
+import io.gravitee.apim.core.audit.model.AuditInfo;
+import io.gravitee.apim.core.subscription.domain_service.SubscriptionCRDSpecDomainService;
+import io.gravitee.apim.core.subscription.model.crd.SubscriptionCRDSpec;
+import java.time.ZonedDateTime;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * @author Antoine CORDIER (antoine.cordier at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+@UseCase
+@RequiredArgsConstructor
+public class ImportSubscriptionSpecUseCase {
+
+    public record Input(AuditInfo auditInfo, SubscriptionCRDSpec spec) {}
+
+    public record Output(String id, String status, ZonedDateTime startingAt, ZonedDateTime endingAt) {}
+
+    private final SubscriptionCRDSpecDomainService domainService;
+
+    public Output execute(Input input) {
+        var subscription = domainService.createOrUpdate(input.auditInfo, input.spec);
+        return new Output(subscription.getId(), subscription.getStatus().name(), subscription.getStartingAt(), subscription.getEndingAt());
+    }
+}

gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/apim/infra/adapter/SubscriptionAdapter.java
@@ -23,7 +23,9 @@ import io.gravitee.apim.core.json.JsonProcessingException;
 import io.gravitee.apim.core.json.JsonSerializer;
 import io.gravitee.apim.core.subscription.model.SubscriptionConfiguration;
 import io.gravitee.apim.core.subscription.model.SubscriptionEntity;
+import io.gravitee.apim.core.subscription.model.crd.SubscriptionCRDSpec;
 import io.gravitee.repository.management.model.Subscription;
+import io.gravitee.rest.api.model.SubscriptionStatus;
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -81,7 +83,59 @@ public abstract class SubscriptionAdapter {
     @Mapping(source = "applicationId", target = "application")
     @Mapping(source = "requestMessage", target = "request")
     @Mapping(source = "reasonMessage", target = "reason")
+    @Mapping(target = "security", ignore = true)
+    @Mapping(target = "keys", ignore = true)
     public abstract io.gravitee.rest.api.model.SubscriptionEntity map(SubscriptionEntity subscription);
+
+    @Mapping(target = "type", ignore = true)
+    @Mapping(source = "api", target = "apiId")
+    @Mapping(source = "plan", target = "planId")
+    @Mapping(source = "application", target = "applicationId")
+    @Mapping(source = "request", target = "requestMessage")
+    @Mapping(source = "reason", target = "reasonMessage")
+    @Mapping(target = "generalConditionsContentRevision", ignore = true)
+    @Mapping(target = "generalConditionsContentPageId", ignore = true)
+    @Mapping(target = "generalConditionsAccepted", ignore = true)
+    public abstract SubscriptionEntity toCore(io.gravitee.rest.api.model.SubscriptionEntity subscription);
+
+    public SubscriptionEntity.Status toCore(SubscriptionStatus status) {
+        return status == SubscriptionStatus.RESUMED ? SubscriptionEntity.Status.ACCEPTED : SubscriptionEntity.Status.valueOf(status.name());
+    }
+
+    @Mapping(target = "status", ignore = true)
+    @Mapping(target = "startingAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "type", ignore = true)
+    @Mapping(target = "subscribedBy", ignore = true)
+    @Mapping(target = "requestMessage", ignore = true)
+    @Mapping(target = "reasonMessage", ignore = true)
+    @Mapping(target = "processedBy", ignore = true)
+    @Mapping(target = "processedAt", ignore = true)
+    @Mapping(target = "pausedAt", ignore = true)
+    @Mapping(target = "metadata", ignore = true)
+    @Mapping(target = "generalConditionsContentRevision", ignore = true)
+    @Mapping(target = "generalConditionsContentPageId", ignore = true)
+    @Mapping(target = "generalConditionsAccepted", ignore = true)
+    @Mapping(target = "failureCause", ignore = true)
+    @Mapping(target = "environmentId", ignore = true)
+    @Mapping(target = "daysToExpirationOnLastNotification", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "consumerStatus", ignore = true)
+    @Mapping(target = "consumerPausedAt", ignore = true)
+    @Mapping(target = "configuration", ignore = true)
+    @Mapping(target = "closedAt", ignore = true)
+    @Mapping(target = "clientId", ignore = true)
+    @Mapping(target = "clientCertificate", ignore = true)
+    public abstract SubscriptionEntity fromSpec(SubscriptionCRDSpec spec);
+
+    @Mapping(target = "request", ignore = true)
+    @Mapping(target = "apiKeyMode", ignore = true)
+    @Mapping(source = "applicationId", target = "application")
+    @Mapping(source = "planId", target = "plan")
+    @Mapping(target = "generalConditionsContentRevision", ignore = true)
+    public abstract io.gravitee.rest.api.model.NewSubscriptionEntity fromCoreForCreate(SubscriptionEntity entity);
+
+    public abstract io.gravitee.rest.api.model.UpdateSubscriptionEntity fromCoreForUpdate(SubscriptionEntity entity);
 
     @Named("deserializeConfiguration")
     public SubscriptionConfiguration deserializeConfiguration(String configuration) {