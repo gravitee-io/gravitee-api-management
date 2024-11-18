5996ea46f5cb45b508c7bb4fb02228a213c5c982
Florent CHAMFROY <florent.chamfroy@graviteesource.com>
2024-11-18 08:55:34 +0100

    ci: use 4.1 prod image for bridge tests

--- .circleci/ci/src/workflows/workflow-bridge-compatibility-tests.ts
+++ .circleci/ci/src/workflows/workflow-bridge-compatibility-tests.ts
@@ -61,7 +61,7 @@ export class BridgeCompatibilityTestsWorkflow {
         matrix: {
           execution_mode: ['v3', 'v4-emulation-engine'],
           database: ['bridge'],
-          apim_client_tag: ['4.4.x-latest', '4.3.x-latest', '4.2.x-latest', '4.1.x-latest'],
+          apim_client_tag: ['4.4.x-latest', '4.3.x-latest', '4.2.x-latest', 'graviteeio@4.1'],
         },
       }),
     ];
