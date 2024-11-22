b97b868d816446ef12a812e92a16d1ca2ab8a409
Ouahid Khelifi <ouahid.khelifi@gmail.com>
2024-11-22 10:40:51 +0100

    fix: limit size of the APIs pages saved in the audit logs
    (cherry picked from commit 44a5f40b7467a3141cc61704c16052409a08dd51)
    (cherry picked from commit 8f1006d10e0dd6f70207efc5810c08fbab55712c)

--- gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/test/java/io/gravitee/rest/api/service/impl/PageServiceImplTests.java
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/test/java/io/gravitee/rest/api/service/impl/PageServiceImplTests.java
@@ -15,38 +15,72 @@
  */
 package io.gravitee.rest.api.service.impl;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.isNull;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import io.gravitee.repository.exceptions.TechnicalException;
 import io.gravitee.repository.management.api.PageRepository;
+import io.gravitee.repository.management.model.Audit;
 import io.gravitee.repository.management.model.Page;
 import io.gravitee.repository.management.model.PageMedia;
+import io.gravitee.repository.management.model.PageReferenceType;
+import io.gravitee.rest.api.model.NewPageEntity;
+import io.gravitee.rest.api.model.PageSourceEntity;
+import io.gravitee.rest.api.model.PageType;
+import io.gravitee.rest.api.model.Visibility;
+import io.gravitee.rest.api.service.AuditService;
+import io.gravitee.rest.api.service.PageRevisionService;
 import io.gravitee.rest.api.service.common.ExecutionContext;
-import io.gravitee.rest.api.service.converter.PageConverter;
 import io.gravitee.rest.api.service.exceptions.TechnicalManagementException;
+import io.gravitee.rest.api.service.swagger.OAIDescriptor;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import java.util.List;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
 
 /**
  * @author GraviteeSource Team
  */
-@RunWith(MockitoJUnitRunner.class)
+@ExtendWith(MockitoExtension.class)
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class PageServiceImplTests {
+
+    public static final String ORGANIZATION_ID = "my-org";
+    public static final String ENVIRONMENT_ID = "my-env";
+    public static final String API_ID = "my-api";
+    public static final ExecutionContext executionContext = new ExecutionContext(ORGANIZATION_ID, ENVIRONMENT_ID);
 
     @InjectMocks
     private PageServiceImpl pageService;
 
     @Mock
-    private PageRepository pageRepository;
+    AuditService auditService;
+
+    @Mock
+    PageRepository pageRepository;
+
+    @Mock
+    PageRevisionService pageRevisionService;
+
+    @Captor
+    private ArgumentCaptor<Page> pageCaptor;
 
     @Test
     public void getParentPathFromFilePath_should_return_correct_path() {
@@ -127,14 +161,15 @@ public class PageServiceImplTests {
         assertThat(result).isFalse();
     }
 
-    @Test(expected = TechnicalManagementException.class)
+    @Test
     public void isMediaUsedInPages_shouldThrowTechnicalManagementExceptionOnError() throws Exception {
         var mediaHash = "media-hash";
         var executionContext = new ExecutionContext("org-id", "env-id");
 
         when(pageRepository.findAll(any())).thenThrow(TechnicalException.class);
 
-        pageService.isMediaUsedInPages(executionContext, mediaHash);
+        assertThatThrownBy(() -> pageService.isMediaUsedInPages(executionContext, mediaHash))
+            .isInstanceOf(TechnicalManagementException.class);
     }
 
     @Test
@@ -161,5 +196,112 @@ public class PageServiceImplTests {
         page.setAttachedMedia(mediaList);
         page.setContent(content);
         return page;
+    }
+
+    @Test
+    @SneakyThrows
+    public void create_page_should_not_save_content_in_audit() {
+        ReflectionTestUtils.setField(pageService, "maxContentSize", -1);
+        var aNewPageEntity = aNewPageEntity();
+        var createdPage = aPage(aNewPageEntity);
+        when(pageRepository.create(any())).thenReturn(createdPage);
+
+        pageService.createPage(executionContext, API_ID, aNewPageEntity);
+
+        verify(auditService)
+            .createApiAuditLog(
+                any(ExecutionContext.class),
+                eq(API_ID),
+                any(),
+                any(Audit.AuditEvent.class),
+                any(),
+                isNull(),
+                pageCaptor.capture()
+            );
+        var auditPage = pageCaptor.getValue();
+        assertThat(auditPage).isNotNull();
+        assertThat(auditPage.getContent()).isNotNull();
+        assertThat(auditPage.getContent().length()).isEqualTo(aNewPageEntity.getContent().length());
+    }
+
+    @Test
+    @SneakyThrows
+    public void create_page_should_limit_content_in_audit() {
+        ReflectionTestUtils.setField(pageService, "maxContentSize", 10);
+        var aNewPageEntity = aNewPageEntity();
+        var createdPage = aPage(aNewPageEntity);
+        when(pageRepository.create(any())).thenReturn(createdPage);
+
+        pageService.createPage(executionContext, API_ID, aNewPageEntity);
+
+        verify(auditService)
+            .createApiAuditLog(
+                any(ExecutionContext.class),
+                eq(API_ID),
+                any(),
+                any(Audit.AuditEvent.class),
+                any(),
+                isNull(),
+                pageCaptor.capture()
+            );
+        var auditPage = pageCaptor.getValue();
+        assertThat(auditPage).isNotNull();
+        assertThat(auditPage.getContent()).isEqualTo("""
+{
+  "opena...""");
+    }
+
+    @Test
+    @SneakyThrows
+    public void create_page_should_force_null_content_in_audit() {
+        ReflectionTestUtils.setField(pageService, "maxContentSize", 0);
+        var aNewPageEntity = aNewPageEntity();
+        var createdPage = aPage(aNewPageEntity);
+        when(pageRepository.create(any())).thenReturn(createdPage);
+
+        pageService.createPage(executionContext, API_ID, aNewPageEntity);
+
+        verify(auditService)
+            .createApiAuditLog(
+                any(ExecutionContext.class),
+                eq(API_ID),
+                any(),
+                any(Audit.AuditEvent.class),
+                any(),
+                isNull(),
+                pageCaptor.capture()
+            );
+        var auditPage = pageCaptor.getValue();
+        assertThat(auditPage).isNotNull();
+        assertThat(auditPage.getContent()).isNull();
+    }
+
+    private NewPageEntity aNewPageEntity() throws JsonProcessingException {
+        var page = new NewPageEntity();
+        page.setType(PageType.SWAGGER);
+        page.setVisibility(Visibility.PUBLIC);
+        page.setName("my awesome page");
+
+        var source = new PageSourceEntity();
+        page.setSource(source);
+
+        OpenAPI openAPI = new OpenAPI();
+        openAPI.setInfo(new Info().title("My API").version("1.0.0").description("A description"));
+        openAPI.setServers(List.of());
+        var descriptor = new OAIDescriptor(openAPI);
+        page.setContent(descriptor.toJson());
+
+        return page;
+    }
+
+    private Page aPage(NewPageEntity newPageEntity) {
+        return Page
+            .builder()
+            .type(newPageEntity.getType().name())
+            .visibility(newPageEntity.getVisibility().name())
+            .name(newPageEntity.getName())
+            .content(newPageEntity.getContent())
+            .referenceType(PageReferenceType.API)
+            .build();
     }
 }
