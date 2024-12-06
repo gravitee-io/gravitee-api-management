031ee5fe10d5e4c3df4a550ef23da41c2b034a35
mukul-tyagi08 <mukul.tyagi@graviteesource.com>
2024-12-06 10:07:22 +0530

    refactor: rename findById to findByIdAndStatus and add status parameter
    (cherry picked from commit 34113ec1f0a1311539df0ef71440e33841adc18f)

--- gravitee-apim-rest-api/gravitee-apim-rest-api-management-v2/gravitee-apim-rest-api-management-v2-rest/src/main/java/io/gravitee/rest/api/management/v2/rest/resource/api/ApiSubscriptionsResource.java
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-management-v2/gravitee-apim-rest-api-management-v2-rest/src/main/java/io/gravitee/rest/api/management/v2/rest/resource/api/ApiSubscriptionsResource.java
@@ -28,6 +28,7 @@ import io.gravitee.apim.core.subscription.use_case.ImportSubscriptionSpecUseCase
 import io.gravitee.apim.core.subscription.use_case.RejectSubscriptionUseCase;
 import io.gravitee.common.data.domain.Page;
 import io.gravitee.common.http.MediaType;
+import io.gravitee.repository.management.model.ApplicationStatus;
 import io.gravitee.rest.api.management.v2.rest.mapper.*;
 import io.gravitee.rest.api.management.v2.rest.model.*;
 import io.gravitee.rest.api.management.v2.rest.model.Error;
@@ -534,7 +535,7 @@ public class ApiSubscriptionsResource extends AbstractResource {
                 .map(subscription -> (subscription.getApplication()).getId())
                 .collect(Collectors.toSet());
             final Collection<BaseApplication> applications = applicationMapper.mapToBaseApplicationList(
-                applicationService.findByIds(executionContext, applicationIds)
+                applicationService.findByIdsAndStatus(executionContext, applicationIds, ApplicationStatus.ACTIVE)
             );
             applications.forEach(application ->
                 subscriptions
