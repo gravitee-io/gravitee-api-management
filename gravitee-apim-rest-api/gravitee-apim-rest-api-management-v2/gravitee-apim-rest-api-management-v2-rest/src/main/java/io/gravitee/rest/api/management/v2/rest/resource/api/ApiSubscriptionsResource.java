3a31f123b320d6d6f3cb1ee65912f873c1b5b4d4
mukul-tyagi08 <mukul.tyagi@graviteesource.com>
2024-12-06 12:55:03 +0530

    fix: get archived applications to get application info for subscriptions
    (cherry picked from commit aa0ae4095f18f1686f85fa46dc0d892b8298eebc)

--- gravitee-apim-rest-api/gravitee-apim-rest-api-management-v2/gravitee-apim-rest-api-management-v2-rest/src/main/java/io/gravitee/rest/api/management/v2/rest/resource/api/ApiSubscriptionsResource.java
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-management-v2/gravitee-apim-rest-api-management-v2-rest/src/main/java/io/gravitee/rest/api/management/v2/rest/resource/api/ApiSubscriptionsResource.java
@@ -28,7 +28,6 @@ import io.gravitee.apim.core.subscription.use_case.ImportSubscriptionSpecUseCase
 import io.gravitee.apim.core.subscription.use_case.RejectSubscriptionUseCase;
 import io.gravitee.common.data.domain.Page;
 import io.gravitee.common.http.MediaType;
-import io.gravitee.repository.management.model.ApplicationStatus;
 import io.gravitee.rest.api.management.v2.rest.mapper.*;
 import io.gravitee.rest.api.management.v2.rest.model.*;
 import io.gravitee.rest.api.management.v2.rest.model.Error;
@@ -535,7 +534,7 @@ public class ApiSubscriptionsResource extends AbstractResource {
                 .map(subscription -> (subscription.getApplication()).getId())
                 .collect(Collectors.toSet());
             final Collection<BaseApplication> applications = applicationMapper.mapToBaseApplicationList(
-                applicationService.findByIdsAndStatus(executionContext, applicationIds, ApplicationStatus.ACTIVE)
+                applicationService.findByIds(executionContext, applicationIds)
             );
             applications.forEach(application ->
                 subscriptions
