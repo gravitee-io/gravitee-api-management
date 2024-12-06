3649c66025aea3a3b1c51ca46d61874c7c85f111
mukul-tyagi08 <mukul.tyagi@graviteesource.com>
2024-12-06 12:55:03 +0530

    fix: get archived applications to get application info for subscriptions
    (cherry picked from commit aa0ae4095f18f1686f85fa46dc0d892b8298eebc)

--- gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/rest/api/service/impl/ApplicationServiceImpl.java
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/rest/api/service/impl/ApplicationServiceImpl.java
@@ -249,6 +249,11 @@ public class ApplicationServiceImpl extends AbstractService implements Applicati
     }
 
     @Override
+    public Set<ApplicationListItem> findByIds(ExecutionContext executionContext, Collection<String> applicationIds) {
+        return findByIdsAndStatus(executionContext, applicationIds, null);
+    }
+
+    @Override
     public Set<ApplicationListItem> findByIdsAndStatus(
         final ExecutionContext executionContext,
         Collection<String> applicationIds,
@@ -261,9 +266,11 @@ public class ApplicationServiceImpl extends AbstractService implements Applicati
                 return Collections.emptySet();
             }
 
-            ApplicationCriteria.Builder criteriaBuilder = new ApplicationCriteria.Builder()
-                .ids(new HashSet<>(applicationIds))
-                .status(ApplicationStatus.ACTIVE);
+            ApplicationCriteria.Builder criteriaBuilder = new ApplicationCriteria.Builder().ids(new HashSet<>(applicationIds));
+
+            if (applicationStatus != null) {
+                criteriaBuilder.status(applicationStatus);
+            }
 
             if (executionContext.hasEnvironmentId()) {
                 criteriaBuilder.environmentIds(executionContext.getEnvironmentId());
