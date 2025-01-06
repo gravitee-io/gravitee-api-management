e6f16592f94e26ffa5b204594b4706843a0cbb10
Florent CHAMFROY <florent.chamfroy@graviteesource.com>
2025-01-06 08:17:30 +0100

    ci: update bridge compatibility matrix

--- .circleci/ci/src/workflows/workflow-bridge-compatibility-tests.ts
+++ .circleci/ci/src/workflows/workflow-bridge-compatibility-tests.ts
@@ -61,7 +61,7 @@ export class BridgeCompatibilityTestsWorkflow {
         matrix: {
           execution_mode: ['v3', 'v4-emulation-engine'],
           database: ['bridge'],
-          apim_client_tag: ['master-latest', '4.4.x-latest', '4.3.x-latest', '4.2.x-latest'],
+          apim_client_tag: ['4.5.x-latest', '4.4.x-latest', '4.3.x-latest', '4.2.x-latest'],
         },
       }),
     ];
