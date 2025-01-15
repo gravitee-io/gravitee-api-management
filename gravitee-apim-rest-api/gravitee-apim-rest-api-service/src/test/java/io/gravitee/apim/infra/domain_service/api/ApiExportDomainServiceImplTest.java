00a91b6f7f5095b52425f3fb32642a1034e8f834
Michel Barret <michel.barret@graviteesource.com>
2025-01-15 15:38:50 +0100

    fix: export should set the type
    **Issue** [APIM-8360](https://gravitee.atlassian.net/browse/APIM-8360)
    
    ## Description
    
    I made the minimal change because this code is fully reworked for 4.7

---
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/test/java/io/gravitee/apim/infra/domain_service/api/ApiExportDomainServiceImplTest.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright © 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.apim.infra.domain_service.api;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import io.gravitee.apim.core.api.model.import_definition.GraviteeDefinition;
+import io.gravitee.apim.core.audit.model.AuditInfo;
+import io.gravitee.definition.model.v4.ApiType;
+import io.gravitee.rest.api.model.v4.api.ApiEntity;
+import io.gravitee.rest.api.model.v4.api.ExportApiEntity;
+import io.gravitee.rest.api.service.v4.ApiImportExportService;
+import java.util.UUID;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class ApiExportDomainServiceImplTest {
+
+    @Mock
+    ApiImportExportService exportService;
+
+    @InjectMocks
+    ApiExportDomainServiceImpl sut;
+
+    @Test
+    void exportServiceMustMapTypeWhenExportV4() {
+        // Given
+        String apiId = UUID.randomUUID().toString();
+        ApiEntity api = new ApiEntity();
+        api.setType(ApiType.PROXY);
+        when(exportService.exportApi(any(), any(), any(), any())).thenReturn(new ExportApiEntity(api, null, null, null, null, null));
+
+        // When
+        GraviteeDefinition export = sut.export(apiId, AuditInfo.builder().build());
+
+        // Then
+        assertThat(export.getApi().getType()).isEqualTo(ApiType.PROXY);
+    }
+}
