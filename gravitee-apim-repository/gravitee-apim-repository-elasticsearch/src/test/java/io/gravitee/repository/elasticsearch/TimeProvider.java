485d5683926a20daa0bce4083b2a0797c8303b44
Florent CHAMFROY <florent.chamfroy@graviteesource.com>
2025-01-16 23:50:55 +0100

    test: make elastic tests independent of the time
    Some of the tests of the elastic repository depend on the time when they are executed.
    This commit introduces a TimeProvider class that is used to provide the same date for both the data put in ES and the tests themselves.
    
    (cherry picked from commit 94b9e2a9c950a225d0e8147b212dc681af4c4fd0)

---
+++ gravitee-apim-repository/gravitee-apim-repository-elasticsearch/src/test/java/io/gravitee/repository/elasticsearch/TimeProvider.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright © 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.repository.elasticsearch;
+
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import lombok.Getter;
+
+@Getter
+public class TimeProvider {
+
+    private static final DateTimeFormatter DATE_TIME_FORMATTER_WITH_DASH = DateTimeFormatter
+        .ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSxxx")
+        .withZone(ZoneId.systemDefault());
+    private static final DateTimeFormatter DATE_FORMATTER_WITH_DASH = DateTimeFormatter
+        .ofPattern("yyyy-MM-dd")
+        .withZone(ZoneId.systemDefault());
+    private static final DateTimeFormatter DATE_FORMATTER_WITH_DOT = DateTimeFormatter
+        .ofPattern("yyyy.MM.dd")
+        .withZone(ZoneId.systemDefault());
+
+    private final Instant now;
+    private final String dateToday;
+    private final String dateYesterday;
+    private final String dateTimeToday;
+    private final String dateTimeYesterday;
+    private final String todayWithDot;
+    private final String yesterdayWithDot;
+
+    public TimeProvider() {
+        now = Instant.now().minus(30, ChronoUnit.MINUTES);
+        final Instant yesterday = now.minus(1, ChronoUnit.DAYS);
+
+        dateToday = DATE_FORMATTER_WITH_DASH.format(now);
+        dateYesterday = DATE_FORMATTER_WITH_DASH.format(yesterday);
+
+        dateTimeToday = DATE_TIME_FORMATTER_WITH_DASH.format(now);
+        dateTimeYesterday = DATE_TIME_FORMATTER_WITH_DASH.format(yesterday);
+
+        todayWithDot = DATE_FORMATTER_WITH_DOT.format(now);
+        yesterdayWithDot = DATE_FORMATTER_WITH_DOT.format(yesterday);
+    }
+}
