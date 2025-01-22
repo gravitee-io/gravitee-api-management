b4ccf8de3050d607cc3e582f1ff62fcfd2a79a8e
Antoine CORDIER <antoine.cordier@graviteesource.com>
2025-01-22 11:30:46 +0000

    refactor: rename isReadOnly for isKubernetesOrigin in spg

--- gravitee-apim-console-webui/src/management/settings/shared-policy-groups/shared-policy-group/shared-policy-group-studio/shared-policy-group-studio.component.ts
+++ gravitee-apim-console-webui/src/management/settings/shared-policy-groups/shared-policy-group/shared-policy-group-studio/shared-policy-group-studio.component.ts
@@ -86,7 +86,7 @@ export class SharedPolicyGroupStudioComponent {
         ...sharedPolicyGroup,
         // Restore unsaved steps if any
         steps: this.enableSaveButton ? this.sharedPolicyGroup().steps : sharedPolicyGroup.steps,
-        isReadOnly: sharedPolicyGroup.originContext?.origin === 'KUBERNETES',
+        isKubernetesOrigin: sharedPolicyGroup.originContext?.origin === 'KUBERNETES',
       })),
     ),
   );
