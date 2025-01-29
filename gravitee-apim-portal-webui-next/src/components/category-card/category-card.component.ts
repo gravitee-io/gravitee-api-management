aab07ddb986a190a6d64169715dfd1c00216d52a
jourdiw <jourdi.waller@graviteesource.com>
2025-01-29 18:35:37 +0100

    feat(portal-next): create categories-view

---
+++ gravitee-apim-portal-webui-next/src/components/category-card/category-card.component.ts
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2024 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { Component, ViewChild, ElementRef, AfterViewInit, ChangeDetectorRef, InputSignal, input } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatTooltip } from '@angular/material/tooltip';
+import { RouterLink } from '@angular/router';
+
+import { Category } from '../../entities/categories/categories';
+import { PictureComponent } from '../picture/picture.component';
+
+@Component({
+  selector: 'app-category-card',
+  standalone: true,
+  imports: [MatButtonModule, MatCardModule, PictureComponent, RouterLink, MatTooltip],
+  templateUrl: './category-card.component.html',
+  styleUrl: './category-card.component.scss',
+})
+export class CategoryCardComponent implements AfterViewInit {
+  @ViewChild('categoryName', { static: true }) categoryNameElement!: ElementRef;
+
+  category: InputSignal<Category> = input.required<Category>();
+
+  isOverflowing: boolean = false;
+
+  constructor(private readonly cdr: ChangeDetectorRef) {}
+
+  ngAfterViewInit() {
+    this.checkOverflow();
+    this.cdr.detectChanges();
+  }
+
+  checkOverflow() {
+    const el = this.categoryNameElement.nativeElement;
+    this.isOverflowing = el.scrollWidth > el.clientWidth || el.scrollHeight > el.clientHeight;
+  }
+}
