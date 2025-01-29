716db966d6d53256d3c7b4d7e9d7032f7f1ab70f
ThibaudAv <thibaud.avenier@gmail.com>
2025-01-29 17:28:22 +0100

    feat(portal): display MaintenanceMode
    (cherry picked from commit 6a2cea9e509e4bb9763a6b5a862f467b5e2846dc)

--- gravitee-apim-portal-webui/src/proxy.conf.mjs
+++ gravitee-apim-portal-webui/src/proxy.conf.mjs
@@ -30,6 +30,7 @@ export default {
       });
       proxyRes.on('end', function() {
         body = body.toString();
+        res.writeHead(proxyRes.statusCode);
         res.end(body.replace(`${target}/`, ""));
       });
     },
