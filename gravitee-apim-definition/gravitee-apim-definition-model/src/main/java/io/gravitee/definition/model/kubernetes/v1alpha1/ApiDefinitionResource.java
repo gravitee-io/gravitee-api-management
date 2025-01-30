200c2459e33c524bf96de838d7f809a94509c4a1
Antoine CORDIER <antoine.cordier@graviteesource.com>
2025-01-30 11:34:26 +0000

    fix: revert map endpoint headers to map on v2 crd export
    This reverts commit afa1c2df0a8e50182337bff49e40f1083ac0baa5.

--- gravitee-apim-definition/gravitee-apim-definition-model/src/main/java/io/gravitee/definition/model/kubernetes/v1alpha1/ApiDefinitionResource.java
+++ gravitee-apim-definition/gravitee-apim-definition-model/src/main/java/io/gravitee/definition/model/kubernetes/v1alpha1/ApiDefinitionResource.java
@@ -171,7 +171,7 @@ public class ApiDefinitionResource extends CustomResource<ObjectNode> {
         if (spec.hasNonNull(PROXY_FIELD)) {
             var proxy = getSpec().get(PROXY_FIELD);
             if (proxy.hasNonNull(ENDPOINT_GROUPS_FIELD)) {
-                proxy.get(ENDPOINT_GROUPS_FIELD).forEach(this::prepareEndpointGroup);
+                proxy.get(ENDPOINT_GROUPS_FIELD).forEach(this::removeUnsupportedEndpointGroupFields);
             }
         }
     }
@@ -189,30 +189,5 @@ public class ApiDefinitionResource extends CustomResource<ObjectNode> {
             pagesMap.set(key.asText(), ((ObjectNode) page).remove(UNSUPPORTED_PAGE_FIELDS));
         }
         return pagesMap;
-    }
-
-    private void prepareEndpointGroup(JsonNode group) {
-        removeUnsupportedEndpointGroupFields(group);
-        replaceEndpointHeaders((ObjectNode) group);
-        if (group.hasNonNull(ENDPOINTS_FIELD)) {
-            var endpoints = (ArrayNode) group.get(ENDPOINTS_FIELD);
-            endpoints.forEach(endpoint -> replaceEndpointHeaders((ObjectNode) endpoint));
-        }
-    }
-
-    private void replaceEndpointHeaders(ObjectNode endpoint) {
-        if (endpoint.has("headers")) {
-            endpoint.replace("headers", mapHeaders((ArrayNode) endpoint.get("headers")));
-        }
-    }
-
-    private ObjectNode mapHeaders(ArrayNode headers) {
-        var headerMap = JsonNodeFactory.instance.objectNode();
-        headers.forEach(header -> {
-            var name = header.get("name").asText();
-            var value = header.get("value").asText();
-            headerMap.put(name, value);
-        });
-        return headerMap;
     }
 }
