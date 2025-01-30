673a0d903565cef297521f1914ba8da061b990af
Antoine CORDIER <antoine.cordier@graviteesource.com>
2025-01-30 11:34:26 +0000

    fix: revert map endpoint headers to map on v2 crd export
    This reverts commit afa1c2df0a8e50182337bff49e40f1083ac0baa5.

--- gravitee-apim-definition/gravitee-apim-definition-model/src/test/java/io/gravitee/definition/model/kubernetes/v1alpha1/ApiDefinitionResourceTest.java
+++ gravitee-apim-definition/gravitee-apim-definition-model/src/test/java/io/gravitee/definition/model/kubernetes/v1alpha1/ApiDefinitionResourceTest.java
@@ -88,19 +88,6 @@ class ApiDefinitionResourceTest {
     }
 
     @Test
-    void shouldMapHeadersToMap() throws Exception {
-        var resource = new ApiDefinitionResource("api-definition", readDefinition("api-definition.json"));
-        var proxy = (ObjectNode) resource.getSpec().get("proxy");
-        assertTrue(proxy.has("groups"));
-        var groups = (ArrayNode) proxy.get("groups");
-        assertEquals(1, groups.size());
-        var group = groups.get(0);
-        assertTrue(group.has("headers"));
-        var headers = group.get("headers");
-        assertEquals("true", headers.get("x-test").asText());
-    }
-
-    @Test
     public void shouldMapPageWithoutName() throws Exception {
         ApiDefinitionResource resource = new ApiDefinitionResource(
             "api-definition",
