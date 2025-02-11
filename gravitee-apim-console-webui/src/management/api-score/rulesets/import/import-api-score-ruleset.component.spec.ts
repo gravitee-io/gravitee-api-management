gravitee-io/gravitee-gateway-core

feat(console): add api-score rulesets for providers: native kafka, federated api

gravitee-apim-console-webui/src/management/api-score/rulesets/import/import-api-score-ruleset.component.spec.ts
@@ -112,6 +112,7 @@ describe('NewRulesetComponent', () => {
   [
     { input: { selectedFormat: 'GRAVITEE_MESSAGE' }, expected: RulesetFormat.GRAVITEE_MESSAGE },
     { input: { selectedFormat: 'GRAVITEE_PROXY' }, expected: RulesetFormat.GRAVITEE_PROXY },
+    { input: { selectedFormat: 'KAFKA_NATIVE' }, expected: RulesetFormat.KAFKA_NATIVE },
   ].forEach((testParams) => {
     it('should send request with values selected in form for GraviteeAPI format: ' + testParams.input.selectedFormat, async () => {
       const definitionFormat = await componentHarness.locatorForDefinitionFormatRadioGroup();
@@ -146,6 +147,32 @@ describe('NewRulesetComponent', () => {
 
       expectCreateRulesetRequest(data);
     });
+  });
+
+  it('should not send request if cancel import', async () => {
+    const definitionFormat = await componentHarness.locatorForDefinitionFormatRadioGroup();
+    await definitionFormat.select('GraviteeAPI');
+    expect(await definitionFormat.getSelectedValue()).toEqual('GraviteeAPI');
+
+    const graviteeApiDefinitionFormat = await componentHarness.locatorForGraviteeApiDefinitionFormatRadioGroup();
+    await graviteeApiDefinitionFormat.select(RulesetFormat.GRAVITEE_MESSAGE);
+    expect(await graviteeApiDefinitionFormat.getSelectedValue()).toEqual(RulesetFormat.GRAVITEE_MESSAGE);
+
+    await componentHarness.setName('Test ruleset name');
+    await componentHarness.setDescription('Test description');
+
+    fixture.detectChanges();
+
+    expect(fixture.componentInstance.form.value.name).toEqual('Test ruleset name');
+    expect(fixture.componentInstance.form.value.description).toEqual('Test description');
+    expect(fixture.componentInstance.form.value.graviteeApiFormat).toEqual(RulesetFormat.GRAVITEE_MESSAGE);
+
+    await componentHarness.pickFiles([new File([JSON.stringify('test')], 'gravitee-api-definition.json', { type: 'application/json' })]);
+    const importButton = await componentHarness.locatorForSubmitImportButton();
+    const cancelButton = await componentHarness.locatorForCancelImportButton();
+    expect(await importButton.isDisabled()).toEqual(false);
+
+    await cancelButton.click();
   });
 
   function expectCreateRulesetRequest(data: CreateRulesetRequestData) {

gravitee-apim-console-webui/src/management/api-score/rulesets/import/import-api-score-ruleset.component.ts
@@ -32,6 +32,33 @@ export class ImportApiScoreRulesetComponent implements OnInit {
   protected importType: string;
   protected readonly RulesetFormat = RulesetFormat;
 
+  graviteeAPIFormats = [
+    {
+      title: 'Gravitee Proxy API',
+      subtitle: 'For gateway or proxy services handling API calls.',
+      value: RulesetFormat.GRAVITEE_PROXY,
+      isDisabled: false,
+    },
+    {
+      title: 'Gravitee Message API',
+      subtitle: 'For message-based services like queues or pub/sub.',
+      value: RulesetFormat.GRAVITEE_MESSAGE,
+      isDisabled: false,
+    },
+    {
+      title: 'Native Kafka',
+      subtitle: 'For Apache Kafka-based messaging and event processing.',
+      value: RulesetFormat.KAFKA_NATIVE,
+      isDisabled: false,
+    },
+    {
+      title: 'Gravitee Federated API',
+      subtitle: 'For APIs aggregating data from multiple sources.',
+      value: RulesetFormat.GRAVITEE_FEDERATION,
+      isDisabled: false,
+    },
+  ];
+
   public isLoading = false;
 
   public form: FormGroup = this.formBuilder.group({
@@ -87,23 +114,18 @@ export class ImportApiScoreRulesetComponent implements OnInit {
   public importRuleset() {
     this.isLoading = true;
 
-    let payload: CreateRulesetRequestData = {
-      format: this.form.value.definitionFormat,
+    const payload: CreateRulesetRequestData = {
+      format: this.graviteeApiFormat?.value || this.form.value.definitionFormat,
       name: this.form.value.name,
       description: this.form.value.description,
       payload: this.form.value.fileContent,
     };
-
-    if (this.graviteeApiFormat?.value) {
-      payload = { ...payload, format: this.graviteeApiFormat.value };
-    }
 
     this.rulesetV2Service
       .createRuleset(payload)
       .pipe(takeUntilDestroyed(this.destroyRef))
       .subscribe({
         next: () => {
-          this.isLoading = false;
           this.snackBarService.success('Ruleset imported.');
           this.router.navigate(['..'], { relativeTo: this.activatedRoute });
         },

gravitee-apim-console-webui/src/management/api-score/rulesets/ruleset-format-mapper.pipe.ts
@@ -31,10 +31,12 @@ export class RulesetFormatPipe implements PipeTransform {
         return 'Gravitee Message API';
       case RulesetFormat.GRAVITEE_PROXY:
         return 'Gravitee Proxy API';
+      case RulesetFormat.KAFKA_NATIVE:
+        return 'Kafka Native API';
       case RulesetFormat.OPENAPI:
         return 'OpenAPI';
       case RulesetFormat.ASYNCAPI:
-        return 'AsyncApi';
+        return 'AsyncAPI';
       default:
         return '';
     }

gravitee-io/gravitee-api-management

gravitee-apim-console-webui/src/management/api-score/rulesets/import/import-api-score-ruleset.component.spec.ts
@@ -112,6 +112,7 @@ describe('NewRulesetComponent', () => {
   [
     { input: { selectedFormat: 'GRAVITEE_MESSAGE' }, expected: RulesetFormat.GRAVITEE_MESSAGE },
     { input: { selectedFormat: 'GRAVITEE_PROXY' }, expected: RulesetFormat.GRAVITEE_PROXY },
+    { input: { selectedFormat: 'GRAVITEE_KAFKA' }, expected: RulesetFormat.GRAVITEE_KAFKA },
   ].forEach((testParams) => {
     it('should send request with values selected in form for GraviteeAPI format: ' + testParams.input.selectedFormat, async () => {
       const definitionFormat = await componentHarness.locatorForDefinitionFormatRadioGroup();
@@ -146,6 +147,32 @@ describe('NewRulesetComponent', () => {
 
       expectCreateRulesetRequest(data);
     });
+  });
+
+  it('should not send request if cancel import', async () => {
+    const definitionFormat = await componentHarness.locatorForDefinitionFormatRadioGroup();
+    await definitionFormat.select('GraviteeAPI');
+    expect(await definitionFormat.getSelectedValue()).toEqual('GraviteeAPI');
+
+    const graviteeApiDefinitionFormat = await componentHarness.locatorForGraviteeApiDefinitionFormatRadioGroup();
+    await graviteeApiDefinitionFormat.select(RulesetFormat.GRAVITEE_MESSAGE);
+    expect(await graviteeApiDefinitionFormat.getSelectedValue()).toEqual(RulesetFormat.GRAVITEE_MESSAGE);
+
+    await componentHarness.setName('Test ruleset name');
+    await componentHarness.setDescription('Test description');
+
+    fixture.detectChanges();
+
+    expect(fixture.componentInstance.form.value.name).toEqual('Test ruleset name');
+    expect(fixture.componentInstance.form.value.description).toEqual('Test description');
+    expect(fixture.componentInstance.form.value.graviteeApiFormat).toEqual(RulesetFormat.GRAVITEE_MESSAGE);
+
+    await componentHarness.pickFiles([new File([JSON.stringify('test')], 'gravitee-api-definition.json', { type: 'application/json' })]);
+    const importButton = await componentHarness.locatorForSubmitImportButton();
+    const cancelButton = await componentHarness.locatorForCancelImportButton();
+    expect(await importButton.isDisabled()).toEqual(false);
+
+    await cancelButton.click();
   });
 
   function expectCreateRulesetRequest(data: CreateRulesetRequestData) {

gravitee-apim-console-webui/src/management/api-score/rulesets/import/import-api-score-ruleset.component.ts
@@ -32,6 +32,33 @@ export class ImportApiScoreRulesetComponent implements OnInit {
   protected importType: string;
   protected readonly RulesetFormat = RulesetFormat;
 
+  graviteeAPIFormats = [
+    {
+      title: 'Gravitee Proxy API',
+      subtitle: 'For gateway or proxy services handling API calls.',
+      value: RulesetFormat.GRAVITEE_PROXY,
+      isDisabled: false,
+    },
+    {
+      title: 'Gravitee Message API',
+      subtitle: 'For message-based services like queues or pub/sub.',
+      value: RulesetFormat.GRAVITEE_MESSAGE,
+      isDisabled: false,
+    },
+    {
+      title: 'Native Kafka',
+      subtitle: 'For Apache Kafka-based messaging and event processing.',
+      value: RulesetFormat.GRAVITEE_KAFKA,
+      isDisabled: false,
+    },
+    {
+      title: 'Gravitee Federated API',
+      subtitle: 'For APIs aggregating data from multiple sources.',
+      value: RulesetFormat.GRAVITEE_FEDERATION,
+      isDisabled: false,
+    },
+  ];
+
   public isLoading = false;
 
   public form: FormGroup = this.formBuilder.group({
@@ -87,23 +114,18 @@ export class ImportApiScoreRulesetComponent implements OnInit {
   public importRuleset() {
     this.isLoading = true;
 
-    let payload: CreateRulesetRequestData = {
-      format: this.form.value.definitionFormat,
+    const payload: CreateRulesetRequestData = {
+      format: this.graviteeApiFormat?.value || this.form.value.definitionFormat,
       name: this.form.value.name,
       description: this.form.value.description,
       payload: this.form.value.fileContent,
     };
-
-    if (this.graviteeApiFormat?.value) {
-      payload = { ...payload, format: this.graviteeApiFormat.value };
-    }
 
     this.rulesetV2Service
       .createRuleset(payload)
       .pipe(takeUntilDestroyed(this.destroyRef))
       .subscribe({
         next: () => {
-          this.isLoading = false;
           this.snackBarService.success('Ruleset imported.');
           this.router.navigate(['..'], { relativeTo: this.activatedRoute });
         },

gravitee-apim-console-webui/src/management/api-score/rulesets/ruleset-format-mapper.pipe.ts
@@ -31,10 +31,12 @@ export class RulesetFormatPipe implements PipeTransform {
         return 'Gravitee Message API';
       case RulesetFormat.GRAVITEE_PROXY:
         return 'Gravitee Proxy API';
+      case RulesetFormat.GRAVITEE_KAFKA:
+        return 'Gravitee Native API';
       case RulesetFormat.OPENAPI:
         return 'OpenAPI';
       case RulesetFormat.ASYNCAPI:
-        return 'AsyncApi';
+        return 'AsyncAPI';
       default:
         return '';
     }