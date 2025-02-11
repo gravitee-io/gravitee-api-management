09d8d3de9deae3c4239eb0b31032f0bd23a1ba2a
jourdiw <jourdi.waller@graviteesource.com>
2025-02-11 14:20:07 +0100

    feat(repository): handle log query and response mapping for v2 and v4 indexes

--- gravitee-apim-repository/gravitee-apim-repository-elasticsearch/src/main/java/io/gravitee/repository/elasticsearch/v4/log/adapter/connection/SearchConnectionLogQueryAdapter.java
+++ gravitee-apim-repository/gravitee-apim-repository-elasticsearch/src/main/java/io/gravitee/repository/elasticsearch/v4/log/adapter/connection/SearchConnectionLogQueryAdapter.java
@@ -20,8 +20,12 @@ import io.gravitee.repository.log.v4.model.connection.ConnectionLogQuery;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
 import org.springframework.util.CollectionUtils;
 
 public class SearchConnectionLogQueryAdapter {
@@ -47,11 +51,70 @@ public class SearchConnectionLogQueryAdapter {
             return null;
         }
 
-        var terms = new ArrayList<JsonObject>();
-        if (filter.getApiId() != null) {
-            terms.add(JsonObject.of("term", JsonObject.of("api-id", filter.getApiId())));
+        var mustFilterList = new ArrayList<JsonObject>();
+
+        addApiIdFilter(filter, mustFilterList);
+
+        addFromAndToFilters(filter, mustFilterList);
+
+        addApplicationsFilter(filter, mustFilterList);
+
+        addPlansFilter(filter, mustFilterList);
+
+        addHttpMethodsFilter(filter, mustFilterList);
+
+        addStatusesFilter(filter, mustFilterList);
+
+        addEntrypointIdsFilter(filter, mustFilterList);
+
+        if (!mustFilterList.isEmpty()) {
+            return JsonObject.of("bool", JsonObject.of("must", JsonArray.of(mustFilterList.toArray())));
         }
 
+        return null;
+    }
+
+    private static void addApiIdFilter(ConnectionLogQuery.Filter filter, List<JsonObject> mustFilterList) {
+        if (filter.getApiId() != null) {
+            var apiShouldTerms = new ArrayList<JsonObject>();
+            apiShouldTerms.add(JsonObject.of("term", JsonObject.of("api-id", filter.getApiId())));
+            apiShouldTerms.add(JsonObject.of("term", JsonObject.of("api", filter.getApiId())));
+
+            mustFilterList.add(buildShould(apiShouldTerms));
+        }
+    }
+
+    private static void addEntrypointIdsFilter(ConnectionLogQuery.Filter filter, List<JsonObject> mustFilterList) {
+        if (!CollectionUtils.isEmpty(filter.getEntrypointIds())) {
+            mustFilterList.add(JsonObject.of("terms", JsonObject.of("entrypoint-id", filter.getEntrypointIds())));
+        }
+    }
+
+    private static void addStatusesFilter(ConnectionLogQuery.Filter filter, List<JsonObject> mustFilterList) {
+        if (!CollectionUtils.isEmpty(filter.getStatuses())) {
+            mustFilterList.add(JsonObject.of("terms", JsonObject.of("status", filter.getStatuses())));
+        }
+    }
+
+    private static void addHttpMethodsFilter(ConnectionLogQuery.Filter filter, List<JsonObject> mustFilterList) {
+        if (!CollectionUtils.isEmpty(filter.getMethods())) {
+            mustFilterList.add(buildV2AndV4Terms("method", "http-method", filter.getMethods().stream().map(HttpMethod::code).toList()));
+        }
+    }
+
+    private static void addPlansFilter(ConnectionLogQuery.Filter filter, List<JsonObject> mustFilterList) {
+        if (!CollectionUtils.isEmpty(filter.getPlanIds())) {
+            mustFilterList.add(buildV2AndV4Terms("plan", "plan-id", filter.getPlanIds()));
+        }
+    }
+
+    private static void addApplicationsFilter(ConnectionLogQuery.Filter filter, List<JsonObject> mustFilterList) {
+        if (!CollectionUtils.isEmpty(filter.getApplicationIds())) {
+            mustFilterList.add(buildV2AndV4Terms("application", "application-id", filter.getApplicationIds()));
+        }
+    }
+
+    private static void addFromAndToFilters(ConnectionLogQuery.Filter filter, List<JsonObject> mustFilterList) {
         if (filter.getFrom() != null || filter.getTo() != null) {
             var timestampJsonObject = new JsonObject();
             if (filter.getFrom() != null) {
@@ -60,37 +123,22 @@ public class SearchConnectionLogQueryAdapter {
             if (filter.getTo() != null) {
                 timestampJsonObject.put("lte", new Date(filter.getTo()));
             }
-            terms.add(JsonObject.of("range", JsonObject.of("@timestamp", timestampJsonObject)));
+            mustFilterList.add(JsonObject.of("range", JsonObject.of("@timestamp", timestampJsonObject)));
         }
-
-        if (!CollectionUtils.isEmpty(filter.getApplicationIds())) {
-            terms.add(JsonObject.of("terms", JsonObject.of("application-id", filter.getApplicationIds())));
-        }
-
-        if (!CollectionUtils.isEmpty(filter.getPlanIds())) {
-            terms.add(JsonObject.of("terms", JsonObject.of("plan-id", filter.getPlanIds())));
-        }
-
-        if (!CollectionUtils.isEmpty(filter.getMethods())) {
-            terms.add(JsonObject.of("terms", JsonObject.of("http-method", filter.getMethods().stream().map(HttpMethod::code).toList())));
-        }
-
-        if (!CollectionUtils.isEmpty(filter.getStatuses())) {
-            terms.add(JsonObject.of("terms", JsonObject.of("status", filter.getStatuses())));
-        }
-
-        if (!CollectionUtils.isEmpty(filter.getEntrypointIds())) {
-            terms.add(JsonObject.of("terms", JsonObject.of("entrypoint-id", filter.getEntrypointIds())));
-        }
-
-        if (!terms.isEmpty()) {
-            return JsonObject.of("bool", JsonObject.of("must", JsonArray.of(terms.toArray())));
-        }
-
-        return null;
     }
 
     private static JsonObject buildSort() {
         return JsonObject.of("@timestamp", JsonObject.of("order", "desc"));
+    }
+
+    private static JsonObject buildV2AndV4Terms(String v2RequestIndexRef, String v4MetricsIndexRef, Collection<?> value) {
+        var terms = new ArrayList<JsonObject>();
+        terms.add(JsonObject.of("terms", JsonObject.of(v2RequestIndexRef, value.toArray())));
+        terms.add(JsonObject.of("terms", JsonObject.of(v4MetricsIndexRef, value.toArray())));
+        return buildShould(terms);
+    }
+
+    private static JsonObject buildShould(List<JsonObject> terms) {
+        return JsonObject.of("bool", JsonObject.of("should", JsonArray.of(terms.toArray())));
     }
 }
