b1386ca85765f999eeb380b653c0f31b2dbad1cf
Aditya Goyal <aditya.goyal@graviteesource.com>
2025-02-12 17:12:38 +0530

    fix: Access public APIs listed in a category when user is logged out
    Signed-off-by: aditya.goyal <aditya.goyal@graviteesource.com>

--- gravitee-apim-e2e/api-test/src/portal/mapi-v1/portal-api-view-and-search.spec.ts
+++ gravitee-apim-e2e/api-test/src/portal/mapi-v1/portal-api-view-and-search.spec.ts
@@ -13,9 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { afterAll, beforeAll, describe, test, expect } from '@jest/globals';
+import { afterAll, beforeAll, describe, expect, test } from '@jest/globals';
 
-import { forManagementAsAdminUser, forPortalAsAdminUser } from '@gravitee/utils/configuration';
+import { ANONYMOUS, forManagementAsAdminUser, forPortal, forPortalAsAdminUser } from '@gravitee/utils/configuration';
 import { APIsApi } from '@gravitee/management-webclient-sdk/src/lib/apis/APIsApi';
 import { ApisFaker } from '@gravitee/fixtures/management/ApisFaker';
 import { UpdateApiEntity, UpdateApiEntityFromJSON } from '@gravitee/management-webclient-sdk/src/lib/models/UpdateApiEntity';
@@ -26,6 +26,8 @@ import { FilterApiQuery } from '@gravitee/portal-webclient-sdk/src/lib/models/Fi
 import { ApiLifecycleState } from '@gravitee/management-webclient-sdk/src/lib/models/ApiLifecycleState';
 import { ConfigurationApi } from '@gravitee/management-webclient-sdk/src/lib/apis/ConfigurationApi';
 import { CategoryEntity } from '@gravitee/management-webclient-sdk/src/lib/models/CategoryEntity';
+import { Visibility } from '../../../../lib/management-v2-webclient-sdk/src/lib';
+import faker from '@faker-js/faker';
 
 const orgId = 'DEFAULT';
 const envId = 'DEFAULT';
@@ -33,6 +35,7 @@ const envId = 'DEFAULT';
 const apisManagementApiAsAdmin = new APIsApi(forManagementAsAdminUser());
 const apiPortalApiAsAdmin = new ApiApi(forPortalAsAdminUser());
 const configurationApiAsAdmin = new ConfigurationApi(forManagementAsAdminUser());
+const portalApiAsAnonymous = new ApiApi(forPortal({ auth: ANONYMOUS }));
 
 async function createAndPublish(apiManagementClient: APIsApi, attributes?: Partial<UpdateApiEntity>): Promise<ApiEntity> {
   let createdApi = await apiManagementClient.createApi({
@@ -54,7 +57,9 @@ describe('Portal - View and search APIs', () => {
   let apiWith2Labels: ApiEntity;
   let apiFeatured: ApiEntity;
   let apiWithCategory: ApiEntity;
+  let privateApiWithCategory: ApiEntity;
   let createdCategory: CategoryEntity;
+  const cat1 = `cat-${faker.datatype.uuid()}`;
 
   beforeAll(async () => {
     // create all APIs needed for testing
@@ -62,10 +67,15 @@ describe('Portal - View and search APIs', () => {
       configurationApiAsAdmin.createCategoryRaw({
         orgId,
         envId,
-        newCategoryEntity: { name: 'cat1' },
+        newCategoryEntity: { name: cat1 },
       }),
     );
-    apiWithCategory = await createAndPublish(apisManagementApiAsAdmin, { categories: ['cat1'], description: 'API with one category' });
+    apiWithCategory = await createAndPublish(apisManagementApiAsAdmin, { categories: [cat1], description: 'API with one category' });
+    privateApiWithCategory = await createAndPublish(apisManagementApiAsAdmin, {
+      categories: [cat1],
+      description: 'Private API with one category',
+      visibility: Visibility.PRIVATE,
+    });
     apiWith1Label = await createAndPublish(apisManagementApiAsAdmin, { labels: ['testlabel1'], description: 'API with one label' });
     apiWith2Labels = await createAndPublish(apisManagementApiAsAdmin, {
       labels: ['testlabel1', 'testlabel2'],
@@ -81,6 +91,8 @@ describe('Portal - View and search APIs', () => {
     await apisManagementApiAsAdmin.deleteApi({ orgId, envId, api: apiWith2Labels.id });
     await apisManagementApiAsAdmin.deleteApi({ orgId, envId, api: apiFeatured.id });
     await apisManagementApiAsAdmin.deleteApi({ orgId, envId, api: apiWithCategory.id });
+    await apisManagementApiAsAdmin.deleteApi({ orgId, envId, api: privateApiWithCategory.id });
+    await configurationApiAsAdmin.deleteTopApiRaw({ orgId, envId, topAPI: privateApiWithCategory.id });
     await configurationApiAsAdmin.deleteTopApiRaw({ orgId, envId, topAPI: apiFeatured.id });
     await configurationApiAsAdmin.deleteCategory({ orgId, envId, categoryId: createdCategory.id });
   });
@@ -89,11 +101,12 @@ describe('Portal - View and search APIs', () => {
     describe('Filter API list regarding FilterApiQuery', () => {
       test('should list all published APIs', async () => {
         const getApisResponse = await succeed(apiPortalApiAsAdmin.getApisRaw({ filter: FilterApiQuery.ALL }));
-        expect(getApisResponse.data).toHaveLength(4);
+        expect(getApisResponse.data).toHaveLength(5);
         expect(getApisResponse.data.some((filteredApi) => filteredApi.id === apiWith1Label.id)).toBeTruthy();
         expect(getApisResponse.data.some((filteredApi) => filteredApi.id === apiWith2Labels.id)).toBeTruthy();
         expect(getApisResponse.data.some((filteredApi) => filteredApi.id === apiFeatured.id)).toBeTruthy();
         expect(getApisResponse.data.some((filteredApi) => filteredApi.id === apiWithCategory.id)).toBeTruthy();
+        expect(getApisResponse.data.some((filteredApi) => filteredApi.id === privateApiWithCategory.id)).toBeTruthy();
       });
 
       test('should only list featured APIs', async () => {
@@ -106,18 +119,25 @@ describe('Portal - View and search APIs', () => {
     describe('Exclude APIs from list', function () {
       test('should exclude APIs with certain query param', async () => {
         const getApisResponse = await succeed(apiPortalApiAsAdmin.getApisRaw({ filter2: FilterApiQuery.FEATURED }));
-        expect(getApisResponse.data).toHaveLength(3);
+        expect(getApisResponse.data).toHaveLength(4);
         expect(getApisResponse.data.some((filteredApi) => filteredApi.id === apiWith1Label.id)).toBeTruthy();
         expect(getApisResponse.data.some((filteredApi) => filteredApi.id === apiWith2Labels.id)).toBeTruthy();
         expect(getApisResponse.data.some((filteredApi) => filteredApi.id === apiWithCategory.id)).toBeTruthy();
+        expect(getApisResponse.data.some((filteredApi) => filteredApi.id === privateApiWithCategory.id)).toBeTruthy();
       });
     });
 
     describe('Filter API list regarding category', function () {
       test('should list all APIs with certain category', async () => {
-        let getApisResponse = await succeed(apiPortalApiAsAdmin.getApisRaw({ category: 'cat1' }));
-        expect(getApisResponse.data).toHaveLength(1);
-        expect(getApisResponse.data[0].id).toBe(apiWithCategory.id);
+        let getApisResponse = await succeed(apiPortalApiAsAdmin.getApisRaw({ category: cat1 }));
+        expect(getApisResponse.data).toHaveLength(2);
+        expect(getApisResponse.data.some((filteredApi) => filteredApi.id === apiWithCategory.id)).toBeTruthy();
+        expect(getApisResponse.data.some((filteredApi) => filteredApi.id === privateApiWithCategory.id)).toBeTruthy();
+      });
+
+      test('should list no APIs for anonymous user because all APIs are private', async () => {
+        let getApisResponse = await succeed(portalApiAsAnonymous.getApisRaw({ category: cat1 }));
+        expect(getApisResponse.data).toHaveLength(0);
       });
     });
 
@@ -149,7 +169,7 @@ describe('Portal - View and search APIs', () => {
 
       test('should show certain page when limiting API to certain value', async () => {
         const getApisResponse = await succeed(apiPortalApiAsAdmin.getApisRaw({ size: 3, page: 2 }));
-        expect(getApisResponse.data).toHaveLength(1);
+        expect(getApisResponse.data).toHaveLength(2);
       });
     });
   });
@@ -170,6 +190,20 @@ describe('Portal - View and search APIs', () => {
     test('should not find any APIs if search string does not match anything', async () => {
       const searchResponse = await succeed(apiPortalApiAsAdmin.searchApisRaw({ q: 'unmatchedString' }));
       expect(searchResponse.data).toHaveLength(0);
+    });
+  });
+
+  describe('Update apiWithCategory visibility and fetch APIs', () => {
+    test('should get public API only when user is anonymous', async () => {
+      const publicApiWithCategory = await createAndPublish(apisManagementApiAsAdmin, {
+        categories: [cat1],
+        description: 'API with one category',
+        visibility: 'PUBLIC',
+      });
+      let getApisResponse = await succeed(portalApiAsAnonymous.getApisRaw({ category: cat1 }));
+      expect(getApisResponse.data).toHaveLength(1);
+      expect(getApisResponse.data.some((foundApi) => foundApi.id === publicApiWithCategory.id)).toBeTruthy();
+      await apisManagementApiAsAdmin.deleteApi({ orgId, envId, api: publicApiWithCategory.id });
     });
   });
 });
