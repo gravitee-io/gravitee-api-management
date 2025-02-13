853c09087f6d878c73f72c3531a820be861d768d
Marek Mazur <marek.mazur@graviteesource.com>
2025-02-13 14:45:23 +0100

    fix(repository-mongo): improve SubscriptionApplicationNameUpgrader to limit bulkActions

--- gravitee-apim-repository/gravitee-apim-repository-mongodb/src/main/java/io/gravitee/repository/mongodb/management/upgrade/upgrader/subscription/SubscriptionApplicationNameUpgrader.java
+++ gravitee-apim-repository/gravitee-apim-repository-mongodb/src/main/java/io/gravitee/repository/mongodb/management/upgrade/upgrader/subscription/SubscriptionApplicationNameUpgrader.java
@@ -19,8 +19,8 @@ import static io.gravitee.repository.mongodb.management.upgrade.upgrader.sharedp
 
 import com.mongodb.client.model.Filters;
 import com.mongodb.client.model.UpdateManyModel;
+import com.mongodb.client.model.Updates;
 import io.gravitee.repository.mongodb.management.upgrade.upgrader.common.MongoUpgrader;
-import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 import org.bson.Document;
@@ -33,36 +33,18 @@ public class SubscriptionApplicationNameUpgrader extends MongoUpgrader {
 
     @Override
     public boolean upgrade() {
-        // 1. Find plan IDs with security != 'KEY_LESS'
-        // note: security isn't indexed so we need to handle filtering in code to satisfy notablescan rule
-        var planDocs = getCollection("plans").find().projection(new Document("_id", 1).append("security", 1));
-        List<String> planIds = StreamSupport
-            .stream(planDocs.spliterator(), false)
-            .filter(doc -> !"KEY_LESS".equals(doc.getString("security")))
-            .map(doc -> doc.get("_id").toString())
-            .collect(Collectors.toList());
+        // Fetch all documents from applications
+        var applicationDocs = getCollection("applications").find().projection(new Document("_id", 1).append("name", 1));
 
-        // 2. Find subscriptions with those plan IDs
-        var subscriptionDocs = getCollection("subscriptions")
-            .find(Filters.and(Filters.in("plan", planIds), Filters.exists("applicationName", false)))
-            .projection(new Document("_id", 1).append("application", 1));
         var bulkActions = StreamSupport
-            .stream(subscriptionDocs.spliterator(), false)
-            .map(subscriptionDoc -> {
-                // 3. Pair with applications
-                String applicationId = subscriptionDoc.get("application").toString();
-                return new SubscriptionApplicationPair(
-                    subscriptionDoc,
-                    getCollection("applications").find(Filters.eq("_id", applicationId)).first()
-                );
-            })
-            .filter(pair -> pair.application() != null)
-            .map(pair -> {
-                String applicationName = pair.application.getString("name");
-                // 4. Update subscription with application name
+            .stream(applicationDocs.spliterator(), false)
+            .map(applicationDoc -> {
+                String applicationId = applicationDoc.get("_id").toString();
+                String applicationName = applicationDoc.getString("name");
+                // Create UpdateManyModel for each application
                 return new UpdateManyModel<Document>(
-                    Filters.eq("_id", pair.subscription.get("_id").toString()),
-                    new Document("$set", new Document("applicationName", applicationName))
+                    Filters.eq("application", applicationId),
+                    Updates.set("applicationName", applicationName)
                 );
             })
             .collect(Collectors.toList());
@@ -78,6 +60,4 @@ public class SubscriptionApplicationNameUpgrader extends MongoUpgrader {
     public int getOrder() {
         return SUBSCRIPTION_APPLICATION_NAME_UPGRADER_ORDER;
     }
-
-    record SubscriptionApplicationPair(Document subscription, Document application) {}
 }
