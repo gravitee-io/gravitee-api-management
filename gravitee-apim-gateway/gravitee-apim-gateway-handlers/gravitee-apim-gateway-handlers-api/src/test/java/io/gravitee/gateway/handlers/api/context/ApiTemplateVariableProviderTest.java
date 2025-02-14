d4e252e94ecab5a7b6a15f9ccaafd6b036f3507f
Julien Giovaresco <dev@giovaresco.fr>
2025-02-14 09:23:53 +0100

    test: update exception message asserted due to Spring change
    https://github.com/spring-projects/spring-framework/pull/29847
    (cherry picked from commit 8d7bd2e7e5084b5cd88d63b6a368f436be5871fd)

--- gravitee-apim-gateway/gravitee-apim-gateway-handlers/gravitee-apim-gateway-handlers-api/src/test/java/io/gravitee/gateway/handlers/api/context/ApiTemplateVariableProviderTest.java
+++ gravitee-apim-gateway/gravitee-apim-gateway-handlers/gravitee-apim-gateway-handlers-api/src/test/java/io/gravitee/gateway/handlers/api/context/ApiTemplateVariableProviderTest.java
@@ -113,7 +113,7 @@ class ApiTemplateVariableProviderTest {
                 assertThat(e)
                     .isInstanceOf(ExpressionEvaluationException.class)
                     .hasCauseInstanceOf(SpelEvaluationException.class)
-                    .hasStackTraceContaining("EL1007E: Property or field 'prop2' cannot be found on null");
+                    .hasStackTraceContaining("EL1012E: Cannot index into a null value");
 
                 return true;
             });
