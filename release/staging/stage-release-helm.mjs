2b6261a67a3b78613657826c07401a5ddc2bd9d6
Julien Giovaresco <dev@giovaresco.fr>
2025-02-17 16:17:47 +0100

    chore(deps): bump dependencies of releaser module
    Define all dependencies as dev dependencies
    Upgrade Prettier and apply the formatting
    Upgrade ZX to the latest version

--- release/staging/stage-release-helm.mjs
+++ release/staging/stage-release-helm.mjs
@@ -18,59 +18,46 @@ const dry_run = true;
 
 const pipelineURI = 'https://circleci.com/api/v2/project/gh/gravitee-io/gravitee-api-management/pipeline';
 
+console.log(chalk.yellow(`This script will stage a new helm release in order to test it on ${branch}.`));
 console.log(
-    chalk.yellow(
-        `This script will stage a new helm release in order to test it on ${branch}.`
-    )
+  chalk.yellow(`This will be done by overwriting or creating chart apim ${graviteeio_version} at oci://graviteeio.azurecr.io/helm.`),
 );
-console.log(
-    chalk.yellow(
-        `This will be done by overwriting or creating chart apim ${graviteeio_version} at oci://graviteeio.azurecr.io/helm.`)
-);
-console.log(
-    chalk.yellow(
-        `If you are performing an actual release, then you're in the wrong place.`
-    )
-);
+console.log(chalk.yellow(`If you are performing an actual release, then you're in the wrong place.`));
 
 console.log();
 
-const iKnowWhatImDoing = await question(
-    chalk.blue(
-        `Do you want to continue ? (Type 'yes' if you want to)\n`
-    )
-);
+const iKnowWhatImDoing = await question(chalk.blue(`Do you want to continue ? (Type 'yes' if you want to)\n`));
 
 console.log();
 
 if (iKnowWhatImDoing !== 'yes') {
-    console.log(chalk.greenBright('👋 Farewell, pal!'));
-    process.exit(0);
+  console.log(chalk.greenBright('👋 Farewell, pal!'));
+  process.exit(0);
 }
 
 const body = {
-    branch,
-    parameters: {
-        gio_action,
-        dry_run,
-        graviteeio_version,
-    },
+  branch,
+  parameters: {
+    gio_action,
+    dry_run,
+    graviteeio_version,
+  },
 };
 
 const response = await fetch(pipelineURI, {
-    method: 'post',
-    body: JSON.stringify(body),
-    headers: {
-        'Content-Type': 'application/json',
-        'Circle-Token': process.env.CIRCLECI_TOKEN,
-    }
+  method: 'post',
+  body: JSON.stringify(body),
+  headers: {
+    'Content-Type': 'application/json',
+    'Circle-Token': process.env.CIRCLECI_TOKEN,
+  },
 });
 
 const data = await response.json();
 
 if (response.status === 201) {
-    console.log(chalk.green(`Pipeline created with number: ${data.number}`));
-    echo`Follow its progress on: https://app.circleci.com/pipelines/github/gravitee-io/gravitee-api-management/${data.number}`;
+  console.log(chalk.green(`Pipeline created with number: ${data.number}`));
+  echo`Follow its progress on: https://app.circleci.com/pipelines/github/gravitee-io/gravitee-api-management/${data.number}`;
 } else {
-    console.log(chalk.red(`Something went wrong. Expected status 201, got ${response.status}.`));
+  console.log(chalk.red(`Something went wrong. Expected status 201, got ${response.status}.`));
 }
