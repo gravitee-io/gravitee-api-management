627b8cdb104ef669ecb110dc07e68986746d2c3c
Florent CHAMFROY <florent.chamfroy@graviteesource.com>
2025-02-24 20:36:53 +0100

    ci: update bridge compatibility tests
    use 4.4.2 as 4.4.0 & 4.4.1 were not stable and sync process was not working correctly

--- .circleci/ci/src/workflows/workflow-bridge-compatibility-tests.ts
+++ .circleci/ci/src/workflows/workflow-bridge-compatibility-tests.ts
@@ -67,7 +67,7 @@ export class BridgeCompatibilityTestsWorkflow {
             '4.5.x-latest',
             'graviteeio@4.5.0',
             '4.4.x-latest',
-            'graviteeio@4.4.0',
+            'graviteeio@4.4.2',
             '4.3.x-latest',
             'graviteeio@4.3.0',
           ],
