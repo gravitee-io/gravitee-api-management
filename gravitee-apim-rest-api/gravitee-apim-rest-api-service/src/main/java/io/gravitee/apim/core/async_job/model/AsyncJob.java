gravitee-io/gateway

feat: Create deadline functionality for AsyncJob
APIM-8435

gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/apim/core/async_job/model/AsyncJob.java
@@ -16,6 +16,7 @@
 package io.gravitee.apim.core.async_job.model;
 
 import io.gravitee.common.utils.TimeProvider;
+import jakarta.annotation.Nullable;
 import java.time.ZonedDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -50,6 +51,9 @@ public class AsyncJob {
     ZonedDateTime createdAt;
     ZonedDateTime updatedAt;
 
+    @Nullable
+    ZonedDateTime deadLine;
+
     Status status;
 
     /** The error message if the job failed */
@@ -64,7 +68,12 @@ public class AsyncJob {
     public enum Status {
         PENDING,
         SUCCESS,
-        ERROR,
+        TIMEOUT,
+        ERROR;
+
+        public boolean isFinal() {
+            return this != PENDING;
+        }
     }
 
     public enum Type {
@@ -74,5 +83,13 @@ public class AsyncJob {
 
     public AsyncJob complete() {
         return toBuilder().status(Status.SUCCESS).updatedAt(TimeProvider.now()).build();
+    }
+
+    public AsyncJob timeout() {
+        return toBuilder().status(Status.TIMEOUT).updatedAt(TimeProvider.now()).build();
+    }
+
+    public boolean isTimeout() {
+        return !status.isFinal() && deadLine != null && deadLine.isBefore(TimeProvider.now());
     }
 }

gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/apim/infra/crud_service/async_job/AsyncJobCrudServiceImpl.java
@@ -21,6 +21,8 @@ import io.gravitee.apim.infra.adapter.AsyncJobAdapter;
 import io.gravitee.repository.exceptions.TechnicalException;
 import io.gravitee.repository.management.api.AsyncJobRepository;
 import io.gravitee.rest.api.service.exceptions.TechnicalManagementException;
+import java.time.ZonedDateTime;
+import java.util.Date;
 import java.util.Optional;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
@@ -47,7 +49,10 @@ public class AsyncJobCrudServiceImpl implements AsyncJobCrudService {
     @Override
     public Optional<AsyncJob> findById(String id) {
         try {
-            return asyncJobRepository.findById(id).map(AsyncJobAdapter.INSTANCE::toEntity);
+            return asyncJobRepository
+                .findById(id)
+                .map(AsyncJobAdapter.INSTANCE::toEntity)
+                .map(job -> job.isTimeout() ? update(job.timeout()) : job);
         } catch (TechnicalException e) {
             throw new TechnicalManagementException("An error occurs while trying to find the AsyncJob: " + id, e);
         }
@@ -60,6 +65,15 @@ public class AsyncJobCrudServiceImpl implements AsyncJobCrudService {
             return AsyncJobAdapter.INSTANCE.toEntity(updated);
         } catch (TechnicalException e) {
             throw new TechnicalManagementException("An error occurred when updating AsyncJob: " + job.getId(), e);
+        }
+    }
+
+    @Override
+    public void delay(String id, ZonedDateTime newDeadLine) {
+        try {
+            asyncJobRepository.delay(id, Date.from(newDeadLine.toInstant()));
+        } catch (TechnicalException e) {
+            throw new TechnicalManagementException("An error occurred when give more delay to AsyncJob: " + id, e);
         }
     }
 

gravitee-io/gravitee-api-management

gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/apim/core/async_job/model/AsyncJob.java
@@ -16,6 +16,7 @@
 package io.gravitee.apim.core.async_job.model;
 
 import io.gravitee.common.utils.TimeProvider;
+import jakarta.annotation.Nullable;
 import java.time.ZonedDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -50,6 +51,9 @@ public class AsyncJob {
     ZonedDateTime createdAt;
     ZonedDateTime updatedAt;
 
+    @Nullable
+    ZonedDateTime deadLine;
+
     Status status;
 
     /** The error message if the job failed */
@@ -64,7 +68,12 @@ public class AsyncJob {
     public enum Status {
         PENDING,
         SUCCESS,
-        ERROR,
+        TIMEOUT,
+        ERROR;
+
+        public boolean isFinal() {
+            return this != PENDING;
+        }
     }
 
     public enum Type {
@@ -74,5 +83,17 @@ public class AsyncJob {
 
     public AsyncJob complete() {
         return toBuilder().status(Status.SUCCESS).updatedAt(TimeProvider.now()).build();
+    }
+
+    public AsyncJob timeout() {
+        return toBuilder().status(Status.TIMEOUT).updatedAt(TimeProvider.now()).build();
+    }
+
+    public boolean isTimedOut() {
+        return !status.isFinal() && deadLine != null && deadLine.isBefore(TimeProvider.now());
+    }
+
+    public boolean isLate() {
+        return !status.isFinal() && deadLine != null && deadLine.isBefore(TimeProvider.now());
     }
 }

gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/apim/infra/crud_service/async_job/AsyncJobCrudServiceImpl.java
@@ -21,6 +21,8 @@ import io.gravitee.apim.infra.adapter.AsyncJobAdapter;
 import io.gravitee.repository.exceptions.TechnicalException;
 import io.gravitee.repository.management.api.AsyncJobRepository;
 import io.gravitee.rest.api.service.exceptions.TechnicalManagementException;
+import java.time.ZonedDateTime;
+import java.util.Date;
 import java.util.Optional;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
@@ -47,7 +49,10 @@ public class AsyncJobCrudServiceImpl implements AsyncJobCrudService {
     @Override
     public Optional<AsyncJob> findById(String id) {
         try {
-            return asyncJobRepository.findById(id).map(AsyncJobAdapter.INSTANCE::toEntity);
+            return asyncJobRepository
+                .findById(id)
+                .map(AsyncJobAdapter.INSTANCE::toEntity)
+                .map(job -> job.isLate() ? update(job.timeout()) : job);
         } catch (TechnicalException e) {
             throw new TechnicalManagementException("An error occurs while trying to find the AsyncJob: " + id, e);
         }
@@ -60,6 +65,15 @@ public class AsyncJobCrudServiceImpl implements AsyncJobCrudService {
             return AsyncJobAdapter.INSTANCE.toEntity(updated);
         } catch (TechnicalException e) {
             throw new TechnicalManagementException("An error occurred when updating AsyncJob: " + job.getId(), e);
+        }
+    }
+
+    @Override
+    public void delay(String id, ZonedDateTime newDeadLine) {
+        try {
+            asyncJobRepository.delay(id, Date.from(newDeadLine.toInstant()));
+        } catch (TechnicalException e) {
+            throw new TechnicalManagementException("An error occurred when give more delay to AsyncJob: " + id, e);
         }
     }
 