33da1e52c6ada2b7359a64a8ce9072d8c3d80ec0
NIVEDITA SINGH <niveditashenoy@gmail.com>
2025-03-03 15:46:47 +0530

    fix: error on disable analytics
    (cherry picked from commit dcf51b8e39c1f60d05e5b1da9c99814155ac5196)

---
+++ gravitee-apim-repository/gravitee-apim-repository-noop/src/main/java/io/gravitee/repository/noop/log/v4/NoOpAnalyticsRepository.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright © 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.repository.noop.log.v4;
+
+import io.gravitee.repository.common.query.QueryContext;
+import io.gravitee.repository.log.v4.api.AnalyticsRepository;
+import io.gravitee.repository.log.v4.model.analytics.*;
+import io.reactivex.rxjava3.annotations.NonNull;
+import java.util.Optional;
+
+public class NoOpAnalyticsRepository implements AnalyticsRepository {
+
+    @Override
+    public Optional<CountAggregate> searchRequestsCount(QueryContext queryContext, RequestsCountQuery requestsCountQuery) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<AverageAggregate> searchAverageMessagesPerRequest(QueryContext queryContext, AverageMessagesPerRequestQuery query) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<AverageAggregate> searchAverageConnectionDuration(QueryContext queryContext, AverageConnectionDurationQuery query) {
+        return Optional.empty();
+    }
+
+    @Override
+    public @NonNull Optional<ResponseStatusRangesAggregate> searchResponseStatusRanges(
+        QueryContext queryContext,
+        ResponseStatusRangesQuery query
+    ) {
+        return Optional.empty();
+    }
+}
