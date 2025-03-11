e11d6bca4fd7e5525a594ffc1f36af65b5d6d983
Ouahid Khelifi <ouahid.khelifi@gmail.com>
2025-03-11 16:37:13 +0100

    feat: portal user can see his subscription configuration

---
+++ gravitee-apim-portal-webui-next/src/app/api/api-details/api-tab-subscriptions/subscriptions-details/subscription-consumer-configuration/subscription-consumer-configuration.component.ts
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2025 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { Component, effect, input, InputSignal } from '@angular/core';
+import { MatAnchor } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+
+import { SubscriptionConsumerConfiguration } from '../../../../../../entities/subscription';
+
+@Component({
+  imports: [MatCardModule, MatListModule, MatAnchor],
+  selector: 'app-subscription-consumer-configuration',
+  standalone: true,
+  templateUrl: './subscription-consumer-configuration.component.html',
+  styleUrls: ['./subscription-consumer-configuration.component.scss'],
+})
+export class SubscriptionConsumerConfigurationComponent {
+  consumerConfiguration: InputSignal<SubscriptionConsumerConfiguration> = input.required<SubscriptionConsumerConfiguration>();
+  hasChannel: boolean = false;
+  hasHeaders: boolean = false;
+
+  constructor() {
+    effect(() => {
+      const consumerConfiguration: SubscriptionConsumerConfiguration = this.consumerConfiguration();
+      const entrypointConfiguration = consumerConfiguration.entrypointConfiguration;
+      this.hasChannel = !!consumerConfiguration.channel;
+      this.hasHeaders = !!entrypointConfiguration?.headers && entrypointConfiguration?.headers?.length > 0;
+    });
+  }
+}
--- gravitee-apim-portal-webui-next/src/entities/subscription/subscription-consumer-configuration.fixture.ts
+++ gravitee-apim-portal-webui-next/src/entities/subscription/subscription-consumer-configuration.fixture.ts
@@ -13,33 +13,48 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import {SubscriptionConsumerConfiguration} from "./subscription-consumer-configuration";
-import {isFunction} from "rxjs/internal/util/isFunction";
+import { isFunction } from 'rxjs/internal/util/isFunction';
 
-export function fakeSubscriptionConsumerConfiguration(modifier?: Partial<SubscriptionConsumerConfiguration> | ((baseSubscriptionConsumerConfiguration: SubscriptionConsumerConfiguration) => SubscriptionConsumerConfiguration)): SubscriptionConsumerConfiguration {
+import { SubscriptionConsumerConfiguration } from './subscription-consumer-configuration';
+
+export function fakeSubscriptionConsumerConfiguration(
+  modifier?:
+    | Partial<SubscriptionConsumerConfiguration>
+    | ((baseSubscriptionConsumerConfiguration: SubscriptionConsumerConfiguration) => SubscriptionConsumerConfiguration),
+): SubscriptionConsumerConfiguration {
   const base: SubscriptionConsumerConfiguration = {
     entrypointId: 'webhook',
     channel: 'test',
     entrypointConfiguration: {
+      callbackUrl: 'https://webhook.example/1234',
+      headers: [
+        {
+          name: 'Content-Type',
+          value: 'application/json',
+        },
+        {
+          name: 'X-Custom-Key',
+          value: '1234',
+        },
+      ],
       auth: {
-        type: "none"
+        type: 'none',
       },
-      callbackUrl: "https://webhook.example/1234",
       ssl: {
         keyStore: {
-          type: ""
+          type: '',
         },
         hostnameVerifier: false,
         trustStore: {
-          "type": ""
+          type: '',
         },
-        trustAll: true
+        trustAll: true,
       },
       retry: {
-        retryOption: "No Retry"
-      }
-    }
-  }
+        retryOption: 'No Retry',
+      },
+    },
+  };
 
   if (isFunction(modifier)) {
     return modifier(base);
