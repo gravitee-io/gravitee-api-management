1e0ad673987f173aad270c251ecf5e0659d4ddc0
jourdiw <jourdi.waller@graviteesource.com>
2025-03-11 10:31:13 +0100

    feat(portal-next): create application log messages component

---
+++ gravitee-apim-portal-webui-next/src/app/applications/application/application-tab-logs/application-log-messages/application-log-messages.component.ts
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2025 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { Component, input } from '@angular/core';
+
+import { Log } from '../../../../../entities/log/log';
+
+@Component({
+  selector: 'app-application-log-messages',
+  standalone: true,
+  imports: [],
+  templateUrl: './application-log-messages.component.html',
+  styleUrl: './application-log-messages.component.scss',
+})
+export class ApplicationLogMessagesComponent {
+  log = input.required<Log>();
+}
