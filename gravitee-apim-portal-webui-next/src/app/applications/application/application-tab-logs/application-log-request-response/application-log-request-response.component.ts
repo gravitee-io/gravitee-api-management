9df4a49b025e170123916f4ef16bb903b0201a93
jourdiw <jourdi.waller@graviteesource.com>
2025-03-11 10:30:53 +0100

    refactor(portal-next): separate pagination and log request/response components
    refactor(portal-next): move pagination to a separate component
    
    (cherry picked from commit 23a6b0dd9705ada8b888a583c9afec6e9e69396d)

---
+++ gravitee-apim-portal-webui-next/src/app/applications/application/application-tab-logs/application-log-request-response/application-log-request-response.component.ts
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2025 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { Component, computed, input, Signal } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatExpansionModule } from '@angular/material/expansion';
+
+import { CopyCodeComponent } from '../../../../../components/copy-code/copy-code.component';
+import { Log, LogMetadataApi, LogMetadataPlan } from '../../../../../entities/log';
+
+interface LogVM extends Log {
+  apiName: string;
+  planName: string;
+  requestHeaders: { key: string; value: string }[];
+  responseHeaders: { key: string; value: string }[];
+}
+
+@Component({
+  selector: 'app-application-log-request-response',
+  standalone: true,
+  imports: [CopyCodeComponent, MatCardModule, MatExpansionModule],
+  templateUrl: './application-log-request-response.component.html',
+  styleUrl: './application-log-request-response.component.scss',
+})
+export class ApplicationLogRequestResponseComponent {
+  log = input.required<Log>();
+  logVM: Signal<LogVM> = computed(() => {
+    const log = this.log();
+
+    const apiName = log.metadata?.[log.api]
+      ? `${(log.metadata[log.api] as LogMetadataApi).name} (${(log.metadata[log.api] as LogMetadataApi).version})`
+      : '';
+    const apiType = (log.metadata?.[log.api] as LogMetadataApi)?.apiType;
+    const planName = log.metadata?.[log.plan] ? (log.metadata[log.plan] as LogMetadataPlan).name : '';
+    const requestHeaders = log.request?.headers
+      ? Object.entries(log.request.headers).map(keyValueArray => ({ key: keyValueArray[0], value: keyValueArray[1] }))
+      : [];
+    const responseHeaders = log.response?.headers
+      ? Object.entries(log.response.headers).map(keyValueArray => ({ key: keyValueArray[0], value: keyValueArray[1] }))
+      : [];
+    return { ...log, apiName, apiType, planName, requestHeaders, responseHeaders };
+  });
+}
---
+++ gravitee-apim-portal-webui-next/src/components/pagination/pagination.component.ts
@@ -0,0 +1,69 @@
+/*
+ * Copyright (C) 2025 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { Component, computed, input, output, Signal } from '@angular/core';
+import { MatButton, MatIconButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+
+interface PaginationVM {
+  hasPreviousPage: boolean;
+  hasNextPage: boolean;
+  currentPage: number;
+  totalPages: number;
+}
+
+@Component({
+  selector: 'app-pagination',
+  standalone: true,
+  imports: [MatButton, MatIcon, MatIconButton],
+  templateUrl: './pagination.component.html',
+  styleUrl: './pagination.component.scss',
+})
+export class PaginationComponent {
+  totalResults = input.required<number>();
+  currentPage = input.required<number>();
+  pageSize = input<number>(10);
+
+  selectPage = output<number>();
+
+  pagination: Signal<PaginationVM> = computed(() => {
+    const totalPages = Math.ceil(this.totalResults() / this.pageSize());
+
+    return {
+      hasPreviousPage: this.currentPage() > 1,
+      hasNextPage: this.currentPage() < totalPages,
+      currentPage: this.currentPage(),
+      totalPages,
+    };
+  });
+
+  goToPage(page: number) {
+    if (page > 0 && page <= this.pagination().totalPages) {
+      this.selectPage.emit(page);
+    }
+  }
+
+  goToPreviousPage() {
+    if (this.currentPage() > 0) {
+      this.selectPage.emit(this.currentPage() - 1);
+    }
+  }
+
+  goToNextPage() {
+    if (this.currentPage() < this.pagination().totalPages) {
+      this.selectPage.emit(this.currentPage() + 1);
+    }
+  }
+}
