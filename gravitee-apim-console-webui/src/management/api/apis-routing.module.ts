7b78729e83849e850fdc0653feab67bcc788cb8b
vikrant <vikrant.kumar@graviteesource.com>
2025-03-13 10:29:56 +0530

    fix: documentation tab for read only users

--- gravitee-apim-console-webui/src/management/api/apis-routing.module.ts
+++ gravitee-apim-console-webui/src/management/api/apis-routing.module.ts
@@ -869,7 +869,7 @@ const apisRoutes: Routes = [
             data: {
               docs: null,
               permissions: {
-                anyOf: ['api-documentation-u'],
+                anyOf: ['api-documentation-u', 'api-documentation-r'],
               },
             },
             component: ApiDocumentationV4DefaultPageComponent,
