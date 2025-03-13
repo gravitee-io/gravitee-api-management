04b61c549713209c007f95ed2ad305c178bc1da8
jourdiw <jourdi.waller@graviteesource.com>
2025-03-13 17:47:01 +0100

    refactor(portal-next): move pagination to a separate component

--- gravitee-apim-portal-webui-next/src/app/applications/application/application-tab-logs/application-log-table/application-log-table.component.spec.ts
+++ gravitee-apim-portal-webui-next/src/app/applications/application/application-tab-logs/application-log-table/application-log-table.component.spec.ts
@@ -27,6 +27,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { BehaviorSubject } from 'rxjs';
 
 import { ApplicationLogTableComponent } from './application-log-table.component';
+import { PaginationHarness } from '../../../../../components/pagination/pagination.harness';
 import { fakeApplication } from '../../../../../entities/application/application.fixture';
 import { LogsResponse } from '../../../../../entities/log/log';
 import { fakeLogListItem, fakeLogsResponse } from '../../../../../entities/log/log.fixture';
@@ -135,41 +136,6 @@ describe('ApplicationLogTableComponent', () => {
     });
 
     describe('Pagination', () => {
-      describe('Only one page of results', () => {
-        beforeEach(async () => {
-          expectGetApplicationLogs(
-            fakeLogsResponse({
-              data: [
-                fakeLogListItem({ api: 'my-api', plan: 'my-plan', status: 201, timestamp: 1466424490000 }),
-                fakeLogListItem({ api: 'my-api-2', plan: 'my-plan', status: 204, timestamp: 1566424490000 }),
-              ],
-              metadata: {
-                'my-api': { name: 'My API', version: '1.0' },
-                'my-api-2': { name: 'My API 2', version: '2.0' },
-                'my-plan': { name: 'My Plan' },
-                data: { total: 3 },
-              },
-            }),
-          );
-          expectGetSubscriptions(fakeSubscriptionResponse());
-          fixture.detectChanges();
-        });
-        it('should not allow previous page on load', async () => {
-          const previousPageButton = await getPreviousPageButton();
-          expect(previousPageButton).toBeTruthy();
-          expect(await previousPageButton.isDisabled()).toEqual(true);
-        });
-        it('should not allow next page when on last page', async () => {
-          const nextPageButton = await getNextPageButton();
-          expect(nextPageButton).toBeTruthy();
-          expect(await nextPageButton.isDisabled()).toEqual(true);
-        });
-        it('should highlight current page', async () => {
-          const currentPaginationPage = await getCurrentPaginationPage();
-          expect(currentPaginationPage).toBeTruthy();
-          expect(await currentPaginationPage.getText()).toEqual('1');
-        });
-      });
       describe('First of many pages of results', () => {
         beforeEach(async () => {
           expectGetApplicationLogs(
@@ -189,40 +155,10 @@ describe('ApplicationLogTableComponent', () => {
           expectGetSubscriptions(fakeSubscriptionResponse());
           fixture.detectChanges();
         });
-        it('should not allow previous page on load', async () => {
-          const previousPageButton = await getPreviousPageButton();
-          expect(previousPageButton).toBeTruthy();
-          expect(await previousPageButton.isDisabled()).toEqual(true);
-        });
-        it('should allow next page', async () => {
-          const nextPageButton = await getNextPageButton();
-          expect(nextPageButton).toBeTruthy();
-          expect(await nextPageButton.isDisabled()).toEqual(false);
-        });
-        it('should show "2" for next page', async () => {
-          const secondPageButton = await getPageButtonByLabel('2');
-          expect(secondPageButton).toBeTruthy();
-          expect(await secondPageButton.isDisabled()).toEqual(false);
-        });
-        it('should show "3" for page option', async () => {
-          const thirdPageButton = await getPageButtonByLabel('3');
-          expect(thirdPageButton).toBeTruthy();
-          expect(await thirdPageButton.isDisabled()).toEqual(false);
-        });
 
-        it('should show "8" for last page', async () => {
-          const lastPageButton = await getPageButtonByLabel('8');
-          expect(lastPageButton).toBeTruthy();
-          expect(await lastPageButton.isDisabled()).toEqual(false);
-        });
-
-        it('should highlight current page', async () => {
-          const currentPaginationPage = await getCurrentPaginationPage();
-          expect(currentPaginationPage).toBeTruthy();
-          expect(await currentPaginationPage.getText()).toEqual('1');
-        });
         it('should go to next page via page number button', async () => {
-          const secondPageButton = await getPageButtonByLabel('2');
+          const pagination = await harnessLoader.getHarness(PaginationHarness);
+          const secondPageButton = await pagination.getPageButtonByNumber(2);
           await secondPageButton.click();
           fixture.detectChanges();
 
@@ -244,7 +180,8 @@ describe('ApplicationLogTableComponent', () => {
           );
         });
         it('should go to last page', async () => {
-          const lastPageButton = await getPageButtonByLabel('8');
+          const pagination = await harnessLoader.getHarness(PaginationHarness);
+          const lastPageButton = await pagination.getPageButtonByNumber(8);
           await lastPageButton.click();
 
           expectGetApplicationLogs(
@@ -294,38 +231,10 @@ describe('ApplicationLogTableComponent', () => {
         );
         expectGetSubscriptions(fakeSubscriptionResponse());
       });
-      it('should allow previous page', async () => {
-        const previousPageButton = await getPreviousPageButton();
-        expect(previousPageButton).toBeTruthy();
-        expect(await previousPageButton.isDisabled()).toEqual(false);
-      });
-      it('should allow next page', async () => {
-        const nextPageButton = await getNextPageButton();
-        expect(nextPageButton).toBeTruthy();
-        expect(await nextPageButton.isDisabled()).toEqual(false);
-      });
-      it('should show "1" for first page', async () => {
-        const firstPageButton = await getPageButtonByLabel('1');
-        expect(firstPageButton).toBeTruthy();
-        expect(await firstPageButton.isDisabled()).toEqual(false);
-      });
-      it('should show "2" for previous page', async () => {
-        const secondPageButton = await getPageButtonByLabel('2');
-        expect(secondPageButton).toBeTruthy();
-        expect(await secondPageButton.isDisabled()).toEqual(false);
-      });
-      it('should show "8" for last page', async () => {
-        const lastPageButton = await getPageButtonByLabel('8');
-        expect(lastPageButton).toBeTruthy();
-        expect(await lastPageButton.isDisabled()).toEqual(false);
-      });
-      it('should highlight current page', async () => {
-        const currentPaginationPage = await getCurrentPaginationPage();
-        expect(currentPaginationPage).toBeTruthy();
-        expect(await currentPaginationPage.getText()).toEqual('3');
-      });
+
       it('should go to previous page via arrow', async () => {
-        const previousPageButton = await getPreviousPageButton();
+        const pagination = await harnessLoader.getHarness(PaginationHarness);
+        const previousPageButton = await pagination.getPreviousPageButton();
         await previousPageButton.click();
 
         expectGetApplicationLogs(
@@ -1183,22 +1092,6 @@ describe('ApplicationLogTableComponent', () => {
       .then(table => table.getRows())
       .then(rows => rows[i].getCells({ columnName }))
       .then(cells => cells[0].getText());
-  }
-
-  async function getPreviousPageButton(): Promise<MatButtonHarness> {
-    return await harnessLoader.getHarness(MatButtonHarness.with({ selector: '[aria-label="Previous page of logs"]', variant: 'icon' }));
-  }
-
-  async function getNextPageButton(): Promise<MatButtonHarness> {
-    return await harnessLoader.getHarness(MatButtonHarness.with({ selector: '[aria-label="Next page of logs"]', variant: 'icon' }));
-  }
-
-  async function getPageButtonByLabel(label: string): Promise<MatButtonHarness> {
-    return await harnessLoader.getHarness(MatButtonHarness.with({ text: label }));
-  }
-
-  async function getCurrentPaginationPage(): Promise<MatButtonHarness> {
-    return await harnessLoader.getHarness(MatButtonHarness.with({ selector: '[aria-label="Current page of logs"]' }));
   }
 
   async function noChipFiltersDisplayed(): Promise<boolean> {
---
+++ gravitee-apim-portal-webui-next/src/components/pagination/pagination.component.spec.ts
@@ -0,0 +1,157 @@
+/*
+ * Copyright (C) 2025 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PaginationComponent } from './pagination.component';
+import { PaginationHarness } from './pagination.harness';
+
+describe('PaginationComponent', () => {
+  let component: PaginationComponent;
+  let fixture: ComponentFixture<PaginationComponent>;
+  let componentHarness: PaginationHarness;
+  let selectPageSpy: jest.SpyInstance;
+
+  const init = async (currentPage: number, totalResults: number) => {
+    await TestBed.configureTestingModule({
+      imports: [PaginationComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(PaginationComponent);
+
+    fixture.componentRef.setInput('currentPage', currentPage);
+    fixture.componentRef.setInput('totalResults', totalResults);
+
+    component = fixture.componentInstance;
+    componentHarness = await TestbedHarnessEnvironment.harnessForFixture(fixture, PaginationHarness);
+    selectPageSpy = jest.spyOn(component.selectPage, 'emit');
+    fixture.detectChanges();
+  };
+
+  describe('Only one page of results', () => {
+    beforeEach(async () => {
+      await init(1, 5);
+    });
+
+    it('should not allow previous page on load', async () => {
+      const previousPageButton = await componentHarness.getPreviousPageButton();
+      expect(previousPageButton).toBeTruthy();
+      expect(await previousPageButton.isDisabled()).toEqual(true);
+    });
+    it('should not allow next page when on last page', async () => {
+      const nextPageButton = await componentHarness.getNextPageButton();
+      expect(nextPageButton).toBeTruthy();
+      expect(await nextPageButton.isDisabled()).toEqual(true);
+    });
+    it('should highlight current page', async () => {
+      const currentPaginationPage = await componentHarness.getCurrentPaginationPage();
+      expect(currentPaginationPage).toBeTruthy();
+      expect(await currentPaginationPage.getText()).toEqual('1');
+    });
+  });
+
+  describe('First of many pages', () => {
+    beforeEach(async () => {
+      await init(1, 79);
+    });
+
+    it('should not allow previous page on load', async () => {
+      const previousPageButton = await componentHarness.getPreviousPageButton();
+      expect(previousPageButton).toBeTruthy();
+      expect(await previousPageButton.isDisabled()).toEqual(true);
+    });
+    it('should allow next page', async () => {
+      const nextPageButton = await componentHarness.getNextPageButton();
+      expect(nextPageButton).toBeTruthy();
+      expect(await nextPageButton.isDisabled()).toEqual(false);
+    });
+    it('should show "2" for next page', async () => {
+      const secondPageButton = await componentHarness.getPageButtonByNumber(2);
+      expect(secondPageButton).toBeTruthy();
+      expect(await secondPageButton.isDisabled()).toEqual(false);
+    });
+    it('should show "3" for page option', async () => {
+      const thirdPageButton = await componentHarness.getPageButtonByNumber(3);
+      expect(thirdPageButton).toBeTruthy();
+      expect(await thirdPageButton.isDisabled()).toEqual(false);
+    });
+
+    it('should show "8" for last page', async () => {
+      const lastPageButton = await componentHarness.getPageButtonByNumber(8);
+      expect(lastPageButton).toBeTruthy();
+      expect(await lastPageButton.isDisabled()).toEqual(false);
+    });
+
+    it('should highlight current page', async () => {
+      const currentPaginationPage = await componentHarness.getCurrentPaginationPage();
+      expect(currentPaginationPage).toBeTruthy();
+      expect(await currentPaginationPage.getText()).toEqual('1');
+    });
+    it('should go to next page via page number button', async () => {
+      const secondPageButton = await componentHarness.getPageButtonByNumber(2);
+      await secondPageButton.click();
+      fixture.detectChanges();
+    });
+    it('should go to last page', async () => {
+      const lastPageButton = await componentHarness.getPageButtonByNumber(8);
+      await lastPageButton.click();
+      expect(selectPageSpy).toHaveBeenCalledWith(8);
+    });
+  });
+
+  describe('Third page of many pages of results', () => {
+    beforeEach(async () => {
+      await init(3, 79);
+    });
+
+    it('should allow previous page', async () => {
+      const previousPageButton = await componentHarness.getPreviousPageButton();
+      expect(previousPageButton).toBeTruthy();
+      expect(await previousPageButton.isDisabled()).toEqual(false);
+    });
+    it('should allow next page', async () => {
+      const nextPageButton = await componentHarness.getNextPageButton();
+      expect(nextPageButton).toBeTruthy();
+      expect(await nextPageButton.isDisabled()).toEqual(false);
+    });
+    it('should show "1" for first page', async () => {
+      const firstPageButton = await componentHarness.getPageButtonByNumber(1);
+      expect(firstPageButton).toBeTruthy();
+      expect(await firstPageButton.isDisabled()).toEqual(false);
+    });
+    it('should show "2" for previous page', async () => {
+      const secondPageButton = await componentHarness.getPageButtonByNumber(2);
+      expect(secondPageButton).toBeTruthy();
+      expect(await secondPageButton.isDisabled()).toEqual(false);
+    });
+    it('should show "8" for last page', async () => {
+      const lastPageButton = await componentHarness.getPageButtonByNumber(8);
+      expect(lastPageButton).toBeTruthy();
+      expect(await lastPageButton.isDisabled()).toEqual(false);
+    });
+    it('should highlight current page', async () => {
+      const currentPaginationPage = await componentHarness.getCurrentPaginationPage();
+      expect(currentPaginationPage).toBeTruthy();
+      expect(await currentPaginationPage.getText()).toEqual('3');
+    });
+    it('should go to previous page via arrow', async () => {
+      const previousPageButton = await componentHarness.getPreviousPageButton();
+      await previousPageButton.click();
+
+      expect(selectPageSpy).toHaveBeenCalledWith(2);
+    });
+  });
+});
