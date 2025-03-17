11d9b3c67098f0a4bb6b339f3bd2b486cf50a7a2
jourdiw <jourdi.waller@graviteesource.com>
2025-03-17 17:33:33 +0100

    feat(portal-next): add connector service for entrypoint and endpoint calls

---
+++ gravitee-apim-portal-webui-next/src/entities/connector/connector.fixture.ts
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2025 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { isFunction } from 'rxjs/internal/util/isFunction';
+
+import { Connector, ConnectorsResponse } from './connector';
+
+export function fakeConnector(modifier?: Partial<Connector> | ((baseConnector: Connector) => Connector)) {
+  const base: Connector = {
+    id: 'mock',
+    name: 'Mock',
+  };
+
+  return isFunction(modifier) ? modifier(base) : { ...base, ...modifier };
+}
+
+export function fakeConnectorsResponse(
+  modifier?: Partial<ConnectorsResponse> | ((baseConnectorsResponse: ConnectorsResponse) => ConnectorsResponse),
+) {
+  const base: ConnectorsResponse = {
+    data: [fakeConnector()],
+  };
+
+  return isFunction(modifier) ? modifier(base) : { ...base, ...modifier };
+}
---
+++ gravitee-apim-portal-webui-next/src/services/connector.service.spec.ts
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2025 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { HttpTestingController } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+
+import { ConnectorService } from './connector.service';
+import { fakeConnectorsResponse } from '../entities/connector';
+import { AppTestingModule, TESTING_BASE_URL } from '../testing/app-testing.module';
+
+describe('ConnectorService', () => {
+  let service: ConnectorService;
+  let httpTestingController: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({ imports: [AppTestingModule] });
+    httpTestingController = TestBed.inject(HttpTestingController);
+    service = TestBed.inject(ConnectorService);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
+  });
+
+  it('should return entrypoints', done => {
+    const connectorsResponse = fakeConnectorsResponse();
+
+    service.getEntrypoints().subscribe(response => {
+      expect(response).toMatchObject(connectorsResponse);
+      done();
+    });
+
+    const req = httpTestingController.expectOne(`${TESTING_BASE_URL}/entrypoints`);
+    expect(req.request.method).toEqual('GET');
+
+    req.flush(connectorsResponse);
+  });
+
+  it('should return endpoints', done => {
+    const connectorsResponse = fakeConnectorsResponse();
+
+    service.getEndpoints().subscribe(response => {
+      expect(response).toMatchObject(connectorsResponse);
+      done();
+    });
+
+    const req = httpTestingController.expectOne(`${TESTING_BASE_URL}/endpoints`);
+    expect(req.request.method).toEqual('GET');
+
+    req.flush(connectorsResponse);
+  });
+});
