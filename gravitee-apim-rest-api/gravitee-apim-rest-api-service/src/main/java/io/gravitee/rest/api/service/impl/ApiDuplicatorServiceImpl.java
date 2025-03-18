d871cae25dd94889a5b049976c72c66fcff9588f
vikrant <vikrant.kumar@graviteesource.com>
2025-03-18 21:20:19 +0530

    fix: ignore unknown access control groups

--- gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/rest/api/service/impl/ApiDuplicatorServiceImpl.java
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/rest/api/service/impl/ApiDuplicatorServiceImpl.java
@@ -849,10 +849,9 @@ public class ApiDuplicatorServiceImpl extends AbstractService implements ApiDupl
                                     }
                                 }
                             );
-                            if (groupId != null) {
-                                accessControlEntity.setReferenceId(groupId);
-                            }
+                            accessControlEntity.setReferenceId(groupId);
                         })
+                        .filter(accessControlEntity -> accessControlEntity.getReferenceId() != null)
                         .collect(toSet())
                 );
             }
