0bbc0d8234e115d7f09a5f6abc84a0da407c0617
Florent CHAMFROY <florent.chamfroy@graviteesource.com>
2025-03-27 12:49:27 +0000

    style: apply prettier

--- gravitee-apim-definition/gravitee-apim-definition-model/src/main/java/io/gravitee/definition/model/v4/mcp/MCP.java
+++ gravitee-apim-definition/gravitee-apim-definition-model/src/main/java/io/gravitee/definition/model/v4/mcp/MCP.java
@@ -3,13 +3,13 @@ package io.gravitee.definition.model.v4.mcp;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.gravitee.definition.model.Plugin;
 import io.swagger.v3.oas.annotations.media.Schema;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -23,10 +23,9 @@ public class MCP implements Serializable {
 
     @Builder.Default
     private List<Tool> tools = new ArrayList<>();
-
     // TODO MCP: Return with correct information for plugins?
-//    @JsonIgnore
-//    public List<Plugin> getPlugins() {
-//        return List.of(new Plugin("server", "mcp-server"));
-//    }
+    //    @JsonIgnore
+    //    public List<Plugin> getPlugins() {
+    //        return List.of(new Plugin("server", "mcp-server"));
+    //    }
 }
--- gravitee-apim-definition/gravitee-apim-definition-model/src/main/java/io/gravitee/definition/model/v4/mcp/Tool.java
+++ gravitee-apim-definition/gravitee-apim-definition-model/src/main/java/io/gravitee/definition/model/v4/mcp/Tool.java
@@ -1,12 +1,12 @@
 package io.gravitee.definition.model.v4.mcp;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import java.util.List;
+import java.util.Map;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import java.util.List;
-import java.util.Map;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -14,6 +14,7 @@ import java.util.Map;
 @Data
 @Schema(name = "MCPToolV4")
 public class Tool {
+
     private String name;
     private String description;
     private InputSchema inputSchema;
@@ -24,6 +25,7 @@ public class Tool {
     @Data
     @Schema(name = "MCPToolInputSchemaV4")
     public static class InputSchema {
+
         @Builder.Default
         private String type = "object";
 
@@ -32,14 +34,13 @@ public class Tool {
         // Required property names
         private List<String> required;
     }
-
-//    @NoArgsConstructor
-//    @AllArgsConstructor
-//    @Builder(toBuilder = true)
-//    @Data
-//    @Schema(name = "MCPToolPropertyV4")
-//    public static class Property {
-//        private String type;
-//        private Map<String, Object> items;
-//    }
+    //    @NoArgsConstructor
+    //    @AllArgsConstructor
+    //    @Builder(toBuilder = true)
+    //    @Data
+    //    @Schema(name = "MCPToolPropertyV4")
+    //    public static class Property {
+    //        private String type;
+    //        private Map<String, Object> items;
+    //    }
 }
