6d279c54644653947e3c742325a36351b45dbaa9
Kamiel Ahmadpour <kamiel.ahmadpour@graviteesource.com>
2025-03-28 23:14:44 +0000

    fix: undeploy disable APIs on updates
    see: https://gravitee.atlassian.net/browse/APIM-9150
    (cherry picked from commit 45925eea6fda9cc03b7b06c51f66a7e563ecaad8)

--- gravitee-apim-gateway/gravitee-apim-gateway-handlers/gravitee-apim-gateway-handlers-api/src/main/java/io/gravitee/gateway/handlers/api/manager/impl/ApiManagerImpl.java
+++ gravitee-apim-gateway/gravitee-apim-gateway-handlers/gravitee-apim-gateway-handlers-api/src/main/java/io/gravitee/gateway/handlers/api/manager/impl/ApiManagerImpl.java
@@ -137,6 +137,10 @@ public class ApiManagerImpl implements ApiManager {
     @Override
     public ActionOnApi requiredActionFor(final ReactableApi<?> reactableApi) {
         ReactableApi<?> deployedApi = get(reactableApi.getId());
+        if (!reactableApi.enabled()) {
+            return ActionOnApi.UNDEPLOY;
+        }
+
         if (gatewayConfiguration.hasMatchingTags(reactableApi.getTags())) {
             boolean apiToDeploy = deployedApi == null;
             boolean apiToUpdate = !apiToDeploy && deployedApi.getDeployedAt().before(reactableApi.getDeployedAt());
@@ -165,7 +169,7 @@ public class ApiManagerImpl implements ApiManager {
 
     @Override
     public void refresh() {
-        if (apis != null && !apis.isEmpty()) {
+        if (!apis.isEmpty()) {
             final long begin = System.currentTimeMillis();
 
             log.info("Starting apis refresh. {} apis to be refreshed.", apis.size());
@@ -243,7 +247,7 @@ public class ApiManagerImpl implements ApiManager {
         Deployer deployer = deployers.get(api.getClass());
         List<String> plans = deployer.getPlans(api);
 
-        if (!plans.isEmpty()) {
+        if (api.isEnabled() && !plans.isEmpty()) {
             log.debug("Deploying {} plan(s) for {}:", plans.size(), api);
             for (String plan : plans) {
                 log.info("\t- {}", plan);
@@ -252,8 +256,11 @@ public class ApiManagerImpl implements ApiManager {
             apis.put(api.getId(), api);
             eventManager.publishEvent(ReactorEvent.UPDATE, api);
             log.info("{} has been updated", api);
-        } else {
+        } else if (plans.isEmpty()) {
             log.warn("There is no published plan associated to this API, undeploy it...");
+            undeploy(api.getId());
+        } else {
+            log.info("API {} is disabled, undeploy it...", api.getId());
             undeploy(api.getId());
         }
 
