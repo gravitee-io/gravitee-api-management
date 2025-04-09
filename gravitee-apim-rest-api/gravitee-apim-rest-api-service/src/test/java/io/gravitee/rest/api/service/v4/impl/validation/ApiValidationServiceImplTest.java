5ea8c3e5221924c93857df361368276a8d7e409c
Florent CHAMFROY <florent.chamfroy@graviteesource.com>
2025-04-09 18:49:12 +0200

    fix: add default group only at creation time
    https://gravitee.atlassian.net/browse/APIM-9168
    https://gravitee.atlassian.net/browse/APIM-7730
    (cherry picked from commit d8753e801ad1283b075205087cdd2161754e6b40)

--- gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/test/java/io/gravitee/rest/api/service/v4/impl/validation/ApiValidationServiceImplTest.java
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/test/java/io/gravitee/rest/api/service/v4/impl/validation/ApiValidationServiceImplTest.java
@@ -137,7 +137,8 @@ public class ApiValidationServiceImplTest {
         apiValidationService.validateAndSanitizeNewApi(GraviteeContext.getExecutionContext(), newApiEntity, primaryOwnerEntity);
 
         verify(tagsValidationService, times(1)).validateAndSanitize(GraviteeContext.getExecutionContext(), null, null);
-        verify(groupValidationService, times(1)).validateAndSanitize(GraviteeContext.getExecutionContext(), null, null, primaryOwnerEntity);
+        verify(groupValidationService, times(1))
+            .validateAndSanitize(GraviteeContext.getExecutionContext(), null, null, primaryOwnerEntity, true);
         verify(listenerValidationService, times(1)).validateAndSanitizeHttpV4(GraviteeContext.getExecutionContext(), null, null, null);
         verify(endpointGroupsValidationService, times(1)).validateAndSanitizeHttpV4(newApiEntity.getType(), null);
         verify(loggingValidationService, times(1)).validateAndSanitize(GraviteeContext.getExecutionContext(), newApiEntity.getType(), null);
@@ -159,7 +160,8 @@ public class ApiValidationServiceImplTest {
         assertNull(apiEntity.getLifecycleState());
 
         verify(tagsValidationService, times(1)).validateAndSanitize(GraviteeContext.getExecutionContext(), null, Set.of());
-        verify(groupValidationService, times(1)).validateAndSanitize(GraviteeContext.getExecutionContext(), null, null, primaryOwnerEntity);
+        verify(groupValidationService, times(1))
+            .validateAndSanitize(GraviteeContext.getExecutionContext(), null, null, primaryOwnerEntity, true);
         verify(listenerValidationService, times(1)).validateAndSanitizeHttpV4(GraviteeContext.getExecutionContext(), null, null, null);
         verify(endpointGroupsValidationService, times(1)).validateAndSanitizeHttpV4(apiEntity.getType(), null);
         verify(loggingValidationService, times(1)).validateAndSanitize(GraviteeContext.getExecutionContext(), apiEntity.getType(), null);
