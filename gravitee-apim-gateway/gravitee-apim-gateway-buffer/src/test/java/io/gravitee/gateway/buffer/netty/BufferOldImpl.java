937a83a13884912c0a5ad099b84d1982cb9c5740
Jeoffrey Haeyaert <jeoffrey.haeyaert@graviteesource.com>
2025-05-05 18:19:08 +0200

    fix: avoid stack overflow on buffer append
    https://gravitee.atlassian.net/browse/APIM-9523

--- gravitee-apim-gateway/gravitee-apim-gateway-buffer/src/test/java/io/gravitee/gateway/buffer/netty/BufferOldImpl.java
+++ gravitee-apim-gateway/gravitee-apim-gateway-buffer/src/test/java/io/gravitee/gateway/buffer/netty/BufferOldImpl.java
@@ -17,8 +17,6 @@ package io.gravitee.gateway.buffer.netty;
 
 import io.gravitee.gateway.api.buffer.Buffer;
 import io.netty.buffer.ByteBuf;
-import io.netty.buffer.ByteBufAllocator;
-import io.netty.buffer.CompositeByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.util.CharsetUtil;
 import io.reactivex.rxjava3.annotations.NonNull;
@@ -30,35 +28,35 @@ import java.util.Objects;
  * @author David BRASSELY (david at gravitee.io)
  * @author GraviteeSource Team
  */
-public class BufferImpl implements Buffer {
+public class BufferOldImpl implements Buffer {
 
     private ByteBuf buffer;
 
-    BufferImpl() {
+    BufferOldImpl() {
         this(0);
     }
 
-    BufferImpl(ByteBuf nativeBuffer) {
+    BufferOldImpl(ByteBuf nativeBuffer) {
         this.buffer = nativeBuffer;
     }
 
-    BufferImpl(int initialSizeHint) {
+    BufferOldImpl(int initialSizeHint) {
         buffer = Unpooled.unreleasableBuffer(Unpooled.buffer(initialSizeHint, Integer.MAX_VALUE));
     }
 
-    BufferImpl(byte[] bytes) {
+    BufferOldImpl(byte[] bytes) {
         buffer = Unpooled.unreleasableBuffer(Unpooled.buffer(bytes.length, Integer.MAX_VALUE)).writeBytes(bytes);
     }
 
-    BufferImpl(String str, String enc) {
+    BufferOldImpl(String str, String enc) {
         this(str.getBytes(Charset.forName(Objects.requireNonNull(enc))));
     }
 
-    BufferImpl(String str, Charset cs) {
+    BufferOldImpl(String str, Charset cs) {
         this(str.getBytes(cs));
     }
 
-    BufferImpl(String str) {
+    BufferOldImpl(String str) {
         this(str, StandardCharsets.UTF_8);
     }
 
@@ -85,20 +83,11 @@ public class BufferImpl implements Buffer {
     }
 
     private Buffer append(String str, Charset charset) {
-        return this.appendBuffer(new BufferImpl(str.getBytes(charset)));
+        return this.appendBuffer(new BufferOldImpl(str.getBytes(charset)));
     }
 
     private Buffer appendBuf(ByteBuf cb, int length) {
-        if (cb.writerIndex() > length) {
-            // Slice is needed only when appending sub part of the buffer.
-            cb = cb.slice(0, length);
-        }
-
-        if (buffer instanceof CompositeByteBuf composite && composite.maxNumComponents() == Integer.MAX_VALUE) {
-            buffer = composite.addComponent(true, cb);
-        } else {
-            buffer = new CompositeByteBuf(ByteBufAllocator.DEFAULT, false, Integer.MAX_VALUE, buffer, cb);
-        }
+        buffer = Unpooled.wrappedBuffer(buffer, cb.slice(0, length));
         return this;
     }
 
