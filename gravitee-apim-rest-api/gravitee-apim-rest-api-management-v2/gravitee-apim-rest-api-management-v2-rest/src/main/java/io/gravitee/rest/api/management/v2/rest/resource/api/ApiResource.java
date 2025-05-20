dc18c104cc694be474bb462943b196ca6562f922
vikrant <vikrant.kumar@graviteesource.com>
2025-05-20 08:21:16 +0530

    fix: prevent multiple primary owners through ownership transfer

--- gravitee-apim-rest-api/gravitee-apim-rest-api-management-v2/gravitee-apim-rest-api-management-v2-rest/src/main/java/io/gravitee/rest/api/management/v2/rest/resource/api/ApiResource.java
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-management-v2/gravitee-apim-rest-api-management-v2-rest/src/main/java/io/gravitee/rest/api/management/v2/rest/resource/api/ApiResource.java
@@ -100,6 +100,7 @@ import io.gravitee.rest.api.service.exceptions.ForbiddenAccessException;
 import io.gravitee.rest.api.service.exceptions.ForbiddenFeatureException;
 import io.gravitee.rest.api.service.exceptions.InvalidLicenseException;
 import io.gravitee.rest.api.service.exceptions.TechnicalManagementException;
+import io.gravitee.rest.api.service.exceptions.TransferOwnershipNotAllowedException;
 import io.gravitee.rest.api.service.v4.ApiDuplicateService;
 import io.gravitee.rest.api.service.v4.ApiImagesService;
 import io.gravitee.rest.api.service.v4.ApiImportExportService;
@@ -592,6 +593,7 @@ public class ApiResource extends AbstractResource {
         List<RoleEntity> newRoles = new ArrayList<>();
 
         if (apiTransferOwnership.getPoRole() != null) {
+            assertNoPrimaryOwnerReassignment(apiTransferOwnership.getPoRole());
             roleService
                 .findByScopeAndName(RoleScope.API, apiTransferOwnership.getPoRole(), GraviteeContext.getCurrentOrganization())
                 .ifPresent(newRoles::add);
@@ -990,6 +992,12 @@ public class ApiResource extends AbstractResource {
                 default:
                     break;
             }
+        }
+    }
+
+    private void assertNoPrimaryOwnerReassignment(String poRole) {
+        if ("PRIMARY_OWNER".equals(poRole)) {
+            throw new TransferOwnershipNotAllowedException(poRole);
         }
     }
 }
