15eefd616d3d005f1e4c5f800600a0530fc1e3a1
jourdiw <jourdi.waller@graviteesource.com>
2025-05-23 11:53:10 +0200

    feat(console): show mcp entrypoint info + edit

--- gravitee-apim-console-webui/src/management/api/mcp/mcp.component.spec.ts
+++ gravitee-apim-console-webui/src/management/api/mcp/mcp.component.spec.ts
@@ -17,12 +17,13 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
 import { HttpTestingController } from '@angular/common/http/testing';
 import { ActivatedRoute, Router } from '@angular/router';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { McpComponent } from './mcp.component';
 import { McpHarness } from './mcp.harness';
 
 import { CONSTANTS_TESTING, GioTestingModule } from '../../../shared/testing';
-import { ApiV4, ConnectorPlugin, fakeApiV4, fakeConnectorPlugin } from '../../../entities/management-api-v2';
+import { ApiV4, ConnectorPlugin, fakeApiV4, fakeConnectorPlugin, fakeProxyApiV4 } from '../../../entities/management-api-v2';
 import { GioTestingPermissionProvider } from '../../../shared/components/gio-permission/gio-permission.service';
 
 describe('McpComponent', () => {
@@ -35,7 +36,7 @@ describe('McpComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [GioTestingModule, McpComponent],
+      imports: [GioTestingModule, McpComponent, NoopAnimationsModule],
       providers: [
         {
           provide: ActivatedRoute,
@@ -93,8 +94,65 @@ describe('McpComponent', () => {
     });
   });
 
+  describe('when mcp entrypoint is found', () => {
+    describe('when entrypoint configuration defined + empty tools', () => {
+      const API = fakeProxyApiV4({
+        id: API_ID,
+        listeners: [
+          {
+            type: 'HTTP',
+            entrypoints: [
+              { type: 'http-proxy' },
+              {
+                type: 'mcp',
+                configuration: {
+                  mcpPath: '/cats-rule',
+                  tools: [],
+                },
+              },
+            ],
+          },
+        ],
+      });
+      beforeEach(async () => {
+        expectGetApi(API);
+      });
+      it('should display the mcp entrypoint edit form', async () => {
+        const mcpConfigurationForm = await componentHarness.getConfigureMcpEntrypoint();
+        expect(await mcpConfigurationForm.getMcpPathValue()).toEqual('/cats-rule');
+        expect(await mcpConfigurationForm.hasTools()).toEqual(false);
+      });
+      it('should edit mcp entrypoint form and submit', async () => {
+        const mcpConfigurationForm = await componentHarness.getConfigureMcpEntrypoint();
+        await mcpConfigurationForm.setMcpPathValue('/dogs-drool');
+
+        const saveBar = await componentHarness.getSaveBar();
+        expect(saveBar).toBeTruthy();
+
+        await saveBar.clickSubmit();
+
+        const newApi = { ...API };
+        const httpListener = newApi.listeners[0];
+        httpListener.entrypoints = [...httpListener.entrypoints, { type: 'mcp', configuration: { mcpPath: '/dogs-drool', tools: [] } }];
+        newApi.listeners[0] = httpListener;
+
+        expectUpdateApiCalls(API, newApi);
+      });
+    });
+  });
+
   function expectGetApi(api: ApiV4): void {
     httpTestingController.expectOne({ url: `${CONSTANTS_TESTING.env.v2BaseURL}/apis/${api.id}`, method: 'GET' }).flush(api);
+    fixture.detectChanges();
+  }
+
+  function expectUpdateApiCalls(oldApi: ApiV4, newApi: ApiV4): void {
+    expectGetApi(oldApi);
+
+    const req = httpTestingController.expectOne({ url: `${CONSTANTS_TESTING.env.v2BaseURL}/apis/${newApi.id}`, method: 'PUT' });
+    expect(req.request.body).toEqual(newApi);
+    req.flush(newApi);
+
     fixture.detectChanges();
   }
 
