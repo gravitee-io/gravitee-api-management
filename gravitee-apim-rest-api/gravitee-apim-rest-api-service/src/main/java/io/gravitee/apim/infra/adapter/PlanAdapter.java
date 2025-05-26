1ab9f5b0a8bd07bab1f4f7bdc94a8086e2a0e459
aqibmohammadkhan <aqib.khan@graviteesource.com>
2025-05-26 19:47:33 +0530

    fix:Alert trigger issue due to v4plancasting issue

--- gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/apim/infra/adapter/PlanAdapter.java
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/apim/infra/adapter/PlanAdapter.java
@@ -24,6 +24,7 @@ import io.gravitee.definition.model.federation.FederatedPlan;
 import io.gravitee.definition.model.v4.plan.PlanMode;
 import io.gravitee.definition.model.v4.plan.PlanSecurity;
 import io.gravitee.definition.model.v4.plan.PlanStatus;
+import io.gravitee.rest.api.model.v4.plan.GenericPlanEntity;
 import io.gravitee.rest.api.model.v4.plan.NewPlanEntity;
 import io.gravitee.rest.api.model.v4.plan.PlanEntity;
 import io.gravitee.rest.api.model.v4.plan.PlanSecurityType;
@@ -86,6 +87,7 @@ public interface PlanAdapter {
     PlanCRD toCRD(Plan source);
 
     PlanEntity toEntityV4(PlanCRD source);
+
     io.gravitee.definition.model.v4.plan.Plan toApiDefinition(PlanCRD source);
 
     @Mapping(target = "security", expression = "java(computeBasePlanEntitySecurityV4(source))")
@@ -258,4 +260,19 @@ public interface PlanAdapter {
         }
         return null;
     }
+
+    default io.gravitee.rest.api.model.PlanEntity map(GenericPlanEntity entity) {
+        if (entity == null) {
+            return null;
+        }
+        if (entity instanceof io.gravitee.rest.api.model.PlanEntity p) {
+            return p;
+        } else if (entity instanceof io.gravitee.rest.api.model.v4.plan.PlanEntity v4) {
+            return map(v4);
+        } else {
+            return null;
+        }
+    }
+
+    io.gravitee.rest.api.model.PlanEntity map(io.gravitee.rest.api.model.v4.plan.PlanEntity v4);
 }
