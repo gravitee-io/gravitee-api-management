126aacad086903a3aa8c3a6fea7594080346aa98
jourdiw <jourdi.waller@graviteesource.com>
2025-06-18 15:55:41 +0200

    feat(portal-next): add hamburger menu for mobile

--- gravitee-apim-portal-webui-next/src/components/nav-bar/nav-bar.component.ts
+++ gravitee-apim-portal-webui-next/src/components/nav-bar/nav-bar.component.ts
@@ -13,28 +13,64 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { Component, Input, input, InputSignal } from '@angular/core';
+import { Component, Input, input, InputSignal, OnInit, OnDestroy } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
 import { isEmpty } from 'lodash';
+import { Subscription } from 'rxjs';
 
 import { NavBarButtonComponent } from './nav-bar-button/nav-bar-button.component';
 import { User } from '../../entities/user/user';
 import { PortalMenuLink } from '../../services/portal-menu-links.service';
+import { ResponsiveService } from '../../services/responsive.service';
 import { CompanyTitleComponent } from '../company-title/company-title.component';
 import { UserAvatarComponent } from '../user-avatar/user-avatar.component';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [MatButtonModule, CompanyTitleComponent, NavBarButtonComponent, UserAvatarComponent],
+  imports: [MatButtonModule, MatIconModule, RouterModule, CompanyTitleComponent, NavBarButtonComponent, UserAvatarComponent],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss',
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit, OnDestroy {
   @Input()
   siteTitle!: string;
 
   customLinks: InputSignal<PortalMenuLink[]> = input<PortalMenuLink[]>([]);
   currentUser: InputSignal<User> = input({});
   logo: InputSignal<string> = input('');
+
+  // Mobile menu state
+  isMobileMenuOpen = false;
+  isMobile = false;
+
   protected readonly isEmpty = isEmpty;
+
+  private subscription?: Subscription;
+
+  constructor(private responsiveService: ResponsiveService) {}
+
+  ngOnInit(): void {
+    this.subscription = this.responsiveService.screenSize$.subscribe(screenInfo => {
+      this.isMobile = screenInfo.isMobile;
+
+      // Close mobile menu when switching to desktop
+      if (!this.isMobile && this.isMobileMenuOpen) {
+        this.isMobileMenuOpen = false;
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
+  }
+
+  toggleMobileMenu(): void {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+  }
+
+  closeMobileMenu(): void {
+    this.isMobileMenuOpen = false;
+  }
 }
