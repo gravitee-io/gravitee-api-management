c99a550e929fa266cb50de16bdf285d0c1ea9508
aqibmohammadkhan <aqib.khan@graviteesource.com>
2025-06-23 12:06:48 +0530

    fix: pagination on portal UI subscription tab of applicaiton
    (cherry picked from commit 21424ed96daf49452f1d9a437e08cafd183e2686)
    
    # Conflicts:
    #	gravitee-apim-rest-api/gravitee-apim-rest-api-portal/gravitee-apim-rest-api-portal-rest/src/test/java/io/gravitee/rest/api/portal/rest/resource/SubscriptionResourceTest.java

--- gravitee-apim-rest-api/gravitee-apim-rest-api-portal/gravitee-apim-rest-api-portal-rest/src/test/java/io/gravitee/rest/api/portal/rest/resource/SubscriptionResourceTest.java
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-portal/gravitee-apim-rest-api-portal-rest/src/test/java/io/gravitee/rest/api/portal/rest/resource/SubscriptionResourceTest.java
@@ -16,20 +16,42 @@
 package io.gravitee.rest.api.portal.rest.resource;
 
 import static jakarta.ws.rs.client.Entity.json;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.eq;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
 
 import io.gravitee.common.http.HttpStatusCode;
-import io.gravitee.rest.api.model.*;
+import io.gravitee.rest.api.model.ApiKeyEntity;
+import io.gravitee.rest.api.model.SubscriptionConfigurationEntity;
+import io.gravitee.rest.api.model.SubscriptionConsumerStatus;
+import io.gravitee.rest.api.model.SubscriptionEntity;
+import io.gravitee.rest.api.model.SubscriptionStatus;
+import io.gravitee.rest.api.model.UpdateSubscriptionConfigurationEntity;
+import io.gravitee.rest.api.model.pagedresult.Metadata;
 import io.gravitee.rest.api.model.permissions.RolePermission;
 import io.gravitee.rest.api.model.permissions.RolePermissionAction;
-import io.gravitee.rest.api.portal.rest.model.*;
 import io.gravitee.rest.api.portal.rest.model.Error;
+import io.gravitee.rest.api.portal.rest.model.ErrorResponse;
+import io.gravitee.rest.api.portal.rest.model.Key;
+import io.gravitee.rest.api.portal.rest.model.Subscription;
+import io.gravitee.rest.api.portal.rest.model.SubscriptionConfigurationInput;
+import io.gravitee.rest.api.portal.rest.model.UpdateSubscriptionInput;
 import io.gravitee.rest.api.service.common.GraviteeContext;
 import io.gravitee.rest.api.service.exceptions.SubscriptionNotFoundException;
 import jakarta.ws.rs.core.Response;
+import java.util.Collections;
 import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -406,6 +428,145 @@ class SubscriptionResourceTest extends AbstractResourceTest {
 
             verify(subscriptionService, times(1)).resumeFailed(GraviteeContext.getExecutionContext(), subscriptionEntity.getId());
             assertEquals(200, response.getStatus());
+        }
+
+        @Test
+        public void shouldUpdateSubscriptionConfiguration() {
+            UpdateSubscriptionInput updateSubscriptionInput = new UpdateSubscriptionInput();
+            SubscriptionConfigurationInput subscriptionConfigurationInput = new SubscriptionConfigurationInput();
+            subscriptionConfigurationInput.setEntrypointConfiguration("{\"url\":\"my-url\"}");
+            updateSubscriptionInput.setConfiguration(subscriptionConfigurationInput);
+
+            SubscriptionEntity subscriptionEntity = new SubscriptionEntity();
+            SubscriptionConfigurationEntity subscriptionConfigurationEntity = new SubscriptionConfigurationEntity();
+            subscriptionConfigurationEntity.setEntrypointConfiguration("{\"url\":\"my-url\"}");
+            subscriptionEntity.setConfiguration(subscriptionConfigurationEntity);
+            when(subscriptionService.update(eq(GraviteeContext.getExecutionContext()), any(UpdateSubscriptionConfigurationEntity.class)))
+                .thenReturn(subscriptionEntity);
+
+            Response response = target(SUBSCRIPTION).request().put(json(updateSubscriptionInput));
+
+            assertEquals(200, response.getStatus());
+
+            SubscriptionEntity subscriptionEntityResponse = response.readEntity(SubscriptionEntity.class);
+            assertEquals("{\"url\":\"my-url\"}", subscriptionEntityResponse.getConfiguration().getEntrypointConfiguration());
+
+            ArgumentCaptor<UpdateSubscriptionConfigurationEntity> subscriptionCaptor = ArgumentCaptor.forClass(
+                UpdateSubscriptionConfigurationEntity.class
+            );
+            verify(subscriptionService).update(eq(GraviteeContext.getExecutionContext()), subscriptionCaptor.capture());
+            assertEquals(SUBSCRIPTION, subscriptionCaptor.getValue().getSubscriptionId());
+        }
+
+        @Test
+        public void shouldPauseSubscriptionByConsumer() {
+            Response response = target(SUBSCRIPTION).path("_changeConsumerStatus").queryParam("status", "STOPPED").request().post(null);
+
+            assertEquals(200, response.getStatus());
+            verify(subscriptionService, times(1)).pauseConsumer(eq(GraviteeContext.getExecutionContext()), eq(subscriptionEntity.getId()));
+        }
+
+        @Test
+        public void shouldResumeSubscriptionByConsumer() {
+            Response response = target(SUBSCRIPTION).path("_changeConsumerStatus").queryParam("status", "STARTED").request().post(null);
+
+            assertEquals(200, response.getStatus());
+            verify(subscriptionService, times(1)).resumeConsumer(eq(GraviteeContext.getExecutionContext()), eq(subscriptionEntity.getId()));
+        }
+
+        @Test
+        public void shouldHaveBadRequestIfTryingAWrongConsumerStatus() {
+            Response response = target(SUBSCRIPTION).path("_changeConsumerStatus").queryParam("status", "INVALID").request().post(null);
+
+            assertEquals(400, response.getStatus());
+            verify(subscriptionService, times(0)).pauseConsumer(eq(GraviteeContext.getExecutionContext()), eq(subscriptionEntity.getId()));
+            verify(subscriptionService, times(0)).resumeConsumer(eq(GraviteeContext.getExecutionContext()), eq(subscriptionEntity.getId()));
+        }
+
+        @Test
+        public void shouldBeForbiddenWhenUpdatingConsumerStatus() {
+            reset(permissionService);
+
+            doReturn(false)
+                .when(permissionService)
+                .hasPermission(
+                    eq(GraviteeContext.getExecutionContext()),
+                    eq(RolePermission.APPLICATION_SUBSCRIPTION),
+                    eq(APPLICATION),
+                    eq(RolePermissionAction.UPDATE)
+                );
+            Response response = target(SUBSCRIPTION).path("_changeConsumerStatus").queryParam("status", "STOPPED").request().post(null);
+
+            assertEquals(HttpStatusCode.FORBIDDEN_403, response.getStatus());
+        }
+
+        @Test
+        public void shouldReturnEmptyWhenNoAppsFound() {
+            when(applicationService.findByUser(any(), any())).thenReturn(Collections.emptySet());
+
+            final Response response = target().queryParam("apiId", API).request().get();
+
+            assertEquals(HttpStatusCode.OK_200, response.getStatus());
+        }
+
+        @Test
+        public void shouldThrowForbiddenWhenNoPermission() {
+            doReturn(false).when(permissionService).hasPermission(any(), any(), any(), any());
+
+            final Response response = target().queryParam("apiId", API).queryParam("applicationId", APPLICATION).request().get();
+
+            assertEquals(HttpStatusCode.FORBIDDEN_403, response.getStatus());
+        }
+
+        @Test
+        public void shouldReturnEmptyWhenNoSubscriptions() {
+            doReturn(true).when(permissionService).hasPermission(any(), any(), any(), any());
+            doReturn(Collections.emptyList()).when(subscriptionService).search(any(), any());
+
+            final Response response = target().queryParam("apiId", API).queryParam("applicationId", APPLICATION).request().get();
+
+            assertEquals(HttpStatusCode.OK_200, response.getStatus());
+        }
+
+        @Test
+        public void shouldReturnMappedSubscriptions() {
+            doReturn(true).when(permissionService).hasPermission(any(), any(), any(), any());
+
+            SubscriptionEntity subscription = new SubscriptionEntity();
+            subscription.setId("sub-id");
+            subscription.setStatus(SubscriptionStatus.ACCEPTED);
+            doReturn(Collections.singletonList(subscription)).when(subscriptionService).search(any(), any());
+            doReturn(new Metadata()).when(subscriptionService).getMetadata(any(), any());
+
+            final Response response = target().queryParam("apiId", API).queryParam("applicationId", APPLICATION).request().get();
+
+            assertEquals(HttpStatusCode.OK_200, response.getStatus());
+        }
+
+        @Test
+        public void shouldReturnAllSubscriptionsWhenPaginationIsDisabled() {
+            doReturn(true).when(permissionService).hasPermission(any(), any(), any(), any());
+            SubscriptionEntity subscription = new SubscriptionEntity();
+            subscription.setId("sub-id");
+            subscription.setStatus(SubscriptionStatus.ACCEPTED);
+            doReturn(Collections.singletonList(subscription)).when(subscriptionService).search(any(), any());
+            doReturn(new Metadata()).when(subscriptionService).getMetadata(any(), any());
+
+            final Response response = target()
+                .queryParam("apiId", API)
+                .queryParam("applicationId", APPLICATION)
+                .queryParam("size", -1)
+                .request()
+                .get();
+
+            assertEquals(HttpStatusCode.OK_200, response.getStatus());
+        }
+
+        @Getter
+        @AllArgsConstructor
+        private class SubscriptionConfiguration {
+
+            private String url;
         }
     }
 }
