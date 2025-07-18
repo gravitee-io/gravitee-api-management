2d4b71e03addb71eb0797167792fa38dfc6adfe0
Michel Barret <michel.barret@graviteesource.com>
2025-07-18 11:32:28 +0200

    refactor: rollback operator
    https://gravitee.atlassian.net/browse/APIM-10221

--- gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/apim/core/api/model/mapper/V4toV2RollbackOperator.java
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/apim/core/api/model/mapper/V4toV2RollbackOperator.java
@@ -15,98 +15,35 @@
  */
 package io.gravitee.apim.core.api.model.mapper;
 
-import static io.gravitee.apim.core.utils.CollectionUtils.stream;
-
 import io.gravitee.apim.core.api.model.Api;
 import io.gravitee.apim.core.plan.model.Plan;
 import io.gravitee.common.utils.TimeProvider;
 import io.gravitee.definition.model.DefinitionVersion;
-import io.gravitee.definition.model.plugins.resources.Resource;
 import io.gravitee.definition.model.v4.ApiType;
-import java.util.Date;
-import java.util.List;
 
 public class V4toV2RollbackOperator {
 
     public Api rollback(Api source, io.gravitee.definition.model.Api apiDefinition) {
-        var definition = apiDefinition
+        return source
             .toBuilder()
-            .tags(source.getApiDefinitionHttpV4().getTags())
-            .resources(mapResources(source.getApiDefinitionHttpV4().getResources()))
-            .responseTemplates(source.getApiDefinitionHttpV4().getResponseTemplates())
+            .name(apiDefinition.getName())
+            .version(apiDefinition.getVersion())
+            .definitionVersion(DefinitionVersion.V2)
+            .updatedAt(TimeProvider.now())
+            .type(ApiType.PROXY)
+            .apiDefinition(apiDefinition.toBuilder().plans(null).build())
+            .apiDefinitionHttpV4(null)
             .build();
-        return new Api(
-            source.getId(),
-            source.getEnvironmentId(),
-            source.getCrossId(),
-            source.getHrid(),
-            apiDefinition.getName(),
-            source.getDescription(),
-            apiDefinition.getVersion(),
-            source.getOriginContext(),
-            DefinitionVersion.V2,
-            null,
-            null,
-            definition,
-            null,
-            null,
-            ApiType.PROXY,
-            source.getDeployedAt(),
-            source.getCreatedAt(),
-            TimeProvider.now(),
-            source.getVisibility(),
-            source.getLifecycleState(),
-            source.getPicture(),
-            source.getGroups(),
-            source.getCategories(),
-            source.getLabels(),
-            source.isDisableMembershipNotifications(),
-            source.getApiLifecycleState(),
-            source.getBackground()
-        );
     }
 
     public Plan rollback(Plan source, io.gravitee.definition.model.Plan planDefinition) {
-        return new Plan(
-            source.getId(),
-            DefinitionVersion.V2,
-            source.getCrossId(),
-            planDefinition.getName(),
-            source.getDescription(),
-            source.getCreatedAt(),
-            TimeProvider.now(),
-            source.getPublishedAt(),
-            source.getClosedAt(),
-            Date.from(TimeProvider.instantNow()),
-            source.getValidation(),
-            source.getType(),
-            source.getApiId(),
-            source.getEnvironmentId(),
-            source.getOrder(),
-            source.getCharacteristics(),
-            source.getExcludedGroups(),
-            source.isCommentRequired(),
-            source.getCommentMessage(),
-            source.getGeneralConditions(),
-            null,
-            null,
-            planDefinition,
-            null,
-            source.getApiType()
-        );
-    }
-
-    private List<Resource> mapResources(List<io.gravitee.definition.model.v4.resource.Resource> resources) {
-        return stream(resources).map(this::mapResource).toList();
-    }
-
-    private Resource mapResource(io.gravitee.definition.model.v4.resource.Resource resource) {
-        return Resource
-            .builder()
-            .enabled(resource.isEnabled())
-            .name(resource.getName())
-            .type(resource.getType())
-            .configuration(resource.getConfiguration())
+        return source
+            .toBuilder()
+            .planDefinitionV2(planDefinition)
+            .definitionVersion(io.gravitee.definition.model.DefinitionVersion.V2)
+            .updatedAt(TimeProvider.now())
+            .planDefinitionHttpV4(null)
+            .name(planDefinition.getName())
             .build();
     }
 }
