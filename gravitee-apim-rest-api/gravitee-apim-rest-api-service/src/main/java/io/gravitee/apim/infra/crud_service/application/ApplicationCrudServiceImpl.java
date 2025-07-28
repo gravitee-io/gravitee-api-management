bedeed6a2a94d2630c3546aafec28c8c42ee04fb
samirtechlab <samir.tech.lab@gmail.com>
2025-07-28 14:24:13 +0200

    chore(logs): Fixed log messages

--- gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/apim/infra/crud_service/application/ApplicationCrudServiceImpl.java
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/apim/infra/crud_service/application/ApplicationCrudServiceImpl.java
@@ -59,7 +59,7 @@ public class ApplicationCrudServiceImpl implements ApplicationCrudService {
                 .map(ApplicationAdapter.INSTANCE::toEntity)
                 .orElseThrow(() -> new ApplicationNotFoundException(applicationId));
         } catch (TechnicalException ex) {
-            throw new TechnicalManagementException("An error occurs while trying to find an application using its ID " + applicationId, ex);
+            throw new TechnicalManagementException(String.format("An error occurs while trying to find an application using its ID %s", applicationId), ex);
         }
     }
 }
