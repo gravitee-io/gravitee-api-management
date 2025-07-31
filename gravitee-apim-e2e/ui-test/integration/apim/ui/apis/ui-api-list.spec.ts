aac13fcf526ec21d4fa0106a4cbb7a1ab7ac8fb1
ThibaudAv <thibaud.avenier@gmail.com>
2025-07-31 11:36:09 +0200

    fix(e2e): fix sdk enum for sort params

--- gravitee-apim-e2e/ui-test/integration/apim/ui/apis/ui-api-list.spec.ts
+++ gravitee-apim-e2e/ui-test/integration/apim/ui/apis/ui-api-list.spec.ts
@@ -47,10 +47,10 @@ describe('API List feature', { defaultCommandTimeout: 10000 }, () => {
     it('should have correct table header', function () {
       cy.getByDataTestId('api_list_table_header')
         .should('contain.text', 'Name')
-        .and('contain.text', 'Definition')
+        .and('contain.text', 'API Type')
         .and('contain.text', 'Status')
         .and('contain.text', 'Access')
-        .and('contain.text', 'Tags')
+        .and('contain.text', 'Sharding Tags')
         .and('contain.text', 'Owner')
         .and('contain.text', 'Visibility');
     });
