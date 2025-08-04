876c86f139f30abf0951e2c46b7ad26481b7b352
jourdiw <jourdi.waller@graviteesource.com>
2025-08-04 11:29:15 +0200

    chore(console): remove unused components after api analytics refactor

--- gravitee-apim-console-webui/src/management/api/api-traffic-v4/analytics/api-analytics-proxy/api-analytics-proxy.component.harness.ts
+++ gravitee-apim-console-webui/src/management/api/api-traffic-v4/analytics/api-analytics-proxy/api-analytics-proxy.component.harness.ts
@@ -15,33 +15,16 @@
  */
 import { ComponentHarness } from '@angular/cdk/testing';
 
-import { ApiAnalyticsRequestStatsHarness } from '../components/api-analytics-requests-stats/api-analytics-request-stats.component.harness';
 import { ApiAnalyticsFiltersBarHarness } from '../components/api-analytics-filters-bar/api-analytics-filters-bar.component.harness';
-import { ApiAnalyticsResponseStatusRangesHarness } from '../../../../../shared/components/api-analytics-response-status-ranges/api-analytics-response-status-ranges.component.harness';
 
 export class ApiAnalyticsProxyHarness extends ComponentHarness {
   static hostSelector = 'api-analytics-proxy';
 
   protected emptyPanelHarness = this.locatorForOptional('gio-card-empty-state');
-  protected loaderElement = this.locatorForOptional('.loader gio-loader');
-  protected requestStats = (title: string) => this.locatorForOptional(ApiAnalyticsRequestStatsHarness.with({ title }));
-  protected responseStatusRanges = (title: string) => this.locatorForOptional(ApiAnalyticsResponseStatusRangesHarness.with({ title }));
 
   getFiltersBarHarness = this.locatorForOptional(ApiAnalyticsFiltersBarHarness);
 
   async isEmptyPanelDisplayed(): Promise<boolean> {
     return (await this.emptyPanelHarness()) !== null;
-  }
-
-  async isLoaderDisplayed(): Promise<boolean> {
-    return (await this.loaderElement()) !== null;
-  }
-
-  async getRequestStatsHarness(title: string): Promise<ApiAnalyticsRequestStatsHarness> {
-    return this.requestStats(title)();
-  }
-
-  async getResponseStatusRangesHarness(title: string): Promise<ApiAnalyticsResponseStatusRangesHarness> {
-    return this.responseStatusRanges(title)();
   }
 }
