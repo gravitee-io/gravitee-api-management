5e2245c24d115cb5addda17b2b8657c4684aaea0
vikrant <vikrant.kumar@graviteesource.com>
2025-08-07 16:38:48 +0530

    fix: reindex API after ownership transfer to prevent stale search results

--- gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/test/java/io/gravitee/rest/api/service/impl/MembershipService_GetMembersTest.java
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/test/java/io/gravitee/rest/api/service/impl/MembershipService_GetMembersTest.java
@@ -86,6 +86,8 @@ public class MembershipService_GetMembersTest {
                 null,
                 null,
                 null,
+                null,
+                null,
                 null
             );
     }
