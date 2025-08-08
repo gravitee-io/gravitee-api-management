61f55c67b0c3f26231584406d582e87c1f8e1652
mukultyagi <mukul.tyagi@graviteesource.com>
2025-08-08 17:09:37 +0530

    feat: add empty page for native api analytics

---
+++ gravitee-apim-console-webui/src/management/api/api-traffic-v4/analytics/api-analytics-native/api-analytics-native.component.ts
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Component } from '@angular/core';
+import { GioCardEmptyStateModule, GioLoaderModule } from '@gravitee/ui-particles-angular';
+import { MatButton } from '@angular/material/button';
+import { MatCard, MatCardContent, MatCardHeader, MatCardSubtitle, MatCardTitle } from '@angular/material/card';
+
+@Component({
+  selector: 'api-analytics-native',
+  imports: [GioCardEmptyStateModule, MatButton, MatCard, MatCardContent, MatCardHeader, MatCardSubtitle, MatCardTitle, GioLoaderModule],
+  templateUrl: './api-analytics-native.component.html',
+  styleUrl: './api-analytics-native.component.scss',
+})
+export class ApiAnalyticsNativeComponent {
+  isEmptyNativeAnalytics = true;
+}
