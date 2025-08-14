855e8c45f9cf26618be8c7b4905c4d0d17760702
ThibaudAv <thibaud.avenier@gmail.com>
2025-08-14 16:25:28 +0200

    test(e2e): ensure api and app user are properly initialized before using them in tests
    (cherry picked from commit 0a1e81ab0927bc53d7d06eec3b7ceeab7c813c41)

--- gravitee-apim-e2e/api-test/src/apis/settings/mapi-v1/notification-setting.spec.ts
+++ gravitee-apim-e2e/api-test/src/apis/settings/mapi-v1/notification-setting.spec.ts
@@ -33,7 +33,7 @@ describe('Notification settings tests', () => {
 
   describe('Create', () => {
     it('should create a notification setting', async () => {
-      const users = await managementUserResourceAsAdminUser.getAllUsers({ orgId, envId });
+      const users = await managementUserResourceAsAdminUser.getAllUsers({ orgId, envId, size: 200 });
       apiUser = users.data.find((user) => user.displayName === API_USER.username);
 
       const userOrganizationRole = await succeed(
