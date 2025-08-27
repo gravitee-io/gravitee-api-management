f5a7731829f481dbb3f99a37ad18002b3332c6d7
Jedrzej Janasiak <jedrzej.janasiak@graviteesource.com>
2025-08-27 16:23:46 +0200

    feat: revert aqua integration removal

--- .circleci/ci/src/jobs/job-build-backend-images.ts
+++ .circleci/ci/src/jobs/job-build-backend-images.ts
@@ -16,9 +16,10 @@
 import { commands, Config, Job, reusable } from '@circleci/circleci-config-sdk';
 import { OpenJdkExecutor } from '../executors';
 import { Command } from '@circleci/circleci-config-sdk/dist/src/lib/Components/Commands/exports/Command';
-import { computeImagesTag } from '../utils';
+import { computeImagesTag, isSupportBranchOrMaster } from '../utils';
 import { CircleCIEnvironment } from '../pipelines';
 import { CreateDockerContextCommand, DockerAzureLoginCommand, DockerAzureLogoutCommand } from '../commands';
+import { orbs } from '../orbs';
 import { config } from '../config';
 
 export class BuildBackendImagesJob {
@@ -51,6 +52,53 @@ docker buildx build --push --platform=linux/arm64,linux/amd64 -f gravitee-apim-g
 gateway-docker-context`,
       }),
     ];
+
+    if (isSupportBranchOrMaster(environment.branch)) {
+      dynamicConfig.importOrb(orbs.keeper).importOrb(orbs.aquasec);
+
+      steps.push(
+        new reusable.ReusedCommand(orbs.keeper.commands['env-export'], {
+          'secret-url': config.secrets.aquaKey,
+          'var-name': 'AQUA_KEY',
+        }),
+        new reusable.ReusedCommand(orbs.keeper.commands['env-export'], {
+          'secret-url': config.secrets.aquaSecret,
+          'var-name': 'AQUA_SECRET',
+        }),
+        new reusable.ReusedCommand(orbs.keeper.commands['env-export'], {
+          'secret-url': config.secrets.aquaRegistryUsername,
+          'var-name': 'AQUA_USERNAME',
+        }),
+        new reusable.ReusedCommand(orbs.keeper.commands['env-export'], {
+          'secret-url': config.secrets.aquaRegistryPassword,
+          'var-name': 'AQUA_PASSWORD',
+        }),
+        new reusable.ReusedCommand(orbs.keeper.commands['env-export'], {
+          'secret-url': config.secrets.aquaScannerKey,
+          'var-name': 'SCANNER_TOKEN',
+        }),
+        new reusable.ReusedCommand(orbs.keeper.commands['env-export'], {
+          'secret-url': config.secrets.githubApiToken,
+          'var-name': 'GITHUB_TOKEN',
+        }),
+        new reusable.ReusedCommand(orbs.aquasec.commands['install_billy']),
+        new reusable.ReusedCommand(orbs.aquasec.commands['pull_aqua_scanner_image']),
+        new reusable.ReusedCommand(orbs.aquasec.commands['register_artifact'], {
+          artifact_to_register: `graviteeio.azurecr.io/apim-management-api:${tag}`,
+        }),
+        new reusable.ReusedCommand(orbs.aquasec.commands['register_artifact'], {
+          artifact_to_register: `graviteeio.azurecr.io/apim-gateway:${tag}`,
+        }),
+        new reusable.ReusedCommand(orbs.aquasec.commands['scan_docker_image'], {
+          docker_image_to_scan: `graviteeio.azurecr.io/apim-management-api:${tag}`,
+          scanner_url: config.aqua.scannerUrl,
+        }),
+        new reusable.ReusedCommand(orbs.aquasec.commands['scan_docker_image'], {
+          docker_image_to_scan: `graviteeio.azurecr.io/apim-gateway:${tag}`,
+          scanner_url: config.aqua.scannerUrl,
+        }),
+      );
+    }
 
     steps.push(new reusable.ReusedCommand(DockerAzureLogoutCommand.get()));
 
