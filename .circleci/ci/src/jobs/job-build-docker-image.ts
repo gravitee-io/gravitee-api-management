4790a188a79ad5cadd610931b8dc2268a9814c44
Jedrzej Janasiak <jedrzej.janasiak@graviteesource.com>
2025-08-27 10:29:00 +0200

    ci: revert aqua integration removal
    (cherry picked from commit b8704ab4cdef34d7f9013819330a5b46d9370cc0)
    
    # Conflicts:
    #	.circleci/ci/src/jobs/job-build-docker-image.ts
    #	.circleci/ci/src/pipelines/tests/resources/build-docker-images/build-docker-images-prerelease-dry-run.yml
    #	.circleci/ci/src/pipelines/tests/resources/build-docker-images/build-docker-images-prerelease-no-dry-run.yml
    #	.circleci/ci/src/pipelines/tests/resources/build-docker-images/build-docker-images-release-dry-run.yml
    #	.circleci/ci/src/pipelines/tests/resources/build-docker-images/build-docker-images-release-no-dry-run-as-latest.yml
    #	.circleci/ci/src/pipelines/tests/resources/build-docker-images/build-docker-images-release-no-dry-run.yml
    #	.circleci/ci/src/pipelines/tests/resources/full-release/release-4-2-0-alpha.yml
    #	.circleci/ci/src/pipelines/tests/resources/full-release/release-4-2-0-dry-run.yml
    #	.circleci/ci/src/pipelines/tests/resources/full-release/release-4-2-0-latest.yml
    #	.circleci/ci/src/pipelines/tests/resources/full-release/release-4-2-0-no-dry-run.yml
    #	.circleci/ci/src/pipelines/tests/resources/publish-docker-images/publish-docker-images-4-1-x.yml
    #	.circleci/ci/src/pipelines/tests/resources/publish-docker-images/publish-docker-images-dry-run.yml
    #	.circleci/ci/src/pipelines/tests/resources/publish-docker-images/publish-docker-images-master.yml
    #	.circleci/ci/src/pipelines/tests/resources/pull-requests/pull-requests-4-1-x.yml
    #	.circleci/ci/src/pipelines/tests/resources/pull-requests/pull-requests-custom-branch-backend-distribution-only.yml
    #	.circleci/ci/src/pipelines/tests/resources/pull-requests/pull-requests-custom-branch-backend-gateway-only.yml
    #	.circleci/ci/src/pipelines/tests/resources/pull-requests/pull-requests-custom-branch-backend-integration-tests-only.yml
    #	.circleci/ci/src/pipelines/tests/resources/pull-requests/pull-requests-custom-branch-backend-only.yml
    #	.circleci/ci/src/pipelines/tests/resources/pull-requests/pull-requests-custom-branch-backend-plugin-only.yml
    #	.circleci/ci/src/pipelines/tests/resources/pull-requests/pull-requests-custom-branch-backend-rest-api-only.yml
    #	.circleci/ci/src/pipelines/tests/resources/pull-requests/pull-requests-custom-branch-console-only.yml
    #	.circleci/ci/src/pipelines/tests/resources/pull-requests/pull-requests-custom-branch-helm-only.yml
    #	.circleci/ci/src/pipelines/tests/resources/pull-requests/pull-requests-custom-branch-portal-only.yml
    #	.circleci/ci/src/pipelines/tests/resources/pull-requests/pull-requests-custom-branch.yml
    #	.circleci/ci/src/pipelines/tests/resources/pull-requests/pull-requests-master.yml
    #	.circleci/ci/src/pipelines/tests/resources/pull-requests/pull-requests-mergify.yml
    #	.circleci/ci/src/pipelines/tests/resources/pull-requests/pull-requests-run-e2e.yml
    #	.circleci/ci/src/pipelines/tests/resources/run-e2e-tests/run-e2e-tests.yml
    #	.circleci/ci/src/workflows/workflow-pull-requests.ts

--- .circleci/ci/src/jobs/job-build-docker-image.ts
+++ .circleci/ci/src/jobs/job-build-docker-image.ts
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 import { commands, Config, parameters, reusable } from '@circleci/circleci-config-sdk';
-import { computeImagesTag, GraviteeioVersion, isBlank, parse } from '../utils';
+import { computeImagesTag, GraviteeioVersion, isBlank, isSupportBranchOrMaster, parse } from '../utils';
 import { CircleCIEnvironment } from '../pipelines';
 import { DockerLoginCommand, DockerLogoutCommand, CreateDockerContextCommand } from '../commands';
 import { Command } from '@circleci/circleci-config-sdk/dist/src/lib/Components/Commands/exports/Command';
@@ -31,6 +31,11 @@ export class BuildDockerImageJob {
   ]);
 
   public static create(dynamicConfig: Config, environment: CircleCIEnvironment, isProd: boolean): reusable.ParameterizedJob {
+<<<<<<< HEAD
+=======
+    dynamicConfig.importOrb(orbs.keeper).importOrb(orbs.aquasec);
+
+>>>>>>> b8704ab4cd (ci: revert aqua integration removal)
     const createDockerContextCommand = CreateDockerContextCommand.get();
     dynamicConfig.addReusableCommand(createDockerContextCommand);
 
@@ -62,8 +67,62 @@ export class BuildDockerImageJob {
     return new reusable.ParameterizedJob(
       BuildDockerImageJob.jobName,
       BaseExecutor.create(),
+<<<<<<< HEAD
       BuildDockerImageJob.customParametersList,
       steps,
+=======
+      BuildDockerWebUiImageJob.customParametersList,
+      [
+        new commands.Checkout(),
+        new commands.workspace.Attach({ at: '.' }),
+        new commands.SetupRemoteDocker({ version: config.docker.version }),
+        new reusable.ReusedCommand(createDockerContextCommand),
+        new reusable.ReusedCommand(dockerLoginCommand),
+        new commands.Run({
+          name: 'Build docker image for << parameters.apim-project >>',
+          command: `${dockerBuildCommand(environment, dockerTags, isProd)}`,
+          working_directory: '<< parameters.apim-project >>',
+        }),
+        ...(isProd || isSupportBranchOrMaster(environment.branch)
+          ? [
+              new reusable.ReusedCommand(orbs.keeper.commands['env-export'], {
+                'secret-url': config.secrets.aquaKey,
+                'var-name': 'AQUA_KEY',
+              }),
+              new reusable.ReusedCommand(orbs.keeper.commands['env-export'], {
+                'secret-url': config.secrets.aquaSecret,
+                'var-name': 'AQUA_SECRET',
+              }),
+              new reusable.ReusedCommand(orbs.keeper.commands['env-export'], {
+                'secret-url': config.secrets.aquaRegistryUsername,
+                'var-name': 'AQUA_USERNAME',
+              }),
+              new reusable.ReusedCommand(orbs.keeper.commands['env-export'], {
+                'secret-url': config.secrets.aquaRegistryPassword,
+                'var-name': 'AQUA_PASSWORD',
+              }),
+              new reusable.ReusedCommand(orbs.keeper.commands['env-export'], {
+                'secret-url': config.secrets.aquaScannerKey,
+                'var-name': 'SCANNER_TOKEN',
+              }),
+              new reusable.ReusedCommand(orbs.keeper.commands['env-export'], {
+                'secret-url': config.secrets.githubApiToken,
+                'var-name': 'GITHUB_TOKEN',
+              }),
+              new reusable.ReusedCommand(orbs.aquasec.commands['install_billy']),
+              new reusable.ReusedCommand(orbs.aquasec.commands['pull_aqua_scanner_image']),
+              new reusable.ReusedCommand(orbs.aquasec.commands['register_artifact'], {
+                artifact_to_register: `${dockerTags[0]}`,
+              }),
+              new reusable.ReusedCommand(orbs.aquasec.commands['scan_docker_image'], {
+                docker_image_to_scan: `${dockerTags[0]}`,
+                scanner_url: config.aqua.scannerUrl,
+              }),
+            ]
+          : []),
+        new reusable.ReusedCommand(dockerLogoutCommand),
+      ],
+>>>>>>> b8704ab4cd (ci: revert aqua integration removal)
     );
   }
 
@@ -75,16 +134,107 @@ export class BuildDockerImageJob {
       command += ' --push';
     }
 
+<<<<<<< HEAD
     if (isProd) {
       command += ` --quiet`;
     }
+=======
+  public static create(dynamicConfig: Config, environment: CircleCIEnvironment, isProd: boolean): reusable.ParameterizedJob {
+    dynamicConfig.importOrb(orbs.keeper).importOrb(orbs.aquasec);
+>>>>>>> b8704ab4cd (ci: revert aqua integration removal)
 
     command += ` --platform=linux/arm64,linux/amd64 -f docker/Dockerfile \\\n`;
 
     command += `${dockerTags.map((t) => `-t ${t}`).join(' ')} \\\n`;
     command += `<< parameters.docker-context >>`;
 
+<<<<<<< HEAD
     return command;
+=======
+    const dockerLogoutCommand = DockerLogoutCommand.get(environment, isProd);
+    dynamicConfig.addReusableCommand(dockerLogoutCommand);
+
+    const parsedGraviteeioVersion = parse(environment.graviteeioVersion);
+
+    const variants: Variant[] = ['alpine', 'debian'];
+    return new reusable.ParameterizedJob(
+      BuildDockerBackendImageJob.jobName,
+      BaseExecutor.create(),
+      BuildDockerBackendImageJob.customParametersList,
+      [
+        new commands.Checkout(),
+        new commands.workspace.Attach({ at: '.' }),
+        new commands.SetupRemoteDocker({ version: config.docker.version }),
+        new reusable.ReusedCommand(createDockerContextCommand),
+        new reusable.ReusedCommand(dockerLoginCommand),
+        ...aquaSetupCommands(),
+        ...variants.flatMap((variant) => {
+          const dockerTags: string[] = dockerTagsArgument(environment, parsedGraviteeioVersion, isProd, variant);
+          return [
+            new commands.Run({
+              name: `Build docker image for << parameters.apim-project >>-${variant}`,
+              command: `${dockerBuildCommand(environment, dockerTags, isProd, variant)}`,
+              working_directory: '<< parameters.apim-project >>',
+            }),
+            ...(isProd || isSupportBranchOrMaster(environment.branch)
+              ? [
+                  new reusable.ReusedCommand(orbs.aquasec.commands['register_artifact'], {
+                    artifact_to_register: `${dockerTags[0]}`,
+                  }),
+                  new reusable.ReusedCommand(orbs.aquasec.commands['scan_docker_image'], {
+                    docker_image_to_scan: `${dockerTags[0]}`,
+                    scanner_url: config.aqua.scannerUrl,
+                  }),
+                ]
+              : []),
+          ];
+        }),
+        new reusable.ReusedCommand(dockerLogoutCommand),
+      ],
+    );
+  }
+}
+
+function aquaSetupCommands() {
+  return [
+    new reusable.ReusedCommand(orbs.keeper.commands['env-export'], {
+      'secret-url': config.secrets.aquaKey,
+      'var-name': 'AQUA_KEY',
+    }),
+    new reusable.ReusedCommand(orbs.keeper.commands['env-export'], {
+      'secret-url': config.secrets.aquaSecret,
+      'var-name': 'AQUA_SECRET',
+    }),
+    new reusable.ReusedCommand(orbs.keeper.commands['env-export'], {
+      'secret-url': config.secrets.aquaRegistryUsername,
+      'var-name': 'AQUA_USERNAME',
+    }),
+    new reusable.ReusedCommand(orbs.keeper.commands['env-export'], {
+      'secret-url': config.secrets.aquaRegistryPassword,
+      'var-name': 'AQUA_PASSWORD',
+    }),
+    new reusable.ReusedCommand(orbs.keeper.commands['env-export'], {
+      'secret-url': config.secrets.aquaScannerKey,
+      'var-name': 'SCANNER_TOKEN',
+    }),
+    new reusable.ReusedCommand(orbs.keeper.commands['env-export'], {
+      'secret-url': config.secrets.githubApiToken,
+      'var-name': 'GITHUB_TOKEN',
+    }),
+    new reusable.ReusedCommand(orbs.aquasec.commands['install_billy']),
+    new reusable.ReusedCommand(orbs.aquasec.commands['pull_aqua_scanner_image']),
+  ];
+}
+
+function dockerBuildCommand(environment: CircleCIEnvironment, dockerTags: string[], isProd: boolean, variant?: Variant) {
+  let command = 'docker buildx build';
+
+  const dockerfile = variant === 'debian' ? 'docker/Dockerfile.debian' : 'docker/Dockerfile';
+
+  // Only publish if not dry run or not prod
+  if (!isProd || !environment.isDryRun) {
+    command += ' --push';
+>>>>>>> b8704ab4cd (ci: revert aqua integration removal)
   }
 
   private static dockerTagsArgument(environment: CircleCIEnvironment, graviteeioVersion: GraviteeioVersion, isProd: boolean): string[] {
