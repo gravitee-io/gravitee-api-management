edb45267a9e94853b0a82ab26f07f12811ce7f80
jourdiw <jourdi.waller@graviteesource.com>
2025-08-28 16:26:57 +0200

    feat(repository): create portal page context jdbc repository

--- gravitee-apim-repository/gravitee-apim-repository-jdbc/src/test/java/io/gravitee/repository/jdbc/JdbcTestRepositoryInitializer.java
+++ gravitee-apim-repository/gravitee-apim-repository-jdbc/src/test/java/io/gravitee/repository/jdbc/JdbcTestRepositoryInitializer.java
@@ -152,7 +152,8 @@ public class JdbcTestRepositoryInitializer implements TestRepositoryInitializer 
         "portal_menu_links",
         "clusters",
         "cluster_groups",
-        "portal_pages"
+        "portal_pages",
+        "portal_page_contexts"
     );
     private static final List<String> tablesToDrop = concatenate(tablesToTruncate, List.of("databasechangelog", "databasechangeloglock"));
     private final DataSource dataSource;
