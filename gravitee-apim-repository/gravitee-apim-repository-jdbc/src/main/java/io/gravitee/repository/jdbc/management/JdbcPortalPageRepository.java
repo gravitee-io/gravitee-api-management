310973a5803fca18c73fa906f16c2e1256c74aad
jourdiw <jourdi.waller@graviteesource.com>
2025-08-28 15:00:28 +0200

    feat(repository): create portal page jdbc repository

---
+++ gravitee-apim-repository/gravitee-apim-repository-jdbc/src/main/java/io/gravitee/repository/jdbc/management/JdbcPortalPageRepository.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright © 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.repository.jdbc.management;
+
+import io.gravitee.repository.jdbc.orm.JdbcObjectMapper;
+import io.gravitee.repository.management.api.PortalPageRepository;
+import io.gravitee.repository.management.model.PortalPage;
+import java.sql.Types;
+import java.util.Date;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author GraviteeSource Team
+ */
+@Repository
+public class JdbcPortalPageRepository extends JdbcAbstractCrudRepository<PortalPage, String> implements PortalPageRepository {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(JdbcPortalPageRepository.class);
+
+    JdbcPortalPageRepository(@Value("${management.jdbc.prefix:}") String tablePrefix) {
+        super(tablePrefix, "portal_pages");
+    }
+
+    @Override
+    protected JdbcObjectMapper<PortalPage> buildOrm() {
+        return JdbcObjectMapper
+            .builder(PortalPage.class, this.tableName, "id")
+            .addColumn("id", Types.NVARCHAR, String.class)
+            .addColumn("environment_id", Types.NVARCHAR, String.class)
+            .addColumn("name", Types.NVARCHAR, String.class)
+            .addColumn("content", Types.LONGNVARCHAR, String.class)
+            .addColumn("created_at", Types.TIMESTAMP, Date.class)
+            .addColumn("updated_at", Types.TIMESTAMP, Date.class)
+            .build();
+    }
+
+    @Override
+    protected String getId(PortalPage item) {
+        return item.getId();
+    }
+
+    @Override
+    protected PreparedStatementCreator buildUpdatePreparedStatementCreator(PortalPage item) {
+        LOGGER.debug("Building update statement for PortalPage: {}", item);
+        return getOrm().buildUpdatePreparedStatementCreator(item, getId(item));
+    }
+}
