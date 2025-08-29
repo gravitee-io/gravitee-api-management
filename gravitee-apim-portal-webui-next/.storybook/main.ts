4c1bc5b0a89c7ec88e98bf28bf8ba07f10fad5ed
jourdiw <jourdi.waller@graviteesource.com>
2025-08-29 15:36:21 +0200

    feat(portal-next): integrate gravitee-markdown stories into storybook

--- gravitee-apim-portal-webui-next/.storybook/main.ts
+++ gravitee-apim-portal-webui-next/.storybook/main.ts
@@ -1,7 +1,11 @@
 import type { StorybookConfig } from '@storybook/angular';
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: [
+    '../src/**/*.mdx',
+    '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+    '../projects/gravitee-markdown/src/**/*.stories.@(js|jsx|ts|tsx|mdx)',
+  ],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@chromatic-com/storybook', '@storybook/addon-interactions'],
   framework: {
     name: '@storybook/angular',
