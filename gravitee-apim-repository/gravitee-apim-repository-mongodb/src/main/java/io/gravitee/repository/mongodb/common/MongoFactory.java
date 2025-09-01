1aa7e4ae9af97b06b1758fee91647b278d8580fc
Wojciech Baszczyk <wojciech.baszczyk@graviteesource.com>
2025-09-01 16:28:43 +0200

    fix: manage index upgraders with dedicated mongo client to handle large index creation

--- gravitee-apim-repository/gravitee-apim-repository-mongodb/src/main/java/io/gravitee/repository/mongodb/common/MongoFactory.java
+++ gravitee-apim-repository/gravitee-apim-repository-mongodb/src/main/java/io/gravitee/repository/mongodb/common/MongoFactory.java
@@ -341,29 +341,25 @@ public class MongoFactory implements FactoryBean<MongoClient> {
         // According to https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/beans/factory/FactoryBean.html#isSingleton--
         // It is the responsibility of the bean factory to ensure singleton instance.
         if (mongoClient == null) {
-            // Trying to get the MongoClientURI if uri property is defined
-            String uri = readPropertyValue(propertyPrefix + "uri");
-
-            if (uri != null && !uri.isEmpty()) {
-                // The builder can be configured with default options, which may be overridden by options specified in the URI string.
-                mongoClient = MongoClients.create(buildClientSettingsFromURI(uri, false));
-            } else {
-                mongoClient = MongoClients.create(buildClientSettingsFromProperties(false));
-            }
+            mongoClient = MongoClients.create(buildMongoClientSettings(false));
         }
 
         return mongoClient;
     }
 
-    public com.mongodb.reactivestreams.client.MongoClient getReactiveClient() {
+    public MongoClientSettings buildMongoClientSettings(boolean isReactive) {
         // Trying to get the MongoClientURI if uri property is defined
         String uri = readPropertyValue(propertyPrefix + "uri");
 
         if (uri != null && !uri.isEmpty()) {
-            return com.mongodb.reactivestreams.client.MongoClients.create(buildClientSettingsFromURI(uri, true));
+            return buildClientSettingsFromURI(uri, isReactive);
         } else {
-            return com.mongodb.reactivestreams.client.MongoClients.create(buildClientSettingsFromProperties(true));
+            return buildClientSettingsFromProperties(isReactive);
         }
+    }
+
+    public com.mongodb.reactivestreams.client.MongoClient getReactiveClient() {
+        return com.mongodb.reactivestreams.client.MongoClients.create(buildMongoClientSettings(true));
     }
 
     private int getServersCount() {
