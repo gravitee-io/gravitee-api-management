5dfacf3fa19befe82568c346b057ddf947d02cab
Wojciech Baszczyk <wojciech.baszczyk@graviteesource.com>
2025-09-01 16:28:43 +0200

    fix: manage index upgraders with dedicated mongo client to handle large index creation

--- gravitee-apim-repository/gravitee-apim-repository-mongodb/src/main/java/io/gravitee/repository/mongodb/management/upgrade/upgrader/index/IndexUpgrader.java
+++ gravitee-apim-repository/gravitee-apim-repository-mongodb/src/main/java/io/gravitee/repository/mongodb/management/upgrade/upgrader/index/IndexUpgrader.java
@@ -15,20 +15,19 @@
  */
 package io.gravitee.repository.mongodb.management.upgrade.upgrader.index;
 
-import com.mongodb.MongoCommandException;
-import com.mongodb.client.model.IndexOptions;
-import io.gravitee.repository.mongodb.management.upgrade.upgrader.common.MongoUpgrader;
-import org.bson.BsonDocument;
+import io.gravitee.repository.mongodb.management.upgrade.upgrader.common.IndexMongoUpgrader;
+import java.time.Duration;
 import org.bson.BsonInt32;
-import org.bson.BsonString;
 import org.bson.BsonValue;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 /**
  * @author GraviteeSource Team
  */
-public abstract class IndexUpgrader extends MongoUpgrader {
+public abstract class IndexUpgrader extends IndexMongoUpgrader {
 
     private static final Logger LOG = LoggerFactory.getLogger(IndexUpgrader.class);
 
@@ -42,26 +41,42 @@ public abstract class IndexUpgrader extends MongoUpgrader {
         return new BsonInt32(-1);
     }
 
-    protected static BsonValue text(String fieldName) {
-        return new BsonDocument(fieldName, new BsonString("text"));
-    }
-
     @Override
     public boolean upgrade() {
         Index index = buildIndex();
-        BsonDocument bson = index.bson();
-        IndexOptions options = index.options();
         String collection = buildCollectionName(index.getCollection());
-        String name = options.getName();
-        try {
-            LOG.info("creating index {} on collection {}", name, collection);
-            template.getCollection(collection).createIndex(bson, options);
-            LOG.debug("index {} has been created: {}", name, bson.toJson());
-        } catch (MongoCommandException e) {
-            String message = "unable to create index {} on collection {}: {}";
-            LOG.warn(message, name, collection, e.getMessage());
-        }
-        return true;
+        String name = index.options().getName();
+
+        Mono<Boolean> create = template
+            .indexOps(collection)
+            .ensureIndex(index.toIndexDefinition())
+            .doOnSubscribe(s ->
+                LOG.info("Starting creation of index {} on {}", name, collection)
+            )
+            .doOnSuccess(r ->
+                LOG.info("Index {} has been created successfully on {}", name, collection)
+            )
+            .thenReturn(true)
+            .onErrorResume(e -> {
+                LOG.error(
+                    "Unexpected error while creating index {} on {}",
+                    name,
+                    collection,
+                    e
+                );
+                return Mono.just(false);
+            })
+            .cache();
+
+        Flux
+            .interval(Duration.ofSeconds(10))
+            .doOnNext(t ->
+                LOG.info("Index {} on {} is still being created...", name, collection)
+            )
+            .takeUntilOther(create)
+            .subscribe();
+
+        return Boolean.TRUE.equals(create.block());
     }
 
     @Override
