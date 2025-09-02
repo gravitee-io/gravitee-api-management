d5f6ecc1102de1c0b201b597685064d73b040c99
Benoit Bordigoni <benoit.bordigoni@graviteesource.com>
2025-09-02 09:02:35 +0200

    fix: persist useAutoFetch attribute
    (cherry picked from commit 056e7882f425376badc7c8a614c7c4e909aafc69)

--- gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/apim/core/api/model/crd/PageCRD.java
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/apim/core/api/model/crd/PageCRD.java
@@ -43,6 +43,7 @@ public class PageCRD {
     private Map<String, String> configuration;
     private Set<AccessControl> accessControls;
     private boolean excludedAccessControls;
+    private Boolean useAutoFetch;
 
     // Folder attributes
     @With
