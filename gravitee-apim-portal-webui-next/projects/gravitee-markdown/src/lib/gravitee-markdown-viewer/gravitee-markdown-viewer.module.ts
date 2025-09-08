dbafce990132c6ea2be3f0f4aeaf745374a50694
Ouahid Khelifi <ouahid.khelifi@gmail.com>
2025-09-08 08:49:13 +0200

    feat: add viewer service using marked and highlight libraries

--- gravitee-apim-portal-webui-next/projects/gravitee-markdown/src/lib/gravitee-markdown-viewer/gravitee-markdown-viewer.module.ts
+++ gravitee-apim-portal-webui-next/projects/gravitee-markdown/src/lib/gravitee-markdown-viewer/gravitee-markdown-viewer.module.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2025 The Gravitee team (http://gravitee.io)
+ * Copyright (C) 2024 The Gravitee team (http://gravitee.io)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,11 +13,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/*
- * Public API Surface of gravitee-markdown
- */
+import { NgModule } from '@angular/core';
+import { DynamicHooksComponent } from 'ngx-dynamic-hooks';
 
-export * from './lib/gravitee-markdown.service';
-export * from './lib/gravitee-markdown.component';
-export * from './lib/gravitee-markdown-editor/public-api';
-export * from './lib/gravitee-markdown-viewer/public-api';
+import { GraviteeMarkdownViewerComponent } from './gravitee-markdown-viewer.component';
+
+@NgModule({
+  imports: [DynamicHooksComponent],
+  exports: [GraviteeMarkdownViewerComponent],
+  declarations: [GraviteeMarkdownViewerComponent],
+})
+export class GraviteeMarkdownViewerModule {}
