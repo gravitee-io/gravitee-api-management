c6825e2274a928316f28bea1061af345104f2b8b
ankita-gupta21 <ankita.gupta@graviteesource.com>
2025-09-17 11:03:33 +0530

    fix(api): prevent multiple primary owners

--- gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/rest/api/service/impl/ApiServiceImpl.java
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/rest/api/service/impl/ApiServiceImpl.java
@@ -550,14 +550,6 @@ public class ApiServiceImpl extends AbstractService implements ApiService {
             // Make sure visibility is PRIVATE by default if not set.
             repoApi.setVisibility(api.getVisibility() == null ? Visibility.PRIVATE : Visibility.valueOf(api.getVisibility().toString()));
 
-<<<<<<< HEAD
-            // Add Default groups
-            Set<String> defaultGroups = groupService
-                .findByEvent(executionContext.getEnvironmentId(), GroupEvent.API_CREATE)
-                .stream()
-                .map(GroupEntity::getId)
-                .collect(toSet());
-=======
             log.debug("Add default groups");
             Set<GroupEntity> defaultGroupEntities = groupService.findByEvent(executionContext.getEnvironmentId(), GroupEvent.API_CREATE);
 
@@ -570,8 +562,6 @@ public class ApiServiceImpl extends AbstractService implements ApiService {
                     .filter(group -> StringUtils.isEmpty(group.getApiPrimaryOwner()))
                     .map(GroupEntity::getId)
                     .collect(toSet());
-
->>>>>>> de72768890 (fix(api): prevent multiple primary owners)
             if (repoApi.getGroups() == null) {
                 repoApi.setGroups(defaultGroups.isEmpty() ? null : defaultGroups);
             } else {
