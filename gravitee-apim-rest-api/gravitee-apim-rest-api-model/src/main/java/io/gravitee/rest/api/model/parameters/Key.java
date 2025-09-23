2101ab1a7dfb2972650579da6baef3d68790c212
Ouahid Khelifi <ouahid.khelifi@gmail.com>
2025-09-23 16:39:25 +0200

    feat(portal-next): update theme colors and icon
    (cherry picked from commit eb557c4878245dec507b561c580291bd5884c562)

--- gravitee-apim-rest-api/gravitee-apim-rest-api-model/src/main/java/io/gravitee/rest/api/model/parameters/Key.java
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-model/src/main/java/io/gravitee/rest/api/model/parameters/Key.java
@@ -117,18 +117,18 @@ public enum Key {
         new HashSet<>(singletonList(ENVIRONMENT))
     ),
     PORTAL_NEXT_ACCESS_ENABLED("portal.next.access.enabled", "false", new HashSet<>(singletonList(ENVIRONMENT))),
-    PORTAL_NEXT_THEME_COLOR_PRIMARY("portal.next.theme.color.primary", "#DA3B00", new HashSet<>(singletonList(ENVIRONMENT))),
-    PORTAL_NEXT_THEME_COLOR_SECONDARY("portal.next.theme.color.secondary", "#FFA685", new HashSet<>(singletonList(ENVIRONMENT))),
-    PORTAL_NEXT_THEME_COLOR_TERTIARY("portal.next.theme.color.tertiary", "#22A3B3", new HashSet<>(singletonList(ENVIRONMENT))),
-    PORTAL_NEXT_THEME_COLOR_ERROR("portal.next.theme.color.error", "#C72224", new HashSet<>(singletonList(ENVIRONMENT))),
+    PORTAL_NEXT_THEME_COLOR_PRIMARY("portal.next.theme.color.primary", "#275CF6", new HashSet<>(singletonList(ENVIRONMENT))),
+    PORTAL_NEXT_THEME_COLOR_SECONDARY("portal.next.theme.color.secondary", "#F4F7FD", new HashSet<>(singletonList(ENVIRONMENT))),
+    PORTAL_NEXT_THEME_COLOR_TERTIARY("portal.next.theme.color.tertiary", "#275CF6", new HashSet<>(singletonList(ENVIRONMENT))),
+    PORTAL_NEXT_THEME_COLOR_ERROR("portal.next.theme.color.error", "#EE2623", new HashSet<>(singletonList(ENVIRONMENT))),
     PORTAL_NEXT_THEME_COLOR_BACKGROUND_PAGE(
         "portal.next.theme.color.background.page",
-        "#F7F7F8",
+        "#FFFFFF",
         new HashSet<>(singletonList(ENVIRONMENT))
     ),
     PORTAL_NEXT_THEME_COLOR_BACKGROUND_CARD(
         "portal.next.theme.color.background.card",
-        "#FFFFFF",
+        "#F4F7FD",
         new HashSet<>(singletonList(ENVIRONMENT))
     ),
     PORTAL_NEXT_THEME_CUSTOM_CSS("portal.next.theme.customCss", new HashSet<>(singletonList(ENVIRONMENT))),
