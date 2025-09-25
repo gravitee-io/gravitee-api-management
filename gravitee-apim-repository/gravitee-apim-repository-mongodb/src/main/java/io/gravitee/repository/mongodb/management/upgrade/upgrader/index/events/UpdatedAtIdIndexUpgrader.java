8e50d3b4babda06483c9a846960cbc679dd05f46
samirtechlab <samir.tech.lab@gmail.com>
2025-09-25 11:53:19 +0200

    feat(upgrader): Add mongobd indexes based on SaaS instancies reco
    (cherry picked from commit dbd6f4b53b4b77fd6607b8e3ccc85f9eb4a81c22)

---
+++ gravitee-apim-repository/gravitee-apim-repository-mongodb/src/main/java/io/gravitee/repository/mongodb/management/upgrade/upgrader/index/events/UpdatedAtIdIndexUpgrader.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright © 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.repository.mongodb.management.upgrade.upgrader.index.events;
+
+import io.gravitee.repository.mongodb.management.upgrade.upgrader.index.Index;
+import io.gravitee.repository.mongodb.management.upgrade.upgrader.index.IndexUpgrader;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author GraviteeSource Team
+ */
+@Component("UpdatedAtIdIndexUpgrader")
+public class UpdatedAtIdIndexUpgrader extends IndexUpgrader {
+
+    @Override
+    protected Index buildIndex() {
+        return Index.builder()
+            .collection("events")
+            .name("u-1i-1")
+            .key("updatedAt", descending())
+            .key("_id", descending())
+            .build();
+    }
+}
