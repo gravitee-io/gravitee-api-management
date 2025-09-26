9acb915312bd1b399c8986056b9596f53442aafe
Ouahid Khelifi <ouahid.khelifi@gmail.com>
2025-09-26 10:14:17 +0200

    feat: init default portal next home page
    (cherry picked from commit 6dd9575006fa9413f3d9c5b1c0a149527811b4d7)

--- gravitee-apim-portal-webui-next/projects/gravitee-markdown/src/lib/gravitee-markdown-viewer/gravitee-markdown-viewer.stories.ts
+++ gravitee-apim-portal-webui-next/projects/gravitee-markdown/src/lib/gravitee-markdown-viewer/gravitee-markdown-viewer.stories.ts
@@ -141,3 +141,119 @@ and scrambled it to make a type specimen book. It has survived not only five cen
     },
   }),
 };
+
+export const PortalHomePage: StoryObj<GraviteeMarkdownViewerComponent> = {
+  render: () => ({
+    template: `
+        <gmd-viewer [content]="content"></gmd-viewer>
+    `,
+    props: {
+      content: `
+<gmd-grid>
+    <gmd-md class="homepage-title">
+        # Welcome to the Developer Portal
+        Access all APIs, documentation, and tools to build your next integration.
+    </gmd-md>
+    <gmd-cell style="text-align: center; margin: auto;">
+        <gmd-button link="/catalog">Explore all APIs</gmd-button>
+        <gmd-button link="/guides" appearance="outlined" style="--gmd-button-outlined-label-text-weight: 700; --gmd-button-outlined-label-text-color: black;"
+        >Get started</gmd-button>
+    </gmd-cell>
+    <gmd-md class="homepage-cover-photo"> ![homepage](assets/images/homepage/desk.png "Homepage picture")</gmd-md>
+</gmd-grid>
+
+### Your toolkit for building
+
+<gmd-grid [columns]="3">
+    <gmd-md>
+        ![book](./assets/images/homepage/book.svg "Book icon")
+        #### API catalog
+        Browse and test all available APIs in one place.
+    </gmd-md>
+    <gmd-md>
+        ![laptop](./assets/images/homepage/laptop.svg "Laptop icon")
+        #### Interactive docs
+        Explore clear, structured documentation with code samples.
+    </gmd-md>
+    <gmd-md>
+        ![vector](./assets/images/homepage/vector.svg "Vector icon")
+        #### Usage analytics
+        Track API usage, error rates, and performance metrics.
+    </gmd-md>
+    <gmd-md>
+        ![group](./assets/images/homepage/group.svg "Group icon")
+        #### API catalog
+        Browse and test all available APIs in one place.
+    </gmd-md>
+    <gmd-md>
+        ![support](./assets/images/homepage/support.svg "Support icon")
+        #### Interactive docs
+        Explore clear, structured documentation with code samples.
+    </gmd-md>
+    <gmd-md>
+        ![support](./assets/images/homepage/service.svg "Service icon")
+        #### Usage analytics
+        Track API usage, error rates, and performance metrics.
+    </gmd-md>
+</gmd-grid>
+
+### Get started in minutes
+
+<gmd-grid [columns]="3">
+    <gmd-card backgroundColor="none">
+        <gmd-card-title>Your first API call</gmd-card-title>
+        <gmd-md>Learn how to make a basic request and receive a response.Learn how to make a basic request and receive a response.</gmd-md>
+        <div class="flex-container">
+          <gmd-button link="/guides" appearance="outlined" class="get-started-card__button"
+          >Read <img src="assets/images/homepage/arrow-right.svg" alt="arrow right icon" title="Arrow right icon"/></gmd-button>
+        </div>
+    </gmd-card>
+    <gmd-card backgroundColor="none">
+        <gmd-card-title>Authentication walkthrough</gmd-card-title>
+        <gmd-md>A step-by-step guide to generating and managing API keys.</gmd-md>
+        <div class="flex-container">
+          <gmd-button link="/guides" appearance="outlined" class="get-started-card__button"
+          >Read <img src="assets/images/homepage/arrow-right.svg" alt="arrow right icon" title="Arrow right icon"/></gmd-button>
+        </div>
+    </gmd-card>
+    <gmd-card backgroundColor="none">
+        <gmd-card-title>Integrating SDK into your project</gmd-card-title>
+        <gmd-md>Use our official library to simplify your code.</gmd-md>
+        <div class="flex-container">
+          <gmd-button link="/guides" appearance="outlined" class="get-started-card__button"
+          >Read <img src="assets/images/homepage/arrow-right.svg" alt="arrow right icon" title="Arrow right icon"/></gmd-button>
+        </div>
+    </gmd-card>
+</gmd-grid>
+<style>
+  .homepage-title {
+    display: flex;
+    flex-direction: column;
+    max-width: 100%;
+    text-align: center;
+    margin: auto;
+  }
+
+  .homepage-cover-photo {
+    display: flex;
+    max-width: 100%;
+    margin: 80px auto;
+  }
+
+  .get-started-card__button {
+    --gmd-button-outlined-label-text-weight: 700;
+    --gmd-button-outlined-label-text-color: black;
+    margin-top: auto;
+    padding-top: 12px;
+  }
+
+  .flex-container {
+    display: flex;
+    flex-direction: column;
+    height: 100%
+  }
+</style>
+      `,
+    },
+  }),
+};
