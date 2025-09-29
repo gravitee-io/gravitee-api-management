bd256cc1db7cfe7c67b6d8f769d82e89c6c54d3b
Pawel Lesniowski <135582189+pes-spyro-soft-com@users.noreply.github.com>
2025-09-29 16:11:15 +0200

    Revert "fix: publish / unpublish page - 500 resp fix"
    This reverts commit df54e92044b62c364d69ca40297e93da5cc24828.

--- gravitee-apim-console-webui/src/services-ngx/portal-pages.service.ts
+++ gravitee-apim-console-webui/src/services-ngx/portal-pages.service.ts
@@ -38,12 +38,12 @@ export class PortalPagesService {
     return this.http.get<PortalPagesResponse>(`${this.constants.env.v2BaseURL}/portal-pages?type=homepage&expands=content`);
   }
 
-  publishPage(pageId: string): Observable<PortalPagesResponse> {
-    return this.http.post<PortalPagesResponse>(`${this.constants.env.v2BaseURL}/portal-pages/${pageId}/_publish`, {});
+  publishPage(pageId: string): Observable<PortalPageWithDetails> {
+    return this.http.post<PortalPageWithDetails>(`${this.constants.env.v2BaseURL}/portal-pages/${pageId}/_publish`, {});
   }
 
-  unpublishPage(pageId: string): Observable<PortalPagesResponse> {
-    return this.http.post<PortalPagesResponse>(`${this.constants.env.v2BaseURL}/portal-pages/${pageId}/_unpublish`, {});
+  unpublishPage(pageId: string): Observable<PortalPageWithDetails> {
+    return this.http.post<PortalPageWithDetails>(`${this.constants.env.v2BaseURL}/portal-pages/${pageId}/_unpublish`, {});
   }
 
   patchPortalPage(portalPageId: string, patchedPage: PatchPortalPage): Observable<PortalPageWithDetails> {
