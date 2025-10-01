ac07d1d5685d76022c3b693258b88387ecf4c774
Jedrzej Janasiak <jedrzej.janasiak@graviteesource.com>
2025-10-01 16:29:37 +0200

    chore: bump gravitee-expression-language version to 4.2.0
    (cherry picked from commit fc886c92c75ba655a1ff0a2e6f72896475f83b5b)
    
    # Conflicts:
    #	pom.xml

--- gravitee-apim-gateway/gravitee-apim-gateway-handlers/gravitee-apim-gateway-handlers-api/src/test/java/io/gravitee/gateway/reactive/handlers/api/el/ApiTemplateVariableProviderTest.java
+++ gravitee-apim-gateway/gravitee-apim-gateway-handlers/gravitee-apim-gateway-handlers-api/src/test/java/io/gravitee/gateway/reactive/handlers/api/el/ApiTemplateVariableProviderTest.java
@@ -31,7 +31,6 @@ import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.springframework.expression.spel.SpelEvaluationException;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class ApiTemplateVariableProviderTest {
@@ -105,7 +104,7 @@ class ApiTemplateVariableProviderTest {
                 .assertError(e -> {
                     assertThat(e)
                         .isInstanceOf(ExpressionEvaluationException.class)
-                        .hasCauseInstanceOf(SpelEvaluationException.class)
+                        .hasCauseInstanceOf(IllegalArgumentException.class)
                         .hasStackTraceContaining("EL1012E: Cannot index into a null value");
 
                     return true;
@@ -182,7 +181,7 @@ class ApiTemplateVariableProviderTest {
                 .assertError(e -> {
                     assertThat(e)
                         .isInstanceOf(ExpressionEvaluationException.class)
-                        .hasCauseInstanceOf(SpelEvaluationException.class)
+                        .hasCauseInstanceOf(IllegalArgumentException.class)
                         .hasStackTraceContaining("EL1012E: Cannot index into a null value");
 
                     return true;
