d67e41f0ee537660425599543eb6ad78475c41df
Ouahid Khelifi <ouahid.khelifi@gmail.com>
2025-10-06 15:54:05 +0200

    fix: reflow api analytics charts when menu is collapsed or expanded
    (cherry picked from commit aad44743b28ffc94d678ea2208fdb8e23362ac92)

--- gravitee-apim-console-webui/src/shared/components/gio-chart-abstract/gio-chart-abstract.component.ts
+++ gravitee-apim-console-webui/src/shared/components/gio-chart-abstract/gio-chart-abstract.component.ts
@@ -13,26 +13,30 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { Directive, effect, inject, signal, WritableSignal } from '@angular/core';
+import { DestroyRef, Directive, inject, signal, WritableSignal } from '@angular/core';
 import * as Highcharts from 'highcharts';
-import { toSignal } from '@angular/core/rxjs-interop';
 import { GioMenuService } from '@gravitee/ui-particles-angular';
+import { tap } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Directive()
 export abstract class GioChartAbstractComponent {
-  private reduced = toSignal(inject(GioMenuService).reduced$);
-
   Highcharts: typeof Highcharts = Highcharts;
   private chart: WritableSignal<Highcharts.Chart | undefined> = signal<Highcharts.Chart>(undefined);
 
   constructor() {
-    effect(() => {
-      if (this.reduced() && this.chart()) {
-        setTimeout(() => {
-          this.chart().reflow();
-        }, 0);
-      }
-    });
+    inject(GioMenuService)
+      .reduced$.pipe(
+        tap(() => {
+          if (this.chart()) {
+            setTimeout(() => {
+              this.chart().reflow();
+            }, 0);
+          }
+        }),
+        takeUntilDestroyed(inject(DestroyRef)),
+      )
+      .subscribe();
   }
 
   protected chartInstance(chart: Highcharts.Chart) {
