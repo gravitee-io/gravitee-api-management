f0c9068693a950a42a73d669662ba71a56115308
Ouahid Khelifi <ouahid.khelifi@gmail.com>
2025-10-20 14:50:02 +0200

    feat: get api name from the database when the user lists the promotions tasks

--- gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/rest/api/service/impl/promotion/PromotionTasksServiceImpl.java
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/rest/api/service/impl/promotion/PromotionTasksServiceImpl.java
@@ -19,15 +19,13 @@ import static io.gravitee.rest.api.model.permissions.RolePermission.ENVIRONMENT_
 import static io.gravitee.rest.api.model.permissions.RolePermissionAction.CREATE;
 import static io.gravitee.rest.api.model.permissions.RolePermissionAction.UPDATE;
 import static java.util.Collections.emptyList;
-import static java.util.stream.Collectors.*;
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.toList;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.gravitee.common.data.domain.Page;
 import io.gravitee.rest.api.model.EnvironmentEntity;
 import io.gravitee.rest.api.model.TaskEntity;
 import io.gravitee.rest.api.model.TaskType;
-import io.gravitee.rest.api.model.api.ApiEntity;
 import io.gravitee.rest.api.model.common.SortableImpl;
 import io.gravitee.rest.api.model.promotion.PromotionEntity;
 import io.gravitee.rest.api.model.promotion.PromotionEntityStatus;
@@ -35,17 +33,20 @@ import io.gravitee.rest.api.model.promotion.PromotionQuery;
 import io.gravitee.rest.api.service.EnvironmentService;
 import io.gravitee.rest.api.service.PermissionService;
 import io.gravitee.rest.api.service.common.ExecutionContext;
-import io.gravitee.rest.api.service.exceptions.TechnicalManagementException;
+import io.gravitee.rest.api.service.common.GraviteeContext;
 import io.gravitee.rest.api.service.impl.AbstractService;
 import io.gravitee.rest.api.service.promotion.PromotionService;
 import io.gravitee.rest.api.service.promotion.PromotionTasksService;
 import io.gravitee.rest.api.service.v4.ApiSearchService;
-import io.gravitee.rest.api.service.v4.ApiService;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import org.jetbrains.annotations.NotNull;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
@@ -56,11 +57,8 @@ import org.springframework.util.StringUtils;
 @Component
 public class PromotionTasksServiceImpl extends AbstractService implements PromotionTasksService {
 
-    private final Logger logger = LoggerFactory.getLogger(PromotionTasksServiceImpl.class);
-
     private final PromotionService promotionService;
     private final PermissionService permissionService;
-    private final ObjectMapper objectMapper;
     private final EnvironmentService environmentService;
     private final ApiSearchService apiSearchService;
 
@@ -68,13 +66,11 @@ public class PromotionTasksServiceImpl extends AbstractService implements Promot
         PromotionService promotionService,
         PermissionService permissionService,
         EnvironmentService environmentService,
-        ObjectMapper objectMapper,
         ApiSearchService apiSearchService
     ) {
         this.promotionService = promotionService;
         this.permissionService = permissionService;
         this.environmentService = environmentService;
-        this.objectMapper = objectMapper;
         this.apiSearchService = apiSearchService;
     }
 
@@ -149,16 +145,10 @@ public class PromotionTasksServiceImpl extends AbstractService implements Promot
         taskEntity.setType(TaskType.PROMOTION_APPROVAL);
         taskEntity.setCreatedAt(promotionEntity.getCreatedAt());
 
-        ApiEntity apiEntity;
-        try {
-            apiEntity = objectMapper.readValue(promotionEntity.getApiDefinition(), ApiEntity.class);
-        } catch (JsonProcessingException e) {
-            logger.warn("Problem while deserializing api definition for promotion {}", promotionEntity.getId());
-            throw new TechnicalManagementException();
-        }
+        var api = apiSearchService.findRepositoryApiById(GraviteeContext.getExecutionContext(), promotionEntity.getApiId());
 
         Map<String, Object> data = new HashMap<>();
-        data.put("apiName", apiEntity.getName());
+        data.put("apiName", api.getName());
         data.put("apiId", promotionEntity.getApiId());
         data.put("sourceEnvironmentName", promotionEntity.getSourceEnvName());
         data.put("targetEnvironmentName", promotionEntity.getTargetEnvName());
