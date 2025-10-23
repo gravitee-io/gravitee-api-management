37b552257897a98178ff5f8f07f8d59e23a09e9a
samirtechlab <samir.tech.lab@gmail.com>
2025-10-23 10:16:07 +0200

    feat(dashboard-lib): Fix pie-chart UT

--- gravitee-apim-portal-webui-next/projects/gravitee-dashboard/src/lib/components/chart/pie-chart/pie-chart.component.spec.ts
+++ gravitee-apim-portal-webui-next/projects/gravitee-dashboard/src/lib/components/chart/pie-chart/pie-chart.component.spec.ts
@@ -18,20 +18,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PieChartComponent } from './pie-chart.component';
 
 describe('PieChartComponent', () => {
-  // /*  let component: PieChartComponent;
-  //   let fixture: ComponentFixture<PieChartComponent>;*/
-  //
-  //   beforeEach(async () => {
-  //     await TestBed.configureTestingModule({
-  //       imports: [PieChartComponent],
-  //     }).compileComponents();
-  //
-  //     fixture = TestBed.createComponent(PieChartComponent);
-  //     component = fixture.componentInstance;
-  //     fixture.detectChanges();
-  //   });
-  //
-  //   it('should create', () => {
-  //     expect(component).toBeTruthy();
-  //   });
+  let component: PieChartComponent<'pie'>;
+  let fixture: ComponentFixture<PieChartComponent<'pie'>>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [PieChartComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(PieChartComponent<'pie'>);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 });
