b9a74567b79d53a65784bba245346a0b083b4a79
pes <pes@spyro-soft.com>
2025-10-24 14:21:20 +0200

    feat: empty state for top-bar

--- gravitee-apim-console-webui/src/portal/top-bar/portal-top-bar.component.ts
+++ gravitee-apim-console-webui/src/portal/top-bar/portal-top-bar.component.ts
@@ -13,15 +13,34 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import { GraviteeMarkdownEditorModule } from '@gravitee/gravitee-markdown';
+
+import { GioCardEmptyStateModule } from '@gravitee/ui-particles-angular';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { MatButton } from '@angular/material/button';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 import { PortalHeaderComponent } from '../components/header/portal-header.component';
+import { EmptyStateComponent } from '../../shared/components/empty-state/empty-state.component';
 
 @Component({
   selector: 'portal-top-bar',
   templateUrl: './portal-top-bar.component.html',
   styleUrls: ['./portal-top-bar.component.scss'],
-  imports: [PortalHeaderComponent, RouterOutlet],
+  imports: [
+    PortalHeaderComponent,
+    GraviteeMarkdownEditorModule,
+    ReactiveFormsModule,
+    EmptyStateComponent,
+    GioCardEmptyStateModule,
+    MatButton,
+  ],
 })
-export class PortalTopBarComponent {}
+export class PortalTopBarComponent {
+  contentControl = new FormControl({
+    value: '',
+    disabled: true,
+  });
+
+  isEmpty = true;
+}
