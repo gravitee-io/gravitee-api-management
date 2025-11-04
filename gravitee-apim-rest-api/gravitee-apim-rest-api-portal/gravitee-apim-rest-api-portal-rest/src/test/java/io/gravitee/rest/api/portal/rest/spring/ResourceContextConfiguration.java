0af032c2acbcd4b8dc1eb886895354fb6c75a153
Marek Mazur <marek.mazur@graviteesource.com>
2025-11-04 12:45:39 +0100

    test: add ListPortalNavigationItemsUseCase bean to ResourceContextConfiguration for tests

--- gravitee-apim-rest-api/gravitee-apim-rest-api-portal/gravitee-apim-rest-api-portal-rest/src/test/java/io/gravitee/rest/api/portal/rest/spring/ResourceContextConfiguration.java
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-portal/gravitee-apim-rest-api-portal-rest/src/test/java/io/gravitee/rest/api/portal/rest/spring/ResourceContextConfiguration.java
@@ -86,6 +86,7 @@ import io.gravitee.apim.core.plan.domain_service.PlanSynchronizationService;
 import io.gravitee.apim.core.plugin.crud_service.PolicyPluginCrudService;
 import io.gravitee.apim.core.plugin.domain_service.EndpointConnectorPluginDomainService;
 import io.gravitee.apim.core.policy.domain_service.PolicyValidationDomainService;
+import io.gravitee.apim.core.portal_page.use_case.ListPortalNavigationItemsUseCase;
 import io.gravitee.apim.core.promotion.service_provider.CockpitPromotionServiceProvider;
 import io.gravitee.apim.core.promotion.use_case.CreatePromotionUseCase;
 import io.gravitee.apim.core.promotion.use_case.ProcessPromotionUseCase;
@@ -953,6 +954,11 @@ public class ResourceContextConfiguration {
     @Bean
     public CreatePromotionUseCase createPromotionUseCase() {
         return mock(CreatePromotionUseCase.class);
+    }
+
+    @Bean
+    public ListPortalNavigationItemsUseCase listPortalNavigationItemsUseCase() {
+        return mock(ListPortalNavigationItemsUseCase.class);
     }
 
     @Bean
