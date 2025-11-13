029038fb13ce28ecb43d7a287ac0d98e8a94f9c9
Marek Mazur <marek.mazur@graviteesource.com>
2025-11-13 15:17:05 +0100

    feat(portal): implement GetPortalPageContent use case and related components

---
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/test/java/fixtures/core/model/PortalPageContentFixtures.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright © 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package fixtures.core.model;
+
+import io.gravitee.apim.core.portal_page.model.GraviteeMarkdownPageContent;
+import io.gravitee.apim.core.portal_page.model.PortalPageContent;
+import io.gravitee.apim.core.portal_page.model.PortalPageContentId;
+import java.util.List;
+
+public class PortalPageContentFixtures {
+
+    private static final String CONTENT_ID = "00000000-0000-0000-0000-000000000001";
+
+    public static GraviteeMarkdownPageContent aGraviteeMarkdownPageContent() {
+        return new GraviteeMarkdownPageContent(PortalPageContentId.of(CONTENT_ID), "# Welcome\n\nThis is a sample page content.");
+    }
+
+    public static GraviteeMarkdownPageContent aGraviteeMarkdownPageContent(String content) {
+        return new GraviteeMarkdownPageContent(PortalPageContentId.of(CONTENT_ID), content);
+    }
+
+    public static GraviteeMarkdownPageContent aGraviteeMarkdownPageContent(PortalPageContentId id, String content) {
+        return new GraviteeMarkdownPageContent(id, content);
+    }
+
+    public static List<PortalPageContent> samplePortalPageContents() {
+        return List.of(aGraviteeMarkdownPageContent());
+    }
+}
