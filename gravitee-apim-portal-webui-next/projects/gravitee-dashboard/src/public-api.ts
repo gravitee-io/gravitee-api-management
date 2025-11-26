3a3f20afaca14e044a75720d931a6eba7f96b597
paulatulis <paula.tulis@graviteesource.com>
2025-11-26 16:07:59 -0600

    feat: add line graph to analytics dashboard

--- gravitee-apim-portal-webui-next/projects/gravitee-dashboard/src/public-api.ts
+++ gravitee-apim-portal-webui-next/projects/gravitee-dashboard/src/public-api.ts
@@ -24,3 +24,4 @@ export * from './lib/components/grid/grid.component';
 export * from './lib/components/widget/model/widget/widget';
 export * from './lib/components/widget/widget.component';
 export * from './lib/components/chart/pie-chart/pie-chart.component';
+export * from './lib/components/chart/line-chart/line-chart.component';
