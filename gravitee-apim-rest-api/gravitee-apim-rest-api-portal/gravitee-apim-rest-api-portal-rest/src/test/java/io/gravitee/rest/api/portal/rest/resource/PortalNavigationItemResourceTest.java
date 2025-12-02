288c2600318dc63e5e60a7ba0a6ae04445d6262f
jourdiw <jourdi.waller@graviteesource.com>
2025-12-02 14:49:40 +0100

    feat(papi): add endpoint to get a portal nav item content by id

--- gravitee-apim-rest-api/gravitee-apim-rest-api-portal/gravitee-apim-rest-api-portal-rest/src/test/java/io/gravitee/rest/api/portal/rest/resource/PortalNavigationItemResourceTest.java
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-portal/gravitee-apim-rest-api-portal-rest/src/test/java/io/gravitee/rest/api/portal/rest/resource/PortalNavigationItemResourceTest.java
@@ -15,10 +15,16 @@
  */
 package io.gravitee.rest.api.portal.rest.resource;
 
+import static fixtures.core.model.PortalPageContentFixtures.ORGANIZATION_ID;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import fixtures.core.model.PortalPageContentFixtures;
 import inmemory.PortalNavigationItemsQueryServiceInMemory;
+import inmemory.PortalPageContentQueryServiceInMemory;
+import io.gravitee.apim.core.portal_page.model.GraviteeMarkdownPageContent;
 import io.gravitee.apim.core.portal_page.model.PortalArea;
+import io.gravitee.apim.core.portal_page.model.PortalNavigationPage;
+import io.gravitee.apim.core.portal_page.model.PortalPageContentId;
 import io.gravitee.rest.api.portal.rest.fixture.PortalNavigationFixtures;
 import io.gravitee.rest.api.service.common.GraviteeContext;
 import jakarta.ws.rs.core.Response;
@@ -39,6 +45,9 @@ public class PortalNavigationItemResourceTest extends AbstractResourceTest {
     @Autowired
     private PortalNavigationItemsQueryServiceInMemory portalNavigationItemsQueryService;
 
+    @Autowired
+    private PortalPageContentQueryServiceInMemory portalPageContentQueryService;
+
     @Override
     protected String contextPath() {
         return "portal-navigation-items/";
@@ -53,6 +62,7 @@ public class PortalNavigationItemResourceTest extends AbstractResourceTest {
     public void tearDown() {
         GraviteeContext.cleanContext();
         portalNavigationItemsQueryService.reset();
+        portalPageContentQueryService.reset();
     }
 
     @Nested
@@ -113,6 +123,114 @@ public class PortalNavigationItemResourceTest extends AbstractResourceTest {
 
             // When
             Response response = target(itemId.toString()).request().get();
+
+            // Then
+            assertThat(response.getStatus()).isEqualTo(404);
+        }
+    }
+
+    @Nested
+    class GetPortalNavigationItemContentById {
+
+        @Test
+        void should_return_content_when_page_found_and_published() {
+            // Given
+            var itemId = PortalNavigationFixtures.randomNavigationId();
+            var contentId = PortalNavigationFixtures.randomPageId();
+            var pageContent = PortalPageContentFixtures.aGraviteeMarkdownPageContent(
+                contentId,
+                ORGANIZATION_ID,
+                ENV_ID,
+                "Page content text"
+            );
+
+            var publishedPage = PortalNavigationFixtures.page(itemId, "Published Page", PortalArea.TOP_NAVBAR, contentId);
+            publishedPage.setPublished(true);
+            publishedPage.setEnvironmentId(ENV_ID);
+
+            portalNavigationItemsQueryService.initWith(List.of(publishedPage));
+            portalPageContentQueryService.initWith(List.of(pageContent));
+
+            // When
+            Response response = target(itemId.toString()).path("content").request().get();
+
+            // Then
+            assertThat(response.getStatus()).isEqualTo(200);
+            var content = response.readEntity(String.class);
+            assertThat(content).isEqualTo("Page content text");
+        }
+
+        @Test
+        void should_return_404_when_item_not_found() {
+            // Given
+            var unknownId = PortalNavigationFixtures.randomNavigationId();
+            portalNavigationItemsQueryService.initWith(List.of());
+
+            // When
+            Response response = target(unknownId.toString()).path("content").request().get();
+
+            // Then
+            assertThat(response.getStatus()).isEqualTo(404);
+        }
+
+        @Test
+        void should_return_404_when_item_is_unpublished() {
+            // Given
+            var itemId = PortalNavigationFixtures.randomNavigationId();
+            var contentId = PortalNavigationFixtures.randomPageId();
+            var pageContent = PortalPageContentFixtures.aGraviteeMarkdownPageContent(
+                contentId,
+                ORGANIZATION_ID,
+                ENV_ID,
+                "Page content text"
+            );
+
+            var unpublishedPage = PortalNavigationFixtures.page(itemId, "Unpublished Page", PortalArea.TOP_NAVBAR, contentId);
+            unpublishedPage.setPublished(false);
+            unpublishedPage.setEnvironmentId(ENV_ID);
+
+            portalNavigationItemsQueryService.initWith(List.of(unpublishedPage));
+            portalPageContentQueryService.initWith(List.of(pageContent));
+
+            // When
+            Response response = target(itemId.toString()).path("content").request().get();
+
+            // Then
+            assertThat(response.getStatus()).isEqualTo(404);
+        }
+
+        @Test
+        void should_return_400_when_item_is_not_a_page() {
+            // Given
+            var folderId = PortalNavigationFixtures.randomNavigationId();
+            var folder = PortalNavigationFixtures.folder(folderId, "Folder", PortalArea.TOP_NAVBAR);
+            folder.setPublished(true);
+            folder.setEnvironmentId(ENV_ID);
+
+            portalNavigationItemsQueryService.initWith(List.of(folder));
+
+            // When
+            Response response = target(folderId.toString()).path("content").request().get();
+
+            // Then
+            assertThat(response.getStatus()).isEqualTo(400);
+        }
+
+        @Test
+        void should_return_404_when_page_content_not_found() {
+            // Given
+            var itemId = PortalNavigationFixtures.randomNavigationId();
+            var contentId = PortalNavigationFixtures.randomPageId();
+
+            var publishedPage = PortalNavigationFixtures.page(itemId, "Published Page", PortalArea.TOP_NAVBAR, contentId);
+            publishedPage.setPublished(true);
+            publishedPage.setEnvironmentId(ENV_ID);
+
+            portalNavigationItemsQueryService.initWith(List.of(publishedPage));
+            portalPageContentQueryService.initWith(List.of()); // No content
+
+            // When
+            Response response = target(itemId.toString()).path("content").request().get();
 
             // Then
             assertThat(response.getStatus()).isEqualTo(404);
