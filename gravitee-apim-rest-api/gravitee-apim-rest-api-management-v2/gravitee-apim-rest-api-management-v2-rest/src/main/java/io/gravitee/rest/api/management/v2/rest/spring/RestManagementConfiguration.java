f23d3a7378eacfc0e916e666719f562604a8db77
Andres Osorio <andres.osorio@graviteesource.com>
2025-12-04 10:02:11 -0500

    feat(analytics): introduce `NamesPostprocessor` to enhance naming in facets and time series responses

--- gravitee-apim-rest-api/gravitee-apim-rest-api-management-v2/gravitee-apim-rest-api-management-v2-rest/src/main/java/io/gravitee/rest/api/management/v2/rest/spring/RestManagementConfiguration.java
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-management-v2/gravitee-apim-rest-api-management-v2-rest/src/main/java/io/gravitee/rest/api/management/v2/rest/spring/RestManagementConfiguration.java
@@ -16,13 +16,13 @@
 package io.gravitee.rest.api.management.v2.rest.spring;
 
 import io.gravitee.apim.core.analytics_engine.domain_service.AnalyticsQueryFilterDecorator;
+import io.gravitee.apim.core.analytics_engine.domain_service.NamesPostprocessor;
 import io.gravitee.apim.infra.domain_service.analytics_engine.permissions.ApiAnalyticsQueryFilterDecoratorImpl;
+import io.gravitee.apim.infra.domain_service.analytics_engine.processors.NamesPostprocessorImpl;
 import io.gravitee.apim.infra.spring.UsecaseSpringConfiguration;
 import io.gravitee.el.ExpressionLanguageInitializer;
 import io.gravitee.rest.api.service.ApplicationService;
-import io.gravitee.rest.api.service.PermissionService;
 import io.gravitee.rest.api.service.spring.ServiceConfiguration;
-import io.gravitee.rest.api.service.v4.ApiAuthorizationService;
 import io.gravitee.rest.api.service.v4.ApiSearchService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -41,6 +41,11 @@ public class RestManagementConfiguration {
     @Bean
     public ExpressionLanguageInitializer expressionLanguageInitializer() {
         return new ExpressionLanguageInitializer();
+    }
+
+    @Bean
+    public NamesPostprocessor namesPostprocessor(ApplicationService applicationSearchService) {
+        return new NamesPostprocessorImpl(applicationSearchService);
     }
 
     @Bean
