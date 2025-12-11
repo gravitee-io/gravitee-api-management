9023a39c401eea73ae82971fe8717fb8bbd5488a
Jaroslaw Lakomy <jaroslaw.lakomy@graviteesource.com>
2025-12-11 14:39:07 +0100

    fix: added missing id_token_hint in SSO logout url https://gravitee.atlassian.net/browse/APIM-8954

--- gravitee-apim-console-webui/src/auth/auth.service.ts
+++ gravitee-apim-console-webui/src/auth/auth.service.ts
@@ -18,7 +18,7 @@ import { HttpClient } from '@angular/common/http';
 import { UserManager, WebStorageStateStore, Log } from 'oidc-client-ts';
 import { Router } from '@angular/router';
 import { from, Observable, of } from 'rxjs';
-import { catchError, map, switchMap } from 'rxjs/operators';
+import {catchError, map, switchMap} from 'rxjs/operators';
 import { LocationStrategy } from '@angular/common';
 
 import { Constants } from '../entities/Constants';
@@ -184,7 +184,9 @@ export class AuthService {
         }
         this.providerIdSelectedStore = null;
 
-        return from([oidcManager.removeUser(), oidcManager.signoutRedirect().catch(() => null), oidcManager.clearStaleState()]);
+        return from(oidcManager.getUser()).pipe(
+          switchMap(user => from([oidcManager.removeUser(), oidcManager.signoutRedirect({ id_token_hint: user.id_token}).catch(() => null), oidcManager.clearStaleState()]))
+        ) ;
       }),
       switchMap(() => {
         if (!options.disableRedirect) {
