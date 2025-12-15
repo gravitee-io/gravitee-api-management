68234cbe3d70cffbcdc434057ddec7781e825e52
jourdiw <jourdi.waller@graviteesource.com>
2025-12-15 11:32:20 +0100

    feat(portal-next): create nav item content viewer component
    (cherry picked from commit f043880ae91b80f6f93f8456d4d6d1eba3bc899c)

---
+++ gravitee-apim-portal-webui-next/src/components/navigation-item-content-viewer/navigation-item-content-viewer.component.spec.ts
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2025 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { HarnessLoader } from '@angular/cdk/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { GraviteeMarkdownViewerHarness } from '@gravitee/gravitee-markdown';
+
+import { NavigationItemContentViewerComponent } from './navigation-item-content-viewer.component';
+import { PortalPageContent } from '../../entities/portal-navigation/portal-page-content';
+
+interface initData {
+  pageContent: PortalPageContent;
+}
+
+describe('NavigationItemContentViewerComponent', () => {
+  let fixture: ComponentFixture<NavigationItemContentViewerComponent>;
+  let loader: HarnessLoader;
+
+  const init = async (data: initData) => {
+    await TestBed.configureTestingModule({
+      imports: [NavigationItemContentViewerComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(NavigationItemContentViewerComponent);
+    loader = TestbedHarnessEnvironment.loader(fixture);
+    fixture.componentRef.setInput('pageContent', data.pageContent);
+
+    fixture.detectChanges();
+  };
+
+  describe('if page content is GRAVITEE_MARKDOWN', () => {
+    const pageContent: PortalPageContent = {
+      type: 'GRAVITEE_MARKDOWN',
+      content: '# Hello World\nThis is a test markdown content.',
+    };
+    beforeEach(async () => {
+      await init({ pageContent });
+    });
+    it('should render markdown viewer', async () => {
+      const markdownViewer = await loader.getHarnessOrNull(GraviteeMarkdownViewerHarness);
+      expect(markdownViewer).not.toBeNull();
+
+      expect(await markdownViewer?.getRenderedHtml()).toContain('<h1 id="hello-world">Hello World</h1>');
+    });
+  });
+});
