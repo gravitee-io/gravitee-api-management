e979e7bb0b448a79bd3f7888b231da789802dcfd
ThibaudAv <thibaud.avenier@gmail.com>
2026-01-08 12:09:41 +0100

    feat(console): add dynamic general column to improve UX with endpoints table

--- gravitee-apim-console-webui/src/management/api/endpoints-v4/endpoint-groups/standard/api-endpoint-groups-standard.adapter.ts
+++ gravitee-apim-console-webui/src/management/api/endpoints-v4/endpoint-groups/standard/api-endpoint-groups-standard.adapter.ts
@@ -15,12 +15,13 @@
  */
 import { get, isEmpty, remove } from 'lodash';
 
-import { ApiV4 } from '../../../../../entities/management-api-v2';
+import { ApiV4, EndpointGroupV4, EndpointV4 } from '../../../../../entities/management-api-v2';
 
 export type EndpointGroup = {
   name: string;
   type: string;
   loadBalancerType: string;
+  generalColumnName?: string;
   endpoints: Endpoint[];
   endpointsDisplayedColumns: string[];
 };
@@ -31,6 +32,7 @@ export type Endpoint = {
     title: string;
     tooltip?: string;
   };
+  general?: string;
   options: {
     class: string;
     tooltip: string;
@@ -53,7 +55,7 @@ const toEndpointsFromApiV4 = (api: ApiV4): EndpointGroup[] => {
 
   return api.endpointGroups.flatMap((endpointGroup, endpointGroupIndex) => {
     const loadBalancerType = endpointGroup?.loadBalancer?.type?.replace(/_/g, ' ');
-    const endpointsDisplayedColumns = ['drag-icon', 'name', 'options', 'weight', 'actions'];
+    const endpointsDisplayedColumns = ['drag-icon', 'name', 'general', 'options', 'weight', 'actions'];
     if (api.type === 'NATIVE') {
       remove(endpointsDisplayedColumns, (o) => o === 'weight');
     }
@@ -63,6 +65,7 @@ const toEndpointsFromApiV4 = (api: ApiV4): EndpointGroup[] => {
       name: endpointGroup.name,
       type: endpointGroup.type,
       loadBalancerType: loadBalancerType ? loadBalancerType.charAt(0).toUpperCase() + loadBalancerType.slice(1).toLowerCase() : '',
+      generalColumnName: toGeneralColumnName(api, endpointGroup),
       endpoints:
         endpointGroup.endpoints && endpointGroup.endpoints.length > 0
           ? endpointGroup.endpoints.map((endpoint, endpointIndex) => {
@@ -73,18 +76,7 @@ const toEndpointsFromApiV4 = (api: ApiV4): EndpointGroup[] => {
                   tooltip: 'The default endpoint used by the API is the first one',
                 };
               }
-              const options = [];
-
-              const groupHealthCheckEnabled = get(endpointGroup, 'services.healthCheck.enabled', false);
-              const endpointHealthCheckEnabled = get(endpoint, 'services.healthCheck.enabled', false);
-              if (groupHealthCheckEnabled || endpointHealthCheckEnabled)
-                options.push({
-                  class: 'gio-badge-neutral',
-                  tooltip: endpointHealthCheckEnabled
-                    ? 'Health check enabled by endpoint configuration'
-                    : 'Health check enabled via inherited group configuration',
-                  textContent: 'Health Check',
-                });
+              const options = toEndpointOptions(api, endpointGroup, endpoint);
 
               if (!isEmpty(options)) {
                 hasOptions = true;
@@ -92,6 +84,7 @@ const toEndpointsFromApiV4 = (api: ApiV4): EndpointGroup[] => {
 
               return {
                 name: endpoint.name,
+                general: toGeneralInfo(api, endpoint),
                 nameBadge,
                 options,
                 weight: endpoint.weight,
@@ -106,9 +99,74 @@ const toEndpointsFromApiV4 = (api: ApiV4): EndpointGroup[] => {
       remove(endpointsDisplayedColumns, (o) => o === 'options');
     }
 
+    if (isEmpty(partialEndpointGroup.generalColumnName)) {
+      remove(endpointsDisplayedColumns, (o) => o === 'general');
+    }
+
     return {
       ...partialEndpointGroup,
       endpointsDisplayedColumns,
     } satisfies EndpointGroup;
   });
+};
+
+const toGeneralColumnName = (api: ApiV4, endpointGroup: EndpointGroupV4): string | undefined => {
+  switch (api.type + '.' + endpointGroup.type) {
+    case 'PROXY.http-proxy':
+      return 'Target URL';
+    case 'NATIVE.native-kafka':
+      return 'Bootstrap Servers';
+    case 'MESSAGE.kafka':
+      return 'Bootstrap Servers';
+  }
+};
+
+const toGeneralInfo = (api: ApiV4, endpoint: EndpointV4): string | undefined => {
+  switch (api.type + '.' + endpoint.type) {
+    case 'PROXY.http-proxy':
+      return get(endpoint.configuration, 'target', '');
+    case 'NATIVE.native-kafka':
+      return get(endpoint.configuration, 'bootstrapServers', '');
+    case 'MESSAGE.kafka':
+      return get(endpoint.configuration, 'bootstrapServers', '');
+  }
+};
+
+const toEndpointOptions = (api: ApiV4, endpointGroup: EndpointGroupV4, endpoint: EndpointV4): Endpoint['options'] => {
+  switch (api.type + '.' + endpoint.type) {
+    case 'PROXY.http-proxy': {
+      const groupHealthCheckEnabled = get(endpointGroup, 'services.healthCheck.enabled', false);
+      const endpointHealthCheckEnabled = get(endpoint, 'services.healthCheck.enabled', false);
+      if (groupHealthCheckEnabled || endpointHealthCheckEnabled) {
+        return [
+          {
+            class: 'gio-badge-neutral',
+            tooltip: endpointHealthCheckEnabled
+              ? 'Health check enabled by endpoint configuration'
+              : 'Health check enabled via inherited group configuration',
+            textContent: 'Health Check',
+          },
+        ];
+      }
+      return [];
+    }
+    case 'NATIVE.native-kafka': {
+      const groupSecurityProtocol = get(endpointGroup.sharedConfiguration, 'security.protocol');
+      const endpointSecurityProtocol = endpoint.inheritConfiguration
+        ? undefined
+        : get(endpoint.sharedConfigurationOverride, 'security.protocol');
+      if (groupSecurityProtocol || endpointSecurityProtocol) {
+        return [
+          {
+            class: 'gio-badge-neutral',
+            tooltip: endpointSecurityProtocol
+              ? `Security protocol override by endpoint configuration`
+              : 'Security protocol inherited from group configuration',
+            textContent: endpointSecurityProtocol ?? groupSecurityProtocol,
+          },
+        ];
+      }
+    }
+  }
+  return [];
 };
