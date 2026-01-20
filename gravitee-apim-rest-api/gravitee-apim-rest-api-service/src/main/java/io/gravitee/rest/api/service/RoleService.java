b9ac6daf75ef75fca816ce6d3feb7f8ff4a53f58
ThibaudAv <thibaud.avenier@gmail.com>
2026-01-20 20:54:03 +0100

    fix: implement batch retrieval of roles by IDs in RoleService
    (cherry picked from commit f5749343e8104fa04fbebe21da794af771d099b8)

--- gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/rest/api/service/RoleService.java
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/rest/api/service/RoleService.java
@@ -44,6 +44,7 @@ public interface RoleService {
     void delete(ExecutionContext executionContext, String roleId);
     List<RoleEntity> findAllByOrganization(String organizationId);
     RoleEntity findById(String roleId);
+    Map<String, RoleEntity> findByIds(Set<String> roleIds);
     Optional<RoleEntity> findByIdAndOrganizationId(String roleId, String organizationId);
     List<RoleEntity> findByScope(RoleScope scope, String organizationId);
     Optional<RoleEntity> findByScopeAndName(RoleScope scope, String name, String organizationId);
