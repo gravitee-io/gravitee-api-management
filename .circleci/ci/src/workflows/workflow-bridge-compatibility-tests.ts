becde967c223cf39150bafaae0d5c6a62e3ab254
Florent CHAMFROY <florent.chamfroy@graviteesource.com>
2026-01-26 09:35:58 +0100

    ci(bridge): update bridge compatibility tests

--- .circleci/ci/src/workflows/workflow-bridge-compatibility-tests.ts
+++ .circleci/ci/src/workflows/workflow-bridge-compatibility-tests.ts
@@ -66,7 +66,7 @@ export class BridgeCompatibilityTestsWorkflow {
             'graviteeio@4.8.0',
             '4.7.x-latest',
             'graviteeio@4.7.0',
-            '4.6.x-latest',
+            'graviteeio@4.6',
             'graviteeio@4.6.0',
             'graviteeio@4.5',
             'graviteeio@4.5.0',
