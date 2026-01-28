09fee46c79a830537f10617e41efe74aa9ca8ba2
gaurav-gravitee <gaurav.sharma@graviteesource.com>
2026-01-28 18:05:03 +0530

    Update NewHttpApi.java

--- gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/apim/core/api/model/NewHttpApi.java
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/apim/core/api/model/NewHttpApi.java
@@ -52,11 +52,11 @@ public class NewHttpApi extends AbstractNewApi {
     private Failover failover;
 
     /**
-     * @return An instance of {@link io.gravitee.definition.model.v4.Api.ApiBuilder} based on the current state of this NewV4Api.
+     * @return An instance of {@link io.gravitee.definition.model.v4.Api.ApiBuilder} based on the current state of this NewHttpApi.
      */
     public io.gravitee.definition.model.v4.Api.ApiBuilder<?, ?> toApiDefinitionBuilder() {
         // Currently we can't use MapStruct in core. We will need to discuss as team if we want to introduce a rule to allow MapStruct in core.
-        return io.gravitee.definition.model.v4.Api.builder()
+        io.gravitee.definition.model.v4.Api.ApiBuilder<?, ?> builder = io.gravitee.definition.model.v4.Api.builder()
             .name(name)
             .type(type)
             .apiVersion(apiVersion)
@@ -68,5 +68,12 @@ public class NewHttpApi extends AbstractNewApi {
             .flows(flows)
             .flowExecution(flowExecution)
             .failover(failover);
+
+        // Business rule: new V4 HTTP Proxy APIs should be allowed in API Products by default.
+        if (type == io.gravitee.definition.model.v4.ApiType.PROXY) {
+            builder.allowInApiProduct(true);
+        }
+
+        return builder;
     }
 }
