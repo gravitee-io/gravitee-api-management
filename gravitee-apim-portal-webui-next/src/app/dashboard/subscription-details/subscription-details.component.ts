09a28fba93cb238ab4674badc409cd83e0e1c9f3
Ivan Kovarin <ivan.kovarin@graviteesource.com>
2026-01-29 16:58:25 +0100

    WIP feat: view portal subscriptions - details.
    https://gravitee.atlassian.net/browse/APIM-12270

--- gravitee-apim-portal-webui-next/src/app/dashboard/subscription-details/subscription-details.component.ts
+++ gravitee-apim-portal-webui-next/src/app/dashboard/subscription-details/subscription-details.component.ts
@@ -1,22 +1,72 @@
-import {Component, inject} from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
-import {MatProgressBar} from "@angular/material/progress-bar";
-import {MatIcon} from "@angular/material/icon";
-import {MatButton} from "@angular/material/button";
+/*
+ * Copyright (C) 2026 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import {Component, inject, input} from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { MatProgressBar } from '@angular/material/progress-bar';
+import { ActivatedRoute } from '@angular/router';
+import {PlanSecurityEnum, PlanUsageConfiguration} from "../../../entities/plan/plan";
+import {
+  SubscriptionConsumerConfiguration,
+  SubscriptionConsumerStatusEnum,
+  SubscriptionStatusEnum
+} from "../../../entities/subscription";
+import {ApiType} from "../../../entities/api/api";
+
+interface SubscriptionDetailsVM {
+  result?: SubscriptionDetailsData;
+  error?: boolean;
+}
+
+interface SubscriptionDetailsData {
+  applicationName: string;
+  planName: string;
+  planSecurity: PlanSecurityEnum;
+  planUsageConfiguration: PlanUsageConfiguration;
+  subscriptionStatus: SubscriptionStatusEnum;
+  consumerStatus: SubscriptionConsumerStatusEnum;
+  failureCause?: string;
+  createdAt?: string;
+  updatedAt?: string;
+  apiKey?: string;
+  apiKeyConfigUsername?: string;
+  entrypointUrls?: string[];
+  clientId?: string;
+  clientSecret?: string;
+  apiType?: ApiType;
+  consumerConfiguration?: SubscriptionConsumerConfiguration;
+}
 
 @Component({
   selector: 'app-subscription-details',
-  imports: [
-    MatProgressBar,
-    MatIcon,
-    MatButton,
-  ],
+  imports: [MatProgressBar, MatIcon, MatButton],
   templateUrl: './subscription-details.component.html',
   styleUrl: './subscription-details.component.scss',
 })
 export default class SubscriptionDetailsComponent {
   private route = inject(ActivatedRoute);
 
+  subscription = input.required<SubscriptionDetailsVM>();
+
   // Get the ID from the URL
   subscriptionId = this.route.snapshot.paramMap.get('subscriptionId');
+
+  copyToClipboard() {
+    navigator.clipboard.writeText(this.subscriptionId!);
+    alert('copied!')
+    // Optional: Show a snackbar "Copied!"
+  }
 }
