18197855fb2b1594856bb2d54af59e98c3156986
ThibaudAv <thibaud.avenier@gmail.com>
2026-02-10 06:05:27 +0100

    feat: clean and make test, build, lint, storybook, prettier, license pass - Step 5

---
+++ gravitee-apim-webui-libs/gravitee-dashboard/jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  displayName: 'gravitee-dashboard',
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  coverageDirectory: '../../coverage/gravitee-dashboard',
+  transform: {
+    '^.+\\.(ts|mjs|js|html)$': [
+      'jest-preset-angular',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        stringifyContentPathRegex: '\\.(html|svg)$',
+      },
+    ],
+  },
+  transformIgnorePatterns: ['/node_modules/(?!(.*\\.mjs$)|lodash-es|(@gravitee/.*?\\.js)$)'],
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
+  ],
+};
