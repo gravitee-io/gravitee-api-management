f8e49d6a8aa926ef9a9420ef08792c0945456d43
/dev/marek <164865929+dev-marek@users.noreply.github.com>
2026-02-17 09:14:05 +0100

    fix: replace hardcoded SVG icons with a dynamic icon retrieval function and remove root API create (#15161)
    https://gravitee.atlassian.net/browse/APIM-12541

---
+++ gravitee-apim-console-webui/src/portal/icon/portal-navigation-item-icon.pipe.spec.ts
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { TestBed } from '@angular/core/testing';
+
+import { PortalNavigationItemIconPipe } from './portal-navigation-item-icon.pipe';
+
+describe('PortalNavigationItemIconPipe', () => {
+  let pipe: PortalNavigationItemIconPipe;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [PortalNavigationItemIconPipe],
+    });
+
+    pipe = TestBed.inject(PortalNavigationItemIconPipe);
+  });
+
+  it.each([
+    ['PAGE', 'gio:page'],
+    ['LINK', 'gio:link'],
+    ['FOLDER', 'gio:folder'],
+    ['API', 'gio:folder-api'],
+  ] as const)('should map %s to %s', (type, expected) => {
+    expect(pipe.transform(type)).toBe(expected);
+  });
+});
