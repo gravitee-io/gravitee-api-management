6d9e25fd7160fb1e98dcc3c061c9eb7e6782ec7a
ThibaudAv <thibaud.avenier@gmail.com>
2026-02-17 15:50:56 +0100

    feat: init Kafka Explorer nx library

---
+++ gravitee-apim-webui-libs/gravitee-kafka-explorer/jest.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  displayName: 'gravitee-kafka-explorer',
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  coverageDirectory: 'coverage',
+  transform: {
+    '^.+\\.(ts|mjs|js|html)$': [
+      'jest-preset-angular',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        stringifyContentPathRegex: '\\.(html|svg)$',
+      },
+    ],
+  },
+  transformIgnorePatterns: ['/node_modules/(?!(.*\\.mjs$)|lodash-es|(@gravitee/.*?\\.js)$)'],
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
+  ],
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      {
+        outputDirectory: __dirname + '/coverage',
+        outputName: 'junit.xml',
+      },
+    ],
+  ],
+};
