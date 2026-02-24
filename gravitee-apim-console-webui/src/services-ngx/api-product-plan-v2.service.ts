aa73078b78aeb56ea2154ef0273be6a1f291513f
pragatityagi <pragati.tyagi@graviteesource.com>
2026-02-24 23:03:04 +0530

    feat: Added API product plan list, create & update plans

---
+++ gravitee-apim-console-webui/src/services-ngx/api-product-plan-v2.service.ts
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2026 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { Constants } from '../entities/Constants';
+import { ApiPlansResponse, CreatePlan, Plan, PlanMode, PlanStatus, UpdatePlan } from '../entities/management-api-v2';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiProductPlanV2Service {
+  private readonly http = inject(HttpClient);
+  private readonly constants = inject<Constants>(Constants);
+
+  list(
+    apiProductId: string,
+    securities?: string[],
+    statuses?: PlanStatus[],
+    mode?: PlanMode,
+    fields?: ('flow' | '-flow')[],
+    page = 1,
+    perPage = 10,
+  ): Observable<ApiPlansResponse> {
+    return this.http.get<ApiPlansResponse>(`${this.constants.env.v2BaseURL}/api-products/${apiProductId}/plans`, {
+      params: {
+        page,
+        perPage,
+        ...(securities ? { securities: securities.join(',') } : {}),
+        ...(statuses ? { statuses: statuses.join(',') } : {}),
+        ...(mode ? { mode } : {}),
+        ...(fields ? { fields: fields.join(',') } : {}),
+      },
+    });
+  }
+
+  public create(apiProductId: string, plan: CreatePlan): Observable<Plan> {
+    return this.http.post<Plan>(`${this.constants.env.v2BaseURL}/api-products/${apiProductId}/plans`, plan);
+  }
+
+  public get(apiProductId: string, planId: string): Observable<Plan> {
+    return this.http.get<Plan>(`${this.constants.env.v2BaseURL}/api-products/${apiProductId}/plans/${planId}`);
+  }
+
+  public update(apiProductId: string, planId: string, plan: UpdatePlan): Observable<Plan> {
+    return this.http.put<Plan>(`${this.constants.env.v2BaseURL}/api-products/${apiProductId}/plans/${planId}`, plan);
+  }
+
+  public publish(apiProductId: string, planId: string): Observable<Plan> {
+    return this.http.post<Plan>(`${this.constants.env.v2BaseURL}/api-products/${apiProductId}/plans/${planId}/_publish`, {});
+  }
+
+  public deprecate(apiProductId: string, planId: string): Observable<Plan> {
+    return this.http.post<Plan>(`${this.constants.env.v2BaseURL}/api-products/${apiProductId}/plans/${planId}/_deprecate`, {});
+  }
+
+  public close(apiProductId: string, planId: string): Observable<Plan> {
+    return this.http.post<Plan>(`${this.constants.env.v2BaseURL}/api-products/${apiProductId}/plans/${planId}/_close`, {});
+  }
+}
