4ef3febab13cdf8d2f6b7442073317c20c30bd62
jourdiw <jourdi.waller@graviteesource.com>
2026-02-25 17:07:50 +0100

    feat(console): choose default nav id of folder when only folder and no content in nav items

--- gravitee-apim-console-webui/src/portal/navigation-items/portal-navigation-items.component.ts
+++ gravitee-apim-console-webui/src/portal/navigation-items/portal-navigation-items.component.ts
@@ -680,6 +680,7 @@ export function findFirstAvailablePage(
       if (element.type === 'FOLDER') {
         const found = search(element);
         if (found) return found;
+        return element;
       }
     }
     return null;
