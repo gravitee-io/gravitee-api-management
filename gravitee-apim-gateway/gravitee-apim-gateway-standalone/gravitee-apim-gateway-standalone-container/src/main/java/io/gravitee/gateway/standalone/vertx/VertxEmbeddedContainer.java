89ad06d49c424b50922ffd1e2ad67c1970f5f297
Wojciech Baszczyk <wojciech.baszczyk@graviteesource.com>
2026-02-26 14:02:52 +0100

    fix(gateway): improve performance by reducing http and tcp verticle per cpu core

--- gravitee-apim-gateway/gravitee-apim-gateway-standalone/gravitee-apim-gateway-standalone-container/src/main/java/io/gravitee/gateway/standalone/vertx/VertxEmbeddedContainer.java
+++ gravitee-apim-gateway/gravitee-apim-gateway-standalone/gravitee-apim-gateway-standalone-container/src/main/java/io/gravitee/gateway/standalone/vertx/VertxEmbeddedContainer.java
@@ -21,7 +21,7 @@ import io.gravitee.gateway.reactive.standalone.vertx.TcpProtocolVerticle;
 import io.gravitee.node.vertx.verticle.factory.SpringVerticleFactory;
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Vertx;
-import io.vertx.core.VertxOptions;
+import io.vertx.core.impl.cpu.CpuCoreSensor;
 import lombok.CustomLog;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -59,8 +59,8 @@ public class VertxEmbeddedContainer extends AbstractLifecycleComponent<VertxEmbe
 
     @Override
     protected void doStart() {
-        httpInstances = (httpInstances < 1) ? VertxOptions.DEFAULT_EVENT_LOOP_POOL_SIZE : httpInstances;
-        tcpInstances = (tcpInstances < 1) ? VertxOptions.DEFAULT_EVENT_LOOP_POOL_SIZE : tcpInstances;
+        httpInstances = (httpInstances < 1) ? CpuCoreSensor.availableProcessors() : httpInstances;
+        tcpInstances = (tcpInstances < 1) ? CpuCoreSensor.availableProcessors() : tcpInstances;
         startHttpInstances();
         startTcpInstances();
     }
