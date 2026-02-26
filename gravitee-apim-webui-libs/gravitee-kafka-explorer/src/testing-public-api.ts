80a1b0121f1d3ec38978dc7302bdd240567a796c
ThibaudAv <thibaud.avenier@gmail.com>
2026-02-26 09:30:10 +0100

    refactor(kafka-explorer): isolate test utilities in secondary entry point /testing
    Move harnesses and fixtures out of the production public-api into a
    dedicated @gravitee/gravitee-kafka-explorer/testing secondary entry point
    so they are not compiled into the main app bundle.

---
+++ gravitee-apim-webui-libs/gravitee-kafka-explorer/src/testing-public-api.ts
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2026 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/*
+ * Public API Surface of gravitee-kafka-explorer/testing
+ */
+
+export * from './lib/kafka-explorer/kafka-explorer.harness';
+export * from './lib/brokers/brokers.harness';
+export * from './lib/topics/topics.harness';
+export * from './lib/topic-detail/topic-detail.harness';
+export * from './lib/models/kafka-cluster.fixture';
