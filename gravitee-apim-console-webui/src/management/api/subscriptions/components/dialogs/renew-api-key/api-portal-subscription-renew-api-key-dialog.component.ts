97bd650e180dddcc765ab70d0bfdaceb60582b35
Elamrani Azize <azize.elamrani@gmail.com>
2026-02-27 09:54:26 +0100

    fix: update gravitee-apim-console-webui/src/management/api/subscriptions/components/dialogs/renew-api-key/api-portal-subscription-renew-api-key-dialog.component.ts (workflow 6cf9f743-5efb-49f5-b87a-7b4d27e4a249)

---
+++ gravitee-apim-console-webui/src/management/api/subscriptions/components/dialogs/renew-api-key/api-portal-subscription-renew-api-key-dialog.component.ts
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { UntypedFormControl, UntypedFormGroup } from '@angular/forms';
+
+export interface ApiPortalSubscriptionRenewApiKeyDialogData {
+  customApiKeyAllowed?: boolean;
+}
+
+export interface ApiPortalSubscriptionRenewApiKeyDialogResult {
+  confirmed: boolean;
+  customApiKey?: string;
+}
+
+@Component({
+  selector: 'api-portal-subscription-renew-api-key-dialog',
+  templateUrl: './api-portal-subscription-renew-api-key-dialog.component.html',
+  styleUrls: ['./api-portal-subscription-renew-api-key-dialog.component.scss'],
+})
+export class ApiPortalSubscriptionRenewApiKeyDialogComponent {
+  customApiKeyAllowed: boolean = false;
+  form: UntypedFormGroup;
+
+  constructor(
+    private readonly dialogRef: MatDialogRef<ApiPortalSubscriptionRenewApiKeyDialogComponent, ApiPortalSubscriptionRenewApiKeyDialogResult>,
+    @Inject(MAT_DIALOG_DATA) dialogData: ApiPortalSubscriptionRenewApiKeyDialogData,
+  ) {
+    this.customApiKeyAllowed = dialogData?.customApiKeyAllowed ?? false;
+    this.form = new UntypedFormGroup({
+      customApiKey: new UntypedFormControl(''),
+    });
+  }
+
+  onRenew() {
+    const customApiKey = this.customApiKeyAllowed ? this.form.get('customApiKey')?.value : undefined;
+    this.dialogRef.close({ confirmed: true, customApiKey: customApiKey || undefined });
+  }
+
+  onCancel() {
+    this.dialogRef.close();
+  }
+}
