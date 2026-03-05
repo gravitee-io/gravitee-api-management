d72325d59d0f26219de7caa34b4d93d207ecd11f
jourdiw <jourdi.waller@graviteesource.com>
2026-03-05 14:07:14 +0000

    fix(gamma): serve angular app-beta successfully

--- app_beta/src/app/remote-entry/mount.ts
+++ app_beta/src/app/remote-entry/mount.ts
@@ -1,10 +1,23 @@
 import { createApplication } from '@angular/platform-browser';
 import { appConfig } from '../app.config';
 import { RemoteEntry } from './entry';
-import { ApplicationRef } from '@angular/core';
+import { ApplicationRef, Provider } from '@angular/core';
+import { APP_BASE_HREF } from '@angular/common';
 
-export async function mount(hostElement: HTMLElement): Promise<() => void> {
-    const appRef: ApplicationRef = await createApplication(appConfig);
+interface MountOptions {
+    basePath?: string;
+}
+
+export async function mount(hostElement: HTMLElement, options?: MountOptions): Promise<() => void> {
+    const extraProviders: Provider[] = [];
+    if (options?.basePath) {
+        extraProviders.push({ provide: APP_BASE_HREF, useValue: options.basePath });
+    }
+
+    const appRef: ApplicationRef = await createApplication({
+        ...appConfig,
+        providers: [...appConfig.providers, ...extraProviders],
+    });
     const compRef = appRef.bootstrap(RemoteEntry, hostElement);
 
     return () => {
--- gravitee-gamma/module-federation.config.ts
+++ gravitee-gamma/module-federation.config.ts
@@ -14,7 +14,7 @@ const config: ModuleFederationConfig = {
      * declare module 'my-external-remote';
      *
      */
-    remotes: ['app_alpha', 'app_beta'],
+    remotes: ['app_alpha'],
 };
 
 /**
