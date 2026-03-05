dd46a33d5ec0c28f9fc095e9531e8bb9803ede64
aurelien-pacaud <aurelien.pacaud@graviteesource.com>
2026-03-05 10:16:08 +0100

    feat: introduce TagKeyUpgrader to migrate tags with new key-based model
    handle migration of tags, replacing the use of `id` with the `key` field and generating new UUIDs for tag IDs.

---
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/rest/api/service/impl/upgrade/upgrader/TagKeyUpgrader.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright © 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.rest.api.service.impl.upgrade.upgrader;
+
+import io.gravitee.common.utils.UUID;
+import io.gravitee.node.api.upgrader.Upgrader;
+import io.gravitee.repository.exceptions.TechnicalException;
+import io.gravitee.repository.management.api.TagRepository;
+import io.gravitee.repository.management.model.Tag;
+import lombok.CustomLog;
+import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author GraviteeSource Team
+ */
+@CustomLog
+@Component
+public class TagKeyUpgrader implements Upgrader {
+
+    @Lazy
+    @Autowired
+    private TagRepository tagRepository;
+
+    @SneakyThrows
+    @Override
+    public boolean upgrade() {
+        try {
+            tagRepository
+                .findAll()
+                .forEach(tag -> {
+                    log.info("Migrating sharding tag {}", tag.getName());
+
+                    tag.setKey(tag.getId());
+
+                    try {
+                        tagRepository.update(tag);
+                    } catch (TechnicalException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+            return true;
+        } catch (Exception ex) {
+            log.error("Failed to migrate sharding tags", ex);
+            return false;
+        }
+    }
+
+    @Override
+    public int getOrder() {
+        return UpgraderOrder.TAG_KEY_UPGRADER;
+    }
+}
---
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/test/java/io/gravitee/rest/api/service/impl/upgrade/upgrader/TagKeyUpgraderTest.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright © 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.rest.api.service.impl.upgrade.upgrader;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import io.gravitee.repository.management.api.TagRepository;
+import io.gravitee.repository.management.model.Tag;
+import io.gravitee.repository.management.model.TagReferenceType;
+import java.util.List;
+import java.util.Set;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+/**
+ * @author GraviteeSource Team
+ */
+@ExtendWith(MockitoExtension.class)
+class TagKeyUpgraderTest {
+
+    @Mock
+    private TagRepository tagRepository;
+
+    @InjectMocks
+    private TagKeyUpgrader upgrader;
+
+    @Test
+    void shouldMigrateTags() throws Exception {
+        var tag = Tag.builder()
+            .id("tag-key")
+            .key(null)
+            .name("Tag Name")
+            .description("description")
+            .restrictedGroups(List.of())
+            .referenceId("DEFAULT")
+            .referenceType(TagReferenceType.ORGANIZATION)
+            .build();
+
+        when(tagRepository.findAll()).thenReturn(Set.of(tag));
+        assertThat(upgrader.upgrade()).isTrue();
+
+        var tagCaptor = ArgumentCaptor.forClass(Tag.class);
+        verify(tagRepository).update(tagCaptor.capture());
+
+        var migratedTag = tagCaptor.getValue();
+        assertThat(migratedTag.getKey()).isEqualTo("tag-key");
+        assertThat(migratedTag.getId()).isEqualTo("tag-key");
+        assertThat(migratedTag.getName()).isEqualTo("Tag Name");
+    }
+
+    @Test
+    void shouldReturnFalseOnException() throws Exception {
+        when(tagRepository.findAll()).thenThrow(new RuntimeException("error"));
+        assertThat(upgrader.upgrade()).isFalse();
+    }
+}
