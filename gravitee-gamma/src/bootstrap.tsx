7aa63ae7af8123542ae26f2b8f9faa0961990317
jourdiw <jourdi.waller@graviteesource.com>
2026-03-05 15:48:24 +0000

    fix(gamma): import styles

--- gravitee-gamma/src/bootstrap.tsx
+++ gravitee-gamma/src/bootstrap.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import './styles.css';
 import App from './app/app';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
