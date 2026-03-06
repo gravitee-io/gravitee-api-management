d478d93fe59e2debd4c8edfc7b7c12cd5ce6dfc8
aurelien-pacaud <aurelien.pacaud@graviteesource.com>
2026-03-06 11:31:37 +0100

    feat: add TenantKeyUpgrader to migrate tenants with new key-based identifiers

---
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/rest/api/service/impl/upgrade/upgrader/TenantKeyUpgrader.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright © 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.rest.api.service.impl.upgrade.upgrader;
+
+import io.gravitee.node.api.upgrader.Upgrader;
+import io.gravitee.repository.management.api.TenantRepository;
+import java.util.UUID;
+import lombok.CustomLog;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author GraviteeSource Team
+ */
+@CustomLog
+@Component
+public class TenantKeyUpgrader implements Upgrader {
+
+    @Lazy
+    @Autowired
+    private TenantRepository tenantRepository;
+
+    @Override
+    public boolean upgrade() {
+        try {
+            tenantRepository
+                .findAll()
+                .forEach(tenant -> {
+                    log.info("Migrate tenant {}", tenant.getName());
+                    tenant.setKey(tenant.getId());
+                    try {
+                        tenantRepository.update(tenant);
+                    } catch (Exception e) {
+                        log.error("Unable to upgrade tenant {}", tenant.getId());
+                        throw new RuntimeException(e);
+                    }
+                });
+            return true;
+        } catch (Exception e) {
+            log.error("Unable to upgrade tenants", e);
+            return false;
+        }
+    }
+
+    @Override
+    public int getOrder() {
+        return UpgraderOrder.TENANT_KEY_UPGRADER;
+    }
+}
