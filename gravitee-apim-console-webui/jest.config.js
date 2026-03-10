b978c45653adb08efddf44dc56d61c933ba230c7
jourdiw <jourdi.waller@graviteesource.com>
2026-03-10 11:42:19 +0100

    fix(test): include coverage in ui testing with nx

--- gravitee-apim-console-webui/jest.config.js
+++ gravitee-apim-console-webui/jest.config.js
@@ -3,7 +3,20 @@ process.env.TZ = 'UTC';
 module.exports = {
   testTimeout: 30000,
   preset: 'jest-preset-angular',
+  roots: [__dirname + '/src'],
   setupFilesAfterEnv: [__dirname + '/src/setup-jest.ts'],
+  collectCoverageFrom: [
+    'src/**/*.{ts,html}',
+    '!src/**/*.spec.{ts,tsx}',
+    '!src/**/*.module.ts',
+    '!src/index.ts',
+    '!src/**/index.ts',
+    '!src/environments/**',
+    '!src/**/*.stories.ts',
+    '!src/**/__mocks__/**',
+    '!src/**/*.harness.ts',
+  ],
+  coverageDirectory: __dirname + '/coverage',
   transformIgnorePatterns: [
     '/node_modules/(?!(.*\\.mjs$)|(@gravitee/ui-components/.*?\\.js)|lodash-es||lit|@lit/reactive-element|(lit-element/.*?\\.js)|(lit-html/.*?\\.js)|(resize-observer-polyfill/.*?\\.js)|(date-fns/.*?\\.js)$)',
   ],
