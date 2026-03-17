7396f98e1d50961e35c58ca2495d9bc7fd328ebd
ThibaudAv <thibaud.avenier@gmail.com>
2026-03-17 10:18:40 +0100

    feat(rest-api): add GET /organizations/{orgId}/users/{userId}/groups endpoint
    (cherry picked from commit 60e53cb1b6a2518aff5fd406125debe58c42a3b1)

--- gravitee-apim-rest-api/gravitee-apim-rest-api-management-v2/gravitee-apim-rest-api-management-v2-rest/src/test/java/io/gravitee/rest/api/management/v2/rest/spring/ResourceContextConfiguration.java
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-management-v2/gravitee-apim-rest-api-management-v2-rest/src/test/java/io/gravitee/rest/api/management/v2/rest/spring/ResourceContextConfiguration.java
@@ -155,6 +155,7 @@ import io.gravitee.apim.core.subscription.use_case.RejectSubscriptionUseCase;
 import io.gravitee.apim.core.user.domain_service.UserDomainService;
 import io.gravitee.apim.core.user.use_case.GetUserApisUseCase;
 import io.gravitee.apim.core.user.use_case.GetUserApplicationsUseCase;
+import io.gravitee.apim.core.user.use_case.GetUserGroupsUseCase;
 import io.gravitee.apim.infra.adapter.SubscriptionAdapter;
 import io.gravitee.apim.infra.adapter.SubscriptionAdapterImpl;
 import io.gravitee.apim.infra.domain_service.analytics_engine.definition.AnalyticsDefinitionYAMLQueryService;
@@ -1035,5 +1036,10 @@ public class ResourceContextConfiguration {
     @Bean
     public GetUserApplicationsUseCase getUserApplicationsUseCase() {
         return mock(GetUserApplicationsUseCase.class);
+    }
+
+    @Bean
+    public GetUserGroupsUseCase getUserGroupsUseCase() {
+        return mock(GetUserGroupsUseCase.class);
     }
 }
