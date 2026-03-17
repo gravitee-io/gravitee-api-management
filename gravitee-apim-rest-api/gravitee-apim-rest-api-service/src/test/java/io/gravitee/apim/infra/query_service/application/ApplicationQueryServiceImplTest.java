006dfe1d5b16727edcb5627417009e475ed8b7c1
ThibaudAv <thibaud.avenier@gmail.com>
2026-03-17 10:17:43 +0100

    feat(rest-api): add GET /organizations/{orgId}/users/{userId}/applications endpoint
    (cherry picked from commit 644b731d60d72b95adc06028de7da508197620f0)
    (cherry picked from commit 70d652ce487098c80ca4e9078eb0622166efe6a4)

--- gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/test/java/io/gravitee/apim/infra/query_service/application/ApplicationQueryServiceImplTest.java
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/test/java/io/gravitee/apim/infra/query_service/application/ApplicationQueryServiceImplTest.java
@@ -17,15 +17,20 @@ package io.gravitee.apim.infra.query_service.application;
 
 import static assertions.CoreAssertions.assertThat;
 import static org.assertj.core.api.Assertions.catchThrowable;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 import fixtures.core.model.ApplicationFixture;
 import io.gravitee.apim.infra.adapter.ApplicationAdapter;
+import io.gravitee.common.data.domain.Page;
 import io.gravitee.repository.exceptions.TechnicalException;
 import io.gravitee.repository.management.api.ApplicationRepository;
+import io.gravitee.repository.management.model.Application;
 import io.gravitee.repository.management.model.ApplicationStatus;
 import io.gravitee.rest.api.model.BaseApplicationEntity;
+import io.gravitee.rest.api.model.common.PageableImpl;
 import io.gravitee.rest.api.service.exceptions.TechnicalManagementException;
+import java.util.List;
 import java.util.Set;
 import lombok.SneakyThrows;
 import org.assertj.core.api.SoftAssertions;
@@ -44,6 +49,45 @@ class ApplicationQueryServiceImplTest {
 
     @InjectMocks
     ApplicationQueryServiceImpl service;
+
+    @Nested
+    class SearchByIds {
+
+        @Test
+        @SneakyThrows
+        void should_return_paginated_applications_matching_ids() {
+            var app = ApplicationFixture.anApplication();
+            when(applicationRepository.search(any(), any())).thenReturn(new Page<>(List.of(app), 1, 10, 1));
+
+            var result = service.searchByIds(Set.of("app-id"), null, new PageableImpl(1, 10));
+
+            assertThat(result.getTotalElements()).isEqualTo(1);
+            assertThat(result.getContent()).hasSize(1);
+            assertThat(result.getContent().get(0).getId()).isEqualTo("app-id");
+        }
+
+        @Test
+        @SneakyThrows
+        void should_return_empty_page_when_no_match() {
+            when(applicationRepository.search(any(), any())).thenReturn(new Page<>(List.of(), 1, 10, 0));
+
+            var result = service.searchByIds(Set.of("unknown"), null, new PageableImpl(1, 10));
+
+            assertThat(result.getTotalElements()).isZero();
+            assertThat(result.getContent()).isEmpty();
+        }
+
+        @Test
+        void should_throw_when_technical_exception_occurs() throws TechnicalException {
+            when(applicationRepository.search(any(), any())).thenThrow(TechnicalException.class);
+
+            Throwable throwable = catchThrowable(() -> service.searchByIds(Set.of("app-id"), null, new PageableImpl(1, 10)));
+
+            assertThat(throwable)
+                .isInstanceOf(TechnicalManagementException.class)
+                .hasMessage("An error occurred while searching applications by ids");
+        }
+    }
 
     @Nested
     class FindByEnvironmentId {
