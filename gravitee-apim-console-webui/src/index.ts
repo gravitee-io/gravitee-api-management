2793768f7a8d3600efdda0aabae63bfb9d73c993
ThibaudAv <thibaud.avenier@gmail.com>
2026-03-26 12:31:50 +0100

    fix: send cookies with fetch requests to fix CSRF token mismatch
    Add credentials: 'include' to fetch requestConfig so cookies are
    sent cross-origin, preventing CSRF token regeneration on parallel
    requests. Store CSRF token from bootstrap response via CsrfInterceptor.

--- gravitee-apim-console-webui/src/index.ts
+++ gravitee-apim-console-webui/src/index.ts
@@ -26,9 +26,11 @@ import { Build, Constants, DefaultPortal } from './entities/Constants';
 import { getFeatureInfoData } from './shared/components/gio-license/gio-license-data';
 import { ConsoleCustomization } from './entities/management-api-v2/consoleCustomization';
 import { environment } from './environments/environment';
+import { CsrfInterceptor } from './shared/interceptors/csrf.interceptor';
 
 const requestConfig: RequestInit = {
   headers: { 'Cache-Control': 'no-cache', Pragma: 'no-cache' },
+  credentials: 'include',
 };
 
 // fix angular-schema-form angular<1.7
@@ -61,7 +63,8 @@ function fetchData(): Promise<{ constants: Constants; build: any }> {
         enforcedOrganizationId ? `${baseURL}/v2/ui/bootstrap?organizationId=${enforcedOrganizationId}` : `${baseURL}/v2/ui/bootstrap`,
         requestConfig,
       )
-        .then(r => getSuccessJsonDataOrThrowError(r))
+        .then(storeCsrfToken)
+        .then(getSuccessJsonDataOrThrowError)
         .then((bootstrapResponse: { baseURL: string; organizationId: string }) => ({
           bootstrapResponse,
           build: buildResponse,
@@ -207,6 +210,13 @@ function bootstrapApplication(constants: Constants) {
       // eslint-disable-next-line
       console.error(err);
     });
+}
+
+function storeCsrfToken(response: Response): Response {
+  if (response.headers.has(CsrfInterceptor.xsrfTokenHeaderName)) {
+    CsrfInterceptor.xsrfToken = response.headers.get(CsrfInterceptor.xsrfTokenHeaderName);
+  }
+  return response;
 }
 
 function getSuccessJsonDataOrThrowError(response: Response): Promise<any> {
