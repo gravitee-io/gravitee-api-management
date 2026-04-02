ea2ec67b4f138c78baf3aa0af5daf61aa934fd31
jourdiw <jourdi.waller@graviteesource.com>
2026-04-02 18:06:42 +0200

    feat(gamma-console): add auth store and login route components
    - Session initialization, login, and logout via management API
    - ProtectedRoute and PublicOnlyRoute using Zustand selectors
    - Facade hooks for user and auth actions

---
+++ gravitee-gamma/gravitee-gamma-control-plane-webui/src/features/auth/components/ProtectedRoute.tsx
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2026 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { Navigate, Outlet, useLocation } from 'react-router-dom';
+
+import { useUser } from '../auth.selectors';
+
+export function ProtectedRoute() {
+    const user = useUser();
+    const location = useLocation();
+
+    if (!user) {
+        const redirect = location.pathname !== '/' ? `?redirect=${encodeURIComponent(location.pathname)}` : '';
+        return <Navigate to={`/login${redirect}`} replace />;
+    }
+
+    return (
+        <div style={{ fontFamily: 'system-ui, sans-serif', padding: '2rem' }}>
+            <Outlet />
+        </div>
+    );
+}
+
+export function PublicOnlyRoute() {
+    const user = useUser();
+
+    if (user) {
+        return <Navigate to="/" replace />;
+    }
+
+    return <Outlet />;
+}
