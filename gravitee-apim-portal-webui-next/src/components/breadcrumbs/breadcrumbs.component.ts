16b62f79908754c2ad0a4155c7ca9f6425e4d45c
Ivan Kovarin <ivan.kovarin@graviteesource.com>
2026-04-02 22:15:36 +0200

    fix: handle long header labels and breadcrumbs.
    (cherry picked from commit 575ae72981d507c25835c735f37055fd5e4f906c)

--- gravitee-apim-portal-webui-next/src/components/breadcrumbs/breadcrumbs.component.ts
+++ gravitee-apim-portal-webui-next/src/components/breadcrumbs/breadcrumbs.component.ts
@@ -30,9 +30,9 @@ export interface Breadcrumb {
         <span class="breadcrumb-separator">/</span>
       }
       @if (breadcrumb.url) {
-        <a class="internal-link" [routerLink]="breadcrumb.url">{{ breadcrumb.label }}</a>
+        <a class="internal-link next-gen-small" [routerLink]="breadcrumb.url">{{ breadcrumb.label }}</a>
       } @else {
-        <span class="breadcrumb-item">{{ breadcrumb.label }}</span>
+        <span class="breadcrumb-item next-gen-small">{{ breadcrumb.label }}</span>
       }
     }
   `,
@@ -41,7 +41,13 @@ export interface Breadcrumb {
       :host {
         display: flex;
         align-items: center;
+        flex-wrap: wrap;
         gap: 15px;
+        padding: 20px 0;
+      }
+
+      .breadcrumb-item {
+        overflow-wrap: anywhere;
       }
     `,
   ],
