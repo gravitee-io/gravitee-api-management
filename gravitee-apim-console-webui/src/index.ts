228026e55dd1233fd94c1f63930ee704074d7d3d
Julien Giovaresco <dev@giovaresco.fr>
2026-04-03 17:49:22 +0200

    Revert "fix: send cookies with fetch requests to fix CSRF token mismatch"
    This reverts commit 1211bd960a8af0c4559d892f8030b68232687458.

--- gravitee-apim-console-webui/src/index.ts
+++ gravitee-apim-console-webui/src/index.ts
@@ -27,11 +27,9 @@ import { Build, Constants, DefaultPortal } from './entities/Constants';
 import { getFeatureInfoData } from './shared/components/gio-license/gio-license-data';
 import { ConsoleCustomization } from './entities/management-api-v2/consoleCustomization';
 import { environment } from './environments/environment';
-import { CsrfInterceptor } from './shared/interceptors/csrf.interceptor';
 
 const requestConfig: RequestInit = {
   headers: { 'Cache-Control': 'no-cache', Pragma: 'no-cache' },
-  credentials: 'include',
 };
 
 // fix angular-schema-form angular<1.7
@@ -67,7 +65,6 @@ function fetchData(): Promise<{ constants: Constants; build: any }> {
         enforcedOrganizationId ? `${baseURL}/v2/ui/bootstrap?organizationId=${enforcedOrganizationId}` : `${baseURL}/v2/ui/bootstrap`,
         requestConfig,
       )
-        .then((r) => storeCsrfToken(r))
         .then((r) => getSuccessJsonDataOrThrowError(r))
         .then((bootstrapResponse: { baseURL: string; organizationId: string }) => ({
           bootstrapResponse,
@@ -214,13 +211,6 @@ function bootstrapApplication(constants: Constants) {
       // eslint-disable-next-line
       console.error(err);
     });
-}
-
-function storeCsrfToken(response: Response): Response {
-  if (response.headers.has(CsrfInterceptor.xsrfTokenHeaderName)) {
-    CsrfInterceptor.xsrfToken = response.headers.get(CsrfInterceptor.xsrfTokenHeaderName);
-  }
-  return response;
 }
 
 function getSuccessJsonDataOrThrowError(response: Response): Promise<any> {
