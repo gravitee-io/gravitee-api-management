36c207f9b855217d1b3e5a836255152584236531
Michel Barret <michel.barret@graviteesource.com>
2026-04-13 09:42:00 +0200

    feat: repository API

---
+++ gravitee-apim-repository/gravitee-apim-repository-api/src/main/java/io/gravitee/repository/management/api/CatalogItemRepository.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright © 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.repository.management.api;
+
+import io.gravitee.common.data.domain.Page;
+import io.gravitee.repository.exceptions.TechnicalException;
+import io.gravitee.repository.management.api.search.Pageable;
+import io.gravitee.repository.management.model.catalog.CatalogItem;
+import java.util.List;
+import java.util.Optional;
+
+public interface CatalogItemRepository {
+    CatalogItem create(CatalogItem item) throws TechnicalException;
+
+    Optional<CatalogItem> findById(String id) throws TechnicalException;
+
+    void delete(String id) throws TechnicalException;
+
+    <T extends CatalogItem> Page<T> findByType(Class<T> type, Pageable pageable) throws TechnicalException;
+
+    List<CatalogItem> findBySourceId(String sourceId) throws TechnicalException;
+
+    Page<CatalogItem> findAll(Pageable pageable) throws TechnicalException;
+}
