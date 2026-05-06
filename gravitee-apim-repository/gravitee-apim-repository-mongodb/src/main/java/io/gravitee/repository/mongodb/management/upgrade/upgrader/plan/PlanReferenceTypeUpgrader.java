cd315f47fd81001ac5f71bc8a40bcaab4daa6b33
Florent CHAMFROY <florent.chamfroy@graviteesource.com>
2026-05-06 07:49:42 +0200

    perf(mongodb): use updateMany pipeline in PlanReferenceTypeUpgrader
    Replace the find + bulkWrite loop, which loaded every matching plan
    into JVM heap before issuing a single bulk write, with one server-side
    updateMany using an aggregation pipeline that copies api -> referenceId
    and sets referenceType to API. Avoids OOM risk on large tenants and
    removes per-document network roundtrips.
    
    https://gravitee.atlassian.net/browse/APIM-13985
    (cherry picked from commit f1dedadc277c7f5712e735cfc08a879bb48c5ce9)

--- gravitee-apim-repository/gravitee-apim-repository-mongodb/src/main/java/io/gravitee/repository/mongodb/management/upgrade/upgrader/plan/PlanReferenceTypeUpgrader.java
+++ gravitee-apim-repository/gravitee-apim-repository-mongodb/src/main/java/io/gravitee/repository/mongodb/management/upgrade/upgrader/plan/PlanReferenceTypeUpgrader.java
@@ -16,11 +16,9 @@
 package io.gravitee.repository.mongodb.management.upgrade.upgrader.plan;
 
 import com.mongodb.client.model.Filters;
-import com.mongodb.client.model.Projections;
-import com.mongodb.client.model.UpdateManyModel;
-import com.mongodb.client.model.Updates;
+import io.gravitee.node.api.upgrader.UpgraderException;
 import io.gravitee.repository.mongodb.management.upgrade.upgrader.common.MongoUpgrader;
-import java.util.ArrayList;
+import java.util.List;
 import lombok.CustomLog;
 import org.bson.Document;
 import org.springframework.stereotype.Component;
@@ -37,47 +35,23 @@ public class PlanReferenceTypeUpgrader extends MongoUpgrader {
     }
 
     @Override
-    public boolean upgrade() {
-        try {
+    public boolean upgrade() throws UpgraderException {
+        return this.wrapException(() -> {
             log.debug("Starting plan reference type upgrader");
-            var query = Filters.and(
+
+            var filter = Filters.and(
                 Filters.exists("api", true),
                 Filters.ne("api", null),
+                Filters.ne("api", ""),
                 Filters.or(Filters.exists("referenceType", false), Filters.eq("referenceType", null))
             );
 
-            var projection = Projections.fields(Projections.include("_id", "api"));
+            var pipeline = List.of(new Document("$set", new Document("referenceType", "API").append("referenceId", "$api")));
 
-            var bulkActions = new ArrayList<UpdateManyModel<Document>>();
-
-            this.getCollection("plans")
-                .find(query)
-                .projection(projection)
-                .forEach(plan -> {
-                    String apiId = plan.getString("api");
-                    if (apiId != null && !apiId.isEmpty()) {
-                        bulkActions.add(
-                            new UpdateManyModel<>(
-                                Filters.eq("_id", plan.getString("_id")),
-                                Updates.combine(Updates.set("referenceType", "API"), Updates.set("referenceId", apiId))
-                            )
-                        );
-                    }
-                });
-
-            if (bulkActions.isEmpty()) {
-                log.debug("No plans found requiring reference type update");
-                return true;
-            }
-
-            log.debug("Updating {} plan(s) with reference type and reference id", bulkActions.size());
-            boolean acknowledged = this.getCollection("plans").bulkWrite(bulkActions).wasAcknowledged();
-            log.debug("Plan reference type upgrade completed successfully");
-            return acknowledged;
-        } catch (Exception ex) {
-            log.error("An error occurred while running the plan reference type upgrader, skipping it", ex);
-            return true;
-        }
+            var result = this.getCollection("plans").updateMany(filter, pipeline);
+            log.debug("Upgraded {} plan(s) with reference type and reference id", result.getModifiedCount());
+            return result.wasAcknowledged();
+        });
     }
 
     @Override
