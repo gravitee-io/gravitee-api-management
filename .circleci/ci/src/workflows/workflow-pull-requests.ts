c3999a8b981ed53d4372747c1431ccce8221939e
Julien Giovaresco <dev@giovaresco.fr>
2026-05-07 11:34:58 +0200

    Revert "ci: trigger jobs when only gamma modules are updated"
    This reverts commit ea971b389ff782327d313984e533e2a423377bda.

--- .circleci/ci/src/workflows/workflow-pull-requests.ts
+++ .circleci/ci/src/workflows/workflow-pull-requests.ts
@@ -795,7 +795,9 @@ function shouldBuildBackend(changedFiles: string[]): boolean {
     'gravitee-apim-reporter',
     'gravitee-apim-repository',
     'gravitee-apim-rest-api',
-    'gravitee-gamma',
+    'gravitee-gamma-module-apim',
+    'gravitee-gamma-plugin',
+    'gravitee-gamma-rest-api',
   ];
   return (
     shouldBuildAll(changedFiles) || changedFiles.some((file) => mavenProjectsIdentifiers.some((identifier) => file.includes(identifier)))
