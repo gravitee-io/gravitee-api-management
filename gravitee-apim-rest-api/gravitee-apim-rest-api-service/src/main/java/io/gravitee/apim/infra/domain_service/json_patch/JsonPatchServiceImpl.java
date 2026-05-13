a1debc2e63009efc037ce4e1078751948b6321ec
Thomas Ballivet <thomas.ballivet@graviteesource.com>
2026-05-13 17:22:27 +0200

    fix(json-patch): rename domain JsonPatchServiceImpl bean to avoid bean conflict

--- gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/apim/infra/domain_service/json_patch/JsonPatchServiceImpl.java
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/apim/infra/domain_service/json_patch/JsonPatchServiceImpl.java
@@ -26,7 +26,7 @@ import org.springframework.stereotype.Service;
 /**
  * @author GraviteeSource Team
  */
-@Service
+@Service("jsonPatchDomainServiceImpl")
 public class JsonPatchServiceImpl implements JsonPatchService {
 
     @Override
