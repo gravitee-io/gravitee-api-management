225d5c17a55a7f98890f45bf64c07896e5fef5ca
prateek.tiwari <prateek.tiwari@graviteesource.com>
2026-05-29 00:33:27 +0530

    fix(console): performance fix for audit api fetching

--- gravitee-apim-console-webui/src/management/audit/env-audit.component.ts
+++ gravitee-apim-console-webui/src/management/audit/env-audit.component.ts
@@ -64,7 +64,7 @@ export class EnvAuditComponent implements OnInit, OnDestroy {
 
   public eventsName$ = this.auditService.getAllEventsName();
 
-  public apis$ = this.apiV2Service.listAll();
+  public apis$ = this.apiV2Service.getAll();
 
   public applications$ = this.applicationService.getAll();
 
