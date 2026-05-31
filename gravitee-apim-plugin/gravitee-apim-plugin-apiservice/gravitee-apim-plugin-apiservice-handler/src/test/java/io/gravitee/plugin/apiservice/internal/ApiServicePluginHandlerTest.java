8bc1635980d872308600b6e1a74a5f2719ef4932
Florent CHAMFROY <florent.chamfroy@graviteesource.com>
2026-05-31 14:44:44 +0200

    test(plugin): remove leftover junit 4 import in apiservice-handler

--- gravitee-apim-plugin/gravitee-apim-plugin-apiservice/gravitee-apim-plugin-apiservice-handler/src/test/java/io/gravitee/plugin/apiservice/internal/ApiServicePluginHandlerTest.java
+++ gravitee-apim-plugin/gravitee-apim-plugin-apiservice/gravitee-apim-plugin-apiservice-handler/src/test/java/io/gravitee/plugin/apiservice/internal/ApiServicePluginHandlerTest.java
@@ -29,7 +29,6 @@ import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
