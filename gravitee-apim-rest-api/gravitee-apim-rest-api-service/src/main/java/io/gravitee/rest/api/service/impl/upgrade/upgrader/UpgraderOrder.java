fd9a729dec380475cd24629e376a57b23484955d
Guillaume Cusnieux <guillaume.cusnieux@graviteesource.com>
2026-06-18 15:40:16 +0200

    feat(permissions): add CATALOG scope and AI_CATALOG environment permission

--- gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/rest/api/service/impl/upgrade/upgrader/UpgraderOrder.java
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/rest/api/service/impl/upgrade/upgrader/UpgraderOrder.java
@@ -80,4 +80,5 @@ public class UpgraderOrder {
     public static final int EDGE_ROLES_UPGRADER = 955;
     public static final int GAMMA_IDENTITY_ROLES_UPGRADER = 956;
     public static final int GAMMA_AUTHZ_ROLES_UPGRADER = 957;
+    public static final int CATALOG_ROLES_UPGRADER = 958;
 }
