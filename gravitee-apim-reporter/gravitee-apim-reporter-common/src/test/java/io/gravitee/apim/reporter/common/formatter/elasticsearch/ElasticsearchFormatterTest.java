1afd6928eebbd1cd81db99f703e4eaae7c697f17
araffass <ayoub.raffass@graviteesource.com>
2026-06-24 17:30:11 +0100

    fix(reporter): add JSON escaping for warning messages in FreeMarker templates
    https://gravitee.atlassian.net/browse/APIM-14495
    (cherry picked from commit 92332565e743be678e1069ceb60f8345414ae6aa)

--- gravitee-apim-reporter/gravitee-apim-reporter-common/src/test/java/io/gravitee/apim/reporter/common/formatter/elasticsearch/ElasticsearchFormatterTest.java
+++ gravitee-apim-reporter/gravitee-apim-reporter-common/src/test/java/io/gravitee/apim/reporter/common/formatter/elasticsearch/ElasticsearchFormatterTest.java
@@ -53,6 +53,7 @@ class ElasticsearchFormatterTest extends AbstractFormatterTest {
             "v4 metrics with null transaction id, v4.metric.Metrics, v4/metrics-with-null-transaction-id.json, elasticsearch/v4/metrics-with-null-transaction-id.json",
             "message metrics, v4.metric.MessageMetrics, v4/message-metrics.json, elasticsearch/v4/message-metrics.json",
             "message log, v4.log.MessageLog, v4/message-log.json, elasticsearch/v4/message-log.json",
+            "v4 metrics with warnings, v4.metric.Metrics, v4/metrics-with-warnings.json, elasticsearch/v4/metrics-with-warnings.json",
             "v4 metrics with additional, v4.metric.Metrics, v4/metrics-with-additional.json, elasticsearch/v4/metrics-with-additional.json",
             "message metrics with additional, v4.metric.MessageMetrics, v4/message-metrics-with-additional.json, elasticsearch/v4/message-metrics-with-additional.json",
             "api event metrics, v4.metric.event.ApiEventMetrics, v4/api-event-metrics.json, elasticsearch/v4/api-event-metrics.json",
