d92f5d4c196f26ddf1f63f7f0b975bedaf727322
vikrant <vikrant.kumar@graviteesource.com>
2026-06-25 15:13:46 +0530

    revert(gamma-apim): widen alert and plan forms (#18186)
    This reverts commit 304a9821473e5653128b91ec5c69c46454920b3d.

--- gravitee-gamma/gravitee-gamma-module-apim/src/main/ui/features/api-products/pages/detail/plans/ApiProductPlanFormPage.tsx
+++ gravitee-gamma/gravitee-gamma-module-apim/src/main/ui/features/api-products/pages/detail/plans/ApiProductPlanFormPage.tsx
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { Button, Skeleton } from '@gravitee/graphene-core';
+import { Button, PageFocused, Skeleton } from '@gravitee/graphene-core';
 import { Navigate, useNavigate, useParams } from 'react-router-dom';
 
 import { usePlan } from '../../../../apis/hooks/usePlans';
@@ -50,7 +50,11 @@ export function ApiProductPlanFormPage() {
         if (!canCreate) {
             return <Navigate to=".." replace />;
         }
-        return <PlanFormWizard ctx={ctx} securityType={securityType as PlanSecurityType} referenceTags={product?.tags} />;
+        return (
+            <PageFocused>
+                <PlanFormWizard ctx={ctx} securityType={securityType as PlanSecurityType} referenceTags={product?.tags} />
+            </PageFocused>
+        );
     }
 
     return <PlanEditWrapper ctx={ctx} planId={planId ?? ''} canUpdate={canUpdate} referenceTags={product?.tags} />;
@@ -86,13 +90,15 @@ function PlanEditWrapper({
     }
 
     return (
-        <PlanFormWizard
-            ctx={ctx}
-            securityType={plan.security.type}
-            planId={planId}
-            readOnly={plan.status === 'CLOSED' || !canUpdate}
-            securityLocked={plan.status !== 'STAGING' && plan.status !== 'CLOSED'}
-            referenceTags={referenceTags}
-        />
+        <PageFocused>
+            <PlanFormWizard
+                ctx={ctx}
+                securityType={plan.security.type}
+                planId={planId}
+                readOnly={plan.status === 'CLOSED' || !canUpdate}
+                securityLocked={plan.status !== 'STAGING' && plan.status !== 'CLOSED'}
+                referenceTags={referenceTags}
+            />
+        </PageFocused>
     );
 }
