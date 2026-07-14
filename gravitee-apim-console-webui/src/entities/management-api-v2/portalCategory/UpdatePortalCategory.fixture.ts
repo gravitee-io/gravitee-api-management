42f21de5e6ed8ce436d766f3cdf4912cc40916e9
Marek Mazur <marek.mazur@graviteesource.com>
2026-07-14 17:10:41 +0200

    feat(console): add portal category entities and API service
    https://gravitee.atlassian.net/browse/PORTAL-16

---
+++ gravitee-apim-console-webui/src/entities/management-api-v2/portalCategory/UpdatePortalCategory.fixture.ts
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { isFunction } from 'lodash';
+
+import { UpdatePortalCategory } from './UpdatePortalCategory';
+
+export function fakeUpdatePortalCategory(
+  modifier?: Partial<UpdatePortalCategory> | ((base: UpdatePortalCategory) => UpdatePortalCategory),
+): UpdatePortalCategory {
+  const base: UpdatePortalCategory = {
+    title: 'Portal category',
+    description: 'Description of the portal category',
+    visible: true,
+  };
+
+  if (isFunction(modifier)) {
+    return modifier(base);
+  }
+
+  return {
+    ...base,
+    ...modifier,
+  };
+}
