ee9e5759d39b9614f65b864a3f6d2fe6569aa69f
Jaroslaw Lakomy <jaroslaw.lakomy@graviteesource.com>
2026-07-30 07:28:12 +0200

    fix(console): include inactive plans in subscription filter
    https://gravitee.atlassian.net/browse/APIM-14795
    (cherry picked from commit fd137282c85a66bb80573c1ddd3b965bdcc4f293)

--- gravitee-apim-console-webui/src/management/api/subscriptions/list/api-subscription-list.component.ts
+++ gravitee-apim-console-webui/src/management/api/subscriptions/list/api-subscription-list.component.ts
@@ -132,7 +132,17 @@ export class ApiSubscriptionListComponent implements OnInit, OnDestroy {
             !this.isKubernetesOrigin &&
             this.api.definitionVersion !== 'V1';
         }),
-        switchMap(() => this.apiPlanService.list(this.activatedRoute.snapshot.params.apiId, null, null, null, undefined, 1, 9999)),
+        switchMap(() =>
+          this.apiPlanService.list(
+            this.activatedRoute.snapshot.params.apiId,
+            undefined,
+            ['PUBLISHED', 'DEPRECATED', 'CLOSED'],
+            undefined,
+            undefined,
+            1,
+            9999,
+          ),
+        ),
         tap(plansResponse => (this.plans = plansResponse.data.filter(plan => plan.security?.type !== 'KEY_LESS'))),
         catchError(error => {
           this.snackBarService.error(error.message);
@@ -256,7 +266,7 @@ export class ApiSubscriptionListComponent implements OnInit, OnDestroy {
         data: {
           isFederatedApi: this.api.definitionVersion === 'FEDERATED',
           availableSubscriptionEntrypoints: this.getApiSubscriptionEntrypoints(this.api),
-          plans: this.plans.filter(plan => plan.status),
+          plans: this.plans.filter(plan => plan.status === 'PUBLISHED'),
         },
       })
       .afterClosed()
