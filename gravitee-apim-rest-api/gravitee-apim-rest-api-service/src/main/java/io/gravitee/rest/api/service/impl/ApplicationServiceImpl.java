06ef7d5ad0f4bfb0c75db23d12dbfe948ff08dfb
ankita-gupta21 <ankita.gupta@graviteesource.com>
2026-08-10 12:50:32 +0530

    fix: avoid N+1 certificate lookup when listing applications
    (cherry picked from commit 894b5161b6a4b4447cd0d69058c3dccf0f43c090)
    
    # Conflicts:
    #	gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/test/java/io/gravitee/rest/api/service/impl/ApplicationServiceImplTest.java

--- gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/rest/api/service/impl/ApplicationServiceImpl.java
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/rest/api/service/impl/ApplicationServiceImpl.java
@@ -575,7 +575,7 @@ public class ApplicationServiceImpl extends AbstractService implements Applicati
                 notificationConfigEntity.setConfig(userEntity.getEmail());
                 genericNotificationConfigService.create(notificationConfigEntity);
             }
-            return convert(executionContext, createdApplication, userEntity);
+            return convert(executionContext, createdApplication, userEntity, true);
         } catch (TechnicalException ex) {
             throw new TechnicalManagementException(
                 "An error occurs while trying create " +
@@ -699,7 +699,7 @@ public class ApplicationServiceImpl extends AbstractService implements Applicati
             );
 
             updateActiveSubscriptions(executionContext, applicationId, application);
-            return convertApplication(executionContext, Collections.singleton(updatedApplication)).iterator().next();
+            return convertApplication(executionContext, Collections.singleton(updatedApplication), true).iterator().next();
         } catch (TechnicalException ex) {
             throw new TechnicalManagementException(
                 String.format("An error occurs while trying to update application %s", applicationId),
@@ -990,7 +990,7 @@ public class ApplicationServiceImpl extends AbstractService implements Applicati
                 updatedApplication
             );
 
-            return convertApplication(executionContext, Collections.singleton(updatedApplication)).iterator().next();
+            return convertApplication(executionContext, Collections.singleton(updatedApplication), true).iterator().next();
         } catch (TechnicalException ex) {
             String error = String.format(
                 "An error occurs while trying to update application %s with apiKeyMode %s",
@@ -1094,7 +1094,7 @@ public class ApplicationServiceImpl extends AbstractService implements Applicati
                     updatedApplication
                 );
 
-                return convertApplication(executionContext, Collections.singleton(updatedApplication)).iterator().next();
+                return convertApplication(executionContext, Collections.singleton(updatedApplication), true).iterator().next();
             }
 
             throw new ApplicationRenewClientSecretException(applicationToUpdate.getName());
@@ -1162,7 +1162,7 @@ public class ApplicationServiceImpl extends AbstractService implements Applicati
             // Audit
             createAuditLog(executionContext, APPLICATION_RESTORED, restoredApplication.getUpdatedAt(), application, restoredApplication);
 
-            return convert(executionContext, restoredApplication, userEntity);
+            return convert(executionContext, restoredApplication, userEntity, true);
         } catch (TechnicalException ex) {
             throw new TechnicalManagementException(String.format("An error occurs while trying to restore %s", applicationId), ex);
         }
@@ -1250,7 +1250,11 @@ public class ApplicationServiceImpl extends AbstractService implements Applicati
         }
     }
 
-    private Set<ApplicationEntity> convertApplication(ExecutionContext executionContext, Collection<Application> applications) {
+    private Set<ApplicationEntity> convertApplication(
+        ExecutionContext executionContext,
+        Collection<Application> applications,
+        boolean fetchCertificate
+    ) {
         if (applications == null || applications.isEmpty()) {
             return Collections.emptySet();
         }
@@ -1303,7 +1307,12 @@ public class ApplicationServiceImpl extends AbstractService implements Applicati
         return applications
             .stream()
             .map(publicApplication ->
-                convert(executionContext, publicApplication, userIdToUserEntity.get(applicationToUser.get(publicApplication.getId())))
+                convert(
+                    executionContext,
+                    publicApplication,
+                    userIdToUserEntity.get(applicationToUser.get(publicApplication.getId())),
+                    fetchCertificate
+                )
             )
             .collect(Collectors.toCollection(LinkedHashSet::new));
     }
@@ -1351,7 +1360,8 @@ public class ApplicationServiceImpl extends AbstractService implements Applicati
     }
 
     private Set<ApplicationListItem> convertToList(final ExecutionContext executionContext, Collection<Application> applications) {
-        Set<ApplicationEntity> entities = convertApplication(executionContext, applications);
+        // fetchCertificate=false: certificates are resolved in bulk below, resolving them per-application here would be N+1 and wasted work
+        Set<ApplicationEntity> entities = convertApplication(executionContext, applications, false);
 
         Set<ApplicationListItem> apps = entities
             .stream()
@@ -1412,7 +1422,12 @@ public class ApplicationServiceImpl extends AbstractService implements Applicati
         return apps;
     }
 
-    private ApplicationEntity convert(final ExecutionContext executionContext, Application application, UserEntity primaryOwner) {
+    private ApplicationEntity convert(
+        final ExecutionContext executionContext,
+        Application application,
+        UserEntity primaryOwner,
+        boolean fetchCertificate
+    ) {
         if (primaryOwner == null) {
             // add a default unknown user
             primaryOwner = new UserEntity();
@@ -1440,7 +1455,7 @@ public class ApplicationServiceImpl extends AbstractService implements Applicati
         applicationEntity.setUpdatedAt(application.getUpdatedAt());
         applicationEntity.setPrimaryOwner(new PrimaryOwnerEntity(primaryOwner));
 
-        applicationEntity.setSettings(getSettings(executionContext, application, true));
+        applicationEntity.setSettings(getSettings(executionContext, application, fetchCertificate));
         applicationEntity.setDisableMembershipNotifications(application.isDisableMembershipNotifications());
         if (application.getApiKeyMode() != null) {
             applicationEntity.setApiKeyMode(ApiKeyMode.valueOf(application.getApiKeyMode().name()));
@@ -1749,9 +1764,9 @@ public class ApplicationServiceImpl extends AbstractService implements Applicati
             if (!ApplicationStatus.ARCHIVED.equals(application.getStatus())) {
                 log.error("The Application {} doesn't have any primary owner.", application.getId());
             }
-            return convert(executionContext, application, null);
+            return convert(executionContext, application, null, true);
         }
 
-        return convert(executionContext, application, userService.findById(executionContext, primaryOwnerMemberEntity.getMemberId()));
+        return convert(executionContext, application, userService.findById(executionContext, primaryOwnerMemberEntity.getMemberId()), true);
     }
 }
