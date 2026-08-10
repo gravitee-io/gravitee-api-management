c42da4596f9072c2f301ce991cc3e15b4d4c04a7
Konrad Król <konrad.krol@incubly.com>
2026-08-10 22:25:23 +0200

    feat(portal-next): reject API-attached documentation outside TOP_NAVBAR

--- gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/apim/core/portal_page/exception/InvalidPortalNavigationItemDataException.java
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/apim/core/portal_page/exception/InvalidPortalNavigationItemDataException.java
@@ -41,6 +41,10 @@ public class InvalidPortalNavigationItemDataException extends ValidationDomainEx
         return new InvalidPortalNavigationItemDataException("API Product items can only be added to TOP_NAVBAR area.");
     }
 
+    public static InvalidPortalNavigationItemDataException apiDocumentationMustBeInTopNavbar() {
+        return new InvalidPortalNavigationItemDataException("API-attached documentation can only be added to TOP_NAVBAR area.");
+    }
+
     public static InvalidPortalNavigationItemDataException sourceNotAllowedForType(String type) {
         return new InvalidPortalNavigationItemDataException(
             "An external source can only be configured on PAGE and FOLDER navigation items (got %s).".formatted(type)
