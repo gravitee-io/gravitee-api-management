7d92a8e8c390866a43c33be277592338e771881d
Jaroslaw Lakomy <jaroslaw.lakomy@graviteesource.com>
2026-08-17 12:37:19 +0200

    fix: hide subscribe action for APIs nested in API products
    https://gravitee.atlassian.net/browse/PORTAL-128

--- gravitee-apim-portal-webui-next/src/app/documentation/components/documentation-folder/documentation-folder.component.ts
+++ gravitee-apim-portal-webui-next/src/app/documentation/components/documentation-folder/documentation-folder.component.ts
@@ -39,7 +39,7 @@ import { ApiService } from '../../../../services/api.service';
 import { CurrentUserService } from '../../../../services/current-user.service';
 import { PortalNavigationItemsService } from '../../../../services/portal-navigation-items.service';
 import { ApiTabToolsComponent } from '../../../api/api-details/api-tab-tools/api-tab-tools.component';
-import { DocumentationSubscriptionTarget, TreeNode, TreeService } from '../../services/tree.service';
+import { DocumentationActionContext, TreeNode, TreeService } from '../../services/tree.service';
 
 interface FolderData {
   children: PortalNavigationItem[];
@@ -85,17 +85,16 @@ export class DocumentationFolderComponent {
   tree = signal<TreeNode[]>([]);
   breadcrumbs = signal<Breadcrumb[]>([]);
 
-  subscriptionTarget = signal<DocumentationSubscriptionTarget | null>(null);
-  apiId = computed(() => {
-    const target = this.subscriptionTarget();
-    return target?.type === 'API' ? target.apiId : null;
-  });
+  documentationActionContext = signal<DocumentationActionContext>({ apiId: null, subscriptionTarget: null });
+  mcpDrawerOpen = signal(false);
+  subscriptionTarget = computed(() => this.documentationActionContext().subscriptionTarget);
+  apiId = computed(() => this.documentationActionContext().apiId);
   api = rxResource<Api | null, string | null>({
     params: this.apiId,
     stream: ({ params }) => (params ? this.apiService.details(params) : of(null)),
   });
   apiHasMcp = computed(() => !this.api.error() && !!this.api.value()?.mcp);
-  mcpDrawerOpen = signal(false);
+  hasBreadcrumbActions = computed(() => !!this.subscriptionTarget() || this.apiHasMcp());
 
   constructor(
     private readonly router: Router,
@@ -156,7 +155,7 @@ export class DocumentationFolderComponent {
   }
 
   private loadContentOrRedirect(selectedId: string, children = this.folderData()?.children ?? []): Observable<FolderData> {
-    this.subscriptionTarget.set(null);
+    this.documentationActionContext.set({ apiId: null, subscriptionTarget: null });
 
     if (!selectedId) {
       return of({ children, selectedPageContent: null }).pipe(
@@ -176,10 +175,10 @@ export class DocumentationFolderComponent {
       return of({ children, selectedPageContent: null }).pipe(tap(() => firstPageId && this.navigateToPage(firstPageId)));
     }
 
-    const subscriptionTarget = this.treeService.getSubscriptionTarget(selectedId);
+    const documentationActionContext = this.treeService.getDocumentationActionContext(selectedId);
     return this.itemsService.getNavigationItemContent(selectedId).pipe(
       tap(() => this.breadcrumbs.set(this.treeService.getBreadcrumbsByNodeId(selectedId))),
-      tap(() => this.subscriptionTarget.set(subscriptionTarget)),
+      tap(() => this.documentationActionContext.set(documentationActionContext)),
       map(selectedPageContent => ({ children, selectedPageContent })),
     );
   }
