64d7d83d4042b2a06fe41ce7ed933d4c96fd6c89
aqibmohammadkhan <aqib.khan@graviteesource.com>
2026-09-03 18:44:45 +0100

    fix: API promotion multi env not working
    (cherry picked from commit f7d1a90687a5ee26dff2aaffd50aac4c6afa8b59)
    
    # Conflicts:
    #	gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/rest/api/service/impl/promotion/PromotionServiceImpl.java
    #	gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/test/java/io/gravitee/apim/core/api/domain_service/import_definition/ImportDefinitionUpdateDomainServiceTest.java
    #	gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/test/java/io/gravitee/rest/api/service/impl/promotion/PromotionServiceTest.java

--- gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/rest/api/service/impl/promotion/PromotionServiceImpl.java
+++ gravitee-apim-rest-api/gravitee-apim-rest-api-service/src/main/java/io/gravitee/rest/api/service/impl/promotion/PromotionServiceImpl.java
@@ -236,7 +236,17 @@ public class PromotionServiceImpl extends AbstractService implements PromotionSe
             Promotion createdOrUpdatedPromotion;
 
             if (existingPromotion.isPresent()) {
+<<<<<<< HEAD
                 LOGGER.debug("Updating existing promotion: {}", promotion.getId());
+=======
+                log.debug("Updating existing promotion: {}", promotion.getId());
+                // The Cockpit round-trip may echo a promotion without its targetApiId (e.g. the source environment
+                // replaying a promotion request). Never lose the link to the API already promoted: it is what lets
+                // the next promotion update that API in place when the crossId lookup misses.
+                if (promotion.getTargetApiId() == null && existingPromotion.get().getTargetApiId() != null) {
+                    promotion.setTargetApiId(existingPromotion.get().getTargetApiId());
+                }
+>>>>>>> f7d1a90 (fix: API promotion multi env not working)
                 createdOrUpdatedPromotion = promotionRepository.update(promotion);
             } else {
                 LOGGER.debug("Creating promotion: {}", promotion.getId());
@@ -438,6 +448,7 @@ public class PromotionServiceImpl extends AbstractService implements PromotionSe
         promotion.setApiDefinition(promotionEntity.getApiDefinition());
         promotion.setStatus(convert(promotionEntity.getStatus()));
         promotion.setAuthor(promotionAuthor);
+        promotion.setTargetApiId(promotionEntity.getTargetApiId());
 
         return promotion;
     }
