69520ec2afe3fdc3fd5de2af1be49fff526f5e2c
Konrad Król <konrad.krol@incubly.com>
2026-09-15 14:25:30 +0200

    feat(console-dictionary): encrypt and mask dictionary property values

---
+++ gravitee-apim-console-webui/src/services/dictionary.service.spec.ts
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import DictionaryService from './dictionary.service';
+
+describe('DictionaryService', () => {
+  let $http: any;
+  let service: DictionaryService;
+
+  const Constants = { env: { baseURL: 'http://host/management/environments/DEFAULT' } };
+
+  beforeEach(() => {
+    $http = { put: jest.fn() };
+    service = new DictionaryService($http, Constants);
+  });
+
+  describe('update', () => {
+    it('should send propertyOptions alongside properties', () => {
+      service.update({
+        id: 'dic-1',
+        name: 'My dictionary',
+        type: 'MANUAL',
+        properties: { url: 'https://backend', apiKey: 's3cr3t' },
+        propertyOptions: { apiKey: { encryptable: true } },
+      });
+
+      expect($http.put).toHaveBeenCalledWith(
+        'http://host/management/environments/DEFAULT/configuration/dictionaries/dic-1',
+        expect.objectContaining({
+          properties: { url: 'https://backend', apiKey: 's3cr3t' },
+          propertyOptions: { apiKey: { encryptable: true } },
+        }),
+      );
+    });
+
+    it('should omit propertyOptions when the dictionary carries none', () => {
+      service.update({
+        id: 'dic-1',
+        name: 'My dictionary',
+        type: 'MANUAL',
+        properties: { url: 'https://backend' },
+      });
+
+      expect($http.put.mock.calls[0][1].propertyOptions).toBeUndefined();
+    });
+  });
+});
